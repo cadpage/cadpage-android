a91220247e858a5063bea4af9a72404d0e541702
dibbler <dibbler@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-09-06 02:37:43 +0000

    Resolved issues 2 and 3. Added check for our calls and abortBroadcast

--- cadpage/src/net/anei/cadpage/SmsReceiver.java
+++ cadpage/src/net/anei/cadpage/SmsReceiver.java
@@ -3,6 +3,8 @@ package net.anei.cadpage;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 
 public class SmsReceiver extends BroadcastReceiver {
 
@@ -16,6 +18,26 @@ public class SmsReceiver extends BroadcastReceiver {
      * This service will process the activity and show the popup (+ play notifications)
      * after it's work is done the service will be stopped.
      */
+    if (CadPageCall(context,intent)==true){
     SmsReceiverService.beginStartingService(context, intent);
+    }
+  }
+  // We need to determine if this message is for us. If so then stop further alerting to the default sms app.
+  public boolean CadPageCall(Context context,Intent intent){
+	    Bundle bundle = intent.getExtras();
+	    if (bundle != null) {
+	      SmsMessage[] messages = SmsPopupUtils.getMessagesFromIntent(intent);
+	      SmsMmsMessage message = new SmsMmsMessage(context, messages,System.currentTimeMillis() );
+	      String strMessage = message.getMessageFull();
+	      if (strMessage.contains("Call:")==true){
+	    	  this.abortBroadcast();
+	    	  return true;
+	      } else if (strMessage.contains("TYPE:")==true){
+	    	  this.abortBroadcast();
+	    	  return true;
+	      }
+	    }
+	    return false;
   }
 }
+
