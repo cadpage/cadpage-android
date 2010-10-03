43f3220b6a02dc2547166beec63a957fa1cab5b8
dibbler <dibbler@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-10-03 03:36:40 +0000

    V 1.1.14 tag

---
+++ cadpage/src/net/anei/cadpage/CadPageApplication.java
@@ -0,0 +1,21 @@
+package net.anei.cadpage;
+
+import android.app.Application;
+
+/**
+ * Main CadPage application
+ * which is where we need to do our one time initialization
+ */
+public class CadPageApplication extends Application {
+
+  /* (non-Javadoc)
+   * @see android.app.Application#onCreate()
+   */
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    
+    TopExceptionHandler.enable(this);
+    ManagePreferences.setupPreferences(this);
+  }
+}
