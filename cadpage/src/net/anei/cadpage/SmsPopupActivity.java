afd60cf158d563db8bb5eead7e86427f5b9c06a6
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-10-25 03:59:33 +0000

    Added a safety check against the unlikely event that someone is sending a rouge intent to SmsPopupActivity.

--- cadpage/src/net/anei/cadpage/SmsPopupActivity.java
+++ cadpage/src/net/anei/cadpage/SmsPopupActivity.java
@@ -377,6 +377,10 @@ public class SmsPopupActivity extends Activity {
     int msgId = intent.getIntExtra(EXTRAS_MSG_ID, 0);
     SmsMmsMessage msg = msgQueue.getMessage(msgId);
     
+    // This shouldn't be possible, unless someone other than SmsReceiver is
+    // sending rouge intents to us.  But we had better catch it, just in case
+    if (msg == null) finish();
+    
     // Flag message read
     msg.setRead(true);
     msgQueue.notifyDataChange();
