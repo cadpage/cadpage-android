78eeb177e52ca93e6884c853e51aa4c916e712c1
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-11-11 16:22:32 +0000

    Finished implementation of Longview TX location code

--- cadpage/src/net/anei/cadpage/parsers/TXLongviewParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TXLongviewParser.java
@@ -1,6 +1,6 @@
 package net.anei.cadpage.parsers;
 
-import java.util.Properties;
+import java.util.regex.Pattern;
 
 import net.anei.cadpage.SmsMsgInfo.Data;
 
@@ -12,12 +12,17 @@ RC:Run# 29907/PR Event/Public Service/CRYSTAL FARMS RD///TATUM/19:15/////PR EVEN
 RC:Run# 29793/*Non Life Emergency P/1/Falls/301 B FORREST ACRES///TATUM/22:18/////<Unknown>/////
 RC:Run# 29764/*Non Life Emergency P/1/Overdose / Poisoning (Ingestion)/1100 S ADAMS ST//Panola Apts: 32/CARTHAGE/17:23/////<Unknown>/////
 
+We have no examples of cross street or intersections, so we are probably handling them badly.
+
  */
 
 public class TXLongviewParser extends SmartAddressParser {
   
-  private static final String DEF_STATE = "MD";
+  private static final String DEF_STATE = "TX";
   private static final String DEF_CITY = "LONGVIEW";
+  
+  private static final Pattern SPLIT_PATTERN = Pattern.compile(" */+ *");
+  private static final Pattern TIME_PATTERN = Pattern.compile("\\d\\d:\\d\\d");
   
   public TXLongviewParser() {
     super(DEF_STATE);
@@ -31,19 +36,59 @@ public class TXLongviewParser extends SmartAddressParser {
   @Override
   protected void parse(String body, Data data) {
 
-    data.defState=DEF_CITY;
-    data.defCity=DEF_STATE;
-    // Skip everything up to first colon
-    int ipt = body.indexOf(':');
-    if (ipt >= 0) body = body.substring(ipt+1).trim();
-
-    // Replace # with :
-    body = body.replace("Run#", "Call:");
-
-    Properties props = parseMessage(body, new String[]{"CALL","CITY"});
-    // Now try to find call description and address
-    parseAddress(StartType.START_CALL,  body, data);
-    body = getLeft();
+    data.defCity=DEF_CITY;
+    data.defState=DEF_STATE;
     
+    String[] flds = SPLIT_PATTERN.split(body);
+    
+    // Extract call ID from first field
+    String fld = flds[0];
+    int pt = fld.lastIndexOf(' ');
+    if (pt >= 0) data.strCallId = fld.substring(pt+1);
+    
+    // Fields are tightly delimited but very loosely positioned
+    // Only consistent field we can identify is a time marker, so lets find it
+    int timeLine = -1;
+    for (int ndx = 0; ndx < flds.length; ndx++) {
+      if (TIME_PATTERN.matcher(flds[ndx]).matches()) {
+        timeLine = ndx;
+        break;
+      }
+    }
+    if (timeLine == -1) return;
+    
+    // Field in front of time marker is always the city
+    int ndx = timeLine-1;
+    if (ndx < 0) return;
+    data.strCity = flds[ndx];
+    
+    // Field in front of that is usually the address, but may contain an apartment number
+    if (--ndx < 0) return;
+    fld = flds[ndx];
+    if (fld.toUpperCase().contains("APT")) {
+      pt = fld.lastIndexOf(':');
+      if (pt < 0) pt = fld.lastIndexOf(' ');
+      if (pt >= 0) {
+        data.strApt = fld.substring(pt+1).trim();
+        if (--ndx < 0) return;
+      }
+    }
+    
+    // Next field has to be the address
+    data.strAddress = flds[ndx];
+    
+    // Construct the call description with everything from field 2 up to the
+    // address field
+    for (pt = 1; pt<ndx; pt++) {
+      if (data.strCall.length() > 0) data.strCall += "/";
+      data.strCall += flds[pt];
+    }
+    
+    // Construct extra information field from everything behind the time marker
+    for (ndx = timeLine+1; ndx<flds.length; ndx++) {
+      if (flds[ndx].equalsIgnoreCase("<Unknown>")) continue;
+      if (data.strSupp.length() > 0) data.strSupp += "/";
+      data.strSupp += flds[ndx];
+    }
   }
 }
