64b69e7adb725c5cdc3fc8ed5c0ad0b9b12c188d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-11-14 16:36:21 +0000

    Added municipality code converstion to Berks County parser

--- cadpage/src/net/anei/cadpage/parsers/PABerksCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PABerksCountyParser.java
@@ -21,7 +21,7 @@ public class PABerksCountyParser extends SmsMsgParser {
   private static final String DEF_STATE = "PA";
   private static final String DEF_CITY = "BERKS COUNTY";
   
-  private static final Pattern DIGITS = Pattern.compile(" \\d{4} ");
+  private static final Pattern MUNI_CODE_PAT = Pattern.compile(" 00\\d\\d ");
 
   @Override
   public boolean isPageMsg(String body) {
@@ -39,8 +39,17 @@ public class PABerksCountyParser extends SmsMsgParser {
     body = body.substring(20);
     
     // Look for a 4 digit station ID, this marks the end of the address
-    Matcher match = DIGITS.matcher(body);
+    Matcher match = MUNI_CODE_PAT.matcher(body);
     if (! match.find()) return;
+    
+    String muniCode = body.substring(match.start()+1, match.end()-1);
+    int iMuniCode = Integer.parseInt(muniCode);
+    iMuniCode -= FIRST_MUNI_CODE;
+    if (iMuniCode >= 0 && iMuniCode < MUNI_CODES.length && MUNI_CODES[iMuniCode] != null) {
+      data.strCity = MUNI_CODES[iMuniCode];
+    } else {
+      data.strCity = muniCode;
+    }
     
     // An '@' splits place name from address
     String address = body.substring(0, match.start()).trim();
@@ -55,4 +64,90 @@ public class PABerksCountyParser extends SmsMsgParser {
     // Anything beyond that is supplemental info
     data.strSupp = body.replaceAll("//+", "/");
   }
+  
+  private static final int FIRST_MUNI_CODE = 19;
+  private static final String[] MUNI_CODES = new String[]{
+    /*19*/  "READING",
+    /*20*/  null,
+    /*21*/  "MOUNT PENN",
+    /*22*/  "ST LAWRENCE",
+    /*23*/  "LOWER ALSACE",
+    /*24*/  "ALSACE",
+    /*25*/  "EXETER",
+    /*26*/  "AMITY",
+    /*27*/  "DOUGLASS",
+    /*28*/  "UNION",
+    /*29*/  "BIRDSBORO",
+    /*30*/  "NEW MORGAN",
+    /*31*/  "ROBESON",
+    /*32*/  "CAERNARVON",
+    /*33*/  "BRECKNOCK",
+    /*34*/  "CUMRU",
+    /*35*/  "MOHTON",
+    /*36*/  "SHILLINGTON",
+    /*37*/  "KENHORST",
+    /*38*/  "WEST READING",
+    /*39*/  "WYOMISSING",
+    /*40*/  null,
+    /*41*/  "OLD WYOMISSING HILLS",
+    /*42*/  "OLD WEST LAWN",
+    /*43*/  "SPRING",
+    /*44*/  "SINKING SPRINGS",
+    /*45*/  "SOUTH HEIDELBERG",
+    /*46*/  "WERNERSVILLE",
+    /*47*/  "ROBESONIA",
+    /*48*/  "HEIDELBERG",
+    /*49*/  "WOMELSDORF",
+    /*50*/  null,
+    /*51*/  "MARION",
+    /*52*/  "NORTH HEIDELBERG",
+    /*53*/  "LOWER HEIDELBERG",
+    /*54*/  "BERN",
+    /*55*/  "CENTRE",
+    /*56*/  "PENN",
+    /*57*/  "BERNVILLE",
+    /*58*/  "JEFFERSON",
+    /*59*/  "UPPER BERN",
+    /*60*/  "CENTERPORT",
+    /*61*/  "TILDEN",
+    /*62*/  "UPPER TULPEHOCKEN",
+    /*63*/  "STRAUSSTOWN",
+    /*64*/  "BETHEL",
+    /*65*/  "TULPEHOCKEN",
+    /*66*/  "MUHLENBERG",
+    /*67*/  "LAURELDALE",
+    /*68*/  null,
+    /*69*/  "ONTELAUNEE",
+    /*70*/  null,
+    /*71*/  "LEESPORT",
+    /*72*/  "MADIENCREEK",
+    /*73*/  "PERRY",
+    /*74*/  "SHOEMAKERSVILE",
+    /*75*/  "WINDSOR",
+    /*76*/  "HAMBURG",
+    /*77*/  "ALBANY",
+    /*78*/  "GREENWICH",
+    /*79*/  "KUTZTOWN", /*"MAXATAWNY"*/
+    /*80*/  "LENHARTSVILLE",
+    /*81*/  "KUTZTOWN",
+    /*82*/  "LYONS",
+    /*83*/  "FLEETWOOD",
+    /*84*/  "RICHMOND",
+    /*85*/  "TOPTON",
+    /*86*/  "LONGSWAMP",
+    /*87*/  "FLEETWOOD", /*"ROCKLAND"*/
+    /*88*/  "DISTRICT",
+    /*89*/  "PIKE TOWNSJIP",
+    /*90*/  null,
+    /*91*/  "RUSCOMBMANOR",
+    /*92*/  "OLEY",
+    /*93*/  "EARL",
+    /*94*/  "BOYERTOWN",
+    /*95*/  "COLEBROOKDALE",
+    /*96*/  "BECHTELSVILLE",
+    /*97*/  "WASHINGTON",
+    /*98*/  "BALLY",
+    /*99*/  "HEREFORD"
+  };
+
 }
