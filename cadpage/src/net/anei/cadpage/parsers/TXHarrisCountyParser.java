e1fea7d9b6f2a46e1522bee6077fdf6ec4f58118
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-11-15 19:30:58 +0000

    Fixed some minor issues with Harris County parser

--- cadpage/src/net/anei/cadpage/parsers/TXHarrisCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TXHarrisCountyParser.java
@@ -2,11 +2,9 @@ package net.anei.cadpage.parsers;
 
 import java.util.Properties;
 
-import net.anei.cadpage.Log;
 import net.anei.cadpage.SmsMsgInfo.Data;
 
 /* Sample Harris Page
-11:58 W HILLSIDE DR/EASTEX FRWY, ; Map:414D Sub: Nat:MA-MUTUAL AID / ASSIST AGENCY Units:E91 T81 T73 E-E39 X-St:EASTEX
 1of2:09/06 11:56 W HILLSIDE DR/EASTEX FRWY, ; Map:414D Sub: Nat:MA-MUTUAL AID / ASSIST AGENCY Units:E91 T81 T73 E-L19 X-St:EASTEX
 1of2:09/06 11:28 19707 WOOD WALK LN, ; Map:337U Sub:PINEHURST OF ATASCOCITA Nat:09E01-ARREST - NOT BREATHING Units:E-M19 E-M29 E-7900
 1of2:09/05 08:56 19226 AQUATIC DR, ; Map:378A Sub:WALDEN ON LAKE HOUSTON Nat:52B01G-FIRE ALARM - RESIDENTIAL Units:ATFD E-E39 X-
@@ -24,7 +22,6 @@ public class TXHarrisCountyParser extends SmsMsgParser {
   @Override
   protected void parse(String body, Data data) {
   
-    Log.v("DecodeHarrisPage: Message Body of:" + body);
     data.defState="TX";
     data.defCity="HARRIS COUNTY";
     
@@ -35,9 +32,10 @@ public class TXHarrisCountyParser extends SmsMsgParser {
     Properties props = parseMessage(body, new String[]{"Map", "Sub", "Nat", "Units", "X-St"});
     parseAddress(props.getProperty("Loc", ""), data);
     data.strMap = props.getProperty("Map", "");
+    data.strPlace = props.getProperty("Sub", "");
     data.strCall = props.getProperty("Nat", "");
     data.strUnit = props.getProperty("Units", "");
-    data.strCross = props.getProperty("X-st", "");
+    data.strCross = props.getProperty("X-St", "");
     
     // Strip extra stuff off of address line
     int ipt = data.strAddress.indexOf(',');
