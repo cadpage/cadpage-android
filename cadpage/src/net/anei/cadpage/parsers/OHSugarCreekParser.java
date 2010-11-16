93a552bd63f0e0f63936d3955c56ea2b5abc6f5e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-11-16 00:04:57 +0000

    Fixed some minor problems with Sugar Creek parser

--- cadpage/src/net/anei/cadpage/parsers/OHSugarCreekParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OHSugarCreekParser.java
@@ -5,7 +5,7 @@ import net.anei.cadpage.SmsMsgInfo.Data;
 
 /***
 "Ct:EMS MINOR       Loc:4336 STATE RT 725            Apt:      XSt:BAYBERRY COVE D LAKEMAN DR      Grid:003F Units:72                                        Rmk:"
-We use Cisco Public Safety’s ECAD program to page out CFS events.  CAD does send out multiple pages per event – one for the call and each responding unit.  Below are sample texts for review:
+We use Cisco Public Safetyï¿½s ECAD program to page out CFS events.  CAD does send out multiple pages per event ï¿½ one for the call and each responding unit.  Below are sample texts for review:
 "Ct:FIRE ALARM - RE Loc:4334 KILLIAN CT              Apt:      XSt:BELFAST DR                      Grid:0001 Units:M71   B70   P219                          Rmk:"
 "Ct:EMS MINOR       Loc:I 675 N/8MM                  Apt:      XSt:WILMINGTON PIKE FEEDWIRE RD     Grid:0045 Units:72                                        Rmk:"
 "Ct:TRAFFIC ACCIDEN Loc:FEEDWIRE RD/WINSHIRE TER     Apt:      XSt:LITTLE SUGARCRE LEDGESTONE BLVD Grid:0002 Units:C70   B70   E71   M72   P228              Rmk:"
@@ -14,7 +14,7 @@ We use Cisco Public Safety’s ECAD program to page out CFS events.  CAD does send
 
 public class OHSugarCreekParser extends SmsMsgParser {
 
-  private static final String[]SugarCreekkeywords = new String[]{"Ct", "Loc", "Apt", "XSt", "Grid", "Units"};
+  private static final String[]SugarCreekkeywords = new String[]{"Ct", "Loc", "Apt", "XSt", "Grid", "Units", "Rmk"};
 
   @Override
   public boolean isPageMsg(String body) {
@@ -25,12 +25,13 @@ public class OHSugarCreekParser extends SmsMsgParser {
   protected void parse(String body, Data data) {
     data.defState = "OH";
     data.defCity = "SugarCreek";
-    body = body.replaceAll("\t", ",");
+    body = body.replaceAll("\t", " ");
     Properties props = parseMessage(body, SugarCreekkeywords);
     data.strCall = props.getProperty("Ct", "");
     parseAddress(props.getProperty("Loc", ""), data);
     data.strApt = props.getProperty("Apt", "");
     data.strCross = props.getProperty("XSt", "");
+    data.strMap = props.getProperty("Grid", "");
     data.strUnit = props.getProperty("Units", "");
     data.strBox = props.getProperty("Grid", "");
     data.strSupp = props.getProperty("Rmk","");
