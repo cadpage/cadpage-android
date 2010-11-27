6b2fd62cab548eca8ba9301677834b14984b822c
derrick@cadpage.org <derrick@cadpage.org@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-11-27 20:40:39 +0000

    updated VA Pittsylvania County Tester

--- cadpageTest/src/net/anei/cadpage/parsers/VAPittsylvaniaCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/VAPittsylvaniaCountyParserTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 
 public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
-  
+   
   public VAPittsylvaniaCountyParserTest() {
     setParser(new VAPittsylvaniaCountyParser(), "PITTSYLVANIA", "VA");
   }
@@ -14,14 +14,14 @@ public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
     doTest("T1",
     		"701:CAD:TRAFFIC ACCIDENT PI;100 MALMAISON RD/U S HIGHWAY NO 29; BLA",
     		"CALL:TRAFFIC ACCIDENT PI",
-    		"ADDR:100 MALMAISON RD/U S HIGHWAY NO 29",
+    		"ADDR:100 MALMAISON RD & U S HIGHWAY NO 29",
     		"CITY:Blairs"
     	);
     doTest("T2",
     		"700:CAD:ABDOMINAL DISTRESS; 701 E WITT RD;BLA;WITT LN;MOUNT VIEW RD;[Medical Priority Info] PROBLEM:CHEST HURTING #PATS: 1 AGE: 77 Years SEX: Male",
     		"CALL:ABDOMINAL DISTRESS",
     		"ADDR:701 E WITT RD",
-    		"CROSS:WITT LN",
+    		"X:WITT LN",
     		"CITY:Blairs",
     		"INFO:CHEST HURTING #PATS: 1 AGE: 77 Years SEX: Male"
     	);
@@ -29,7 +29,7 @@ public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
     		"699:CAD:RESPIRATORY DISTRESS;600 NORTH POINTE LN;DAN;SEMINOLE DR;[Medical Priority Info] PROBLEM: TROUBLE BREATHING #PATS: 1 AGE: 5 Months SEX",
     		"CALL:RESPIRATORY DISTRESS",
     		"ADDR:600 NORTH POINTE LN",
-    		"CROSS:SEMINOLE DR",
+    		"X:SEMINOLE DR",
     		"CITY:Danville",
     		"INFO:TROUBLE BREATHING #PATS: 1 AGE: 5 Months SEX"
     	);
@@ -38,7 +38,7 @@ public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
     		"CALL:RESPIRATORY DISTRESS",
     		"ADDR:1000 LAWLESS CREEK RD",
     		"CITY:Blairs",
-    		"CROSS:PRITCHETT LN",
+    		"X:PRITCHETT LN",
     		"INFO:DIFF BREATHING #PATS: 1 AGE: 13 Y"
     	);
     doTest("T5",
@@ -46,7 +46,7 @@ public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
     		"CALL:OTHER FIRE EMERGENCY",
     		"ADDR:7180 U S HIGHWAY NO 29",
     		"CITY:Blairs",
-    		"CROSS:DANVILLE EXPW"
+    		"X:DANVILLE EXPW"
     	);
   }
 }
