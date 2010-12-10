9e4958908ebcdcd550a54178a6143e50dd8a53e4
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-12-10 06:50:57 +0000

    Modified Madison County parser to ignore extraneious leading text

--- cadpage/src/net/anei/cadpage/parsers/INMadisonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/INMadisonCountyParser.java
@@ -12,6 +12,8 @@ Unit:AM45 UnitSts: Loc:6197 N SR 9 Venue:RICHLA TWP Inc:EMS Call Date:10/17/2010
 Unit:ST40 UnitSts: Loc:E 400N/100E Venue:RICHLA TWP Inc:Brush Fire Date:10/15/2010 Time:14:33 OPEN BURN BETWEEEN 100E AND ALEX PIKE/ S SIDE Addtl: OF ROAD
 Unit:ST40 UnitSts: Loc:3551 N SR 9 Venue:LAFAYE TWP TOM WOOD HONDA Inc:Accidnt PI Date:10/14/2010 Time:07:43 UNKNOWN 2 VEHS Addtl:
 Unit:AM45 UnitSts: Loc:124 DARIN CT Venue:RICHLA TWP Inc:SEIZURE Date:10/29/2010 Time:19:31 18 MO/POSS SEIZURE Addtl:
+Subject:{EMAOPS} (62533) CAD Page Unit:EMA1 Loc:3727 N 200E Venue:RICHLA TWP Inc:Accidnt PI Date:12/09/2010 Time:10:31 UNKNOWN  CAR VS POL
+Subject:(62534) Dispatch\nUnit:ST50 UnitSts: Loc:207 E MAIN ST Venue:CHESTERFIE STATION 50 Inc:Fire Date:12/09/2010 Time:12:00 STAND BY YO
 */
 
 public class INMadisonCountyParser extends SmsMsgParser {
@@ -26,7 +28,9 @@ public class INMadisonCountyParser extends SmsMsgParser {
   @Override
   protected boolean parseMsg(String body, Data data) {
 
-    if (! body.startsWith("Unit:")) return false;
+    int ipt = body.indexOf("Unit:");
+    if (ipt < 0) return false;
+    body = body.substring(ipt);
     
     data.defState="IN";
     data.defCity = "MADISON COUNTY";
@@ -44,7 +48,6 @@ public class INMadisonCountyParser extends SmsMsgParser {
       if (data.strSupp.length() > 0) data.strSupp += ' ';
       data.strSupp += sAddtl;
     }
-    data.strCity = data.defCity + ", Anderson ";
     return true;
   }
 }
