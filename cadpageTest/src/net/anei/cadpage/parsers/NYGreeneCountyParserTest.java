ad471c092f2c89edc61222b0071517352038274a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-12-20 22:56:19 +0000

    More message documentation

--- cadpageTest/src/net/anei/cadpage/parsers/NYGreeneCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NYGreeneCountyParserTest.java
@@ -26,5 +26,25 @@ public class NYGreeneCountyParserTest extends BaseParserTest {
         "CITY:ATHENS",
         "ID:2010-0000033344");
     
+    doTest("T3",
+        "GREENE 911:SMOKE >SMOKE INVESTIGATION 2ND ST VENTE JOEAN Cad: 2010-0000034969",
+        "CALL:SMOKE INVESTIGATION",
+        "ADDR:2ND ST",
+        "ID:2010-0000034969");
+    
+    doTest("T4",
+        "GREENE 911:PIAA >MVA WITH INJURIES MARKET ST ATHENS BUCHAKIAN,DAVID Cad: 2010-0000033852",
+        "CALL:MVA WITH INJURIES",
+        "ADDR:MARKET ST",
+        "CITY:ATHENS",
+        "ID:2010-0000033852");
+    
+    doTest("T5",
+        "GREENE 911:STRUCT>STRUCTURE FIRE 408 W BRIDGE ST CATSKILL LAIRD, JACQUELYNN Cad: 2010-0000034875",
+        "CALL:STRUCTURE FIRE",
+        "ADDR:408 W BRIDGE ST",
+        "CITY:CATSKILL",
+        "ID:2010-0000034875");
+    
   }
 }
