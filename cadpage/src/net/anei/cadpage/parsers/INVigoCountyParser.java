902da9a92fe9ca40d7f47f3afe30d51adc74caf5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-12-21 18:25:01 +0000

    Added Vigo County, IN location

---
+++ cadpage/src/net/anei/cadpage/parsers/INVigoCountyParser.java
@@ -0,0 +1,25 @@
+package net.anei.cadpage.parsers;
+
+/***
+Vigo County, IN
+Contact: Billy Roberts <thfd334@gmail.com>
+Sender: 37284
+System: Cisco
+Ct:FALL Loc:3301 ST MARY'S RD Apt: XSt:BLOOMTOWN RD US HWY 150 Grid:112D Units:E-42 TC-1 Rmk:92 YOLD FML; FELL; FACE DOWN ON FLOOR -
+Ct:FALL Loc:309 N CHURCH ST Apt: XSt:PARIS AV Grid:0WTH Units:E-41 TC-2 Rmk:65 Years F ;fall
+Ct:CHEST PAIN Loc:500 W PARIS AV Apt: XSt:6TH ST WTH 4TH ST WTH Grid:0WTH Units:E-41 TC-1 Rmk:
+Ct:CHOKING Loc:2925 N MAPLE PL Apt: XSt:LOCUST AV Grid:0126 Units:E-42 TC-1 Rmk:18 Months F ;CHOKING
+Ct:MVA-UNKNOWN-INJ Loc:US HWY 40/INTERSTATE 70 Apt: XSt: Grid:0169 Units:E-41 Rmk:SEMI AND PASSENGER CAR//UNKNOWN Injuries
+***/
+
+public class INVigoCountyParser extends DispatchCiscoParser {
+  
+  public INVigoCountyParser() {
+    super("VIGO COUNTY", "IN");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "37284";
+  }
+}
--- cadpage/src/net/anei/cadpage/parsers/OHSugarCreekParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OHSugarCreekParser.java
@@ -1,8 +1,5 @@
 package net.anei.cadpage.parsers;
 
-import java.util.Properties;
-import net.anei.cadpage.SmsMsgInfo.Data;
-
 /***
 "Ct:EMS MINOR       Loc:4336 STATE RT 725            Apt:      XSt:BAYBERRY COVE D LAKEMAN DR      Grid:003F Units:72                                        Rmk:"
 We use Cisco Public Safety�s ECAD program to page out CFS events.  CAD does send out multiple pages per event � one for the call and each responding unit.  Below are sample texts for review:
@@ -12,33 +9,9 @@ We use Cisco Public Safety�s ECAD program to page out CFS events.  CAD does se
 "Ct:OTHER FIRE      Loc:5980 WILMINGTON PIKE         Apt:      XSt:CLYO RD         WILMINGTON PIKE Grid:003A Units:72                                        Rmk:"
 ***/
 
-public class OHSugarCreekParser extends SmsMsgParserLegacy {
-
-  private static final String[]SugarCreekkeywords = new String[]{"Ct", "Loc", "Apt", "XSt", "Grid", "Units"};
-
-  @Override
-  public boolean isPageMsg(String body) {
-    return isPageMsg(body, SugarCreekkeywords);
-  }
-
-  @Override
-  protected void parse(String body, Data data) {
-    data.defState = "OH";
-    data.defCity = "SugarCreek";
-    body = body.replaceAll("(  )+", " ");
-    Properties props = parseMessage(body, SugarCreekkeywords);
-    data.strCall = props.getProperty("Ct", "");
-    data.strCall = data.strCall.trim();
-    parseAddress(props.getProperty("Loc", ""), data);
-    data.strApt = props.getProperty("Apt", "");
-    data.strCross = props.getProperty("XSt", "");
-    data.strBox = props.getProperty("Grid", "");
-    data.strUnit = props.getProperty("Units", "");
-    if (data.strUnit.contains("Rmk:")) {
-        data.strUnit = data.strUnit.substring(0,data.strUnit.indexOf("Rmk:"));
-        data.strUnit = data.strUnit.trim();
-      }
-    data.strSupp = props.getProperty("Rmk","");
-    
+public class OHSugarCreekParser extends DispatchCiscoParser {
+  
+  public OHSugarCreekParser() {
+    super("SugarCreek", "OH");
   }
 }
