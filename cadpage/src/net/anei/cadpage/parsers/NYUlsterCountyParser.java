31cab10abc06ceef2f7665f656d067c05c3cd1a0
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2010-12-25 20:27:51 +0000

    Renamed Kingston, NY parser to Ulster County, NY

--- cadpage/src/net/anei/cadpage/parsers/NYUlsterCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NYUlsterCountyParser.java
@@ -4,20 +4,26 @@ import java.util.Properties;
 import net.anei.cadpage.SmsMsgInfo.Data;
 
 /***
+Ulster County, NY
+Contact: Andrew Buboltz <ajb5620@gmail.com>
+Sender: cad@co.ulster.ny.us
+
 ((2058) RLIN/ ) Unit:ENG6 UnitSts: Loc:487 WASHINGTON AV  XSts:TAYLOR/CITY LINE Venue:King City Inc:Autoalarm Date:11/05/2010  Time:20:44 GENERAL FIRE ALARM Addtl
-
 ((63937) MDAV/ ) Unit:ENG6 UnitSts: Loc:86 HOFFMAN XSts:BROADWAY/MARYS  AV Venue:King City Inc:Struct Fir Date:11/03/2010 Time:21:22 SMOKE CODITION IN  AREA Addtl
-
 ((59728) SQUI/ )  Unit:ENG6 UnitSts: Loc:77 CORNELL XSts:TREMPER AV/SMITH AV Venue:King  City Inc:Haz Mat Date:11/02/2010 Time:00:45 UNKNOWN ODOR IN BUILDING  Addt
-
 ((2203) BKIN/ ) Unit:ENG6 UnitSts: Loc:89 NEWKIRK AV XSts:HASBROUCK AV/MAPLE  Venue:King City Inc:Struct Fir Date:11/05/2010 Time:21:39 BASEMENT FULL OF SMOKE ***/
 
-public class NYKingstonParser extends SmsMsgParser {
+public class NYUlsterCountyParser extends SmsMsgParser {
 
   private static final String[]Kingstonkeywords = new String[]{"Unit","UnitSts","Loc", "XSts", "Venue", "Inc","Date", "Time"};
   
-  public NYKingstonParser() {
-    super("Kingston", "NY");
+  public NYUlsterCountyParser() {
+    super("ULSTER COUNTY", "NY");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "cad@co.ulster.ny.us";
   }
 
   @Override
@@ -27,7 +33,7 @@ public class NYKingstonParser extends SmsMsgParser {
 
     Properties props = parseMessage(body, Kingstonkeywords);
     data.strCity = props.getProperty("Venue", "").replaceAll(" +", " ");
-    if (data.strCity.toUpperCase().startsWith("KING CITY")) { data.strCity="Kingston,Ulster";} 
+    if (data.strCity.toUpperCase().startsWith("KING CITY")) { data.strCity="KINGSTON";} 
     data.strCall = props.getProperty("Inc", "");
     parseAddress(props.getProperty("Loc", ""), data);
     data.strCross = props.getProperty("XSts", "");
