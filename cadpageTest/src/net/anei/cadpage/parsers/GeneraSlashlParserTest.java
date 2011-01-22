f319a96a26a65694f9c8a71de8151c636f5b3502
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-01-22 22:57:57 +0000

    Updated test classes

---
+++ cadpageTest/src/net/anei/cadpage/parsers/GeneraSlashlParserTest.java
@@ -0,0 +1,34 @@
+package net.anei.cadpage.parsers;
+
+import net.anei.cadpage.TestManagePreferences;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class GeneraSlashlParserTest extends BaseParserTest {
+  
+  public GeneraSlashlParserTest() {
+    setParser(new GeneralSlashParser(), "", "");
+  }
+  
+  @SuppressWarnings("static-access")
+  @BeforeClass
+  public static void classSetup() {
+    TestManagePreferences pmgr = new TestManagePreferences();
+    pmgr.setLocation("GeneralSlash");
+    pmgr.setOverrideDefault(true);
+    pmgr.setFilter("1112223333");
+  }
+  
+  @Test
+  public void testParser() {
+    doTest("T1",
+        "(- part 1 of 1) RC:Run# 4581/General Transport/STUBBS/ JAMES/1718 S I  ST//ER-10//TACOMA/773 G6/11315 BRIDGEPORT WAY SW",
+        "ID:4581",
+        "CALL:JAMES",
+        "ADDR:1718 S I ST",
+        "INFO:ER-10 | TACOMA | 773 G6",
+        "X:11315 BRIDGEPORT WAY SW");
+  }
+}
