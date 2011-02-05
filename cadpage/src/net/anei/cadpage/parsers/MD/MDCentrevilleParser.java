17f7adac29ed6d9c80f2a11784d0725b84fc4bbf
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-02-05 18:40:50 +0000

    Added new sender username to prefix marker Added default sender filters Modified to include trailing information in supplemental info

--- cadpage/src/net/anei/cadpage/parsers/MD/MDCentrevilleParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDCentrevilleParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.MD;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.SmsMsgInfo.Data;
 import net.anei.cadpage.parsers.SmartAddressParser;
 
@@ -13,6 +16,10 @@ qac911:*D 4-5 UNCONSCIOUS/FAINTING RT301NB / ROLLING BRIDGE RD UNCONSCIOUS Q04
 qac911:*D 5-2 MVC W/ROLLOVER RT301NB / HAYDEN RD RESCUE BOX Q05
 qac911:*D 4-7 MVC W/ROLLOVER 1627 RUTHSBURG RD RESCUE BOX Q04
 (text message) qac911:*D 4-2 CO ALARM 212 NORTHFIELD WAY LOCAL BOX Q04
+
+Contact: david baker <baker.davidm@gmail.com>
+sender:QA911com@qac.org <From%3AQA911com@qac.org>
+(Text Message) QA911com:*D 4-1 UNKNOWN PROBLEM 206 ORCHESTRA PL LIFE STATUS ? Q04
 ******************************************************************************/
 
 public class MDCentrevilleParser extends SmartAddressParser {
@@ -20,19 +27,27 @@ public class MDCentrevilleParser extends SmartAddressParser {
   private static final String DEF_STATE = "MD";
   private static final String DEF_CITY = "CENTREVILLE";
   
+  private static final Pattern MARKER = Pattern.compile("^(qac911|QA911com):\\*D ");
+  
   public MDCentrevilleParser() {
     super(DEF_CITY, DEF_STATE);
+  }
+  
+  @Override
+  public String getFilter() {
+    return "qac911@qac.org,QA911com@qac.org";
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
 
-    int ipt = body.indexOf("qac911:*D ");
-    if (ipt < 0) return false;
-    body = body.substring(ipt+10);
+    Matcher match = MARKER.matcher(body);
+    if (! match.find()) return false;
+    body = body.substring(match.end());
     
     // OK, go do your magic!!
     parseAddress(StartType.START_CALL, body, data);
+    data.strSupp = getLeft();
     return true;
   }
 }
