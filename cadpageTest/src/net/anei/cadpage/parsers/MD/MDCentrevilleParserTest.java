cd07015003c4453f05cdcb2b1ea23ae50e44b5d1
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-02-05 18:41:06 +0000

    Updated test classes

--- cadpageTest/src/net/anei/cadpage/parsers/MD/MDCentrevilleParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/MD/MDCentrevilleParserTest.java
@@ -18,37 +18,50 @@ public class MDCentrevilleParserTest extends BaseParserTest {
     doTest("T1",
         "qac911:*D 4-2 COMMERCIAL BLDG FIRE DUNKIN DONUTS @500 CHURCH HILL RD COMMERCIAL BOX Q04",
         "CALL:4-2 COMMERCIAL BLDG FIRE DUNKIN DONUTS",
-        "ADDR:500 CHURCH HILL RD");
+        "ADDR:500 CHURCH HILL RD",
+        "INFO:COMMERCIAL BOX Q04");
     
     doTest("T2",
         "qac911:*D 4-5 MVC W/ENTRAPMENT RT301NB / RT304 RESCUE BOX Q04",
         "CALL:4-5 MVC W/ENTRAPMENT",
-        "ADDR:RT301 & RT304");
+        "ADDR:RT301 & RT304",
+        "INFO:RESCUE BOX Q04");
 
     doTest("T3",
         "qac911:*D 5-1 LG BRUSH/GRASS FIRE RT405 / WINDSWEPT FARM LN BRUSH BOX Q05",
         "CALL:5-1 LG BRUSH/GRASS FIRE",
-        "ADDR:RT405 & WINDSWEPT FARM LN");
+        "ADDR:RT405 & WINDSWEPT FARM LN",
+        "INFO:BRUSH BOX Q05");
     
     doTest("T4",
         "qac911:*D 4-5 UNCONSCIOUS/FAINTING RT301NB / ROLLING BRIDGE RD UNCONSCIOUS Q04",
         "CALL:4-5 UNCONSCIOUS/FAINTING",
-        "ADDR:RT301 & ROLLING BRIDGE RD");
+        "ADDR:RT301 & ROLLING BRIDGE RD",
+        "INFO:UNCONSCIOUS Q04");
     
     doTest("T5",
         "qac911:*D 5-2 MVC W/ROLLOVER RT301NB / HAYDEN RD RESCUE BOX Q05",
         "CALL:5-2 MVC W/ROLLOVER",
-        "ADDR:RT301 & HAYDEN RD");
+        "ADDR:RT301 & HAYDEN RD",
+        "INFO:RESCUE BOX Q05");
     
     doTest("T6",
         "qac911:*D 4-7 MVC W/ROLLOVER 1627 RUTHSBURG RD RESCUE BOX Q04",
         "CALL:4-7 MVC W/ROLLOVER",
-        "ADDR:1627 RUTHSBURG RD");
+        "ADDR:1627 RUTHSBURG RD",
+        "INFO:RESCUE BOX Q04");
     
     doTest("T7",
         "(text message) qac911:*D 4-2 CO ALARM 212 NORTHFIELD WAY LOCAL BOX Q04",
         "CALL:4-2 CO ALARM",
-        "ADDR:212 NORTHFIELD WAY");
+        "ADDR:212 NORTHFIELD WAY",
+        "INFO:LOCAL BOX Q04");
+    
+    doTest("T8",
+        "(Text Message) QA911com:*D 4-1 UNKNOWN PROBLEM 206 ORCHESTRA PL LIFE STATUS ? Q04",
+        "CALL:4-1 UNKNOWN PROBLEM",
+        "ADDR:206 ORCHESTRA PL",
+        "INFO:LIFE STATUS ? Q04");
 
   }
 }
