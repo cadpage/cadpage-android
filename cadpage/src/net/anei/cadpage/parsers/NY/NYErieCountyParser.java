3e2cef49085a941b142c85db26567c46f445f7a8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-02-12 17:06:21 +0000

    Updated Erie County parser to handle new text format

--- cadpage/src/net/anei/cadpage/parsers/NY/NYErieCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYErieCountyParser.java
@@ -12,6 +12,9 @@ AMH 670 LONGMEADOW RD EMS 71 Y/O F CHEST PAINS, DIFFICULTY BREATHING
 AMH 3030 SHERIDAN DR EMS RM 146 58 M TROUBLE BREATHING
 AMH 35 ELM RD EMS 69M CHEST/ARM PAIN
 
+Contact: nyerpa96 <nyerpa96@gmail.com>
+ALERT@ERIE.GOV FIRE CO-DETECTOR 262 MILLER ST APT: GARAGE LANCASTER TOWN CO DETECTOR ACTIV / NO SYMPTOMS
+
 Possible call code values
 
 AMA             MUTUAL AID-Automatic Mutual Aid                   
@@ -48,18 +51,84 @@ public class NYErieCountyParser extends SmartAddressParser {
   private static final String DEF_STATE = "NY";
   private static final String DEF_CITY = "ERIE COUNTY";
   
+  private static final String[] CITY_LIST = new String[]{
+    "ALDEN TOWN",
+    "ALDEN",
+    "AMHERST TOWN",
+    "ANGOLA ON THE LAKE",
+    "ANGOLA",
+    "AURORA TOWN",
+    "BILLINGTON HEIGHTS",
+    "BLASDELL",
+    "BOSTON TOWN",
+    "BRANT TOWN",
+    "BUFFALO",
+    "CHEEKTOWAGA TOWN",
+    "CLARENCE CENTER",
+    "CLARENCE TOWN",
+    "COLDEN TOWN",
+    "COLLINS TOWN",
+    "CONCORD TOWN",
+    "DEPEW",
+    "EAST AMHERST",
+    "EAST AURORA",
+    "EDEN TOWN",
+    "ELMA CENTER",
+    "ELMA TOWN",
+    "EVANS TOWN",
+    "FARNHAM",
+    "GOWANDA",
+    "GRAND ISLAND TOWN",
+    "HAMBURG TOWN",
+    "HAMBURG",
+    "HARRIS HILL",
+    "HOLLAND TOWN",
+    "KENMORE",
+    "LACKAWANNA",
+    "LAKE ERIE BEACH",
+    "LAKE VIEW",
+    "LANCASTER TOWN",
+    "LANCASTER",
+    "MARILLA TOWN",
+    "NEWSTEAD TOWN",
+    "NORTH BOSTON",
+    "NORTH COLLINS TOWN",
+    "NORTH COLLINS",
+    "ORCHARD PARK TOWN",
+    "ORCHARD PARK",
+    "SARDINIA TOWN",
+    "SLOAN",
+    "SPRINGVILLE",
+    "TONAWANDA TOWN",
+    "TONAWANDA",
+    "TOWN LINE",
+    "WALES TOWN",
+    "WEST SENECA TOWN",
+    "WILLIAMSVILLE"
+  };
+  
   public NYErieCountyParser() {
-    super(DEF_CITY, DEF_STATE);
+    super(CITY_LIST, DEF_CITY, DEF_STATE);
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
     
-    if (! body.startsWith("AMH ")) return false;
-    
-    parseAddress(StartType.START_ADDR, body.substring(4), data);
+    if (body.startsWith("AMH ")) return parseAmhMsg(body.substring(4), data);
+    if (body.startsWith("ALERT@ERIE.GOV ")) return parseErieMsg(body.substring(15), data);
+    return false;
+  }
+
+  public boolean parseAmhMsg(String body, Data data) {
+    parseAddress(StartType.START_ADDR, body, data);
     data.strCall = getLeft();
     
+    return true;
+  }
+
+  private boolean parseErieMsg(String body, Data data) {
+    parseAddress(StartType.START_CALL, body, data);
+    data.strSupp = getLeft();
     return true;
   }
 }
