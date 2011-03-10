bea8e523fe7b260efbc04f2bf08895d5254f17be
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-10 20:11:27 +0000

    More changes to Crook County, OR parser

--- cadpage/src/net/anei/cadpage/parsers/OR/ORCrookCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORCrookCountyParser.java
@@ -9,11 +9,7 @@ Prineville, OR
 Contact: 5414603655@vzwpix.com
 Sender:dispatch@prinevillepd.org
 
-[NEW INCIDENT]  INCIDENT NEW\n3/9/2011 1003\nEVENT # 1103090013 PFD\nTEST-FIRE - TEST INCIDENT\nPRIORITY 2 \nLOCATION 387 NE 3RD ST\nCITY PRINEVILLE\nAPT \nPREMISE
-[UNIT DISPATCH]  UNIT DISPATCH\n3/9/2011 1203\nUNITS 1291 \nEVENT # 1103090021 PFD\nFSMK - SMOKE UNKNOWN FIRE S...\nPRIORITY 1 \nLOCATION 1231 SE 5TH ST\nCITY PRI
-[NEW INCIDENT]  INCIDENT NEW\n3/9/2011 1203\nEVENT # 1103090021 PFD\nFSMK - SMOKE UNKNOWN FIRE S...\nPRIORITY 1 \nLOCATION 1231 SE 5TH ST\nCITY PRINEVILLE\nAPT \nP
-(NEW INCIDENT) INCIDENT NEW\n3/9/2011 1203\nEVENT # 1103090021 PFD\nFSMK - SMOKE UNKNOWN FIRE S...\nPRIORITY 1 \nLOCATION 1231 SE 5TH ST\nCITY PRINEVILLE\nAPT \nP
-(NEW INCIDENT) INCIDENT NEW\n3/9/2011 1203\nEVENT # 1103090021 PFD\nFSMK - SMOKE UNKNOWN FIRE S...\nPRIORITY 1 \nLOCATION 1231 SE 5TH ST\nCITY PRINEVILLE\nAPT \nP
+(NEW INCIDENT) 3/10/2011 1003\nEVENT # 1103100020 PFD\nMABD - ABDOMINAL PAIN\nPRIORITY 1 \nLOCATION 2250 SE KYLE RD\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOMMENT: HA
 
 */
 
@@ -21,7 +17,7 @@ public class ORCrookCountyParser extends FieldProgramParser {
   
   public ORCrookCountyParser() {
     super("CROOK COUNTY", "OR",
-           "SKIP SKIP UNITS:UNIT EVENT:ID! CALL! PRIORITY:SKIP! LOCATION:ADDR! CITY:CITY! APT:APT");
+           "SKIP EVENT:ID! CALL! PRIORITY:SKIP! LOCATION:ADDR! CITY:CITY! APT:APT PREMISE:SKIP COMMENT:INFO");
   }
   
   @Override
@@ -30,11 +26,11 @@ public class ORCrookCountyParser extends FieldProgramParser {
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
+  protected boolean parseMsg(String subject, String body, Data data) {
 
-    if (!body.startsWith("INCIDENT NEW\n") && !body.startsWith("UNIT DISPATCH\n")) return false;
+    if (!subject.equals("NEW INCIDENT")) return false;
     
-    body = body.replace("\nEVENT # ","\nEVENT:").replace("\nUNITS ","\nUNITS:")
+    body = body.replace("\nEVENT # ","\nEVENT:")
                .replace("\nPRIORITY ","\nPRIORITY:").replace("\nLOCATION ","\nLOCATION:")
                .replace("\nCITY ","\nCITY:").replace("\nAPT \n","\nAPT:");
     return parseFields(body.split("\n"), data);
