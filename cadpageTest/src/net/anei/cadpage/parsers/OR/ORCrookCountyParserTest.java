f60f250bc207b3c226a63119e47277d70409aed8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-11 19:35:24 +0000

    Updated test class

--- cadpageTest/src/net/anei/cadpage/parsers/OR/ORCrookCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/OR/ORCrookCountyParserTest.java
@@ -14,15 +14,6 @@ public class ORCrookCountyParserTest extends BaseParserTest {
   
   @Test
   public void testParser() {
-
-    doTest("T1",
-        "(NEW INCIDENT) 3/10/2011 1003\nEVENT # 1103100020 PFD\nMABD - ABDOMINAL PAIN\nPRIORITY 1 \nLOCATION 2250 SE KYLE RD\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOMMENT: HA",
-        "ID:1103100020 PFD",
-        "CALL:MABD - ABDOMINAL PAIN",
-        "ADDR:2250 SE KYLE RD",
-        "CITY:PRINEVILLE",
-        "APT:PREMISE:",
-        "INFO:HA");
   }
   
   public static void main(String[] args) {
