3418f1f6017d1cb25e2c174fda518f8db7b41146
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-11 19:35:00 +0000

    Updated parser for Crook County, OR

--- cadpage/src/net/anei/cadpage/parsers/OR/ORCrookCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORCrookCountyParser.java
@@ -10,6 +10,9 @@ Contact: 5414603655@vzwpix.com
 Sender:dispatch@prinevillepd.org
 
 (NEW INCIDENT) 3/10/2011 1003\nEVENT # 1103100020 PFD\nMABD - ABDOMINAL PAIN\nPRIORITY 1 \nLOCATION 2250 SE KYLE RD\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOMMENT: HA
+(NEW INCIDENT) 3/11/2011 1003\nEVENT # 1103110010 PFD\nBURN - BURN INFO ONLY\nPRIORITY 1 \nLOCATION 6282 NW GREEN VALLEY RD\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOM
+(NEW INCIDENT) 3/11/2011 0903\nEVENT # 1103110009 PFD\nFALARM - FIRE ALARM\nPRIORITY 1 \nLOCATION 210 NE 3RD ST\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOMMENT: BACK R\n
+(NEW INCIDENT) 3/10/2011 0803\nEVENT # 1103100051 PFD\nMUNK - UNKNOWN MEDICAL\nPRIORITY 1 \nLOCATION 587 SE KNIGHT ST\nCITY PRINEVILLE\nAPT \nPREMISE: \nCOMMENT:
 
 */
 
@@ -17,7 +20,7 @@ public class ORCrookCountyParser extends FieldProgramParser {
   
   public ORCrookCountyParser() {
     super("CROOK COUNTY", "OR",
-           "SKIP EVENT:ID! CALL! PRIORITY:SKIP! LOCATION:ADDR! CITY:CITY! APT:APT PREMISE:SKIP COMMENT:INFO");
+           "SKIP EVENT:ID! CALL! PRIORITY:SKIP! LOCATION:ADDR! CITY:CITY! APT:APT PREMISE:PLACE COMMENT:INFO");
   }
   
   @Override
@@ -32,7 +35,7 @@ public class ORCrookCountyParser extends FieldProgramParser {
     
     body = body.replace("\nEVENT # ","\nEVENT:")
                .replace("\nPRIORITY ","\nPRIORITY:").replace("\nLOCATION ","\nLOCATION:")
-               .replace("\nCITY ","\nCITY:").replace("\nAPT \n","\nAPT:");
+               .replace("\nCITY ","\nCITY:").replace("\nAPT ","\nAPT:");
     return parseFields(body.split("\n"), data);
   }
 }
