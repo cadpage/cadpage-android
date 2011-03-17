953601668b8f721323f23af90515aec454eb8e14
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-17 22:48:05 +0000

    Added test class

---
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCRowanCountyParserTest.java
@@ -0,0 +1,53 @@
+package net.anei.cadpage.parsers.NC;
+
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+
+public class NCRowanCountyParserTest extends BaseParserTest {
+  
+  public NCRowanCountyParserTest() {
+    setParser(new NCRowanCountyParser(), "ROWAN COUNTY", "NC");
+  }
+  
+  @Test
+  public void testParser() {
+
+    doTest("T1",
+        "CAD:10C4 CHEST PAIN;201 N FRANKLIN ST;SWINK ST;PARK ST;CHGV;4301;RFG1",
+        "CALL:10C4 CHEST PAIN",
+        "ADDR:201 N FRANKLIN ST",
+        "X:SWINK ST & PARK ST",
+        "CITY:CHINA GROVE",
+        "INFO:4301 / RFG1");
+
+    doTest("T2",
+        "CAD:FIRE DEPT SERVICE ASSIGNMENT;213 WASHINGTON ST;KLONDALE ST;CHGV;4301",
+        "CALL:FIRE DEPT SERVICE ASSIGNMENT",
+        "ADDR:213 WASHINGTON ST",
+        "X:KLONDALE ST",
+        "CITY:CHINA GROVE",
+        "INFO:4301");
+
+    doTest("T3",
+        "CAD:13D1 DIABETIC PROBLEM;1075 DEAL RD;KARRIKER FARMS RD;FIELD TRACE RD;MOOR;CARE INN RETIREMENT CENTER;4002;RFG4",
+        "CALL:13D1 DIABETIC PROBLEM",
+        "ADDR:1075 DEAL RD",
+        "X:KARRIKER FARMS RD & FIELD TRACE RD",
+        "CITY:MOORESVILLE",
+        "PLACE:CARE INN RETIREMENT CENTER",
+        "INFO:4002 / RFG4");
+
+    doTest("T4",
+        "CAD:RFD COMMAND ESTABLISHED;2470 E NC 152 HWY; CHGV",
+        "CALL:RFD COMMAND ESTABLISHED",
+        "ADDR:2470 E NC 152 HWY",
+        "CITY:CHINA GROVE");
+  }
+  
+
+  public static void main(String[] args) {
+    new NCRowanCountyParserTest().generateTests("T1");
+  }
+}
