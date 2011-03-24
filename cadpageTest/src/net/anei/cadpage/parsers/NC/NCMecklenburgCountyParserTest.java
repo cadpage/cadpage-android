f8a80adc4e12b4407cfb992e0a31f446c963230f
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-24 19:05:59 +0000

    Added test class

---
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCMecklenburgCountyParserTest.java
@@ -0,0 +1,36 @@
+package net.anei.cadpage.parsers.NC;
+
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+
+public class NCMecklenburgCountyParserTest extends BaseParserTest {
+  
+  public NCMecklenburgCountyParserTest() {
+    setParser(new NCMecklenburgCountyParser(), "MECKLENBURG COUNTY", "NC");
+  }
+  
+  @Test
+  public void testParser() {
+
+    doTest("T1",
+        "Subject:Text Page\n18758 Silver Quay Dr                                                  Charlie                       17- Falls/Back injur",
+        "ADDR:18758 Silver Quay Dr",
+        "INFO:Charlie",
+        "CALL:17- Falls/Back injur");
+
+    doTest("T2",
+        "Subject:Text Page\n19485 Old Jetton Rd           ste 100   Lakeside Familty Physicians anDelta                         31- Unconsciousness/",
+        "ADDR:19485 Old Jetton Rd",
+        "APT:ste 100",
+        "PLACE:Lakeside Familty Physicians an",
+        "INFO:Delta",
+        "CALL:31- Unconsciousness/");
+ }
+  
+
+  public static void main(String[] args) {
+    new NCMecklenburgCountyParserTest().generateTests("T1", "ADDR APT PLACE INFO CALL");
+  }
+}
