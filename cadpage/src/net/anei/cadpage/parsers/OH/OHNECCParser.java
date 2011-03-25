92201e074c84456f3989549c7985163835dcc874
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-03-25 00:10:14 +0000

    Added NECC, OH location

---
+++ cadpage/src/net/anei/cadpage/parsers/OH/OHNECCParser.java
@@ -0,0 +1,104 @@
+package net.anei.cadpage.parsers.OH;
+
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.SmsMsgInfo.Data;
+import net.anei.cadpage.parsers.FieldProgramParser;
+
+/*
+NECC, OH
+Contact: Jon Frye <jfrye@safety-center.org>
+Sender; Dispatcher@safety-center.org
+
+(NECC) [F] LV:1 ALARM CARBON MONOXIDE NO SYMPTOMS\n/\n1619 W LOVELAND AV\n(LV)\nXS: S LEBANON RD & HEIDELBURG DR\n\nRCVD AS Phone\n\nCOM:\nALARM ACTIVATED
+[NECC]  [F] LV:1 ALARM FIRE\n/\nCHILI'S GRILL & BAR\n11329<tel:11329> MONTGOMERY RD\n(SY)\nXS: HARPERSPOINTE DR & E KEMPER RD\n\nRCVD AS Phone\n\nCOM:\nF28 FIRE DUCT ALARM\nE9
+(NECC) [F] LV:1 AUTO ACCIDENT INJURY\n/\nFIELDS ERTEL RD/UNION CEMETERY RD\n(SY)\nXS: UNION CEMETERY RD & GREEN ARBORS LN\n\nRCVD AS Phone\n\nCOM:
+(NECC) [F] LV:1 AUTO ACCIDENT VEHICLE FIRE\n/\nBECKER\n9695 LOVELAND MADEIRA RD\n(SY)\nXS: HUMPHREY RD & MORGANS TRACE\n\nRCVD AS Phone
+
+*/
+public class OHNECCParser extends FieldProgramParser {
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "LV", "LOVELAND",
+      "SY", "SYMMES"
+  });
+  
+  private static final Pattern PHONE_PTN = Pattern.compile("<tel:(.*)>");
+
+  public OHNECCParser() {
+    super(CITY_CODES, "", "OH",
+           "LV:CALL! SKIP PLACE? ADDR! CITY XS:X COM:SKIP INFO+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "Dispatcher@safety-center.org";
+  }
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    
+    if (! subject.equals("NECC|F")) return false;
+    
+    return parseFields(body.split("\\n"), data);
+  }
+  
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(' ');
+      if (pt >= 0) field = field.substring(pt+1).trim();
+      super.parse(field, data);
+    }
+  }
+  
+  private class MyAddressField extends AddressField {
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      field = removePhone(field, data);
+      return super.checkParse(field, data);
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      field = removePhone(field, data);
+      super.parse(field, data);
+    }
+    
+    private String removePhone(String field, Data data) {
+      Matcher match = PHONE_PTN.matcher(field);
+      if (match.find()) {
+        data.strPhone = match.group(1);
+        field = field.substring(0,match.start()) + field.substring(match.end());
+      }
+      return field;
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "PHONE " + super.getFieldNames();
+    }
+    
+  }
+  
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("(")) field = field.substring(1);
+      if (field.endsWith(")")) field = field.substring(0,field.length()-1);
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("CITY")) return new MyCityField();
+    return super.getField(name);
+  }
+  
+}
