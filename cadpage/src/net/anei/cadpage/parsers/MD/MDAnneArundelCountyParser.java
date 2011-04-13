25979aa42bc283bdb754f6c5cde9d411a811236a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-04-13 04:22:16 +0000

    Added second Anne Arundel County, MD location

--- cadpage/src/net/anei/cadpage/parsers/MD/MDAnneArundelCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDAnneArundelCountyParser.java
@@ -1,38 +1,11 @@
 package net.anei.cadpage.parsers.MD;
 
-import java.util.regex.Pattern;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-import net.anei.cadpage.SmsMsgInfo.Data;
-import net.anei.cadpage.parsers.FieldProgramParser;
-
-/*
-Anne Arundel county, MD
-Contact: krist teal <sqdman43@gmail.com>
-
-SEVERN | ANNE ARUNDEL | FIRE ACTIVITY | I-97 & BENFIELD | BENFIELD RAMP CLOSED DUE TO BRUSH FIRE. | 31 |
-GLEN BURNIE | ANNE ARUNDEL | SHOOTING | 1911 CRAIN HIGHWAY | 1 VICTIM SHOT AT THE MOOSE LODGE. | 31 |
-ODENTON | ANNE ARUNDEL | *U/D* | CONWAY RD & MEYERS STATION | MULTI BARNS & STRUCTURES HAVE BEEN LOST. MULTI DWELLINGS AT RISK. | 31 |
-HANOVER | ANNE ARUNDEL | 3RD ALM | 1703 MAPLE AVE | E291 O/L WITH A LARGE BRUSH FIRE. A HOUSE IS NOW ON FIRE. REQ THE 2ND & 3RD ALARM. MULTI EXPOSURE
-SEVERNA PARK | ANNE ARUNDEL | WORKING FIRE | 750 OAK GROVE CIRCLE | Q23 O/L WITH A WORKING BASEMENT FIRE. | 31
-LINTHICUM | ANNE ARUNDEL | AUTO ACCIDENT | AVIATION BLVD & TERMINAL RD | MVC INVOL BWI BUS. 1 INJ. | 31 |
-
-*/
-
-public class MDAnneArundelCountyParser extends FieldProgramParser {
-  
-  private static final Pattern SEPARATOR = Pattern.compile(" *\\| *");
+public class MDAnneArundelCountyParser extends GroupBestParser {
   
   public MDAnneArundelCountyParser() {
-    super("ANNE ARUNDEL COUNTY", "MD",
-           "CITY SKIP CALL ADDR! INFO");
+    super(new MDAnneArundelCountyFireParser(), new MDAnneArundelCountyEMSParser());
   }
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    
-    String[] flds = SEPARATOR.split(body);
-    if (flds.length < 5) return false;
-    if (!flds[1].equals("ANNE ARUNDEL")) return false;
-    return parseFields(flds, data);
-  }
+
 }
--- cadpage/src/net/anei/cadpage/parsers/NY/NYNassauCountyFiretrackerParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYNassauCountyFiretrackerParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NY;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.SmsMsgInfo.Data;
 import net.anei.cadpage.parsers.FieldProgramParser;
 
@@ -12,6 +15,7 @@ Sender: dispatch@firetracker.net
 (FirePage) ** FFD FIRE CALL ** DISTRIC DIST CALL FOR SMOKE IN THE AREA CHURCH ST\nC/S: ARCHER ST TOA: 19:12 [FireTracker]
 (FirePage) ** FFD FIRE CALL ** GENERAL APT 72 / 3RD FLOOR / SMOKE 100 N MAIN ST\nC/S: RANDALL AVE / GRAND AVE TOA: 18:50 [FireTracker]
 (FirePage) ** FFD FIRE CALL ** STILL WILLOWBROOK LA C/S: BROOKSIDE (N) AVE TOA:\n09:16 [FireTracker]
+(FirePage) ** FFD FIRE CALL ** GENERAL ELEC PROBLEM/APT 415 45 WALLACE ST C/S:\nBROOKLYN AVE / RANDALL AVE TOA: 18:26 [FireTracker]
 
 */
 public class NYNassauCountyFiretrackerParser extends FieldProgramParser {
@@ -35,5 +39,24 @@ public class NYNassauCountyFiretrackerParser extends FieldProgramParser {
     body = body.substring(20,body.length()-13).trim();
     body = body.replace('\n', ' ').replace("C/S:", "CS:").replace(" LA ", " LN ");
     return super.parseMsg(body, data);
+  }
+  
+  private static final Pattern APT_PTN = Pattern.compile("\\bAPT ([^ ]+)\\b");
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = APT_PTN.matcher(field);
+      if (match.find()) {
+        data.strApt = match.group(1);
+        field = field.substring(0,match.start()) + field.substring(match.end());
+      }
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADDR")) return new MyAddressField();
+    return super.getField(name);
   }
 }
