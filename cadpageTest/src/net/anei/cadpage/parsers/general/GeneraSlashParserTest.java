b757c461ea0713c49ad7a01f6275ad557c694954
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-04-20 21:00:15 +0000

    Updated test classes

--- cadpageTest/src/net/anei/cadpage/parsers/general/GeneraSlashParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/general/GeneraSlashParserTest.java
@@ -26,5 +26,14 @@ public class GeneraSlashParserTest extends BaseParserTest {
         "ADDR:1718 S I ST",
         "INFO:ER-10 / TACOMA / 773 G6",
         "X:11315 BRIDGEPORT WAY SW");
+    
+    doTest("T2",
+        "S:Dispatch Center M:11-004682 GENERAL MEDICAL/AMBULANCE\n208 N 12 ST\nSTATE ST / PARK ST/\nMEDICAL ASSOCIATES OF BELLEVUE  BELLEVUE\nBEA1\n\n\n",
+        "CALL:AMBULANCE",
+        "ADDR:208 N 12 ST",
+        "X:STATE ST & PARK ST",
+        "INFO:MEDICAL ASSOCIATES OF BELLEVUE  BELLEVUE",
+        "UNIT:BEA1");
+
   }
 }
