f71921ae4b8420a4f413c492af8f561b98c26c7d
derrick@cadpage.org <derrick@cadpage.org@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-05-05 23:35:33 +0000

    NCRutherford Tester completed.

--- cadpageTest/src/net/anei/cadpage/parsers/NC/NCRutherfordCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCRutherfordCountyParserTest.java
@@ -13,12 +13,59 @@ public class NCRutherfordCountyParserTest extends BaseParserTest {
   
   @Test
   public void testParser() {
-    
+    doTest("T1",
+        "Paging: Location= 159 WALKER DR*\nAPT/ROOM* City=Forest City* Call \nType= FIRE STRUCT* Units=SMFD1*\nApr24,3:05am",
+        "ADDR:159 WALKER DR",
+        "CITY:Forest City",
+        "CALL:FIRE STRUCT",
+        "UNIT:SMFD1");
+
+    doTest("T2",
+        "Paging: Location= 557 US 221A HWY*\nAPT/ROOM* City=Forest City* Call \nType= FIRE POWERLN* Units=DUKE1, SMFD1*\nApr23,4:27am",
+        "ADDR:557 US 221A HWY",
+        "CITY:Forest City",
+        "CALL:FIRE POWERLN",
+        "UNIT:DUKE1, SMFD1");
+
+    doTest("T3",
+        "Paging: Location= 140 WALKER DR*\nAPT/ROOM* City=Forest City* Call \nType= FIRE ILLEGAL* Units=SMFD1*\nApr23,3:53am",
+        "ADDR:140 WALKER DR",
+        "CITY:Forest City",
+        "CALL:FIRE ILLEGAL",
+        "UNIT:SMFD1");
+
+    doTest("T4",
+        "Paging: Location= 139 GREENE RD*\nAPT/ROOM* City=Forest City* Call \nType= FIRE STRUCT* Units=SMFD1*\nApr19,5:36pm",
+        "ADDR:139 GREENE RD",
+        "CITY:Forest City",
+        "CALL:FIRE STRUCT",
+        "UNIT:SMFD1");
+
+    doTest("T5",
+        "Paging: Location= HIGH SHOALS CHURCH RD*\nAPT/ROOM* City=Cliffside* Call \nType= FIRE STRUCT* Units=CFD1, EFD1,SMFD1*\nApr18,10:50pm",
+        "ADDR:HIGH SHOALS CHURCH RD",
+        "CITY:Cliffside",
+        "CALL:FIRE STRUCT",
+        "UNIT:CFD1, EFD1,SMFD1");
+
+    doTest("T6",
+        "Paging: Location= OLD HENRIETTA RD & BYERS RD*\nAPT/ROOM* City=Ellenboro* Call \nType= STRANGE ODOR* Units=SMFD1*\nApr 25,10:59am",
+        "ADDR:OLD HENRIETTA RD & BYERS RD",
+        "CITY:Ellenboro",
+        "CALL:STRANGE ODOR",
+        "UNIT:SMFD1");
+
+    doTest("T7",
+        "Paging: Location= US221 S HWY & HARRIS HENRIETTA RD*\nAPT/ROOM* City=Forest City* Call \nType= ACCIDENT PI* Units=SMFD1,RCR1,EMS1*\nApr 25, 3:31pm",
+        "ADDR:US221 S HWY & HARRIS HENRIETTA RD",
+        "CITY:Forest City",
+        "CALL:ACCIDENT PI",
+        "UNIT:SMFD1,RCR1,EMS1");
     
   }
 
-public static void main(String[] args) {
-    new NCRutherfordCountyParserTest().generateTests("T1");
-  }
+//public static void main(String[] args) {
+//    new NCRutherfordCountyParserTest().generateTests("T1");
+//  }
 
 }
