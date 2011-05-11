056a4c5c3c5b7425d9ebcfc49e74cacaa75ffad5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-05-11 21:02:50 +0000

    Updated test class

--- cadpageTest/src/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyBParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyBParserTest.java
@@ -37,7 +37,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "ADDR:12003 WALLACE LN",
         "X:DULEY STATION RD and WALLACE LANDING CT",
         "CH:TGC 2",
-        "MAP:5884 C 1");
+        "MAP:5884 C 1",
+        "UNIT:A820");
 
     doTest("T4",
         "DISPATCH From FED10:BLS0, BLS Amb, 12003 WALLACE LN, PP, btwn DULEY STATION RD and WALLACE LANDING CT, TGC 2, 4504, 5884 C 1, Medical ProQA recommends dispatch at this time, Unit:A820",
@@ -46,7 +47,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "ADDR:12003 WALLACE LN",
         "X:DULEY STATION RD and WALLACE LANDING CT",
         "CH:TGC 2",
-        "MAP:5884 C 1");
+        "MAP:5884 C 1",
+        "UNIT:A820");
 
     doTest("T5",
         "DISPATCH From FED10:HOUSEF, House Fire, 10511 MULLIKIN DR, PP, btwn PARKVIEW LN and GATOR PL, TGC3, 2514, 5882 G 1, Fire ProQA recommends dispatch at this time, Units:BO887, E823, E825, E847, RE840, SQ827, TK832, TK837",
@@ -56,7 +58,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:PARKVIEW LN and GATOR PL",
         "CH:TGC3",
         "MAP:5882 G 1",
-        "UNIT:BO887");
+        "UNIT:BO887, E823, E825, E847, RE840, SQ827, TK832, TK837");
 
     doTest("T6",
         "DISPATCH From FED01:ALS1, Medic Local, 4905 ASHFORD PL, PP, btwn CRANFORD DR and ASHFORD DR, TGA2, 2017, 5 651 K 9, Medical ProQA recommends dispatch at this time, Units:A820, MD845",
@@ -66,7 +68,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:CRANFORD DR and ASHFORD DR",
         "CH:TGA2",
         "MAP:5 651 K 9",
-        "UNIT:A820");
+        "UNIT:A820, MD845");
 
     doTest("T7",
         "DISPATCH From FED01:BLS, BLS Amb, 14741 GOVERNOR ODEN BOWIE DR, PP, btwn ELM ST and MAIN ST, TGA2, 2009, 5652 K 10, PER ID 1879 FB NEEDED TO RESPOND TO 5TH FL FOR SEMI CONCIOUS SICK CITZ..NFI @ THIS TIME, Units:A843, E820",
@@ -76,7 +78,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:ELM ST and MAIN ST",
         "CH:TGA2",
         "MAP:5652 K 10",
-        "UNIT:A843");
+        "INFO:PER ID 1879 FB NEEDED TO RESPOND TO 5TH FL FOR SEMI CONCIOUS SICK CITZ..NFI @ THIS TIME",
+        "UNIT:A843, E820");
   }
   
   @Test
@@ -90,17 +93,37 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:ELM WAY and PENNY AVE",
         "CH:TGC3",
         "MAP:5882 G 2",
-        "UNIT:BO887");
+        "UNIT:BO887, E825, RE840, SQ827, TK832");
 
     doTest("T2",
-        "E820 ALS, Medic Local, 15903 BISHOPSTONE TER, PP, btwn GALESHEAD DR and END, TGA2, 2001, 5653 D 5, Police ProQA recommends dispatch at this time, Units:A843, E820, NMR Eff Body:ProQA recommends dispatch at this time, Units:A843, E820, NMR",
+        "E820 ALS, Medic Local, 15903 BISHOPSTONE TER, PP, btwn GALESHEAD DR and END, TGA2, 2001, 5653 D 5, Police ProQA recommends dispatch at this time, Units:A843, E820, NMR",
         "SRC:E820",
         "CALL:ALS-Medic Local",
         "ADDR:15903 BISHOPSTONE TER",
         "X:GALESHEAD DR and END",
         "CH:TGA2",
         "MAP:5653 D 5",
-        "UNIT:A843");
+        "UNIT:A843, E820, NMR");
+
+    doTest("T3",
+        "A820 ALS1, Medic Local, 9805 BROADMOOR TER, PP, btwn BROADMOOR LN and BROADMOOR CT, TGC2, 4505, 5768 C 10, Medical ProQA recommends dispatch at this time, Units:A820, MD845",
+        "SRC:A820",
+        "CALL:ALS1-Medic Local",
+        "ADDR:9805 BROADMOOR TER",
+        "X:BROADMOOR LN and BROADMOOR CT",
+        "CH:TGC2",
+        "MAP:5768 C 10",
+        "UNIT:A820, MD845");
+
+    doTest("T4",
+        "A820 BLS, BLS Amb, 20 DIANE DR, TGA2, MAAA, ANNE ARUNDEL BOX 0904 A820 FOR SICK PERSON ON BRAVO W/ MEDIC 9, Unit:A820",
+        "SRC:A820",
+        "CALL:BLS-BLS Amb",
+        "ADDR:20 DIANE DR",
+        "CH:TGA2",
+        "CITY:ANNE ARUNDEL COUNTY",
+        "INFO:ANNE ARUNDEL BOX 0904 A820 FOR SICK PERSON ON BRAVO W/ MEDIC 9",
+        "UNIT:A820");
   }
   
   @Test
@@ -115,7 +138,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:14TH AVE and 15TH AVE",
         "CH:TGA2",
         "MAP:5409 C 2",
-        "UNIT:BO884");
+        "UNIT:BO884, E702, E712, E834, PE844, SQ814, TK801, TK812");
 
     doTest("T2",
         "SQ814 BUILDG, Building Gas Leak, 13922 BALTIMORE AVE, LP, btwn LAUREL LAKES CT and CYPRESS ST, TGA9, 1007, 5169 B 7, Fire ProQA recommends dispatch at this time, Units:BO886, E810, RE849, SQ814, TK831",
@@ -125,7 +148,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:LAUREL LAKES CT and CYPRESS ST",
         "CH:TGA9",
         "MAP:5169 B 7",
-        "UNIT:BO886");
+        "UNIT:BO886, E810, RE849, SQ814, TK831");
 
     doTest("T3",
         "SQ814 HOUSEF, House Fire, 3400 PENNSYLVANIA ST, PP, btwn WELLS BLVD and ADELPHI RD, TGA2, 0117, 5409 G 4, Fire ProQA recommends dispatch at this time, Units:BO884, E807, E811, E834, PE844, SQ814, TK801, TK812",
@@ -135,7 +158,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:WELLS BLVD and ADELPHI RD",
         "CH:TGA2",
         "MAP:5409 G 4",
-        "UNIT:BO884");
+        "UNIT:BO884, E807, E811, E834, PE844, SQ814, TK801, TK812");
   }
   
   @Test
@@ -149,7 +172,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:GOHAGEN RD and TERRYTOWN CT",
         "CH:TGC7",
         "MAP:5653 G 8",
-        "UNIT:A820");
+        "UNIT:A820, E820, NMR");
 
     doTest("T2",
         "Medic 829 ALS1, Medic Local, 2411 KIRTLAND AVE, PP, btwn MARBURY DR and MASON ST, TGC7, 2622, 5650 K 3, Medical ProQA recommends dispatch at this time, Units:A826, MD829",
@@ -159,14 +182,18 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:MARBURY DR and MASON ST",
         "CH:TGC7",
         "MAP:5650 K 3",
-        "UNIT:A826");
+        "UNIT:A826, MD829");
 
     doTest("T3",
         "Ambulance 812 PIAH, PIA Limited Access, SB CAP BELT IL A HWY/SB BALTIMORE AVE, PP, at SB CAP BELT IL A HWY/SB BALTIMORE AVE, PP <0/0>, TGA2, 4113, 5288 B 8, 3 VEHICLES ACCIDENT, UNK INJ...MSP CP ADV, Units:A812, E831",
         "SRC:Ambulance 812",
         "CALL:PIAH-PIA Limited Access",
         "ADDR:SB CAP BELT IL A HWY & SB BALTIMORE AVE",
-        "UNIT:A812");
+        "X:SB CAP BELT IL A HWY/SB BALTIMORE AVE",
+        "CH:TGA2",
+        "MAP:5288 B 8",
+        "INFO:3 VEHICLES ACCIDENT / UNK INJ...MSP CP ADV",
+        "UNIT:A812, E831");
 
     doTest("T4",
         "Battalion Chief 886 HOUSEF, House Fire, 5031 HURON ST, PP, btwn 50TH PL and 51ST AVE, TA11, 1112, 5288 C 9, HOUSE ON FIRE, CAN SEE THE SMOKE COMING FROM THE HOME - COMPL LEFT THE PHONE BUT STILL HAVE AN O/L, Units:BO886, E811, E813, E831, E835, SQ814, TK812, TW807",
@@ -176,7 +203,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:50TH PL and 51ST AVE",
         "CH:TA11",
         "MAP:5288 C 9",
-        "UNIT:BO886");
+        "INFO:HOUSE ON FIRE / CAN SEE THE SMOKE COMING FROM THE HOME - COMPL LEFT THE PHONE BUT STILL HAVE AN O/L",
+        "UNIT:BO886, E811, E813, E831, E835, SQ814, TK812, TW807");
 
     doTest("T5",
         "Ambulance 827 RAP, Rape, 4220 APPLE ORCHARD LN, PP, btwn APPLEGATE LN and APPLE ORCHARD CT, TGC2, 2917, 5650 A 8, Police ProQA recommends dispatch at this time, Units:A827, TK829",
@@ -186,7 +214,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:APPLEGATE LN and APPLE ORCHARD CT",
         "CH:TGC2",
         "MAP:5650 A 8",
-        "UNIT:A827");
+        "UNIT:A827, TK829");
 
     doTest("T6",
         "Paramedic Ambulance 838 ALS1, Medic Local, 1114 HILL RD, PP, btwn LOWLAND DR and HIGHLAND PARK DR, TGA2, 3812, 5530 H 6, Medical ProQA recommends dispatch at this time, Units:PA838, QT838",
@@ -196,7 +224,7 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "X:LOWLAND DR and HIGHLAND PARK DR",
         "CH:TGA2",
         "MAP:5530 H 6",
-        "UNIT:PA838");
+        "UNIT:PA838, QT838");
 
     doTest("T7",
         "Engine 820 ASPD, Assist Police, 4709 COLONEL ASHTON PL, PP, btwn END and COLONELS CHOICE RD, TG1, 2015, 5652 J 9, Police ProQA recommends dispatch at this time, Unit:E820",
@@ -205,7 +233,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "ADDR:4709 COLONEL ASHTON PL",
         "X:END and COLONELS CHOICE RD",
         "CH:TG1",
-        "MAP:5652 J 9");
+        "MAP:5652 J 9",
+        "UNIT:E820");
 
     doTest("T8",
         "Ambulance 836 BLS0, BLS Amb, 15407 GIDEON GILPIN ST, PP, btwn COMMANDER HOWE TER and CAPTAIN ANDRE CT, TGC2, 4001, 5998 J 2, Medical ProQA recommends dispatch at this time, Unit:A836",
@@ -214,7 +243,8 @@ public class MDPrinceGeorgesCountyBParserTest extends BaseParserTest {
         "ADDR:15407 GIDEON GILPIN ST",
         "X:COMMANDER HOWE TER and CAPTAIN ANDRE CT",
         "CH:TGC2",
-        "MAP:5998 J 2");
+        "MAP:5998 J 2",
+        "UNIT:A836");
   }
   
   public static void main(String[] args) {
