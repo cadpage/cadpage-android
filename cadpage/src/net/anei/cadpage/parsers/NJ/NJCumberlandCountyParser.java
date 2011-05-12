1808d27cebc57dc885f44820e2e1757cbc3ebb13
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-05-12 17:02:52 +0000

    Fixed major parsing problems with Cumberland County, NJ parser

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJCumberlandCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJCumberlandCountyParser.java
@@ -10,6 +10,7 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 /*
 Cumberland County, NJ
 Contact: Ryan Feaster <feaster.ryan@gmail.com>
+Sender: E911@co.cumberland.nj.us
 
 E911:E22_INVEST_(VINELAND CITY)1763 CHEROKEE LA_2011-04-24 19:02:46_
 E911:E2902_STRUCTURE_(GREENWICH TWP)176 BAYSIDE RD_2011-04-24 17:27:45_AREA OF THE BAY
@@ -20,6 +21,7 @@ E911:E21_VEHICLE_(VINELAND CITY)S DELSEA DR & W ELMER RD_2011-05-01 18:46:26_
 E911:L2_COMM ALARM_(VINELAND CITY)COURTYARD SCHOOL INC / 1270 S EAST AVE_2011-05-01 17:03:36_
 E911:B2_UNKNOWNF_(VINELAND CITY)1775 ROOSEVELT BLVD / BEHIND_2011-05-01 00:10:13_
 E911:E2901_MVC_(DEERFIELD TWP)BIG OAK RD & MORTON AV_2011-04-30 00:06:19_
+E911:E2901_COMM ALARM_(DEERFIELD TWP)MAJOR OIL / 733 LANDIS AV_2011-05-12 09:29:52_
 
 */
 
@@ -28,7 +30,12 @@ public class NJCumberlandCountyParser extends FieldProgramParser {
   
   public NJCumberlandCountyParser() {
     super("CUMBERLAND COUNTY", "NJ",
-           "CALL ADDR SKIP NAME");
+           "UNIT CALL ADDR SKIP PLACE");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "E911@co.cumberland.nj.us";
   }
   
   @Override
@@ -39,15 +46,39 @@ public class NJCumberlandCountyParser extends FieldProgramParser {
   }
   
   private static final Pattern ADDR_PTN = Pattern.compile("^\\((.*)\\)");
+  private static final Pattern LA_PTN = Pattern.compile(" LA\\b");
   private class MyAddressField extends AddressField {
 
     @Override
     public void parse(String field, Data data) {
+      
+      // City is in front in parenthesis
       Matcher match = ADDR_PTN.matcher(field);
       if (match.find()) {
         data.strCity = match.group(1);
         field = field.substring(match.end()).trim();
       }
+      
+      // Replace LA abbrv with LN
+      field = LA_PTN.matcher(field).replaceAll(" LN");
+      
+      // slash divides address into two parts, either which can be a place name
+      // or they can both be streets
+      
+      int pt = field.indexOf('/');
+      if (pt >= 0) {
+        String fld1 = field.substring(0,pt).trim();
+        String fld2 = field.substring(pt+1).trim();
+        
+        if (checkAddress(fld1) == 0) {
+          data.strPlace = fld1;
+          field = fld2;
+        } else if (checkAddress(fld2) == 0) {
+          data.strPlace = fld2;
+          field = fld1;
+        }
+      }
+      
       super.parse(field, data);
     }
     
@@ -57,9 +88,17 @@ public class NJCumberlandCountyParser extends FieldProgramParser {
     }
   }
   
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strPlace = append(data.strPlace, " / ", field);
+    }
+  }
+  
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("PLACE")) return new MyPlaceField();
     return super.getField(name);
   }
 }
