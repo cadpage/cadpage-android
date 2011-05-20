ffa0439f26e6b7d32613be6ed3539b90ebfc228e
derrick@cadpage.org <derrick@cadpage.org@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-05-20 02:01:04 +0000

    more widget updates. Widget not yet enabled in manifest

--- cadpage/src/net/anei/cadpage/CadPageWidget.java
+++ cadpage/src/net/anei/cadpage/CadPageWidget.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
@@ -8,9 +9,22 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class CadPageWidget extends AppWidgetProvider {
+  
+  public static final String ACTION_SMSPOPUP_ENABLE = "net.anei.cadpage.ManagePreferences.";
+  public static final String ACTION_SMSPOPUP_DISABLE = "net.everythingandroid.smspopup.DISABLE";
+  
   @Override
   public void onUpdate(Context context,AppWidgetManager appWidgetManager, int[] appWidgetIds){
+    final int N = appWidgetIds.length;
+    for (int i=0; i<N; i++) {
+      int appWidgetId = appWidgetIds[i];
     updateEnabled(context,appWidgetManager,appWidgetIds);
+    Intent intent = new Intent(context, CallHistoryActivity.class);
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+    views.setOnClickPendingIntent(R.id.widget_button_cadpage, pendingIntent);
+    appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
   }
   
   public void updateEnabled(Context context,AppWidgetManager appWidgetManager, int[] appWidgetIds) {
