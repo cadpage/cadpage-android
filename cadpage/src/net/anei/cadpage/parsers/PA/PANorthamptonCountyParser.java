cad3ba2561a1bb939795f7fc6a09ceb5aa06d7e9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-05-23 16:57:23 +0000

    Added Northampton County, PA location

---
+++ cadpage/src/net/anei/cadpage/parsers/PA/PANorthamptonCountyParser.java
@@ -0,0 +1,101 @@
+package net.anei.cadpage.parsers.PA;
+
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.SmsMsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchBParser;
+
+/*
+Northampton County, PA
+Contact: Matt Winter <mattgwinter@gmail.com>
+Sender: @notifync.orgaddress
+DispatchBParser subclass
+
+[e49]ALS >ADVANCED LIFE SUPPORT CALL 602 E 21ST ST Apt: 119 Bldg NORTHAMPTON DIANE ECK Map: Grids:0,0 Cad: 2011-0000086714
+[e49]BLS >BASIC LIFE SUPPORT CALL 1323 NEWPORT AVE Apt: REAR Bldg NORTHAMPTON TONY FERRERA Map: Grids:0,0 Cad: 2011-00000
+[e49]MVAI >MVA WITH INJURIES 248 AT PENNSVILLE LIGHT LEHIGH TWP NATALIE BRODIANO Cad: 2011-0000086361
+[e49]ALS >ADVANCED LIFE SUPPORT CALL 612 E 10TH ST NORTHAMPTON ROBERTS PAMELA Map: Grids:0,0 Cad: 2011-0000086262
+[e49]BLS >BASIC LIFE SUPPORT CALL 1001 WASHINGTON AVE Apt: 105 Bldg NORTHAMPTON MEGAN MOREY Map: Grids:0,0 Cad: 2011-000008
+[e49]ALS >ADVANCED LIFE SUPPORT CALL 5962 KEYSTONE DR EAST ALLEN CHRISTINA WIGMER Map: Grids:0,0 Cad: 2011-0000086103
+[e49]BLS >BASIC LIFE SUPPORT CALL 1323 NEWPORT AVE NORTHAMPTON TONY CABRERA Map: Grids:0,0 Cad: 2011-0000086010
+
+*/
+
+public class PANorthamptonCountyParser extends DispatchBParser {
+  
+  private static final String[] CITY_LIST = new String[]{
+    "BETHLEHEM",
+    "EASTON",
+
+    "BANGOR",
+    "BATH",
+    "CHAPMAN",
+    "EAST ALLEN",
+    "EAST BANGOR",
+    "FREEMANSBURG",
+    "GLENDON",
+    "HELLERTOWN",
+    "NAZARETH",
+    "NORTH CATASAUQUA",
+    "NORTHAMPTON",
+    "PEN ARGYL",
+    "PORTLAND",
+    "ROSETO",
+    "STOCKERTOWN",
+    "TATAMY",
+    "WALNUTPORT",
+    "WEST EASTON",
+    "WILSON",
+    "WIND GAP",
+
+    "ALLEN TWP",
+    "BETHLEHEM TWP",
+    "BUSHKILL TWP",
+    "EAST ALLEN TWP",
+    "FORKS TWP",
+    "HANOVER TWP",
+    "LEHIGH TWP",
+    "LOWER MOUNT BETHEL TWP",
+    "LOWER NAZARETH TWP",
+    "LOWER SAUCON TWP",
+    "MOORE TWP",
+    "PALMER TWP",
+    "PLAINFIELD TWP",
+    "UPPER MOUNT BETHEL TWP",
+    "UPPER NAZARETH TWP",
+    "WASHINGTON TWP",
+    "WILLIAMS TWP"
+  };
+  
+  private static final Pattern MARKER = Pattern.compile(">.* Cad: ");
+  
+  @Override
+  public String getFilter() {
+    return "@notifync.orgaddress";
+  }
+ 
+  @Override
+  protected boolean isPageMsg(String body) {
+    return MARKER.matcher(body).find();
+  }
+
+  public PANorthamptonCountyParser() {
+    super(CITY_LIST, "NORTHAMPTON COUNTY", "PA");
+  }
+  
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    data.strUnit = subject;
+    body = body.replace(" CALL ", " AT ");
+    if (!super.parseMsg(body, data)) return false;
+    return true;
+  }
+
+  @Override
+  protected boolean parseAddrField(String field, Data data) {
+    // TODO Auto-generated method stub
+    return super.parseAddrField(field, data);
+  }
+  
+  
+}
