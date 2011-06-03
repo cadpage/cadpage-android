b2386eee2ef20fcc3e33a5db244934fc5a25fea9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-03 01:46:05 +0000

    Added test class

---
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParserTest.java
@@ -0,0 +1,83 @@
+package net.anei.cadpage.parsers.NC;
+
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+
+public class NCWilsonCountyParserTest extends BaseParserTest {
+  
+  public NCWilsonCountyParserTest() {
+    setParser(new NCWilsonCountyParser(), "WILSON COUNTY", "NC");
+  }
+  
+  @Test
+  public void testParser() {
+
+    doTest("T1",
+        "CAD:11000831;06D02;BREATHING PROBLEMS;5036 US 264A E;2C;RANCH FARM CT;HOLDENS CROSS RD;[Medical Priority Info] Key Questions Complete RESPONSE:",
+        "ID:11000831",
+        "CODE:06D02",
+        "CALL:BREATHING PROBLEMS",
+        "ADDR:5036 US 264A E",
+        "SRC:2C",
+        "X:RANCH FARM CT & HOLDENS CROSS RD");
+
+    doTest("T2",
+        "CAD:OUTSIDE FIRE - SMALL;5047 SAWDUST RD;4C;LASSITER MILL RD;TARTTS MILL RD",
+        "CALL:OUTSIDE FIRE - SMALL",
+        "ADDR:5047 SAWDUST RD",
+        "SRC:4C",
+        "X:LASSITER MILL RD & TARTTS MILL RD");
+
+    doTest("T3",
+        "CAD:11000827;69D10;STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;403 enroute [05/19/11 13:53:19 DPITTMAN] [Fire Priority Info] Key Que",
+        "ID:11000827",
+        "CODE:69D10",
+        "CALL:STRUCTURE FIRE 1",
+        "ADDR:4659 EVANSDALE RD",
+        "SRC:5B",
+        "X:OLD STANTONSBURG RD & CASTLEWOOD LN",
+        "INFO:403 enroute");
+
+    doTest("T4",
+        "CAD:STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;291-5847 [05/19/11 13:51:05 SBATTS]",
+        "CALL:STRUCTURE FIRE 1",
+        "ADDR:4659 EVANSDALE RD",
+        "SRC:5B",
+        "X:OLD STANTONSBURG RD & CASTLEWOOD LN",
+        "INFO:291-5847");
+
+    doTest("T5",
+        "CAD:11000827;69D10;STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;403 enroute [05/19/11 13:53:19 DPITTMAN] [Fire Priority Info] Key Que",
+        "ID:11000827",
+        "CODE:69D10",
+        "CALL:STRUCTURE FIRE 1",
+        "ADDR:4659 EVANSDALE RD",
+        "SRC:5B",
+        "X:OLD STANTONSBURG RD & CASTLEWOOD LN",
+        "INFO:403 enroute");
+
+    doTest("T6",
+        "CAD:11000831;06D02;BREATHING PROBLEMS;5036 US 264A E;2C;RANCH FARM CT;HOLDENS CROSS RD;[Medical Priority Info] Key Questions Complete RESPONSE: MEDIC & FR RESPO",
+        "ID:11000831",
+        "CODE:06D02",
+        "CALL:BREATHING PROBLEMS",
+        "ADDR:5036 US 264A E",
+        "SRC:2C",
+        "X:RANCH FARM CT & HOLDENS CROSS RD");
+
+    doTest("T7",
+        "CAD:11000844;ACCIDENT PROPERTY DAMAGE;NC 222/ASPEN GROVE CHURCH RD;2A;on nc 222 just before aspen grove church while on the phone with hp she advised a hissing",
+        "ID:11000844",
+        "CALL:ACCIDENT PROPERTY DAMAGE",
+        "ADDR:NC 222 & ASPEN GROVE CHURCH RD",
+        "SRC:2A",
+        "INFO:on nc 222 just before aspen grove church while on the phone with hp she advised a hissing");
+ }
+  
+
+  public static void main(String[] args) {
+    new NCWilsonCountyParserTest().generateTests("T1");
+  }
+}
