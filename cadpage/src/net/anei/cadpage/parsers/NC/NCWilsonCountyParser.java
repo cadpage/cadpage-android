faabda87d6ce3e5456755012e311d158e24e50bd
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-03 01:45:47 +0000

    Added Wilson County location

--- cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
@@ -24,6 +24,8 @@ CAD:STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;291-
 CAD:11000827;69D10;STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;403 enroute [05/19/11 13:53:19 DPITTMAN] [Fire Priority Info] Key Que
 CAD:11000831;06D02;BREATHING PROBLEMS;5036 US 264A E;2C;RANCH FARM CT;HOLDENS CROSS RD;[Medical Priority Info] Key Questions Complete RESPONSE: MEDIC & FR RESPO
 CAD:11000844;ACCIDENT PROPERTY DAMAGE;NC 222/ASPEN GROVE CHURCH RD;2A;on nc 222 just before aspen grove church while on the phone with hp she advised a hissing
+
+
 CAD:B&E to occupied residence occurred at 612 Fairfax Ave. 2 suspects busted in the back door, homeowner yelled and subjects ran. One subject 10-72 at this time
 CAD:ROLLING DOMESTIC - 701 HNES STREET - MALE PARTY STABBED - BOTH SUBJECTS 10-72 - MALE PARTY HAS NON-LIFE THREATNING INJURIES - AUTH 301 -- WILSON 911 TC LYLE
 
@@ -33,7 +35,7 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
   
   public NCWilsonCountyParser() {
     super("WILSON COUNTY", "NC",
-           "ID? CODE CALL ADDR! SRC X+? INFO+");
+           "ID? CODE? CALL ADDR! SRC X+? INFO+");
   }
 
   @Override
@@ -47,10 +49,18 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
       setPattern(Pattern.compile("\\d{6,}"));
     }
   }
+  
+  // CODE field also has to validate its existence
+  private class MyCodeField extends CodeField {
+    public MyCodeField() {
+      setPattern(Pattern.compile("\\d\\d[A-Z]\\d\\d"));
+    }
+  }
 
   @Override
   protected Field getField(String name) {
     if (name.equals("ID")) return new MyIdField();
+    if (name.equals("CODE")) return new MyCodeField();
     return super.getField(name);
   }
   
