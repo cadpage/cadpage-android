c50d35ba6874b0da95f52ba4907e059ca35abd5a
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-06 19:41:49 +0000

    Updated test class

--- cadpageTest/src/net/anei/cadpage/parsers/PA/PACambriaCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/PA/PACambriaCountyParserTest.java
@@ -4,14 +4,6 @@ import net.anei.cadpage.parsers.BaseParserTest;
 import net.anei.cadpage.parsers.PA.PACambriaCountyParserTest;
 
 import org.junit.Test;
-/*
-[29] 11 Time: 15:02:29 Nature: 17A01G-Alpha FALL Location: 216 MAIN ST-JO | Sta 35
-[29] 11 Time: 11:54:47 Nature: 29B01-Bravo VEH ACC WITH INJURIES Location: 110 PLAZA DR-LY | Sta 30, Sta 23, Sta 35
-[29] 11 Time: 08:03:01 Nature: 10C02-Charlie CHEST PAIN Location: 315 LOCUST ST-JO | Sta 35 
-[29] 11 Time: 06:45:29 Nature: 17B03G-Bravo FALL Location: WALNUT ST-JO/MAIN ST-JO | Sta 35
-[28] 11 Time: 21:39:31 N t 06D03A D lt BREATHING PROBLEMS Location: 205 CHANDLER AVE-JO | Sta 35, Sta 21, Sta 36
-
-*/
 
 public class PACambriaCountyParserTest extends BaseParserTest {
   
@@ -22,7 +14,7 @@ public class PACambriaCountyParserTest extends BaseParserTest {
  
   
   @Test
-  public void testParser() {
+  public void testParser1() {
 
     doTest("T1",
         "[29] 11 Time: 15:02:29 Nature: 17A01G-Alpha FALL Location: 216 MAIN ST-JO | Sta 35",
@@ -58,8 +50,33 @@ public class PACambriaCountyParserTest extends BaseParserTest {
         "ADDR:205 CHANDLER AVE",
         "CITY:JOHNSTOWN",
         "UNIT:Sta 35, Sta 21, Sta 36");
+  
+  }
+  
+  @Test
+  public void testParser2() {
 
-    
+    doTest("T1",
+        "Date: 06/02/11\nTime: 20:47:52\nNature: 10C01-Charlie CHEST PAIN\nLocation: 1518 W 2ND ST-CR\n| Sta 75",
+        "CALL:10C01-Charlie CHEST PAIN",
+        "ADDR:1518 W 2ND ST",
+        "CITY:CARROLLTOWN",
+        "UNIT:Sta 75");
+
+    doTest("T2",
+        "Date: 06/04/11\nTime: 11:03:04\nNature: 06D02-Delta\2sBREATHING PROBLEMS\nLocation: 908 SHORT AVE-CR\n| Sta 75",
+        "CALL:06D02-Delta\2sBREATHING PROBLEMS",
+        "ADDR:908 SHORT AVE",
+        "CITY:CARROLLTOWN",
+        "UNIT:Sta 75");
+
+    doTest("T3",
+        "Date: 06/04/11\nTime: 17:24:03\nNature: STB-STAND BY\nLocation: 721 MAIN ST-PORB\n| Sta 73",
+        "CALL:STB-STAND BY",
+        "ADDR:721 MAIN ST",
+        "CITY:PORTAGE",
+        "UNIT:Sta 73");
+
   }
   
   
