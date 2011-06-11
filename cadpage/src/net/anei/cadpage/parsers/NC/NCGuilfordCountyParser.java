b79b96524c14d7aca40de5d697e2cf678824932c
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-11 00:34:00 +0000

    Fixed parsing problems with Guilford County, NC parser

--- cadpage/src/net/anei/cadpage/parsers/NC/NCGuilfordCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCGuilfordCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.SmsMsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
@@ -21,20 +24,25 @@ System: OSSI CAD?
 (summerfieldfiredist Jan24-10:31) CAD:UNDER CONTROL;7607 BLUE SAGE CT; SU
 (summerfieldfiredist Jan23-11:25) CAD:UNDER CONTROL;6308 PEPPERMILL DR; OAK
 (summerfieldfiredist Jan23-19:48) CAD:UNDER CONTROL;4529 PEEPLES RD; OAK
+(summerfieldfiredist Jun10-01:00) summerfieldfiredist\nCAD:UNDER CONTROL;5322 BUNCH RD; SU
+(summerfieldfiredist Jun10-19:27) summerfieldfiredist\nCAD:FD13;ST58;1;60C02;GAS;210 JAMES DOAK PKWY;BRIXHAM DR
 
-Not yet implemented
 Contact: krpaschal@gmail.com <krpaschal@gmail.com>
 Sender: CAD@greensboro-nc.gov
 CAD@greensboro-nc.gov:CAD:CODE 47;7700 NEWSOME CT; B;CARDIAC / RESPIRATORY ARREST
+
+
 
 
 */
 
 public class NCGuilfordCountyParser extends DispatchOSSIParser {
   
+  private static final Pattern MARKER = Pattern.compile("^([\\w@\\-\\.]+?)[\n:](?=CAD:)");
+  
   public NCGuilfordCountyParser() {
     super("GUILFORD COUNTY", "NC",
-           "( CALL2 ADDR! | SKIP MUTUAL ADDR! | INFO INFO INFO CODE? CALL ADDR! ) X X");
+           "( CALL2 ADDR! | SKIP MUTUAL ADDR! | INFO INFO INFO CODE? CALL ADDR! ) XINFO+");
   }
   
   @Override
@@ -43,7 +51,15 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
   }
   
   @Override
-  protected boolean parseMsg(String body, Data data) {
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() > 0) {
+      data.strSource = new Parser(subject).get(' ');
+    }
+    Matcher match = MARKER.matcher(body);
+    if (match.find()) {
+      data.strSource = match.group(1);
+      body = body.substring(match.end()).trim();
+    }
     if (!super.parseMsg(body, data)) return false;
     
     // If out of county mutual aid call, cancel the default county
@@ -82,12 +98,35 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
       return true;
     }
   }
+  
+  private class CrossInfoField extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      if (checkAddress(field) > 0) {
+        data.strCross = append(data.strCross, " & ", field);
+      } else {
+        data.strSupp = append(data.strSupp, " / ", field);
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "X INFO";
+    }
+  }
 
   @Override
   protected Field getField(String name) {
     if (name.equals("CALL2")) return new Call2Field();
     if (name.equals("MUTUAL")) return new MutualField();
+    if (name.equals("XINFO")) return new CrossInfoField();
     return super.getField(name);
+  }
+  
+  @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
   }
   
 }
