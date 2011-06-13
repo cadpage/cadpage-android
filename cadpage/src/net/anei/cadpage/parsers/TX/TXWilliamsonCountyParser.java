7ba9d9754afc6eb79bec35eb2de4cf3839d5a9cf
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-13 17:38:21 +0000

    Misc fixes

--- cadpage/src/net/anei/cadpage/parsers/TX/TXWilliamsonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXWilliamsonCountyParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.TX;
 
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.SmsMsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 /*
@@ -27,6 +28,17 @@ public class TXWilliamsonCountyParser extends DispatchOSSIParser {
   public TXWilliamsonCountyParser() {
     super("WILLIAMSON COUNTY", "TX",
            "HEAD? CALL ADDR UNIT+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "cad@wilco.org";
+  }
+  
+  @Override
+  public boolean parseMsg(String body, Data data) {
+    body = body.replaceAll("SH 130 TOLL", "TX 130");
+    return super.parseMsg(body, data);
   }
   
   private class HeadField extends SkipField {
