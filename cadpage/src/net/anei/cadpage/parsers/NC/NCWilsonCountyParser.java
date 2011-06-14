8619f37e20d26ba09780890d0e03d33530ef4dd0
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-06-14 17:42:18 +0000

    Fixed parsing problem at Wilson Count, NC location.

--- cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
@@ -24,10 +24,9 @@ CAD:STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;291-
 CAD:11000827;69D10;STRUCTURE FIRE 1;4659 EVANSDALE RD;5B;OLD STANTONSBURG RD;CASTLEWOOD LN;403 enroute [05/19/11 13:53:19 DPITTMAN] [Fire Priority Info] Key Que
 CAD:11000831;06D02;BREATHING PROBLEMS;5036 US 264A E;2C;RANCH FARM CT;HOLDENS CROSS RD;[Medical Priority Info] Key Questions Complete RESPONSE: MEDIC & FR RESPO
 CAD:11000844;ACCIDENT PROPERTY DAMAGE;NC 222/ASPEN GROVE CHURCH RD;2A;on nc 222 just before aspen grove church while on the phone with hp she advised a hissing
-
-
 CAD:B&E to occupied residence occurred at 612 Fairfax Ave. 2 suspects busted in the back door, homeowner yelled and subjects ran. One subject 10-72 at this time
 CAD:ROLLING DOMESTIC - 701 HNES STREET - MALE PARTY STABBED - BOTH SUBJECTS 10-72 - MALE PARTY HAS NON-LIFE THREATNING INJURIES - AUTH 301 -- WILSON 911 TC LYLE
+CAD:11000968;67D02R;OUTSIDE FIRE - WOODS/LRG BRUSH;4164 NC 42 E;4C;KENT RD;VARNELL RD;[Fire Priority Info] Key Questions Complete RESPONSE: Delta RESPONDER SCRI
 
 */
 
@@ -53,7 +52,7 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
   // CODE field also has to validate its existence
   private class MyCodeField extends CodeField {
     public MyCodeField() {
-      setPattern(Pattern.compile("\\d\\d[A-Z]\\d\\d"));
+      setPattern(Pattern.compile("\\d\\d[A-Z]\\d\\d[A-Z]?"));
     }
   }
 
