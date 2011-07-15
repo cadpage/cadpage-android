e4ce3d99069079184585ab863b6d66f15fa72d09
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-07-15 02:33:39 +0000

    Modified Benton County, OR to allow for some cites in south end of Philomath District that do not apparently have Philomath addresses

--- cadpage/src/net/anei/cadpage/parsers/OR/ORBentonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORBentonCountyParser.java
@@ -1,8 +1,6 @@
 package net.anei.cadpage.parsers.OR;
 
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import net.anei.cadpage.SmsMsgInfo.Data;
 import net.anei.cadpage.parsers.SmsMsgParser;
@@ -14,11 +12,26 @@ Sender: alerts@corvallis.ealertgov.com
 (Corvallis Alert) INC: CODE 1 MEDICAL\nADD:1740 MAIN ST\nAPT:\nCITY:PHILOMATH\nX:N 17TH ST * N 18TH ST\nMAP:540-365\nCFS:0907010-119\nDIS:PHILOMATH FIRE
 (Corvallis Alert) INC:COMM FIRE ALARM\nADD:421 S 19TH ST\nAPT:\nCITY:PHILOMATH\nX:ASH ST * CEDAR ST\nMAP:540-360\nCFS:100410-188\nDIS:PHILOMATH FIRE\nDIS:PHI
 (Corvallis Alert) INC:UNKNOWN MEDICAL\nADD:504 MELVILL CRESCENT AV [PHILOMATH]\nAPT:\nCITY:PHILOMATH\nX:SUNSHINE AV\nMAP:550-385\nCFS:101710-223\nDIS:PHILOM
+(Corvallis Alert) INC:BICYCLE ACCIDENT\nADD:DECKER RD and DECKER RIDGE RD\nAPT:\nCITY:PHILOMATH\nX:DECKER RD * BANKS LN\nMAP:480-420\nCFS:070611-52\nDIS:BEN
+
 */
 
 public class ORBentonCountyParser extends SmsMsgParser {
   
-  private static final Pattern ADDRESS_PTN = Pattern.compile("(\\d+) *(.*)");
+  // List of streets that extend wholly or partly into a region that Google does
+  // not recognize as part of Philomath
+  private static final String[] NOT_IN_PHILOMATH = new String[]{
+    "BANKS",
+    "BREWSTER",
+    "BEAVER CREEK",
+    "BLUEROCK",
+    "DECKER",
+    "GRASS HEIGHTS",
+    "LINVILLE",
+    "NEUMAN",
+    "PETERSON",
+    "STARR CREEK"
+  }; 
   
   public ORBentonCountyParser() {
     super("BENTON COUNTY", "OR");
@@ -57,10 +70,11 @@ public class ORBentonCountyParser extends SmsMsgParser {
     // Now for some special fixes to work around Dispatch map issues
     if (data.strCity.equals("PHILOMATH")) {
       data.strAddress = data.strAddress.replace("MELVILL CRESCENT AV","MELVILL CRESCENT");
-      if (data.strAddress.contains("BEAVER CREEK RD")) {
-        Matcher match = ADDRESS_PTN.matcher(data.strAddress);
-        if (match.matches()) {
-          if (Integer.parseInt(match.group(1)) >= 29397) data.strCity = "CORVALLIS";
+      String tmp = data.strAddress.toUpperCase();
+      for (String badRoad : NOT_IN_PHILOMATH) {
+        if (tmp.contains(badRoad)) {
+          data.strCity = "";
+          break;
         }
       }
     }
