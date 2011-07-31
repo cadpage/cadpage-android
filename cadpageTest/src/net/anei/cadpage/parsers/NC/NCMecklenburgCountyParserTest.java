59dabf5dc835d53d2a82f0d22830f20c3dcaaf8e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-07-31 03:18:05 +0000

    Updated test class

--- cadpageTest/src/net/anei/cadpage/parsers/NC/NCMecklenburgCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCMecklenburgCountyParserTest.java
@@ -39,7 +39,8 @@ public class NCMecklenburgCountyParserTest extends BaseParserTest {
         "ADDR:19180-19209 Coachmans Trace",
         "PLACE:GLENRIDGE",
         "INFO:Fire -  Emergency",
-        "CALL:69-Structure Fire             Meadow Crossing L");
+        "CALL:69-Structure Fire",
+        "X:Meadow Crossing L");
 
     doTest("T5",
         "Subject:Incoming Message\n16738 Amberside Rd East                 Alexander Chase Condos        Fire -  Emergency             52F-Alarm-FIRE",
@@ -68,11 +69,32 @@ public class NCMecklenburgCountyParserTest extends BaseParserTest {
         "ADDR:19432 Fridley Ln",
         "PLACE:medical alarm .. no pt info ..",
         "INFO:Charlie",
-        "CALL:32- Unknown problem (man down)Denae Lynn");
+        "CALL:32- Unknown problem (man down)",
+        "X:Denae Lynn");
+
+    doTest("T9",
+        "9120 Willow Ridge Rd          307A      Regency Retirement            Charlie                       26- Sick person               Goodsell",
+        "ADDR:9120 Willow Ridge Rd",
+        "APT:307A",
+        "PLACE:Regency Retirement",
+        "INFO:Charlie",
+        "CALL:26- Sick person",
+        "X:Goodsell");
+
+    doTest("T10",
+        "12463 Stratfield Place Cr               CAROLINA CROSSING SUBD        Delta                         31- Unconsciousness/Fainting  Stineway Ct/Sam Meeks Rd                                    PINR3     Map - 405405/J9 07302011-086",
+        "ADDR:12463 Stratfield Place Cr",
+        "PLACE:CAROLINA CROSSING SUBD",
+        "INFO:Delta",
+        "CALL:31- Unconsciousness/Fainting",
+        "X:Stineway Ct/Sam Meeks Rd",
+        "CH:PINR3",
+        "MAP:405405/J9",
+        "ID:07302011-086");
  }
   
 
   public static void main(String[] args) {
-    new NCMecklenburgCountyParserTest().generateTests("T8", "ADDR APT PLACE INFO CALL");
+    new NCMecklenburgCountyParserTest().generateTests("T11", "ADDR APT PLACE INFO CALL X CH MAP ID");
   }
 }
