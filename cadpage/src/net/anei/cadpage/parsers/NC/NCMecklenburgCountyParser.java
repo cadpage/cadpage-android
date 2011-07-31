0a67f5140da8effe670439537321c716e758470a
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-07-31 03:17:44 +0000

    Fixed parsing problem with Meclenburg County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCMecklenburgCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCMecklenburgCountyParser.java
@@ -31,19 +31,21 @@ Subject:Incoming Message\n10043 Idlewild Rd             126       Willow Grove R
 Contact: David Boyd <rookiefire14@gmail.com>
 (Incoming Message) 19432 Fridley Ln                        medical alarm .. no pt info ..Charlie                       32- Unknown problem (man down)Denae Lynn
 
+Contact: Michael Filliben <mfillibene16@gmail.com>
+Sender: Group_Page_Notification@archwireless.net
+9120 Willow Ridge Rd          307A      Regency Retirement            Charlie                       26- Sick person               Goodsell
+12463 Stratfield Place Cr               CAROLINA CROSSING SUBD        Delta                         31- Unconsciousness/Fainting  Stineway Ct/Sam Meeks Rd                                    PINR3     Map - 405405/J9 07302011-086
+
 ** NOT IMPLEMENTED **
 Contact: John Stroup <j.stroup@northmeckrescue.org>
 Subject:Incoming Message\n15503 N Old Statesville Rd North Meck Rescue Charlie 12- Convulsion/Seizure 60 Foot St/Unnamed St Nmr1 Map - 266266/
 Subject:Incoming Message\n15503 N Old Statesville Rd North Meck Rescue Charlie 12- Convulsion/Seizure 60 Foot St/Unnamed St Nmr2 Map - 266266/
 
-Contact: Michael Filliben <mfillibene16@gmail.com>
-Sender: Group_Page_Notification@archwireless.net
-9120 Willow Ridge Rd          307A      Regency Retirement            Charlie                       26- Sick person               Goodsell
-
-
 */
 
 public class NCMecklenburgCountyParser extends SmsMsgParser {
+  
+  private static final String[] PRI_VALUES = new String[]{"Charlie", "Delta", "Fire - Emergency"};
   
   public NCMecklenburgCountyParser() {
     super("MECKLENBURG COUNTY", "NC");
@@ -51,22 +53,47 @@ public class NCMecklenburgCountyParser extends SmsMsgParser {
   
   @Override
   public String getFilter() {
-    return "paging@rcscom.com,@huntersvillefd.com";
+    return "paging@rcscom.com,@huntersvillefd.com,Group_Page_Notification@archwireless.net";
   }
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     
-    if (!subject.equals("Text Page") && !subject.equals("Incoming Message")) return false;
     if(body.length() < 110) return false;
     if (body.contains("Received:")) return false;
-    parseAddress(body.substring(0,30).trim(), data);
-    data.strApt = body.substring(30,40).trim();
-    data.strPlace = body.substring(40,70).trim();
-    data.strSupp = body.substring(70,100).trim();
-    data.strCall = body.substring(100).trim();
     
-    return true;
+    boolean good = subject.equals("Text Page") || subject.equals("Incoming Message");
+    
+    parseAddress(substring(body,0,30), data);
+    data.strApt = substring(body,30,40);
+    data.strPlace = substring(body,40,70);
+    data.strSupp = substring(body,70,100);
+    data.strCall = substring(body,100,130);
+    data.strCross = substring(body,130,190);
+    data.strChannel = substring(body,190,200);
+    data.strMap = substring(body,206,216);
+    data.strCallId = substring(body,216);
+
+    if (!good) {
+      for (String priVal : PRI_VALUES) {
+        if (data.strSupp.equals(priVal)) {
+          good = true;
+          break;
+        }
+      }
+    }
+    return good;
+  }
+  
+  private String substring(String body, int st) {
+    return substring(body, st, Integer.MAX_VALUE);
+  }
+  
+  private String substring(String body, int st, int end) {
+    int len = body.length();
+    if (st >= len) return "";
+    if (end > len) end = len;
+    return body.substring(st, end).trim();
   }
 
 }
