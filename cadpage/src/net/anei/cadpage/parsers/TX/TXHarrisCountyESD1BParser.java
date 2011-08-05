9f35c680a3e796daead4a3b3b74486fc9e87a794
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-08-05 01:57:08 +0000

    Fixed problem with Harris County, TX (B)

--- cadpage/src/net/anei/cadpage/parsers/TX/TXHarrisCountyESD1BParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXHarrisCountyESD1BParser.java
@@ -9,6 +9,7 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 Harris County ESD #1, TX
 Contact: Nathan Mathews <nbmathews807@gmail.com>
 Sender: cadnoreply@proxy.hcec.com
+Sender: 930010nn
 ID#:11-06-20685 - - TRASH FIRE - 111 Bayou Dr - Apt:C - Bldg: - Key Map: 498D - Cross Streets:EAST FWY/MARKET - Box #:2002
 ID#:11-06-20710 - - FIRE - PreAlert - 16217 Ridlon - Apt: - Bldg: intra SERVICES - Key Map: 498C - Cross Streets:SHEILA LN/WOODLAND D
 ID#:11-06-20658 - - 29A1 MOTOR VEHICLE A - East Fwy / Delldale - Apt: - Bldg: - Key Map: 498E - Cross Streets: - Box #:2004
@@ -16,36 +17,13 @@ ID#:11-06-20627 - - 29A1 MOTOR VEHICLE A - 16410 Ave D - Apt:1 - Bldg: AVE D APT
 
 *** NOT IMPLEMENTED YET ***
 ID#:11-07-25331 - - ALARMS - 1518 Great Dover Cir - Apt: - Bldg: - Key Map: 458W
-ID#:11-07-25331 - UNIT:L32 - DISP: 10:27:45 - ER: 10:27:49 - OUT: 10:41:48 - TO:
 ID#:11-07-25341 - 06D02 - 6D1 RESPIRATORY - 11870 Greenloch Ln - Apt: - Bldg: - Key Map: 416K - Cross Streets:GREENCANYON DR/GREENROC 
-ID#:11-07-25342 - 25A01 - PSYCHIATRIC - 15215 S Brentwood Dr - Apt: - Bldg: - Ke
-ID#:11-07-25342 - 25A01 - PSYCHIATRIC - 15215 S Brentwood Dr - Apt: - Bldg: - Ke
-y Map: 498E - Cross Streets:Dead End/DELLDALE - Box #:2004
-
+ID#:11-07-25342 - 25A01 - PSYCHIATRIC - 15215 S Brentwood Dr - Apt: - Bldg: - Key Map: 498E - Cross Streets:Dead End/DELLDALE - Box #:2004
 ID#:11-07-25378 - - Stroke (CVA) - 11715 Greenglen Dr - Apt: - Bldg: - Key Map: 416K - Cross Streets:JOHN RALSTON RD/GREENROCK LN - B 
-ID#:11-07-25378 - UNIT:M15 - DISP:19:01:23 - ER:19:03:02 - OUT:19:11:01 - TO:19:26:34 - AT:19:40:29 
-ID#:11-07-25378 - UNIT:M15 - DISP: 19:01:23 - ER: 19:03:02 - OUT: 19:11:01 - TO: 19:26:34 - AT: 19:40:29 - AIR: 19:56:20 
 ID#:11-07-25416 - - HEMORRHAGE/LACERATIO - 12370 S San Circle Dr - Apt: - Bldg: UNK TRAILER PARK - Key Map: 456C - Cross Streets:BARK 
-:3304 
-
-Sender: 930010nn
-ID#:11-07-25442 - - UNKNOWN PROBLEM - 16208 Wood Dr - Apt:21 - Bldg: fuente de s
-ID#:11-07-25442- UNIT:E22 - DISP: 08:10:13 - ER: - OUT: - AIR: 08:11:39
-olas jarr - Key Map: 458U - Cross Streets:BECKER/AMY MICHELLE - Box #:2003
-
-- AT: - AIR: 15:58:38
-
-ID#:11-08-25623 - UNIT:BRU22 - DISP: 17:31:20 - ER: 17:33:52 - OUT: 17:36:17 - T
-O: - AT: - AIR: 18:12:20
-
-- Bldg: - Key Map: 498N - Cross Streets: - Box #:2001
-
-ID#:11-08-25632 - UNIT:E12 - DISP: 18:59:45 - ER: 19:01:09 - OUT: - TO: - AT: -
-
-Box #:2002
-ID#:11-08-25733 - 55B04 - ELECTRICAL HAZARD - 16102 E Ih 10 - Apt: - Bldg: OLD R
-IVER CHURCH DAY - Key Map: 498G - Cross Streets:IH 10 EAST BAYOU RAMP/IH 10 SH -
-ID#:11-08-25733 - 55B04 - ELECTRICAL HAZARD - 16102 E Ih 10 - Apt: - Bldg: OLD R
+ID#:11-08-25733 - 55B04 - ELECTRICAL HAZARD - 16102 E Ih 10 - Apt: - Bldg: OLD RIVER CHURCH DAY - Key Map: 498G - Cross Streets:IH 10 EAST BAYOU RAMP/IH 10 SH -
+ID#:11-08-25969 - 29D02m - 29D5 MOTOR VEHICLE A - 539 S Sheldon Rd - Apt: - Bldg: - Key Map: 498K - Cross Streets:JACINTO PORT BLVD/MARKET - Box #:2001
+ID#:11-08-25936 - 29B04 - Traffic/Trans Incide - E BW 8 N / WOODFOREST BW 8 NB - Apt: - Bldg: - Key Map: 457Y - Cross Streets: - Box #:2005
 
 */
 
@@ -56,17 +34,16 @@ public class TXHarrisCountyESD1BParser extends FieldProgramParser {
 
   public TXHarrisCountyESD1BParser() {
     super("HARRIS COUNTY", "TX",
-           "ID:ID! CODE? CALL! PREALERT? ADDR! Apt:APT! Bldg:APT! Key_Map:MAP% Cross_Streets:X Box:BOX");
+           "ID#:ID! CODE? CALL! PREALERT? ADDR! Apt:APT Bldg:APT Key_Map:MAP% Cross_Streets:X Box_#:BOX");
   }
   
   @Override
   public String getFilter() {
-    return "cadnoreply@proxy.hcec.com";
+    return "cadnoreply@proxy.hcec.com,93001";
   }
   
   @Override
   public boolean parseMsg(String body, Data data) {
-    body = body.replace("ID#:","ID:").replace("Box #:", "Box:");
     return parseFields(DELIM.split(body), data);
   }
   
