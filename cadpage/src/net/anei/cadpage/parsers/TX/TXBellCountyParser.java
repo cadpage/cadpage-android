d920275298503c23259062f36a82728caa74a5db
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-08-13 03:09:02 +0000

    Updated Bell County, TX sender filter

--- cadpage/src/net/anei/cadpage/parsers/TX/TXBellCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXBellCountyParser.java
@@ -8,7 +8,7 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 /* 
 Bell County, TX
 Contact: Jonathan Phelps <texasking254@yahoo.com>
-Sender: +17192437900
+Sender:930010xx
 
 LOC: 640 SCARLET OAK DR BELL TYPE CODE: STRUCT SubType: CALLER NAME: CLRNUM: TIME: 14:35:00 Comments: AC UNIT ON FIRE
 LOC: CHAPPARAL RD/E TRIMMIER RD BELL TYPE CODE: TTA SubType: CALLER NAME: CLAY/CBF&R 513 CLRNUM: 830-426-0696 TIME: 15:38:34 Comments: TX 8304260696 -097.690451 +031.030079 CALLED IN BY A CENTAL BELL FIRE &RECUE NOT BLOCKING, BOTH VEH IN DIRT 5 INDIVIDUAL INVOLVED - 4 FEM, 1 MALE 2 VEH ACC Number of patients: 5 Age: Unknown Range Gender: Unknown Conscious: Unknown Breathing: Unknown ProQA chief complaint code: 29 Responder script: Age unknown, 2 VEH ACC. RED HYUNDAI SEDAN VS TAN SUBURBAN SUBURBAN RADIATOR IS LEAKING AIRBAG DEPLOYED ON SEDAN
@@ -27,6 +27,10 @@ public class TXBellCountyParser extends FieldProgramParser {
   public TXBellCountyParser() {
     super(CITY_CODES, "BELL COUNTY", "TX",
         "LOC:ADDR/S! TYPE_CODE:CALL! SubType:CALL CALLER_NAME:NAME! CLRNUM:PHONE! TIME:SKIP! Comments:INFO");
+  }
+  
+  public String getFilter() {
+    return "930010";
   }
   
   private class MyAddressField extends AddressField {
