d227772b37f48383877dd52af1ef178ec5a14518
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-08-22 17:53:29 +0000

    Improved parsing for Steuben County, NY Updated user list

--- cadpage/src/net/anei/cadpage/parsers/NY/NYSteubenCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYSteubenCountyParser.java
@@ -70,11 +70,172 @@ Contact: "Conrad, Robert" <RobertC@co.steuben.ny.us>
 /MILE 154 ( I86 BETWEEN EXIT 39 + ) 29D2 Traffic Accidents High mechanism BTFD:2011:177
 --{Don't Respond: Controlled Burn}-- 7630 COUNTY ROUTE 16 , BATH TOWN OF (QUINN RD / FREEMAN HOLLOW RD) Controlled Burn
 105 GENEVA ST , BATH VILLAGE OF (STATE ROUTE 54 / GRATTON DR; Near: LAKEVIEW APARTMENTS) AUTOMATIC ALARM Fire BTFD:2011:263
+[2ndCall] 25 MAINE ST , BATH VILLAGE OF (ROBIE ST / HAVERLING ST) 1D1 Abdominal Pain Not alert BATHAMB:2011:1393 
+[2ndCall] 3 CLUB VIEW DR , BATH VILLAGE OF ( / MAY ST) 28C06 Stroke (CVA) Sudden loss of balance or coordination BATHAMB:2011:1392
 
 */
 
 
 public class NYSteubenCountyParser extends SmartAddressParser {
+  
+  public NYSteubenCountyParser() {
+    super(CITY_LIST, "STEUBEN COUNTY", "NY");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "messaging@iamresponding.com";
+  }
+
+  private static final Pattern MARKER = Pattern.compile("^messaging@iamresponding.com \\( *(.*?) *\\) ");
+  private static final Pattern MASTER1 = Pattern.compile("/ *(.*?) *\\( *(.*?) *\\)  *(?:(\\d{1,2}[A-Z]\\d{1,2}) +)?(.*)");
+  private static final Pattern MASTER2 = Pattern.compile("(.*?) *, *(.*?) *\\( *(.*?/.*?) *\\) *(?:(\\d{1,2}[A-Z]\\d{1,2}) +)?(.*)");
+  private static final Pattern MASTER3 = Pattern.compile("(.*?) *, *([^\\(\\)]*?) *(\\d{1,2}[A-Z]\\d{1,2}) +(.*)");
+  private static final Pattern MASTER4 = Pattern.compile("(.*?) *, *([^\\(\\)]*?) (?:TOWN|VILLAGE) OF +(.*)");
+
+  
+  private static final Pattern TRAILER = Pattern.compile("\\n[A-Z]+(?::\\d*)*$");
+  private static final Pattern TRAILER2 = Pattern.compile("\\b([A-Z]+)(?::\\d+){2}$");
+  
+  private static final Pattern INFO_HEADER = Pattern.compile("^--\\{(.*)\\}--");
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) { 
+
+    // There are a couple differnent text signatures we look for
+    boolean sigMatch = true;
+    Matcher match;
+    do {
+	    // Look for starting page signature
+	    match = MARKER.matcher(body);
+	    if (match.find()) {
+	      data.strSource = match.group(1);
+	      body = body.substring(match.end()).trim();
+	      break;
+	    }
+	    
+	    // Sometimes the starting sender address is missing, which means the
+	    // station code ends up in the description
+	    if (subject.endsWith(" FD")) {
+	      data.strSource = subject;
+	      break;
+	    }
+	    
+	    // Nothing worked, we will keep trying, but will have a much stricter 
+	    // standard of what constitutes a CAD page
+	    sigMatch = false;
+    } while (false);
+
+    // If we found a start signature match, which included a station source
+    // Look for and remove any trailing station and call ID
+    if (sigMatch) {
+      match = TRAILER.matcher(body);
+      if (match.find()) body = body.substring(0,match.start()).trim();
+    }
+    
+    // If we didn't fidn a start signature match, we have a stricter definition
+    // of what the trailer should look like, and we will extract the source code
+    // from it
+    else {
+      match = TRAILER2.matcher(body);
+	    if (match.find()) {
+	      data.strSource = match.group(1);
+	      body = body.substring(0,match.start()).trim();
+	    }
+    }
+    body = body.replace('\n', ' ');
+    
+    // Check for special information header
+    match = INFO_HEADER.matcher(body);
+    if (match.find()) {
+      data.strSupp = match.group(1);
+      body = body.substring(match.end()).trim();
+    }
+    
+    // Check for RECALLED tag
+    if (body.startsWith("::::RECALLED:::::: ")) {
+      data.strCall = "RECALLED - ";
+      body = body.substring(19).trim();
+    } else if (subject.equals("2ndCall")) {
+      data.strCall = "2ndCall - ";
+    }
+    
+    body = body.replace("COUNTY ROUTE", "COUNTY ROAD");
+    
+    // There a couple basic pattern
+    // /<place> (<addr city) [code] <call>
+    if ((match = MASTER1.matcher(body)).matches()) {
+      parseAddress(match.group(1), data);
+      data.strPlace = match.group(1);
+      parseAddress(StartType.START_ADDR, match.group(2), data);
+      String left = getLeft();
+      if (left.startsWith("TOWN OF")) left = left.substring(7).trim();
+      else if (left.startsWith("VILLAGE OF")) left = left.substring(10).trim();
+      data.strCross = left;
+      data.strCode = cleanCode(match.group(3));
+      data.strSupp = append(data.strSupp, " / ", match.group(4));
+    }
+    
+    // <addr> , <city> ( <cross> ) [code] <info>
+    else if ((match = MASTER2.matcher(body)).matches()) {
+      parseAddress(match.group(1), data);
+      data.strCity = cleanCity(match.group(2));
+      data.strCross = cleanCross(match.group(3));
+      data.strCode = cleanCode(match.group(4));
+      data.strSupp = append(data.strSupp, " / ", match.group(5));
+    }
+    
+    // <addr> , <city> <code> <info>
+    else if ((match = MASTER3.matcher(body)).matches()) {
+      parseAddress(match.group(1), data);
+      data.strCity = cleanCity(match.group(2));
+      data.strCode = cleanCode(match.group(3));
+      data.strSupp = append(data.strSupp, " / ", match.group(4));
+    }
+    
+    // <addr> , <city> <info>
+    else if ((match = MASTER4.matcher(body)).matches()) {
+      parseAddress(match.group(1), data);
+      data.strCity = cleanCity(match.group(2));
+      data.strSupp = append(data.strSupp, " / ", match.group(3));
+    }
+    
+    // Anything else is just a info
+    // If we didn't find a matching start signature and didn't find a
+    // master pattern match, the we had better conclude that this is not
+    // reall a CAD page
+    else {
+      if (!sigMatch) return false;
+      data.strSupp = append(data.strSupp, " / ", body);
+    }
+    
+    if (data.strSupp.length() > 0 && data.strSupp.length() <= 40) {
+      data.strCall = data.strCall + data.strSupp;
+      data.strSupp = "";
+    } else {
+      data.strCall = data.strCall + "ALERT";
+    }
+    return true;
+  }
+
+  private String cleanCity(String field) {
+    if (field.endsWith(" TOWN OF")) {
+      field = field.substring(0, field.length()-8).trim();
+    } else if (field.endsWith(" VILLAGE OF")) {
+      field = field.substring(0, field.length()-11);
+    }
+    return field;
+  }
+
+  private String cleanCross(String field) {
+    if (field.startsWith("/")) field = field.substring(1).trim();
+    if (field.endsWith("/")) field = field.substring(0,field.length()-1).trim();
+    return field;
+  }
+  
+  private String cleanCode(String field) {
+    return (field == null ? "" : field);
+  }
   
   private static final String[] CITY_LIST = new String[]{
     "ADDISON",
@@ -122,162 +283,5 @@ public class NYSteubenCountyParser extends SmartAddressParser {
     "WHEELER",
     "WOODHULL"
   };
-    
-    public NYSteubenCountyParser() {
-      super(CITY_LIST, "STEUBEN COUNTY", "NY");
-    }
-    
-    @Override
-    public String getFilter() {
-      return "messaging@iamresponding.com";
-    }
-
-    private static final Pattern MARKER = Pattern.compile("^messaging@iamresponding.com \\( *(.*?) *\\) ");
-    private static final Pattern MASTER1 = Pattern.compile("/ *(.*?) *\\( *(.*?) *\\)  *(?:(\\d{1,2}[A-Z]\\d{1,2}) +)?(.*)");
-    private static final Pattern MASTER2 = Pattern.compile("(.*?) *, *(.*?) *\\( *(.*?/.*?) *\\) *(?:(\\d{1,2}[A-Z]\\d{1,2}) +)?(.*)");
-    private static final Pattern MASTER3 = Pattern.compile("(.*?) *, *([^\\(\\)]*?) *(\\d{1,2}[A-Z]\\d{1,2}) +(.*)");
-    private static final Pattern MASTER4 = Pattern.compile("(.*?) *, *([^\\(\\)]*?) (?:TOWN|VILLAGE) OF +(.*)");
-
-    
-    private static final Pattern TRAILER = Pattern.compile("\\n[A-Z]+(?::\\d*)*$");
-    private static final Pattern TRAILER2 = Pattern.compile("\\b([A-Z]+)(?::\\d+){2}$");
-    
-    private static final Pattern INFO_HEADER = Pattern.compile("^--\\{(.*)\\}--");
-    
-	  @Override
-	  protected boolean parseMsg(String subject, String body, Data data) { 
-
-	    // There are a couple differnent text signatures we look for
-	    boolean sigMatch = true;
-	    Matcher match;
-	    do {
-  	    // Look for starting page signature
-  	    match = MARKER.matcher(body);
-  	    if (match.find()) {
-  	      data.strSource = match.group(1);
-  	      body = body.substring(match.end()).trim();
-  	      break;
-  	    }
-  	    
-  	    // Sometimes the starting sender address is missing, which means the
-  	    // station code ends up in the description
-  	    if (subject.endsWith(" FD")) {
-  	      data.strSource = subject;
-  	      break;
-  	    }
-  	    
-  	    // Nothing worked, we will keep trying, but will have a much stricter 
-  	    // standard of what constitutes a CAD page
-  	    sigMatch = false;
-	    } while (false);
-
-      // If we found a start signature match, which included a station source
-	    // Look for and remove any trailing station and call ID
-	    if (sigMatch) {
-	      match = TRAILER.matcher(body);
-	      if (match.find()) body = body.substring(0,match.start()).trim();
-	    }
-	    
-	    // If we didn't fidn a start signature match, we have a stricter definition
-	    // of what the trailer should look like, and we will extract the source code
-	    // from it
-	    else {
-	      match = TRAILER2.matcher(body);
-  	    if (match.find()) {
-  	      data.strSource = match.group(1);
-  	      body = body.substring(0,match.start()).trim();
-  	    }
-	    }
-	    body = body.replace('\n', ' ');
-	    
-	    // Check for special information header
-	    match = INFO_HEADER.matcher(body);
-	    if (match.find()) {
-	      data.strSupp = match.group(1);
-	      body = body.substring(match.end()).trim();
-	    }
-	    
-	    // Check for RECALLED tag
-	    if (body.startsWith("::::RECALLED:::::: ")) {
-	      data.strCall = "RECALLED - ";
-	      body = body.substring(19).trim();
-	    }
-	    
-	    body = body.replace("COUNTY ROUTE", "COUNTY ROAD");
-	    
-	    // There a couple basic pattern
-	    // /<place> (<addr city) [code] <call>
-	    if ((match = MASTER1.matcher(body)).matches()) {
-	      parseAddress(match.group(1), data);
-	      data.strPlace = match.group(1);
-	      parseAddress(StartType.START_ADDR, match.group(2), data);
-	      String left = getLeft();
-	      if (left.startsWith("TOWN OF")) left = left.substring(7).trim();
-	      else if (left.startsWith("VILLAGE OF")) left = left.substring(10).trim();
-	      data.strCross = left;
-	      data.strCode = cleanCode(match.group(3));
-	      data.strSupp = append(data.strSupp, " / ", match.group(4));
-	    }
-	    
-	    // <addr> , <city> ( <cross> ) [code] <info>
-	    else if ((match = MASTER2.matcher(body)).matches()) {
-        parseAddress(match.group(1), data);
-        data.strCity = cleanCity(match.group(2));
-        data.strCross = cleanCross(match.group(3));
-        data.strCode = cleanCode(match.group(4));
-        data.strSupp = append(data.strSupp, " / ", match.group(5));
-      }
-      
-      // <addr> , <city> <code> <info>
-      else if ((match = MASTER3.matcher(body)).matches()) {
-        parseAddress(match.group(1), data);
-        data.strCity = cleanCity(match.group(2));
-        data.strCode = cleanCode(match.group(3));
-        data.strSupp = append(data.strSupp, " / ", match.group(4));
-      }
-      
-      // <addr> , <city> <info>
-      else if ((match = MASTER4.matcher(body)).matches()) {
-        parseAddress(match.group(1), data);
-        data.strCity = cleanCity(match.group(2));
-        data.strSupp = append(data.strSupp, " / ", match.group(3));
-      }
-	    
-	    // Anything else is just a info
-	    // If we didn't find a matching start signature and didn't find a
-	    // master pattern match, the we had better conclude that this is not
-	    // reall a CAD page
-      else {
-        if (!sigMatch) return false;
-        data.strSupp = append(data.strSupp, " / ", body);
-      }
-	    
-	    if (data.strSupp.length() > 0 && data.strSupp.length() <= 40) {
-        data.strCall = data.strCall + data.strSupp;
-        data.strSupp = "";
-      } else {
-        data.strCall = data.strCall + "ALERT";
-      }
-	    return true;
-	  }
-
-    private String cleanCity(String field) {
-      if (field.endsWith(" TOWN OF")) {
-        field = field.substring(0, field.length()-8).trim();
-      } else if (field.endsWith(" VILLAGE OF")) {
-        field = field.substring(0, field.length()-11);
-      }
-      return field;
-    }
-
-    private String cleanCross(String field) {
-      if (field.startsWith("/")) field = field.substring(1).trim();
-      if (field.endsWith("/")) field = field.substring(0,field.length()-1).trim();
-      return field;
-    }
-    
-    private String cleanCode(String field) {
-      return (field == null ? "" : field);
-    }
-	}
+}
 	
