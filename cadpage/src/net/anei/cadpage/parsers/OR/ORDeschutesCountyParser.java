86fd87c74e8a7239c2c9549592c330d93eefb691
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-09-09 21:22:25 +0000

    Fixed parsing problem with Deschutes County, OR

--- cadpage/src/net/anei/cadpage/parsers/OR/ORDeschutesCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORDeschutesCountyParser.java
@@ -3,7 +3,7 @@ package net.anei.cadpage.parsers.OR;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.SmsMsgInfo.Data;
-import net.anei.cadpage.parsers.SmsMsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 
 /*
 Deschutes County, OR
@@ -17,15 +17,18 @@ Falls Unk status Ground-B-STA500-13345 SW CINDER DR-Map 131215-00:30:04
 Traffic Accidents HIGH MECH - Rollover-D2-STA500-SW BADGER RD/SW CHINOOK DR--13:11:56
 Sick Person Abnormal breathing -C-512,571-8431 SW BASALT DR-Map 131216-10:46:05
 Sick Person Vomiting   -A-STA500-13812 SW CANYON DR-Map 131216-18:47:56
+
+Contact: Dustin Miller <ff3650dm@gmail.com>
+STRUCTURE FIRE-SF-422,412,421,471,531,431,447,510,4PC-1995 NW LOWER BRIDGE WAY-Map 1413NW-19:04:36 
+
 */
 
 
-public class ORDeschutesCountyParser extends SmsMsgParser {
-  
-  private static final Pattern UNIT_PAT = Pattern.compile("(\\d\\d\\d)(,\\d\\d\\d)*");
+public class ORDeschutesCountyParser extends FieldProgramParser {
   
   public ORDeschutesCountyParser() {
-    super("DESCHUTES COUNTY", "OR");
+    super("DESCHUTES COUNTY", "OR",
+          "CALL PRI UNITSRC ADDR! MAP?");
   }
   
   @Override
@@ -38,52 +41,67 @@ public class ORDeschutesCountyParser extends SmsMsgParser {
     
     // Normally broken by dash field separators.
     // but a dash with a space on both side is probably a "REAL" dash and
-    // needs to be projected form our parsing breaks
+    // needs to be projected from our parsing breaks
     body = body.replaceAll(" - ", "__");
     String[] flds = body.split("-");
     if (flds.length < 5) return false;
+    return parseFields(flds, data);
+  }
+  
+  // Call field, replace double underscores with dash
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      super.parse(field.replace("__", " - "), data);
+    }
+  }
+  
+  // Priority has to match form
+  private class MyPriorityField extends PriorityField {
+    public MyPriorityField() {
+      setPattern(Pattern.compile("[A-Z]\\d*|SF"), true);
+    }
+  }
+  
+  private static final Pattern STATION_PAT = Pattern.compile("STA\\d+");
+  private class UnitSourceField extends Field {
     
-    int ndx = 1;
-    for (String fld : flds) {
-      fld = fld.trim();
-      switch (ndx) {
-      
-      // First field is always the call description
-      // remember to restore protected dashes
-      case 1:
-        data.strCall = fld.replaceAll("__", " - ");
-        ndx++;
-        break;
-
-      // Now we are looking for a station identifier or unit designation
-      // and skipping everything until we find it  
-      case 2:
-        if (fld.startsWith("STA")) {
-          data.strSource = fld;
-          ndx++;
-        } else if (UNIT_PAT.matcher(fld).matches()) {
-          data.strUnit = fld;
-          ndx++;
-        }
-        break;
-        
-      // Following the station ID comes the address
-      case 3:
-        parseAddress(fld, data);
-        ndx++;
-        break;
-        
-      // Usually, but not always, followed by a map page
-      case 4:
-        if (fld.startsWith("Map ")) {
-          data.strMap = fld.substring(4).trim();
-        }
-        ndx++;
-        break;
+    @Override
+    public void parse(String field, Data data) {
+      if (STATION_PAT.matcher(field).matches()) {
+        data.strSource = field;
+      } else  {
+        data.strUnit = field;
       }
     }
     
-    // We have to at least gotten to the address to be considered successful
-    return (ndx >= 4);
+    @Override
+    public String getFieldNames() {
+      return "SRC UNIT";
+    }
+  }
+  
+  private class MyMapField extends MapField {
+    
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.startsWith("Map ")) return false;
+      super.parse(field.substring(4).trim(), data);
+      return true;
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("PRI")) return new MyPriorityField();
+    if (name.equals("UNITSRC")) return new UnitSourceField();
+    if (name.equals("MAP")) return new MyMapField();
+    return super.getField(name);
   }
 }
