f2df09644cd85023298840d2167ca755ec84ab11
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-09-09 05:19:54 +0000

    Fixed parsing problem with Anderson County, SC Updates msg doc

--- cadpage/src/net/anei/cadpage/parsers/SC/SCAndersonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCAndersonCountyParser.java
@@ -16,12 +16,13 @@ ANDERSON CO 911:17B1 >FALLS-ALS PRI1-FR 110 FRANKLIN RD ANDERSON HAWKINS, PAUL M
 ANDERSON CO 911:31 >UNCONSCIOUS/FAINTING 210 KIRK LN Apt: 11 Bldg PENDLETON6463347 MICHELLE DURHAM Map: Grids:0,0 Cad: 2011-0000004465
 ANDERSON CO 911:13A1 >DIABETIC-ALS PRI2 7900 HIGHWAY 76 PENDLETON OFFICER SUMMERS 934-3443 Map: Grids:0,0 Cad: 2011-0000004494
 
+Contact: afd_jlo@yahoo.com <afd_jlo@yahoo.com> 
+Sender: 8643146900
+AND 911:17A01 >FALLS-BLS PRI2 141 ASHTON LN Apt: 141 Bldg ANDERSON LOUIE SHEILDS 6264049863 Map: Grids:0,0 Cad: 2011-0000202072
+
 */
 
 public class SCAndersonCountyParser extends DispatchBParser {
-  
-  private static final String DEF_STATE = "SC";
-  private static final String DEF_CITY = "ANDERSON COUNTY";
   
   private static final String[] CITY_CODES = new String[]{
     "ANDERSON", "BELTON", "CLEMSON", "EASLEY", "HONEA PATH", "IVA", "PELZER", 
@@ -31,7 +32,7 @@ public class SCAndersonCountyParser extends DispatchBParser {
   };
  
   public SCAndersonCountyParser() {
-    super(CITY_CODES, DEF_CITY, DEF_STATE);
+    super(CITY_CODES, "ANDERSON COUNTY", "SC");
   }
   
   @Override
@@ -43,8 +44,18 @@ public class SCAndersonCountyParser extends DispatchBParser {
   protected boolean parseMsg(String body, Data data) {
     
     // See if this is one of our pages
-    if (! body.startsWith("ANDERSON CO 911:")) return false;
-    body = body.substring(16);
+    do {
+      if (body.startsWith("ANDERSON CO 911:")) {
+        body = body.substring(16);
+        break;
+      }
+      if (body.startsWith("AND 911:")) {
+        body = body.substring(8);
+        break;
+      }
+      return false;
+    } while (false);
+    
     int pt = body.indexOf('>');
     if (pt < 0) return false;
     data.strCode = body.substring(0,pt).trim();
