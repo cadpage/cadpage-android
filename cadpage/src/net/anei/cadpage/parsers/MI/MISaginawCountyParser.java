5077af56222a8514c97a33659066a867f35b689c
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-09-27 23:01:49 +0000

    Fixed parsing problem with Saginaw County, MI

--- cadpage/src/net/anei/cadpage/parsers/MI/MISaginawCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MI/MISaginawCountyParser.java
@@ -16,9 +16,12 @@ TRITWP *: STAND BY *: BENTLEY ST&FIRWOOD ST CTY: 46 *: STANDBY REF POSS FIRE IN 
 TRITWP *: MUTUAL AID *: 2385 S FORDNEY RD CTY: 12 *: NEED LZ SET UP
 TRITWP *: WIRES UP/ARCING *: 1125 W BELLE CTY: 21 *: *TREE ON WIRE SPARKING
 TRITWP  *: FIRE OUTSIDE *: PARKWAY&N SAGINAW CTY: 39 *: *BARREL ON FIRE AT PARK
-
+TRITWP  *: FIRE ALARM *: 810 W WALNUT CTY: 39
+  
 Contact: Chris Gerding <atfdgerding@yahoo.com>
 ALBEE   *: ASSIST PERSON UP *: 4737 W BURT RD CTY: 01 *: *LIFT ASSIST CMT1: PRIORITY ONE
+
+Contact: 
 
 
  */
@@ -28,7 +31,7 @@ public class MISaginawCountyParser extends FieldProgramParser {
   
   public MISaginawCountyParser() {
     super("SAGINAW COUNTY", "MI",
-           "SRC CALL ADDR INFO! INFO+");
+           "SRC CALL ADDR! INFO+");
   }
   
   @Override
@@ -40,7 +43,7 @@ public class MISaginawCountyParser extends FieldProgramParser {
   protected boolean parseMsg(String body, Data data) {
     
     String[] flds = DELIM.split(body);
-    if (flds.length < 4) return false;
+    if (flds.length < 3) return false;
     return parseFields(flds, data);
   }
   
