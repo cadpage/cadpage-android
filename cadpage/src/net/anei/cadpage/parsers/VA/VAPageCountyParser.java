73b308792268d235be7ce3f7faefbf11e13de37f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-10 22:39:37 +0000

    Added new city code for Page County, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VAPageCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAPageCountyParser.java
@@ -26,6 +26,10 @@ Contact: greg seal <grgifts02@yahoo.com>
 Sender: MAILBOX@hrecc.org
 R30 TRAFFIC CRASH 14171 SPOTSWOOD TRL ELK CFS# 2011-066741 CROSS: WALKING HORSE LN/MOUNT HERMON RD
 
+Contact: Shane Taylor <vtdb8829taylor@gmail.com>
+Sender: MAILBOX@hrecc.org
+R35 EMS-ILLNESS 201 HILL AVE ELK CFS# 2011-073441 CROSS: W C ST/W D ST
+
 */
 
 public class VAPageCountyParser extends DispatchDAPROParser {
@@ -33,6 +37,7 @@ public class VAPageCountyParser extends DispatchDAPROParser {
   
   private static final Properties CITY_CODE_TABLE = 
     buildCodeTable(new String[]{
+      "ELK", "ELKTON",
       "LUR", "LURAY",
       "SHE", "SHENANDOAH",
       "STA", "STANLEY"
