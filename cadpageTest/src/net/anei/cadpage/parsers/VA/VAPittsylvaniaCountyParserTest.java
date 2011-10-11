ee8d0c9c49baec7213425496819a4f567f4827ba
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-11 13:02:50 +0000

    Fixed parsing problem with Pittsylvania, VA

--- cadpageTest/src/net/anei/cadpage/parsers/VA/VAPittsylvaniaCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/VA/VAPittsylvaniaCountyParserTest.java
@@ -57,4 +57,83 @@ public class VAPittsylvaniaCountyParserTest extends BaseParserTest {
     		"X:DANVILLE EXPW & GEORGES LN"
     	);
   }
+  
+  @Test
+  public void testParser2() {
+
+    doTest("T1",
+        "1357:CHEST PAINS;1855 RAINBOW CIR;DAN;2011044707;BUCKHORN DR;POWHATAN DR;[Medical Priority Info] PROBLEM: CHEST PAINS AND SWEATING, NAUSEUA # PATS: 1 AGE: 39 Years",
+        "CALL:CHEST PAINS",
+        "ADDR:1855 RAINBOW CIR",
+        "CITY:Danville",
+        "ID:2011044707",
+        "X:BUCKHORN DR & POWHATAN DR",
+        "INFO:CHEST PAINS AND SWEATING, NAUSEUA # PATS: 1 AGE: 39 Years");
+
+    doTest("T2",
+        "1363:EMS CALL FOR PROQA;12321 FRANKLIN TPKE;CHA;2011045077;F C BEVERLY RD;STRAWBERRY RD;[Medical Priority Info] PROBLEM: DIZZY SPELLS # PATS: 1 AGE: 85 Years SEX: F",
+        "CALL:EMS CALL FOR PROQA",
+        "ADDR:12321 FRANKLIN TPKE",
+        "CITY:Chatham",
+        "ID:2011045077",
+        "X:F C BEVERLY RD & STRAWBERRY RD",
+        "INFO:DIZZY SPELLS # PATS: 1 AGE: 85 Years SEX: F");
+
+    doTest("T3",
+        "1358:HEMORRHAGING;438 BONNY AVE;CAS;2011044807;PEACEFUL LN;SERENE CT;[Medical Priority Info] PROBLEM: BLEEDING FRON URINE AND BOWEL MOMENT # PATS: 1 AGE: 47 Years S",
+        "CALL:HEMORRHAGING",
+        "ADDR:438 BONNY AVE",
+        "CITY:Cascade",
+        "ID:2011044807",
+        "X:PEACEFUL LN & SERENE CT",
+        "INFO:BLEEDING FRON URINE AND BOWEL MOMENT # PATS: 1 AGE: 47 Years S");
+
+    doTest("T4",
+        "1360:RESPIRATORY DISTRESS;113 HUNTERS RDG;DAN;2011044913;HUNTERS TRL;HUNTERS CHASE;[Medical Priority Info] PROBLEM: HUSBAND TROUBLE BREATHING&CHEST PAINS # PATS: 1",
+        "CALL:RESPIRATORY DISTRESS",
+        "ADDR:113 HUNTERS RDG",
+        "CITY:Danville",
+        "ID:2011044913",
+        "X:HUNTERS TRL",
+        "INFO:HUNTERS CHASE / HUSBAND TROUBLE BREATHING&CHEST PAINS # PATS: 1");
+
+    doTest("T5",
+        "1366:EMS CALL FOR PROQA;127 MEDICAL CENTER RD;DAN;2011045246;MOUNT CROSS RD;HINESVILLE RD",
+        "CALL:EMS CALL FOR PROQA",
+        "ADDR:127 MEDICAL CENTER RD",
+        "CITY:Danville",
+        "ID:2011045246",
+        "X:MOUNT CROSS RD & HINESVILLE RD");
+
+    doTest("T6",
+        "1365:ENTRAPMENT;127 MEDICAL CENTER RD;DAN;2011045245;MOUNT CROSS RD;HINESVILLE RD",
+        "CALL:ENTRAPMENT",
+        "ADDR:127 MEDICAL CENTER RD",
+        "CITY:Danville",
+        "ID:2011045245",
+        "X:MOUNT CROSS RD & HINESVILLE RD");
+
+    doTest("T7",
+        "1367:UNKNOWN MEDICAL EMERGENCY;1976 HILL CREEK RD;DRY;2011045254;SUNSET DR;MOUNT CROSS RD;[Medical Priority Info] PROBLEM: MEDICAL ALARM # PATS: 1 AGE: Unknown Rang",
+        "CALL:UNKNOWN MEDICAL EMERGENCY",
+        "ADDR:1976 HILL CREEK RD",
+        "CITY:Dry Fork",
+        "ID:2011045254",
+        "X:SUNSET DR & MOUNT CROSS RD",
+        "INFO:MEDICAL ALARM # PATS: 1 AGE: Unknown Rang");
+
+    doTest("T8",
+        "1364:EMS CALL FOR PROQA;1976 HILL CREEK RD;DRY;2011045231;SUNSET DR;MOUNT CROSS RD;[Medical Priority Info] PROBLEM: MEDICAL ALARM # PATS: 1 AGE: Unknown Range SEX:",
+        "CALL:EMS CALL FOR PROQA",
+        "ADDR:1976 HILL CREEK RD",
+        "CITY:Dry Fork",
+        "ID:2011045231",
+        "X:SUNSET DR & MOUNT CROSS RD",
+        "INFO:MEDICAL ALARM # PATS: 1 AGE: Unknown Range SEX:");
+
+  }
+  
+  public static void main(String[] args) {
+    new VAPittsylvaniaCountyParserTest().generateTests("T1");
+  }
 }
