47c9b7b2f0228c9bb6e416bf82ea72756315b493
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-28 14:17:07 +0000

    Fixed parsing problem with Nueces County, TX

--- cadpageTest/src/net/anei/cadpage/parsers/TX/TXNuecesCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/TX/TXNuecesCountyParserTest.java
@@ -15,47 +15,59 @@ public class TXNuecesCountyParserTest extends BaseParserTest {
   public void testParser() {
 
     doTest("T1",
-        "Subject:MetroCom Notification\nMEDICAL - UNCON, ALRM: 0, PRI: 1, ESZ: 67 / 2141 TULOSO RD CC NUECS: @SUNTIDE FEDERAL CU- TULOSO RD",
-        "CALL:MEDICAL - UNCON",
+        "Subject:CAD Notify\nTRAUMA - INJURY ALRM: 0 PRI: 1 ESZ: 67 / 11308 GUESS DR CC NUECS EV: 1110029194\n",
+        "CALL:TRAUMA - INJURY",
         "PRI:1",
         "UNIT:67",
-        "ADDR:2141 TULOSO RD",
+        "ADDR:11308 GUESS DR",
         "CITY:CORPUS CHRISTI",
-        "PLACE:SUNTIDE FEDERAL CU- TULOSO RD");
+        "ID:1110029194");
 
     doTest("T2",
-        "Subject:MetorCom Notifiction\nAlarm – COMM-SML, ALRM: 0, PRI: 2, ESZ: 107 / 16318 fm 624 NUECS: @RIVER HILLS BASPTIST CHURCH",
-        "CALL:Alarm – COMM-SML",
+        "Subject:CAD Notify\nINV-FIRE - SMOKE ALRM: 0 PRI: 2 ESZ: 107 / 3695 CR 61 NUECS EV: 1110029353",
+        "CALL:INV-FIRE - SMOKE",
         "PRI:2",
         "UNIT:107",
-        "ADDR:16318 fm 624",
-        "PLACE:RIVER HILLS BASPTIST CHURCH");
+        "ADDR:3695 CR 61",
+        "ID:1110029353");
 
     doTest("T3",
-        "Subject:MetorCom Notifiction\nMEDICAL – ALLERGY, ALRM: 0, PRI: 2, ESZ: 2009 / 11559 LEOPARD ST CC NUECS: @CALALLEN MINOR EMERGENCY CLINI",
-        "CALL:MEDICAL – ALLERGY",
-        "PRI:2",
-        "UNIT:2009",
-        "ADDR:11559 LEOPARD ST",
-        "CITY:CORPUS CHRISTI",
-        "PLACE:CALALLEN MINOR EMERGENCY CLINI");
-
-    doTest("T4",
-        "Subject:MetorCom Notifiction\nFIRE-OTHER – GRASS, ALRM: 0, PRI: 1, ESZ 107 / HWY 77 FWY SB/CR 48 NUECS",
-        "CALL:FIRE-OTHER – GRASS",
+        "Subject:CAD Notify\nTA - EMS ALRM: 0 PRI: 1 ESZ: 107 / CR 48/HWY 77 FWY SB NUECS EV: 1110031040\n\n",
+        "CALL:TA - EMS",
         "PRI:1",
         "UNIT:107",
-        "ADDR:HWY 77 FWY SB & CR 48");
+        "ADDR:CR 48 & HWY 77 FWY SB",
+        "ID:1110031040");
+
+    doTest("T4",
+        "Subject:CAD Notify\nMEDICAL - SICK ALRM: 0 PRI: 1 ESZ: 6 / 4909 FM 2826 NUECS: @LABROCK CORRECTIONS SERVICES EV: 1110031803\n\n",
+        "CALL:MEDICAL - SICK",
+        "PRI:1",
+        "UNIT:6",
+        "ADDR:4909 FM 2826",
+        "PLACE:LABROCK CORRECTIONS SERVICES",
+        "ID:1110031803");
 
     doTest("T5",
-        "Subject:MetorCom Notifiction\nMEDICAL – CHEST, ALRM: 0, PRI: 1, 104 / FM 43/FM 763 NUECS",
-        "CALL:MEDICAL – CHEST",
+        "Subject:CAD Notify\nFIRE-OTHER - VEH-OUTSIDE ALRM: 0 PRI: 1 ESZ: 104 / 1332 FM 665 NUECS: @MEMORY GARDENS CEMETERY - FM 665 EV: 111003414",
+        "CALL:FIRE-OTHER - VEH-OUTSIDE",
         "PRI:1",
         "UNIT:104",
-        "ADDR:FM 43 & FM 763");
+        "ADDR:1332 FM 665",
+        "PLACE:MEMORY GARDENS CEMETERY - FM 665",
+        "ID:111003414");
+
+    doTest("T6",
+        "Subject:CAD Notify\nTRAUMA - INJURY ALRM: 0 PRI: 1 ESZ: 67 / 4729 CALALLEN DR CC NUECS EV: 1110034169\n\n",
+        "CALL:TRAUMA - INJURY",
+        "PRI:1",
+        "UNIT:67",
+        "ADDR:4729 CALALLEN DR",
+        "CITY:CORPUS CHRISTI",
+        "ID:1110034169");
   }
   
   public static void main(String[] args) {
-    new TXNuecesCountyParserTest().generateTests("T1", "CALL PRI UNIT ADDR CITY PLACE");
+    new TXNuecesCountyParserTest().generateTests("T1");
   }
 }
