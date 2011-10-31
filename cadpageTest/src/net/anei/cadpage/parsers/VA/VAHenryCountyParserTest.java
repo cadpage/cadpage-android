ff8262643c72f205405e256d9efadd2589fd3ba6
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-31 01:06:11 +0000

    Fixed Henry County, VA parser

--- cadpageTest/src/net/anei/cadpage/parsers/VA/VAHenryCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/VA/VAHenryCountyParserTest.java
@@ -1,7 +1,6 @@
 package net.anei.cadpage.parsers.VA;
 
 import net.anei.cadpage.parsers.BaseParserTest;
-import net.anei.cadpage.parsers.VA.VAHenryCountyParser;
 
 import org.junit.Test;
 
@@ -14,98 +13,65 @@ public class VAHenryCountyParserTest extends BaseParserTest {
   
   @Test
   public void testParser() {
-    
+
     doTest("T1",
-        "(CAD Call) 2010094339\nR2-(1) DIFFICULTY BREATHING\n999 MORGAN FORD RD RIDGEWAY",
-        "ID:2010094339",
-        "UNIT:R2-(1)",
-        "CALL:DIFFICULTY BREATHING",
-        "ADDR:999 MORGAN FORD RD",
-        "CITY:RIDGEWAY");
-    
-    doTest("T2",
-        "(CAD Call) 2010094406\nRFA-(3) FALL / FRACTURE\n247 DEVONSHIRE DR RIDGEWAY\nALTERED MENTAL STATUS",
-        "ID:2010094406",
+        "480 LEWIS RD MARTINSVILLE 2011095252 12:30:03 RFA-(3) FALL - FRACTURE",
+        "ADDR:480 LEWIS RD",
+        "CITY:MARTINSVILLE",
+        "ID:2011095252",
+        "TIME:12:30:03",
         "UNIT:RFA-(3)",
-        "CALL:FALL / FRACTURE",
-        "ADDR:247 DEVONSHIRE DR",
-        "CITY:RIDGEWAY",
-        "INFO:ALTERED MENTAL STATUS");
-    
+        "CALL:FALL - FRACTURE");
+
+    doTest("T2",
+        "2900 DILLONS FORK RD FIELDALE 2011095631 08:16:38 ROV-(2) OVERDOSE",
+        "ADDR:2900 DILLONS FORK RD",
+        "CITY:FIELDALE",
+        "ID:2011095631",
+        "TIME:08:16:38",
+        "UNIT:ROV-(2)",
+        "CALL:OVERDOSE");
+
     doTest("T3",
-        "(CAD Call) 2010094372\n1 LEE FORD CAMP RD RIDGEWAY\n@ 220 - TK & SUV",
-        "ID:2010094372",
-        "ADDR:1 LEE FORD CAMP RD",
-        "CITY:RIDGEWAY",
-        "INFO:@ 220 - TK & SUV");
-    
+        "2487 SPRUCE ST MARTINSVILLE 2011095614 06:57:08 RFA-(3) FALL - FRACTURE",
+        "ADDR:2487 SPRUCE ST",
+        "CITY:MARTINSVILLE",
+        "ID:2011095614",
+        "TIME:06:57:08",
+        "UNIT:RFA-(3)",
+        "CALL:FALL - FRACTURE");
+
     doTest("T4",
-        "(CAD Call) 2010094604\nRSI-(2) SICK UNKNOWN MEDICAL\n 89 DAYLIGHT DR RIDGEWAY",
-        "ID:2010094604",
+        "1890 DANIELS CREEK RD COLLINSVILLE 2011095584 22:15:45 RSI-(2) SICK",
+        "ADDR:1890 DANIELS CREEK RD",
+        "CITY:COLLINSVILLE",
+        "ID:2011095584",
+        "TIME:22:15:45",
         "UNIT:RSI-(2)",
-        "CALL:SICK UNKNOWN MEDICAL",
-        "ADDR:89 DAYLIGHT DR",
-        "CITY:RIDGEWAY");
-    
+        "CALL:SICK");
+
     doTest("T5",
-        "(CAD Call) 2010095255\nMOTOR VEHICLE CRASH\nHENRY COUNTY PLYWOOD CO. 1580 PHOSPHO SPRINGS RD RIDGEWAY",
-        "ID:2010095255",
-        "CALL:MOTOR VEHICLE CRASH",
-        "PLACE:HENRY COUNTY PLYWOOD CO",
-        "ADDR:1580 PHOSPHO SPRINGS RD",
-        "CITY:RIDGEWAY");
-    
+        "2630 VIRGINIA AVE COLLINSVILLE 2011095480 11:35:03 RDZ-(3) DIZZINESS, WEAKNESS",
+        "ADDR:2630 VIRGINIA AVE",
+        "CITY:COLLINSVILLE",
+        "ID:2011095480",
+        "TIME:11:35:03",
+        "UNIT:RDZ-(3)",
+        "CALL:DIZZINESS",
+        "INFO:, WEAKNESS");
+
     doTest("T6",
-        "(CAD Call) 2010095057\nRSI-(2) SICK UNKNOWN MEDICAL\n129 KIRK ST 4 MARTINSVILLE",
-        "ID:2010095057",
-        "UNIT:RSI-(2)",
-        "CALL:SICK UNKNOWN MEDICAL",
-        "ADDR:129 KIRK ST 4",
-        "CITY:MARTINSVILLE");
-    
-    doTest("T7",
-        "(CAD Call) 2010095041\nMOTOR VEHICLE CRASH\n3 MARHILL DR RIDGEWAY",
-        "ID:2010095041",
-        "CALL:MOTOR VEHICLE CRASH",
-        "ADDR:3 MARHILL DR",
-        "CITY:RIDGEWAY");
+        "1385 WINGFIELD ORCHARD RD MARTINSVILLE 2011095447 07:31:58 R3-(1) CHEST PAINS",
+        "ADDR:1385 WINGFIELD ORCHARD RD",
+        "CITY:MARTINSVILLE",
+        "ID:2011095447",
+        "TIME:07:31:58",
+        "UNIT:R3-(1)",
+        "CALL:CHEST PAINS");
 
-    doTest("T8",
-        "(CAD Call) 2010095011\nR2-(1) DIFFICULTY BREATHING\n999 MORGAN FORD RD RIDGEWAY",
-        "ID:2010095011",
-        "UNIT:R2-(1)",
-        "CALL:DIFFICULTY BREATHING",
-        "ADDR:999 MORGAN FORD RD",
-        "CITY:RIDGEWAY");
-    
-    doTest("T9",
-        "(CAD Call) 2010098333\nSUSPICIOUS CIRCUMSTANCES\nDOLLAR GENERAL (MOREHEAD AVE)\n390 MOREHEAD AVE RIDGEWAY\nWHT MALE, DARK SHIRT",
-        "ID:2010098333",
-        "CALL:SUSPICIOUS CIRCUMSTANCES",
-        "PLACE:DOLLAR GENERAL (MOREHEAD AVE)",
-        "ADDR:390 MOREHEAD AVE",
-        "CITY:RIDGEWAY",
-        "INFO:WHT MALE, DARK SHIRT");
-
-    doTest("T10",
-        "(CAD Call) 2011055772 \nF-HELICOPTER LANDING ZONE \nBLUE RIDGE REGIONAL LIBRARY - RIDGEWAY 900 VISTA VIEW LN RIDGEWAY \nFIELD A",
-        "ID:2011055772",
-        "CALL:F-HELICOPTER LANDING ZONE",
-        "PLACE:BLUE RIDGE REGIONAL LIBRARY - RIDGEWAY",
-        "ADDR:900 VISTA VIEW LN",
-        "CITY:RIDGEWAY",
-        "INFO:FIELD A");
-
-    doTest("T11",
-        "fcrs911callsbounce+caf_=5404880229=vtext.com@gmail.com (CAD Call) 2011086521\nMOTOR VEHICLE CRASH\n1 WILLIAM F STONE PARK HY Not Entered\n1 MILE SOUTH OF 609 EXIT N BOUND SIL 4 WH",
-        "ID:2011086521",
-        "CALL:MOTOR VEHICLE CRASH",
-        "ADDR:1 WILLIAM F STONE PARK HY",
-        "INFO:1 MILE SOUTH OF 609 EXIT N BOUND SIL 4 WH");
-  
   }
   
   public static void main(String[] args) {
-    new VAHenryCountyParserTest().generateTests("T12", "ID CALL PLACE ADDR CITY INFO");
+    new VAHenryCountyParserTest().generateTests("T1", "ADDR CITY ID TIME UNIT CALL INFO");
   }
 }
