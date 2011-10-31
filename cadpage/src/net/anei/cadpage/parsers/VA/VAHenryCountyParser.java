923cb46c97ebdda3b6739c246b67642769c85703
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-31 01:05:52 +0000

    Fixed Henry County, VA parser

--- cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
@@ -1,122 +1,41 @@
 package net.anei.cadpage.parsers.VA;
 
 import net.anei.cadpage.SmsMsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 /*
-Henry County, VA (class I)
-reporting email: Lieutenant Pruitt <rdvrs602@ridgewayrescue.com>
-sender:  *@ridgewayrescue.com
+Henry County, VA
+Contact: Hamp Ingram <emt.basics@gmail.com>
+Sender: MHC911@co.henry.va.us
 
-(CAD Call) 2010094339\nR2-(1) DIFFICULTY BREATHING\n999 MORGAN FORD RD RIDGEWAY
-(CAD Call) 2010094406\nRFA-(3) FALL / FRACTURE\n247 DEVONSHIRE DR RIDGEWAY\nALTERED MENTAL STATUS
-(CAD Call) 2010094372\n1 LEE FORD CAMP RD RIDGEWAY\n@ 220 - TK & SUV
-(CAD Call) 2010094604\nRSI-(2) SICK UNKNOWN MEDICAL\n 89 DAYLIGHT DR RIDGEWAY
-(CAD Call) 2010095255\nMOTOR VEHICLE CRASH\nHENRY COUNTY PLYWOOD CO. 1580 PHOSPHO SPRINGS RD RIDGEWAY
-(CAD Call) 2010095057\nRSI-(2) SICK UNKNOWN MEDICAL\n129 KIRK ST 4 MARTINSVILLE
-(CAD Call) 2010095041\nMOTOR VEHICLE CRASH\n3 MARHILL DR RIDGEWAY
-(CAD Call) 2010095011\nR2-(1) DIFFICULTY BREATHING\n999 MORGAN FORD RD RIDGEWAY
-(CAD Call) 2010098333\nSUSPICIOUS CIRCUMSTANCES\nDOLLAR GENERAL (MOREHEAD AVE)\n390 MOREHEAD AVE RIDGEWAY\nWHT MALE, DARK SHIRT
-
-Contact: Joshua Wilson <rdvfd70@gmail.com>
-Sender:911calls+bncCPSY0azfFhDP5JnwBBoEF_IV0w@ridgewayfire.com
-(CAD Call) 2011055772 \nF-HELICOPTER LANDING ZONE \nBLUE RIDGE REGIONAL LIBRARY - RIDGEWAY 900 VISTA VIEW LN RIDGEWAY \nFIELD A
-
-Contact: "william.hodges01@gmail.com" <william.hodges01@gmail.com>
-fcrs911callsbounce+caf_=5404880229=vtext.com@gmail.com (CAD Call) 2011086521\nMOTOR VEHICLE CRASH\n1 WILLIAM F STONE PARK HY Not Entered\n1 MILE SOUTH OF 609 EXIT N BOUND SIL 4 WH
+480 LEWIS RD MARTINSVILLE 2011095252 12:30:03 RFA-(3) FALL - FRACTURE
+2900 DILLONS FORK RD FIELDALE 2011095631 08:16:38 ROV-(2) OVERDOSE
+2487 SPRUCE ST MARTINSVILLE 2011095614 06:57:08 RFA-(3) FALL - FRACTURE
+1890 DANIELS CREEK RD COLLINSVILLE 2011095584 22:15:45 RSI-(2) SICK
+2630 VIRGINIA AVE COLLINSVILLE 2011095480 11:35:03 RDZ-(3) DIZZINESS, WEAKNESS
+1385 WINGFIELD ORCHARD RD MARTINSVILLE 2011095447 07:31:58 R3-(1) CHEST PAINS
 
 */
 
-public class VAHenryCountyParser extends SmartAddressParser {
+public class VAHenryCountyParser extends DispatchSouthernParser {
   
-  private static final String DEF_STATE = "VA";
-  private static final String DEF_CITY = "HENRY COUNTY";
-
   private static final String[] CITY_LIST = new String[]
-      {"RIDGEWAY", "MARTINSVILLE", "SPENCER", "BASSETT", "COLLINSVILLE", "FIELDALE", "AXTON"}; 
-  private static final MatchList CITY_MATCH = new MatchList(CITY_LIST);
+     {"RIDGEWAY", "MARTINSVILLE", "SPENCER", "BASSETT", "COLLINSVILLE", "FIELDALE", "AXTON"}; 
   
   public VAHenryCountyParser() {
-    super(CITY_LIST, DEF_CITY, DEF_STATE);
+    super(CITY_LIST, "HENRY COUNTY", "VA", DSFLAG_UNIT);
   }
 
   @Override
   public String getFilter() {
-    return "@ridgewayrescue.com,@ridgewayfire.com";
+    return "MHC911@co.henry.va.us";
   }
-
+  
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-
-    if (! subject.equals("CAD Call")) return false;
-
-    // Split body by newline terminators
-    String[] lines = body.split(" *\n *");
-    
-    // First pass through lines looking for the address line
-    // which we identify because it ends with one of our known city names
-    int addrNdx = -1;
-    for (int ndx = 1; ndx < lines.length; ndx++) {
-      String line = lines[ndx];
-      int pt = line.lastIndexOf(' ');
-      if (pt >= 0) {
-        String token = line.substring(pt+1);
-        if (CITY_MATCH.contains(token)) {
-          addrNdx = ndx;
-          break;
-        }
-      }
-    }
-    
-    // If we didn't find an address line, pick line 2 if there is one
-    // or the last line if their isn't
-    if (addrNdx < 0) addrNdx = Math.min(2, lines.length-1);
-
-    // Now make a second pass through the lines
-    for (int ndx = 0; ndx < lines.length; ndx++) {
-      String line = lines[ndx];
-      
-      // First line always contains call ID
-      if (ndx == 0) {
-        if (line.startsWith("(CAD Call)")) line = line.substring(10).trim();
-        data.strCallId = line;
-      }
-      
-      // If not address line, line 2 contains the unit and call description
-      else if (ndx == 1 && ndx < addrNdx) {
-        int pt = line.indexOf(' ');
-        if (pt >= 0) {
-          String tmp = line.substring(0, pt);
-          if (tmp.contains("-(")) {
-            data.strUnit = tmp.trim();
-            line = line.substring(pt+1);
-          }
-        }
-        data.strCall = line.trim();
-      }
-      
-      // Beyond the second line, any line before the address line contains a place name
-      else if (ndx < addrNdx) {
-        if (data.strPlace.length() > 0) data.strPlace += ' ';
-        data.strPlace += line;
-      }
-      
-      // When we get to the address line, use the smart address parser to pick
-      // the beginning of the address from any preceding place names.  Unless we
-      // already have a place name, in which case address must start the line
-      else if (ndx == addrNdx) {
-        if (line.endsWith(" Not Entered")) line = line.substring(0,line.length()-12).trim();
-        StartType start = (data.strPlace.length() > 0 ? StartType.START_ADDR : StartType.START_PLACE);
-        parseAddress(start, line, data);
-      }
-      
-      // Lines beyond the address line go into supplemental info
-      else {
-        if (data.strSupp.length() > 0) data.strSupp += " / ";
-        data.strSupp += line;
-      }
-    }
-    
+  public boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    // Dispatch - Google spat
+    data.strAddress = data.strAddress.replace("WILLIAM F STONE PARK ", "WILLIAM F STONE ");
     return true;
   }
 }
