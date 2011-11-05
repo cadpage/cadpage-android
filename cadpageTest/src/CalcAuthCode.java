326e38e211b8a89b0236aa5c32da450b0f9b35f0
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-11-05 05:02:33 +0000

    Modified to generate and display two daily auth codes

--- cadpageTest/src/CalcAuthCode.java
+++ cadpageTest/src/CalcAuthCode.java
@@ -1,12 +1,18 @@
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import net.anei.cadpage.donation.DonationManager;
 
-/** Class calcualtes the daily release exemption authorization code */ 
+/** Class calculates and displays the daily authorization codes */ 
 
 public class CalcAuthCode {
   public static void main(String[] args) {
-    String code = DonationManager.getAuthCode();
-    System.out.println("Todays authorization code is " + code + " or " + code.toLowerCase());
+    String code1 = DonationManager.getAuthCode(1);
+    String code2 = DonationManager.getAuthCode(2);
+    System.out.println("Today is " + new SimpleDateFormat("MM/dd/yyyy").format(new Date()));
+    System.out.println("Exempt release code is " + code1 + " or " + code1.toLowerCase());
+    System.out.println("Extend 1 Month code is " + code2 + " or " + code2.toLowerCase());
   }
 
 }
