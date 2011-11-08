a769fb109d493cf2ab4238c85b6ed96bfb670279
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-11-08 01:16:31 +0000

    Fixed parsing problem with Cambria County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PACambriaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PACambriaCountyParser.java
@@ -11,7 +11,6 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 Cambria County, PA
 Contact: jacob berkey <jwb932@gmail.com>
 Sender: alerts@cambria.ealertgov.com
-JO-JOHNTOWN
 
 [29] 11 Time: 15:02:29 Nature: 17A01G-Alpha FALL Location: 216 MAIN ST-JO | Sta 35
 [29] 11 Time: 11:54:47 Nature: 29B01-Bravo VEH ACC WITH INJURIES Location: 110 PLAZA DR-LY | Sta 30, Sta 23, Sta 35
@@ -20,18 +19,47 @@ JO-JOHNTOWN
 [28] 11 Time: 21:39:31 Nature: 06D03A-Delta BREATHING PROBLEMS Location: 205 CHANDLER AVE-JO | Sta 35, Sta 21, Sta 36
 
 Contact: Ben Smith <cnfederatediesel@gmail.com>
+Contact: Paul Zabinsky <pzabinsky@gmail.com>
 Date: 06/02/11\nTime: 20:47:52\nNature: 10C01-Charlie CHEST PAIN\nLocation: 1518 W 2ND ST-CR\n| Sta 75
 Date: 06/04/11\nTime: 11:03:04\nNature: 06D02-Delta\2sBREATHING PROBLEMS\nLocation: 908 SHORT AVE-CR\n| Sta 75
 Date: 06/04/11\nTime: 17:24:03\nNature: STB-STAND BY\nLocation: 721 MAIN ST-PORB\n| Sta 73
 Date: 06/05/11\nTime: 18:00:42\nNature: 52C04G-Charlie FIRE ALARM\nLocation: 111 ASHCROFT AVE-CB\n| Sta 70
-
-Contact: Paul Zabinsky <pzabinsky@gmail.com>
 Date: 06/13/11\nTime: 15:53:48\nNature: 02D02-Delta  ALLERGIC REACTION\nLocation: 1237 ST AUGUSTINE RD-CF\n| Sta 60, Sta 67
 
+Contact: Paul Bomboy <training@easthillsems.com>
+Sender: Cambria 9-1-1
+FRM:Cambria 9-1-1\nMSG:Date: 11/07/11\nTime: 18:02:55\nNature: 10C01-Charlie CHEST PAIN\nLocation: 349 VO TECH DR-RI\n|
 
 */
 
 public class PACambriaCountyParser extends FieldProgramParser {
+  
+  private static final Pattern MARKER1 = Pattern.compile("\\d\\d");
+  private static final Pattern MARKER2 = Pattern.compile("^\\d\\d ");
+  
+  public PACambriaCountyParser() {
+    super(CITY_CODES, "CAMBRIA COUNTY", "PA",
+           "Date:DATE? Time:TIME! Nature:CALL! Location:ADDR/y! Sta:UNIT");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "alerts@cambria.ealertgov.com,Cambria 9-1-1";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    
+    if (MARKER1.matcher(subject).matches()) {
+      Matcher match = MARKER2.matcher(body);
+      if (!match.find()) return false;
+      body = body.substring(match.end()).trim();
+    }
+    
+    body = body.replace('\n', ' ').replace(" | ", " Sta: ");
+    if (body.endsWith("|")) body = body.substring(0,body.length()-1).trim();
+    return super.parseMsg(body, data);
+  }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "ADAM", "ADAMS TWP",
@@ -97,39 +125,5 @@ public class PACambriaCountyParser extends FieldProgramParser {
       "WM", "WESTMONT",
       "WS", "WEST TAYLOR TWP",
       "WT", "WASHINGTON TWP"
-
   });
-  
-  private static final Pattern MARKER1 = Pattern.compile("\\d\\d");
-  private static final Pattern MARKER2 = Pattern.compile("^\\d\\d ");
-  private static final Pattern MARKER3 = Pattern.compile("^Date: \\d\\d/\\d\\d/\\d\\d\n");
-  
-  public PACambriaCountyParser() {
-    super(CITY_CODES, "CAMBRIA COUNTY", "PA",
-           "Time:SKIP! Nature:CALL! Location:ADDR/y! Sta:UNIT!");
-  }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    
-    // Check for multiple signatures
-    Matcher match;
-    do {
-      // Signature 1
-      if (MARKER1.matcher(subject).matches()) {
-        match = MARKER2.matcher(body);
-        if (match.find())break;
-      }
-      
-      // Signature 2
-      if ((match = MARKER3.matcher(body)).find()) break;
-      
-      // No go
-      return false;
-    } while (false);
-    
-    body = body.substring(match.end()).trim();
-    body = body.replace('\n', ' ').replace(" | ", " Sta: ");
-    return super.parseMsg(body, data);
-  }
 }
