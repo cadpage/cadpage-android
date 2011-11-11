e86c18b0f058567c810995e6fb961f89285bdd8a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-11-11 17:21:31 +0000

    Re-enabled a kinder gentler sob-story event

--- cadpage/src/net/anei/cadpage/donation/SobStoryDonateEvent.java
+++ cadpage/src/net/anei/cadpage/donation/SobStoryDonateEvent.java
@@ -5,20 +5,17 @@ import net.anei.cadpage.R;
 /*
 I can't afford $10
 
-So, you are paying monthly charges for a smart phone, probably have a text
-package and data package on top of that, which runs to how much a month?  
-And you can't afford $10/year to pay hard working developers to keep Cadpage
-up and running?
+    Tell us why you can't afford a $10 donation.  We're really easy on these.\n\n
+    Is there anything we can do to make Cadpage easier to use?  Do you any
+    suggestions on better ways we can finance the considerable costs of maintaining
+    Cadpage?
 
-OK, we're suckers for a good story, and we don't wan\\'t to leave anyone
-out.  Email us a good story about why you can't afford $10 and we will
-probably give you a free subscription.  
  */
 public class SobStoryDonateEvent extends DonateScreenEvent {
 
   protected SobStoryDonateEvent() {
-    super(AlertStatus.YELLOW, R.string.donate_sob_story_title, R.string.donate_sob_story_text);
-           // EmailSobStoryEvent.instance());
+    super(AlertStatus.YELLOW, R.string.donate_sob_story_title, R.string.donate_sob_story_text,
+           EmailSobStoryEvent.instance());
   }
 
   @Override
