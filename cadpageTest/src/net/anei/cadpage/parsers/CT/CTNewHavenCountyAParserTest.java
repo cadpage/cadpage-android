5b1daa2dae49310299f6dc43e4fffb3bd0aae50e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-11-21 06:56:36 +0000

    Fixed parsing problem with North Brandon, CT And merged it into New Haven County, CT

---
+++ cadpageTest/src/net/anei/cadpage/parsers/CT/CTNewHavenCountyAParserTest.java
@@ -0,0 +1,110 @@
+package net.anei.cadpage.parsers.CT;
+
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+
+public class CTNewHavenCountyAParserTest extends BaseParserTest {
+  
+  public CTNewHavenCountyAParserTest() {
+    setParser(new CTNewHavenCountyAParser(), "NEW HAVEN COUNTY", "CT");
+  }
+  
+  @Test
+  public void testParser() {
+
+    doTest("T1",
+        "MARINE INCIDENT, - SEARCH at BLOCK ISLAND RD BRANFORD, GUILFORD . . 14:27:56",
+        "CALL:MARINE INCIDENT - SEARCH",
+        "ADDR:BLOCK ISLAND RD",
+        "CITY:BRANFORD",
+        "SRC:GUILFORD");
+
+    doTest("T2",
+        "CODE 5,- AUTOMATIC FIRE ALARM at 275 LAKE DRIVE GUILFORD 15:57",
+        "PRI:5",
+        "CALL:AUTOMATIC FIRE ALARM",
+        "ADDR:275 LAKE DRIVE",
+        "CITY:GUILFORD");
+
+    doTest("T3",
+        "CODE 1,- AUTOMATIC FIRE ALARM at 35 STATE STREET, GUILFORD 17:15 cross street 1-begins at broad st;115-Boston Post Road;180-North Street",
+        "PRI:1",
+        "CALL:AUTOMATIC FIRE ALARM",
+        "ADDR:35 STATE STREET",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD",
+        "INFO:cross street 1-begins at broad st;115-Boston Post Road;180-North Street");
+
+    doTest("T4",
+        "CODE 2,- DTN MEDICAL EMERGENCY at 669 BOSTON POST ROAD, GUILFORD 10:19",
+        "PRI:2",
+        "CALL:DTN MEDICAL EMERGENCY",
+        "ADDR:669 BOSTON POST ROAD",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+    doTest("T5",
+        "CODE 1,- APPLIANCE,TV,ETC. at 258 CHESTNUT GROVE ROAD 19:50",
+        "PRI:1",
+        "CALL:APPLIANCE,TV,ETC.",
+        "ADDR:258 CHESTNUT GROVE ROAD");
+
+    doTest("T6",
+        "CODE 2,-DTN MEDICAL EMERGENCY at 49 GUILFORD COURT, GUILFORD 15:47",
+        "PRI:2",
+        "CALL:DTN MEDICAL EMERGENCY",
+        "ADDR:49 GUILFORD COURT",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+    doTest("T7",
+        "CODE 5,- AUTOMATIC FIRE ALARM at 159 LEDGE HILL ROAD, GUILFORD 11:40",
+        "PRI:5",
+        "CALL:AUTOMATIC FIRE ALARM",
+        "ADDR:159 LEDGE HILL ROAD",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+
+    doTest("T8",
+        "CODE 1,- AUTOMATIC FIRE ALARM at 53 IRONWOOD ROAD, GUILFORD 11:43",
+        "PRI:1",
+        "CALL:AUTOMATIC FIRE ALARM",
+        "ADDR:53 IRONWOOD ROAD",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+
+    doTest("T9",
+        "CODE 2,-DTN MEDICAL EMERGENCY at 51 WATER STREET, GUILFORD 10:46",
+        "PRI:2",
+        "CALL:DTN MEDICAL EMERGENCY",
+        "ADDR:51 WATER STREET",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+
+    doTest("T10",
+        "CODE 4, - DTN STRUCTURE FIRE at 9 OLD BARN LN, GUILFORD 08:33",
+        "PRI:4",
+        "CALL:DTN STRUCTURE FIRE",
+        "ADDR:9 OLD BARN LN",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+    doTest("T11",
+        "CODE 4, - DTN STRUCTURE FIRE at 125 GOOSE LANE, GUILFORD 09:32",
+        "PRI:4",
+        "CALL:DTN STRUCTURE FIRE",
+        "ADDR:125 GOOSE LANE",
+        "CITY:GUILFORD",
+        "SRC:GUILFORD");
+
+  }
+  
+  public static void main(String[] args) {
+    new CTNewHavenCountyAParserTest().generateTests("T1", "PRI CALL ADDR CITY SRC INFO");
+  }
+}
--- cadpageTest/src/net/anei/cadpage/parsers/CT/CTNewHavenCountyBParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/CT/CTNewHavenCountyBParserTest.java
@@ -5,10 +5,10 @@ import net.anei.cadpage.parsers.BaseParserTest;
 import org.junit.Test;
 
 
-public class CTNorthBranfordParserTest extends BaseParserTest {
+public class CTNewHavenCountyBParserTest extends BaseParserTest {
   
-  public CTNorthBranfordParserTest() {
-    setParser(new CTNorthBranfordParser(), "NORTH BRANFORD", "CT");
+  public CTNewHavenCountyBParserTest() {
+    setParser(new CTNewHavenCountyBParser(), "NORTH BRANFORD", "CT");
   }
   
   @Test
@@ -20,7 +20,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MEDICAL MEDA",
         "ADDR:167 BRANFORD RD",
         "X:HUBBARD RD / TWIN LAKE RD",
-        "UNIT:MED4 503");
+        "UNIT:MED4 503",
+        "TIME:11:50");
 
     doTest("T2",
         "1100008148 MEDICAL MEDD 00009  SALEM ST CLINTONVILLE RD/VILLAGE ST  MED4 R2 110909 17:55",
@@ -28,7 +29,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MEDICAL MEDD",
         "ADDR:9 SALEM ST",
         "X:CLINTONVILLE RD / VILLAGE ST",
-        "UNIT:MED4 R2");
+        "UNIT:MED4 R2",
+        "TIME:17:55");
 
     doTest("T3",
         "1100008142 MUTUAL AID 00392  HOPE HILL RD WALLINGFORD    MED4 110909 15:38",
@@ -36,7 +38,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MUTUAL AID",
         "ADDR:392 HOPE HILL RD",
         "X:WALLINGFORD",
-        "UNIT:MED4");
+        "UNIT:MED4",
+        "TIME:15:38");
 
     doTest("T4",
         "1100008130 AUTOMATIC FIRE ALARM 00051  CIRO RD FOXON RD/DEAD END  E1 ET11 ET22 T1 R11 515 110909 10:07",
@@ -44,7 +47,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:AUTOMATIC FIRE ALARM",
         "ADDR:51 CIRO RD",
         "X:FOXON RD / DEAD END",
-        "UNIT:E1 ET11 ET22 T1 R11 515");
+        "UNIT:E1 ET11 ET22 T1 R11 515",
+        "TIME:10:07");
 
     doTest("T5",
         "1100008128 MEDICAL MEDB 00049  CAPUTO RD FOXON RD ROUTE 80/MILL RD  R1 MED4 515 110909 08:38",
@@ -52,14 +56,16 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MEDICAL MEDB",
         "ADDR:49 CAPUTO RD",
         "X:FOXON RD ROUTE 80 / MILL RD",
-        "UNIT:R1 MED4 515");
+        "UNIT:R1 MED4 515",
+        "TIME:08:38");
 
     doTest("T6",
         "1100007980 BRUSH FIRE TWIN LAKES RD/ LAKE RD   BR1 ET11 502 110904 12:22",
         "ID:1100007980",
         "CALL:BRUSH FIRE TWIN",
         "ADDR:LAKES RD & LAKE RD",
-        "UNIT:BR1 ET11 502");
+        "UNIT:BR1 ET11 502",
+        "TIME:12:22");
   }
   
   @Test
@@ -71,7 +77,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MVA W/INJURIES",
         "ADDR:N MAIN ST & RT68",
         "CITY:WALLINFORD",
-        "UNIT:A44");
+        "UNIT:A44",
+        "TIME:17:05");
 
     doTest("T2",
         "1100007014 MUTUAL AID 00909  BEAVER HEAD RD , GUILFORD    MED4 110811 01:16",
@@ -79,7 +86,8 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MUTUAL AID",
         "ADDR:909 BEAVER HEAD RD",
         "CITY:GUILFORD",
-        "UNIT:MED4");
+        "UNIT:MED4",
+        "TIME:01:16");
    
   }
   
@@ -92,11 +100,22 @@ public class CTNorthBranfordParserTest extends BaseParserTest {
         "CALL:MEDICAL MEDC",
         "ADDR:254 BRANFORD RD",
         "X:HARRISON RD / FOXON RD",
-        "UNIT:MED4 R1");
+        "UNIT:MED4 R1",
+        "TIME:23:04");
+
+    doTest("T2",
+        "1100005182 ALPHA MEDICAL 00055 THOMPSON ST  Prem Map -14 PP 65 FOXON RD/GAY ST  (Prem Map -14 PP 65)    S5 111120 15:40",
+        "ID:1100005182",
+        "CALL:ALPHA MEDICAL",
+        "ADDR:55 THOMPSON ST",
+        "MAP:14 PP 65",
+        "X:FOXON RD / GAY ST",
+        "UNIT:S5",
+        "TIME:15:40");
 
   }
   
   public static void main(String[] args) {
-    new CTNorthBranfordParserTest().generateTests("T1", "ID CALL ADDR CITY X UNIT");
+    new CTNewHavenCountyBParserTest().generateTests("T1", "ID CALL ADDR CITY MAP X UNIT TIME");
   }
 }
