a319c8a043601c378856ccd4b2f86f5a8fbf8888
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-11-27 21:33:01 +0000

    Fixed parsing problem with Chautauqua County, NY

--- cadpageTest/src/net/anei/cadpage/parsers/NY/NYChautauquaCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NY/NYChautauquaCountyParserTest.java
@@ -16,6 +16,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T1",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 07:06 *EMS CALL ; 3735 WESTMAN RD ; C/T/V Ellery ; BRET SANTILLI ; 17 yr female seizure  ; A441",
+        "SRC:MSP CAD",
+        "TIME:07:06",
         "CALL:EMS CALL",
         "ADDR:3735 WESTMAN RD",
         "CITY:Ellery",
@@ -25,6 +27,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T2",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 00:44 *EMS CALL ; 170 HIGH ACRES MHP ; C/T/V Ellery ; DONALD SCHAUMAN ; hip pain and back pain ; A441 E301 E302 T301 A301 R302 E373 E383 in Sun Room Zone 27 ;",
+        "SRC:MSP CAD",
+        "TIME:00:44",
         "CALL:EMS CALL",
         "ADDR:170 HIGH ACRES MHP",
         "CITY:Ellery",
@@ -34,6 +38,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T3",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 12:18 *STRUCTURE FIRE -3000 ; 5457 BLY HILL RD ; C/T/V North Harmony ; Richard Robinson ; Fire alarm A301 R302 A281",
+        "SRC:MSP CAD",
+        "TIME:12:18",
         "CALL:STRUCTURE FIRE -3000",
         "ADDR:5457 BLY HILL RD",
         "CITY:North Harmony",
@@ -42,6 +48,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T4",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 19:43 *EMS CALL ; 3246 PANAMA STEDMAN RD ; C/T/V North Harmony ; Gregory Mcchesney ; MALE INCOHERENT / ELEV BP AND TEMP ;",
+        "SRC:MSP CAD",
+        "TIME:19:43",
         "CALL:EMS CALL",
         "ADDR:3246 PANAMA STEDMAN RD",
         "CITY:North Harmony",
@@ -50,6 +58,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T5",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 12:06 *EMS CALL ; 5410 MEADOWS RD ; C/T/V Chautauqua ; C MERRITT ; 73 yoa fm shortness in breath gene ELLE M10 MAPL",
+        "SRC:MSP CAD",
+        "TIME:12:06",
         "CALL:EMS CALL",
         "ADDR:5410 MEADOWS RD",
         "CITY:Chautauqua",
@@ -58,6 +68,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T6",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 18:46 *EMS CALL ; 4412 DUTCH HOLLOW RD ; C/T/V Ellery ; FLOYD DELLAHOY ; MALE - DIFF BREATHING ; A481",
+        "SRC:MSP CAD",
+        "TIME:18:46",
         "CALL:EMS CALL",
         "ADDR:4412 DUTCH HOLLOW RD",
         "CITY:Ellery",
@@ -67,13 +79,18 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T7",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 14:05 *EMS CALL ; 4900 RT430 lot 1 ; C/T/V Ellery ; ; ; A481",
+        "SRC:MSP CAD",
+        "TIME:14:05",
         "CALL:EMS CALL",
         "ADDR:4900 RT430 lot 1",
+        "MADDR:4900 RT 430 lot 1",
         "CITY:Ellery",
         "UNIT:A481");
 
     doTest("T8",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 17:10 *EMS CALL ; 2916 BENTLEY AVE ; C/T/V Ellicott ; ; FEMALE - DIFF BREATHING ; FLUV A441",
+        "SRC:MSP CAD",
+        "TIME:17:10",
         "CALL:EMS CALL",
         "ADDR:2916 BENTLEY AVE",
         "CITY:Ellicott",
@@ -82,6 +99,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T9",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 17:24 *EMS CALL ; 5011 STONELEDGE RD ; C/T/V North Harmony ; ; MALE - FALLEN, POSS STROKE ; A301 R30",
+        "SRC:MSP CAD",
+        "TIME:17:24",
         "CALL:EMS CALL",
         "ADDR:5011 STONELEDGE RD",
         "CITY:North Harmony",
@@ -90,6 +109,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T10",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 11:27 *STRUCTURE FIRE -3000 ; 1136 HOAG RD ; C/T/V Harmony ; RICK NORRIS ; house fire ;",
+        "SRC:MSP CAD",
+        "TIME:11:27",
         "CALL:STRUCTURE FIRE -3000",
         "ADDR:1136 HOAG RD",
         "CITY:Harmony",
@@ -98,6 +119,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T11",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 18:36 *SINGLE COMPANY ; 27 E  MAIN ST ; C/T/V Panama ; ; ASHVILLE ONE ENGINR STAND BY AT PANAMA FD  ;",
+        "SRC:MSP CAD",
+        "TIME:18:36",
         "CALL:SINGLE COMPANY",
         "ADDR:27 E MAIN ST",
         "CITY:Panama",
@@ -105,6 +128,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T12",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 15:18 *EMS CALL ; S  MAPLE AVE GLEASON RD ; C/T/V Busti ; ; ; A301 R302",
+        "SRC:MSP CAD",
+        "TIME:15:18",
         "CALL:EMS CALL",
         "ADDR:S MAPLE AVE & GLEASON RD",
         "CITY:Busti",
@@ -112,6 +137,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T13",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 00:15 *EMS CALL ; 28 LIBERTY ST ; C/T/V Bemus Point ; CHARLES CROSSLEY ; f/70 back pains and leg pain",
+        "SRC:MSP CAD",
+        "TIME:00:15",
         "CALL:EMS CALL",
         "ADDR:28 LIBERTY ST",
         "CITY:Bemus Point",
@@ -120,6 +147,8 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T14",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 15:11 *SINGLE COMPANY ; BEMUS ELLERY RD MAHANNA RD ; C/T/V Ellery ; Philip Erickson ; live electric line across the road.",
+        "SRC:MSP CAD",
+        "TIME:15:11",
         "CALL:SINGLE COMPANY",
         "ADDR:BEMUS ELLERY RD & MAHANNA RD",
         "CITY:Ellery",
@@ -128,13 +157,31 @@ public class NYChautauquaCountyParserTest extends BaseParserTest {
 
     doTest("T15",
         "CHAUTAUQUA_COUNTY_SHERIFF (MSP CAD) 05:01 *EMS CALL ; 5325 RAMSEY RD ; C/T/V North Harmony ; BRUCE HARRIS ; 62/F  SEMI CONSCIOUS ; A441",
+        "SRC:MSP CAD",
+        "TIME:05:01",
         "CALL:EMS CALL",
         "ADDR:5325 RAMSEY RD",
         "CITY:North Harmony",
         "NAME:BRUCE HARRIS",
         "INFO:62/F  SEMI CONSCIOUS",
         "UNIT:A441");
- }
+  }
+  
+  public void doTestBemusPoint() {
+
+    doTest("T1",
+        "(BEMUS POINT) 21:45 *MVA PI ; 1220 86 WB ; C/T/V Ellery ; NYSP Jamestown ; 3A14 OUT WITH AN MVA WITH MINOR INJURIES   I86 WB NEAR OVERLOOK ; A441 R442 E",
+        "SRC:BEMUS POINT",
+        "TIME:21:45",
+        "CALL:MVA PI",
+        "ADDR:1220 86 WB",
+        "MADDR:1220 86",
+        "CITY:Ellery",
+        "NAME:NYSP Jamestown",
+        "INFO:3A14 OUT WITH AN MVA WITH MINOR INJURIES   I86 WB NEAR OVERLOOK / E",
+        "UNIT:A441 R442");
+
+  }
   
   public static void main(String[] args) {
     new NYChautauquaCountyParserTest().generateTests("T1");
