afd7881ce6ed614cf3be9bb059a09cc72e0def79
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-10 11:59:32 +0000

    Added Outagamie and WInnebago counties in WI, both redirects to Calumet County.

---
+++ cadpage/src/net/anei/cadpage/parsers/WI/WIWinnebagoCountyParser.java
@@ -0,0 +1,8 @@
+package net.anei.cadpage.parsers.WI;
+
+public class WIWinnebagoCountyParser extends WICalumetCountyParser {
+  
+  public WIWinnebagoCountyParser() {
+    super("WINNEBAGO COUNTY");
+  }
+}
