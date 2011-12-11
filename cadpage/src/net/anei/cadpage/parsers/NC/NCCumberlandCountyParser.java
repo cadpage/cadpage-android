8515ea12afcf514d0f30c9cf87f65b4390c10c55
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-11 16:44:08 +0000

    Beta release Fixed parsing problems with Cumberland County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCCumberlandCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCCumberlandCountyParser.java
@@ -15,6 +15,7 @@ Sender: cad@co.cumberland.nc.us
 [S] SPRING LAKE (PLAT BOOK 11, PAGE 3)+/- (N)C4;01/27/2011 22:09:28;CLAMMY BREATHING PROBLEMS;ST22;115-401 LAKE AVE;S FIRST ST;GATEWAY IN
 [S]  (N)C4;01/27/2011 17:08:27;UNKNOWN STATUS TRAFFIC ACCIDEN;ST22;670 LILLINGTON HWY;HOLLAND DR;WALMART
 [S] OVERHILLS PARK (N)C4;01/27/2011 15:20:35;ABNORMAL BREATHING SICK PERSON;ST22;3110 HUNTLEY ST;ROBBIE CIR
+
 [S] R G CREECH (N)C4;01/27/2011 13:23:42;WIRES DOWN (NO SMOKE OR ARC);ST22;200 RUPE ST;SCARBOROUGH ST
 CAD:(S) (N)C4;03/04/2011 21:18:43;UNKNOWN OUTSIDE FIRE;ST22;2700-BLK BRAGG BLVD
 CAD:DIST: 174.69 FT (S)UPTON TYSON (BELRIDGR DR) (N)C10;05/20/2011 08:34:01;CLAMMY CHEST PAIN;ST18;3570 LACEWOOD CT;NC HWY 87 S
@@ -23,17 +24,14 @@ Contact: Brandon Norris <lawranger197@gmail.com>
 Sender: messaging@iamresponding.com
 (Station 13) 2011 07:30:53;ALERT WITH ABNORMAL BREATHING;ST13;2881 STRICKLAND BRIDGE RD;CENTURY CIR
 (Station 13) 08/2011 18:00:08;RESIDENTIAL ALARM;0431,2131,ST13,ST19;5681 CAMDEN RD;STATE ST
-
-** NOT PARSING **
 (Station 13) 7/11/2011 16:16:30;DIFFICULTY SPEAKING BREATHING;ST13,ST19;4737 DESERT RIDGE RD;HYPONY TRL
 (Station 13) POINT (N)C8;07/18/2011 16:13:08;NOT ALERT BREATHING PROBSLEMS;ST13;6240 STONEY POINT LOOP;SPRINGRUN RD
 (Station 13) 2011 07:30:53;ALERT WITH ABNORMAL BREATHING;ST13;2881 STRICKLAND BRIDGE RD;CENTURY CIR
 (Station 13) 08/2011 18:00:08;RESIDENTIAL ALARM;0431,2131,ST13,ST19;5681 CAMDEN RD;STATE ST
-(Station 13) NTIAL SINGLE STRUCTURE;ST13;6405 DOROTHY PL
-(Station 13) :36;CONTINUOUS OR MULTIPLE SEIZURE;ST13;7956 CAMDEN RD;POSSUM HOLLER RD
-(Station 13) IVE BREATHING NOT VERIFI;ST13;DUNDLE RD/STONEY POINT RD;From FAY:
-(Station 13) :45;VEHICLE FIRE THREAT NONSTRUCTU;ST13;6123 ELLEN LN;BAMMEL DR
-(Station 13) NTIAL SINGLE STRUCTURE;ST13;6405 DOROTHY PL
+
+Contact: Jr McElyea <j.mcelyea1@gmail.com>
+Sender: essaging@iamresponding.com
+(Station #4) PAPER MILL 12/09/2011 22:21:31;ABNORMAL BREATHING CHEST PN;4474 CAMERON RD;PECAN DR
 
 */
 
@@ -61,16 +59,14 @@ public class NCCumberlandCountyParser extends FieldProgramParser {
     
     Matcher match = MARKER.matcher(body);
     if (match.find()) {
-      data.strPlace = match.group(1);
+      data.strPlace = match.group(1).trim();
       body = body.substring(match.end());
+    } else {
+      int pt = body.indexOf("(N)");
+      if (pt >= 0) body = body.substring(pt+3).trim();
     }
     
     return parseFields(body.split(";"), data);
-  }
-  
-  @Override
-  public String getProgram() {
-    return "PLACE " + super.getProgram();
   }
   
   private class MyUnitField extends UnitField {
@@ -79,16 +75,29 @@ public class NCCumberlandCountyParser extends FieldProgramParser {
     }
   }
   
-  private class DateTimeField extends SkipField {
-    public DateTimeField() {
-      setPattern(Pattern.compile("(?:\\d\\d/){0,2}?\\d{4} \\d\\d:\\d\\d:\\d\\d"), true);
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(?:(.+?) +)??(?:([0-9/]+) +)?(\\b\\d\\d:\\d\\d:\\d\\d)");
+  private static final Pattern DATE_PTN = Pattern.compile("\\d\\d/\\d\\d/\\d{4}");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strPlace = append(data.strPlace, " / ", getOptGroup(match.group(1)));
+      String sDate = match.group(2);
+      data.strTime = match.group(3);
+      if (sDate != null && DATE_PTN.matcher(sDate).matches()) data.strDate = sDate;
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "PLACE DATE TIME";
     }
   }
   
   @Override
   public Field getField(String name) {
     if (name.equals("UNIT")) return new MyUnitField();
-    if (name.equals("DATETIME")) return new DateTimeField();
+    if (name.equals("DATETIME")) return new MyDateTimeField();
     return super.getField(name);
   }
 }
