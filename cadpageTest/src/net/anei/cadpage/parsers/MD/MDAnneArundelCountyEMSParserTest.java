d4bec4dfa9f4e2c984c2971e77164f4a309f1a38
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-13 05:38:56 +0000

    Fixed more parsing problems with Anne Arundel County, MD (EMS)

--- cadpageTest/src/net/anei/cadpage/parsers/MD/MDAnneArundelCountyEMSParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/MD/MDAnneArundelCountyEMSParserTest.java
@@ -22,17 +22,17 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "PLACE:MT CALVARY UNITED METH CHURCH",
         "UNIT:E171,PM17",
         "CALL:TASER PATIENT",
-        "ID:1145");
+        "TIME:11:45");
     
     doTest("T2",
         "Box Alarm 12-19 540 BENFIELD RD (CENTER DR) Echo E12,QNT30,E18,E171,QNT23,TK26,SQ23,PM12,BC2,SCA ODOR GAS IN STRUCTURE (HOT); 1351 [2/50]",
         "BOX:12-19",
         "ADDR:540 BENFIELD RD",
         "X:CENTER DR",
-        "PRI:E",
+        "CH:Echo",
         "UNIT:E12,QNT30,E18,E171,QNT23,TK26,SQ23,PM12,BC2,SCA",
         "CALL:ODOR GAS IN STRUCTURE (HOT)",
-        "ID:1351");
+        "TIME:13:51");
 
     doTest("T3",
         "Local Alarm 17-12 24 CASCADE RD (GRANDVIEW RD) E171 ALARMS/NON HIGH LIFE (HOT); 1705 [3/51]", 
@@ -41,7 +41,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "X:GRANDVIEW RD",
         "UNIT:E171",
         "CALL:ALARMS/NON HIGH LIFE (HOT)",
-        "ID:1705");
+        "TIME:17:05");
     
     doTest("T4",
         "*MEDICAL BOX 23-5 831 RITCHIE HWY N  [Unit 307A]  [KRIS LEIGH CATERED LIVING] QNT23,PM17,TRO CARDIAC ARREST; 0548 [2/42]  /2d-96/",
@@ -51,7 +51,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "PLACE:KRIS LEIGH CATERED LIVING",
         "UNIT:QNT23,PM17,TRO",
         "CALL:CARDIAC ARREST",
-        "ID:0548");
+        "TIME:05:48");
     
     doTest("T5",
         "*MEDICAL Box 23-12 1244 RITCHIE HWY S  [Unit 18]  [SEVERN COMMERCE CENTER] QNT23,PM17 STROKE; 0921 [3/43]  /lm-2/", 
@@ -61,7 +61,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "PLACE:SEVERN COMMERCE CENTER",
         "UNIT:QNT23,PM17",
         "CALL:STROKE",
-        "ID:0921");
+        "TIME:09:21");
     
     doTest("T6",
         "*Medical Box 40-83 RT50 W/JENNIFER RD MU40,E40,E171 MVC; 1517 [4/44]  /18d-19/", 
@@ -70,17 +70,17 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MADDR:RT 50 W & JENNIFER RD",
         "UNIT:MU40,E40,E171",
         "CALL:MVC",
-        "ID:1517");
+        "TIME:15:17");
 
     doTest("T7",
         "Box Alarm 23-15 812 WINDSOR RD (FOREST LN) Echo QNT23,E171,E19,TW40,QNT30,SQ23,PM17,BC2,SCA STRUCTURE/BARN/GARAGE (HOT); 1607 [5/45]", 
         "BOX:23-15",
         "ADDR:812 WINDSOR RD",
         "X:FOREST LN",
-        "PRI:E",
+        "CH:Echo",
         "UNIT:QNT23,E171,E19,TW40,QNT30,SQ23,PM17,BC2,SCA",
         "CALL:STRUCTURE/BARN/GARAGE (HOT)",
-        "ID:1607");
+        "TIME:16:07");
     
     doTest("T8",
         "MEDICAL Box 19-5 1604 ORCHARD WAY (HARMONY ACRES LN) E172,MU39 STROKE; 1643 [6/46]", 
@@ -89,7 +89,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "X:HARMONY ACRES LN",
         "UNIT:E172,MU39",
         "CALL:STROKE",
-        "ID:1643");
+        "TIME:16:43");
     
     doTest("T9",
         "MEDICAL Box 40-26 1724 VINEYARD TRL (RAMSHORN TRL) TW40,PM17 SEIZURES; 0035 [1/41]",
@@ -98,14 +98,14 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "X:RAMSHORN TRL",
         "UNIT:TW40,PM17",
         "CALL:SEIZURES",
-        "ID:0035");
+        "TIME:00:35");
 
     doTest("T10",
         "HazMat Box 17-17 1939 BALTO ANNAP BLVD (CARVEL DR) Kilo E171,SQ2,BC2,SU23,SCA,PM39 Chemical ODOR ...",
         "BOX:17-17",
         "ADDR:1939 BALTO ANNAP BLVD",
         "X:CARVEL DR",
-        "PRI:K",
+        "CH:Kilo",
         "UNIT:E171,SQ2,BC2,SU23,SCA,PM39",
         "CALL:Chemical ODOR ...");
     
@@ -114,17 +114,17 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "BOX:23-6",
         "ADDR:88 HOYLE LN",
         "X:BRENDA JOYCE LN",
-        "PRI:C",
+        "CH:Charlie",
         "UNIT:B17",
         "CALL:ELECTRIC WIRES OUTSIDE (COLD)",
-        "ID:1404");
+        "TIME:14:04");
     
     doTest("T12",
         "Still Box 40-24A 1872 ANNAPOLIS MALL [CHEESCAKE FACTORY] Kilo E172,TK36 ALARMS/MALLS (WARM ...",
         "BOX:40-24A",
         "ADDR:1872 ANNAPOLIS MALL",
         "PLACE:CHEESCAKE FACTORY",
-        "PRI:K",
+        "CH:Kilo",
         "UNIT:E172,TK36",
         "CALL:ALARMS/MALLS (WARM ...");
     
@@ -133,7 +133,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "BOX:19-5",
         "ADDR:1652 PLEASANT PLAINS RD",
         "X:OLD SAINT MARGARETS RD",
-        "PRI:C",
+        "CH:Charlie",
         "UNIT:HOLD,U@*,E172",
         "CALL:ELECTRIC ...");
     
@@ -144,7 +144,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "X:MAN O WAR CT",
         "UNIT:A19,PM17",
         "CALL:CHEST PAIN",
-        "ID:1341");
+        "TIME:13:41");
     
     doTest("T15",
         "Medical Box 40-83 RT50 E/BESTGATE RD MU40,E40,E172 MVC; 1238 [5/283] /lm-7",
@@ -153,7 +153,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MADDR:RT 50 E & BESTGATE RD",
         "UNIT:MU40,E40,E172",
         "CALL:MVC",
-        "ID:1238");
+        "TIME:12:38");
     
     doTest("T16",
         "Medical Box 17-QA BRIDE POINT DR /COX [Unit NECK] [RD BX1-10] MOA PM17 MVC; PM17 REQ TAX 3 1133 ...",
@@ -163,7 +163,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "PLACE:RD BX1-10",
         "UNIT:MOA",
         "CALL:PM17 MVC; PM17 REQ TAX 3",
-        "ID:1133");
+        "TIME:11:33");
   }
 
   @Test
@@ -177,7 +177,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-F1",
         "UNIT:E281,MU29",
         "CALL:MVC",
-        "ID:2306");
+        "TIME:23:06");
 
     doTest("T2",
         "*MEDICAL Box 28-13 TELEGRAPH RD/JACKSON GROVE RD 6-K13 E283,MU4 FAINTING; 1328 [9/90]  14m-2",
@@ -186,7 +186,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:6-K13",
         "UNIT:E283,MU4",
         "CALL:FAINTING",
-        "ID:1328");
+        "TIME:13:28");
 
     doTest("T3",
         "Medical Box 34-5 7390 BALTO ANNAP BLVD  [MTA LIGHT RAIL CROMWELL] 7-J1 TK33,MU21,A28 ABDOM PAIN; 1256 [8/89]",
@@ -196,7 +196,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:7-J1",
         "UNIT:TK33,MU21,A28",
         "CALL:ABDOM PAIN",
-        "ID:1256");
+        "TIME:12:56");
     
 
     doTest("T4",
@@ -207,7 +207,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-H6",
         "UNIT:E283,MU6",
         "CALL:BREATHING PROB",
-        "ID:1145");
+        "TIME:11:45");
 
     doTest("T5",
         "*Medical Box 28-8 1496 ANNAPOLIS RD  [BLOCKBUSTER VIDEO] 12-F1 E281,MU29 MVC; 2306 [14/81]  lm-33",
@@ -217,7 +217,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-F1",
         "UNIT:E281,MU29",
         "CALL:MVC",
-        "ID:2306");
+        "TIME:23:06");
 
     doTest("T6",
         "*Medical Box 28-27 PINEY ORCHARD PKWY/RIVERSCAPE RD 12-E7 E281,PM26 MVC; 1415 [6/91]  lm-8",
@@ -226,7 +226,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-E7",
         "UNIT:E281,PM26",
         "CALL:MVC",
-        "ID:1415");
+        "TIME:14:15");
 
     doTest("T7",
         "MEDICAL Box 28-27 RIVERSCAPE RD/RUNNING WOLF TRL 12-F8 E283,MU21 CHEST PAIN; 2010 [13/78]",
@@ -235,7 +235,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-F8",
         "UNIT:E283,MU21",
         "CALL:CHEST PAIN",
-        "ID:2010");
+        "TIME:20:10");
 
     doTest("T8",
         "Local Box 28-27 2615 LOTUSWOOD CT (RIVERSCAPE RD) 12-E7 E283 SERVICE CALL (COLD); 1740 [9/293]",
@@ -245,7 +245,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-E7",
         "UNIT:E283",
         "CALL:SERVICE CALL (COLD)",
-        "ID:1740");
+        "TIME:17:40");
 
     doTest("T9",
         "*MEDICAL Box 28-27 8718 THORNBROOK DR (RIVERSCAPE RD) 12-E7 E281,MU6 HEADACHE; BURNING INSIDE  2001 [15/297]  41m-1",
@@ -255,7 +255,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-E7",
         "UNIT:E281,MU6",
         "CALL:HEADACHE; BURNING INSIDE",
-        "ID:2001");
+        "TIME:20:01");
 
     doTest("T10",
         "*MEDICAL Box 28-27 8700 NATURES TRAIL CT  [Unit 304] (RIVERSCAPE RD) 12-E8 A28,PM5 BREATHING PROB; 0524 [4/313]  6m-5",
@@ -266,7 +266,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-E8",
         "UNIT:A28,PM5",
         "CALL:BREATHING PROB",
-        "ID:0524");
+        "TIME:05:24");
 
     doTest("T11",
         "*Medical Box 28-6 2012 BEALMEAR MILL LN  [Unit 7208] (COWMAN CT) 12-G1 E281,MU29 BACK PAIN; 0539 [1/68]  25d-2",
@@ -277,7 +277,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "MAP:12-G1",
         "UNIT:E281,MU29",
         "CALL:BACK PAIN",
-        "ID:0539");
+        "TIME:05:39");
 
     doTest("T12",
         "*Local Box 17-13 1641 WINCHESTER RD  [MONTESSORI INTL CHILDRENS HOME] E171 HI LIFE/ALARMS SOUNDING (WARM); 1243 [6/88]  /9d-7/",
@@ -286,7 +286,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "ADDR:1641 WINCHESTER RD",
         "PLACE:MONTESSORI INTL CHILDRENS HOME",
         "UNIT:E171",
-        "ID:1243");
+        "TIME:12:43");
 
     doTest("T13",
         "MEDICAL Box 23-16 254 PENDLETON CT (WAYCROSS WAY) QNT23,PM17 BREATHING PROB; 0911 [3/127]",
@@ -295,7 +295,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "ADDR:254 PENDLETON CT",
         "X:WAYCROSS WAY",
         "UNIT:QNT23,PM17",
-        "ID:0911");
+        "TIME:09:11");
 
     doTest("T14",
         "MEDICAL BOX 19-6 824 HOLLY DR E A19,PM17,TRO MVC; 1808 PM2 [3/25]\n",
@@ -303,7 +303,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "CALL:MVC",
         "ADDR:824 HOLLY DR E",
         "UNIT:A19,PM17,TRO",
-        "ID:1808",
+        "TIME:18:08",
         "INFO:PM2");
 
     doTest("T14",
@@ -312,7 +312,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "CALL:CHEST PAIN",
         "ADDR:1220 VIKING DR N",
         "UNIT:E171,PM17",
-        "ID:1834",
+        "TIME:18:34",
         "INFO:PM1 Y>HHC,HHC R>UoM,UoM");
 
     doTest("T15",
@@ -321,7 +321,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "CALL:SICK PERSON",
         "ADDR:2156 MULBERRY HILL RD",
         "UNIT:A19",
-        "ID:1549",
+        "TIME:15:49",
         "INFO:A Y>UoM");
 
     doTest("T16",
@@ -330,7 +330,7 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "CALL:CO DET / NO INJURIES (COLD)",
         "ADDR:133 BOONE TRL",
         "UNIT:E171",
-        "ID:1159",
+        "TIME:11:59",
         "INFO:CO");
 
     doTest("T17",
@@ -341,9 +341,9 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "APT:446",
         "X:PARKRIDGE CIR",
         "MAP:17-K3",
-        "PRI:E",
+        "CH:Echo",
         "UNIT:E7,E5,PE39,PE16,TK28,PTW43,SQ1,PM5,BC3,SCA",
-        "ID:1008");
+        "TIME:10:08");
 
     doTest("T18",
         "Medical Box 7-13 1719 DRYDEN WY, CR - btwn DANA ST and DANA ST (5291K8) Bravo A79 Medical Alert Alarm; 1500 [7/52]",
@@ -353,15 +353,38 @@ public class MDAnneArundelCountyEMSParserTest extends BaseParserTest {
         "CITY:CROFTON",
         "X:DANA ST and DANA ST",
         "MAP:5291K8",
-        "PRI:B",
+        "CH:Bravo",
         "UNIT:A79",
-        "ID:1500");
+        "TIME:15:00");
+
+    doTest("T19",
+        "Medical Box 5-27 SB CRAIN HY/CAPITOL RACEWAY RD (5291K7) Bravo PM6,E5,A839,A289 MVC: Uknown Status; 2234 [6/118]",
+        "BOX:5-27",
+        "CALL:MVC: Uknown Status",
+        "ADDR:SB CRAIN HY & CAPITOL RACEWAY RD",
+        "MADDR:CRAIN HWY & CAPITOL RACEWAY RD",
+        "MAP:5291K7",
+        "CH:Bravo",
+        "UNIT:PM6,E5,A839,A289",
+        "TIME:22:34");
+
+    doTest("T20",
+        "Box Alarm 21-14 1562 PENZANCE WY, HA - btwn DORCHESTER BL and DORCHESTER BL (5055C5) Echo E29,TA5,QNT4,E10,SQ43,TK45,E45,TK28,E34,MU21,BC1,SCA1 Dwelling Fire (Hot ); 1920 [5/117]",
+        "BOX:21-14",
+        "CALL:Dwelling Fire (Hot )",
+        "ADDR:1562 PENZANCE WY",
+        "CITY:HANOVER",
+        "X:DORCHESTER BL and DORCHESTER BL",
+        "MAP:5055C5",
+        "CH:Echo",
+        "UNIT:E29,TA5,QNT4,E10,SQ43,TK45,E45,TK28,E34,MU21,BC1,SCA1",
+        "TIME:19:20");
    
   }
 
 
   public static void main(String[] args) {
-    new MDAnneArundelCountyEMSParserTest().generateTests("T19", "BOX CALL ADDR APT CITY PLACE X PRI UNIT ID INFO");
+    new MDAnneArundelCountyEMSParserTest().generateTests("T19", "BOX CALL ADDR APT CITY PLACE X MAP CH UNIT TIME INFO");
   }
   
 }
