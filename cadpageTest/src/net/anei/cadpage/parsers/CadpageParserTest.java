8eb693d0187e1f0fe4679b14956de4358744e1c9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-15 18:14:53 +0000

    Added Cadpage location parser

---
+++ cadpageTest/src/net/anei/cadpage/parsers/CadpageParserTest.java
@@ -0,0 +1,39 @@
+package net.anei.cadpage.parsers;
+
+import net.anei.cadpage.SmsMmsMessage;
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CadpageParserTest extends BaseParserTest {
+  
+  public CadpageParserTest() {
+    setParser(new CadpageParser(), "", "");
+  }
+  
+  @Test
+  public void testActve911() {
+    doTest("T1",
+        "title:LIFELINE ACTIVATION  address:2037 MAIN ST  city:PHILOMATH  cad_code:121411-18  map_code:545-360  ",
+        "CALL:LIFELINE ACTIVATION",
+        "ADDR:2037 MAIN ST",
+        "CITY:PHILOMATH",
+        "ID:121411-18",
+        "MAP:545-360");
+  }
+  
+  @Test
+  public void testEverything() {
+    doAllTest("PRI: 1\nDATE: 12/15/2011\nTIME: 09:12\nCALL: HELP ME\nPL: MY HOUSE\nADDR: 611 PINE ST\nCITY: PHILOMATH\nST: XX\nAPT: J3\nX: BLACK ST & BLUE AV\nBOX: XX77\nMAP: J75S2\nCH: OP6\nUNIT: 221\nINFO: BEHIND HOUSE\nNAME: KEN\nPH: 5551212\nCODE: 77J23\nGPS: SOMEWHERE\nID: 2011-001\nSRC: ST202");
+    doAllTest("PRI: 1\nDATE: 12/15/2011\nTIME: 09:12\nCALL: HELP ME\nPL: MY HOUSE\nADDR: 611 PINE ST\nAPT: J3\nX: BLACK ST & BLUE AV\nBOX: XX77\nMAP: J75S2\nCH: OP6\nUNIT: 221\nINFO: BEHIND HOUSE\nNAME: KEN\nPH: 5551212\nCODE: 77J23\nGPS: SOMEWHERE\nID: 2011-001\nSRC: ST202\nDCITY: BENTON COUNTY\nDST: OR");
+  }
+  
+  private void doAllTest(String body) {
+    SmsMmsMessage msg = new SmsMmsMessage("ken@cadpage.org", body, 0, SmsMmsMessage.MESSAGE_TYPE_SMS);
+    SmsMsgParser parser = new CadpageParser();
+    assertTrue(parser.isPageMsg(msg));
+    assertEquals(body, CadpageParser.formatInfo(msg.getInfo(), "\n", false));
+  }
+}
