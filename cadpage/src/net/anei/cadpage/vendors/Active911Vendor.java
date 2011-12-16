1eb902e758b391272cc4b6dc041f83aebbfa99f3
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-16 17:18:11 +0000

    Updated Active911 vendor info Updated mes doc Updated user list

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -9,7 +9,7 @@ class Active911Vendor extends Vendor {
            R.string.active911_summary,
            R.string.active911_text,
            0,
-           "http://www.active911.com",
+           "http://www.active911.com/interface/cadpage/",
            ">A91");
   }
 }
