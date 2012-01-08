56c1cee967d6418b203519b4f852ca74e29693ea
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-01-08 16:29:27 +0000

    Fixed parsing problem with Gaston County, NC

--- cadpageTest/src/net/anei/cadpage/parsers/NC/NCGastonCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NC/NCGastonCountyParserTest.java
@@ -20,7 +20,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:RIVERSIDE DR & HICKORY GROVE RD",
         "PHONE:(704) 905-8283",
         "SRC:40",
-        "UNIT:LD400, EN403, C161, RSU,");
+        "UNIT:LD400, EN403, C161, RSU,",
+        "DATE:08/25/2011",
+        "TIME:15:48:11");
 
     doTest("T2",
         "(*CAD*) [CAD]   29D2L ~WESLEYAN~DR/~MAIN~ST  X-ST: Phone:~(704) 460-7016 - Station:~Station 40 Station 40 08/29/2011 16:49:18 1\nEN403  WRECK.",
@@ -28,7 +30,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:WESLEYAN DR & MAIN ST",
         "PHONE:(704) 460-7016",
         "SRC:40",
-        "UNIT:EN403  WRECK.");
+        "UNIT:EN403  WRECK.",
+        "DATE:08/29/2011",
+        "TIME:16:49:18");
 
     doTest("T3",
         "(*CAD*) [CAD]   Tree In Roadway 124~CHURCH~ST~  X-ST: ROBINETTE LN - Phone:~(704) 634-0269 Station 40 08/30/2011 05:13:05 1\n",
@@ -36,8 +40,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:124 CHURCH ST",
         "X:ROBINETTE LN",
         "PHONE:(704) 634-0269",
-        "SRC:40");
-    
+        "SRC:40",
+        "DATE:08/30/2011",
+        "TIME:05:13:05");
 
     doTest("T4",
         "(*CAD*) [CAD]   Alarm-Fire (High Risk) 250~BEATTY~DR~  X-ST: UNKNOWN - Station:~Station 34 Station 34 09/07/2011 21:00:32 3\nLD400, LD34, EN33",
@@ -45,7 +50,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:250 BEATTY DR",
         "X:UNKNOWN",
         "SRC:34",
-        "UNIT:LD400, LD34, EN33");
+        "UNIT:LD400, LD34, EN33",
+        "DATE:09/07/2011",
+        "TIME:21:00:32");
 
     doTest("T5",
         "(*CAD*) [CAD]   29D4 S ~I~85/~S ~EXIT 23 MCADENVILLE  X-ST: Station:~Station 40 - Quadrant:~40D Station 40 08/30/2011 07:01:38 7\nLD400, EN403, SP172, RSU, E33P, E3",
@@ -53,7 +60,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:S I 85 & S EXIT 23 MCADENVILLE",
         "SRC:40",
         "MAP:40D",
-        "UNIT:LD400, EN403, SP172, RSU, E33P, E3");
+        "UNIT:LD400, EN403, SP172, RSU, E33P, E3",
+        "DATE:08/30/2011",
+        "TIME:07:01:38");
 
     doTest("T6",
         "(*CAD*) [CAD]   Tree In Roadway 124~CHURCH~ST~  X-ST: ROBINETTE LN - Phone:~(704) 634-0269 Station 40 08/30/2011 05:13:05 1\nEN403",
@@ -62,7 +71,9 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "X:ROBINETTE LN",
         "PHONE:(704) 634-0269",
         "SRC:40",
-        "UNIT:EN403");
+        "UNIT:EN403",
+        "DATE:08/30/2011",
+        "TIME:05:13:05");
 
     doTest("T7",
         "(*CAD*) [CAD]   29D2L ~WESLEYAN~DR/~MAIN~ST  X-ST: Phone:~(704) 460-7016 - Station:~Station 40 Station 40 08/29/2011 16:49:18 1\nEN403  WRECK.",
@@ -70,7 +81,21 @@ public class NCGastonCountyParserTest extends BaseParserTest {
         "ADDR:WESLEYAN DR & MAIN ST",
         "PHONE:(704) 460-7016",
         "SRC:40",
-        "UNIT:EN403  WRECK.");
+        "UNIT:EN403  WRECK.",
+        "DATE:08/29/2011",
+        "TIME:16:49:18");
+  }
+  
+  @Test
+  public void testParser2() {
+
+    doTest("T1",
+        "Fire-Commerical Business 100~BUCKEYE~DR~ X-ST: UNKNOWN / TWINBROOKS DR Phone:~(704) 524-5798 Station 30 01/06/2012",
+        "CALL:Fire-Commerical Business",
+        "ADDR:100 BUCKEYE DR",
+        "X:UNKNOWN / TWINBROOKS DR",
+        "PHONE:(704) 524-5798",
+        "SRC:30");
   }
   
 
