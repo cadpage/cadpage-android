4873b24da64d061b6ebcfc49d29cb06482fff5d5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-01-12 20:59:46 +0000

    Replaced Wyoming County, NY location parser

--- cadpageTest/src/net/anei/cadpage/parsers/NY/NYWyomingCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NY/NYWyomingCountyParserTest.java
@@ -15,96 +15,53 @@ public class NYWyomingCountyParserTest extends BaseParserTest {
   public void testParser() {
 
     doTest("T1",
-        "2314, STEDMAN RD;BENNINGTON TOWN (TINKHAM RD / GEISE RD)\n4105-Fire Alarm\nBE:2011:59\nThe document(s) accompanying t",
-        "ADDR:2314 STEDMAN RD",
+        "SICK PERSON\n90 KERN RD BENT (Between ERIE CO LINE & URF RD) ADULT FML NOT FEELING WELL",
+        "CALL:SICK PERSON",
+        "ADDR:90 KERN RD",
         "CITY:BENNINGTON",
-        "X:TINKHAM RD / GEISE RD",
-        "CALL:4105-Fire Alarm",
-        "SRC:BE",
-        "MAP:2011:59");
+        "X:ERIE CO LINE & URF RD",
+        "INFO:ADULT FML NOT FEELING WELL");
 
     doTest("T2",
-        "560, BURROUGH RD;BENNINGTON TOWN (GETMAN RD / FRIEDMAN RD)\n4001-Abdominal Pain\nBE:2011:57\nThe document(s) accompan",
-        "ADDR:560 BURROUGH RD",
+        "FALLS\n1387 ROUTE 77 BENT (Between ROUTE 354 & POLAND HILL RD)",
+        "CALL:FALLS",
+        "ADDR:1387 ROUTE 77",
         "CITY:BENNINGTON",
-        "X:GETMAN RD / FRIEDMAN RD",
-        "CALL:4001-Abdominal Pain",
-        "SRC:BE",
-        "MAP:2011:57");
+        "X:ROUTE 354 & POLAND HILL RD");
 
     doTest("T3",
-        "ATTICA RODEO GROUNDS ;ATTICA VILLAGE ( 230 EXCHANGE ST)\n4109-Stand-by\nAT:2011:211",
-        "PLACE:ATTICA RODEO GROUNDS",
-        "ADDR:230 EXCHANGE ST",
-        "CITY:ATTICA",
-        "CALL:4109-Stand-by",
-        "SRC:AT",
-        "MAP:2011:211");
+        "HEART PROBLEMS\n1535 FRIEDMAN RD BENT (Between ROUTE 77 & TINKHAM RD)",
+        "CALL:HEART PROBLEMS",
+        "ADDR:1535 FRIEDMAN RD",
+        "CITY:BENNINGTON",
+        "X:ROUTE 77 & TINKHAM RD");
 
     doTest("T4",
-        "709, BURROUGH RD;BENNINGTON TOWN (FRIEDMAN RD / ROUTE 354)\n4026-Sick Person\nBE:2011:54",
-        "ADDR:709 BURROUGH RD",
+        "STRUCTURE FIRE\nFRENCH RD BENT (Between ROUTE 354 & HOOVER RD) barn fire",
+        "CALL:STRUCTURE FIRE",
+        "ADDR:FRENCH RD",
+        "MADDR:FRENCH RD & ROUTE 354",
         "CITY:BENNINGTON",
-        "X:FRIEDMAN RD / ROUTE 354",
-        "CALL:4026-Sick Person",
-        "SRC:BE",
-        "MAP:2011:54");
+        "X:ROUTE 354 & HOOVER RD",
+        "INFO:barn fire");
 
     doTest("T5",
-        "1204, FRENCH RD;BENNINGTON TOWN (ROUTE 354 / HOOVER RD)\n4001-Abdominal Pain\nBE:2011:51",
-        "ADDR:1204 FRENCH RD",
+        "SEIZURE/CONVULSION\n1375 ROUTE 354 BENT (Between ROUTE 77 & GRAFF RD)",
+        "CALL:SEIZURE/CONVULSION",
+        "ADDR:1375 ROUTE 354",
         "CITY:BENNINGTON",
-        "X:ROUTE 354 / HOOVER RD",
-        "CALL:4001-Abdominal Pain",
-        "SRC:BE",
-        "MAP:2011:51");
+        "X:ROUTE 77 & GRAFF RD");
 
     doTest("T6",
-        "2050, ROUTE 354 #PH;BENNINGTON TOWN (FRENCH RD / MAXON RD)\n4002-Allergies\nBE:2011:46",
-        "ADDR:2050 ROUTE 354",
-        "CITY:BENNINGTON",
-        "X:FRENCH RD / MAXON RD",
-        "CALL:4002-Allergies",
-        "SRC:BE",
-        "MAP:2011:46");
-
-    doTest("T7",
-        "1950, ROUTE 354 ;BENNINGTON TOWN (FRENCH RD / MAXON RD)\n6302-MVA/INJURY\nBE:2011:45",
-        "ADDR:1950 ROUTE 354",
-        "CITY:BENNINGTON",
-        "X:FRENCH RD / MAXON RD",
-        "CALL:6302-MVA/INJURY",
-        "SRC:BE",
-        "MAP:2011:45");
-
-    doTest("T8",
-        "32,S PEARL ST #APT LOWER;ATTICA VILLAGE (MAIN ST / DEAD END)\n4010-Chest Pain\nAT:2011:174",
-        "ADDR:32 S PEARL ST",
-        "APT:LOWER",
+        "STRUCTURE FIRE\n1649 ROUTE 238 ATTT (Between NESBITT RD & ATTICA TOWN LINE)",
+        "CALL:STRUCTURE FIRE",
+        "ADDR:1649 ROUTE 238",
         "CITY:ATTICA",
-        "X:MAIN ST / DEAD END",
-        "CALL:4010-Chest Pain",
-        "SRC:AT",
-        "MAP:2011:174");
-
-    doTest("T9",
-        "380, ROUTE 77 ;BENNINGTON TOWN (GENESEE CO LINE / CHURCH RD)\n4030-Traumatic Injury\nBE:2011:42",
-        "ADDR:380 ROUTE 77",
-        "CITY:BENNINGTON",
-        "X:GENESEE CO LINE / CHURCH RD",
-        "CALL:4030-Traumatic Injury",
-        "SRC:BE",
-        "MAP:2011:42");
-
-    doTest("T10",
-        "(!BE) BURROUGHS RD/ROUTE 20A\n4029-Traffic Accident\nHC:2011:17",
-        "ADDR:BURROUGHS RD & ROUTE 20A",
-        "CALL:4029-Traffic Accident",
-        "SRC:HC",
-        "MAP:2011:17");
- }
+        "X:NESBITT RD & ATTICA TOWN LINE");
+    
+  }
   
   public static void main(String[] args) {
-    new NYWyomingCountyParserTest().generateTests("T1");
+    new NYWyomingCountyParserTest().generateTests("T1", "CALL ADDR APT CITY X INFO");
   }
 }
