b54101abaa594b721ba62e15c8e10f0a97e0238d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-01-15 22:55:10 +0000

    Fixed parsing problem wtih Oconee County, SC

--- cadpage/src/net/anei/cadpage/parsers/SC/SCOconeeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCOconeeCountyParser.java
@@ -3,7 +3,7 @@ package net.anei.cadpage.parsers.SC;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /*
@@ -20,10 +20,13 @@ System: Aegis
 (911 Message) S86 - CHEST PAIN  3440 BLUE RIDGE BLVD XStreet: MISTY DR, THE OLE HOME PLACE LN / TRAVELLERS BLVD WEST UNION  EDWARDS AUTO SALES  2011-00000946  11/
 (911 Message) LIFT ASSISTANCE  313 MANOR LN XStreet: INDUSTRIAL PARK PL / DEAD END SENECA     12/02/11 01:06  Narr:   COME TO THE BACK DOOR  NEEDS HELP GETTING UP
 
+Contact: support@active911.com
+(911 Message) S74 - RESPIRATORY DISTRESS  502 VERA DR XStreet: HOBSON ST / DEAD END WESTMINSTER    2012-00000025  01/14/12 01:28  Narr:   TONED RQ5  DOES HAVE HEART PROBLEMS  HEART PT HAVING TROUBLE BREATHING  E911 Info - Class of Service: RESD Special Response Info: WESTMINSTER CITY PD WESTMINSTER CITY FIRE  EMS ER-5 ER-3
+
  */
 
 
-public class SCOconeeCountyParser extends SmartAddressParser {
+public class SCOconeeCountyParser extends FieldProgramParser {
   
   private static final Pattern ID_PTN = Pattern.compile("  (\\d{4}-\\d{8})  ");
   private static final Pattern DATE_TIME_MARK = Pattern.compile("    (\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d)\\b");
@@ -31,7 +34,8 @@ public class SCOconeeCountyParser extends SmartAddressParser {
   private static final String DATE_TIME_MARK3 = "NN/NN/NN NN:NN";
   
   public SCOconeeCountyParser() {
-    super(CITY_LIST, "OCONEE COUNTY", "SC");
+    super(CITY_LIST, "OCONEE COUNTY", "SC",
+           "BASE! Narr:INFO E911_Info_-_Class_of_Service:INFO Special_Response_Info:UNIT");
   }
   
   @Override
@@ -42,58 +46,83 @@ public class SCOconeeCountyParser extends SmartAddressParser {
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("911 Message")) return false;
-    
-    // Look for an ID marker
-    String sLeader, sTrailer;
-    do {
-      Matcher match = ID_PTN.matcher(body);
-      if (match.find()) {
-        
-        // Found it, get the ID and identify leading portion
-        data.strCallId = match.group(1);
-        sLeader = body.substring(0,match.start()).trim();
-        sTrailer = body.substring(match.end()).trim();
-        
-        // This should be followed by a date and time
-        match = DATE_TIME_MARK2.matcher(sTrailer);
+    return super.parseMsg(body, data);
+  }
+  
+  private class BaseField extends Field {
+
+    @Override
+    public void parse(String body, Data data) {
+      
+      // Look for an ID marker
+      String sLeader = null;
+      String sTrailer = null;
+      do {
+        Matcher match = ID_PTN.matcher(body);
         if (match.find()) {
-          data.strDate = match.group(1);
-          data.strTime = match.group(2);
-          sTrailer = sTrailer.substring(match.end()).trim();
+          
+          // Found it, get the ID and identify leading portion
+          data.strCallId = match.group(1);
+          sLeader = body.substring(0,match.start()).trim();
+          sTrailer = body.substring(match.end()).trim();
+          
+          // This should be followed by a date and time
+          match = DATE_TIME_MARK2.matcher(sTrailer);
+          if (match.find()) {
+            data.strDate = match.group(1);
+            data.strTime = match.group(2);
+            sTrailer = sTrailer.substring(match.end()).trim();
+            break;
+          }
+          
+          // If it isn't, see if it is a truncated Date/time
+          String sCheck = sTrailer.replaceAll("\\d", "N");
+          if (! DATE_TIME_MARK3.startsWith(sCheck)) abort();
+          sTrailer = "";
           break;
         }
         
-        // If it isn't, see if it is a truncated Date/time
-        String sCheck = sTrailer.replaceAll("\\d", "N");
-        if (! DATE_TIME_MARK3.startsWith(sCheck)) return false;
-        sTrailer = "";
-        break;
-      }
-      
-      match = DATE_TIME_MARK.matcher(body);
-      if (match.find()) {
-        data.strDate = match.group(1);
-        data.strTime = match.group(2);
-        sLeader = body.substring(0,match.start()).trim();
-        sTrailer = body.substring(match.end()).trim();
-        break;
-      }
-      return false;
-    } while (false);
+        match = DATE_TIME_MARK.matcher(body);
+        if (match.find()) {
+          data.strDate = match.group(1);
+          data.strTime = match.group(2);
+          sLeader = body.substring(0,match.start()).trim();
+          sTrailer = body.substring(match.end()).trim();
+          break;
+        }
+        abort();
+      } while (false);
 
-    int pt = sLeader.indexOf("  ");
-    if (pt < 0) return false;
-    data.strCall = sLeader.substring(0,pt);
-    String sAddr = sLeader.substring(pt+2).trim();
+      int pt = sLeader.indexOf("  ");
+      if (pt < 0) abort();
+      data.strCall = sLeader.substring(0,pt);
+      String sAddr = sLeader.substring(pt+2).trim();
+      
+      sAddr = sAddr.replace(" XStreet:", " XS:");
+      parseAddress(StartType.START_ADDR, FLAG_START_FLD_REQ, sAddr, data);
+      data.strPlace = getLeft();
+    }
     
-    sAddr = sAddr.replace(" XStreet:", " XS:");
-    parseAddress(StartType.START_ADDR, FLAG_START_FLD_REQ, sAddr, data);
-    data.strPlace = getLeft();
-    
-    if (sTrailer.startsWith("Narr:")) sTrailer = sTrailer.substring(5).trim();
-    if ("Narr:".startsWith(sTrailer)) sTrailer = "";
-    data.strSupp = sTrailer;
-    return true;
+    @Override
+    public String getFieldNames() {
+      return "CALL ADDR X CITY ID DATE TIME";
+    }
+  }
+  
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf("  ");
+      if (pt >= 0) field = field.substring(pt+2).trim();
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  protected Field getField(String name) {
+    if (name.equals("BASE")) return new BaseField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    return super.getField(name);
   }
   
   private static final String[] CITY_LIST = new String[]{
