e6c090aca21e665bf32b0e5ecf451a1239408904
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-01-20 06:16:29 +0000

    Updated msg doc Fixed parsing problem with Lewis and Clark County, MT (B)

--- cadpage/src/net/anei/cadpage/parsers/MT/MTLewisAndClarkCountyBParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MT/MTLewisAndClarkCountyBParser.java
@@ -1,13 +1,14 @@
 package net.anei.cadpage.parsers.MT;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /*
 Lewis & Clark County, MT (alternate)
 Contact: support@active911.com
 Sender: runcards@lcpage.org
 
-(MUTUAL AID REQ) REQ: 1 Management Staff |Engine 6411 |Tender 6423 LOC: 5106 bluebell rd  TYPE: South Structure - 1st Alarm West Valley is requesting mutual aid, South Structure - 1st Alarm. CONTACT West Valley STAGING ON GOLD CHANNEL
+(MUTUAL AID REQ) REQ: 1 Management Staff |Engine 6411 |Tender 6423\nLOC: 5106 bluebell rd  TYPE: South Structure - 1st Alarm West Valley is requesting mutual aid, South Structure - 1st Alarm. CONTACT West Valley STAGING ON GOLD CHANNEL
 
 */
 public class MTLewisAndClarkCountyBParser extends FieldProgramParser {
@@ -15,5 +16,11 @@ public class MTLewisAndClarkCountyBParser extends FieldProgramParser {
   public MTLewisAndClarkCountyBParser() {
     super("LEWIS AND CLARK COUNTY", "MT",
            "REQ:CALL! LOC:ADDR! TYPE:INFO!");
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    body = body.replace('\n', ' ');
+    return super.parseMsg(body, data);
   }
 }
