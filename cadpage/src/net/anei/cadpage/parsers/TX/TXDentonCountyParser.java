df5c06d04b3b98619db8241ab6df63fdca122ae7
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-16 14:41:05 +0000

    Denton County,TX location now sponsored by Lake Cities FD Updated user list Updated msg doc

--- cadpage/src/net/anei/cadpage/parsers/TX/TXDentonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXDentonCountyParser.java
@@ -61,6 +61,11 @@ public class TXDentonCountyParser extends DispatchOSSIParser {
     return "CAD@dentoncounty.com";
   }
   
+  @Override
+  public String getSponsor() {
+    return "Lake Cities Fire Department";
+  }
+
   private class MyIdField extends IdField {
     public MyIdField() {
       setPattern(Pattern.compile("\\d{9}"));
