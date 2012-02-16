4f23fb50a54e340f2e0b55c28c405650b6120a52
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-16 21:40:21 +0000

    Fixed parsing problem with Ulster County, NY

--- cadpageTest/src/net/anei/cadpage/parsers/NY/NYUlsterCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NY/NYUlsterCountyParserTest.java
@@ -23,6 +23,8 @@ public class NYUlsterCountyParserTest extends BaseParserTest {
         "X:TAYLOR/CITY LINE",
         "CITY:KINGSTON",
         "CALL:Autoalarm",
+        "DATE:11/05/2010",
+        "TIME:20:44",
         "INFO:GENERAL FIRE ALARM Addtl");
 
     doTest("T2",
@@ -33,6 +35,8 @@ public class NYUlsterCountyParserTest extends BaseParserTest {
         "X:TAYLOR/CITY LINE",
         "CITY:KINGSTON",
         "CALL:Autoalarm",
+        "DATE:11/05/2010",
+        "TIME:20:44",
         "INFO:GENERAL FIRE ALARM Addtl");
 
     doTest("T3",
@@ -42,6 +46,8 @@ public class NYUlsterCountyParserTest extends BaseParserTest {
         "X:BROADWAY/MARYS  AV",
         "CITY:KINGSTON",
         "CALL:Struct Fir",
+        "DATE:11/03/2010",
+        "TIME:21:22",
         "INFO:SMOKE CODITION IN  AREA Addtl");
 
     doTest("T4",
@@ -51,6 +57,8 @@ public class NYUlsterCountyParserTest extends BaseParserTest {
         "X:TREMPER AV/SMITH AV",
         "CITY:KINGSTON",
         "CALL:Haz Mat",
+        "DATE:11/02/2010",
+        "TIME:00:45",
         "INFO:UNKNOWN ODOR IN BUILDING  Addt");
         		
 
@@ -62,7 +70,30 @@ public class NYUlsterCountyParserTest extends BaseParserTest {
         "X:HASBROUCK AV/MAPLE",
         "CITY:KINGSTON",
         "CALL:Struct Fir",
+        "DATE:11/05/2010",
+        "TIME:21:39",
         "INFO:BASEMENT FULL OF SMOKE");
-    
+
+    doTest("T6",
+        "FRM:CAD@CO.ULSTER.NY.US\n" +
+        "SUBJ:(16733) MGAF/\n" +
+        "MSG:Unit:PG68-2 UnitSts: Loc:85 MAIN XSts:WALL/GREEN Venue:King City\n" +
+        "Inc:Struct Fir\n" +
+        "(Con't) 2 of 2\n" +
+        "Date:01/31/2012 Time:14:32 BUILDING ON FIRE Addtl:CNTX:(End)",
+
+        "UNIT:PG68-2",
+        "ADDR:85 MAIN",
+        "X:WALL/GREEN",
+        "CITY:KINGSTON",
+        "CALL:Struct Fir",
+        "DATE:01/31/2012",
+        "TIME:14:32",
+        "INFO:BUILDING ON FIRE Addtl:CNTX:");
+   
+  }
+  
+  public static void main(String[] args) {
+    new NYUlsterCountyParserTest().generateTests("T1", "UNIT ADDR X CITY CALL INFO");
   }
 }
