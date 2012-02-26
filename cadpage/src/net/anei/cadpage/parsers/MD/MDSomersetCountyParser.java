6c581657c70ad0040a3a14499fec3e9e569a18f2
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-26 05:14:45 +0000

    Fixed parsing problem with Somerset County, MD

--- cadpage/src/net/anei/cadpage/parsers/MD/MDSomersetCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDSomersetCountyParser.java
@@ -2,8 +2,7 @@ package net.anei.cadpage.parsers.MD;
 
 import java.util.Properties;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 /*    
 Somerset County, MD
@@ -16,26 +15,30 @@ CAD:ST2;MEDICAL ASSIST;4248 STROBEL LN;CRIS
 CAD:ST3;MEDICAL ASSIST;29762 LOVERS LN;MARI
 CAD:ST3;MEDICAL ASSIST;28259 CRISFIELD MARION RD;MARI
  
+Contact: Allen <n3zot@verizon.net>
+Contact: Tracey Hall <snofox68@gmail.com>
+Sender: CAD@somerset911.local
+CAD:ST3;OUTSIDE FIRE;30549 MARUMSCO RD;MARI;[Fire Priority Info] RESPONSE: Bravo RESPONDER SCRIPT: Outside Fire. SMALL OUTSIDE fire (Unknown).Caller Statement
+CAD:MUTUAL AID ASSIST OUTSIDE AGEN;24666 SILVER LN
+
 */
 
-public class MDSomersetCountyParser extends FieldProgramParser {
+public class MDSomersetCountyParser extends DispatchOSSIParser {
   
   public MDSomersetCountyParser() {
     super(CITY_CODES, "SOMERSET COUNTY", "MD",
-           "SRC CALL ADDR CITY");
+           "SRC? CALL ADDR! CITY INFO+");
   }
   
   @Override
   public String getFilter() {
     return "CAD@somerset911.local";
   }
-
+  
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!body.startsWith("CAD:")) return false;
-    String[] flds = body.split(";");
-    if (flds.length != 4) return false;
-    return parseFields(flds, data);
+  public Field getField(String name) {
+    if (name.equals("SRC")) return new SourceField("[A-Z0-9]{3}");
+    return super.getField(name);
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
