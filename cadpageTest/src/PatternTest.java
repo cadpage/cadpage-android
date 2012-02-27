320801b0995d7c0630cb6a4ab61be31bbcad8b26
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-27 02:29:39 +0000

    Added Yakima County, WA location

--- cadpageTest/src/PatternTest.java
+++ cadpageTest/src/PatternTest.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 public class PatternTest {
 
   private static final Pattern MASTER = 
-    Pattern.compile("911-CENTER:([A-Z0-9]+) +(.+?) AT (.+?) (\\d{3}\\d{7}?)([ A-Z]+)*");
+    Pattern.compile("(\\d\\d\\.\\d\\d\\.\\d\\d) (\\d\\d/\\d\\d/\\d\\d) (.*?) ([A-Z]{2}FD)((?: [A-Z]+\\d+)+)( +.*)?");
   
   public static void main(String[] args) {
-    doTest("911-CENTER:1046 INJURY ACCIDENT LANTANA AT STORIE 7656250985 COUNTY");
+    doTest("08.22.10 02/07/12 EMR RED 505 MADISON AVE; GARFIELD SCHOOL TPFD TP1 YM06 TP0 UNK TYPE OF INJ  / OUTSIDE IN PLAYGROUND / 8 YR BOY - NURSE ON SCENE RP TOLD TO CALL - NOT ON SCENE MAIN PLAYGROUND - WILL MEET UNITS TO DIRECT");
   }
   
   private static void doTest(String test) {
