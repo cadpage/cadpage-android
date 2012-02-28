74be82c9dfbee7950298cc00e846bf3dfabff63d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-28 03:43:05 +0000

    Added Camden County, NJ (B) location

--- cadpageTest/src/net/anei/cadpage/parsers/NJ/NJCamdenCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NJ/NJCamdenCountyParserTest.java
@@ -1,63 +1,21 @@
 package net.anei.cadpage.parsers.NJ;
 
-import net.anei.cadpage.parsers.BaseParserTest;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
-import org.junit.Test;
 
-
-public class NJCamdenCountyParserTest extends BaseParserTest {
+@RunWith(Enclosed.class)
+public class NJCamdenCountyParserTest {
   
-  public NJCamdenCountyParserTest() {
-    setParser(new NJCamdenCountyParser(), "CAMDEN COUNTY", "NJ");
+  public static class TestA extends NJCamdenCountyAParserTest {
+    public TestA() {
+      setParser(new NJCamdenCountyParser());
+    }
   }
   
-  @Test
-  public void testParser() {
-
-    doTest("T1",
-        "(Dispatch SD45) DWELLING\n312 9TH ST ,14\n#:\nX:CENTER/CHERRY\nZN:14A\nCP:  2011-03-31 10:44:12\nMI#:110073123\nRES#:SD45",
-        "CALL:DWELLING",
-        "ADDR:312 9TH ST",
-        "CITY:Gloucester City",
-        "X:CENTER/CHERRY",
-        "MAP:14A",
-        "ID:110073123",
-        "UNIT:SD45");
-
-    doTest("T2",
-        "(Dispatch SD45) M.V.A\nROUTE 295 SB RAMP TO 76 SB ALJO CUR ,04\n#:\nX:/\nZN:04Q\nCP:ROUTE 295 SB RAMP T  2011-03-31 09:06:36\nMI#:110073062",
-        "CALL:M.V.A",
-        "ADDR:ROUTE 295 SB",
-        "MADDR:RAMP TO 76 SB ALJO CUR ROUTE 295 SB RAMP T,ROUTE 295",
-        "CITY:Bellmawr",
-        "MAP:04Q",
-        "PLACE:RAMP TO 76 SB ALJO CUR ROUTE 295 SB RAMP T",
-        "ID:110073062",
-        "UNIT:SD45");
-
-    doTest("T3",
-        "(Dispatch SD45) DIABETIC\n217 BLACK HORSE PK N ,25\n#:4\nX:CARLISLE/LAKEVIEW\nZN:25B\nCP:MAX GROUP PHILLY CO  2011-03-30 13:23:26\nMI#:11007",
-        "CALL:DIABETIC",
-        "ADDR:217 BLACK HORSE PK N",
-        "MADDR:217 BLACK HORSE PIKE N",
-        "CITY:Mount Ephraim",
-        "X:CARLISLE/LAKEVIEW",
-        "MAP:25B",
-        "PLACE:MAX GROUP PHILLY CO",
-        "ID:11007",
-        "UNIT:SD45");
-
-    doTest("T4",
-        "(Dispatch SD45) DWELLING\nBURLINGTON ST S/MONMOUTH ST ,14\n#:\nX:/\nZN:14A\nCP:  2011-03-30 10:33:28\nMI#:110072287\nRES#:SD45",
-        "CALL:DWELLING",
-        "ADDR:BURLINGTON ST S & MONMOUTH ST",
-        "CITY:Gloucester City",
-        "MAP:14A",
-        "ID:110072287",
-        "UNIT:SD45");
-  }
-  
-  public static void main(String[] args) {
-    new NJCamdenCountyParserTest().generateTests("T1");
+  public static class TestB extends NJCamdenCountyBParserTest {
+    public TestB() {
+      setParser(new NJCamdenCountyParser());
+    }
   }
 }
