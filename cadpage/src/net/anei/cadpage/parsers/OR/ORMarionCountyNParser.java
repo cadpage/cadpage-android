ffe560ed7ef4df8c9e39ad0192d5ee270077191b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-04-08 00:06:32 +0000

    Addd date/time processing to Marion County, OR (N)

--- cadpage/src/net/anei/cadpage/parsers/OR/ORMarionCountyNParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORMarionCountyNParser.java
@@ -35,30 +35,20 @@ System: New World Systems :Aegis Public Safety System
 ((4287) : ) MVA:::STATE:ST:::CASCADE:HW:3630:E425,D411,R404,M24,E52,R484,PGE,E415,LAW,N7,:1 MVA/POSS INJ/NOT BLOCKING:20111003:205247
 
 Contact: robert aerni <aernijr@gmail.com>
+Contact: Gordy <silfire402@msn.com>
+Contact: Nathan Meissner <nathan.meissner@yahoo.com>
+Contact: Megan Patterson <iputfiresout@gmail.com>
+Contact: Barb <firegroundleader@hotmail.com>
 (14036) :  / TRAU:10581::SARATOGA:DR:::::2628:R454,M23,N3,:65 YOF C/B/A LEG PAIN FROM FALL FROM FARM EQ:20110728:220453\n
 (13523) :  /  UNC:  830:N:MAIN:ST:9::::2530:R454,M22,:77YOM UNC/NOT RESP:20110728:114333\n
-
-Contact: Gordy <silfire402@msn.com>
-Sender: 6005000012
 (15331) :| AOA:  141::14TH:ST:LYONS::::3030:E405,E415,N7,:REQ ENGINE TO RESPOND TO FIRE SCN:20110730:170018 \n
-
-Contact: Nathan Meissner <nathan.meissner@yahoo.com>
 (25615) :  /  CVA:  455:W:MARQUAM:ST:33::::2630:R454,M24,N7,:65 YOM POSS CVA  C/B/A SIDE OF FACE DROOPY:20110812:150536\n
-
-Contact: Michael Cool <michael.e.cool@gmail.com>
 ((2463) : ) UNC:20510::BOONES FERRY:RD:::::1530:R903,M21,N1,:22YOF C/DIFF B/SEMI ALERT:20110929:064217
-
-Contact: "audritsh@earthlink.net" <audritsh@earthlink.net>
 ((836) Hi Gina ) LIFT: 4279::THIRD:ST:::::1829:E876,N3,:80 YOF C/B/A  NON INJURY LIFT ASSIST:20110930:090843
 ((339) Hi Gina ) COMM:  490:S:PACIFIC:HW:::::2127:E21,E22,E24,E875,LA21,A21,TIME,PGE,N3,:LOTS OF SMOKE COMING FROM BACK OF LOC/:20110929:222851
 ((432) Hi Gina ) SICK:  950::EVERGREEN:RD:211::::2026:M23,E22,N3,:86 YF C/B/A FEELING DIZZY:20110930:000053
 ((2319) Hi Gina ) MED NO :1285 TIERRA LYNN DR    :    Map:2028 :20YOM C/DIFF B/A FEVER LOW HEMOGLOBIN :09/28/2011 :20:24 :
-
-Contact: Megan Patterson <iputfiresout@gmail.com>
 (639) :  / SICK:25496:S:HW 99E::::::1433:R903,OPPS,N1,:65YOM C/A/B DIZZY,SWEATING,:20110930:070050\n
-
-Contact: Barb <firegroundleader@hotmail.com>
-Sender: trex@ci.woodburn.or.us 
  : / FALL:  653:E:PINE:ST:::::4430:MD2,R81,:FALL SEMI CON 15YRM NO BLEEDING:20120202:210913
  
 Contact: Fred Bridgehouse <bridgehousefred@gmail.com>
@@ -74,7 +64,7 @@ public class ORMarionCountyNParser extends FieldProgramParser {
   
   public ORMarionCountyNParser() {
     super(CITY_LIST, "MARION COUNTY", "OR",
-           "CALL ( ADDRCITY ( CITY | APT ) X | ADDR1 ADDR1 ADDR1 ADDR1 ( CITY | APT ) ADDR2 ADDR2 ADDR2 ) MAP UNIT INFO");
+           "CALL ( ADDRCITY ( CITY | APT ) X | ADDR1 ADDR1 ADDR1 ADDR1 ( CITY | APT ) ADDR2 ADDR2 ADDR2 ) MAP UNIT INFO DATE TIME");
   }
   
   private String address[] = new String[2];
@@ -127,11 +117,29 @@ public class ORMarionCountyNParser extends FieldProgramParser {
     }
   }
   
+  private static final Pattern DATE_PTN = Pattern.compile("\\d{8}");
+  private class MyDateField extends DateField {
+    @Override public void parse(String field, Data data) {
+      if (!DATE_PTN.matcher(field).matches()) return;
+      data.strDate = field.substring(4,6) + "/" + field.substring(6,8) + "/" + field.substring(0,4);
+    }
+  }
+  
+  private static final Pattern TIME_PTN = Pattern.compile("\\d{6}");
+  private class MyTimeField extends TimeField {
+    @Override public void parse(String field, Data data) {
+      if (!TIME_PTN.matcher(field).matches()) return;
+      data.strTime = field.substring(0,2) + ":" + field.substring(2,4) + "/" + field.substring(4,6);
+    }
+  }
+  
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR1")) return new MyAddressField(0);
     if (name.equals("ADDR2")) return new MyAddressField(1);
     if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("DATE")) return new MyDateField();
+    if (name.equals("TIME")) return new MyTimeField();
     return super.getField(name);
   }
   
