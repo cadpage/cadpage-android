eac86eaf3ec6cad823e1bbf242a82fb6a874c8de
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-04-21 20:11:40 +0000

    Fixed parsing problem with Vigo County, IN

--- cadpage/src/net/anei/cadpage/parsers/IN/INVigoCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/IN/INVigoCountyParser.java
@@ -2,12 +2,13 @@ package net.anei.cadpage.parsers.IN;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchCiscoParser;
 
 /***
 Vigo County, IN
 Contact: Billy Roberts <thfd334@gmail.com>
-Sender: 37284
+Sender: 37284,cisco.paging@vigocounty.in.gov
 System: Cisco
 Ct:FALL Loc:3301 ST MARY'S RD Apt: XSt:BLOOMTOWN RD US HWY 150 Grid:112D Units:E-42 TC-1 Rmk:92 YOLD FML; FELL; FACE DOWN ON FLOOR -
 Ct:FALL Loc:309 N CHURCH ST Apt: XSt:PARIS AV Grid:0WTH Units:E-41 TC-2 Rmk:65 Years F ;fall
@@ -18,6 +19,10 @@ Ct:EMS MAJOR Loc:6244 WILMINGTON PIKE Apt: XSt:CLYO RD CENTER POINT DR Grid:003B
 
 Contact: "Dennis Boyd" <dboyd462@gmail.com>
 Ct:FIRE-ENTRAPMENT Loc:101 N 7TH ST WTH Apt: XSt:JOHNSON AV MILLER AV Grid:0WTH Units:E-41 E-42 L-41 R-41 Rmk:HOUSE ON FIRE PEOPLE STILL INSIDE
+
+Contact: Billy Roberts <thfd334@gmail.com>
+Sender: 37284,cisco.paging@vigocounty.in.gov
+[TRASH OR RUBBISH-FIRE]  Loc:428 W POPLAR ST XSt:5TH ST WTH 4TH ST WTH Grid:0WTH Units:E-41 Rmk:RP ADV MALE SUBJECT THERE THAT IS CAUSING PROBLEMS AND
 
 ***/
 
@@ -33,6 +38,15 @@ public class INVigoCountyParser extends DispatchCiscoParser {
   
   @Override
   public String getFilter() {
-    return "37284";
+    return "37284,cisco.paging@vigocounty.in.gov";
+  }
+  
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!body.startsWith("Ct:")) {
+      if (subject.length() == 0) return false;
+      body = "Ct:" + subject + " " + body;
+    }
+    return super.parseMsg(body, data);
   }
 }
