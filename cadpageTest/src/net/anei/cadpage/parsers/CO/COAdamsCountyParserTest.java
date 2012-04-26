2d83a75152acdb448d313499d013eb50fa548fab
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-04-26 16:08:30 +0000

    Fixed parsing probelm with Adams County, CO

--- cadpageTest/src/net/anei/cadpage/parsers/CO/COAdamsCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/CO/COAdamsCountyParserTest.java
@@ -19,34 +19,41 @@ public class COAdamsCountyParserTest extends BaseParserTest {
         "Subject:IPS I/Page Notification E 64TH AVE/MONACO ST ADAM CCPD 09:51:48 TYPE CODE: ACCI CALLER NAME: TIME: 09:51:48 Comments: -104.90947",
         "ADDR:E 64TH AVE & MONACO ST",
         "TIME:09:51:48",
-        "CALL:ACCI");
+        "CALL:ACCI",
+        "INFO:-104.90947");
     
     doTest("T2",
         "Subject:IPS I/Page Notification 6950 NIAGARA ST ADAM CCPD 11:04:10 TYPE CODE: EMS CALLER NAME: FELIX - 13YO TIME: 11:04:10 Comments: 15YO",
         "ADDR:6950 NIAGARA ST",
         "TIME:11:04:10",
-        "CALL:EMS");
+        "CALL:EMS",
+        "NAME:FELIX - 13YO",
+        "INFO:15YO");
     
     doTest("T3",
         "Subject:IPS I/Page Notification 10220 BRIGHTON RD ADAM CCPD:DIVERSIFIES TRUCK AND RV 17:22:11 TYPE CODE: NONSTR CALLER NAME: BOB TIME: 17:",
         "ADDR:10220 BRIGHTON RD",
         "PLACE:DIVERSIFIES TRUCK AND RV",
         "TIME:17:22:11",
-        "CALL:NONSTR");
+        "CALL:NONSTR",
+        "NAME:BOB");
       
     doTest("T4",
         "Subject:IPS I/Page Notification US HIGHWAY 85 NB/E 77TH AVE ADAM ADAM 03:34:04 TYPE CODE: NONSTR CALLER NAME: ASHLEY TIME: 03:34:04 Commen",
         "ADDR:US HIGHWAY 85 NB & E 77TH AVE",
         "MADDR:US HIGHWAY 85 & E 77TH AVE",
         "TIME:03:34:04",
-        "CALL:NONSTR");
+        "CALL:NONSTR",
+        "NAME:ASHLEY");
     
     doTest("T5",
         "Subject:IPS I/Page Notification 9900 E 102ND AVE ADAM CCPD:USF 06:06:49 TYPE CODE: FRALRM CALLER NAME: ADT TIME: 06:06:49 Comments: WATER",
         "ADDR:9900 E 102ND AVE",
         "PLACE:USF",
         "TIME:06:06:49",
-        "CALL:FRALRM");
+        "CALL:FRALRM",
+        "NAME:ADT",
+        "INFO:WATER");
     
     doTest("T6",
         "Subject:IPS I/Page Notification\n26900 E COLFAX AVE ARAP ARAP,454: @FOX RIDGE FARMS 17:19:15 TYPE CODE: EMS CALLER NAME:  TIME: 17:19:15 Co",
@@ -67,11 +74,17 @@ public class COAdamsCountyParserTest extends BaseParserTest {
         "ADDR:E-470 EB & E 56TH AVE",
         "MADDR:E-470 & E 56TH AVE",
         "TIME:22:58:36",
-        "CALL:NONSTR");
+        "CALL:NONSTR",
+        "INFO:-104.701552 +39.798521 WH");
+    
+    doTest("T9",
+        "Subject:IPS I/Page Notification\n510.5 S 2nd Ave major incident-- hazmat  no additional equip needed at this time\r\n\r\n\r",
+        "ADDR:510.5 S 2nd Ave",
+        "CALL:major incident-- hazmat no additional equip needed at this time");
     
   }
   
   public static void main(String[] args) {
-    new COAdamsCountyParserTest().generateTests("T1", "ADDR CITY PLACE TIME CALL");
+    new COAdamsCountyParserTest().generateTests("T1");
   }
 }
