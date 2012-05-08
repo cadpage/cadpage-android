1b7162e6f87bdb715e751c58a8840f8835b6464b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-05-08 17:09:42 +0000

    Fixed parsing problem with Eau Clair, WI

--- cadpageTest/src/net/anei/cadpage/parsers/WI/WIEauClaireParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/WI/WIEauClaireParserTest.java
@@ -96,7 +96,7 @@ public class WIEauClaireParserTest extends BaseParserTest {
     doTest("T1",
         "@4:38:53 PM // Rock Creek (Dunn) // HWY 85 & 870th // Motorvehicle\nCollision // 3 vehicle 10/50 PI, no extrication, Station 5 requested for\nassistance",
         "TIME:4:38:53 PM",
-        "CITY:Rock Creek (Dunn)",
+        "CITY:Rock Creek, Dunn",
         "ADDR:HWY 85 & 870th",
         "CALL:Motorvehicle Collision",
         "INFO:3 vehicle 10/50 PI, no extrication, Station 5 requested for assistance");
@@ -114,6 +114,106 @@ public class WIEauClaireParserTest extends BaseParserTest {
         "ADDR:Peterson Ave & Olson Dr",
         "CALL:MVC Extrication",
         "INFO:10/50 PI with Extrication needed");
+
+  }
+  
+  @Test
+  public void testChrisHedlund() {
+
+    doTest("T1",
+        "(Case# 12-117) @2:40:48 PM // Washington // henry at pamona // Motorvehicle Collision\n//",
+        "ID:12-117",
+        "TIME:2:40:48 PM",
+        "CITY:Washington",
+        "ADDR:henry at pamona",  // <<< Google says this is Pomona
+        "CALL:Motorvehicle Collision");
+
+    doTest("T2",
+        "(Case# 12-116) @1:51:32 PM // Washington // 500 block Deerfield RD  //\nIllegal/Attempted Burn // large brush pile",
+        "ID:12-116",
+        "TIME:1:51:32 PM",
+        "CITY:Washington",
+        "ADDR:500 block Deerfield RD",
+        "MADDR:500  Deerfield RD",
+        "CALL:Illegal/Attempted Burn",
+        "INFO:large brush pile");
+
+    doTest("T3",
+        "(Case# 12-115) @10:31:26 PM // Seymour // 13034 Rossman Dr. // Electrical/Powerline\nEmergency // wires arcing",
+        "ID:12-115",
+        "TIME:10:31:26 PM",
+        "CITY:Seymour",
+        "ADDR:13034 Rossman Dr",
+        "CALL:Electrical/Powerline Emergency",
+        "INFO:wires arcing");
+
+    doTest("T4",
+        "(Case# 12-114) @6:32:48 PM // Union // 2211 Sherman Creek Rd // Service Call // Tree\nhas fallen into a house",
+        "ID:12-114",
+        "TIME:6:32:48 PM",
+        "CITY:Union",
+        "ADDR:2211 Sherman Creek Rd",
+        "CALL:Service Call",
+        "INFO:Tree has fallen into a house");
+
+    doTest("T5",
+        "(Case# 12-112) @5:58:51 AM // Tiffany Twp (Dunn) // N11504 280th St // Search Call //\n" +
+        "Search for hunter missing for 26+hours.  Request is for Manpower and\n" +
+        "possi",
+
+        "ID:12-112",
+        "TIME:5:58:51 AM",
+        "CITY:Tiffany, Dunn",
+        "ADDR:N11504 280th St",
+        "CALL:Search Call",
+        "INFO:Search for hunter missing for 26+hours. Request is for Manpower and possi");
+
+    doTest("T6",
+        "(Case# 12-110) @1:10:09 PM // Brunswick // 4800 Karissa Dr. // Motorvehicle Collision\n// 10/50 PI",
+        "ID:12-110",
+        "TIME:1:10:09 PM",
+        "CITY:Brunswick",
+        "ADDR:4800 Karissa Dr",
+        "CALL:Motorvehicle Collision",
+        "INFO:10/50 PI");
+
+    doTest("T7",
+        "(Case# 12-107) @8:14:11 AM // Seymour // Freeway 53 92mm Southbound // Vehicle Fire //\nvehicle fire",
+        "ID:12-107",
+        "TIME:8:14:11 AM",
+        "CITY:Seymour",
+        "ADDR:Freeway 53 92mm Southbound",
+        "CALL:Vehicle Fire",
+        "INFO:vehicle fire");
+
+    doTest("T8",
+        "(Case# 12-104) @11:13:39 AM // Brunswick // S8725 County Line Rd // Brush/Wildland Fire\n" +
+        "// possible out of control burn/brush fire in a field",
+
+        "ID:12-104",
+        "TIME:11:13:39 AM",
+        "CITY:Brunswick",
+        "ADDR:S8725 County Line Rd",  // <<< DOesn't map if S is separated
+        "CALL:Brush/Wildland Fire",
+        "INFO:possible out of control burn/brush fire in a field");
+
+    doTest("T9",
+        "(Case# 12-099) @2:45:18 AM // Seymour // Peterson Ave & Olson Dr. // MVC Extrication //\n10/50 PI with Extrication needed",
+        "ID:12-099",
+        "TIME:2:45:18 AM",
+        "CITY:Seymour",
+        "ADDR:Peterson Ave & Olson Dr",
+        "CALL:MVC Extrication",
+        "INFO:10/50 PI with Extrication needed");
+
+    doTest("T10",
+        "(Case# 12-100) @1:33:50 PM // Pleasant Valley // S10700 Hwy 93 // Brush/Wildland Fire\n// grass fire",
+        "ID:12-100",
+        "TIME:1:33:50 PM",
+        "CITY:Pleasant Valley",
+        "ADDR:S10700 Hwy 93",  // <<< Maps if S is separated
+        "CALL:Brush/Wildland Fire",
+        "INFO:grass fire");
 
   }
   
