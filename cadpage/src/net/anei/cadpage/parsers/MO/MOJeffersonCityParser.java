e00e883525bc5415e7670cf47bec02dbbb4cb72d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-05-08 16:27:23 +0000

    Fixed parsing problems iwth Jefferson City, MO

--- cadpage/src/net/anei/cadpage/parsers/MO/MOJeffersonCityParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MO/MOJeffersonCityParser.java
@@ -61,14 +61,80 @@ Sender: paging@jeffcitymo.org
 (DONOTREPLY) E4  18:42 Natural Cover  - 7 HILLS -Cross Streets-   NATURAL COVER FIRE IN THE AREA 
 (DONOTREPLY) Suchanek, Celeste 17:02 Medical Emergency 807 MONROE -Cross Streets- E HESS WAY / E ASHLEY ST E1   FALLEN OFF BIKE 
 
+[DONOTREPLY] 1101 1401 1110 112 CCPager  16:58 Fire Alarm - 2227 SADDLEBROOKE LAKE -Cross Streets- CHERISH CT / SCRUGGS STATION RD   ALLEN RESIDENCE \n
+[DONOTREPLY] Suchanek, Celeste 17:02 Medical Emergency 807 MONROE -Cross Streets- E HESS WAY / E ASHLEY ST E1   FALLEN OFF BIKE \n
+[DONOTREPLY] Suchanek, Celeste 17:51 Medical Emergency 8501 NO MORE VICTIMS -Cross Streets- DEAD END / CORTEZ DR S2   nose bleed   front entrance \n
+[DONOTREPLY] E4  18:42 Natural Cover  - 7 HILLS -Cross Streets-   NATURAL COVER FIRE IN THE AREA \n
+[DONOTREPLY]  19:01 Test - 1229 BROADWAY -Cross Streets- W RAILROAD RD, MARION ST / RT NN, MAIN ST   REGIONAL WEST FIRE PROTECTION DISTRICT ALARM AND PAGER TEST. \n
+[DONOTREPLY] Gabathuler, Erin 19:46 Medical Emergency 925 DUNKLIN A -Cross Streets- LEE DR, LINN ST / CHESTNUT ST E1   FEMALE WAS SPEAKING WITH THE RP AND THEN HER EYES\n
+[DONOTREPLY] Gabathuler, Erin 20:08 Medical Alarm 1509 STADIUM -Address Between- MYRTLE AVE / SOUTHWEST BLVD E4   CCEMS ADVISED  KEY - UNDER WHITE ROCK - UNDER/NEX\n
+[DONOTREPLY] S5  22:59 Accident with Injuries - 1700 INDUSTRIAL -Cross Streets- ARGONNE ST / HUGHES ST   EMS ADV  CAR VS TRAIN-  TRAPPED IN VEH \n
+[DONOTREPLY] Batt11  22:59 Extrication - 1700 INDUSTRIAL -Cross Streets- ARGONNE ST / HUGHES ST   UNION PACIFIC ADVISED- THEY CALLED INTO REPORT A PROBLEM - THEY WERE UNSURE OF WHAT THE PROBLEM WAS- THEY HAVE BEEN ADVISED TO STOP ALL TRAIN TRAFFIC  EMS ADV  CAR VS TRAIN-  TRAPPED IN VEH \n
+[DONOTREPLY] DC4 E1 L1 S2 E4  23:39 Fire Alarm - 2033 CHRISTY -Cross Streets- VETERANS LN / RT B   Call Number 434 was created from Call Number 433(May  3 2012 11:37PM)  PERIMETER AND FIRE ALARM - OFFICE HALL MOTION DETECTOR #2406 \n
+[DONOTREPLY] AC2 E1 L1 S2 E3  08:40 Fire Alarm - 1445 CHRISTY -Cross Streets- OSCAR DR / RAMP 31-970733, TANNER BRIDGE RD   GENERAL  FIRE ALARM - ROOM SMOKE  MID MO ANESTHIA \n
+[DONOTREPLY] Degraffenreid, Jenny 09:41 Medical Emergency 2225 WEATHERED ROCK 4 -Cross Streets- LORENZO GREENE DR / MILLBROOK DR E1   SAYS SHE TRIED TO DRIVE BUT COULDN'T. BLEEDING FR\n
+[DONOTREPLY] E3  13:39 Line Down  - 1304 MCCARTY -Cross Streets- HAVANA ST, HART ST / HAVANA ST   TREE DOWN ACROSS W MCCARTY STREET - TOOK DOWN POWER LINES AS WELL  Call #: 617 - RP THINKS WIRES ARE DOWN ON MCCARTY  Call #: 617 - RP HEARD A LOUD POPPING SOUND \n
+[DONOTREPLY] E1  13:46 Assist Police - 2201 MAIN -Cross Streets- MEMORIAL PARK DR / FOREST HILL AVE   93 REQUESTING WASH DOWN AFTER THE SAFETY PATROL PICNIC. (JUST SEND E1) \n
+[DONOTREPLY] Degraffenreid, Jenny 14:24 Medical Emergency 120 DUNKLIN -Cross Streets- W DUNKLIN ST, JEFFERSON ST / MADISON ST S5  \n
+[DONOTREPLY] AC2 E4 S5 E3 L1  14:48 Fire Alarm - 1201 FAIRGROUNDS -Cross Streets- VILLAGE DR / S COUNTRY CLUB DR, W EDGEWOOD DR   OFFICE AREA \n
+[DONOTREPLY] Taylor, Tiffany 15:08 Medical Emergency 711 KANSAS -Cross Streets- WASHINGTON PARK DR / MYRTLE AVE, SOUTH BLVD E1   SUBJ IS NOW TRYING TO LEAVE -  RP WAS ABLE TO GET\n
+[DONOTREPLY] Degraffenreid, Jenny 16:33 Medical Emergency 406 ATCHISON -Cross Streets- No Cross Streets Found E1   SEIZURE - BEING COMBATIVE \n
+[DONOTREPLY] Degraffenreid, Jenny 16:42 Medical Emergency 2021 WILLIAM -Cross Streets- BLAIR DR / DIX RD E3   STOMACH PAIN, FACE IS FLUSH, PRESSURE IN HEAD.  S\n
+[DONOTREPLY] AC2 L1 E4  16:42 Accident with Injuries - JEFFERSON -Cross Streets-   EMS NOTIFIED  Call #: 691 - METALIC CAR  ELDERLY MALE DRIVER  Call #: 691 - AT CONOCO  UNKNOWN INJURIES  SIL CAR RAN INTO GAURD-RAIL  Call #: 691 - CALLER DOES NOT BELIEVE THIS IS AN ACCIDENT, BUT MAYBE ELDERLY MALE HAVING SOME KIND OF ISSUE  Call #: 691 - VEH IS UP ON GUARD RAIL  Call #: 691 - W/M\n
+[DONOTREPLY] Degraffenreid, Jenny 16:42 Medical Emergency 2021 WILLIAM -Cross Streets- BLAIR DR / DIX RD L3   STOMACH PAIN, FACE IS FLUSH, PRESSURE IN HEAD.  S\n
+[DONOTREPLY] Schlitt, Meagen 18:53 Medical Emergency 3038 TRUMAN -Cross Streets- DUPONT CIR / N TEN MILE DR E3   RM 401  RESIDENT MAY BE HAVING HEART ATTACK \n
+[DONOTREPLY] AC2 E1 L1 S2 E4  19:22 Structure Fire - ELM -Cross Streets-   WHITE SMOKE COMING FROM FRONT OF BUILDING \n
+[DONOTREPLY] 1401 1531 1201 114 CCPager  22:41 Accident with Injuries - 54 -Cross Streets-   OPER 133 CALLING TROOP  CCEMS ADVISED  ONE PATIENT INJURED  ONE VEH ACCIDENT   WHITE  93  CHEVY LUMINA-  HIT THE GUARDRAIL ON THE HWY -  FEMALES HEAD IS BLEEDING \n
+[DONOTREPLY] Volmert, F Michelle 01:01 Medical Emergency 401 DUNKLIN -Cross Streets- MULBERRY ST / MISSOURI BLVD, BOLIVAR ST E1   FEMALE IS LISA CREASON-   THEY ARE SITTING AT THE\n
+[DONOTREPLY] Volmert, F Michelle 05:16 Medical Emergency 706 HOUCHIN A -Cross Streets- E DUNKLIN ST / BALD HILL RD E4   ASTHMA ATTACK \n
+[DONOTREPLY] E1  07:12 Test - 621 HIGH -Cross Streets- BOLIVAR ST / RAMP ON RAMP, RAMP OFF RAMP  \n
+[DONOTREPLY] Bashore, April 10:39 Medical Alarm 12 JACKSON 513 -Address Between- DEAD END / E STATE ST E1   GENERAL MEDICAL ALARM \n
+[DONOTREPLY] E1  10:55 Trapped In Elevator - 12 JACKSON -Cross Streets- DEAD END / E STATE ST   SUBL STUCK IN ELEVATOR \n
+[DONOTREPLY] Batt11 E1 L1 E3 S5  12:15 Fire Alarm - 1303 EDMONDS -Cross Streets- MONTANA ST / MYRTLE AVE   SECOND FLOOR ROOM SMOKE \n
+[DONOTREPLY] Bashore, April 15:04 Medical Emergency 221 FILLMORE -Cross Streets- LINDEN DR, BROADWAY ST / WASHINGTON ST E1   SHE IS 101  MALE STATING MARGARET WAS READING AND\n
+[DONOTREPLY] Snyder, Jodi 18:23 Medical Emergency 242 JAYCEE -Cross Streets- SCHELLRIDGE RD / INDUSTRIAL DR E3   CUSTOMER HAVING SEZUIRE \n
+[DONOTREPLY] Snyder, Jodi 21:02 Medical Alarm 945 WINDSOR -Address Between- WESTWOOD DR / TOWER DR E3   JAMES WAGGONER, CODE FOR GARAGE 4949, CAN ACCESS \n
+[DONOTREPLY] 00:47 Trash Dumpster - 1131 MCCARTY 2 -Cross Streets- LOCUST ST / CLARK AVE E1  \n
+[DONOTREPLY] 05:15 Fire Alarm - 1201 FAIRGROUNDS -Cross Streets- VILLAGE DR / S COUNTRY CLUB DR, W EDGEWOOD DR Batt11 E4 S5 E3 L3  \n
+[DONOTREPLY] Medical Emergency 314 BROOKS ST JEFFERSON CITY -Cross Streets- W MCCARTY ST / W HIGH ST E1   E1 \n
+[DONOTREPLY] 08:27 Fire Alarm - 1609 ST MARYS -Cross Streets- HOWARD ST / GIPFERT LN Batt13 E4 L3 E3 S5   DOROTHIA SCOTT RES - HALL WAY ZONE \n
+[DONOTREPLY] 08:31 Fire Alarm - 1201 FAIRGROUNDS -Cross Streets- VILLAGE DR / S COUNTRY CLUB DR, W EDGEWOOD DR S5 E4 L1   MAIN OFFICE AREA \n
+[DONOTREPLY] Medical Emergency 706 OHIO ST JEFFERSON CITY -Cross Streets- MISSOURI BLVD / LOUISIANA AVE E1   ELDERLY CHURCH MEMBER HAS FALLEN AND HIT HEAD, LA E1 \n
+[DONOTREPLY] 12:38 Assist Citizen Fire - 2731 MERCHANTS -Cross Streets- METRO DR / S HWY 179 E3  \n
+[DONOTREPLY] Medical Emergency 730 W  STADIUM BLVD JEFFERSON CITY -Cross Streets- HYDE PARK RD / MISSOURI BLVD, ST MARYS BLVD E3   E3 \n
+[DONOTREPLY] 15:13 Fire Alarm - 1609 ST MARYS -Cross Streets- HOWARD ST / GIPFERT LN Batt13 E1 L3 E3 S5   SON IS ON SITE AND STATED EVERYTHING IS FINE - RESIDENT DOESN'T REMEMBER THE CODE  RESIDENT IS ELDERLY  HALLWAY FIRE  DORTHY SCOTT RESIDENT \n
+[DONOTREPLY] 15:16 Fire Alarm - 3016 DUPONT -Cross Streets- W TRUMAN BLVD / DEAD END Batt13 E1 L3 E4 S5   GENERAL \n
+[DONOTREPLY] Medical Emergency 2700 CHERRY CREEK CT I205 JEFFERSON CITY -Cross Streets- DEAD END / N HWY 179 E3   CCEMS NOTIFIED  ELDERLY FEMALE HAS FALLEN - DOOR  E3 \n
+[DONOTREPLY] Medical Emergency 812 E  HIGH ST A JEFFERSON CITY -Cross Streets- CHERRY ST / CHESTNUT ST E1   BAD PHONE CONNECTION  MALE CAN'T WALK  CCEMS NOTI E1 \n
+[DONOTREPLY] Medical Emergency 811 MONROE ST B JEFFERSON CITY -Cross Streets- E HESS WAY / E ATCHISON ST E1   FEMALE IS ON THE PATIO  GIRLFRIEND FELL OUT OF CH E1 \n
+[DONOTREPLY] 07:23 Line Down  - 800 MYRTLE blk -Cross Streets- PIKE ST / KANSAS ST, SOUTH BLVD E1   POSSIBLE POWER LINE BROKEN AND PARTIALLY LYING IN THE STREET \n
+[DONOTREPLY] 09:17 Fire Alarm - 6336 ALGOA -Cross Streets- CALBACKNA RD / N SHAMROCK RD Batt11 E3 L1 S2 E4  \n
+[DONOTREPLY] 10:00 Fire Alarm - 6336 ALGOA -Cross Streets- CALBACKNA RD / N SHAMROCK RD S2   WATERFLOW FIRE ALARM CAME IN 3 TIMES  AUTOMATIC FIRE ALARM \n
+[DONOTREPLY] Medical Emergency 1100 MISSOURI BLVD JEFFERSON CITY -Cross Streets- GEORGIA ST / KANSAS ST E1   CUSTOMER ON SCEN   CHEST PAIN  E1 \n
+[DONOTREPLY] Medical Emergency 1952 SARATOGA BLVD A JEFFERSON CITY -Cross Streets- LEXINGTON DR / LEXINGTON DR E4   91YO FEMALE  LOCATED IN THE KITCHEN  WIFE FELL AN E4 \n
+[DONOTREPLY] 14:19 Accident with Injuries - MISSOURI -Cross Streets- Batt11 L3   EMS ENROUTE  PULLING IN TO THE COLTONS PARKING LOT  HE STATES THE FEMALE IS REFUSING TO MOVE AWAY FROM THE LIGHT.  AND IS BLOCKING TRAFFIC  INEZ THOMAS IN THE TAN SONATA  2ND PARTY CALLED - COMPLAINING OF NECK PAIN  RP STATES THE OPERATOR OF THE SONATA THREATENED HIM.  HE BUMPED HER BUMPER, NO DAMA\n
+[DONOTREPLY] Medical Emergency 12 JACKSON ST JEFFERSON CITY -Cross Streets- DEAD END / E STATE ST E1   CCEMS NOTIFIED  JUST INSIDE IN THE LOBBY - MALE F E1 \n
+[DONOTREPLY] 17:48 Fire Alarm - 301 HIGH -Cross Streets- BROADWAY ST / RAMP ON RAMP, RAMP OFF RAMP Batt11 E1 L1 S2 E4  \n
+[DONOTREPLY] 18:04 Message Deliver - 5206 MONTICELLO -Cross Streets- WALTHER AVE / MOREAU RIDGE RD, OAKRIDGE RD   TRAINING MEETING TONIGHT 1900HRS AUTHORITY 110 \n
+[DONOTREPLY] 18:57 Unusual Fire Request - 1922 MISSOURI -Cross Streets- HILDA ST / DIX RD L3   OPER 149 CALLED STATION 1 - L3 WILL RESPOND  SOMEONE THREW THEIR KEYS ON THE ROOF OF BASKIN ROBBINS - EMPP DOES NOT HAVE ACCESS TO THE ROOF \n
+[DONOTREPLY] 19:01 Test - 4017 COUNTY PARK -Cross Streets- ROCKRIDGE RD / FAIRGROUNDS RD, LEANDRA LN   COLE CO FIRE PROTECTION DISTRICT WEEKLY ALARM AND PAGER TEST. \n
+[DONOTREPLY] Medical Emergency 210 EL MERCADO PLZ JEFFERSON CITY -Cross Streets- No Cross Streets Found S5   THIS IS METRO BUSINESS COLLEGE  TEACHER IS ILL, F S5 \n
+[DONOTREPLY] Medical Emergency 50 JEFFERSON CITY -Cross Streets- E3 S5   ****2 YO HAVING SEIZURE  2 YO WITH FLASHERS ON  H E3 S5 \n
+[DONOTREPLY] 23:04 Smoke In Structure - 509 TURNBERRY JEFFERSON CITY -Cross Streets- STONE BRIAR RD / PINEHURST CT Batt11 E4 S5 E3 L3  \n
+[DONOTREPLY] 23:39 Accident with Injuries - 1101 S  COUNTRY CLUB JEFFERSON CITY -Cross Streets- OLD LOHMAN RD / DENESE DR Batt11 S5   CCEMS ADVISED  UNKNOWN INJURIES-RP NOT ABLE TO GO OUTSIDE & CHECK  1 VEH IN RP'S FRONT YARD- \n
+[DONOTREPLY] 02:52 Fire Alarm - 509 TURNBERRY JEFFERSON CITY -Cross Streets- STONE BRIAR RD / PINEHURST CT Batt11 E4 S5 E3 L3   OPER TRAVIS REF # 2180  TOM KOLB RESD--GENERAL \n
+[DONOTREPLY] Medical Emergency 235 JOE LN JEFFERSON CITY -Cross Streets- DEAD END / SCENIC DR S2   NO HISTORY OF BREATHING PROBLEMS  63 YO MALE HAVI S2 \n
+
 */
 
 
 public class MOJeffersonCityParser extends MsgParser {
   
   private static final Pattern MASTER_PTN = 
-    Pattern.compile("(.*)\\b(\\d\\d:\\d\\d) (.*) (?:-Cross Streets-|-Address Between-) (.*)");
+    Pattern.compile("(?:(.*)\\b(\\d\\d:\\d\\d) )?(.*) (?:-Cross Streets-|-Address Between-) (.*)");
   private static final Pattern NUMBER_PTN = Pattern.compile("\\b\\d{2,}\\b");
+  private static final Pattern UNIT_PTN = Pattern.compile("(?:(?: |^)[A-Z]+\\d+)+$", Pattern.CASE_INSENSITIVE);
+  private static final Pattern UNIT2_PTN = Pattern.compile("(?:(?: |^)[A-Z]+\\d+)+ ", Pattern.CASE_INSENSITIVE);
   
   public MOJeffersonCityParser() {
     super("JEFFERSON CITY", "MO");
@@ -91,16 +157,17 @@ public class MOJeffersonCityParser extends MsgParser {
     Matcher match = MASTER_PTN.matcher(body);
     if (!match.find()) return false;
     
-    String sPrefix = match.group(1).trim();
+    String sPrefix = getOptGroup(match.group(1));
     if (sPrefix.contains(",")) {
       data.strName = sPrefix;
     } else {
       data.strUnit = sPrefix;
     }
-    data.strTime = match.group(2);
+    data.strTime = getOptGroup(match.group(2));
     String sPart1 = match.group(3).trim();
     String sPart2 = match.group(4);
     
+    if (sPart1.endsWith(" JEFFERSON CITY")) sPart1 = sPart1.substring(0,sPart1.length()-15);
     int pt = sPart1.indexOf(" - ");
     if (pt >= 0) {
       data.strCall = sPart1.substring(0,pt).trim();
@@ -111,8 +178,30 @@ public class MOJeffersonCityParser extends MsgParser {
     } else return false;
     
     Parser p = new Parser(sPart2);
-    data.strCross = p.get("  ");
+    String sCross = p.get("  ");
     data.strSupp = p.get();
+    
+    match = UNIT_PTN.matcher(sCross);
+    if (match.find()) {
+      data.strUnit = match.group().trim();
+      sCross = sCross.substring(0,match.start()).trim();
+    }
+    
+    if (data.strUnit.length() == 0 && data.strSupp.length() == 0) {
+      match = UNIT2_PTN.matcher(sCross);
+      if (match.find()) {
+        data.strUnit = match.group().trim();
+        data.strSupp = sCross.substring(match.end()).trim();
+        sCross = sCross.substring(0,match.start()).trim();
+      }
+    }
+    data.strCross = sCross;
+    
+    if (data.strUnit.length() > 0 && data.strSupp.length() > 0) {
+      if (data.strSupp.endsWith(data.strUnit)) {
+        data.strSupp = data.strSupp.substring(0,data.strSupp.length()-data.strUnit.length()).trim();
+      }
+    }
     return true;
   }
 }
