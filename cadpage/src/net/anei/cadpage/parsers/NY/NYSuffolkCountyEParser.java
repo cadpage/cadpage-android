00c9ac3564677d8ded26ad61615496b4753450be
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-05-10 02:45:56 +0000

    Fixed parsing problem with Suffolk County, NY (E)

--- cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyEParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyEParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NY;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.SmartAddressParser;
 
@@ -21,9 +24,24 @@ Sender: Bob@relaycom.com
 (FROM RELAY) 1272 EAST MAIN ST  DR. BROOK'S OFC  MALE  FAINTING EPISODES
 (MSG FROM RELAY) MICHAEL'S 1440 OLD COUNTRY RD. FEMALE. GENERAL WEAKNESS.
 
+Contact: Jim Lull <jlull65@gmail.com>
+Sender: Bob@relaycom.com
+/RELAY /201 TROT BROOK LANE FEMALE FACIAL INJURY\n
+/FROM RELAY /1035 PARKWAY\nFEMALE WITH CHEST PAINS\n
+/FROM RELAY /641 NORTHVILLE TPKE & FISHEL\nFEMALE IS SICK\n
+/FROM RELAY /MERCY HIGH SCHOOL\nYOUNG ADULT HEAD INJURY\nWEIGHT ROOM\n
+/FROM RELAY /ATLANTIS MARINE WORLD\nGIFT SHOP  FEMALE INJURIES FROM A FALL\n
+/FROM RELAY /819 E MAIN\nDEPT OF SOCIAL SVCS\nPOSSIBLE OVER DOSE\n
+/FROM RELAY /UNITED COMPREHENSIVE CARE\n170 OLD COUNTRY RD\nADULT MALE TROUBLE BREATHING\n
+/FROM RELAY /TANGER 2 STE 101 NY&CO\nFEMALE SUBJECT ILL\n
+
  */
 
 public class NYSuffolkCountyEParser extends SmartAddressParser {
+
+  private static final Pattern MARKER = Pattern.compile("^/(?:FROM )?RELAY */");
+  private static final Pattern DELIM = Pattern.compile(" *\n *|  +| *,+ +");
+  private static final Pattern OFF_OF = Pattern.compile("\\bOFF OF\\b");
   
   public NYSuffolkCountyEParser() {
     super(CITY_LIST, "SUFFOLK COUNTY","NY");
@@ -37,19 +55,56 @@ public class NYSuffolkCountyEParser extends SmartAddressParser {
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
     
-    if (!subject.contains("FROM RELAY")) return false;
+    do {
+      Matcher match = MARKER.matcher(body);
+      if (match.find()) {
+        body = body.substring(match.end()).trim();
+        break;
+      }
+      
+      if (subject.contains("FROM RELAY")) break;
+      
+      return false;
+    } while (false);
     
     // Punctuation is too unreliable to count on
-    body = body.replaceAll("[\n;,]", " ").replaceAll("  +", " ");
-    body = body.replaceAll("\\bOFF OF\\b", "XS:");
+    body = body.replace(".", "");
+    body = OFF_OF.matcher(body).replaceAll("XS:");
     
-    // Feed everything to smart parser
-    parseAddress(StartType.START_PLACE, FLAG_IGNORE_AT, body, data);
-    data.strSupp = getLeft();
+    String[] flds = DELIM.split(body);
+    if (flds.length == 1) {
+      
+      // Feed everything to smart parser
+      parseAddress(StartType.START_PLACE, FLAG_IGNORE_AT, body, data);
+      data.strSupp = getLeft();
+      
+      // Clean up loose ends
+      String sAddr = data.strAddress;
+      if (sAddr.endsWith(" IN")) data.strAddress = sAddr.substring(0, sAddr.length()-3).trim();
+      
+      if (data.strPlace.length() > 0 && data.strAddress.length() == 0 && data.strSupp.length() == 0) {
+        data.strSupp = data.strPlace;
+        data.strPlace = "";
+      }
+    } 
     
-    // Clean up loose ends
-    String sAddr = data.strAddress;
-    if (sAddr.endsWith(" IN")) data.strAddress = sAddr.substring(0, sAddr.length()-3).trim();
+    else {
+      int pt = 0;
+      Result res = parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END | FLAG_CHECK_STATUS, flds[0]);
+      if (flds.length > 2) {
+        Result res2 = parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END | FLAG_CHECK_STATUS, flds[1]);
+        if (res2.getStatus() > res.getStatus()) {
+          data.strPlace = flds[0];
+          res = res2;
+          pt = 1;
+        } 
+      }
+      res.getData(data);
+      
+      for (pt++; pt < flds.length; pt++) {
+        data.strSupp = append(data.strSupp, " - ", flds[pt]);
+      }
+    }
     
     return true;
   }
