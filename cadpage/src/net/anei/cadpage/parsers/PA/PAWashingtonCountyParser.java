de74276e027f1f9b88eb427f4f4ba3a37f25b598
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-05-19 16:12:12 +0000

    Added code table to Washington County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PAWashingtonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PAWashingtonCountyParser.java
@@ -57,6 +57,22 @@ Contact: support@active911.com
 [] (911) Location: 307 ROSEWOOD DR CECI Xstreet: RIDGEWOOD DR RIDGEWOOD DR TYPE: FIRE ALARM TIME: 14:59:51 Comments: SHAWN KELLY\n
 [] (911) Loc: 27 HIGHLAND DR CECI Xsts: SR 980 PROFIO RD Type: FIRAL Time: 13:58:32 Loc Com: MARKOVIC RES 724-9262427\n
 [] (911) Xstreet: MORGANZA RD DEAD END TYPE: STRUCTURE FIRE TIME: 22:27:03 Comments:\n
+[] (911) Location: 232 RAILROAD ST CECI Xstreet: JOHNSTON ST DEAD END TYPE: UNCONSCIOUS PT / IS BREATHING TIME: 22:36:33 Comments:\n
+[] (911) Location: 3351 MILLERS RUN RD CECI: @FD 10 CECIL Xstreet: NUGENT ST COLEMAN RD TYPE: MUTUAL AID REQUEST TIME: 16:25:10 Comments:\n
+[] (911) Location: 3351 MILLERS RUN RD CECI: @FD 10 CECIL Xstreet: NUGENT ST COLEMAN RD TYPE: MUTUAL AID REQUEST TIME: 16:25:10 Comments:\n
+[] (911) Location: 1000 CORPORATE DR CECI: @HILTON GARDEN INN Xstreet: KLINGER RD DEAD END TYPE: FIRE ALARM TIME: 06:57:37 Comments:\n
+[] (911) Location: 1029 MAYFAIR DR CECI Xstreet: GREENFIELD DR FIELDSTONE DR TYPE: FIRE ALARM TIME: 09:01:11 Comments:\n
+[] (911) Location: 11 HICKORY GRADE RD CECI Xstreet: CECIL HENDERSON RD ANDREIS LN TYPE: ODOR OF NATURAL GAS TIME: 13:11:16 Comments:\n
+[] (911) Loc: 323 MUSE BISHOP RD CECI: EST Xsts: CIAFFONI RD BURNSIDE RD Type: RDCLS Time: 22:14:11 Loc Com:\n
+[] (911) Loc: 545 MUSE BISHOP RD CECI Xsts: CUMER RD MCCONNELL RD Type: ACCUN Time: 20:41:52 Loc Com:\n
+[] (911) Location: 307 ROSEWOOD DR CECI Xstreet: RIDGEWOOD DR RIDGEWOOD DR TYPE: FIRE ALARM TIME: 14:59:51 Comments: SHAWN KELLY\n
+[] (911) Loc: 27 HIGHLAND DR CECI Xsts: SR 980 PROFIO RD Type: FIRAL Time: 13:58:32 Loc Com: MARKOVIC RES 724-9262427\n
+[] (911) Xsts: WEAVERTOWN LN MILLERS RUN RD Type: ACCIN Time: 12:17:20 Loc Com:\n
+[] (911) Xsts: CUMER RD BERNARDI LN Type: FIRE Time: 11:45:58 Loc Com: ON CUMER...1/2MI FROM MUSE BISHOP\n
+[] (911) Loc: 1000 CORPORATE DR CECI: @HILTON GARDEN INN Xsts: KLINGER RD DEAD END Type: FIRAL Time: 17:58:17 Loc Com:\n
+[] (911) Loc: 27 HIGHLAND DR CECI Xsts: SR 980 PROFIO RD Type: FIRAL Time: 13:58:32 Loc Com: MARKOVIC RES 724-9262427\n
+[] (911) Loc: 1000 CORPORATE DR CECI: @HILTON GARDEN INN Xsts: KLINGER RD DEAD END Type: FIRAL Time: 17:58:17 Loc Com:\n
+[911] Xsts: SLATEMORE DR MILLERS RUN RD Type: ACCIN Time: 17:48:51 Loc Com:\n
 
 ** Missing address - not parsed
 FRM:WashCo911 \nSUBJ:911\nMSG:Xstreet:   TYPE: MUTUAL AID REQUEST TIME: 09:10:04 Comments: BETWEEN SANITARUM RD/WAYNESBURG RD & 79\n
@@ -84,6 +100,15 @@ public class PAWashingtonCountyParser extends FieldProgramParser {
     if (!super.parseMsg(body, data)) return false;
     if (data.strAddress.length() == 0)  return false;
     return true;
+  }
+  
+  // Call description field parser
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      field = convertCodes(field, CALL_CODES);
+      super.parse(field, data);
+    }
   }
   
   // Address field parser
@@ -120,6 +145,7 @@ public class PAWashingtonCountyParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
@@ -193,5 +219,52 @@ public class PAWashingtonCountyParser extends FieldProgramParser {
       "WFIN", "WEST FINLEY TWP",
       "WMID", "WEST MIDDLETOWN",
       "WPIK", "WEST PIKE RUN TWP"
+  });
+  
+  private static final Properties CALL_CODES = buildCodeTable(new String[]{
+                  
+      "ACC",     "ACCIDENT",
+      "ACCEN",   "ACCIDENT W/ENTRAPMENT",
+      "ACCFA",   "ACCIDENT W/FATALITY",
+      "ACCIN",   "ACCIDENT W/INJURIES",
+      "ACCRB",   "ACCIDENT BLKG RDWY",
+      "ACCUN",   "ACCIDENT W/UNK INJURIES",
+      "AIR",     "AIRCRAFT INCIDENT",
+      "ALSPK",   "SPRINKLER ALARM",
+      "BOAT",    "BOAT EMERGENCY",
+      "BOMB",    "EXPLOSIVE DEVICE",
+      "CARMO",   "CO ALARM",
+      "CAVE",    "CAVE IN",
+      "CHO",     "CHOKING",
+      "COL",     "COLLAPSE",
+      "DRWN",    "DROWNING",
+      "ELEEM",   "ELEVATOR EMERGENCY",
+      "EXPLO",   "EXPLOSION",
+      "FIRAL",   "FIRE ALARM",
+      "FIRCH",   "CHEMICAL FIRE",
+      "FIRE",    "FIRE",
+      "FIREN",   "STRUCTURE FIRE W/ ENTRAPMENT",
+      "FIRGR",   "BRUSH FIRE",
+      "FIRST",   "STRUCTURE FIRE",
+      "FIRVE",   "VEHICLE FIRE",
+      "FLOOD",   "FLOODING",
+      "GASOD",   "NATURAL GAS ODOR",
+      "HAZMA",   "HAZMAT INCIDENT",
+      "LZ",      "LANDING ZONE",
+      "MANDN",   "MAN DOWN",
+      "MASCA",   "MCI INCIDENT",
+      "MINAC",   "MINE ACCIDENT",
+      "MUTAF",   "MUTUAL AID REQUEST",
+      "NUCIN",   "NUCLEAR INCIDENT",
+      "OOS",     "OUT OF SERVICE",
+      "PRI",     "PRIORITY DISPATCH",
+      "PUB",     "PUBLIC SERVICE",
+      "RDCLS",   "ROAD CLOSURE",
+      "SMOKE",   "SMOKE INVESTIGATION",
+      "SPILL",   "CONTAINMENT DETAIL",
+      "SR",      "SEARCH DETAIL",
+      "WEATH",   "WEATHER ADVISORY",
+      "WIRES",   "DOWNED/LOW HANGING WIRES",
+
   });
 }
