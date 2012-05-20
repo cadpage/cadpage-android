57c4bfc8e8dec7c4f863b38afdbfaff2052b6527
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-05-20 02:58:10 +0000

    Updated msg doc

--- cadpage/src/net/anei/cadpage/parsers/WI/WIKenoshaCountyCParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WI/WIKenoshaCountyCParser.java
@@ -5,7 +5,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchProphoenixParser;
 
 /*
 
-Pleasant Prarie Dispatch, WI (Kenosha County)
+Kenosha County, WI (variant C) Pleasant Prarie Dispatch
 Contact: David Wilkinson <dwilkinson@plprairiewi.com>
 Sender: CAD@plprairiewi.com
 System: Prophoenix
@@ -15,6 +15,8 @@ Subject:Phoenix Notification\n2012000454 {04/15/2012 17:21:48}\r\nFIRE SNG - Fir
 Subject:Phoenix Notification\n2012000454 {04/15/2012 17:21:48}\r\nFIRE SNG - Fire - Single Engine{1}\r\n905 114th St\r\nUnits:\r\n    5641    5611  \r
 Subject:Phoenix Notification\n2012000619 {05/16/2012 17:47:56}\r\nFIRE ALA - Fire - Alarm{1}\r\n9901 77th St,CMPLX\r\nUnits:\r\n    5642    \r\nCommen\r
 Subject:Phoenix Notification\n2012000618 {05/16/2012 13:31:17}\r\nMED CALL - Medical Call{1}\r\n13000BLK 104th St\r\nUnits:\r\n    5642    \r\nComment\r
+
+Subject:Phoenix Notification\n2012000618 {05/16/2012 13:31:17}\nMED CALL - Medical Call{1}\n13000BLK 104th St\nUnits:\n    5642    \nComment
 
 */
 
