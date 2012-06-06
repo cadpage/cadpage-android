d990edfbaf906149b65638dac520c3c8e2295194
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-06-06 21:07:27 +0000

    Replaced Las Vegas,NV location parser

--- cadpage/src/net/anei/cadpage/parsers/NV/NVLasVegasParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NV/NVLasVegasParser.java
@@ -8,26 +8,29 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 /*
-Las Vegas, NV
-Contact: Trevor Nelson <robs_trevor@yahoo.com>
+Las Vegas County, NV
+Contact: Andy Stone <akmedic33@gmail.com>
 Sender: sms@pageway.net
-SMS / 725 12011164 Pri:4 Prob:Sch Grid:2625-1/11 3186 S MARYLAND PKWY Bld:ER Apt:25 Zip:89169
-SMS / 725 12011200 Pri:4 Prob:Imm Grid:2429-1/11 61 N NELLIS BLVD Bld: Apt: Zip:89110
-SMS / 725 12011221 Pri:5 Prob:Imm Grid:2826-1/11 2170 E HARMON AV Bld: Apt:324A Zip:89119
-SMS / 725 12011269 Pri:5 Prob:Imm Grid:1823-1/11 2202 W CRAIG RD Bld: Apt: Zip:89032
 
-*** Should not parse ***
-SMS / 725 12011269 Disp:16:59:20 Enroute:16:59:27 On Scene:17:26:43 To Hosp:17:40:01 At Hosp:17:58:28
+(SMS) I: 4273738 U: 3901, P: 3, G: 3423, PH: 17 L: 2711 W WINDMILL LN# , B: , AL: PC:, 1A N:1A], 17:07:17 [Geographic Area: 2711 W WINDMILL LN]
+(SMS) I: 4274400 U: 3901, P: 2, G: 3217, PH: 61 L: S DURANGO DR / W MAULE AVE# , B: , AL: PC:, 32B N:32B], 19:02:59 [Geographic Area: S DURANGO
+(SMS) I: 4273913 U: 3901, P: 1, G: 2821, PH: 13 L: 4285 DUNEVILLE ST-CC# 236 , B: , AL: PC:, 6D N:6D], Location from Fire: 4285 DUNEVILLE ST, 00
+[SMS]  I: 4273807 U: 3901, P: 1, G: 2814, PH: 55 L: 4541 BERSAGLIO ST-CC# , B: , AL: PC:, 5C N:5C], Location from Fire: 4541 BERSAGLIO ST, 19:51:
+(SMS) I: 4274438 U: 3901, P: 1, G: 2815, PH: 17 L: W FLAMINGO RD / N I 215# , B: , AL: PC:, 12D N:12D], 20:33:59 [Geographic Area: W FLAMINGO RD
+(SMS) I: 4274468 U: 3901, P: 3, G: 3425, PH: 25 L: 8105 BURNT SIENNA ST-CC# , B: , AL: PC:, 17A N:17A], Location from Fire: 8105 BURNT SIENNA ST
+(SMS) I: 4274545 U: 3901, P: 7, G: 3216, PH: 11 L: 9300 W SUNSET RD-CC# ER 1 , B: SOUTHERN HILLS HOSPI, AL: 9MOM / IV MON O2 / H1P ER PC:, 430 N
+
+(SMS) RUN#:4273913 /RCV:00:32:57/ENR:00:34:10/ONS:00:40:00/TRS:01:23:33/DES:01:28:03/MIL:
+(SMS) RN#4274400 /RCV:19:03:42/ENR:19:04:20/ONS:19:08:38/AVA:19:13:06
+(SMS) RUN#:A225728 /RCV:16:31:14/ENR:16:31:15/ONS:16:31:15/TRS:/DES:/MIL:
 
 */
 
 public class NVLasVegasParser extends FieldProgramParser {
   
-  private static final Pattern MARKER = Pattern.compile("^SMS / 725 (\\d{8}) ");
-  
   public NVLasVegasParser() {
     super("LAS VEGAS", "NV",
-           "Pri:PRI! Prob:CALL! Grid:ADDR! Bld:APT! Apt:APT! Zip:CITY!");
+           "I:ID! U:UNIT! P:PRI! G:MAP! PH:INFO! L:ADDR! B:PLACE! AL:INFO! PC:INFO! INFO N:SKIP TIME+");
   }
   
   @Override
@@ -36,39 +39,45 @@ public class NVLasVegasParser extends FieldProgramParser {
   }
   
   @Override
-  public boolean parseMsg(String body, Data data) {
-    Matcher match = MARKER.matcher(body);
-    if (!match.find()) return false;
-    data.strCallId = match.group(1);
-    body = body.substring(match.end()).trim();
-    if (body.startsWith("Disp:"))  return false;
-    return super.parseMsg(body, data);
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("SMS")) return false;
+    body = body.replace(" U:", ", U:").replace(" L:", ", L:").replace(" PC:", ", PC:").replace(" N:", ", N:");
+    return parseFields(body.split(","), 9, data);
   }
   
-  @Override
-  public String getProgram() {
-    return "ID " + super.getProgram();
-  }
-  
-  private static final Pattern ADDRESS_PTN = Pattern.compile("(\\d{4}-\\d/\\d{2}) +(.*)");
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = ADDRESS_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strMap = match.group(1);
-      super.parse(match.group(2), data);
+      Parser p = new Parser(field);
+      data.strApt = p.getLastOptional('#');
+      field = p.get();
+      if (field.endsWith("-CC")) {
+        data.strCity = "CLARK COUNTY";
+        field = field.substring(0,field.length()-3).trim();
+      }
+      super.parse(field, data);
     }
     
     @Override
     public String getFieldNames() {
-      return "MAP " + super.getFieldNames();
+      return super.getFieldNames() + " CITY APT";
+    }
+  }
+  
+  private static final Pattern TIME_PTN = Pattern.compile("^(\\d\\d:\\d\\d:\\d\\d)\\b");
+  private class MyTimeField extends TimeField {
+    @Override
+    public void parse(String field, Data data) {
+      if (data.strTime.length() > 0) return;
+      Matcher match = TIME_PTN.matcher(field);
+      if (match.find()) data.strTime = match.group(1);
     }
   }
   
   @Override
   public Field getField(String name) {
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("TIME")) return new MyTimeField();
     return super.getField(name);
   }
 }
