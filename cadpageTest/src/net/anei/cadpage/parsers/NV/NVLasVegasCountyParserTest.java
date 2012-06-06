5f655310bf78cc8ac069f910578ad116f76ea39e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-06-06 21:07:41 +0000

    Replaced Las Vegas,NV location parser

--- cadpageTest/src/net/anei/cadpage/parsers/NV/NVLasVegasCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/NV/NVLasVegasCountyParserTest.java
@@ -12,51 +12,80 @@ public class NVLasVegasCountyParserTest extends BaseParserTest {
   }
   
   @Test
-  public void testBadMsg() {
-    doBadTest("SMS / 725 12011269 Disp:16:59:20 Enroute:16:59:27 On Scene:17:26:43 To Hosp:17:40:01 At Hosp:17:58:28");
-  }
-  
-  @Test
   public void testParser() {
 
     doTest("T1",
-        "SMS / 725 12011164 Pri:4 Prob:Sch Grid:2625-1/11 3186 S MARYLAND PKWY Bld:ER Apt:25 Zip:89169",
-        "ID:12011164",
-        "PRI:4",
-        "CALL:Sch",
-        "MAP:2625-1/11",
-        "ADDR:3186 S MARYLAND PKWY",
-        "APT:ER-25",
-        "CITY:89169");
+        "(SMS) I: 4273738 U: 3901, P: 3, G: 3423, PH: 17 L: 2711 W WINDMILL LN# , B: , AL: PC:, 1A N:1A], 17:07:17 [Geographic Area: 2711 W WINDMILL LN]",
+        "ID:4273738",
+        "UNIT:3901",
+        "PRI:3",
+        "MAP:3423",
+        "INFO:17 / 1A",
+        "ADDR:2711 W WINDMILL LN",
+        "TIME:17:07:17");
 
     doTest("T2",
-        "SMS / 725 12011200 Pri:4 Prob:Imm Grid:2429-1/11 61 N NELLIS BLVD Bld: Apt: Zip:89110",
-        "ID:12011200",
-        "PRI:4",
-        "CALL:Imm",
-        "MAP:2429-1/11",
-        "ADDR:61 N NELLIS BLVD",
-        "CITY:89110");
+        "(SMS) I: 4274400 U: 3901, P: 2, G: 3217, PH: 61 L: S DURANGO DR / W MAULE AVE# , B: , AL: PC:, 32B N:32B], 19:02:59 [Geographic Area: S DURANGO",
+        "ID:4274400",
+        "UNIT:3901",
+        "PRI:2",
+        "MAP:3217",
+        "INFO:61 / 32B",
+        "ADDR:S DURANGO DR & W MAULE AVE",
+        "TIME:19:02:59");
 
     doTest("T3",
-        "SMS / 725 12011221 Pri:5 Prob:Imm Grid:2826-1/11 2170 E HARMON AV Bld: Apt:324A Zip:89119",
-        "ID:12011221",
-        "PRI:5",
-        "CALL:Imm",
-        "MAP:2826-1/11",
-        "ADDR:2170 E HARMON AV",
-        "MADDR:2170 E HARMON AVE",
-        "APT:324A",
-        "CITY:89119");
+        "(SMS) I: 4273913 U: 3901, P: 1, G: 2821, PH: 13 L: 4285 DUNEVILLE ST-CC# 236 , B: , AL: PC:, 6D N:6D], Location from Fire: 4285 DUNEVILLE ST, 00",
+        "ID:4273913",
+        "UNIT:3901",
+        "PRI:1",
+        "MAP:2821",
+        "INFO:13 / 6D",
+        "APT:236",
+        "ADDR:4285 DUNEVILLE ST",
+        "CITY:CLARK COUNTY");
 
     doTest("T4",
-        "SMS / 725 12011269 Pri:5 Prob:Imm Grid:1823-1/11 2202 W CRAIG RD Bld: Apt: Zip:89032",
-        "ID:12011269",
-        "PRI:5",
-        "CALL:Imm",
-        "MAP:1823-1/11",
-        "ADDR:2202 W CRAIG RD",
-        "CITY:89032");
+        "[SMS]  I: 4273807 U: 3901, P: 1, G: 2814, PH: 55 L: 4541 BERSAGLIO ST-CC# , B: , AL: PC:, 5C N:5C], Location from Fire: 4541 BERSAGLIO ST, 19:51:",
+        "ID:4273807",
+        "UNIT:3901",
+        "PRI:1",
+        "MAP:2814",
+        "INFO:55 / 5C",
+        "ADDR:4541 BERSAGLIO ST",
+        "CITY:CLARK COUNTY");
+
+    doTest("T5",
+        "(SMS) I: 4274438 U: 3901, P: 1, G: 2815, PH: 17 L: W FLAMINGO RD / N I 215# , B: , AL: PC:, 12D N:12D], 20:33:59 [Geographic Area: W FLAMINGO RD",
+        "ID:4274438",
+        "UNIT:3901",
+        "PRI:1",
+        "MAP:2815",
+        "INFO:17 / 12D",
+        "ADDR:W FLAMINGO RD & N I 215", // Not mapping
+        "TIME:20:33:59");
+
+    doTest("T6",
+        "(SMS) I: 4274468 U: 3901, P: 3, G: 3425, PH: 25 L: 8105 BURNT SIENNA ST-CC# , B: , AL: PC:, 17A N:17A], Location from Fire: 8105 BURNT SIENNA ST",
+        "ID:4274468",
+        "UNIT:3901",
+        "PRI:3",
+        "MAP:3425",
+        "INFO:25 / 17A",
+        "ADDR:8105 BURNT SIENNA ST",
+        "CITY:CLARK COUNTY");
+
+    doTest("T7",
+        "(SMS) I: 4274545 U: 3901, P: 7, G: 3216, PH: 11 L: 9300 W SUNSET RD-CC# ER 1 , B: SOUTHERN HILLS HOSPI, AL: 9MOM / IV MON O2 / H1P ER PC:, 430 N",
+        "ID:4274545",
+        "UNIT:3901",
+        "PRI:7",
+        "MAP:3216",
+        "INFO:11 / 9MOM / IV MON O2 / H1P ER / 430 N",
+        "APT:ER 1",
+        "ADDR:9300 W SUNSET RD",
+        "CITY:CLARK COUNTY",
+        "PLACE:SOUTHERN HILLS HOSPI");
   }
   
 
