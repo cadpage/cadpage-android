21b9af36a3dd991b50c7f27db48c261bc95991c7
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-06-13 01:06:34 +0000

    Fixed parsing problem with Sussex County, NJ (B)

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJSussexCountyBParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJSussexCountyBParser.java
@@ -14,6 +14,15 @@ Contact: Todd Rudloff <btfd43capt2@gmail.com>
 [Message from HipLink]  BTFD\nMV Acc Injury\nPAGED\nLACKAWANNA DR\nBTF02\n43EMS\nMOTOR VEHICLE ACCIDENT WITH ENTRAPPMENT\nPATIENT REMOVED
 [Message from HipLink]  BTFD\nFire Other\nPAGED\n17 STONEHEDGE LN\nBTF03\nBTFIRE
 
+Contact: David Suriano <dasuriano1@gmail.com>
+Sender: paging@sussexcountysheriff.com
+NWES\r\nMedical Emerg\r\nPAGED\r\n31 PATERSON AVE; APT 28\r\nNWES\r\nNWESPG\r\n9-1-1 CALLER REQ EMS FOR THE 2YOA HAVING A SEIZURE\r
+NWES\r\nFire Alarm\r\nPAGED\r\n4 ACADEMY ST\r\nNWES\r\nNWESPG\r\nACTIVATED FIRE ALARM. MULTIPLE ZONE ACTIVATIONS
+NWES\r\nMedical Emerg\r\nPAGED\r\n39 TRINITY ST\r\nNWES\r\nNWESPG
+NWFD\r\nFire Alarm\r\nPAGED\r\n3 HIGH ST\r\nNWFD\r\nNWESPG\r\nSCSO REQUEST FD RESPOND FOR THE ACTIVATED FIRE ALARM.
+NWES\r\nMedical Emerg\r\nPAGED\r\n7 E CLINTON ST\r\nNWES\r\nNWESPG
+NWES\r\nMedical Emerg\r\nPAGED\r\n160 SPRING STREET\r\nNWES\r\nNWESPG
+
  */
 
 
@@ -28,10 +37,7 @@ public class NJSussexCountyBParser extends FieldProgramParser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     
-    if (!subject.equals("Message from HipLink")) return false;
-    String[] flds = body.split("\n");
-    if (flds.length < 6) return false;
-    return parseFields(flds, data);
+    return parseFields(body.split("\n"), 6, data);
   }
   
   private class MyUnitField extends UnitField {
