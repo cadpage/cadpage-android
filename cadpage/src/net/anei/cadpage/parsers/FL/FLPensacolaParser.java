8e6cc41f22c5c06e040177cd0edaca53dff99a3a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-06-29 13:48:44 +0000

    Replaced Dade County, GA location parser Updated msg doc Updated user list

--- cadpage/src/net/anei/cadpage/parsers/FL/FLPensacolaParser.java
+++ cadpage/src/net/anei/cadpage/parsers/FL/FLPensacolaParser.java
@@ -2,16 +2,18 @@ package net.anei.cadpage.parsers.FL;
 
 
 
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 
 /*
-Lake County, FL
-Contact: 3522354040@messaging.sprintpcs.com
-Sender: varies
-
+Pensacola, FL
+Contact: CodeMessaging
 
 Call_Number: PFD12CAD003118 | Units: L11, | Complaint: PUBLIC ASSISTANCE | Location: X2[SCOTT ST E] | Address: 2400 N 7TH AV  | Xst 1:  YONGE ST E | Xst 2:   | City: PENSACOLA | State: FL | CAddress1 : 2400 N 7TH AV | CAddress2: X2[SCOTT ST E] | Loc_Display: 2400 N 7TH AV [X2[SCOTT ST E]]  x[YONGE ST E]    [PENSACOLA] | Time_Dispatched: 2012-06-25 09:33:23 | Narrative:  | This_Unit: L11
 Call_Number: PFD12CAD003114 | Units: ENG2, | Complaint: RESCUE | Location: X2[9TH AV N] | Address: 813  WOODLAND DR  | Xst 1:  ACACIA DR | Xst 2:   | City: PENSACOLA | State: FL | CAddress1 : 813 WOODLAND DR | CAddress2: X2[9TH AV N] | Loc_Display: 813 WOODLAND DR [X2[9TH AV N]]  x[ACACIA DR]    [PENSACOLA] | Time_Dispatched: 2012-06-25 00:58:02 | Narrative: CHEST PAINS | This_Unit: ENG2
@@ -20,8 +22,6 @@ Call_Number: PFD12CAD003117 | Units: ENG1, | Complaint: RESCUE | Location: X2[LA
 Call_Number: PFD12CAD003115 | Units: ENG1, | Complaint: RESCUE | Location: X2[C ST N] | Address: 815 W LA RUA ST  | Xst 1:  B ST N | Xst 2:   | City: PENSACOLA | State: FL | CAddress1 : 815 W LA RUA ST | CAddress2: X2[C ST N] | Loc_Display: 815 W LA RUA ST [X2[C ST N]]  x[B ST N]    [PENSACOLA] | Time_Dispatched: 2012-06-25 03:32:10 | Narrative:  | This_Unit: ENG1
 Call_Number: PFD12CAD003111 | Units: ENG6, | Complaint: RESCUE | Location: THE FLATS AT 9TH AVENUE  308-5345 X2[BEAU TERRA LN] | Address: 7601 N 9TH AV 107 | Xst 1:  I10 | Xst 2:   | City: PENSACOLA | State: FL | CAddress1 : 7601 N 9TH AV | CAddress2: THE FLATS AT 9TH AVENUE  308-5345 X2[BEAU TERRA LN] | Loc_Display: 7601 N 9TH AV [THE FLATS AT 9TH AVENUE  308-5345 X2[BEAU TERRA LN]]  x[I10]    [ | Time_Dispatched: 2012-06-24 23:18:28 | Narrative:  | This_Unit: ENG6
 
-
-Contact: "David Maxey" <dmaxey09@vt.edu>
 */
 
 public class FLPensacolaParser extends FieldProgramParser {
@@ -37,22 +37,46 @@ public class FLPensacolaParser extends FieldProgramParser {
     return "@c-msg.net";
   }
 
+  @Override
+  public int getMapFlags() {
+    return MAP_FLG_SUPPR_LA;
+  }
 
   @Override 
   public boolean parseMsg(String subject, String body, Data data) {
     body = body.replace("_", " "); 
-    body = body.replaceAll(",", "");
-    body = body.replaceAll("X2", "");
     if (!parseFields(body.split("\\|"), 14, data)) return false;  
-    if (data.strDate.equals("FL")) data.strDate = ""; 
+    if (data.strState.equals("FL")) data.strState = ""; 
     return true;
-
+  }
+  
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.endsWith(",")) field = field.substring(0,field.length()-1).trim();
+      super.parse(field, data);
+    }
+  }
+  
+  private static final Pattern PLACE_X_PTN = Pattern.compile("\\bX2\\[([^\\[\\]]+)\\]$");
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = PLACE_X_PTN.matcher(field);
+      if (match.find()) {
+        data.strCross = match.group(1).trim();
+        field = field.substring(0,match.start()).trim();
+      }
+      super.parse(field, data);
+    }
   }
 
 
   @Override
   public Field getField(String name) {
-    if (name.equals("DATETIME")) return new DateTimeField("\\d{4}-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("DATETIME")) return new DateTimeField(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
     return super.getField(name);
   }
 }
--- cadpage/src/net/anei/cadpage/parsers/GA/GADadeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/GA/GADadeCountyParser.java
@@ -1,51 +1,64 @@
 package net.anei.cadpage.parsers.GA;
 
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchBParser;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
 /*
-Dade County, GA
-Contact: "unit20@msn.com" <unit20@msn.com>
-DADE COUNTY 911:M10 >CHEST PAIN 367 GRAY RD XS: 224 FAWN DAWN DR. NEW SALEM KATY DILLS Map:10-D1,E1 Grids:0,0 Cad: 2011-0000000796
-DADE COUNTY 911:M26 >SICK PERSON 391 BIBLE GULF RD XS: 869 TATUM GULF RD OFF OLD STATE RD HEAD RIVER KEATON, GLENN Map:12-D2 Grids:0,0 Cad: 2011-0000000651
-DADE COUNTY 911:M5 >FALL - ANY 2862 YANKEE RD XS: HWY 157 S HEAD RIVER KEENER, CAROL Map:11,12 Grids:0,0 Cad: 2011-0000001152
-DADE COUNTY 911:50PI >M.V.A. - POSSIBLE INJURIES 13500 HWY 136 E XS: N & S MOORE RD NEW SALEM ROBERTS, CHIP Map:6,15 Grids:0,0 Cad: 2011-0000000992
-DADE COUNTY 911:M1 >ABDOMINAL PAIN/PROBLEM 2321 OLD STATE RD XS: 16548 HWY 157 S HEAD RIVER CHAD LANIER Map:11,12 Grids:0,0 Cad: 2011-0000001270
-DADE COUNTY 911:78 >ASSISTANCE/ MUTUAL AID 84 RUSHING WATER TRL CHRISTY / WALKER Cad: 2011-0000001334
-
-Contact: Rodney <unit20@msn.com>
-Sender: 4238021533
-DADE COUNTY 911:78 >ASSISTANCE/ MUTUAL AID 8376 HWY 157 S HEAD RIVER WALKER CO Cad: 2011-0000015531
+Dale County, GA (replacement)
+Contact: toptech82 <toptech82@gmail.com>
+Sender: E911@dadega.com
+ 1 of 2\nFRM:E911@dadega.com\nSUBJ:!\nMSG:Brush, Grass, Field, Woods 220 HOOKER CEMETERY RD DEAD END / HWY 299 WILDWOOD     BRUSH FIRE ON BOTH SIDES\n(Con't) 2 of 2\nOF THE RAILRIAD TRACKS  06/26/12 20:35(End)
+ 1 of 2\nFRM:E911@dadega.com\nSUBJ:!\nMSG:Convulsions/Seizures 93 MARCH LN DEAD END / HARRIS RD SLYGO     12 YOM, HAVING A SEIZURE, HISTORY OF\n(Con't) 2 of 2\nSEIZURE, HAS NOT COME  06/28/12 00:47(End)
+FRM:E911@dadega.com\nSUBJ:!\nMSG:Fall 650 HWY 299 WILDWOOD    Pilot Travel Center #254 E911 Info - Class of Service: BUSN  06/29/12 08:32
 
 */
 
-public class GADadeCountyParser extends DispatchBParser {
-  
-  private static final String[] CITY_LIST = new String[]{
-    "NEW SALEM", "HEAD RIVER"
-  };
+public class GADadeCountyParser extends SmartAddressParser {
 
-  private static final String DEF_STATE = "GA";
-  private static final String DEF_CITY = "DADE COUNTY";
- 
+  private static final Pattern MASTER = Pattern.compile("(.*?) {3,}(.*?) +(\\d\\d/\\d\\d/\\d\\d) +(\\d\\d:\\d\\d)");
+  
   public GADadeCountyParser() {
-    super(CITY_LIST, DEF_CITY, DEF_STATE);
+    super(CITY_LIST, "DADE COUNTY", "GA");
   }
   
   @Override
-  protected boolean isPageMsg(String body) {
-    return body.startsWith("DADE COUNTY 911:");
+  public String getFilter() {
+    return "E911@dadega.com";
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
+    Matcher match = MASTER.matcher(body);
+    if (!match.matches()) return false;
     
-    // The field we parse as a city isn't recognized by Google maps.  We'll
-    // switch it to the Place field where at least it will show up on the screen
-    // but won't interfere with mapping
-    if (data.strPlace.length() == 0) data.strPlace = data.strCity;
-    data.strCity = "";
+    parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_ANCHOR_END | FLAG_PAD_FIELD, match.group(1), data);
+    data.strCross = getPadField();
+    String city = PLACE_CITY_XREF.getProperty(data.strCity);
+    if (city != null) {
+      data.strPlace = data.strCity;
+      data.strCity = city;
+    }
+    
+    data.strSupp = match.group(2);
+    data.strDate = match.group(3);
+    data.strTime = match.group(4);
     return true;
   }
+  
+  private static final String[] CITY_LIST = new String[]{
+    "NEW ENGLAND",
+    "RISING FAWN",
+    "TRENTON",
+    "WILDWOOD",
+    
+    "SLYGO"
+  };
+  
+  private static final Properties PLACE_CITY_XREF = buildCodeTable(new String[]{
+      "SLYGO",       "TRENTON"
+  }); 
 }
