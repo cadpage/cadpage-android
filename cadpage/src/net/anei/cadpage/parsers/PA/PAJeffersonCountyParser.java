9ab067281ea6a9f1e8b6d05c9e01ecfbfece288f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-07-22 17:29:31 +0000

    More parsing issues with Jefferson County,PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PAJeffersonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PAJeffersonCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.SmartAddressParser;
 
@@ -20,10 +23,14 @@ prvs=0514f9c90b=dispatch@ <dispatch@jeffersoncountypa.com> jeffersoncountypa.com
 prvs=0512e03127=dispatch@ <dispatch@jeffersoncountypa.com> jeffersoncountypa.com <dispatch@jeffersoncountypa.com> (Incident) Structure Fire 106 FOUNDRY ST CRISSMAN ALY / W MAHONING ST Punxsutawney Narrative: 40-A Snyder Hill
 prvs=05312311d9=dispatch@jeffersoncountypa.com (Incident) Tree Down GASKILL AVE ALTMAN AVE Punxsutawney 20-A Snyder Hill
 prvs=054676518f=dispatch@ <dispatch@jeffersoncountypa.com> jeffersoncountypa.com <dispatch@jeffersoncountypa.com> (Incident) Misc. Fire S20 MED50  313 WOODLAND AVE Punxsutawney BLAIR ST / CAMBRIA ST 20-A Snyder Hill
+prvs=054477bfa6=dispatch@jeffersoncountypa.com (Incident) Structure Fire E30 E20 E40 TK40 SQ30 R20 R40 T20 T40 MED50  143 WOODDALE DR Bell ALBION RD / ACORN LN 30-B Snyder Hill
+prvs=054676518f=dispatch@jeffersoncountypa.com (Incident) Structure Fire E30 E20 E40 TK40 T20 T40 MED50  118 CLARK ST Punxsutawney HAMPTON AVE, RIDGE AVE / N PENN ST 30-A Snyder Hi
 
 */
 
 public class PAJeffersonCountyParser extends SmartAddressParser {
+  
+  private static final Pattern BOX_CH_PTN = Pattern.compile("(?: +BOX|-)? +(\\d{1,2}-[A-Z])(?: +([A-Za-z]+(?: +[A-Za-z]+)?))?$");
   
   public PAJeffersonCountyParser() {
     super(CITY_LIST, "JEFFERSON COUNTY", "PA");
@@ -39,6 +46,13 @@ public class PAJeffersonCountyParser extends SmartAddressParser {
     
     if (!subject.equals("Incident")) return false;
     
+    Matcher match = BOX_CH_PTN.matcher(body);
+    if (match.find()) {
+      data.strBox = match.group(1);
+      data.strChannel = getOptGroup(match.group(2));
+      body = body.substring(0,match.start()).trim();
+    }
+    
     int pt = body.indexOf("Narrative:");
     if (pt >= 0) {
       data.strSupp = body.substring(pt+10).trim();
@@ -49,7 +63,7 @@ public class PAJeffersonCountyParser extends SmartAddressParser {
     body = body.replace("Snyder Hill", "Snyder-Hill");
     parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_PAD_FIELD | FLAG_CROSS_FOLLOWS, body, data);
     data.strCross = getPadField();
-    String extra = getLeft().replace("Snyder-Hill", "Snyder Hill");
+    String extra = getLeft();
     
     if (data.strCross.length() == 0 && extra.contains(" / ")) {
       Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS, extra);
