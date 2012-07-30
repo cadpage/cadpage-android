f771d994754e2c56d7492c904255633c434b7a6b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-07-30 16:25:21 +0000

    Added new city to Suffolk County, NY (Firetracker)

--- cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyFiretrackerParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyFiretrackerParser.java
@@ -55,6 +55,7 @@ public class NYSuffolkCountyFiretrackerParser extends FieldProgramParser {
   
   private static final String[] CITY_LIST = new String[]{
     "MASTIC BEACH",
-    "SHIRLEY"
+    "SHIRLEY",
+    "SMITH POINT"
   };
 }
