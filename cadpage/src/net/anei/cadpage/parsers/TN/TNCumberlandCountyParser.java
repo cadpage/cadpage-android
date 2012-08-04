2e82495a46a8ebaf852fb842b19cf4d211204d1e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-08-04 02:47:04 +0000

    Fixed parsing problem with Cumberland County, TN

--- cadpage/src/net/anei/cadpage/parsers/TN/TNCumberlandCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TN/TNCumberlandCountyParser.java
@@ -1,55 +1,34 @@
 package net.anei.cadpage.parsers.TN;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
 
 /* 
 Cumberland County, TN
 Contact: Jeremy <fyrboy55@gmail.com>
-Sender: 911-CENTER@cumberlandtn911.org
+Sender: dispatch@911email.net
 
-911-CENTER:1046 INJURY ACCIDENT LANTANA AT STORIE 7656250985 COUNTY
-911-CENTER:1072A FIRE ALARM 912 GERONIMO LN 9317470058 COUNTY WIDE
-911-CENTER:1072B FIRE, BRUSH/WOODS 18 JASPER DR 931 COUNTY WIDE
-911-CENTER:1072S FIRE, STRUCTURE 171 RED FOX DR 931 COUNTY WIDE
-911-CENTER:1046 INJURY ACCIDENT 3173 GENESIS RD 9312108439 COUNTY WIDE
-911-CENTER:1072S FIRE, STRUCTURE 4503 CRESTON RD 9317071052 COUNTY WIDE
-911-CENTER:1072 FIRE 110 NORTHRIDGE DR 931 COUNTY WIDE
-911-CENTER:1072S FIRE, STRUCTURE 261 FLORIDA AVE 9317885007 COUNTY WIDE
-911-CENTER:1046 INJURY ACCIDENT 217 SNEAD DR 9315290123 COUNTY WIDE
+(E911) FIRE ALARM/10-72A\r\n115 HUNTINGTON DR\r\nKEY PAD ALARM
+(E911) FIRE ALARM/10-72A\r\nSTONEHENGE DR\r\nGENERAL/COMMERCIAL BUILDING SYSTEMS/800-877-3624
+(E911) MVA-INJ/10-46\r\nPEAVINE RD BY ACE HARDWARE\r\nJUV COMPLAINING OF CHEST PAINS , 2 VEHS
 
  */
 
-public class TNCumberlandCountyParser extends SmartAddressParser {
-  
-  private static final Pattern MASTER = Pattern.compile("911-CENTER:([A-Z0-9]+) +(.+?) +(\\d{3}(?:\\d{7})?)([ A-Z]+)*");
+public class TNCumberlandCountyParser extends FieldProgramParser {
   
   public TNCumberlandCountyParser() {
-    super("CUMBERLAND COUNTY", "TN");
+    super("CUMBERLAND COUNTY", "TN",
+           "CALL ADDR INFO");
   }
   
   @Override
   public String getFilter() {
-    return "911-CENTER@cumberlandtn911.org";
+    return "dispatch@911email.net";
   }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    
-    data.strCode = match.group(1);
-    String sAddr = match.group(2).trim();
-    parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_ANCHOR_END, sAddr, data);
-    data.strPhone = match.group(3);
-    String city = match.group(4).trim();
-    if (city.equals("COUNTY") || city.equals("COUNTY WIDE")) city = "";
-    data.strCity = city;
-    
-    return true;
+    if (!subject.equals("E911")) return false;
+    return parseFields(body.split("\n"), 3, data);
   }
 }
