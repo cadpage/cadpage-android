4b14c2b045fb1d7d6aca8568568460df0f544942
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-08-26 21:45:19 +0000

    FIxed parsing problem with Sussex County, NJ (A)

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJSussexCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJSussexCountyAParser.java
@@ -44,6 +44,29 @@ Sender: @nwbcd.org
 (I-2012-000193) FFIRA @ 549 CORBETT PLACE  , ORADELL BORO - 
 (I-2012-000194) FFIRA @ 549 CORBETT PLACE  , ORADELL BORO - GENERAL
 
+Contact: Active911
+Agency name: Fort Lee Fire Department Location: Fort Lee, NJ 
+Sender: @fortleepolice.org
+
+(I-2012-000814) 1045F @ 1350 15TH STREET  , FORT LEE - garage, north and south, waterflow
+(I-2012-000815) 1045F @ 250 HOYM STREET  , FORT LEE - 1ST FL HOYM SIDE
+(I-2012-000816) 1045F @ 1616 PALISADE AVENUE  , FORT LEE - ZONE 0 AND 2ND FL MENS RM SMOKE
+(I-2012-000817) 1045F @ 250 HOYM STREET  , FORT LEE - 2ND FL HOYM ST WING
+(I-2012-000818) 1045F @ 1 HORIZON ROAD  , FORT LEE - fan room
+(I-2012-000819) 1045F @ 1 EXECUTIVE DRIVE  , FORT LEE - LL Level
+(I-2012-000820) 1045F @ 1081 INWOOD TERRACE  , FORT LEE - Cheung - smoke detector
+(I-2012-000821) 1045F @ 1115 BUCKINGHAM ROAD  , FORT LEE - OFFICE SMOKE DETECTOR
+(I-2012-000822) 1045F @ 3021 EDWIN AVENUE  , FORT LEE - 5th floor elevator activation
+(I-2012-000823) 1035 @ 3081 EDWIN AVENUE  , FORT LEE - Reported Stuck Occupied Elevator between 3rd. & 4th. flrs.
+(I-2012-000824) 1037 @  ROUTE 95 NORTH  , FORT LEE - Leonia Boro, rt 95 north express 120.7 -...\n\nActive Units: FL-R
+(I-2012-000825) 1045F @ 1 EXECUTIVE DRIVE  , FORT LEE - penthouse activation
+(I-2012-000826) 1045F @ 1600 PARKER AVENUE  , FORT LEE - General alarm
+(I-2012-000827) 1032 @ 2170 FLETCHER AVENUE  , FORT LEE - report of vehicle fire in taco bell parking lot, 551-574-0053
+(I-2012-000828) 1033 @ 1 PIP  , FORT LEE - brush fire  caller is by cannons
+(I-2012-000829) 1045F @ 2053 ELLERY AVENUE  , FORT LEE - First fl smoke Canerparo residence   apt. building   alarm co to noty   owner cell  854-304-3491
+(I-2012-000830) 1035 @ 1000 OVERLOOK DRIVE  , FORT LEE - check for blocked fire exit, on south side of 2nd floor, previously reported to management, 908-249-2449, mr. lacher.
+(I-2012-000831) 1045F @ 2 HORIZON ROAD  , FORT LEE - laundry room 8 and basement north
+
  */
 
 
@@ -51,7 +74,7 @@ public class NJSussexCountyAParser extends SmartAddressParser {
   
   private static final Pattern SUBJECT_PTN = Pattern.compile("I-[A-Z]?\\d{4}-\\d{6}");
   private static final Pattern MASTER_PTN = 
-    Pattern.compile("([-A-Z ]+) @ ([^,]+) +, ([^-]*) -(?: (.*))?"); 
+    Pattern.compile("([-A-Z0-9 ]+) @ ([^,]+) +, ([^-]*) -(?: (.*))?"); 
   
   public NJSussexCountyAParser() {
     super("SUSSEX COUNTY", "NJ");
