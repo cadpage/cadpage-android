7df6093aa3058a29e711277b5b65af559a8b006a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-08-30 04:28:27 +0000

    Added Charles County, MD (B) location

--- cadpageTest/src/net/anei/cadpage/parsers/MO/MORayCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/MO/MORayCountyParserTest.java
@@ -12,270 +12,81 @@ public class MORayCountyParserTest extends BaseParserTest {
   }
   
   @Test
-  public void testBadStuff() {
-    doBadTest("107 N RAYMORE ST  WOOD HEIGHTS WHFD RCAD RCSO 800 M42 WHR1 678 Clear: 22:48:39 Available: 23:34:02");
-    doBadTest("15595 BLACKBERRY TRAIL  WHFD 800 LFD 802 LT1 LP1 WHP1 Dispatch: 5/24/2011 23:15:33 Enroute: 23:15:35 OnScene: 23:23:58");
-    doBadTest("15141 W COUNTY LINE RD  RAY COUNTY WHFD RCAD 671A 675A 800 M44 WHR1 Clear: 22:20:51 Available: 23:05:20");
-
-  }
-  
-  @Test
-  public void testParser1() {
+  public void testLeeODell() {
 
     doTest("T1",
-        "RCAD WHFD FALLS 514 ARAPAHOE DR  HOMESTEAD VILLAGE CrossStreets: CHEROKEE DR 0.05 mi W HIGHWAY Y 0.16 mi E Description: BETTY JAMES 85 YO FEMALE\nFAL",
-        "SRC:RCAD WHFD",
-        "CALL:FALLS",
-        "ADDR:514 ARAPAHOE DR",
-        "CITY:HOMESTEAD VILLAGE",
-        "INFO:BETTY JAMES 85 YO FEMALE FAL",
-        "X:CHEROKEE DR 0.05 mi W HIGHWAY Y 0.16 mi E");
+        "HIGHWAY Y RAY COUNTY CrossStreets: 10-50 TRAFFIC/TRANSPORT INCIDENT (CRASH)",
+        "ADDR:HIGHWAY Y",
+        "CALL:10-50 TRAFFIC/TRANSPORT INCIDENT (CRASH)");
 
     doTest("T2",
-        "LFD EMS 31004 NOTTINGHAM LN   Description:  CrossStreets: SILVEY RD 6443.06 mi NW SILVEY RD 6443.06 mi NW",
-        "SRC:LFD",
-        "CALL:EMS",
-        "ADDR:31004 NOTTINGHAM LN",
-        "X:SILVEY RD 6443.06 mi NW SILVEY RD 6443.06 mi NW");
-
-    doTest("T3",
-        "WHFD RCAD FALLS 33408 W 160TH ST  RAY COUNTY Description: 91 yof\nfell from standing position\nconsious and breathing\ncomplaining of l hip pain\nup in",
-        "SRC:WHFD RCAD",
-        "CALL:FALLS",
-        "ADDR:33408 W 160TH ST",
-        "INFO:91 yof fell from standing position consious and breathing complaining of l hip pain up in");
-
-    doTest("T4",
-        "WHFD RCAD RCSO DEATH 32534 MAGNOLIA LN  RAY COUNTY Description: 61 yo female\n",
-        "SRC:WHFD RCAD",
-        "UNIT:RCSO",
-        "CALL:DEATH",
-        "ADDR:32534 MAGNOLIA LN",
-        "INFO:61 yo female");
-
-    doTest("T5",
-        "WHFD RCAD TRAUMATIC INJURIES (SPECIFIC) 2062 E RIDGE DR  RAY COUNTY Description: 15 YO MALE\nBICYLCE ACCIDENT\nFELL FROM BIKE\nCONSCIOUS\nBREATHING\nNO S",
-        "SRC:WHFD RCAD",
-        "CALL:TRAUMATIC INJURIES (SPECIFIC)",
-        "ADDR:2062 E RIDGE DR",
-        "INFO:15 YO MALE BICYLCE ACCIDENT FELL FROM BIKE CONSCIOUS BREATHING NO S");
-
-    doTest("T6",
-        "WHFD RCAD M43 800 ABDOMINAL PAIN 12595 ORRICK RD  RAY COUNTY Description: 28 YOLD FEMALE \nCONS AND BREATH \nSEVERE STOMACH PAIN LOWER ABDOMEN \nLUPUS",
-        "SRC:WHFD RCAD",
-        "UNIT:M43 800",
-        "CALL:ABDOMINAL PAIN",
-        "ADDR:12595 ORRICK RD",
-        "INFO:28 YOLD FEMALE CONS AND BREATH SEVERE STOMACH PAIN LOWER ABDOMEN LUPUS");
-
-    doTest("T7",
-        "WHFD RCAD EMS 501 ARAPAHOE DR HOMESTEAD VILLAGE Description: [11/19/2011 02:56:14 0021] female help \n" +
-        "[11/19/2011 02:56:56 0021] calling back \n",
-
-        "SRC:WHFD RCAD",
-        "CALL:EMS",
-        "ADDR:501 ARAPAHOE DR",
-        "CITY:HOMESTEAD VILLAGE",
-        "INFO:female help / calling back",
-        "DATE:11/19/2011",
-        "TIME:02:56:14");
-  }
-  
-  @Test
-  public void testParser2() {
-
-    doTest("T1",
-        "LMED1 LSQ1 L602 SICK PERSON (SPECIFIC DIAGNOSIS) 215 E 5TH ST 3A LAWSON Description: 123254415\nPT IS PATRICIA BIDDIX\nHIGH BP\n67 YO FEMALE\nBURNING SE",
-        "UNIT:LMED1 LSQ1 L602",
-        "CALL:SICK PERSON (SPECIFIC DIAGNOSIS)",
-        "ADDR:215 E 5TH ST 3A",
-        "CITY:LAWSON",
-        "INFO:123254415 PT IS PATRICIA BIDDIX HIGH BP 67 YO FEMALE BURNING SE");
-
-    doTest("T2",
-        "LMED1 HEART PROBLEM / AICD 618 SHEPHERD LN   Description: 17 YO Rapid Heart rate\n\n",
-        "UNIT:LMED1",
-        "CALL:HEART PROBLEM/AICD",
-        "ADDR:618 SHEPHERD LN",
-        "INFO:17 YO Rapid Heart rate");
-
-    doTest("T3",
-        "LMED1 LSQ1 L602 SICK PERSON (SPECIFIC DIAGNOSIS) 316 KINGS DR  LAWSON Description: 90 yof\nill since for in morning\nhigh sugar\nbreathing difficulty\nu",
-        "UNIT:LMED1 LSQ1 L602",
-        "CALL:SICK PERSON (SPECIFIC DIAGNOSIS)",
-        "ADDR:316 KINGS DR",
-        "CITY:LAWSON",
-        "INFO:90 yof ill since for in morning high sugar breathing difficulty u");
-
-    doTest("T4",
-        "LMED1 LSQ1 L602 SEIZURE 34030 W 204TH ST  RAY COUNTY Description: camp wilderness retreat\n13/f\nbreathing\ncons\nseizing at this time\n1st seizure for a",
-        "UNIT:LMED1 LSQ1 L602",
-        "CALL:SEIZURE",
-        "ADDR:34030 W 204TH ST",
-        "INFO:camp wilderness retreat 13/f breathing cons seizing at this time 1st seizure for a");
-
-    doTest("T5",
-        "LMED1 LSQ1 L602 LACERATION 202 E MOSS ST  LAWSON Description: puncture wound in l foot\n42 yof\nconsious and breathing\nnausea CrossStreets: S DONIPHAN",
-        "UNIT:LMED1 LSQ1 L602",
-        "CALL:LACERATION",
-        "ADDR:202 E MOSS ST",
-        "CITY:LAWSON",
-        "INFO:puncture wound in l foot 42 yof consious and breathing nausea",
-        "X:S DONIPHAN");
-  }
-  
-  @Test
-  public void testParser3() {
-
-    doTest("T1",
-        "800 WHP1 WHT2 WHSQ1 LP4 LP1 LT1 OT2 RFD RCAD WHFD WHR1 FIRE STRUCTURE 11783 SIEGEL CEMETERY RD  RAY COUNTY Description: EXCELSIOR SPRINGS\nOUTSIDE CI",
-        "SRC:RFD RCAD WHFD",
-        "UNIT:800 WHP1 WHT2 WHSQ1 LP4 LP1 LT1 OT2 WHR1",
-        "CALL:FIRE STRUCTURE",
-        "ADDR:11783 SIEGEL CEMETERY RD",
-        "INFO:EXCELSIOR SPRINGS OUTSIDE CI");
-
-    doTest("T2",
-        "800 WHB1 WHT2 WHFD WHR1 FIRE NATURAL COVER OR BRUSH 33637 HIGHWAY U  RAY COUNTY Description: SMALL BRUSH FIRE THAT IS NEXT TO A CROP FIELD\nRP IS STA",
-        "SRC:WHFD",
-        "UNIT:800 WHB1 WHT2 WHR1",
-        "CALL:FIRE NATURAL COVER OR BRUSH",
-        "ADDR:33637 HIGHWAY U",
-        "INFO:SMALL BRUSH FIRE THAT IS NEXT TO A CROP FIELD RP IS STA");
-
-    doTest("T3",
-        "WHP1 800 WHFD WHR1 FIRE ALARM 34684 HIGHWAY 10  RAY COUNTY Description: gen fire alarm. covers all zones\n\nkeyholder ron rouse 816-864-4434",
-        "SRC:WHFD",
-        "UNIT:WHP1 800 WHR1",
-        "CALL:FIRE ALARM",
-        "ADDR:34684 HIGHWAY 10",
-        "INFO:gen fire alarm. covers all zones keyholder ron rouse 816-864-4434");
-
-    doTest("T4",
-        "672 671A RCAD WHFD MENTAL 10-96 15041 S BEACH FRONT DR  CRYSTAL LAKES Description: 16 yr old male\n6'1 205\nl CrossStreets: APACHE DR 0.07 mi E EAGLE",
-        "SRC:RCAD WHFD",
-        "UNIT:672 671A",
-        "CALL:MENTAL",
-        "ADDR:10-96 15041 S BEACH FRONT DR",
-        "MADDR:10 15041 S BEACH FRONT DR",
+        "14851 HIGHWAY Y CRYSTAL LAKES CrossStreets: BEACH DR 0.09 mi N RAVENWOOD DR 0.00 mi W OUTSIDE FIRE",
+        "ADDR:14851 HIGHWAY Y",
         "CITY:CRYSTAL LAKES",
-        "INFO:16 yr old male 6'1 205 l",
-        "X:APACHE DR 0.07 mi E EAGLE");
-
-    doTest("T5",
-        "OBTK1 OT1 OT2 OFD OSQ1 WHFD 800 FIRE NATURAL COVER OR BRUSH 9650 FOX LN RAY COUNTY Description: states that she is seeing alot of smoke in the area ",
-        "SRC:OFD WHFD",
-        "UNIT:OBTK1 OT1 OT2 OSQ1 800",
-        "CALL:FIRE NATURAL COVER OR BRUSH",
-        "ADDR:9650 FOX LN",
-        "INFO:states that she is seeing alot of smoke in the area");
-
-    doTest("T6",
-        "OBTK1 OT1 OT2 OFD OSQ1 LFD RFD WHSQ1 LB1 RBT7 800 FIRE NATURAL COVER OR BRUSH 10013 REYNOLDS RD RAY COUNTY Description: ADV THAT FIRE THEY WERE ON Y",
-        "SRC:OFD LFD RFD",
-        "UNIT:OBTK1 OT1 OT2 OSQ1 WHSQ1 LB1 RBT7 800",
-        "CALL:FIRE NATURAL COVER OR BRUSH",
-        "ADDR:10013 REYNOLDS RD",
-        "INFO:ADV THAT FIRE THEY WERE ON Y");
-  }
-  
-  @Test
-  public void testParser5() {
-
-    doTest("T1",
-        "WHFD RCAD EMS 501 ARAPAHOE DR HOMESTEAD VILLAGE Description: [11/19/2011 02:56:14 0021] female help \n[11/19/2011 02:56:56 0021] calling back \n",
-        "SRC:WHFD RCAD",
-        "CALL:EMS",
-        "ADDR:501 ARAPAHOE DR",
-        "CITY:HOMESTEAD VILLAGE",
-        "INFO:female help / calling back",
-        "DATE:11/19/2011",
-        "TIME:02:56:14");
-
-  }
-  
-  @Test
-  public void testParserLeeOdell() {
-
-    doTest("T1",
-        "LMED1 LSQ1 LP2 LP3 L602 MSHP RCSO 10-50 TRAFFIC/TRANSPORTATION INCIDENT W 206TH ST & HIGHWAY C RAY COUNTY Description: on 206th n one mile west of c",
-        "UNIT:LMED1 LSQ1 LP2 LP3 L602 MSHP RCSO",
-        "CALL:10-50 TRAFFIC/TRANSPORTATION INCIDENT",
-        "ADDR:W 206TH ST & HIGHWAY C",
-        "INFO:on 206th n one mile west of c");
-
-    doTest("T2",
-        "LFD LMED1 10-50 TRAFFIC/TRANSPORTATION INCIDENT 69 HWY  Description: *** SOP Begin ***\n\n" +
-        "1. * HOW MANY VEHICLES: 1\n\n" +
-        "2. * ANY INJURIES: YES\n" +
-        "3. * IS TH",
-
-        "SRC:LFD",
-        "UNIT:LMED1",
-        "CALL:10-50 TRAFFIC/TRANSPORTATION INCIDENT",
-        "ADDR:69 HWY",
-        "INFO:*** SOP Begin *** 1. * HOW MANY VEHICLES: 1 2. * ANY INJURIES: YES 3. * IS TH");
+        "X:BEACH DR/0.09 mi N / RAVENWOOD DR/0.00 mi W",
+        "CALL:OUTSIDE FIRE");
 
     doTest("T3",
-        "LFD FIRE MUTUAL AID 1410 HOSPITAL DRIVE  Description: EXCELSIUOR SPRINGS REQUESTING PUMPER AT 1410 VALLEY MANOR\n\n" +
-        "VALLEY MANOR AND REHAB CENTER\n\n" +
-        "Cro",
-
-        "SRC:LFD",
-        "CALL:FIRE MUTUAL AID",
-        "ADDR:1410 HOSPITAL DRIVE",
-        "INFO:EXCELSIUOR SPRINGS REQUESTING PUMPER AT 1410 VALLEY MANOR VALLEY MANOR AND REHAB CENTER");
+        "15659 HIGHWAY Y RAY COUNTY CrossStreets: PINE RD 0.07 mi N W 158TH ST 0.10 mi S OUTSIDE FIRE",
+        "ADDR:15659 HIGHWAY Y",
+        "X:PINE RD/0.07 mi N / W 158TH ST/0.10 mi S",
+        "CALL:OUTSIDE FIRE");
 
     doTest("T4",
-        "LMED1 LSQ1 L602 CHEST PAIN (NON-TRAUMATIC) 319 E 2ND ST, Apt. C2 LAWSON Description: CHEST PAINS LAST NIGHT AND STILL THIS MORNING\n" +
-        "83 YOM\n" +
-        "FRANCIS MC",
-
-        "UNIT:LMED1 LSQ1 L602",
-        "CALL:CHEST PAIN (NON-TRAUMATIC)",
-        "ADDR:319 E 2ND ST",
-        "APT:C2",
-        "CITY:LAWSON",
-        "INFO:CHEST PAINS LAST NIGHT AND STILL THIS MORNING 83 YOM FRANCIS MC");
+        "107 S DONNA DR RAY COUNTY CrossStreets: W DONNA DR 0.02 mi NE CAROL LN 0.05 mi S OUTSIDE FIRE",
+        "ADDR:107 S DONNA DR",
+        "X:W DONNA DR/0.02 mi NE / CAROL LN/0.05 mi S",
+        "CALL:OUTSIDE FIRE");
 
     doTest("T5",
-        "LMED1 10-50 TRAFFIC/TRANSPORTATION INCIDENT SALEM RD & N OF 166TH ST  Description: unknown if there are any injuries \n" +
-        "2 vehicles \n\n" +
-        "CrossStreets:",
-
-        "UNIT:LMED1",
-        "CALL:10-50 TRAFFIC/TRANSPORTATION INCIDENT",
-        "ADDR:SALEM RD & N OF 166TH ST",
-        "INFO:unknown if there are any injuries 2 vehicles");
+        "4364 HIGHWAY 10 RAY COUNTY CrossStreets: MOCKINGBIRD LN 0.18 mi NW LITTLE FARM RD 0.17 mi SE STROKE (CVA)",
+        "ADDR:4364 HIGHWAY 10",
+        "X:MOCKINGBIRD LN/0.18 mi NW / LITTLE FARM RD/0.17 mi SE",
+        "CALL:STROKE (CVA)");
 
     doTest("T6",
-        "LMED1 10-50 TRAFFIC/TRANSPORTATION INCIDENT SALEM RD & 174TH  Description: CLAY COUNTY\n\n" +
-        "DWI ACCIDENT\n" +
-        "VEHICLE IN DITCH\n\n\n" +
-        "CrossStreets:",
+        "4684 HIGHWAY 10 RAY COUNTY CrossStreets: CROWLEY RD 0.20 mi NW SHORT RD 0.30 mi SE FIRE ALARM",
+        "ADDR:4684 HIGHWAY 10",
+        "X:CROWLEY RD/0.20 mi NW / SHORT RD/0.30 mi SE",
+        "CALL:FIRE ALARM");
 
-        "UNIT:LMED1",
-        "CALL:10-50 TRAFFIC/TRANSPORTATION INCIDENT",
-        "ADDR:SALEM RD & 174TH",
-        "INFO:CLAY COUNTY DWI ACCIDENT VEHICLE IN DITCH");
+  }
+  
+  @Test
+  public void testActive911A() {
 
-    doTest("T7",
-        "LFD 10-50 TRAFFIC/TRANSPORTATION INCIDENT SALEM RD LAWSON Description: One mile south of Lawson on Salem Road. Just South of the Golf Course",
-        "SRC:LFD",
-        "CALL:10-50 TRAFFIC/TRANSPORTATION INCIDENT",
-        "ADDR:SALEM RD",
-        "CITY:LAWSON",
-        "INFO:One mile south of Lawson on Salem Road. Just South of the Golf Course");
+    doTest("T1",
+        "[] 15659 HIGHWAY Y RAY COUNTY CrossStreets: PINE RD 0.07 mi N W 158TH ST 0.10 mi S OUTSIDE FIRE\r\n\r\n\r\n\r\n\r\n\r\n",
+        "ADDR:15659 HIGHWAY Y",
+        "X:PINE RD/0.07 mi N / W 158TH ST/0.10 mi S",
+        "CALL:OUTSIDE FIRE");
 
-    doTest("T8",
-        "LFD SUICIDE ATTEMPT IP OR JO ENTRYWAY TO WATKINS MILL  Description: SUICIDAL SUBJ\n\nCrossStreets:",
-        "SRC:LFD",
-        "CALL:SUICIDE ATTEMPT IP OR JO ENTRYWAY TO WATKINS MILL",
-        "INFO:SUICIDAL SUBJ");
+    doTest("T2",
+        "[] 14851 HIGHWAY Y CRYSTAL LAKES CrossStreets: BEACH DR 0.09 mi N RAVENWOOD DR 0.00 mi W OUTSIDE FIRE\r\n\r\n\r\n\r\n\r\n\r\n",
+        "ADDR:14851 HIGHWAY Y",
+        "CITY:CRYSTAL LAKES",
+        "X:BEACH DR/0.09 mi N / RAVENWOOD DR/0.00 mi W",
+        "CALL:OUTSIDE FIRE");
+
+    doTest("T3",
+        "[] HIGHWAY Y RAY COUNTY CrossStreets: 10-50 TRAFFIC/TRANSPORT INCIDENT (CRASH)\r\n\r\n\r\n\r\n\r\n\r\n",
+        "ADDR:HIGHWAY Y",
+        "CALL:10-50 TRAFFIC/TRANSPORT INCIDENT (CRASH)");
+
+    doTest("T4",
+        "[] HIGHWAY 10 & W 133RD ST RAY COUNTY CrossStreets: OUTSIDE FIRE\r\n\r\n\r\n\r\n\r\n\r\n",
+        "ADDR:HIGHWAY 10 & W 133RD ST",
+        "CALL:OUTSIDE FIRE");
+
+    doTest("T5",
+        "[] 18347 N UNION RD RAY COUNTY CrossStreets: HIGHWAY C 0.40 mi S MUTUAL AID/ASSIST OUTSIDE AGENCIES\r\n\r\n\r\n\r\n\r\n\r\n",
+        "ADDR:18347 N UNION RD",
+        "X:HIGHWAY C/0.40 mi S",
+        "CALL:MUTUAL AID/ASSIST OUTSIDE AGENCIES");
 
   }
 
   public static void main(String[] args) {
-    new MORayCountyParserTest().generateTests("T2");
+    new MORayCountyParserTest().generateTests("T1", "ADDR APT CITY X CALL");
   }
 }
