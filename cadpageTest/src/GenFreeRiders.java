88d8eed46b1d4165ec8721a28280e985bd085d01
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-03 15:32:52 +0000

    Removed logic generatede freeriders.xml

--- cadpageTest/src/GenFreeRiders.java
+++ cadpageTest/src/GenFreeRiders.java
@@ -5,15 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import net.anei.cadpage.donation.UserAcctManager;
 
 /**
  * This class converts the input file freeriders.txt to the freeriders.xml
@@ -23,7 +18,6 @@ import net.anei.cadpage.donation.UserAcctManager;
 public class GenFreeRiders {
   
   private static final File IN_FILE = new File("freeriders.txt");
-  private static final File OUT_FILE = new File("./res/values/freeriders.xml");
   private static final File CSV_FILE = new File("freeriders.csv");
 
   private static Map<String,String> purchaseDateMap = new HashMap<String,String>();
@@ -33,18 +27,11 @@ public class GenFreeRiders {
     
     // Open input and output files
     BufferedReader ir = new BufferedReader(new InputStreamReader(new FileInputStream(IN_FILE)));
-    PrintStream ps = new PrintStream(new FileOutputStream(OUT_FILE));
     PrintStream ps2 = new PrintStream(new FileOutputStream(CSV_FILE));
     
-    // Write appropriate output XML headings
-    ps.println("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-    ps.println("<resources>");
-    
-    String listName = null;
     String status = "";
     String sponsor = "";
     String purchase = "";
-    List<String> userList = new ArrayList<String>(); 
     while (true) {
       String line = ir.readLine();
       if (line == null) break;
@@ -52,13 +39,7 @@ public class GenFreeRiders {
       if (pt >= 0) line = line.substring(0,pt);
       line = line.trim();
       if (line.length() == 0) continue;
-      if (line.startsWith("$")) {
-        writeList(listName, userList, ps);
-        listName = line.substring(1).trim();
-        userList.clear();
-      }
-      
-      else if (line.startsWith("%%")) {
+      if (line.startsWith("%%")) {
         purchase = line.substring(2).trim();
       }
       
@@ -77,7 +58,6 @@ public class GenFreeRiders {
         boolean skipHash = line.startsWith(">>");
         if (skipHash) line = line.substring(2);
         UserInfo info = cvtUser(line);
-        if (!skipHash) userList.add(generateHashLine(info));
         String tmpPurchase = purchaseDateMap.get(info.user);
         if (tmpPurchase == null) {
           tmpPurchase = purchase;
@@ -87,12 +67,7 @@ public class GenFreeRiders {
         ps2.println(line);
       }
     }
-    
-    writeList(listName, userList, ps);
-    
-    ps.println("</resources>");
     ir.close();
-    ps.close();
     ps2.close();
     
     System.out.println("freeriders.xml has been generated");
@@ -140,15 +115,6 @@ public class GenFreeRiders {
       throw new RuntimeException(ex.getMessage() + " in line: " + line, ex);
     }
   }
-      
-  
-  private static String generateHashLine(UserInfo info) {
-      String result = "<item>" + UserAcctManager.calcHash(info.user) + "</item>";
-      if (info.name != null && info.name.length() > 0) {
-        result += "  " + "<!-- " + info.name + " -->";
-      }
-      return result;
-  }
 
   
   private static String generateCsvLine(UserInfo info, String status,
@@ -166,29 +132,5 @@ public class GenFreeRiders {
     sb.append(',');
     if (sponsor != null) sb.append(sponsor);
     return sb.toString();
-  }
-
-  /**
-   * Write accumulated user list to output file
-   * @param listName name of user list
-   * @param userList list of users
-   * @param ps output stream
-   */
-  private static void writeList(String listName, List<String> userList,
-                                PrintStream ps) {
-    if (listName == null) {
-      if (userList.size() == 0) return;
-      throw new RuntimeException("Missing initial list name");
-    }
-    if (listName.length() == 0) return;
-    ps.println("  <string-array name=\"" + listName + "\">");
-    
-    String[] users = userList.toArray(new String[userList.size()]);
-    Arrays.sort(users);
-    for (String user : users) {
-      ps.println("    " + user);
-    }
-    
-    ps.println("  </string-array>");
   }
 }
