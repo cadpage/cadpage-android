f8190c4b0bd0e21098375490a099876630e4e92b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-05 21:44:23 +0000

    Updated sender filter for Montgomery County, NY

--- cadpage/src/net/anei/cadpage/parsers/NY/NYMontgomeryCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYMontgomeryCountyParser.java
@@ -49,7 +49,7 @@ public class NYMontgomeryCountyParser extends SmartAddressParser {
   
   @Override
   public String getFilter() {
-    return "paging@impact-sys.com,messaging@iamresponding.com";
+    return "paging@impact-sys.com,messaging@iamresponding.com,impact@impact.co.Montgomery.NY.us";
   }
 
   @Override
