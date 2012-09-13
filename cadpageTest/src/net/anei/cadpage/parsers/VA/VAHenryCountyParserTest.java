270d32c7710494c414ca02df0a5c59b82d1e0e6f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-13 05:14:11 +0000

    Fixed parsing problem with Henry County, VA

--- cadpageTest/src/net/anei/cadpage/parsers/VA/VAHenryCountyParserTest.java
+++ cadpageTest/src/net/anei/cadpage/parsers/VA/VAHenryCountyParserTest.java
@@ -4,6 +4,25 @@ import net.anei.cadpage.parsers.BaseParserTest;
 
 import org.junit.Test;
 
+/*
+Henry County, VA
+Contact: Hamp Ingram <emt.basics@gmail.com>
+Sender: MHC911@co.henry.va.us
+
+480 LEWIS RD MARTINSVILLE 2011095252 12:30:03 RFA-(3) FALL - FRACTURE
+2900 DILLONS FORK RD FIELDALE 2011095631 08:16:38 ROV-(2) OVERDOSE
+2487 SPRUCE ST MARTINSVILLE 2011095614 06:57:08 RFA-(3) FALL - FRACTURE
+1890 DANIELS CREEK RD COLLINSVILLE 2011095584 22:15:45 RSI-(2) SICK
+2630 VIRGINIA AVE COLLINSVILLE 2011095480 11:35:03 RDZ-(3) DIZZINESS, WEAKNESS
+1385 WINGFIELD ORCHARD RD MARTINSVILLE 2011095447 07:31:58 R3-(1) CHEST PAINS
+
+Contact: Bob Phillips <bobphillips2685@gmail.com>
+904 MOUNTAIN RD 11 MARTINSVILLE 2011097764 21:12:40 R3-(1) CHEST PAINS
+
+Contact: Active911
+[] 36 TERRY CT 7 COLLINSVILLE ROCKFORD RD X DEAD END 2012083814 01:48:06 -CARDIAC (WITH PREVIOUS HISTORY) 24 YOA, DIFF BREATHING, CHEST PAINS, Radiating TO HER BACK.\r\n\n
+
+*/
 
 public class VAHenryCountyParserTest extends BaseParserTest {
   
@@ -80,7 +99,21 @@ public class VAHenryCountyParserTest extends BaseParserTest {
 
   }
   
+  @Test
+  public void testActive911A() {
+
+    doTest("T1",
+        "[] 36 TERRY CT 7 COLLINSVILLE ROCKFORD RD X DEAD END 2012083814 01:48:06 -CARDIAC (WITH PREVIOUS HISTORY) 24 YOA, DIFF BREATHING, CHEST PAINS, Radiating TO HER BACK.\r\n\n",
+        "ADDR:36 TERRY CT 7",
+        "CITY:COLLINSVILLE",
+        "X:ROCKFORD RD / DEAD END",
+        "ID:2012083814",
+        "TIME:01:48:06",
+        "INFO:CARDIAC (WITH PREVIOUS HISTORY) 24 YOA, DIFF BREATHING, CHEST PAINS, Radiating TO HER BACK.");
+
+  }
+  
   public static void main(String[] args) {
-    new VAHenryCountyParserTest().generateTests("T1", "ADDR CITY ID TIME UNIT CALL INFO");
+    new VAHenryCountyParserTest().generateTests("T1", "ADDR CITY X ID TIME UNIT CALL INFO");
   }
 }
