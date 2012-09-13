4b63a567baadf8669b8975f466f93d7c0d95897b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-13 05:13:48 +0000

    Fixed parsing problem with Henry County, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
@@ -3,30 +3,13 @@ package net.anei.cadpage.parsers.VA;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
-/*
-Henry County, VA
-Contact: Hamp Ingram <emt.basics@gmail.com>
-Sender: MHC911@co.henry.va.us
-
-480 LEWIS RD MARTINSVILLE 2011095252 12:30:03 RFA-(3) FALL - FRACTURE
-2900 DILLONS FORK RD FIELDALE 2011095631 08:16:38 ROV-(2) OVERDOSE
-2487 SPRUCE ST MARTINSVILLE 2011095614 06:57:08 RFA-(3) FALL - FRACTURE
-1890 DANIELS CREEK RD COLLINSVILLE 2011095584 22:15:45 RSI-(2) SICK
-2630 VIRGINIA AVE COLLINSVILLE 2011095480 11:35:03 RDZ-(3) DIZZINESS, WEAKNESS
-1385 WINGFIELD ORCHARD RD MARTINSVILLE 2011095447 07:31:58 R3-(1) CHEST PAINS
-
-Contact: Bob Phillips <bobphillips2685@gmail.com>
-904 MOUNTAIN RD 11 MARTINSVILLE 2011097764 21:12:40 R3-(1) CHEST PAINS
-
-*/
-
+/**
+ * Henrey County, VA
+ */
 public class VAHenryCountyParser extends DispatchSouthernParser {
   
-  private static final String[] CITY_LIST = new String[]
-     {"RIDGEWAY", "MARTINSVILLE", "SPENCER", "BASSETT", "COLLINSVILLE", "FIELDALE", "AXTON"}; 
-  
   public VAHenryCountyParser() {
-    super(CITY_LIST, "HENRY COUNTY", "VA", DSFLAG_UNIT);
+    super(CITY_LIST, "HENRY COUNTY", "VA", DSFLAG_CROSS_NAME_PHONE | DSFLAG_UNIT);
   }
 
   @Override
@@ -37,8 +20,20 @@ public class VAHenryCountyParser extends DispatchSouthernParser {
   @Override
   public boolean parseMsg(String body, Data data) {
     if (!super.parseMsg(body, data)) return false;
+    
+    // Missing units show up as the first word of the call description (or info) with a leading dash
+    if (data.strUnit.startsWith("-")) {
+      String word = data.strUnit.substring(1) + " ";
+      if (data.strCall.length() > 0) data.strCall = word + data.strCall;
+      else data.strSupp = word + data.strSupp;
+      data.strUnit = "";
+    }
+    
     // Dispatch - Google spat
     data.strAddress = data.strAddress.replace("WILLIAM F STONE PARK ", "WILLIAM F STONE ");
     return true;
   }
+  
+  private static final String[] CITY_LIST = new String[]
+     {"RIDGEWAY", "MARTINSVILLE", "SPENCER", "BASSETT", "COLLINSVILLE", "FIELDALE", "AXTON"}; 
 }
