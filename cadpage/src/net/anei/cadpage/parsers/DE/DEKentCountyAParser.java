16ae3d3e30b84429e0b213cb162bbe8a57df742f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-18 15:58:55 +0000

    Fixed parsing probelm with Kent County, DE (A) Updated msg doc

--- cadpage/src/net/anei/cadpage/parsers/DE/DEKentCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/DE/DEKentCountyAParser.java
@@ -8,32 +8,9 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
-/*
-Kent County, DE (Variant A)
-Contact: John Wothers <eng416res@yahoo.com>
-Sender: rc.187@c-msg.net
-System: New World Systems
-
-[!]  [K] 12D1 Seizure - Not Breathing 263 MORGANS CHOICE RD Camden Wyoming : Xst's: WILLOW GROVE RD / MUMFIELD LN Caller: COLE JR EARL
-[!]  [K] Alarms-Residential (single) 121 BIRKDALE LN Camden Wyoming : TRAYBERN Xst's: BIRKDALE WAY / N HIGH HILL RD, TRAYBERN WAY Caller: ADT
-[!]  [K] Structure-Chimney 6871 WILLOW GROVE RD Camden Wyoming : Xst's: HENRY COWGILL RD / WILLOW TREE CIR Caller: PETE
-[!]  [K] 31D2 Unconscious/Fainting 10829 WESTVILLE RD Camden Wyoming : Xst's: UNKNOWN / GREENS VILLAGE DR Caller: JOHNSON WANDA
-[!]  [K] 29B1 MVC Injuries HENRY COWGILL RD Camden Wyoming : Xst's: Caller:
-[!]  [K] Alarms-Residential (single) 233 TOWER RD Camden Wyoming : Xst's: UNKNOWN / DARLING FARM RD Caller: GUARDIAN TINA
-[!]  [K] Stand-By,Cover-up 2 N MAIN ST STATION 55 Magnolia : MAGNOLIA Xst's: W WALNUT ST, S MAIN ST, E WALNUT ST / BOOTY DR Caller:
-[!]  [K] 17B3G Falls - Unknown 852 STEELES RIDGE RD Camden Wyoming : Xst's: ASHLAND AVE / HENRY COWGILL RD, MAIN ST Caller: GARY
-[!]  [K] Alarms-Commercial 1678 S GOVERNORS AVE Dover : Xst's: S DUPONT HWY / GUNNING BEDFORD DR Caller: DAVID
-[!]  [K] 29D2L MVC Motorcycle/Bicycle 375 GATEWAY SOUTH BLVD SONIC - SOUTH DOVER Dover : Xst's: DEAD END / DEAD END Caller:
-[!]  [K] Structure-Residential Single 241 E CAMDEN WYOMING AVE Camden Wyoming : CAMDEN Xst's: S MAIN ST, N MAIN ST / S WEST ST, N WEST ST Caller: HARRISON THERE'S
-
-Contact: Tom <tw4715@comcast.net>
-[CAD]33C1T Transfer/Interfacility 2809 ANDREWVILLE RD Harrington : Xst's: TODDS CHAPEL RD, PROSPECT CHURCH RD / GREENWOOD RD Caller: CAY HOLLY
-
-Contact: Sam Crawford <nremtde@gmail.com>
-[K] 17B1G Falls - Possibly Dangerous 2 LAKE CREST DR Milford : HAVEN LAKE ESTATES Xst's: HAVEN LN / WILLIAMSVILLE RD Caller: L MURPHY  
-
+/**
+ * Kent County, DE (Variant A)
  */
-
 
 public class DEKentCountyAParser extends FieldProgramParser {
   
@@ -51,9 +28,10 @@ public class DEKentCountyAParser extends FieldProgramParser {
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("!|K") && !subject.equals("K") && !subject.equals("CAD")) return false;
+    boolean good = subject.equals("!|K") || subject.equals("K") || subject.equals("CAD");
     body = body.replace("Xst's:", "Xsts:");
-    return parseFields(splitMsg(body), data);
+    if (!parseFields(splitMsg(body), data)) return false;
+    return good || data.strAddress.length() > 0 || data.strCross.length() > 0 || data.strName.length() > 0;
   }
   
   private String[] splitMsg(String body) {
