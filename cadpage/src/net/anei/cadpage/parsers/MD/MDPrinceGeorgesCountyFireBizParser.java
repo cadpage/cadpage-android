6f26711f0424a12a5ce74fdc7ea1f1065ae8139a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-19 03:26:52 +0000

    Added Jefferson Parish, LA location

--- cadpage/src/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyFireBizParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDPrinceGeorgesCountyFireBizParser.java
@@ -7,42 +7,9 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
-/*
-Prince Georges County, MD
-for Prince Georges, Anne Arundel, and Charles counties. and Howard County
-Contact: charles woodland <lollipopjuggs@gmail.com>
-Contact: Russ <rcfireblitz@gmail.com>
-System: Fireblitz
-From: @fireblitz.com
-
-55: BUILDING FIRE\nE809 TK809 BO804\n4637 37TH ST (ALLISON ST & DEAD END)\n12/07 04:51\nhttp://fireblitz.com/09/9.html
-0-2 H12: PI\nS11 R11\nPG INDIAN HEAD HWY/FARMINGTON RD WEST\nNotes: WANTS ENGINE AND AMBULANCE FROM STATION 11\n12/07 06:14\nhttp://fireblitz.com/11/0.html
-26: INVESTIGATION\nE817\n6036 PARKLAND CT (OLD SILVER HILL RD & OLD SILVER HILL RD)\n12/06 22:40\nhttp://fireblitz.com/17/9.html
-48: FD ACCIDENT\nSQ818 B802\nANNAPOLIS RD/PRINCESS GARDEN PY (9006 ANNAPOLIS RD/5900 PRINCESS GARD\n12/07 07:02\nhttp://fireblitz.com/18/6.html
-24: OVERDOSE - ALS\nA824 MD847, CHARLIE OD/NOT ALRT\n1605 AIRPORT LN (SCHALL RD & BERRY RD)\n12/07 07:14\nhttp://fireblitz.com/24/8.html
-27: INJURED/SICK\nA827\n6304 MAXWELL DR #4 (CARSWELL AV & MORRIS AV)\n12/07 05:14\nhttp://fireblitz.com/27/2.html
-46: BUILDING FIRE\nE833 TK833 BO802\n1200 CAPITAL CENTER BL (BLVD AT THE CAP CTR)\n12/07 00:51\nhttp://fireblitz.com/33/9.html
-46: BUILDING FIRE\nE837 TK837 BO802\n1200 CAPITAL CENTER BL (BLVD AT THE CAP CTR)\n12/07 00:51\nhttp://fireblitz.com/37/5.html
-49: MEDIC LOCAL\nA849\n13218 DEERFIELD RD (DEAD END & MATTHEWS CT)\n12/06 23:00\nhttp://fireblitz.com/49/1.html
-48: TOWNHOUSE FIRE\nE818 BO802\n9903 BREEZY KNOLL CT [DEAD END & GREEN HAVEN RD]\n12/23 23:32\nhttp://fireblitz.com/18/8.shtm
-
-Contact: Ricky Poole <handgunhunting@gmail.com>
-45: PIA-HIGHWAY\nSQ827\nNB CRAIN HWY SE/NB HEATHERMORE BLVD, PP - <8322/ 0>\n07/05 10:02\nhttp://fireblitz.com/27/0.shtm
-27: PIA-HIGHWAY\nA827 SQ827\nSB CAP BELT IL A HWY/NB BRANCH AVE, PP\n07/06 10:20\nhttp://fireblitz.com/27/5.shtm
-32: HOUSE FIRE\nE827 BO885\n5311 ACORN DR (GLEN OAK DR and WHITE OAK AVE)\n10/01 15:59\nhttp://fireblitz.com/PG/27/3.shtm
-26: APARTMENT FIRE\nSQ827 BO883\n6912 WALKER MILL RD #C2 (KAREN BLVD and SHADY GLEN DR)\n10/01 21:35\nhttp://fireblitz.com/PG/27/0.shtm
-
-Contact: g wise <firedawg2009@gmail.com>
-10-09: SEIZURES\nB13\n8896 FORT SMALLWOOD RD-PA 1 SPIN & TRIM LAUDROMAT (ELIZABETH RD & AMHERST CT)\nhttp://fireblitz.com/AA/13/8.shtm
-
-Contact: Christopher Olson <lilsmokeeater5@yahoo.com>
-Sender: @fireblitz.com
-1: MEDIC LOCAL\nE801\n6505 BELCREST RD\n02/10 14:55\nhttp://fireblitz.com/PG/01/5.shtm
-46: PIA\nE837\nNB HARRY S TRUMAN DR/NEW ORCHARD DR\n02/11 11:07\nhttp://fireblitz.com/PG/37/2.shtm
-pg37-e@fireblitz.com: 46: MEDIC LOCAL\nE837B\n2513 BAIKAL LOOP (CAPE BARON CT and CAPE TEAL CT)\n03/10 18:15\nhttp://fireblitz.com/PG/37/4.shtm 
-
+/**
+ * Prince Georges County, MD
  */
-
 public class MDPrinceGeorgesCountyFireBizParser extends FieldProgramParser {
   
   private static final Properties COUNTY_CODES = buildCodeTable(new String[]{
