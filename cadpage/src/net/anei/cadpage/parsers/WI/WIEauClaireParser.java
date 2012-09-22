3701c6013b8fbf7a9885dfb3570b02d92f589419
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-22 00:57:35 +0000

    Fixed parsing problem with Eau Claire, WI

--- cadpage/src/net/anei/cadpage/parsers/WI/WIEauClaireParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WI/WIEauClaireParser.java
@@ -8,41 +8,8 @@ import java.util.regex.Pattern;
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
-/*
-Eau Claire, WI
-Contact: "Cody Davey" <cody@wisconsinrangers.net>
-Sender: tfddispatch@att.net
-
-[Case# 11-253]  @5:23:16 AM // Brunswick // W2250 Rim Rock Rd // Vehicle Fire // vehicle fire, no exposures
-[Case# 11-252]  @7:21:18 PM // Washington // 4940 Meadowbrook Ln // Sounding Fire Alarm // sounding fire alarm, showing 1st & 2nd floor smoke detector
-[Case# 11-251]  @4:03:35 PM // Washington // 4400 blk South Lowes creek  rd // Fire //
-[Case# 11-250]  @12:48:10 PM // Union // HWY 12 & N. Crossing // Motorvehicle Collision // 10/50 PI
-[Case# 11-249]  @1:04:04 PM // Brunswick // W2601 HWY 37 // Electrical/Powerline Emergency // powerline is down and pole is on fire
-[Case# 11-248]  @4:48:14 PM // Union // I-94, 59mm, Westbound // Motorvehicle Collision // 10/50 PI
-[Case# 11-244]  @1:01:11 PM // Brunswick // Jopke Rd & HWY 37 // Motorvehicle Collision // 10/50 PI
-[Case# 11-241]  @10:25:42 AM // Washington // I94 WB Mile Marke 65 // Rescue/Medical Assist // 61 y/o male poss stroke
-
-Contact: Andrew Gaede <veghunter10@gmail.com>
-Sender: tfddispatch@att.net
-@4:38:53 PM // Rock Creek (Dunn) // HWY 85 & 870th // Motorvehicle\nCollision // 3 vehicle 10/50 PI, no extrication, Station 5 requested for\nassistance
-
-Contact: Me <cody@wisconsinrangers.net>
-Sender: tfddispatch@att.net
-(Case# 12-099) @2:45:18 AM // Seymour // Peterson Ave & Olson Dr. // MVC Extrication //\n10/50 PI with Extrication needed
-
-Contact: Chris Hedlund <chrishedlund213@yahoo.com>
-Sender: tfddispatch@att.net
-(Case# 12-117) @2:40:48 PM // Washington // henry at pamona // Motorvehicle Collision\n//
-(Case# 12-116) @1:51:32 PM // Washington // 500 block Deerfield RD  //\nIllegal/Attempted Burn // large brush pile
-(Case# 12-115) @10:31:26 PM // Seymour // 13034 Rossman Dr. // Electrical/Powerline\nEmergency // wires arcing
-(Case# 12-114) @6:32:48 PM // Union // 2211 Sherman Creek Rd // Service Call // Tree\nhas fallen into a house
-(Case# 12-112) @5:58:51 AM // Tiffany Twp (Dunn) // N11504 280th St // Search Call //\nSearch for hunter missing for 26+hours.  Request is for Manpower and\npossi
-(Case# 12-110) @1:10:09 PM // Brunswick // 4800 Karissa Dr. // Motorvehicle Collision\n// 10/50 PI
-(Case# 12-107) @8:14:11 AM // Seymour // Freeway 53 92mm Southbound // Vehicle Fire //\nvehicle fire
-(Case# 12-104) @11:13:39 AM // Brunswick // S8725 County Line Rd // Brush/Wildland Fire\n// possible out of control burn/brush fire in a field
-(Case# 12-099) @2:45:18 AM // Seymour // Peterson Ave & Olson Dr. // MVC Extrication //\n10/50 PI with Extrication needed
-(Case# 12-100) @1:33:50 PM // Pleasant Valley // S10700 Hwy 93 // Brush/Wildland Fire\n// grass fire
-
+/**
+ * Eau Claire, WI
  */
 public class WIEauClaireParser extends FieldProgramParser {
   
@@ -60,10 +27,21 @@ public class WIEauClaireParser extends FieldProgramParser {
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
-    Matcher  match = SUBJECT_PTN.matcher(subject);
-    if (match.matches()) {
+    
+    int pt = body.indexOf('@');
+    if (pt < 0) return false;
+    String sub = body.substring(0,pt).trim();
+    body = body.substring(pt+1).trim();
+    
+    if (subject.length() == 0) subject = sub;
+    if (sub.length() > 0 && !sub.equals(subject)) return false;
+    
+    if (subject.length() > 0) {
+      Matcher  match = SUBJECT_PTN.matcher(subject);
+      if (!match.matches()) return false;
       data.strCallId = match.group(1);
     }
+    
     body = body.replace("\n", " ").replaceAll("  +", " ");
     return parseFields(body.split("//"), data);
   }
@@ -73,8 +51,7 @@ public class WIEauClaireParser extends FieldProgramParser {
     
     @Override
     public void parse(String field, Data data) {
-      if (!field.startsWith("@")) abort();
-      setTime(TIME_FMT, field.substring(1), data);
+      setTime(TIME_FMT, field, data);
     }
   }
   
