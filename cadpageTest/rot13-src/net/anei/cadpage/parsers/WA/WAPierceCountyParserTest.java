1cb8b20ac2a660525a02c3f5fbea4f2320f7efa3
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-24 19:00:29 +0000

    Updated msg doc

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/WA/WAPierceCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/WA/WAPierceCountyParserTest.java
@@ -4,6 +4,22 @@ vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
 
 vzcbeg bet.whavg.Grfg;
 
+/*
+Cvrepr Pbhagl, JN
+Pbagnpg: Qnir Znefgba <qpznefgba@ubgznvy.pbz>
+Fraqre: pnqcntr@ezrgeb.pbz
+
+(- cneg 1 bs 1)EP:0038-N/Eha#36872/PNYY:03:32/QVFC:03:32/RAE:03:33/NGF:03:37/GEN:03:45/NGQ:0 3:53/CHMVC:98408/QB:Fg Wbfrcu Ubfcvgny
+
+(- cneg 1 bs 1) EP:Eha# 36872/Fvpx Crefba/<Haxabja>/F 56GU FG & CNPVSVP NIR///K=56gu&55gu/GNPBZN// NIR//606//GNPBZN/773 T5/\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbzzhavpngvba vagraqrq sbe gur nqq
+(- cneg 1 bs 1) EP:Eha# 45146/Hapbafpvbhf/<Haxabja>/6GU NIR & A FGRIRAF FG//oruvaq 76 fgngvba/K=A Ireqr Fg&A Fgriraf Fg/GNPBZN//\e\a\e\aGuvf vf n pbasvqragvny/cebcev
+(- cneg 1 bs 1) EP:Eha# 46137/Haxabja/<Haxabja>/6016 A UVTUYNAQF CXJL//371//GNPBZN/772 W4/\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbzzhavpngvba vagraqrq sbe gur n
+(- cneg 1 bs 1) EP:Eha# 45014/Fvpx Crefba/<Haxabja>/919 SNJPRGG NIR//606//GNPBZN/773 T5/\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbzzhavpngvba vagraqrq sbe gur nqq
+(- cneg 1 bs 1) EP:Eha# 46129/Genhzn/<Haxabja>/2018 R JEVTUG NIR///K=R Ebbfriryg Nir/GNPBZN//\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbzzhavpngvba vagraqrq sbe gu
+(- cneg 1 bs 1) EP:Eha# 46173/Haxabja/<Haxabja>/6316 F 12GU FG///K=Haxabja Fgerrg jvgu VQ 1319802&F Juvgzna Fg/GNPBZN//\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbz
+(- cneg 1 bs 1) EP:Eha# 46237/ZIN/<Haxabja>/4215 F 30GU FG///K=F Glyre Fg/GNPBZN//\e\a\e\aGuvf vf n pbasvqragvny/cebcevrgnel pbzzhavpngvba vagraqrq sbe gur nqqerffrr
+
+ */
 
 choyvp pynff JNCvreprPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
