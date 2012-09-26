6dfb440b87ab6bb3fc0d5dbb98d426ae4e2c3e4a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-26 15:52:50 +0000

    Fixed parsing problem with Shelby County, AL

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/AL/ALShelbyCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/AL/ALShelbyCountyParserTest.java
@@ -15,6 +15,18 @@ Fraqre: neaf@furyol911.bet
  1 bs 2\aSEZ:neaf@furyol911.bet\aFHOW:FURYOL911 NEAF NYREG\aZFT:\e\a5/17/2012 9:40:01 NZ\e\aSVER1208391\e\aPNU_INYL - FGN184\e\aNYNEZ-SVER\e\a\e\a5116 YVOREGL\a(Pba'g) 2 bs 2\aCXJL @ HEONA PRAGRE CXJL / HEONA PRAGRE CXJL\e\a\e\aOUZ SQ(Raq)
  1 bs 2\aSEZ:neaf@furyol911.bet\aFHOW:FURYOL911 NEAF NYREG\aZFT:\e\a5/17/2012 6:19:47 NZ\e\aSVER1208383\e\aPNU_INYL - FGN184\e\aOERNGUVAT-\e\a\e\a1085 BYQ ZVYY\a(Pba'g) 2 bs 2\aEHA @ BYQ ZVYY PVE / \e\a\e\aUHOREG, YNJFBA(Raq)
 
+Pbagnpg: Npgvir911
+Ntrapl anzr: OISEQ Ybpngvba: Oevresvryq, NY 
+Fraqre: neaf@furyol911.bet
+
+(FURYOL911 NEAF NYREG) \a9/18/2012 3:45:20 CZ\aSVER1216399\aOEVRESYQ - OVOO PB\aOERNGUVAT-\aOESQ\a   @  / \a: @OESQ: 18735 UVTUJNL 139,YBG 1\a CRGREF, QROEN
+(FURYOL911 NEAF NYREG) \a9/20/2012 11:55:55 NZ\aSVER1216515\aOEVRESYQ - OVOO PB\aURNEG-\aOESQ\a   @  / \a: @OESQ: 1373 JRFG NFUOL EQ\a IREVMBA JVERYRFF
+(FURYOL911 NEAF NYREG) \a9/20/2012 8:19:24 CZ\aSVER1216545\aOEVRESYQ - OVOO PB\aOERNGUVAT-\aOESQ\a   @  / \a: @OESQ:28 GUBZNF SHYTNZ EQ\aZBBER
+(FURYOL911 NEAF NYREG) \a9/21/2012 1:53:26 CZ\aSVER1216575\aOEVRESYQ - OVOO PB\aZIP-PURPX_VAWHEVRF\aOESQ\a   @  / \a: @OESQ:ZBERYNAQ EQ @ EE K VAT
+(FURYOL911 NEAF NYREG) \a9/22/2012 10:27:21 CZ\aSVER1216662\aOEVRESYQ - OVOO PB\aNYYRETVRF-\aOESQ\a   @  / \a: @OESQ:110 JNGRE GBJRE EQ\aOVOO PB
+(FURYOL911 NEAF NYREG) \a9/23/2012 11:02:33 CZ\aSVER1216711\aOEVRESYQ - OVOO PB\aPURFGCNVA-\aOESQ\a   @  / \a: @OESQ 9423 ZBAGRINYYB EQ\aOVOO PB
+(FURYOL911 NEAF NYREG) \a9/25/2012 5:20:02 CZ\aSVER1216809\aOEVRESYQ - OVOO PB\aFRVMHER-\aOESQ\a   @  / \a: @OESQ: 137 JRFG NFUOL EQ\a IREVMBA JVERYRFF
+
 */
 
 choyvp pynff NYFuryolPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
@@ -129,6 +141,143 @@ choyvp pynff NYFuryolPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:1085 BYQ ZVYY EHA",
         "K:BYQ ZVYY PVE",
         "VASB:UHOREG, YNJFBA");
+
+  }
+  
+  @Grfg
+  choyvp ibvq grfgNpgvir911N() {
+
+    qbGrfg("G1",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/18/2012 3:45:20 CZ\a" +
+        "SVER1216399\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "OERNGUVAT-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ: 18735 UVTUJNL 139,YBG 1\a" +
+        " CRGREF, QROEN",
+
+        "QNGR:9/18/2012",
+        "GVZR:15:45:20",
+        "VQ:SVER1216399",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:OERNGUVAT",
+        "NQQE:18735 UVTUJNL 139",
+        "NCG:YBG 1",
+        "VASB:CRGREF, QROEN");
+
+    qbGrfg("G2",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/20/2012 11:55:55 NZ\a" +
+        "SVER1216515\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "URNEG-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ: 1373 JRFG NFUOL EQ\a" +
+        " IREVMBA JVERYRFF",
+
+        "QNGR:9/20/2012",
+        "GVZR:11:55:55",
+        "VQ:SVER1216515",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:URNEG",
+        "NQQE:1373 JRFG NFUOL EQ",
+        "VASB:IREVMBA JVERYRFF");
+
+    qbGrfg("G3",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/20/2012 8:19:24 CZ\a" +
+        "SVER1216545\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "OERNGUVAT-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ:28 GUBZNF SHYTNZ EQ\a" +
+        "ZBBER",
+
+        "QNGR:9/20/2012",
+        "GVZR:20:19:24",
+        "VQ:SVER1216545",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:OERNGUVAT",
+        "NQQE:28 GUBZNF SHYTNZ EQ",
+        "VASB:ZBBER");
+
+    qbGrfg("G4",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/21/2012 1:53:26 CZ\a" +
+        "SVER1216575\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "ZIP-PURPX_VAWHEVRF\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ:ZBERYNAQ EQ @ EE K VAT",
+
+        "QNGR:9/21/2012",
+        "GVZR:13:53:26",
+        "VQ:SVER1216575",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:ZIP-PURPX_VAWHEVRF",
+        "NQQE:ZBERYNAQ EQ @ EE K VAT");
+
+    qbGrfg("G5",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/22/2012 10:27:21 CZ\a" +
+        "SVER1216662\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "NYYRETVRF-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ:110 JNGRE GBJRE EQ\a" +
+        "OVOO PB",
+
+        "QNGR:9/22/2012",
+        "GVZR:22:27:21",
+        "VQ:SVER1216662",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:NYYRETVRF",
+        "NQQE:110 JNGRE GBJRE EQ",
+        "VASB:OVOO PB");
+
+    qbGrfg("G6",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/23/2012 11:02:33 CZ\a" +
+        "SVER1216711\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "PURFGCNVA-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ 9423 ZBAGRINYYB EQ\a" +
+        "OVOO PB",
+
+        "QNGR:9/23/2012",
+        "GVZR:23:02:33",
+        "VQ:SVER1216711",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:PURFGCNVA",
+        "NQQE:9423 ZBAGRINYYB EQ",
+        "VASB:OVOO PB");
+
+    qbGrfg("G7",
+        "(FURYOL911 NEAF NYREG) \a" +
+        "9/25/2012 5:20:02 CZ\a" +
+        "SVER1216809\a" +
+        "OEVRESYQ - OVOO PB\a" +
+        "FRVMHER-\a" +
+        "OESQ\a" +
+        "   @  / \a" +
+        ": @OESQ: 137 JRFG NFUOL EQ\a" +
+        " IREVMBA JVERYRFF",
+
+        "QNGR:9/25/2012",
+        "GVZR:17:20:02",
+        "VQ:SVER1216809",
+        "FEP:OEVRESYQ - OVOO PB",
+        "PNYY:FRVMHER",
+        "NQQE:137 JRFG NFUOL EQ",
+        "VASB:IREVMBA JVERYRFF");
 
   }
   
