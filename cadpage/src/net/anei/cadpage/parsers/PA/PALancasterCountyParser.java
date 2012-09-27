97a316e63e23a643d7af905cfa9effac2d340de2
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-27 05:00:56 +0000

    Fixed parsing problem and added time parsing to Lancaster County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PALancasterCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PALancasterCountyParser.java
@@ -2,18 +2,17 @@ package net.anei.cadpage.parsers.PA;
 
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * Lancaster County, PA
  */
-public class PALancasterCountyParser extends MsgParser {
-  
-  private static Pattern LANC_PTN = Pattern.compile("\\bLANC\\b", Pattern.CASE_INSENSITIVE);
+public class PALancasterCountyParser extends FieldProgramParser {
   
   public PALancasterCountyParser() {
-    super("LANCASTER COUNTY", "PA");
+    super("LANCASTER COUNTY", "PA",
+           "CITY ADDR X/Z+? UNIT TIME!");
   }
   
   @Override
@@ -32,52 +31,47 @@ public class PALancasterCountyParser extends MsgParser {
       data.strSource = subject;
     }
     
-    int ndx = 0;
-    for (String line : body.split("~")) {
-      ndx++;
-      line = line.trim();
-      switch (ndx) {
-      
-      case 1:
-        int pt = line.indexOf('/');
-        if (pt < 0) pt = line.indexOf('\n');
-        if (pt >= 0) {
-          data.strCall = append(data.strCall, " / ", line.substring(0,pt).trim());
-          line = line.substring(pt+1).trim();
-        } else {
-          if (data.strCall.length() == 0) return false;
-        }
-        data.strCity = line;
-        if (data.strCity.endsWith(" BORO")) data.strCity = data.strCity.substring(0, data.strCity.length()-5).trim();
-        if (data.strCity.endsWith(" BOROUGH")) data.strCity = data.strCity.substring(0, data.strCity.length()-8).trim();
-        if (data.strCity.startsWith("LANC")) data.strCity = "LANCASTER";
-        break;
-        
-      case 2:
-        line = LANC_PTN.matcher(line).replaceAll("LANCASTER");
-        parseAddress(line, data);
-        break;
-        
-      case 3:
-        data.strCross = line;
-        break;
-        
-      case 4:
-        // Could be second cross st, or if there wasn't a cross street, this
-        // would be the unit field
-        if (line.length() == 0) break;
-        if (data.strCross.length() > 0) {
-          data.strCross += " & " + line;
-          break;
-        }
-        ndx++;
-        // fall through to case 5
-    
-      case 5:
-        data.strUnit = line;
-        break;
+    if (body.endsWith("^")) body = body.substring(0,body.length()-1).trim();
+    return parseFields(body.split("~"), data);
+  }
+  
+  @Override
+  public String getProgram() {
+    return "SRC CALL " + super.getProgram();
+  }
+  
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('\n');
+      if (pt < 0) pt = field.indexOf('/');
+      if (pt >= 0) {
+        data.strCall = append(data.strCall, " / ", field.substring(0,pt).trim());
+        field = field.substring(pt+1).trim();
+      } else {
+        if (data.strCall.length() == 0) abort();
       }
+      data.strCity = field;
+      if (data.strCity.endsWith(" BORO")) data.strCity = data.strCity.substring(0, data.strCity.length()-5).trim();
+      if (data.strCity.endsWith(" BOROUGH")) data.strCity = data.strCity.substring(0, data.strCity.length()-8).trim();
+      if (data.strCity.startsWith("LANC")) data.strCity = "LANCASTER";
     }
-    return true;
+  }
+  
+  private static Pattern LANC_PTN = Pattern.compile("\\bLANC\\b", Pattern.CASE_INSENSITIVE);
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      field = LANC_PTN.matcher(field).replaceAll("LANCASTER");
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CITY")) return new MyCityField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
   }
 }
