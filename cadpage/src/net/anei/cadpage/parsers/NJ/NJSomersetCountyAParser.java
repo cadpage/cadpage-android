74b979fb73716e6c8f8b78d369dbc9cca036713f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-09-30 20:16:50 +0000

    Fixed parsing problem with Somerset County, NJ (A)

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJSomersetCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJSomersetCountyAParser.java
@@ -8,55 +8,15 @@ import net.anei.cadpage.parsers.MsgParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-/*
-Somerset County, NJ
-Contact: Peter Archibald <parchibald27145@gmail.com>
-Contact: Ron DePasquale <rondepasquale@gmail.com>
-Sender: rc.502@c-msg.net
-[CAD]  [CAD] EFD-FD:11051201:05/12/2011 15:44:57:FIRE ALARM: FRANKLI-CARING HOUSE PROJECTS / 18 BLOOMFIELD AVE
-[CAD]  [CAD] EFD-FD:11051982:05/14/2011 11:51:28:FIRE ALARM:GENERAL: FRANKLI-HAMPTON INN / 255 DAVIDSON AVE
-[CAD]  [CAD] EFD-FD:11052014:05/14/2011 13:35:40:FIRE ALARM:ZONE-2ND FL REAR HALL SMK: FRANKLI-1165 HAMILTON ST
-[CAD]  [CAD] EFD-FD:11052016:05/14/2011 13:40:22:FIRE ALARM:GENERAL ALARM: FRANKLI-449 SKILLMANS LANE (ODD)
-[CAD]  [CAD] EFD-FD:11052407:05/15/2011 14:41:50:CO ALARM:NO ILLNESS: FRANKLI-25 MAYNARD ROAD
-[CAD]  [CAD] EFD-FD:11052545:05/15/2011 21:22:21:MVC: FRANKLI-CASTLETON AVE & EASTON AVE
-[CAD]  [CAD] EFD-FD:11052638:05/16/2011 04:39:12:FIRE ALARM:Z-GENERAL: FRANKLI-THE DEN / 700 HAMILTON ST
-[CAD]  [CAD] EFD-FD:11053024:05/16/2011 19:33:38:STRUCTURE FIRE: FRANKLI-12 NEWKIRK ROAD
-[CAD]  [CAD] EFD-FD:11053250:05/17/2011 11:56:48:FIRE ALARM:Z/GEN: FRANKLI-RETA - RENAISSANCE EMPLOYMENT / 630 FRANKLIN BLVD
-[CAD]  [CAD] EFD-FD:11053657:05/18/2011 13:30:59:STOVE/OVEN/FOOD: FRANKLI-138 ELLEN ST
-
-Contact: Dan Ungar <danungar@gmail.com>
-Sender: @co.somerset.nj.us.
-MGT-FD:11084743:07/25/2011 10:51:36:FIRE ALARM:ZONE/ GENERAL: MONTGOM-MONTGOMERY EVANGELICAL CHURCH / 246 BELLE MEAD GRIGGSTOWN ROAD
-GRG-FD:11087510:07/31/2011 13:19:31:FIRE ALARM:GENERAL: FRANKLI-GRIGGSTOWN GROUP HOME / 7 HONEYMAN ST
-MGT-FD:11084928:07/25/2011 17:32:48:SMOKE CONDITION: MONTGOM-952 COUNTY ROUTE 518 HWY
-GRG-FD:11088457:08/02/2011 15:39:21:MV FIRE:SMOKE ONLY: FRANKLI-12 BARBIERI CT
-MGT-FD:11088557:08/02/2011 18:37:19:BRUSH FIRE: MONTGOM-SPRING HILL ROAD
-MGT-FD:11088946:08/03/2011 16:09:20:FIRE ALARM:SMOKE DECTORS: MONTGOM-3 LOWRY CT
-GRG-FD:11089016:08/03/2011 17:54:34:FIRE ALARM:FIRST FL SMK DECTOR: FRANKLI-46 SAPPHIRE LANE
-MGT-FD:11089091:08/03/2011 21:11:26:FIRE ALARM:ZONE / 363: MONTGOM-STONEBRIDGE AT MONTGOMERY / 100 HOLLINSHEAD SPRING ROAD
-GRG-FD:11090214:08/06/2011 16:56:13:FIRE ALARM:SMOKE DETECTOR: FRANKLI-27 KIRBY LANE
-GRG-FD:11090292:08/06/2011 21:34:50:FIRE ALARM:GENERAL: FRANKLI-3333 STATE HWY NO 27 HWY
-GRG-FD:11079916:07/14/2011 17:36:09:MVC:V POLE: FRANKLI-BUTLER ROAD & S MIDDLEBUSH ROAD
-
-Contact: Sean Lambertz <sean.lambertz@gmail.com>
-*3898: *messaging@iamresponding.com / 77 RESCUE / kharju:SBB-RS:12029633 :03/01/2012 23:53:12:SICK PERSON:22 YOM: SOUTH B-SOUTHSIDE GRILL / 2 MAIN ST #10
-*4101: *ghargreave@co.somerset.nj.us /  / SBB-FD:12027787:02/27/2012 11:58:34:FIRE: SOUTH B-SOUTHSIDE GRILL / 2 MAIN ST
-  / SBB-FD:12033968:03/11/2012 10:49:03:STRUCTURE FIRE: BOUND B-547 WHEATLAND AVE\n
-
-Contact: support@active911.com
-Sender: mmisciagna@co.somerset.nj.us
-([Alarm] (no subject)) LBC-FD:12046100:04/03/2012 11:44:34:FIRE ALARM:PULLSTATION: BERNARD-FELLOWSHIP VILLAGE / 8000 FELLOWSHIP RD\n\n_______________________________________________\nAlarm mailing list\nAlarm@libertycornerfire.org\nhttp://libertycornerfire.org/mailman/listinfo/alarm_libertycornerfire.org
-([Alarm] (no subject)) LBC-FD:12046621:04/04/2012 09:39:53:FIRE ALARM:GENERAL PULL ZONE 19: BERNARD-FELLOWSHIP VILLAGE / 8000 FELLOWSHIP RD\n\n_______________________________________________\nAlarm mailing list\nAlarm@libertycornerfire.org\nhttp://libertycornerfire.org/mailman/listinfo/alarm_libertycornerfire.org
-([Alarm] (no subject)) LBC-FD:12045123:04/01/2012 17:54:21:WATER PROBLEM: BERNARD-28 SHANNON HILL RD\n\n_______________________________________________\nAlarm mailing list\nAlarm@libertycornerfire.org\nhttp://libertycornerfire.org/mailman/listinfo/alarm_libertycornerfire.org
-([Alarm] (no subject)) LBC-FD:12045135:04/01/2012 18:31:20:MV FIRE:PATROL FIRE: BERNARD-BERNARDS TOWNSHIP POLICE DEPT / 1 COLLYER LN\n\n_______________________________________________\nAlarm mailing list\nAlarm@libertycornerfire.org\nhttp://libertycornerfire.org/mailman/listinfo/alarm_libertycornerfire.org
-([Alarm] (no subject)) LBC-FD:12045790:04/02/2012 20:49:11:OPEN BURN: BERNARD-15 QUINCY RD\n\n_______________________________________________\nAlarm mailing list\nAlarm@libertycornerfire.org\nhttp://libertycornerfire.org/mailman/listinfo/alarm_libertycornerfire.org
-
-*/
-
+/**
+ * Somerset County, NJ
+ */
 public class NJSomersetCountyAParser extends MsgParser {
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "BOUND B", "BOUND BROOK",
+      "BRANCHB", "BRANCHBURG",
+      "BRIDGEW", "BRIDGEWATER TWP",
       "FRANKLI", "FRANKLIN TWP",
       "MONTGOM", "MONTGOMERY TWP",
       "HOPEWEL", "HOPEWELL TWP",
@@ -65,7 +25,7 @@ public class NJSomersetCountyAParser extends MsgParser {
       "SOUTH B", "SOUTH BRUNSWICK"
   });
   
-  private static final Pattern MARKER = Pattern.compile("^(?:/ [A-Z0-9 ]* / (?:[A-Za-z]+:)?(?:[[^:]*]:)?)?([^: ]+):(\\d{8}) *:(\\d\\d/\\d\\d/\\d\\d\\d\\d) (\\d\\d:\\d\\d:\\d\\d):");
+  private static final Pattern MARKER = Pattern.compile("^(?:(?:/ [A-Z0-9 ]* / )?(?:[A-Za-z]+:)?(?:[[^:]*]:)?)?([^: ]+):(\\d{8}) *:(\\d\\d/\\d\\d/\\d\\d\\d\\d) (\\d\\d:\\d\\d:\\d\\d):");
   private static final Pattern MASTER = Pattern.compile("(.*?): ([A-Z ]+)-(?! )(?:(.*) / +)?([^\\(]*)(?:\\((.*)\\))?");
   private static final Pattern ROUTE_HWY_PTN = Pattern.compile("\\b(STATE|COUNTY|US) (?:ROUTE|ROAD|HWY)(?: NO)? (\\d+)(?: HWY)?\\b");
   
@@ -110,6 +70,15 @@ public class NJSomersetCountyAParser extends MsgParser {
     data.strAddress = ROUTE_HWY_PTN.matcher(data.strAddress).replaceFirst("$1 $2");
     return true;
   }
+  
+  @Override
+  public String adjustMapAddress(String sAddr) {
+    sAddr = US_PTN.matcher(sAddr).replaceAll("US");
+    sAddr = NO_PTN.matcher(sAddr).replaceAll(" ").trim();
+    return sAddr;
+  }
+  private static final Pattern US_PTN = Pattern.compile("\\bU +S\\b");
+  private static final Pattern NO_PTN = Pattern.compile(" *\\bNO\\b *");
 
   private String null2empty(String str) {
     return str == null ? "" : str.trim();
