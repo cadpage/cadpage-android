1393fb2b65e0edc5d4727c8f301460f1e1da448b
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-04 18:15:18 +0000

    Fixed parsing problem with Olmsted County, MN

--- cadpage/src/net/anei/cadpage/parsers/MN/MNOlmstedCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MN/MNOlmstedCountyParser.java
@@ -3,22 +3,9 @@ package net.anei.cadpage.parsers.MN;
 import net.anei.cadpage.parsers.SmartAddressParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
-/*
-Olmsted County, MN
-Contact: Joe Manning <jlmanning@gmail.com>
-Sender: @rochestermn.gov
-
-(Main St N 20 ST NW) Two vehicle Accident with injuries, neck and head pain\r\n\r\nJeff Wellik\r\nPublic Safety Communications\r\nRochester Police/Olmsted County Sh
-(4639 grove st se) Elderly male stroke symptoms, blurred vision weak legs
-(200 lakeshore dr nw) Med
-(200 lakeshore dr nw  florence park) medical
-(500 6 st ne) Medical
-(6220 70 ST SW) Male with chest pains  6220 70 ST SW
-(medical 1106 Sunrise Ct SE  baby with siezure)
-(101 schumann dr nw) GRASS FIRE ON W SIDE OF KWIK TRIP
-
-*/
-
+/**
+ * Olmsted County, MN
+ */
 public class MNOlmstedCountyParser extends SmartAddressParser {
   
   public MNOlmstedCountyParser() {
@@ -40,23 +27,33 @@ public class MNOlmstedCountyParser extends SmartAddressParser {
     // If no body (which gets to us as no subject) treat as special case
     if (subject.length() == 0) {
       Parser p = new Parser(body);
-      data.strSupp = p.getLastOptional("  ");
-      parseAddress(StartType.START_CALL, FLAG_ANCHOR_END, p.get(), data);
-      if (data.strCall.length() == 0) {
-        data.strCall = data.strSupp;
-        data.strSupp = "";
-      }
+      parseAddress(StartType.START_CALL, FLAG_ANCHOR_END, p.get("  "), data);
+      if (data.strCall.length() == 0) data.strCall = p.get("  ");
+      data.strSupp = p.get();
     }
     
     else {
+      if (body.toUpperCase().startsWith(subject.toUpperCase())) {
+        subject = body.substring(0,subject.length()).trim();
+        body = body.substring(subject.length()).trim();
+        if (body.toUpperCase().startsWith("AT ")) body = body.substring(3).trim();
+      }
+      
       Parser p = new Parser(subject);
-      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END | FLAG_IMPLIED_INTERSECT, p.get("  "), data);
+      parseAddress(StartType.START_CALL, FLAG_ANCHOR_END | FLAG_IMPLIED_INTERSECT, p.get("  "), data);
       data.strPlace = p.get();
       
-      String[] flds = body.split("\n");
-      data.strCall = flds[0].trim();
-      for (int ii = 1; ii<flds.length; ii++) {
-        data.strSupp = append(data.strSupp, " / ", flds[ii].trim());
+      if (data.strAddress.length() > 0) {
+        String[] flds = body.split("\n");
+        data.strCall = flds[0].trim();
+        for (int ii = 1; ii<flds.length; ii++) {
+          data.strSupp = append(data.strSupp, " / ", flds[ii].trim());
+        }
+      }
+      
+      else {
+        parseAddress(StartType.START_ADDR, FLAG_IMPLIED_INTERSECT, body, data);
+        data.strSupp = getLeft();
       }
     }
     return true;
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNOlmstedCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNOlmstedCountyParserTest.java
@@ -4,6 +4,27 @@ vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
 
 vzcbeg bet.whavg.Grfg;
 
+/*
+Byzfgrq Pbhagl, ZA
+Pbagnpg: Wbr Znaavat <wyznaavat@tznvy.pbz>
+Fraqre: @ebpurfgreza.tbi
+
+(Znva Fg A 20 FG AJ) Gjb iruvpyr Nppvqrag jvgu vawhevrf, arpx naq urnq cnva\e\a\e\aWrss Jryyvx\e\aChoyvp Fnsrgl Pbzzhavpngvbaf\e\aEbpurfgre Cbyvpr/Byzfgrq Pbhagl Fu
+(4639 tebir fg fr) Ryqreyl znyr fgebxr flzcgbzf, oyheerq ivfvba jrnx yrtf
+(200 ynxrfuber qe aj) Zrq
+(200 ynxrfuber qe aj  syberapr cnex) zrqvpny
+(500 6 fg ar) Zrqvpny
+(6220 70 FG FJ) Znyr jvgu purfg cnvaf  6220 70 FG FJ
+(zrqvpny 1106 Fhaevfr Pg FR  onol jvgu fvrmher)
+(101 fpuhznaa qe aj) TENFF SVER BA J FVQR BS XJVX GEVC
+
+(zrqvpny) Zrqvpny ng 106 4 fg AR ncg 311
+(zrqvpny) 205 1 fg r sbe n zrqvpny
+(106 5 nir fr zrqvpny) Snyy ivpgvz ryqreyl srznyr
+(2538 75 fg fr  zrqvpny  znyr guebja sebz ubefr)
+(zrq 317 23 fg aj) Zrq 317 23 fg aj urnq vaw sebz n snyy
+
+*/
 
 choyvp pynff ZAByzfgrqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
@@ -61,10 +82,38 @@ choyvp pynff ZAByzfgrqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "(101 fpuhznaa qe aj) TENFF SVER BA J FVQR BS XJVX GEVC",
         "NQQE:101 fpuhznaa qe aj",
         "PNYY:TENFF SVER BA J FVQR BS XJVX GEVC");
+
+    qbGrfg("G9",
+        "(zrqvpny) Zrqvpny ng 106 4 fg AR ncg 311",
+        "NQQE:106 4 fg AR",
+        "NCG:311",
+        "PNYY:Zrqvpny");
+
+    qbGrfg("G10",
+        "(zrqvpny) 205 1 fg r sbe n zrqvpny",
+        "NQQE:205 1 fg r",
+        "PNYY:zrqvpny",
+        "VASB:sbe n zrqvpny");
+
+    qbGrfg("G11",
+        "(106 5 nir fr zrqvpny) Snyy ivpgvz ryqreyl srznyr",
+        "NQQE:106 5 nir fr zrqvpny",
+        "PNYY:Snyy ivpgvz ryqreyl srznyr");
+
+    qbGrfg("G12",
+        "(2538 75 fg fr  zrqvpny  znyr guebja sebz ubefr)",
+        "NQQE:2538 75 fg fr",
+        "PNYY:zrqvpny",
+        "VASB:znyr guebja sebz ubefr");
+
+    qbGrfg("G13",
+        "(zrq 317 23 fg aj) Zrq 317 23 fg aj urnq vaw sebz n snyy",
+        "NQQE:317 23 fg aj",
+        "PNYY:urnq vaw sebz n snyy");
         
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
-    arj ZAByzfgrqPbhaglCnefreGrfg().trarengrGrfgf("G1", "NQQE NCG CYNPR PNYY VASB");
+    arj ZAByzfgrqPbhaglCnefreGrfg().trarengrGrfgf("G9", "NQQE NCG CYNPR PNYY VASB");
   }
 }
