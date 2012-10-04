d4f5e29178443190aa319923449b7eff6b39f36d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-04 17:51:00 +0000

    More messages

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/CA/CAVenturaCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CA/CAVenturaCountyParserTest.java
@@ -14,6 +14,11 @@ Flfgrz: VasbEnq
 (SPP Cntr) Vapvqrag Qvfcngpu:G30   FGEHPGHER 905 Snyzbhgu Fg               526-W7 38/3912-0058915
 (SPP Cntr) Vapvqrag Qvfcngpu:G30   GP        796 Cnfrb Qr Yrba             555-R3 38/3912-0058883
 (SPP Cntr) Vapvqrag Qvfcngpu:G30   FVPX CREFB12 Pbarwb Oyiq                556-S1 38/3912-0058846
+(SPP Cntr) Vapvqrag Qvfcngpu:G30   HAPBAFPVBH190 R Uvyyperfg Qe            556-S1 38/3912-0059237
+(SPP Cntr) Vapvqrag Qvfcngpu:G30   SNYYF ABA 3557 Pnzchf Qe                526-R2 38/3912-0059231
+(SPP Cntr) Vapvqrag Qvfcngpu:G30   VAIRFGVTNGF Zbbecnex Eq / Ebyyvat Bnxf Q556-S2 38/3912-0058857
+(SPP Cntr) Vapvqrag Qvfcngpu:G30   URZBEEUNTR760 Jnejvpx Nir               526-S7 38/3912-0058807
+(SPP Cntr) Vapvqrag Qvfcngpu:R30   CHOYVP FREA Ylaa Eq / J Wnaff Eq        526-R5 38/3912-0058155
 
  */
 
@@ -52,6 +57,51 @@ choyvp pynff PNIraghenPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "ZNC:556-S1",
         "PU:38/39",
         "VQ:12-0058846");
+
+    qbGrfg("G4",
+        "(SPP Cntr) Vapvqrag Qvfcngpu:G30   HAPBAFPVBH190 R Uvyyperfg Qe            556-S1 38/3912-0059237",
+        "HAVG:G30",
+        "PNYY:HAPBAFPVBH",
+        "NQQE:190 R Uvyyperfg Qe",
+        "ZNC:556-S1",
+        "PU:38/39",
+        "VQ:12-0059237");
+
+    qbGrfg("G5",
+        "(SPP Cntr) Vapvqrag Qvfcngpu:G30   SNYYF ABA 3557 Pnzchf Qe                526-R2 38/3912-0059231",
+        "HAVG:G30",
+        "PNYY:SNYYF ABA",
+        "NQQE:3557 Pnzchf Qe",
+        "ZNC:526-R2",
+        "PU:38/39",
+        "VQ:12-0059231");
+
+    qbGrfg("G6",
+        "(SPP Cntr) Vapvqrag Qvfcngpu:G30   VAIRFGVTNGF Zbbecnex Eq / Ebyyvat Bnxf Q556-S2 38/3912-0058857",
+        "HAVG:G30",
+        "PNYY:VAIRFGVTNG",
+        "NQQE:F Zbbecnex Eq & Ebyyvat Bnxf Q",
+        "ZNC:556-S2",
+        "PU:38/39",
+        "VQ:12-0058857");
+
+    qbGrfg("G7",
+        "(SPP Cntr) Vapvqrag Qvfcngpu:G30   URZBEEUNTR760 Jnejvpx Nir               526-S7 38/3912-0058807",
+        "HAVG:G30",
+        "PNYY:URZBEEUNTR",
+        "NQQE:760 Jnejvpx Nir",
+        "ZNC:526-S7",
+        "PU:38/39",
+        "VQ:12-0058807");
+
+    qbGrfg("G8",
+        "(SPP Cntr) Vapvqrag Qvfcngpu:R30   CHOYVP FREA Ylaa Eq / J Wnaff Eq        526-R5 38/3912-0058155",
+        "HAVG:R30",
+        "PNYY:CHOYVP FRE",
+        "NQQE:A Ylaa Eq & J Wnaff Eq",
+        "ZNC:526-R5",
+        "PU:38/39",
+        "VQ:12-0058155");
 
   }
   
