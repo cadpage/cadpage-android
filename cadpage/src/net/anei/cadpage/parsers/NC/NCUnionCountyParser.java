bd754caa4b4267fd0803371eb20a9d68d6209376
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-05 06:43:09 +0000

    Fixed parsing problem with Union County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCUnionCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCUnionCountyParser.java
@@ -3,6 +3,8 @@ package net.anei.cadpage.parsers.NC;
 /**
  * Union County, NC
  */
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
@@ -11,12 +13,47 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
   
   public NCUnionCountyParser() {
     super(CITY_LIST, "UNION COUNTY", "NC",
-           "( CANCEL ADDR CITY? | FYI? ADDR CITY? CALL ) SRC? UNIT? INFO+? DATETIME PLACE");
+           "( CANCEL ADDR CITY? | FYI? ADDR CITY? CALL ) SRC? CUSTOM? INFO+? DATETIME PLACE");
   }
   
   @Override
   public String getFilter() {
     return "cad@uc.co.union.nc.us,cad@webmail.co.union.nc.us,cad@co.union.nc.us";
+  }
+  
+  // Custom field is an optional 6 characater field that
+  // different departments use for different purposes.  We will look at the
+  // previous source field to figure out which department this is and what
+  // they want us to do with it.
+  
+  private static final Pattern SRC_CHANNEL_PTN = Pattern.compile("S\\d\\d");
+  private class CustomField extends Field {
+    
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (field.length() > 6 || field.contains(" ")) return false;
+      parse(field, data);
+      return true;
+    }
+
+    @Override
+    public void parse(String field, Data data) {
+      if (SRC_CHANNEL_PTN.matcher(data.strSource).matches()) {
+        data.strChannel = field;
+      } else {
+        data.strUnit = field;
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "UNIT CH";
+    }
   }
   
   private class MyInfoField extends InfoField {
@@ -41,7 +78,7 @@ public class NCUnionCountyParser extends DispatchOSSIParser {
   protected Field getField(String name) {
     if (name.equals("CANCEL")) return new CallField("CANCEL");
     if (name.equals("SRC")) return new SourceField("[A-Z0-9]{2,4}", true);
-    if (name.equals("UNIT")) return new UnitField("[A-Z0-9]{2,4}(?:-[A-Z0-9]{1,4})?", true);
+    if (name.equals("CUSTOM")) return new CustomField();
     if (name.equals("INFO")) return new MyInfoField();
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d");
     return super.getField(name);
