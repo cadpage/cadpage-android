2d79afc1d3ee3e9d502b5d688f67b03f6508657d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-12 17:55:43 +0000

    Fixed parsing problem with Harnett County, NC (A)

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/NC/NCHarnettCountyAParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NC/NCHarnettCountyAParserTest.java
@@ -4,6 +4,48 @@ vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
 
 vzcbeg bet.whavg.Grfg;
 
+/* 
+Uneargg Pbhagl, AP
+Pbagnpg: Orireyl Jvyyvnzf <ojvyyvnzf.pb9@tznvy.pbz>
+Pbagnpg: Xrvgu pbzof <xrvgupbzof2007@tznvy.pbz>
+Pbagnpg: Zvpunry Zbfgba <zvxrzbfgba@tznvy.pbz>
+Pbagnpg: rqjneq jbbqnyy <rjbbqnyy1971@tznvy.pbz>
+Fraqre: pnqcntr@uneargg.bet
+
+1102-012074*8326 AP 27 J***YVYYVATGBA***ERFVQRAGVNY FGEHPGHER SVER****1175****
+F: Z:1107-061052*3058 PENJSBEQ EQ***REJVA***ZBGBE IRUVPYR NPPVQRAG****1186****\a
+SEZ:pnqcntr@uneargg.bet\aZFT:1108-070330*1995 ORGURY ONCGVFG EQ***FCEVAT YNXR***RYRPGEVPNY UNMNEQ****1120****
+1109-079514*117 FPENAGBA PG***PNZREBA***RZF QRNQ BA NEEVINY PBQR 3****1109****
+
+Pbagnpg: Puevf ZpPbezvpx <znp3295@tznvy.pbz>
+SEZ:pnqcntr@uneargg.bet\aZFT:1111-092766*1 PYNEX EQ***YVYYVATGBA***ZVFP. SVER****1175****
+SEZ:pnqcntr@uneargg.bet\aZFT:1111-093153*8030 ENL ZRQR***FCEVAT YNXR***ZHGHNY NVQ JVGU BGURE PBHAGL********
+SEZ:pnqcntr@uneargg.bet\aZFT:1201-003613*2728 QNEEBPU EQ*****SVER BGURE*********
+SEZ:pnqcntr@uneargg.bet\aZFT:1201-002111*11860 AP 210 F*****SVER ERF FGE*********
+
+Pbagnpg: Ebl Tvoof <obunz29@tznvy.pbz>
+O1201-004551*11 OEBBX YA*****RZF NOQ CNVA*********
+1201-009149*1 LBHAT EQ***NATVRE**SVER VYYRTNY*********
+1202-010125*1101 FVYNF ZBBER EQ***ORAFBA**RZF FVPX*********
+1202-010135*51 RYYVR PG***SHDHNL INEVAN**SVER ERF FGE*********
+
+Pbagnpg: Puevf ZpPbezvpx <znp3295@tznvy.pbz>
+SEZ:pnqcntr@uneargg.bet\aZFT:1201-006955*2802 JNYXRE EQ***YVAQRA**SVER ERF FGE*********
+
+Pbagnpg: Ebl Tvoof <obunz29@tznvy.pbz>
+1202-017757*137 ZREPUNAG QE***NATVRE**RZF ONPX CA*******Unmneqf: Ab**
+
+Pbagnpg: Elna Qnivf <urzvsernx16@tznvy.pbz>
+1203-021836*QNEX JBBQ QE***FCEVAT YNXR**SVER FZB VAI*******Unmneqf: Ab**
+
+Pbagnpg: Wrss Wbuafba <esqwwbuafba@tznvy.pbz>
+1112-103007*2915 ZNGGURJF ZVYY CBAQ EQ***NATVRE***ZBGBE IRUVPYR NPPVQRAG****1143****
+
+Pbagnpg: Xrvgu Cbyyneq <xrvguyhif2svfu@tznvy.pbz>
+Fraqre: pnqcntr@uneargg.bet
+1210-092849*460 ONVYRL EQ***PBNGF*ERQ UVYY PUHEPU EQ (*XRYYL YA***RZF OERNGUVA*******Unmneqf: Ab**
+
+*/
 
 choyvp pynff APUnearggPbhaglNCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
@@ -130,6 +172,19 @@ choyvp pynff APUnearggPbhaglNCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:2915 ZNGGURJF ZVYY CBAQ EQ",
         "PVGL:NATVRE",
         "PNYY:ZBGBE IRUVPYR NPPVQRAG");
+
+  }
+  
+  @Grfg
+  choyvp ibvq grfgXrvguCbyyneq() {
+
+    qbGrfg("G1",
+        "1210-092849*460 ONVYRL EQ***PBNGF*ERQ UVYY PUHEPU EQ (*XRYYL YA***RZF OERNGUVA*******Unmneqf: Ab**",
+        "VQ:1210-092849",
+        "NQQE:460 ONVYRL EQ",
+        "PVGL:PBNGF",
+        "K:ERQ UVYY PUHEPU EQ ( & XRYYL YA",
+        "PNYY:RZF OERNGUVA");
 
   }
 
