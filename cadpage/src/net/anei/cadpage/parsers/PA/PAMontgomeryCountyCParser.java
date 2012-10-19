ab2e7a5d64f42119386ac3d3490ac829cfd2571d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-19 17:36:56 +0000

    Fixed parsing problem with Montgomery County, PA (C)

--- cadpage/src/net/anei/cadpage/parsers/PA/PAMontgomeryCountyCParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PAMontgomeryCountyCParser.java
@@ -4,26 +4,6 @@ import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
-/*
-Montgomery County, PA
-Contact: CodeMessaging
-Sender: @c-msg.net
-
-[K] TRUCKS: E47-1,E47-2,SD47,TW47 127 S GULPH RD: @BEST WESTERN XST: W DEKALB PIKE/BILL SMITH BLVD NAT: FIRE ALARM,BOX:56-67/56-229
-[K] TRUCKS: E47-1,E47-2,SD47,TW47 201 ALLENDALE RD: @COSTCO WHOLESALE,APT: 100 XST: WILLS BLVD/ELLIOTT DR NAT: FIRE ALARM,BOX:56-10/
-[K] TRUCKS: E47-2,SD47 175 W VALLEY FORGE RD: @UPPER MERION TOWNSHIP BUILDING XST: N HENDERSON RD/NORTH GATE RD NAT: TRASH/DUMPSTER FIRE,BOX:56-09/56-200
-[K] 6010 COUNTY LINE RD: @OFFICE & WOOD SHOP - VFNP XST: VALLEY FORGE PARK RD/GULPH RD NAT: FIRE ALARM,BOX:58-1/
-[K] TRUCKS: E47-1,E47-2,SD47,TW47 680 W DEKALB PIKE: @MACY'S XST: ALLENDALE RD/MALL BLVD NAT: FIRE ALARM,BOX:56-67/56-202
-[K] TRUCKS: E47-1,SD47,TW47,R47 160 N GULPH RD: @KING OF PRUSSIA PLAZA XST: KIRK AVE/MALL BLVD NAT: FIRE ALARM,BOX:56-77/56-201
-[K] 260 N GULPH RD: @HOLIDAY INN EXPRESS XST: MALL BLVD/SCHUYLKILL EXPY OVERPASS NAT: BUILDING FIRE COMMERCIAL,BOX:56-77/56-218
-[K] TRUCKS: E47-1,E47-2,SD47,TW47 260 N GULPH RD: @HOLIDAY INN EXPRESS XST: MALL BLVD/SCHUYLKILL EXPY OVERPASS NAT: FIRE ALARM,BOX:56-77/
-[K] TRUCKS: E47-1,E47-2,SD47,TW47,3E68-1 1071 PENN CIR: @LAFAYETTE AT VALLEY FORGE - BUILDING G,APT: G306 XST: OLD FORGE LN/PARKVIEW DR NAT: BUILDING FIRE COMMERCIAL,BOX:56-80/56-211
-[K] TRUCKS: E47-1,E47-2,SD47 256 RT202 NB XST: RAMP RT422 EB TO RT202 NB/RAMP I76 EB TO RT202 NB NAT: VEHICLE FIRE,BOX:56-77/
-[K] TRUCKS: E47-1,E47-2,SD47,TW47 400 AMERICAN AVE: @HOMESTEAD VILLAGE HOTEL XST: 1ST AVE/ NAT: FIRE ALARM,BOX:56-75/
-[K] TRUCKS: E47-1,E47-2,SD47,TW47,QT31 122 CROSSFIELD RD XST: FOX RUN RD/HEARTHSTONE RD NAT: BUILDING FIRE COMMERCIAL,BOX:56-09/
-
-
-*/
 
 public class PAMontgomeryCountyCParser extends FieldProgramParser {
   
@@ -33,8 +13,7 @@ public class PAMontgomeryCountyCParser extends FieldProgramParser {
    }
   
   @Override 
-  public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.startsWith("K")) return false;
+  public boolean parseMsg(String body, Data data) {
     body = body.replace(": @", " Place:").replace(",BOX", " BOX");
     return super.parseMsg(body, data) && data.strAddress.length() > 0;
   }
@@ -58,9 +37,19 @@ public class PAMontgomeryCountyCParser extends FieldProgramParser {
     }
   }
   
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("/")) field = field.substring(1).trim();
+      if (field.endsWith("/")) field = field.substring(0,field.length()-1).trim();
+      super.parse(field, data);
+    }
+  }
+  
   @Override
   protected Field getField(String name) {
     if (name.equals("UNITADDR")) return new MyUnitAddressField();
+    if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
 }
