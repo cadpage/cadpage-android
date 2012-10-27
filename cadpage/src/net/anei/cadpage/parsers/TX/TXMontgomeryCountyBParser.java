dcefea804c378e5bf02335e034cac8e0fa5653a8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-10-27 00:45:36 +0000

    Fixed parsing problem with Montgomery County, TX

--- cadpage/src/net/anei/cadpage/parsers/TX/TXMontgomeryCountyBParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXMontgomeryCountyBParser.java
@@ -1,6 +1,8 @@
 package net.anei.cadpage.parsers.TX;
 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchProQAParser;
@@ -12,7 +14,7 @@ public class TXMontgomeryCountyBParser extends DispatchProQAParser {
   
   public TXMontgomeryCountyBParser() {
     super(CITY_CODES, "MONTGOMERY COUNTY", "TX",
-           "CODE MAP ADDR CITY PRI! INFO+");
+           "CODE MAP ADDR APT? CITY PRI! INFO+");
   }
   
   @Override
@@ -27,10 +29,29 @@ public class TXMontgomeryCountyBParser extends DispatchProQAParser {
     return super.parseMsg(body, data);
   }
   
+  private static final Pattern ZIP_CODE_PTN = Pattern.compile("(\\d{5})(?:-\\d{4})?");
+  private class MyCityField extends CityField {
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = ZIP_CODE_PTN.matcher(field);
+      if (!match.matches()) return false;
+      super.parse(match.group(1), data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('-');
+      if (pt >= 0) field = field.substring(0,pt).trim();
+      super.parse(field, data);
+    }
+  }
+  
   @Override
   public Field getField(String name) {
     if (name.equals("CODE")) return new CodeField("\\d{2}[A-Z]\\d{2}", true);
     if (name.equals("MAP")) return new MapField("\\d{1,4}[A-Z]", true);
+    if (name.equals("CITY")) return new MyCityField();
     if (name.equals("PRI")) return new PriorityField("Priority +(\\d)", true);
     return super.getField(name);
   }
@@ -41,5 +62,6 @@ public class TXMontgomeryCountyBParser extends DispatchProQAParser {
       "77306", "CONROE",
       "77356", "SAM HOUSTON NAT FOREST",
       "77380", "SPRING",
+      "77384", "SHENANDOAH"
   });
 }
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/TX/TXMontgomeryCountyBParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/TX/TXMontgomeryCountyBParserTest.java
@@ -15,8 +15,6 @@ Eha# 42118/ 29Q02/ 70H/ SZ 149 & SZ 1375/ 77356/ Cevbevgl 1/ 2 IRU J/ 1 EBYYBIRE
 Eha# 42280/ 29Q02/ 250A/ XHLXRAQNUY EQ & PERRXFVQR SBERFG QE/ 77389/ Cevbevgl 1/ NFFVFG PPRZF/ PBASVEZRQ RAGENCZRAG/ ERD. CEVBEVGL 1. Z29 NAQ R107 BA YBP. XOEBJA PBZZ\a\a\a\a***************************************************************************\aGur pbagragf bs guvf pbzzhavpngvba ner vagraqrq bayl sbe gur nqqerffrr naq znl pbagnva pbasvqragvny naq/be cevivyrtrq zngrevny. Vs lbh ner abg gur vagraqrq erpvcvrag, cyrnfr qb abg ernq, pbcl, hfr be qvfpybfr guvf pbzzhavpngvba naq abgvsl gur fraqre. Bcvavbaf, pbapyhfvbaf naq bgure vasbezngvba va guvf pbzzhavpngvba gung qb abg eryngr gb gur bssvpvny ohfvarff bs Zbagtbzrel Pbhagl Ubfcvgny Qvfgevpg funyy or haqrefgbbq nf arvgure tvira abe raqbefrq ol vg.\a\a***************************************************************************
 Eha# 42160/ 09R01/ 188R/ 114 CBAQREBFN GEY/ 77301/ Cevbevgl 1/ 68 LBZ/ JVGARFFRQ NEERFG/ PCE OL PNYYGNXRE. XOEBJA PBZZ\a\a\a\a***************************************************************************\aGur pbagragf bs guvf pbzzhavpngvba ner vagraqrq bayl sbe gur nqqerffrr naq znl pbagnva pbasvqragvny naq/be cevivyrtrq zngrevny. Vs lbh ner abg gur vagraqrq erpvcvrag, cyrnfr qb abg ernq, pbcl, hfr be qvfpybfr guvf pbzzhavpngvba naq abgvsl gur fraqre. Bcvavbaf, pbapyhfvbaf naq bgure vasbezngvba va guvf pbzzhavpngvba gung qb abg eryngr gb gur bssvpvny ohfvarff bs Zbagtbzrel Pbhagl Ubfcvgny Qvfgevpg funyy or haqrefgbbq nf arvgure tvira abe raqbefrq ol vg.\a\a***************************************************************************
 Eha# 42236/ 09R01/ 189T/ 10931 QBAAN/ 77306/ Cevbevgl 1/ 21LBZ PCE OL PNYYGNXRE. XRIVA/NYNEZ\a\a\a\a***************************************************************************\aGur pbagragf bs guvf pbzzhavpngvba ner vagraqrq bayl sbe gur nqqerffrr naq znl pbagnva pbasvqragvny naq/be cevivyrtrq zngrevny. Vs lbh ner abg gur vagraqrq erpvcvrag, cyrnfr qb abg ernq, pbcl, hfr be qvfpybfr guvf pbzzhavpngvba naq abgvsl gur fraqre. Bcvavbaf, pbapyhfvbaf naq bgure vasbezngvba va guvf pbzzhavpngvba gung qb abg eryngr gb gur bssvpvny ohfvarff bs Zbagtbzrel Pbhagl Ubfcvgny Qvfgevpg funyy or haqrefgbbq nf arvgure tvira abe raqbefrq ol vg.\a\a***************************************************************************
-
-** ABG CNEFVAT LRG **
 EP:Eha# 42923/33P01/217M/111 IVFVBA CNEX OYIQ/200/77384-3002/Cevbevgl 1/56LBZ QHEVAT VAGHONGVBA CG ORPNZR OENQVPNEQVP. QE VF VA GUR CEBPRFF BS JNXVAT HC GUR CG\a\a\a\a***************************************************************************\aGur pbagragf bs guvf pbzzhavpngvba ner vagraqrq bayl sbe gur nqqerffrr naq znl pbagnva pbasvqragvny naq/be cevivyrtrq zngrevny. Vs lbh ner abg gur vagraqrq erpvcvrag, cyrnfr qb abg ernq, pbcl, hfr be qvfpybfr guvf pbzzhavpngvba naq abgvsl gur fraqre. Bcvavbaf, pbapyhfvbaf naq bgure vasbezngvba va guvf pbzzhavpngvba gung qb abg eryngr gb gur bssvpvny ohfvarff bs Zbagtbzrel Pbhagl Ubfcvgny Qvfgevpg funyy or haqrefgbbq nf arvgure tvira abe raqbefrq ol vg.\a\a***************************************************************************
 
  */
@@ -113,7 +111,22 @@ choyvp pynff GKZbagtbzrelPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "PVGL:PBAEBR",
         "CEV:1",
         "VASB:21LBZ PCE OL PNYYGNXRE. XRIVA / NYNEZ");
-     
+
+    qbGrfg("G7",
+        "EP:Eha# 42923/33P01/217M/111 IVFVBA CNEX OYIQ/200/77384-3002/Cevbevgl 1/56LBZ QHEVAT VAGHONGVBA CG ORPNZR OENQVPNEQVP. QE VF VA GUR CEBPRFF BS JNXVAT HC GUR CG\a\a\a\a" +
+        "***************************************************************************\a" +
+        "Gur pbagragf bs guvf pbzzhavpngvba ner vagraqrq bayl sbe gur nqqerffrr naq znl pbagnva pbasvqragvny naq/be cevivyrtrq zngrevny. Vs lbh ner abg gur vagraqrq erpvcvrag, cyrnfr qb abg ernq, pbcl, hfr be qvfpybfr guvf pbzzhavpngvba naq abgvsl gur fraqre. Bcvavbaf, pbapyhfvbaf naq bgure vasbezngvba va guvf pbzzhavpngvba gung qb abg eryngr gb gur bssvpvny ohfvarff bs Zbagtbzrel Pbhagl Ubfcvgny Qvfgevpg funyy or haqrefgbbq nf arvgure tvira abe raqbefrq ol vg.\a\a" +
+        "***************************************************************************",
+
+        "VQ:42923",
+        "PBQR:33P01",
+        "ZNC:217M",
+        "NQQE:111 IVFVBA CNEX OYIQ",
+        "NCG:200",
+        "PVGL:FURANAQBNU",
+        "CEV:1",
+        "VASB:56LBZ QHEVAT VAGHONGVBA CG ORPNZR OENQVPNEQVP. QE VF VA GUR CEBPRFF BS JNXVAT HC GUR CG");
+    
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
