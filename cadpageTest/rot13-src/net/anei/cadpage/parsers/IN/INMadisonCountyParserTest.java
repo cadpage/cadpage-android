8965a9dea4f7f87be20cdca9e20d5bde2c8504b9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-11-27 23:11:50 +0000

    Added Date/Time and city parsing to Madison County, IN (A)

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/IN/INMadisonCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/IN/INMadisonCountyParserTest.java
@@ -34,43 +34,59 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
   @Grfg
   choyvp ibvq grfgCnefre() {
+
     qbGrfg("G1",
         "Havg:FG40 HavgFgf: Ybp:6302 NCBYYB QE Irahr:EVPUYN GJC Vap:Oehfu Sver Qngr:10/17/2010 Gvzr:18:49 BA TRZVAV / 1 FG UBHFR FBHGU RNFG RAQ Nqqgy:GENFU SVER BA GUR TEBHAQ/JUV ZBQHYNE JUV INA FVGGVAT VA QEVIR",
         "HAVG:FG40",
         "NQQE:6302 NCBYYB QE",
+        "PVGL:EVPUYNAQ GJC",
         "PNYY:Oehfu Sver",
-        "VASB:BA TRZVAV / 1 FG UBHFR FBHGU RNFG RAQ GENFU SVER BA GUR TEBHAQ/JUV ZBQHYNE JUV INA FVGGVAT VA QEVIR");
-    
+        "QNGR:10/17/2010",
+        "GVZR:18:49",
+        "VASB:BA TRZVAV / 1 FG UBHFR FBHGU RNFG RAQ / GENFU SVER BA GUR TEBHAQ/JUV ZBQHYNE JUV INA FVGGVAT VA QEVIR");
+
     qbGrfg("G2",
         "Havg:NZ45 HavgFgf: Ybp:6197 A FE 9 Irahr:EVPUYN GJC Vap:RZF Pnyy Qngr:10/17/2010 Gvzr:01:12 56 LBZ / NQI UR QBRFAG SRRY TBBQ / IBZVGVAT N Nqqgy:AQ AB NCCVGVGR NYY QNL / JVYY OR JNGVAT BHGFV QR ERFQ:",
         "HAVG:NZ45",
         "NQQE:6197 A FE 9",
         "ZNQQE:6197 A VA 9",
+        "PVGL:EVPUYNAQ GJC",
         "PNYY:RZF Pnyy",
-        "VASB:56 LBZ / NQI UR QBRFAG SRRY TBBQ / IBZVGVAT N AQ AB NCCVGVGR NYY QNL / JVYY OR JNGVAT BHGFV QR ERFQ:");
+        "QNGR:10/17/2010",
+        "GVZR:01:12",
+        "VASB:56 LBZ / NQI UR QBRFAG SRRY TBBQ / IBZVGVAT N / AQ AB NCCVGVGR NYY QNL / JVYY OR JNGVAT BHGFV QR ERFQ:");
 
     qbGrfg("G3",
         "Havg:FG40 HavgFgf: Ybp:R 400A/100R Irahr:EVPUYN GJC Vap:Oehfu Sver Qngr:10/15/2010 Gvzr:14:33 BCRA OHEA ORGJRRRA 100R NAQ NYRK CVXR/ F FVQR Nqqgy: BS EBNQ",
         "HAVG:FG40",
         "NQQE:R 400A & 100R",
+        "PVGL:EVPUYNAQ GJC",
         "PNYY:Oehfu Sver",
-        "VASB:BCRA OHEA ORGJRRRA 100R NAQ NYRK CVXR/ F FVQR BS EBNQ");
+        "QNGR:10/15/2010",
+        "GVZR:14:33",
+        "VASB:BCRA OHEA ORGJRRRA 100R NAQ NYRK CVXR/ F FVQR / BS EBNQ");
 
     qbGrfg("G4",
         "Havg:FG40 HavgFgf: Ybp:3551 A FE 9 Irahr:YNSNLR GJC GBZ JBBQ UBAQN Vap:Nppvqag CV Qngr:10/14/2010 Gvzr:07:43 HAXABJA 2 IRUF Nqqgy:",
         "HAVG:FG40",
         "NQQE:3551 A FE 9",
         "ZNQQE:3551 A VA 9",
+        "PVGL:YNSNLR GJC GBZ JBBQ UBAQN",
         "PNYY:Nppvqag CV",
+        "QNGR:10/14/2010",
+        "GVZR:07:43",
         "VASB:HAXABJA 2 IRUF");
-        
+
     qbGrfg("G5",
         "Havg:NZ45 HavgFgf: Ybp:124 QNEVA PG Irahr:EVPUYN GJC Vap:FRVMHER Qngr:10/29/2010 Gvzr:19:31 18 ZB/CBFF FRVMHER Nqqgy:",
         "HAVG:NZ45",
         "NQQE:124 QNEVA PG",
+        "PVGL:EVPUYNAQ GJC",
         "PNYY:FRVMHER",
+        "QNGR:10/29/2010",
+        "GVZR:19:31",
         "VASB:18 ZB/CBFF FRVMHER");
-    
+
   }
   
   @Grfg
@@ -80,7 +96,10 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "Fhowrpg:{RZNBCF} (62533) PNQ Cntr Havg:RZN1 Ybp:3727 A 200R Irahr:EVPUYN GJC Vap:Nppvqag CV Qngr:12/09/2010 Gvzr:10:31 HAXABJA  PNE IF CBY",
         "HAVG:RZN1",
         "NQQE:3727 A 200R",
+        "PVGL:EVPUYNAQ GJC",
         "PNYY:Nppvqag CV",
+        "QNGR:12/09/2010",
+        "GVZR:10:31",
         "VASB:HAXABJA  PNE IF CBY");
 
     qbGrfg("G2",
@@ -89,7 +108,10 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
         "HAVG:FG50",
         "NQQE:207 R ZNVA FG",
+        "PVGL:PURFGRESVRYQ",
         "PNYY:Sver",
+        "QNGR:12/09/2010",
+        "GVZR:12:00",
         "VASB:FGNAQ OL LB");
 
     qbGrfg("G3",
@@ -98,7 +120,10 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
         "HAVG:H20",
         "NQQE:53EQ FG & FPNGGRESVRYQ",
+        "PVGL:NAQREFBA",
         "PNYY:Nppvqag CV",
+        "QNGR:12/17/2010",
+        "GVZR:12:06",
         "VASB:ARPX CNVA/VA CEX");
 
     qbGrfg("G4",
@@ -107,7 +132,10 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
         "HAVG:EF35",
         "NQQE:2278 A 600J",
+        "PVGL:YNSNLRGGR GJC",
         "PNYY:SNYY",
+        "QNGR:12/22/2010",
+        "GVZR:07:03",
         "VASB:65LBZ SRYY BA VPR VA QEVIRJ");
 
   }
@@ -119,12 +147,15 @@ choyvp pynff VAZnqvfbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "Sjq: (62208) QVFCNGPU) Havg:NZ51 HavgFgf: Ybp:3363 R 200A Irahr:HAVBA GJC Vap:Nppvqag CV Qngr:12/04/2010 Gvzr:09:59 PNE UVG CBYR/PBAFPVBHF OHG UVG URNQ/ABG N",
         "HAVG:NZ51",
         "NQQE:3363 R 200A",
+        "PVGL:HAVBA GJC",
         "PNYY:Nppvqag CV",
+        "QNGR:12/04/2010",
+        "GVZR:09:59",
         "VASB:PNE UVG CBYR/PBAFPVBHF OHG UVG URNQ/ABG");
 
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
-    arj VAZnqvfbaPbhaglCnefreGrfg().trarengrGrfgf("G1", "HAVG NQQE NCG PNYY VASB");
+    arj VAZnqvfbaPbhaglCnefreGrfg().trarengrGrfgf("G1");
   }
 }
