7b1fb723a6bed5f2715b8871ce2d24c88aa34f57
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-12-01 23:58:33 +0000

    Fixed parsing problem with Delaware County, PA (A)

--- cadpage/src/net/anei/cadpage/parsers/PA/PADelawareCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PADelawareCountyAParser.java
@@ -1,7 +1,6 @@
 package net.anei.cadpage.parsers.PA;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Properties;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -9,11 +8,9 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class PADelawareCountyAParser extends FieldProgramParser {
   
-  private static final Pattern MARKER = Pattern.compile("^\\d\\d:\\d\\d\\*");
-  
   public PADelawareCountyAParser() {
-    super("DELAWARE COUNTY", "PA",
-           "CALL ADDR X INFO+");
+    super(CITY_CODES, "DELAWARE COUNTY", "PA",
+           "( TIME CALL ADDR/S X | UPDATE? ADDR/S1 X/Z? UNIT GPS? ) INFO+");
   }
   
   @Override
@@ -22,10 +19,55 @@ public class PADelawareCountyAParser extends FieldProgramParser {
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    Matcher match = MARKER.matcher(body);
-    if (!match.find()) return false;
-    body = body.substring(match.end()).trim();
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.length() > 0) body = "[" + subject + "] " + body;
     return parseFields(body.split("\\*"), data);
   }
+  
+  private class MyAddressField extends AddressField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf('@');
+      if (pt >= 0) {
+        data.strPlace = field.substring(pt+1).trim();
+        field = field.substring(0,pt).trim();
+      }
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " PLACE";
+    }
+  }
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.indexOf(",@");
+      if (pt >= 0) {
+        data.strPlace = field.substring(pt+2).trim();
+        field = field.substring(0,pt).trim();
+      }
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d", true);
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("UNIT")) return new UnitField("(?:[A-Z]+\\d+|\\d{3}[A-Z]?)(?:/.*)?", true);
+    if (name.equals("GPS")) return new GPSField("([-+]\\d+\\.\\d+ +[-+]\\d+\\.\\d+)\\b.*", true);
+    if (name.equals("UPDATE")) return new SkipField("\\[Update\\]");
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "ME",    "MEDIA",
+      "SP",    "SPRINGFIELD TWP",
+      "U",     "UPPER PROVIDENCE TWP",
+      "UP",    "UPPER PROVIDENCE TWP"
+  });
 }
