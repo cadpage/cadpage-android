c08855766feaf378e65b2514d6d15d5c6dddd2ae
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-12-05 18:03:07 +0000

    Fixed parsing problem with Snyder County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PASnyderCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PASnyderCountyParser.java
@@ -10,6 +10,40 @@ import net.anei.cadpage.parsers.dispatch.DispatchBParser;
 public class PASnyderCountyParser extends DispatchBParser {
   
   private static final Pattern CAD_PTN = Pattern.compile(" C[Aa][Dd][ :](?=\\d)");
+
+  public PASnyderCountyParser() {
+    super(CITY_LIST, "SNYDER COUNTY", "PA");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "SNYDER911@snydercounty.org,5705419455";
+  }
+  
+  @Override
+  protected boolean isPageMsg(String body) {
+    return body.startsWith("SNYDER911:") || body.startsWith("SYNDER911:") ||
+            body.contains(" Cad:");
+  }
+  
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    
+    if (subject.contains(">") && body.startsWith("SNYDER911:")) {
+      body = body.substring(0,10) + subject + ' ' + body.substring(10);
+    } else if (subject.equals("EVENT")  && body.startsWith("SNYDER911:")) {
+      body = body.substring(0,10) + "EVENT:" + body.substring(10);
+    }
+    
+    body = CAD_PTN.matcher(body).replaceAll(" Cad:");
+    body = body.replace(" MAP:", "Map:");
+    body = body.replace(" Map:Grids:", " Map: Grids:");
+    if (! super.parseMsg(body, data)) return false;
+    if (data.strCity.endsWith(" BORO")) {
+      data.strCity = data.strCity.substring(0, data.strCity.length()-5);
+    }
+    return true;
+  }
   
   private static final String[] CITY_LIST = new String[]{
     "BEAVERTOWN BORO",
@@ -35,31 +69,4 @@ public class PASnyderCountyParser extends DispatchBParser {
     "WEST BEAVER TWP",
     "WEST PERRY TWP"
  };
-
-  public PASnyderCountyParser() {
-    super(CITY_LIST, "SNYDER COUNTY", "PA");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "5705419455";
-  }
-  
-  @Override
-  protected boolean isPageMsg(String body) {
-    return body.startsWith("SNYDER911:") || body.startsWith("SYNDER911:") ||
-            body.contains(" Cad:");
-  }
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    body = CAD_PTN.matcher(body).replaceAll(" Cad:");
-    body = body.replace(" MAP:", "Map:");
-    body = body.replace(" Map:Grids:", " Map: Grids:");
-    if (! super.parseMsg(body, data)) return false;
-    if (data.strCity.endsWith(" BORO")) {
-      data.strCity = data.strCity.substring(0, data.strCity.length()-5);
-    }
-    return true;
-  }
 }
