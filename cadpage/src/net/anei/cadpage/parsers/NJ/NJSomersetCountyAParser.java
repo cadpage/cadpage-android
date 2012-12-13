19bd397701270a0b84cf522f40c5a9673f812c4e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-12-13 01:44:09 +0000

    Fixed parsing problem with SOmerset COunty, NJ (A)

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJSomersetCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJSomersetCountyAParser.java
@@ -22,12 +22,14 @@ public class NJSomersetCountyAParser extends MsgParser {
       "HOPEWEL", "HOPEWELL TWP",
       "HILLSBO", "HILLSBOROUGH TWP",
       "PRINCET", "PRINCETON TWP",
+      "SOMERVI", "SOMERVILLE",
       "SOUTH B", "SOUTH BOUND BROOK"
   });
   
-  private static final Pattern MARKER = Pattern.compile("^(?:(?:/ [A-Z0-9 ]* / )?(?:[A-Za-z]+:)?(?:[[^:]*]:)?)?([^: ]+):(\\d{8}) *:(\\d\\d/\\d\\d/\\d\\d\\d\\d) (\\d\\d:\\d\\d:\\d\\d):");
+  private static final Pattern MARKER = Pattern.compile("^(?:(?:/ [A-Z0-9 ]* / )?(?:[A-Za-z]+:)?(?:[[^:]*]:)?)?(?:([^: ]+):)?(\\d{8}) *:(\\d\\d/\\d\\d/\\d\\d\\d\\d) (\\d\\d:\\d\\d:\\d\\d):");
   private static final Pattern MASTER = Pattern.compile("(.*?): ([A-Z ]+)-(?! )(?:(.*) / +)?([^\\(]*)(?:\\((.*)\\))?");
   private static final Pattern ROUTE_HWY_PTN = Pattern.compile("\\b(STATE|COUNTY|US) (?:ROUTE|ROAD|HWY)(?: NO)? (\\d+)(?: HWY)?\\b");
+  private static final Pattern APT_PTN = Pattern.compile(" +(?:APT|FL|FLR|BLDG) +[-A-Z0-9]$");
   
   public NJSomersetCountyAParser() {
     super("SOMERSET COUNTY", "NJ");
@@ -43,7 +45,7 @@ public class NJSomersetCountyAParser extends MsgParser {
 
     Matcher match = MARKER.matcher(body);
     if (!match.find()) return false;
-    data.strSource = match.group(1);
+    data.strSource = getOptGroup(match.group(1));
     data.strCallId = match.group(2);
     data.strDate = match.group(3);
     data.strTime = match.group(4);
@@ -65,6 +67,12 @@ public class NJSomersetCountyAParser extends MsgParser {
       sAddr = sAddr.substring(0,match.start()) +
               match.group(1).substring(0,2) + " " + match.group(2) +
               sAddr.substring(match.end());
+    }
+    match = APT_PTN.matcher(sAddr);
+    if (match.find()) {
+      data.strApt = match.group();
+      if (data.strApt.startsWith("APT ")) data.strApt = data.strApt.substring(4).trim();
+      body = body.substring(0,match.start());
     }
     parseAddress(sAddr, data);
     data.strAddress = ROUTE_HWY_PTN.matcher(data.strAddress).replaceFirst("$1 $2");
