7335549339b931ac7ecaf97fb3074e512fe2c107
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-12-19 03:27:15 +0000

    Fixed parsing problem with Linn County, OR

--- cadpage/src/net/anei/cadpage/parsers/OR/ORLinnCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORLinnCountyParser.java
@@ -1,6 +1,9 @@
 package net.anei.cadpage.parsers.OR;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -51,13 +54,53 @@ public class ORLinnCountyParser extends FieldProgramParser {
     }
   }
   
+  private static final Pattern MAP_PTN = Pattern.compile("(\\d{4}[A-Z]?)(\\d\\d\\*?[A-Z]?\\d\\d)");
+  private class MyMapField extends MapField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (field.length() == 0) return true;
+      Matcher match = MAP_PTN.matcher(field);
+      if (!match.matches()) return false;
+      data.strMap = match.group(1);
+      data.strUnit = match.group(2);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "MAP UNIT";
+    }
+  }
+  
+  private static final Pattern UNIT_PTN = Pattern.compile("[A-Z0-9 ]+");
+  private class MyUnitField extends UnitField {
+    MyUnitField() {
+      setPattern(UNIT_PTN, true);
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      data.strUnit = append(data.strUnit, " ", field);
+    }
+  }
+  
   @Override
   public Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
     if (name.equals("DATE")) return new MyDateField();
     if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
-    if (name.equals("MAP")) return new MapField("(?:\\d{4}[A-Z]?\\d\\d\\*?[A-Z]?\\d\\d|)", true);
-    if (name.equals("UNIT")) return new UnitField("[A-Z0-9 ]+");
+    if (name.equals("MAP")) return new MyMapField();
+    if (name.equals("UNIT")) return new MyUnitField();
     return super.getField(name);
   }
   
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORLinnCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORLinnCountyParserTest.java
@@ -65,8 +65,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "PNYY:BGU FGEPG SVER",
         "NQQE:40560 ZPDHRRA QE",
         "K:UJL 228 &",
-        "ZNC:6654O23*21",
-        "HAVG:R23 CG23 FGA23 FS Z21 N201 R21 R25 FGA21 GBAR JG21 1FG R22 FGA22 JG22 FGA24");
+        "ZNC:6654O",
+        "HAVG:23*21 R23 CG23 FGA23 FS Z21 N201 R21 R25 FGA21 GBAR JG21 1FG R22 FGA22 JG22 FGA24");
 
     qbGrfg("G2",
         "HAPBAP/SNVAGVAT/ 300 J UBYYRL EQ Fjrrg Ubzr FGBAROEBBX NCNEGZRAGF/ 61602221/ S",
@@ -74,8 +74,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:300 J UBYYRL EQ",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:FGBAROEBBX NCNEGZRAGF",
-        "ZNC:61602221",
-        "HAVG:S");
+        "ZNC:6160",
+        "HAVG:2221 S");
 
     qbGrfg("G3",
         "OERNGU CEBO/ 5050 ZBHAGNVA SVE FG Fjrrg Ubzr JVYRL PERRX PBZZHAVGL/ 59662221/",
@@ -83,38 +83,44 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:5050 ZBHAGNVA SVE FG",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:JVYRL PERRX PBZZHAVGL",
-        "ZNC:59662221");
+        "ZNC:5966",
+        "HAVG:2221");
 
     qbGrfg("G4",
         "FVPX CREFBA/ 1048 26GU PG Fjrrg Ubzr/ 60632221/",
         "PNYY:FVPX CREFBA",
         "NQQE:1048 26GU PG",
         "PVGL:Fjrrg Ubzr",
-        "ZNC:60632221");
+        "ZNC:6063",
+        "HAVG:2221");
 
     qbGrfg("G5",
         "FVPX CREFBA/ 24490 JRFG OEHFU PERRX EQ Yvaa Pbhagl/ 705023*21/",
         "PNYY:FVPX CREFBA",
         "NQQE:24490 JRFG OEHFU PERRX EQ",
-        "ZNC:705023*21");
+        "ZNC:7050",
+        "HAVG:23*21");
 
     qbGrfg("G6",
         "FVPX CREFBA/ 45265 FHAALFVQR EQ Yvaa Pbhagl/ 547021*21/",
         "PNYY:FVPX CREFBA",
         "NQQE:45265 FHAALFVQR EQ",
-        "ZNC:547021*21");
+        "ZNC:5470",
+        "HAVG:21*21");
 
     qbGrfg("G7",
         "GENHZN VAW/ 43813 JVYRL PERRX QE Yvaa Pbhagl/ 587022*21/",
         "PNYY:GENHZN VAW",
         "NQQE:43813 JVYRL PERRX QE",
-        "ZNC:587022*21");
+        "ZNC:5870",
+        "HAVG:22*21");
 
     qbGrfg("G8",
         "FGEBXR/PIN/ 43786 JVYRL PERRX QE Yvaa Pbhagl/ 587022*21/",
         "PNYY:FGEBXR/PIN",
         "NQQE:43786 JVYRL PERRX QE",
-        "ZNC:587022*21");
+        "ZNC:5870",
+        "HAVG:22*21");
 
     qbGrfg("G9",
         "FVPX CREFBA/ 1177 1FG NI Fjrrg Ubzr/ 60602221/",
@@ -122,7 +128,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:1177 1FG NI",
         "ZNQQE:1177 1FG NIR",
         "PVGL:Fjrrg Ubzr",
-        "ZNC:60602221");
+        "ZNC:6060",
+        "HAVG:2221");
 
     qbGrfg("G10",
         "HAX CEBO (zna qbja)/ 1228 4GU NI Fjrrg Ubzr/ 60602221/",
@@ -130,7 +137,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:1228 4GU NI",
         "ZNQQE:1228 4GU NIR",
         "PVGL:Fjrrg Ubzr",
-        "ZNC:60602221");
+        "ZNC:6060",
+        "HAVG:2221");
 
     qbGrfg("G11",
         "B Q/VATRFGVBA/CBVFBAVAT/ 1131 29GU NI #P2 Fjrrg Ubzr/ 60632221/",
@@ -139,7 +147,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "ZNQQE:1131 29GU NIR",
         "NCG:P2",
         "PVGL:Fjrrg Ubzr",
-        "ZNC:60632221");
+        "ZNC:6063",
+        "HAVG:2221");
 
     qbGrfg("G12",
         "QVNORGVP CEBO/ 950 ANAQVAN FG Fjrrg Ubzr GJVA BNXF PNER PRAGRE/ 60612221/",
@@ -147,19 +156,22 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:950 ANAQVAN FG",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:GJVA BNXF PNER PRAGRE",
-        "ZNC:60612221");
+        "ZNC:6061",
+        "HAVG:2221");
 
     qbGrfg("G13",
         "OERNGU CEBO/ 43156 ABEGU EVIRE QE Yvaa Pbhagl/ 586622*21/",
         "PNYY:OERNGU CEBO",
         "NQQE:43156 ABEGU EVIRE QE",
-        "ZNC:586622*21");
+        "ZNC:5866",
+        "HAVG:22*21");
 
     qbGrfg("G14",
         "OERNGU CEBO/ 38110 UJL 228 Yvaa Pbhagl/ 6649N23*21/",
         "PNYY:OERNGU CEBO",
         "NQQE:38110 UJL 228",
-        "ZNC:6649N23*21");
+        "ZNC:6649N",
+        "HAVG:23*21");
 
     qbGrfg("G15",
         "NYNEZ-SVER/ 1400 18GU NI Fjrrg Ubzr YVAZNE VAQHFGEVRF YYP/ 60612221/",
@@ -168,7 +180,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "ZNQQE:1400 18GU NIR",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:YVAZNE VAQHFGEVRF YYP",
-        "ZNC:60612221");
+        "ZNC:6061",
+        "HAVG:2221");
 
     qbGrfg("G16",
         "NYNEZ-SVER/1400 18GU NI Fjrrg Ubzr YVAZNE VAQHFGEVRF YYP/UJL 20 & GNZNENPX FG/60612221/ R21 FGA21/Bireurnq qbbe ~ jngre sybj ~ mbar 3 ~ evfre 1",
@@ -178,8 +191,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "PVGL:Fjrrg Ubzr",
         "CYNPR:YVAZNE VAQHFGEVRF YYP",
         "K:UJL 20 & GNZNENPX FG",
-        "ZNC:60612221",
-        "HAVG:R21 FGA21",
+        "ZNC:6061",
+        "HAVG:2221 R21 FGA21",
         "VASB:Bireurnq qbbe ~ jngre sybj ~ mbar 3 ~ evfre 1");
 
     qbGrfg("G17",
@@ -188,7 +201,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:1099 YBAT FG",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:FJRRG UBZR SVER QRCNEGZRAG",
-        "ZNC:60612221");
+        "ZNC:6061",
+        "HAVG:2221");
 
     qbGrfg("G18",
         "PURFG CNVA/ 1530 GNZNENPX FG #62 Fjrrg Ubzr ZG FUNQBJ GENVYRE CNEX/ 60612221/",
@@ -197,14 +211,16 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NCG:62",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:ZG FUNQBJ GENVYRE CNEX",
-        "ZNC:60612221");
+        "ZNC:6061",
+        "HAVG:2221");
 
     qbGrfg("G19",
         "PNEQVNP/ERFC NEERFG/ 930 CBCYNE FG Fjrrg Ubzr/ 60602221/",
         "PNYY:PNEQVNP/ERFC NEERFG",
         "NQQE:930 CBCYNE FG",
         "PVGL:Fjrrg Ubzr",
-        "ZNC:60602221");
+        "ZNC:6060",
+        "HAVG:2221");
 
     qbGrfg("G20",
         "PNEQVNP/ERFC NEERFG/930 CBCYNE FG Fjrrg Ubzr/9GU NI & 11GU NI/60602221/ R21 ERFPHR Z21/arvtuobe pnzr bire naq sbhaq ure",
@@ -212,8 +228,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:930 CBCYNE FG",
         "PVGL:Fjrrg Ubzr",
         "K:9GU NI & 11GU NI",
-        "ZNC:60602221",
-        "HAVG:R21 ERFPHR Z21",
+        "ZNC:6060",
+        "HAVG:2221 R21 ERFPHR Z21",
         "VASB:arvtuobe pnzr bire naq sbhaq ure");
 
     qbGrfg("G21",
@@ -223,7 +239,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NCG:218",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:ZG FUNQBJ GENVYRE CNEX",
-        "ZNC:60612221");
+        "ZNC:6061",
+        "HAVG:2221");
 
     qbGrfg("G22",
         "GENHZN VAW/ 3137 FNAGVNZ UJL FC 11 Fjrrg Ubzr GENVYRE IVYYN/ 60632221/",
@@ -232,14 +249,16 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NCG:11",
         "PVGL:Fjrrg Ubzr",
         "CYNPR:GENVYRE IVYYN",
-        "ZNC:60632221");
+        "ZNC:6063",
+        "HAVG:2221");
 
     qbGrfg("G23",
         "VPBZ/400 abgvsvpngvba,UBHFR SVER/ 1173 J FUREZNA FG Yronaba/ 4142N3131/",
         "PNYY:UBHFR SVER",
         "NQQE:1173 J FUREZNA FG",
         "PVGL:Yronaba",
-        "ZNC:4142N3131");
+        "ZNC:4142N",
+        "HAVG:3131");
 
     qbGrfg("G24",
         "SEZ:yvaa911@yvaafurevss.bet\a" +
@@ -263,21 +282,24 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "PVGL:Nyonal",
         "CYNPR:YVAA PB FUREVSSF BSSVPR",
         "K:WNVY",
-        "ZNC:282511F11");
+        "ZNC:2825",
+        "HAVG:11F11");
 
     qbGrfg("G2",
         "[VPBZ/400 abgvsvpngvba] BGU FGEPG SVER/ 36149 EVIREFVQR QE VAGREFRPGA Yvaa Pbhagl  /  BNXIVYYR EQ/ 30181111/\a",
         "PNYY:BGU FGEPG SVER",
         "NQQE:36149 EVIREFVQR QE",
         "K:BNXIVYYR EQ",
-        "ZNC:30181111");
+        "ZNC:3018",
+        "HAVG:1111");
 
     qbGrfg("G3",
         "[VPBZ/400 abgvsvpngvba] NYNEZ-SVER/ 618 FJ JNFUVATGBA FG Nyonal/ 282411A11/\a",
         "PNYY:NYNEZ-SVER",
         "NQQE:618 FJ JNFUVATGBA FG",
         "PVGL:Nyonal",
-        "ZNC:282411A11");
+        "ZNC:2824",
+        "HAVG:11A11");
 
     qbGrfg("G4",
         "[VPBZ/400 abgvsvpngvba] NYNEZ-SVER/ 525 FJ 25GU NI Nyonal BERTBA SERRMR QEL CYNAG #2/ 30241212/\a",
@@ -286,7 +308,8 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "ZNQQE:525 FJ 25GU NIR",
         "PVGL:Nyonal",
         "CYNPR:BERTBA SERRMR QEL CYNAG #2",
-        "ZNC:30241212");
+        "ZNC:3024",
+        "HAVG:1212");
 
     qbGrfg("G5",
         "[VPBZ/400 abgvsvpngvba] NYNEZ-SVER/ 1929 FR TENAQ CENVEVR EQ Nyonal IVYYNF NG PBHEGLNEQ/ 30261212/\a",
@@ -294,21 +317,24 @@ choyvp pynff BEYvaaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:1929 FR TENAQ CENVEVR EQ",
         "PVGL:Nyonal",
         "CYNPR:IVYYNF NG PBHEGLNEQ",
-        "ZNC:30261212");
+        "ZNC:3026",
+        "HAVG:1212");
 
     qbGrfg("G6",
         "[VPBZ/400 abgvsvpngvba] GENS PBYYVFVBA/ 3700 AR XABK OHGGR EQ Yvaa Pbhagl YVAA PB SNVETEBHAQF NAQ RKCB/ 26291313/\a",
         "PNYY:GENS PBYYVFVBA",
         "NQQE:3700 AR XABK OHGGR EQ",
         "CYNPR:YVAA PB SNVETEBHAQF NAQ RKCB",
-        "ZNC:26291313");
+        "ZNC:2629",
+        "HAVG:1313");
 
     qbGrfg("G7",
         "[VPBZ/400 abgvsvpngvba] PNEQVNP/ERFC NEERFG/ 2801 FJ HZNGVYYN FG Nyonal/ 30231212/\a",
         "PNYY:PNEQVNP/ERFC NEERFG",
         "NQQE:2801 FJ HZNGVYYN FG",
         "PVGL:Nyonal",
-        "ZNC:30231212");
+        "ZNC:3023",
+        "HAVG:1212");
 
     qbGrfg("G8",
         "[VPBZ/400 abgvsvpngvba] N501 Erpnyy Nynez/ GENS PBYYVFVBA/25010 F V5 Yvaa Pbhagl ZC212 F V5//iruvpyr jnf geniryvat fbhgu, chg uvf oyvaxre ba gb gur\a",
