a7e9d65cd8262be480b6a9a259b0f5c4c4edc403
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-01-03 02:20:20 +0000

    Fixed parsing problem with Rock Island County, IL

--- cadpage/src/net/anei/cadpage/parsers/IL/ILRockIslandCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/IL/ILRockIslandCountyParser.java
@@ -5,44 +5,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 
-public class ILRockIslandCountyParser extends MsgParser {
+public class ILRockIslandCountyParser extends DispatchOSSIParser {
   
-  private static final Pattern MASTER = 
-      Pattern.compile("(?:@([A-Z]+): )?(\\d+):([\\w: ]*);([^;]+?);([^;]+?)(?:;([A-Z]+))?");
+  private static final Pattern MARKER = Pattern.compile("^\\d+:");
   
   public ILRockIslandCountyParser() {
-    super("ROCK ISLAND COUNTY", "IL");
+    super(CITY_CODES, "ROCK ISLAND COUNTY", "IL",
+          "ID:FYI ADDR CALL CITY!");
   }
   
   @Override
   public String getFilter() {
-    return "cad@ricoetsb.org";
+    return "CAD@ricoetsb.org";
   }
 
   @Override
   protected boolean parseMsg(String body, Data data) {
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    data.strSource = getOptGroup(match.group(1));
-    data.strCallId = match.group(2);
-    String type = match.group(3).trim();
-    String sAddr;
-    if (type.equals("CANCEL")) {
-      data.strCall = type + " - " + match.group(5).trim();
-      sAddr = match.group(4).trim();
-    } else {
-      data.strCall = match.group(4).trim();
-      sAddr = match.group(5).trim();
-    }
-    String city = match.group(6);
-    if (city != null) data.strCity = convertCodes(city, CITY_CODES);
-      
-    parseAddress(sAddr, data);
-    return true;
+    Matcher match = MARKER.matcher(body);
+    if (!match.find()) return false;
+    body = match.group() + "CAD:" + body.substring(match.end()).trim();
+    return super.parseMsg(body, data);
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/IL/ILRockIslandCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/IL/ILRockIslandCountyParserTest.java
@@ -19,6 +19,13 @@ F: Z:68:PNAPRY;2932 EBQZNA NI; EVN\a
 F: Z:69:Hcqngr: ;SVER ZHGHNY NVQ;2932 EBQZNA NI\a
 @EVSQ: 17437:;ZRQVPNY PNYY;767 30GU FG;EV
 
+Ebpx Vfynaq Pbhagl, VY
+Pbagnpg: Qneva Xrvgu <qneva.xrvgu@ubgznvy.pbz>
+Fraqre: PNQ@evpbrgfo.bet
+
+F: Z:91:SLV: ;3130 9GU FG;FGEHPGHER SVER/TRARENY;EV 
+F: Z:90:Hcqngr: ;2626 EBQZNA NI;FGEHPGHER SVER/TRARENY;EVN 
+
 */
 
 choyvp pynff VYEbpxVfynaqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
@@ -31,72 +38,22 @@ choyvp pynff VYEbpxVfynaqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   choyvp ibvq grfgCnefre() {
 
     qbGrfg("G1",
-        "@EVSQ: 15221:;PENFU CREFBANY VAWHEVRF;24GU FG/18GU NI",
-        "FEP:EVSQ",
-        "VQ:15221",
-        "PNYY:PENFU CREFBANY VAWHEVRF",
-        "NQQE:24GU FG & 18GU NI",
-        "ZNQQE:24GU FG & 18GU NIR");
-
-    qbGrfg("G2",
-        "@EVSQ: 15226:;SVER BGURE;1323 14GU FG",
-        "FEP:EVSQ",
-        "VQ:15226",
-        "PNYY:SVER BGURE",
-        "NQQE:1323 14GU FG");
-
-    qbGrfg("G3",
-        "@EVSQ: 15227:;SVER NYNEZ;3605 11GU NI",
-        "FEP:EVSQ",
-        "VQ:15227",
-        "PNYY:SVER NYNEZ",
-        "NQQE:3605 11GU NI",
-        "ZNQQE:3605 11GU NIR");
-
-    qbGrfg("G4",
-        "@EVSQ: 16193:;ZRQVPNY PNYY;2300-46 79GU NI J",
-        "FEP:EVSQ",
-        "VQ:16193",
-        "PNYY:ZRQVPNY PNYY",
-        "NQQE:2300-46 79GU NI J",
-        "ZNQQE:2300 79GU NIR J");
-  }
-  
-  @Grfg
-  choyvp ibvq grfgCnefre2() {
-
-    qbGrfg("G1",
-        "F: Z:67:SLV: ;FGEHPGHER SVER/TRARENY;2932 EBQZNA NI\a",
-        "VQ:67",
+        "F: Z:91:SLV: ;3130 9GU FG;FGEHPGHER SVER/TRARENY;EV ",
+        "VQ:91",
+        "NQQE:3130 9GU FG",
         "PNYY:FGEHPGHER SVER/TRARENY",
-        "NQQE:2932 EBQZNA NI",
-        "ZNQQE:2932 EBQZNA NIR");
-
-    qbGrfg("G2",
-        "F: Z:68:PNAPRY;2932 EBQZNA NI; EVN\a",
-        "VQ:68",
-        "PNYY:PNAPRY - EVN",
-        "NQQE:2932 EBQZNA NI",
-        "ZNQQE:2932 EBQZNA NIR");
-
-    qbGrfg("G3",
-        "F: Z:69:Hcqngr: ;SVER ZHGHNY NVQ;2932 EBQZNA NI\a",
-        "VQ:69",
-        "PNYY:SVER ZHGHNY NVQ",
-        "NQQE:2932 EBQZNA NI",
-        "ZNQQE:2932 EBQZNA NIR");
-
-    qbGrfg("G4",
-        "@EVSQ: 17437:;ZRQVPNY PNYY;767 30GU FG;EV",
-        "FEP:EVSQ",
-        "VQ:17437",
-        "PNYY:ZRQVPNY PNYY",
-        "NQQE:767 30GU FG",
         "PVGL:EBPX VFYNAQ");
 
+    qbGrfg("G2",
+        "F: Z:90:Hcqngr: ;2626 EBQZNA NI;FGEHPGHER SVER/TRARENY;EVN ",
+        "VQ:90",
+        "NQQE:2626 EBQZNA NI",
+        "ZNQQE:2626 EBQZNA NIR",
+        "PNYY:FGEHPGHER SVER/TRARENY",
+        "PVGL:EBPX VFYNAQ NEFRANY");
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
-    arj VYEbpxVfynaqPbhaglCnefreGrfg().trarengrGrfgf("G1", "FEP VQ PNYY NQQE PVGL");
+    arj VYEbpxVfynaqPbhaglCnefreGrfg().trarengrGrfgf("G1", "FEP VQ NQQE PNYY PVGL");
   }
 }
