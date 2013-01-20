6c9d068742a20a3ef597d8dacfbc04d78d7a852a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-01-20 23:37:20 +0000

    Fixed parsing problem with Erie County, PA (A)

--- cadpage/src/net/anei/cadpage/parsers/PA/PAErieCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PAErieCountyAParser.java
@@ -1,6 +1,7 @@
 package net.anei.cadpage.parsers.PA;
 
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -11,6 +12,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
 public class PAErieCountyAParser extends DispatchB2Parser {
   
   private static final Pattern MARKER2 = Pattern.compile("^[0-9A-Z]+ ?>");
+  private static final Pattern CITY_SUFFIX = Pattern.compile("^(?:BORO|VILLAGE|TWP)\\b *");
  
   public PAErieCountyAParser() {
     super(PAErieCountyParser.CITY_LIST, "ERIE COUNTY", "PA");
@@ -44,6 +46,8 @@ public class PAErieCountyAParser extends DispatchB2Parser {
     
     boolean result = super.parseMsg(body, data);
     if (result) {
+      Matcher match = CITY_SUFFIX.matcher(data.strName);
+      if (match.find()) data.strName = data.strName.substring(match.end());
       result =  
           (data.strCross.length() > 0 || 
            data.strCallId.length() > 0);
@@ -51,7 +55,6 @@ public class PAErieCountyAParser extends DispatchB2Parser {
         int pt = body.indexOf('>');
         result = (pt >= 0 && pt <= 20);
       }
-      
     }
     if (!result && data.strSource.equals("ERIE911")) {
       data.parseGeneralAlert(body.trim());
