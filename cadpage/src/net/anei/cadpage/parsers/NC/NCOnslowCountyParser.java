fc6520038aafbcdfb5f3358efd73d63fe4aac2a5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-01-22 21:31:24 +0000

    Fixed more parsing problems with Onslow County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCOnslowCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCOnslowCountyParser.java
@@ -1,6 +1,7 @@
 package net.anei.cadpage.parsers.NC;
 
 import java.util.Properties;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
@@ -10,13 +11,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 public class NCOnslowCountyParser extends DispatchOSSIParser {
   
-  private static final Pattern CODE_PTN = Pattern.compile("\\d{1,2}[A-Z]\\d{1,2}[A-Z]?"); 
+  private static final Pattern CODE_PTN = Pattern.compile("\\d{1,2}[A-Z]\\d{1,2}[A-Za-z]?"); 
   
   private String selectValue;
   
   public NCOnslowCountyParser() {
     super(CITY_CODES, "ONSLOW COUNTY", "NC",
-           "( ASSIST ADDR CITY DIST? INFO+ | ADDR ( SELECT/EMS PLACE? UNIT UNIT? CALL! CODE | PLACE? CALL SRC UNIT2! ) )");
+           "( ASSIST ADDR CITY DIST? INFO+ | ADDR ( SELECT/EMS APT? PLACE? UNIT CALL! CODE | PLACE? CALL SRC UNIT! ) )");
   }
   
   @Override
@@ -50,14 +51,54 @@ public class NCOnslowCountyParser extends DispatchOSSIParser {
     }
   }
   
+  private static final Pattern APT_PTN = Pattern.compile("(?:LOT|APT|UNIT|SUITE) *(.*)|\\d+");
+  private class MyAptField extends AptField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      
+      // See if we match the master apartment pattern
+      Matcher match = APT_PTN.matcher(field);
+      if (!match.matches()) return false;
+      
+      // Match succeeded, but there are two possible outcomes.  If this had
+      // some kind of apartment prefix, take it as gospel
+      String apt = match.group(1);
+      if (apt != null) {
+        data.strApt = apt;
+        return true;
+      }
+      
+      // Otherwise is is a numeric token that looks a lot like a unit field.
+      // The only way we can determine if this is an apartment
+      // field is by looking at the next two fields to see if one of them looks
+      // like a unit field.  If not, then we must be the unit field
+      boolean found = false;
+      for (int ndx = 1; ndx <= 2; ndx++) {
+        String fld = getRelativeField(ndx);
+        if (UNIT_PTN.matcher(fld).matches()) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) return false;
+      data.strApt = field;
+      return true;
+    }
+  }
+  
+  private static final Pattern UNIT_PTN = Pattern.compile("\\d{1,2}[A-Z]?");
   private class MyUnitField extends UnitField {
     public MyUnitField() {
-      setPattern(Pattern.compile("(?:UNIT +)?\\d{2,4}[A-Z]?"), true);
+      setPattern(UNIT_PTN, true);
     }
     
     @Override
     public void parse(String field, Data data) {
-      if (field.startsWith("UNIT ")) field = field.substring(5).trim();
       data.strUnit = append(data.strUnit, "-", field);
     }
   }
@@ -73,9 +114,9 @@ public class NCOnslowCountyParser extends DispatchOSSIParser {
     if (name.equals("ASSIST")) return new AssistField();
     if (name.equals("DIST")) return new PlaceField("DIST:.*");
     if (name.equals("SRC")) return new SourceField("[A-Z]{1,2}FD", true);
+    if (name.equals("APT")) return new MyAptField();
     if (name.equals("UNIT")) return new MyUnitField();
     if (name.equals("CODE")) return new MyCodeField();
-    if (name.equals("UNIT2")) return new UnitField("\\d{1,2}[A-Z]", true);
     return super.getField(name);
   }
   
