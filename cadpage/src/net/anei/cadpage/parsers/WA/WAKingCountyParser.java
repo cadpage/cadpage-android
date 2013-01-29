88c8ccdf1b45e8e9da6791dc70e6da30a1152cc5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-01-29 23:32:12 +0000

    Added King County, WA (B) location

--- cadpage/src/net/anei/cadpage/parsers/WA/WAKingCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WA/WAKingCountyParser.java
@@ -1,42 +1,13 @@
 package net.anei.cadpage.parsers.WA;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
-
-
-public class WAKingCountyParser extends MsgParser {
+/**
+ * King County, WA
+ */
+public class WAKingCountyParser extends GroupBestParser {
   
   public WAKingCountyParser() {
-    super("KING COUNTY", "WA");
-  }
-  
-  @Override
-  public String getFilter() {
-    return "CAD@bellevuewa.gov";
-  }
-
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (body.length() < 102) return false;
-    if (body.charAt(50) != '#') return false;
-    if (!isStartField(body, 61)) return false;
-    int endCity = isStartField(body, 91) ? 61 : 96;
-    if (!isStartField(body, endCity+30)) return false;
-    if (!isStartField(body, endCity+40)) return false;
-    
-    parseAddress(substring(body, 0, 50), data);
-    data.strApt = substring(body, 51, 60);
-    data.strCity = substring(body, 61, endCity);
-    data.strCall = substring(body, endCity, endCity+29);
-    data.strUnit = substring(body, endCity+40);
-    return true;
-  }
-  
-  private boolean isStartField(String body, int pt) {
-    if (body.length() <= pt) return false;
-    if (body.charAt(pt) == ' ') return false;
-    if (body.charAt(pt-1) != ' ') return false;
-    return true;
+    super(new WAKingCountyAParser(), new WAKingCountyBParser());
   }
 }
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/WA/WAKingCountyBParserTest.java
@@ -0,0 +1,68 @@
+cnpxntr arg.narv.pnqcntr.cnefref.JN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Xvat Pbhagl, JN
+Pbagnpg: "Qna Pbirl" <qna@qnapbirl.arg>
+Flfgrz: Nrtvf
+
+PNQ||44530 Fr 148gu Fg # XVAT PBHAGL Nvq - Rzretrapl SGNP1 Y87,N88 
+PNQ||39664 Fr Fcehpr Fg # Fgehpgher Sver - Erfvqragvny SGNP2 281FG,R281,FDFBC,Y87,R271,O71FG
+PNQ||51699 RO90 # XVAT PBHAGL ZIN - Erfphr SGNP3 Z3,N88,87FG,R291,R2
+PNQ||Nqqe: 79500 Jo90 Ceboyrz: ZIN - Nvq Rzretrapl Havgf: N291,Y87,XGNVQ,N291,FCSQ1,R291, [Trbtencuvp Nern: 79500 JO90] [Zrqv
+PNQ||410 AR 6gu Fg # ABEGU ORAQ Zrqvp SGNP1 N88,Z3,Y87
+
+*/
+
+choyvp pynff JNXvatPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp JNXvatPbhaglOCnefreGrfg() {
+    frgCnefre(arj JNXvatPbhaglOCnefre(), "XVAT PBHAGL", "JN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgQnaPbirl() {
+
+    qbGrfg("G1",
+        "PNQ||44530 Fr 148gu Fg # XVAT PBHAGL Nvq - Rzretrapl SGNP1 Y87,N88 ",
+        "NQQE:44530 Fr 148gu Fg",
+        "PNYY:XVAT PBHAGL Nvq - Rzretrapl",
+        "PU:SGNP1",
+        "HAVG:Y87,N88");
+
+    qbGrfg("G2",
+        "PNQ||39664 Fr Fcehpr Fg # Fgehpgher Sver - Erfvqragvny SGNP2 281FG,R281,FDFBC,Y87,R271,O71FG",
+        "NQQE:39664 Fr Fcehpr Fg",
+        "PNYY:Fgehpgher Sver - Erfvqragvny",
+        "PU:SGNP2",
+        "HAVG:281FG,R281,FDFBC,Y87,R271,O71FG");
+
+    qbGrfg("G3",
+        "PNQ||51699 RO90 # XVAT PBHAGL ZIN - Erfphr SGNP3 Z3,N88,87FG,R291,R2",
+        "NQQE:51699 RO90",
+        "PNYY:XVAT PBHAGL ZIN - Erfphr",
+        "PU:SGNP3",
+        "HAVG:Z3,N88,87FG,R291,R2");
+
+    qbGrfg("G4",
+        "PNQ||Nqqe: 79500 Jo90 Ceboyrz: ZIN - Nvq Rzretrapl Havgf: N291,Y87,XGNVQ,N291,FCSQ1,R291, [Trbtencuvp Nern: 79500 JO90] [Zrqv",
+        "NQQE:79500 Jo90",
+        "PNYY:ZIN - Nvq",
+        "HAVG:N291,Y87,XGNVQ,N291,FCSQ1,R291");
+
+    qbGrfg("G5",
+        "PNQ||410 AR 6gu Fg # ABEGU ORAQ Zrqvp SGNP1 N88,Z3,Y87",
+        "NQQE:410 AR 6gu Fg",
+        "PNYY:ABEGU ORAQ Zrqvp",
+        "PU:SGNP1",
+        "HAVG:N88,Z3,Y87");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj JNXvatPbhaglOCnefreGrfg().trarengrGrfgf("G1", "NQQE NCG PNYY PU HAVG");
+  }
+}
