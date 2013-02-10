598db3a74ca502499dd2da647d8029c787014ed1
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-02-10 02:12:50 +0000

    Added test class

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORJosephineCountyParserTest.java
@@ -0,0 +1,171 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BE;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*    
+Wbfrcuvar Pbhagl, BE
+Pbagnpg: XRIVA UNEEVF <xri911re@zfa.pbz>
+Fraqre: Qvfcngpu@Cnpvsvp.pbz
+
+F:!  Z:Ryrpgevpny Sver: 613 FJ JRFGREA NIR O , Tenagf Cnff:  \a1/6/2013 12:29:06 NZ: 208791: 7317, 7553, 7400: FZRYYVAT JUNG FZRYYF YVXR NA RYRPGEVPNY CEBOYRZ\aUNMR VAFVQR..
+F:!  Z:Pbzzrepvny Sver: 115 FJ U FG, Tenagf Cnff:  \a1/5/2013 9:17:02 CZ: 208791: 7307, 7309, 7328, SN1, 7501, 7553, 161, 7531, 7353: NHQVOYR 2 UVGF\aHCFGNVEF ZBGVBA \aEC CRAQVAT \a*EC:  OHQ UBCFB..
+F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 5098 ARJ UBCR EQ, Tenagf Cnff:  \a1/6/2013 12:09:35 NZ: 201841: 7551, Z1: WBFU ZNVA 541-660-9673\aIRU EBYYRQ \aIRU VA GUR QVGPU\aVA GUR ..
+F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 13000 ERQJBBQ UJL, Jvyqreivyyr:  \a1/10/2013 7:48:12 NZ: 204991: 7514, 7555: 
+F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 4520 WHZC BSS WBR PERRX EQ, Tenagf Cnff:  \a1/10/2013 10:44:44 NZ: 108252: 7512, 7555, Z3 : UR GBYQ PNYYRE UR JNA'G VAWHEVRQ\a19 LBN ZNYR\aORGJRRA NAQ GU..
+F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 3279 SVFU UNGPUREL EQ, Tenagf Cnff:  \a1/10/2013 11:10:24 NZ: 204991: 7514, 7551, Z1: TERRA PNE
+F:!  Z:Ynetr Iruvpyr Sver: 785 JVYQREIVYYR YA, Tenagf Cnff:  \a1/10/2013 9:35:09 CZ: 204991: 7309, 7514, 7541, 7551, 7200, PBSQ: 
+F:!  Z:Fgehpgher Sver: 1730 AR SBBGUVYY OYIQ, Tenagf Cnff:  \a1/8/2013 3:33:58 CZ: 308791: 7307, 7308, 7309, SN5, 7553, Z12, 7531, 7353: JUVGR FZBXR SEBZ NGGVP\aQBRFA'G ORYVRIR GURL UNIR N JBBQ FGB..
+F:!  Z:GN1 - Genssvp Nppvqrag: JVYYVNZF UJL@ FJ TENAQIVRJ NIR , Tenagf Cnff:  \a1/12/2013 9:17:18 NZ: 201897: 7501, 7553, Z1, 171: fvatyr iru vagb cbyr
+F:!  Z:Tenff Sver: EBHAQF NIR@ ERQJBBQ NIR , Tenagf Cnff:  \a1/12/2013 6:45:11 CZ: 109411: 7514, 7553, 7200, PBSQ: GERR BA SVER
+
+*/
+
+choyvp pynff BEWbfrcuvarPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BEWbfrcuvarPbhaglCnefreGrfg() {
+    frgCnefre(arj BEWbfrcuvarPbhaglCnefre(), "WBFRCUVAR PBHAGL", "BE");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+    qbGrfg("G1",
+        "F:!  Z:Ryrpgevpny Sver: 613 FJ JRFGREA NIR O , Tenagf Cnff:  \a" +
+        "1/6/2013 12:29:06 NZ: 208791: 7317, 7553, 7400: FZRYYVAT JUNG FZRYYF YVXR NA RYRPGEVPNY CEBOYRZ\a" +
+        "UNMR VAFVQR..",
+
+        "PNYY:Ryrpgevpny Sver",
+        "NQQE:613 FJ JRFGREA NIR O",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/06/2013",
+        "GVZR:00:29:06",
+        "VQ:208791",
+        "HAVG:7317, 7553, 7400",
+        "VASB:FZRYYVAT JUNG FZRYYF YVXR NA RYRPGEVPNY CEBOYRZ UNMR VAFVQR..");
+
+    qbGrfg("G2",
+        "F:!  Z:Pbzzrepvny Sver: 115 FJ U FG, Tenagf Cnff:  \a" +
+        "1/5/2013 9:17:02 CZ: 208791: 7307, 7309, 7328, SN1, 7501, 7553, 161, 7531, 7353: NHQVOYR 2 UVGF\a" +
+        "HCFGNVEF ZBGVBA \a" +
+        "EC CRAQVAT \a" +
+        "*EC:  OHQ UBCFB..",
+
+        "PNYY:Pbzzrepvny Sver",
+        "NQQE:115 FJ U FG",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/05/2013",
+        "GVZR:21:17:02",
+        "VQ:208791",
+        "HAVG:7307, 7309, 7328, SN1, 7501, 7553, 161, 7531, 7353",
+        "VASB:NHQVOYR 2 UVGF HCFGNVEF ZBGVBA EC CRAQVAT *EC");
+
+    qbGrfg("G3",
+        "F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 5098 ARJ UBCR EQ, Tenagf Cnff:  \a" +
+        "1/6/2013 12:09:35 NZ: 201841: 7551, Z1: WBFU ZNVA 541-660-9673\a" +
+        "IRU EBYYRQ \a" +
+        "IRU VA GUR QVGPU\a" +
+        "VA GUR ..",
+
+        "PNYY:GN1E - Genssvp Nppvqrag Ebyybi",
+        "NQQE:5098 ARJ UBCR EQ",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/06/2013",
+        "GVZR:00:09:35",
+        "VQ:201841",
+        "HAVG:7551, Z1",
+        "VASB:WBFU ZNVA 541-660-9673 IRU EBYYRQ IRU VA GUR QVGPU VA GUR ..");
+
+    qbGrfg("G4",
+        "F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 13000 ERQJBBQ UJL, Jvyqreivyyr:  \a1/10/2013 7:48:12 NZ: 204991: 7514, 7555: ",
+        "PNYY:GN1E - Genssvp Nppvqrag Ebyybi",
+        "NQQE:13000 ERQJBBQ UJL",
+        "PVGL:Jvyqreivyyr",
+        "QNGR:01/10/2013",
+        "GVZR:07:48:12",
+        "VQ:204991",
+        "HAVG:7514, 7555:");
+
+    qbGrfg("G5",
+        "F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 4520 WHZC BSS WBR PERRX EQ, Tenagf Cnff:  \a" +
+        "1/10/2013 10:44:44 NZ: 108252: 7512, 7555, Z3 : UR GBYQ PNYYRE UR JNA'G VAWHEVRQ\a" +
+        "19 LBN ZNYR\a" +
+        "ORGJRRA NAQ GU..",
+
+        "PNYY:GN1E - Genssvp Nppvqrag Ebyybi",
+        "NQQE:4520 WHZC BSS WBR PERRX EQ",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/10/2013",
+        "GVZR:10:44:44",
+        "VQ:108252",
+        "HAVG:7512, 7555, Z3",
+        "VASB:UR GBYQ PNYYRE UR JNA'G VAWHEVRQ 19 LBN ZNYR ORGJRRA NAQ GU..");
+
+    qbGrfg("G6",
+        "F:!  Z:GN1E - Genssvp Nppvqrag Ebyybi: 3279 SVFU UNGPUREL EQ, Tenagf Cnff:  \a" +
+        "1/10/2013 11:10:24 NZ: 204991: 7514, 7551, Z1: TERRA PNE",
+
+        "PNYY:GN1E - Genssvp Nppvqrag Ebyybi",
+        "NQQE:3279 SVFU UNGPUREL EQ",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/10/2013",
+        "GVZR:11:10:24",
+        "VQ:204991",
+        "HAVG:7514, 7551, Z1",
+        "VASB:TERRA PNE");
+
+    qbGrfg("G7",
+        "F:!  Z:Ynetr Iruvpyr Sver: 785 JVYQREIVYYR YA, Tenagf Cnff:  \a1/10/2013 9:35:09 CZ: 204991: 7309, 7514, 7541, 7551, 7200, PBSQ: ",
+        "PNYY:Ynetr Iruvpyr Sver",
+        "NQQE:785 JVYQREIVYYR YA",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/10/2013",
+        "GVZR:21:35:09",
+        "VQ:204991",
+        "HAVG:7309, 7514, 7541, 7551, 7200, PBSQ:");
+
+    qbGrfg("G8",
+        "F:!  Z:Fgehpgher Sver: 1730 AR SBBGUVYY OYIQ, Tenagf Cnff:  \a" +
+        "1/8/2013 3:33:58 CZ: 308791: 7307, 7308, 7309, SN5, 7553, Z12, 7531, 7353: JUVGR FZBXR SEBZ NGGVP\a" +
+        "QBRFA'G ORYVRIR GURL UNIR N JBBQ FGB..",
+
+        "PNYY:Fgehpgher Sver",
+        "NQQE:1730 AR SBBGUVYY OYIQ",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/08/2013",
+        "GVZR:15:33:58",
+        "VQ:308791",
+        "HAVG:7307, 7308, 7309, SN5, 7553, Z12, 7531, 7353",
+        "VASB:JUVGR FZBXR SEBZ NGGVP QBRFA'G ORYVRIR GURL UNIR N JBBQ FGB..");
+
+    qbGrfg("G9",
+        "F:!  Z:GN1 - Genssvp Nppvqrag: JVYYVNZF UJL@ FJ TENAQIVRJ NIR , Tenagf Cnff:  \a" +
+        "1/12/2013 9:17:18 NZ: 201897: 7501, 7553, Z1, 171: fvatyr iru vagb cbyr",
+
+        "PNYY:GN1 - Genssvp Nppvqrag",
+        "NQQE:JVYYVNZF UJL & FJ TENAQIVRJ NIR",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/12/2013",
+        "GVZR:09:17:18",
+        "VQ:201897",
+        "HAVG:7501, 7553, Z1, 171",
+        "VASB:fvatyr iru vagb cbyr");
+
+    qbGrfg("G10",
+        "F:!  Z:Tenff Sver: EBHAQF NIR@ ERQJBBQ NIR , Tenagf Cnff:  \a1/12/2013 6:45:11 CZ: 109411: 7514, 7553, 7200, PBSQ: GERR BA SVER",
+        "PNYY:Tenff Sver",
+        "NQQE:EBHAQF NIR & ERQJBBQ NIR",
+        "PVGL:Tenagf Cnff",
+        "QNGR:01/12/2013",
+        "GVZR:18:45:11",
+        "VQ:109411",
+        "HAVG:7514, 7553, 7200, PBSQ",
+        "VASB:GERR BA SVER");
+  
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat netf[]) {
+    arj BEWbfrcuvarPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
