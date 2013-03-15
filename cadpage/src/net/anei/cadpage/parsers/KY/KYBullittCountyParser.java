a0b49a31165a77a21049ebd76dacd27e6e04c1f1
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-03-15 16:28:39 +0000

    Added LOUISVILLE to Bullitt County, KY city list

--- cadpage/src/net/anei/cadpage/parsers/KY/KYBullittCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/KY/KYBullittCountyParser.java
@@ -5,17 +5,10 @@ import net.anei.cadpage.parsers.dispatch.DispatchBParser;
 
 
 public class KYBullittCountyParser extends DispatchBParser {
-  
-  private static final String DEF_CITY = "BULLITT COUNTY";
-  private static final String DEF_STATE = "KY";
-  
-  private static final String[] CITY_LIST =new String[]{
-	  "BROOKS", "BROWNINGTON", "CLERMONT", "FOX CHASE", "HEBRON ESTATES", 
-	  "HILLVIEW", "HUNTERS HOLLOW" ,"LEBANON JUNCTION", "MOUNT WASHINGTON", "MT WASHINGTON", 
-	  "PIONEER VILLAGE", "SHEPHERDSVILLE", "SOLITUDE", "TAYLORSVILLE", };
 
   public KYBullittCountyParser() {
-    super(CITY_LIST, DEF_CITY, DEF_STATE);
+    super(CITY_LIST, "BULLITT COUNTY", "KY");
+    setupMultiWordStreets("MT CARMEL CHURCH");
   }
   
   public String getFilter() {
@@ -34,7 +27,27 @@ public class KYBullittCountyParser extends DispatchBParser {
     // First token following BULLITT CO 911: is the call type
     if (! body.startsWith("BULLITT CO 911:")) return false;
     body = body.substring(15).trim();
+    body = body.replace('@', '&');
 
     return super.parseMsg(body, data);
   }
+  
+  private static final String[] CITY_LIST =new String[]{
+    "BROOKS", 
+    "BROWNINGTON", 
+    "CLERMONT", 
+    "FOX CHASE", 
+    "HEBRON ESTATES", 
+    "HILLVIEW", 
+    "HUNTERS HOLLOW" ,
+    "LEBANON JUNCTION", 
+    "MOUNT WASHINGTON", 
+    "MT WASHINGTON", 
+    "PIONEER VILLAGE", 
+    "SHEPHERDSVILLE", 
+    "SOLITUDE", 
+    "TAYLORSVILLE",
+  
+    "LOUISVILLE"
+  };
 }
