2b1c70f7813cd2594d5c909aa41b983e89ab800c
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-03-23 03:59:49 +0000

    Fixed parsing problelm with Lee County, FL

--- cadpage/src/net/anei/cadpage/parsers/FL/FLLeeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/FL/FLLeeCountyParser.java
@@ -20,11 +20,21 @@ public class FLLeeCountyParser extends DispatchPrintrakParser {
   public boolean parseMsg(String body, Data data) {
     
     int ipt = body.indexOf(" TYP:");
-    if (ipt < 0) return false;
-    Parser p = new Parser(body.substring(0,ipt).trim());
-    data.strCallId = p.getLast(' ');
-    body = body.substring(ipt+1).trim();
+    if (ipt >= 0) {
+      Parser p = new Parser(body.substring(0,ipt).trim());
+      data.strCallId = p.getLast(' ');
+      body = body.substring(ipt+1).trim();
+    }
+    
+    else if (!body.startsWith("TYP:")) {
+      body = "TYP:" + body;
+    }
     
     return super.parseMsg(body, data);
+  }
+  
+  @Override
+  public String getProgram() {
+    return "ID " + super.getProgram();
   }
 }
