d3471be2e3a6186afd98bf67ed765bf744b77dfa
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-03-30 00:54:58 +0000

    Added skeleton Penn Star, PA location  parser

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/PA/PAPennStarParserTest.java
@@ -0,0 +1,150 @@
+cnpxntr arg.narv.pnqcntr.cnefref.CN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+vzcbeg arg.narv.pnqcntr.cnefref.QhzzlCnefre;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Ntrapl anzr: CraaFGNE Syvtug
+Pbagnpg: Npgvir911
+Ybpngvba: Cuvynqrycuvn, CN, Havgrq Fgngrf
+Fraqre: znvy@tuqf.arg
+
+23-13-31515: CraaFGNE PPG 1, Oela Znje Ubfcvgny, 23-13-31515
+23-13-31515: (FGNAQOL) CraaFGNE PPG 1, Oela Znje Ubfcvgny, 23-13-31515
+23-13-31512: CraaFGNE 5, A40 49.10 J75 54.70, 6.763 az /286.473 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31512: CraaFGNE 3, A40 49.10 J75 54.70, 26.797 az /292.737 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31510: CraaFGNE 3, A40 29.69 J74 27.05, 42.935 az /121.75 qrt. Eboreg Jbbq Wbuafba Havi Ubfc AUEJW
+23-13-31510: (FGNAQOL) CraaFGNE 3, A40 29.69 J74 27.05, 42.935 az /121.75 qrt. Eboreg Jbbq Wbuafba Havi Ubfc AUEJW
+23-13-31509: CraaFGNE 5, A40 49.10 J75 54.70, 6.862 az /286.493 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31508: CraaFGNE PPG 1, Zrepl Cuvynqrycuvn, 23-13-31508
+23-13-31507: CraaFGNE 2, A39 05.26 J74 49.01, 24.834 az /218.707 qrt. Pncr Ertvbany Zrqvpny Pragre AUOGZ
+27-13-4624: CraaFGNE 6, BHGTBVAT, A40 07.12 J75 07.27, 11.197 az /186.91 qrt. Novatgba Zrzbevny Ubfcvgny CUNOZ
+23-13-31504: Cngvrag vf va CPH ebbz #321 - Rkprcgvbany nzohynapr fubhyq or ba fprar.  Hfr 123.025 sbe nal pevgvpny enqvb genssvp
+23-13-31506: CraaFGNE 4, A39 05.26 J74 49.01, 93.448 az /157.061 qrt. Pncr Ertvbany Zrqvpny Pragre AUOGZ
+23-13-31504: CraaFGNE 3, A39 50.80 J74 55.60, 56.698 az /172.678 qrt. Iveghn Urnygu Flfgrz Ibbeurrf AUJWR
+23-13-31506: CraaFGNE 4, A39 05.26 J74 49.01, 93.448 az /157.061 qrt. Pncr Ertvbany Zrqvpny Pragre 
+23-13-31504: CraaFGNE 3, A39 50.80 J74 55.60, 56.698 az /172.678 qrt. Iveghn Urnygu Flfgrz Ibbeurrf AUJWR
+23-13-31505: CraaFGNE 1, A39 51.41 J75 22.13, 9.951 az /248.842 qrt. Pebmre Purfgre Zrqvpny Pragre CUPEP
+23-13-31503: CraaFGNE 1, A39 57.47 J75 12.02, 11.093 az /176.155 qrt. CCZP - Craa Cerfolgrevna ZP 
+23-13-31502: CraaFGNE PPG 1, Bhe Ynql bs Ybheqrf - Pnzqra, 23-13-31502
+23-13-31501: CraaFGNE 1, A39 55.36 J75 05.52, 15.092 az /159.899 qrt. Bhe Ynql bs Ybheqrf - Pnzqra AUBYY
+23-13-31499: CraaFGNE PPG 1, Anmnergu Ubfcvgny, 23-13-31499
+23-13-31498: CraaFGNE 1, A40 03.34 J75 02.36, 11.549 az /126.825 qrt. Anmnergu Ubfcvgny CUANM
+23-13-31498: (FGNAQOL) CraaFGNE 1, A40 03.34 J75 02.36, 11.549 az /126.825 qrt. Anmnergu Ubfcvgny CUANM
+23-13-31494: Ab svany/neevir EUZP enqvb pnyyf - unir lbh ba qrpx 9:32 cre frphevgl
+23-13-31494: CraaFGNE 4, A40 25.36 J76 13.13, 12.051 az /297.479 qrt. Orexf - 115 OP115
+23-13-31494: (FGNAQOL) CraaFGNE 4, A40 25.36 J76 13.13, 12.051 az /297.479 qrt. Orexf - 115 OP115
+23-13-31493: CraaFGNE PPG 1, Zrepl Fhoheona Ubfcvgny, 23-13-31493
+23-13-31493: (FGNAQOL) CraaFGNE PPG 1, Zrepl Fhoheona Ubfcvgny, 23-13-31493
+23-13-31490: CraaFGNE 5, A40 48.124 J75 57.089, 8.668 az /279.103 qrt. Fprar - Fpuhlyxvyy Pbhagl Teq Pag: YM65 Serd: 46.58 
+23-13-31490: (FGNAQOL) CraaFGNE 5, A40 48.124 J75 57.089, 8.668 az /279.103 qrt. Fprar - Fpuhlyxvyy Pbhagl Teq Pag: YM65 Serd: 46.58 
+27-13-4623: CraaFGNE 6, BHGTBVAT, A39 56.91 J75 09.17, 21.36 az /193.233 qrt. Gubznf Wrssrefba Havi  Yriry 1 CUWRS
+23-13-31489: CraaFGNE 4, A39 49.30 J75 53.30, 33.038 az /185.838 qrt. Wraarefivyyr Ertvbany Ubfcvgny CUFPP
+23-13-31486: CraaFGNE PPG 1, Rvafgrva Zrqvpny Pragre - Cuvynqrycuvn, 23-13-31486
+23-13-31486: CraaFGNE PPG 1, Rvafgrva Zrqvpny Pragre - Cuvynqrycuvn, 23-13-31486
+23-13-31486: (FGNAQOL) CraaFGNE PPG 1, Rvafgrva Zrqvpny Pragre - Cuvynqrycuvn, 23-13-31486
+27-13-4621: CraaFGNE 6, A40 06.30 J74 52.10, 16.125 az /66.216 qrt. Ybjre Ohpxf Ubfcvgny CUYOU
+23-13-31484: (FGNAQOL) CraaFGNE 3, A40 49.20 J75 14.26, 6.54 az /54.033 qrt. Fprar - Abegunzcgba Pbhagl 
+27-13-4620: CraaFGNE 6, BHGTBVAT, A39 56.91 J75 09.17, 21.36 az /193.233 qrt. Gubznf Wrssrefba Havi  Yriry 1 CUWRS
+27-13-4620: (FGNAQOL) CraaFGNE 6, BHGTBVAT, A39 56.91 J75 09.17, 21.36 az /193.233 qrt. Gubznf Wrssrefba Havi  Yriry 1 CUWRS
+23-13-31481: CraaFGNE 1, A39 56.50 J75 11.46, 12.196 az /175.348 qrt. Puvyqeraf Ubfcvgny bs Cuvynqrycuvn CUPUC
+23-13-31476: CraaFGNE 2, A39 05.26 J74 49.01, 24.834 az /218.707 qrt. Pncr Ertvbany Zrqvpny Pragre AUOGZ
+27-13-4619: CraaFGNE 6, BHGTBVAT, A40 14.11 J74 45.02, 18.503 az /114.938 qrt. Pncvgny Urnygu Ertvbany Zrqvpny Pragre AUURY
+27-13-4619: CraaFGNE 6, BHGTBVAT, A40 16.60 J74 48.81, 15.214 az /108.254 qrt. Geragba Zrepre GGA
+23-13-31474: CraaFGNE 2, A40 12.35 J74 02.22, 51.337 az /40.833 qrt. Wrefrl Fuber Havirefvgl Zrqvpny Pragre AUWFZ
+23-13-31474: (FGNAQOL) CraaFGNE 2, A40 12.35 J74 02.22, 51.337 az /40.833 qrt. Wrefrl Fuber Havirefvgl Zrqvpny Pragre AUWFZ
+23-13-31472: CraaFGNE 3, A39 54.80 J75 26.00, 49.589 az /197.304 qrt. Evqqyr Zrzbevny Ubfcvgny CUEZU
+23-13-31473: CraaFGNE 4, A39 58.14 J75 36.12, 29.257 az /157.152 qrt. Purfgre Pbhagl Ubfcvgny CUPPU
+23-13-31473: (FGNAQOL) CraaFGNE 4, A39 58.14 J75 36.12, 29.257 az /157.152 qrt. Purfgre Pbhagl Ubfcvgny CUPPU
+23-13-31470: (FGNAQOL) CraaFGNE 1, A40 08.50 J75 18.85, 2.192 az /290.169 qrt. Zrepl Fhoheona Ubfcvgny CUFTUSerd: CFPY 1 
+23-13-31468: (FGNAQOL) CraaFGNE 4, A40 18.47 J75 59.63, 3.922 az /211.415 qrt. Orexf - 193 OP193
+23-13-31464: CraaFGNE 1, A0 00.00 J0 00.00, 4730.228 az /111.621 qrt. Fprar - Zbagtbzrel Pbhagl 
+23-13-31463: CraaFGNE PPG 1, Rvafgrva Zrqvpny Pragre - Cuvynqrycuvn, 23-13-31463
+23-13-31459: CraaFGNE PPG 1, Purfgre Pbhagl Ubfcvgny, 23-13-31459
+23-13-31455: CraaFGNE PPG 1, Craaflyinavn Ubfcvgny, 23-13-31455
+23-13-31454: CraaFGNE 2, A39 09.28 J75 31.46, 47.707 az /259.839 qrt. Xrag Trareny Ubfcvgny QUXTU
+23-13-31454: CraaFGNE 2, A39 09.28 J75 31.46, 47.707 az /259.839 qrt. Xrag Trareny Ubfcvgny QUXTU
+27-13-4618: 40 00.429\a75 07.578
+23-13-31453: CraaFGNE PPG 1, In Zrq Pge Cuvynqrycuvn, 23-13-31453
+27-13-4618: CraaFGNE 6, A40 16.58 J75 13.074, 3.843 az /255.697 qrt. Fprar - Ohpxf Pbhagl Teq Pag: Qrchgl 34 Serd: STAQ 1 
+27-13-4618: CraaFGNE 6, A40 17.016 J75 13.367, 3.882 az /262.968 qrt. Fprar - Ohpxf Pbhagl 
+27-13-4618: (FGNAQOL) CraaFGNE 6, A40 17.016 J75 13.367, 3.882 az /262.968 qrt. Fprar - Ohpxf Pbhagl 
+23-13-31451: CraaFGNE 5, A40 47.367 J75 39.814, 4.71 az /116.812 qrt. Fprar - Pneoba Pbhagl 
+23-13-31450: CraaFGNE 3, A40 43.342 J75 38.309, 13.61 az /278.132 qrt. Fprar - Yruvtu Pbhagl Teq Pag: 904 
+23-13-31450: CraaFGNE 3, A40 43.962 J75 37.606, 13.372 az /281.029 qrt. Fprar - Yruvtu Pbhagl 
+23-13-31450: CraaFGNE 3, A40 43.999 J75 38.613, 14.136 az /281.243 qrt. Fprar - Yruvtu Pbhagl 
+23-13-31449: CraaFGNE PPG 1, Zrepl Cuvynqrycuvn, 23-13-31449
+23-13-31448: CraaFGNE 5, A40 49.10 J75 54.70, 6.862 az /286.493 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31448: (FGNAQOL) CraaFGNE 5, A40 49.10 J75 54.70, 6.862 az /286.493 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31447: CraaFGNE 1, A40 08.50 J75 18.85, 2.192 az /290.169 qrt. Zrepl Fhoheona Ubfcvgny CUFTU
+27-13-4616: 175 yof
+27-13-4616: CraaFGNE 6, A40 04.00 J74 58.60, 16.187 az /163.872 qrt. Nevn Urnygu - Gbeerfqnyr CUSUG
+23-13-31444: CraaFGNE PPG 1, UHC - Ubfc bs Havi bs Craa, 23-13-31444
+23-13-31442: CraaFGNE PPG 1, UHC - Ubfc bs Havi bs Craa, 23-13-31442
+23-13-31441: CraaFGNE 1, A40 16.052 J75 21.881, 10.987 az /334.653 qrt. Fprar - Zbagtbzrel Pbhagl 
+23-13-31440: CraaFGNE 3, A40 03.34 J75 02.36, 43.015 az /173.689 qrt. Anmnergu Ubfcvgny CUANM
+23-13-31439: (FGNAQOL) CraaFGNE 1, A39 43.10 J74 16.70, 44.25 az /119.865 qrt. Fbhgurea Bprna Pbhagl Ubfcvgny AUFBP
+27-13-4615: 67S
+27-13-4615: CraaFGNE 6, A40 00.61 J75 46.73, 34.458 az /250.658 qrt. Oenaqljvar Ubfcvgny CUOEQ
+27-13-4615: (FGNAQOL) CraaFGNE 6, A40 00.61 J75 46.73, 34.458 az /250.658 qrt. Oenaqljvar Ubfcvgny CUOEQ
+23-13-31438: CraaFGNE 1, A40 04.00 J74 58.60, 14.008 az /119.362 qrt. Nevn Urnygu -Gbeerfqnyr CUSUG
+27-13-4614: CraaFGNE 6, A40 18.54 J75 13.01, 2.558 az /320.523 qrt. Fprar - Ohpxf Pbhagl Teq Pag: Puvrs 34 Serd: STAQ 1 
+27-13-4614: CraaFGNE 6, A40 18.54 J75 13.01, 2.558 az /320.523 qrt. Fprar - Ohpxf Pbhagl Teq Pag: Qrchgl 34 
+23-13-31437: CraaFGNE 2, A39 18.70 J74 35.50, 8.779 az /196.394 qrt. Fuber Zrzbevny Ubfcvgny AUFZZ
+23-13-31431: CraaFGNE 1, A40 06.71 J75 04.55, 6.445 az /86.949 qrt. Ubyl Erqrrzre Ubfcvgny CUUEU
+23-13-31431: CraaFGNE 1, A40 06.71 J75 04.55, 6.445 az /86.949 qrt. Ubyl Erqrrzre Ubfcvgny CUUEU
+23-13-31429: CraaFGNE 1, A40 07.12 J75 07.27, 6.797 az /111.039 qrt. Novatgba Zrzbevny Ubfcvgny CUNOZ
+23-13-31428: CraaFGNE 2, A38 54.30 J75 25.40, 51.248 az /242.751 qrt. Zvysbeq Zrzbevny Ubfcvgny QUZZU
+23-13-31428: (FGNAQOL) CraaFGNE 2, A38 54.30 J75 25.40, 51.248 az /242.751 qrt. Zvysbeq Zrzbevny Ubfcvgny QUZZU
+23-13-31426: CraaFGNE 4, A40 36.50 J75 23.60, 29.847 az /73.047 qrt. FYU - Fnvag Yhxrf Ubfcvgny CUFYU
+23-13-31421: CraaFGNE 1, A40 08.50 J75 18.85, 2.235 az /291.156 qrt. Zrepl Fhoheona Ubfcvgny CUFTU
+23-13-31421: CraaFGNE 1, A40 08.50 J75 18.85, 2.235 az /291.156 qrt. Zrepl Fhoheona Ubfcvgny CUFTU
+27-13-4613: CraaFGNE 6, BHGTBVAT, A40 00.10 J75 08.60, 18.168 az /192.072 qrt. Grzcyr Havi Ubfcvgny - Yriry 1 CUGHU
+23-13-31418: 46S Frvmherf ab qevc ab irag VPH->ARHEB VPH 55xt 106/60 99%EN 19 70
+23-13-31418: CraaFGNE PPG 1, Ubyl Erqrrzre Ubfcvgny, 23-13-31418
+23-13-31416: CraaFGNE 4, A40 07.40 J75 30.95, 25.385 az /137.514 qrt. Cubravkivyyr Ubfcvgny CUCIU
+23-13-31416: (FGNAQOL) CraaFGNE 4, A40 07.40 J75 30.95, 25.385 az /137.514 qrt. Cubravkivyyr Ubfcvgny CUCIU
+23-13-31416: (FGNAQOL) CraaFGNE 4, A40 07.40 J75 30.95, 25.385 az /137.514 qrt. Cubravkivyyr Ubfcvgny CUCIU
+23-13-31415: CraaFGNE PPG 1, UHC - Ubfc bs Havi bs Craa, 23-13-31415
+23-13-31414: CraaFGNE 5, A40 49.10 J75 54.70, 6.862 az /286.493 qrt. Zvaref Zrzbevny Ubfcvgny CUPBN
+23-13-31412: CraaFGNE 2, A40 29.60 J74 26.70, 62.394 az /17.543 qrt. Fg Crgref Zrqvpny Pragre AUFCZ
+23-13-31409: (FGNAQOL) CraaFGNE PPG 1, CCZP - Craa Cerfolgrevna ZP, 23-13-31409
+
+Abgrf:  Vg vf n zlfgrel gb zr jul gurfr crbcyr jnag gb hfr Pnqcntr.  Gurl ner na nevny genafcbeg ntrapl, gurl 
+gurve bja anivtngvba nvqf gb trg jurer gurl arrq gb xabj naq fher nf urpx orggre abg or pbhagvat ba fbzrguvat
+Tbbtyr zncf gb fubj gurz ubj gb trg gurer.
+
+Cnlbhg: $100
+Onfvpnyyl n ybg bs vaqrk frnepuf naq cnggrea zngpurf.
+23-13-31409 -> fgePnyyVq
+(FGNAQOL) -> nccraq gb raq bs fgePnyy
+CraaFGNEG PPG 1 -> havg
+A40 29.60 J74 26.70, 62.394 az /17.543 qrt. -> fgeNqqerff
+Zvaref Zrzbevny Ubfcvgny -> fgePnyy
+CUPBN -> fgePbqr
+Serd: -> fgePunaary
+Teq Pag: -> xrrc ynory naq chg va fgeFhcc
+
+Vs lbh pna'g cnefr nalguvat rkprcg gur vavgvny pnyy VQ, chg gur erfg bs gur grkg va fgeCynpr
+naq frg fgePnyy gb "TRARENY NYREG"
+
+*/
+
+choyvp pynff CNCraaFgneCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp CNCraaFgneCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "", "");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj CNCraaFgneCnefreGrfg().trarengrGrfgf("G1");
+  }
+  
+}
