b1113c1712d3875e996562e8496e5c8438dd85c0
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-04-03 04:07:18 +0000

    Fixed parsing problem with Tipton County, IN

--- cadpage/src/net/anei/cadpage/parsers/IN/INTiptonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/IN/INTiptonCountyParser.java
@@ -6,11 +6,17 @@ public class INTiptonCountyParser extends DispatchOSSIParser {
   
   public INTiptonCountyParser() {
     super("TIPTON COUNTY", "IN",
-           "UNIT CALL ID ADDR INFO+");
+           "UNIT CALL ID? ADDR INFO+");
   }
   
   @Override
   public String getFilter() {
     return "CAD@tipco.com";
+  }
+  
+  @Override
+  public Field  getField(String name) {
+    if (name.equals("ID")) return new IdField("\\d+");
+    return super.getField(name);
   }
 }
