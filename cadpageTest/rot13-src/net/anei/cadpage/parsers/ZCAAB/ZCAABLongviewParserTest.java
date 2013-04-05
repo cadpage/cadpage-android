8f2cbc3cfe3685d4e8de0d19c5ae005620b611ed
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-04-05 07:43:26 +0000

    Changed name of test class CAABCanmore to ZCABCanmore Skeleton for ABLongview parser test is ready

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/ZCAAB/ZCAABLongviewParserTest.java
@@ -0,0 +1,108 @@
+cnpxntr arg.narv.pnqcntr.cnefref.MPNNO;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Ybativrj, NO
+Pbagnpg: Npgvir911
+Ntrapl anzr: Sbbguvyyf Sver Qrcg  Ybativrj Fgngvba
+Ybpngvba: Ybativrj, Nyoregn, Pnanqn
+Fraqre: YhpuvnZ@serfp.pn
+
+(Evc & Eha) 09:14CZ PNYY 12q02 NG 0 fj_13_17_4_j5, ubhfr 77, rqra inyyrl ve 216 
+(Evc & Eha) 09:08CZ PNYY 12q02 NG 0 fj_13_17_4_j5, ubhfr 77, rqra inyyrl ve 216 
+(Evc & Eha) 09:08CZ PNYY 12q02 NG 0 fj_13_17_4_j5, ubhfr 77, rqra inyyrl ve 216 
+(Evc & Eha) 04:26CZ PNYY 65n04 NG 216 eblnygvrf pe, yvsg nffvfg, ybativrj 
+(Evc & Eha) 04:21CZ PNYY 65n01 NG 216 yblnygvrf pe, ybativrj 
+(Evc & Eha) 04:21CZ PNYY 65n01 NG 216 yblnygvrf pe, ybativrj 
+(Evc & Eha) 11:20CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 11:19CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 10:35CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 10:32CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 10:30CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 10:23CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 10:23CZ PNYY 67o01 NG , ubhfr 87, rqra inyyrl 
+(Evc & Eha) 01:14NZ PNYY 29q02 NG , ubhfr 98, rqra inyyrl erfreir 
+(Evc & Eha) 01:13NZ PNYY 29q02 NG , ubhfr 98, rqra inyyrl erfreir 
+(Evc & Eha) 01:03NZ PNYY 29q01 NG ,  
+(Evc & Eha) 01:03NZ PNYY 29q01 NG ,  
+(Evc & Eha) 09:51CZ PNYY 65o02 NG 13-10 17_4_j5, ubhfr 13, rqra inyyrl ve 216 
+(Evc & Eha) 09:51CZ PNYY 65o02 NG 13-10 17_4_j5, ubhfr 13, rqra inyyrl ve 216 
+(Evc & Eha) 09:44CZ PNYY 31q01 NG 13-10 17_4_j5, ubhfr 13, rqra inyyrl ve 216 
+(Evc & Eha) 09:44CZ PNYY 31q01 NG 13-10 17_4_j5, ubhfr 13, rqra inyyrl ve 216 
+(Evc & Eha) 10:09CZ PNYY 65o02 NG , ubhfr 66, rqra inyyrl erfreir 
+(Evc & Eha) 09:53CZ PNYY 65o02 NG , ubhfr 66, rqra inyyrl erfreir 
+(Evc & Eha) 09:46CZ PNYY 65o01 NG , ubhfr 66,  
+(Evc & Eha) 09:46CZ PNYY 65o01 NG , ubhfr 66,  
+(Evc & Eha) 04:14NZ PNYY 27q03 NG 0 ar_11_17_4_j5, ubhfr 62, rqra inyyrl ve 216 
+(Evc & Eha) 04:14NZ PNYY 27q03 NG 0 ar_11_17_4_j5, ubhfr 62, rqra inyyrl ve 216 
+(Evc & Eha) 09:43CZ PNYY 65n04 NG 123 xrr qe, ohzclf cvg fgbc, ybativrj 
+(Evc & Eha) 09:38CZ PNYY 65n03 NG 123 xrr qe, ohzclf cvg fgbc, ybativrj 
+(Evc & Eha) 09:38CZ PNYY 65n03 NG 123 xrr qe, ohzclf cvg fgbc, ybativrj 
+(Evc & Eha) 09:37CZ PNYY 21q01 NG , ubhfr 72 rqra inyyrl, rqra inyyrl erfreir 
+(Evc & Eha) 09:30CZ PNYY 21q01 NG , ubhfr 72 rqra inyyrl,  
+(Evc & Eha) 09:30CZ PNYY 21q01 NG , ubhfr 72 rqra inyyrl,  
+(Evc & Eha) 07:23CZ PNYY 69q01 NG 22 ujl & 176 fg j, sbbguvyyf zq 
+(Evc & Eha) 07:23CZ PNYY 69q01 NG 22 ujl & 176 fg j, sbbguvyyf zq 
+(Evc & Eha) 10:08CZ PNYY 65o02 NG 541 ujl & 40 ujl, xnananfxvf vq 
+(Evc & Eha) 10:00CZ PNYY 65o01 NG 541 ujl & 40 ujl, xnananfxvf vq 
+(Evc & Eha) 10:00CZ PNYY 65o01 NG 541 ujl & 40 ujl, xnananfxvf vq 
+(Evc & Eha) 11:42CZ PNYY 10q04 NG 0 ubhfr_3, rqra inyyrl erfreir 
+(Evc & Eha) 11:32CZ PNYY 10p01 NG 0 ubhfr_3, rqra inyyrl erfreir 
+(Evc & Eha) 11:32CZ PNYY 10p01 NG 0 ubhfr_3, rqra inyyrl erfreir 
+(Evc & Eha) 10:03NZ PNYY 52p01 NG , puvrs wnpbo ornefcnj fpubby, rqra inyyrl 
+(Evc & Eha) 10:03NZ PNYY 52p01 NG , puvrs wnpbo ornefcnj fpubby, rqra inyyrl 
+(Evc & Eha) 03:02CZ PNYY 21q02 NG #fr_14_17_4_j5, rqra inyyrl neran, rqra inyyrl ve 216 
+(Evc & Eha) 02:55CZ PNYY 21q02 NG #fr_14_17_4_j5, rqra inyyrl neran, rqra inyyrl ve 216 
+(Evc & Eha) 02:55CZ PNYY 21q02 NG #fr_14_17_4_j5, rqra inyyrl neran, rqra inyyrl ve 216 
+(Evc & Eha) 10:51NZ PNYY 26p02 NG yng 50.433560 yba -114.440860, ufr 109, sbbguvyyf zq 
+(Evc & Eha) 10:43NZ PNYY 26q01 NG yng 50.433560 yba -114.440860, ufr 109, sbbguvyyf zq 
+(Evc & Eha) 10:43NZ PNYY 26q01 NG yng 50.433560 yba -114.440860, ufr 109, sbbguvyyf zq 
+(Evc & Eha) 12:49CZ PNYY 65o02 NG 32015 562 nir j, sbbguvyyf zq 
+(Evc & Eha) 12:39CZ PNYY 65o02 NG 32015 562 nir j, sbbguvyyf zq 
+(Evc & Eha) 12:39CZ PNYY 65o02 NG 32015 562 nir j, sbbguvyyf zq 
+(Evc & Eha) 02:32CZ PNYY 69q01 NG ubhfr 8,  
+(Evc & Eha) 02:32CZ PNYY 69q01 NG ubhfr 8,  
+(Evc & Eha) 08:58CZ PNYY 65o02 NG , ufr 52, rqra inyyrl erfreir 
+(Evc & Eha) 08:51CZ PNYY 31q01 NG , ufr 52, rqra inyyrl erfreir 
+(Evc & Eha) 08:51CZ PNYY 31q01 NG , ufr 52, rqra inyyrl erfreir 
+(Evc & Eha) 05:43CZ PNYY 29q01 NG 530 nir j & 144 fg j, sbbguvyyf zq 
+(Evc & Eha) 05:43CZ PNYY 29q01 NG 530 nir j & 144 fg j, sbbguvyyf zq 
+(Evc & Eha) 07:00CZ PNYY 52o01 NG 530 nir j & 80 fg j, sbbguvyyf zq 
+(Evc & Eha) 06:59CZ PNYY 52o01 NG 530 nir j & 80 fg j, sbbguvyyf zq 
+(Evc & Eha) 06:50CZ PNYY 65n01 NG 530 nir j & 80 fg j, sbbguvyyf zq 
+(Evc & Eha) 06:50CZ PNYY 65n01 NG 530 nir j & 80 fg j, sbbguvyyf zq 
+(Evc & Eha) 05:32CZ PNYY 29q02 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 05:25CZ PNYY 29q02 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 05:18CZ PNYY 12n01 NG 0 ubhfr_72, rqra inyyrl erfreir 
+(Evc & Eha) 04:56CZ PNYY 12n01 NG 0 ubhfr_72, rqra inyyrl erfreir 
+(Evc & Eha) 04:36CZ PNYY 29q02 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 04:35CZ PNYY 29q02 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 04:28CZ PNYY 29q01 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 04:28CZ PNYY 29q01 NG 0 fj_4_17_4_j5, cg vf ng choyvp jbexf, rqra inyyrl ve 216 
+(Evc & Eha) 04:09NZ PNYY 23p02 NG 0 ar_14_17_4_j5, ubhfr 112, rqra inyyrl ve 216 
+(Evc & Eha) 04:09NZ PNYY 23p02 NG 0 ar_14_17_4_j5, ubhfr 112, rqra inyyrl ve 216 
+(Evc & Eha) 04:08NZ PNYY 23p02 NG 0 ar_14_17_4_j5, ubhfr 112, rqra inyyrl ve 216 
+(Evc & Eha) 04:08NZ PNYY 23p02 NG 0 ar_14_17_4_j5, ubhfr 112, rqra inyyrl ve 216 
+
+
+*/
+
+choyvp pynff MPNNOYbativrjCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp MPNNOYbativrjCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "YBATIVRJ", "NO");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+   
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj MPNNOYbativrjCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
