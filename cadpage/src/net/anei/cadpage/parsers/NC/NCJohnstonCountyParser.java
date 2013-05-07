bf7fb66ba47bbc3d8f9e3670e4e549eadadd1414
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-05-07 05:22:15 +0000

    Fixed parsing problem with Johnston County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCJohnstonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCJohnstonCountyParser.java
@@ -14,9 +14,13 @@ public class NCJohnstonCountyParser extends DispatchOSSIParser {
   
   private static final Pattern ENROUTE_PTN = Pattern.compile("CAD:([A-Z0-9]+),Enroute,.*");
   
+  private String lastCrossPlaceFld;
+  
   public NCJohnstonCountyParser() {
     super(CITY_CODES, "JOHNSTON COUNTY", "NC",
-           "( CH SRC CODE? | SRC/Z CODE | SRC? ) CALL CITY? ADDR! CITY? APT? X+? ( PLACE NAME DATETIME | NAME? DATETIME ) UNIT");
+           "( CALL ADDR/Z CITY! APT? X_PLACE_INFO+ | " +
+             "CALL CITY ADDR! APT? X_PLACE_INFO+ | " + 
+             "CH? SRC SRC? CODE? CALL ADDR APT? X_PLACE_INFO+? DATETIME! UNIT CITY% )");
   }
 
   @Override
@@ -36,6 +40,7 @@ public class NCJohnstonCountyParser extends DispatchOSSIParser {
       return true;
     }
     
+    lastCrossPlaceFld = "";
     return super.parseMsg(body, data);
   }
   
@@ -43,67 +48,60 @@ public class NCJohnstonCountyParser extends DispatchOSSIParser {
   public String getProgram() {
     return super.getProgram() + " UNIT PLACE";
   }
-
-  // Source code validates that is is not more than 5 characters long
+  
   private class MySourceField extends SourceField {
-
-    @Override
-    public boolean canFail() {
-      return true;
+    public MySourceField() {
+      setPattern("[A-Z][A-Z0-9]{1,4}", true);
     }
-
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (field.length() > 5) return false;
-      parse(field, data);
-      return true;
-    }
-  }
-  
-  // Cross street won't pass normal address validation
-  // We will take anything that doesn't look like a name with a comma
-  private static final Pattern NS_CROSS_PTN = Pattern.compile("(\\d*) *\\(S\\) *(.*?) *\\(N\\)|[\\.\\d]+|DIST:.*");
-  private class MyCrossField extends CrossField {
-    @Override
-    public boolean checkParse(String field, Data data) {
-      
-      // If this is the first cross/place field, rules are different
-      if (data.strCross.length() == 0 && data.strPlace.length() == 0) {
-        
-        // see if it has the expected (N)..(S) pattern
-        // Or looks like a decimal number
-        // If it is, put it in the place or cross street field
-        // depending on whether or not it looks like an address
-        Matcher match = NS_CROSS_PTN.matcher(field);
-        if (match.matches()) {
-          String tmp = match.group(2);
-          if (tmp != null) {
-            data.strApt = append(data.strApt, "-", match.group(1));
-            field = tmp;
-          }
-          if (checkAddress(field) > 0) {
-            data.strCross = field; 
-          } else {
-            data.strPlace = field;
-          }
-          return true;
-        }
-      }
-      
-      // Otherwise if this contains a comma or colon, it is probably a name
-      // or date/time stamp field and we should reject it
-      if (field.contains(",") || field.contains(":")) return false;
-      
-      // Otherwise return regular address check result
-      return super.checkParse(field, data);
-    }
-  }
-  
-  private class MyPlaceField extends PlaceField {
+    
     @Override
     public void parse(String field, Data data) {
-      if (data.strPlace.equals(field)) return;
-      data.strPlace = append(data.strPlace, " - ", field);
+      data.strSource = append(data.strSource, ",", field);
+    }
+  }
+  
+  private static final Pattern NS_CROSS_PTN = Pattern.compile("(.*?) *\\(S\\) *(.*?) *\\(N\\)|(?:[NSEW]B +)?DIST:.*");
+  private class MyCrossPlaceInfoField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      
+      // see if it has the expected (N)..(S) pattern
+      // Or looks like a decimal number
+      // If it is, put it in the place or cross street field
+      // depending on whether or not it looks like an address
+      Matcher match = NS_CROSS_PTN.matcher(field);
+      if (match.matches()) {
+        String tmp = match.group(2);
+        if (tmp != null) {
+          String prefix = match.group(1);
+          if (prefix.length() > 0) {
+            if (NUMERIC.matcher(prefix).matches()) {
+              data.strApt = append(data.strApt, "-", match.group(1));
+            } else {
+              data.strPlace = append(data.strPlace, " - ", prefix);
+            }
+          }
+          field = tmp;
+        }
+        data.strPlace = append(data.strPlace, " - ", field);
+        return;
+      }
+      
+      // If duplicate of last field, skip it
+      if (field.equals(lastCrossPlaceFld)) return;
+      lastCrossPlaceFld = field;
+      
+      // See if it looks like a cross street or a place name
+      if (field.contains("70 BUS HWY") || checkAddress(field) > 0) {
+        data.strCross = append(data.strCross, " / ", field); 
+      } else {
+        data.strSupp = append(data.strSupp, " / ", cleanWirelessCarrier(field));
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "PLACE X INFO";
     }
   }
   
@@ -128,20 +126,28 @@ public class NCJohnstonCountyParser extends DispatchOSSIParser {
 
   @Override
   protected Field getField(String name) {
-    if (name.equals("CH")) return new ChannelField("OPS.*|.*FR|VPR.*");
+    if (name.equals("CH")) return new ChannelField("OPS.*|.*FR|VPR.*|2ND", true);
     if (name.equals("SRC")) return new MySourceField();
-    if (name.equals("CODE")) return new CodeField("\\d\\d[A-Z]\\d\\d[A-Za-z]?");
-    if (name.equals("X")) return new MyCrossField();
-    if (name.equals("APT")) return new AptField("APT.*|SUITE.*");
-    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("CODE")) return new CodeField("\\d{1,3}[A-Z]\\d\\d[A-Za-z]?", true);
+    if (name.equals("APT")) return new AptField("APT.*|SUITE.*|LOT.*");
+    if (name.equals("X_PLACE_INFO")) return new MyCrossPlaceInfoField();
     if (name.equals("DATETIME")) return new MyDateTimeField();
     return super.getField(name);
   }
   
+  @Override
+  public String adjustMapAddress(String addr) {
+    return US70BUSHWY_PTN.matcher(addr).replaceAll("70 BUS");
+  }
+  Pattern US70BUSHWY_PTN = Pattern.compile("\\b70 BUS HWY\\b", Pattern.CASE_INSENSITIVE);
+  
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "BENS", "BENSON",
       "CLAY", "CLAYTON",
+      "FOUR", "FOUR OAKS",
       "GARN", "GARNER",
-      "SELM", "SELMA"
+      "SELM", "SELMA",
+      "SMIT", "SMITHFIELD",
+      "WAKE", "WAKE COUNTY"
   });
 }
