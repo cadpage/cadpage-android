a0da1c3529a37c018a5f6ed97a0e8701df42a993
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-05-15 00:25:48 +0000

    Fixed parsing problem with Cayuga County,NY

--- cadpage/src/net/anei/cadpage/parsers/NY/NYCayugaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYCayugaCountyParser.java
@@ -10,31 +10,33 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class NYCayugaCountyParser extends SmartAddressParser {
   
-  private static final Pattern MARKER = Pattern.compile("^\\d\\d/\\d\\d/\\d\\d \\d\\d:\\d\\d ");
+  private static final Pattern MARKER = Pattern.compile("^(\\d\\d/\\d\\d/\\d\\d) (\\d\\d:\\d\\d) ");
   private static final Pattern PLACE_PTN = Pattern.compile("^[ A-Z]*[A-Z](?=\\d)");
 
   public NYCayugaCountyParser() {
     super(CITY_LIST, "CAYUGA COUNTY", "NY");
-    setFieldList("PLACE ADDR CITY CALL INFO");
+    setFieldList("DATE TIME PLACE ADDR CITY CALL INFO");
     setupMultiWordStreets("WEST LAKE");
   }
   
   @Override
   public String getFilter() {
-    return "cayuga911@co.cayuga.ny.us,cayuga911@nameservices.net,cayuga911@www.cayugacounty.us";
+    return "cayuga911@co.cayuga.ny.us,cayuga911@nameservices.net,cayuga911@www.cayugacounty.us,messaging@iamresponding.com";
   }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     
-    if (!subject.equals("From 911 Center") &&
-        !subject.equals("From911Center")) return false;
+    boolean confirmed = subject.equals("From 911 Center") || subject.equals("From911Center");
     Matcher match = MARKER.matcher(body);
     if (!match.find()) {
+      if (!confirmed) return false;
       data.strCall = "GENERAL ALERT";
       data.strPlace = body;
       return true;
     }
+    data.strDate = match.group(1);
+    data.strTime = match.group(2);
     body = body.substring(match.end()).trim();
     
     // They do not leave spaces between place names and street numbers :(
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/NY/NYCayugaCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NY/NYCayugaCountyParserTest.java
@@ -7,6 +7,11 @@ vzcbeg bet.whavg.Grfg;
 
 /*
 Pnlhtn Pbhagl, AL
+
+
+Fubhyq or trareny nyreg
+(Sebz 911 Pragre) NAL ZRZORE GUNG PNA QRCYBL SBE FRSH BA FHAQNL PBAGNPG NFFG. PUS FNOVA BA UVF PRYYCUBAR NFNC
+
 Pbagnpg: Pberl <pebbxre1@gjpal.ee.pbz>
 Fraqre: pnlhtn911@pb.pnlhtn.al.hf
 
@@ -28,8 +33,9 @@ Pbagnpg: Pberl <pebbxre1@gjpal.ee.pbz>
 (Sebz 911 Pragre) 08/26/11 09:59 8860 F  JVYYBJ FG JRRQFCBEG Vawhel sebz n Snyy  70 SRZNYR SRYY LRFGREQNL/QVSS NZOHYNGVAT GBQNL  R911 Vasb - Pynff bs Frei
 (Sebz 911 Pragre) 08/24/11 22:00 8750 PRAGRECBEG EQ ZRAGM Pneqvnp  79 L/B SRZNYR PURFG CNVAF...  QREOL ERFVQRAPR
 
-Fubhyq or trareny nyreg
-(Sebz 911 Pragre) NAL ZRZORE GUNG PNA QRCYBL SBE FRSH BA FHAQNL PBAGNPG NFFG. PUS FNOVA BA UVF PRYYCUBAR NFNC
+Pbagnpg: Zvxr Pynex <zpprygvpf5@tznvy.pbz>
+Fraqre: zrffntvat@vnzerfcbaqvat.pbz
+SEZ:zrffntvat@vnzerfcbaqvat.pbz\aFHOW:Bjnfpb\aZFT:05/14/13 05:08 BARVY EQ OEBNQJNL EQ BJNFPB Fgehpgher Sver  CBFF ONEA SVER
 
 Pbagnpg: Npgvir911
 Ntrapl anzr: Fraargg Sver Qrcnegzrag
@@ -75,35 +81,47 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
     qbGrfg("G1",
         "(Sebz 911 Pragre) 02/14/11 16:26 5 PRQNE YA OEHGHF Pneqvnp",
         "NQQE:5 PRQNE YA",
+        "QNGR:02/14/11", 
+        "GVZR:16:26",
         "PVGL:OEHGHF",
         "PNYY:Pneqvnp");
 
     qbGrfg("G2",
         "(Sebz 911 Pragre) 02/14/11 14:59 8318 J  YBBC EQ ZBAGRMHZN Trareny",
+        "QNGR:02/14/11",
+        "GVZR:14:59",
         "NQQE:8318 J YBBC EQ",
         "PVGL:ZBAGRMHZN",
         "PNYY:Trareny");
 
     qbGrfg("G3",
         "(Sebz 911 Pragre) 02/14/11 12:19 CVAR FG & CNEX FG CBEG OLEBA Pbyyncfr",
+        "QNGR:02/14/11",
+        "GVZR:12:1",
         "NQQE:CVAR FG & CNEX FG",
         "PVGL:CBEG OLEBA",
         "PNYY:Pbyyncfr");
 
     qbGrfg("G4",
         "(Sebz 911 Pragre) 02/14/11 10:29 7 RIRETERRA YA OEHGHF Fgebxr",
+        "QNGR:02/14/11",
+        "GVZR:10:29",
         "NQQE:7 RIRETERRA YA",
         "PVGL:OEHGHF",
         "PNYY:Fgebxr");
 
     qbGrfg("G5",
         "SEZ:pnlhtn911@pb.pnlhtn.al.hf\aFHOW:Sebz911Pragre\aZFT:\a04/20/11 13:51 3204 SENAXYVA FGERRG EQ FRAARGG Nynez - Sver\a",
+        "QNGR:04/20/11",
+        "GVZR:13:51",
         "NQQE:3204 SENAXYVA FGERRG EQ",
         "PVGL:FRAARGG",
         "PNYY:Nynez - Sver");
 
     qbGrfg("G6",
         "(Sebz 911 Pragre) 06/15/11 00:03 215 PNLHTN FG HAVBA FCEVATF Pneqvnp  Pnyy Ahzore 467 jnf perngrq sebz Pnyy Ahzore 462(Wha 14 2011 11:20CZ)  fgevrq gur cub",
+        "QNGR:06/15/11",
+        "GVZR:00:03",
         "NQQE:215 PNLHTN FG",
         "PVGL:HAVBA FCEVATF",
         "VASB:Pneqvnp Pnyy Ahzore 467 jnf perngrq sebz Pnyy Ahzore 462(Wha 14 2011 11: 20CZ) fgevrq gur cub");
@@ -114,6 +132,8 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
     qbGrfg("G1",
         "(Sebz 911 Pragre) 08/27/11 02:10 EVIRE EQ EG 38 PBADHRFG ZINCV  fhowrpg pnzr vagb one  oybbql fgngrf pne nppvqrag   cvengrf ynaqvat one  zincv   oryvrirf",
+        "QNGR:08/27/11",
+        "GVZR:02:10",
         "NQQE:EVIRE EQ & EG 38",
         "PVG" +
         "L:PBADHRFG",
@@ -122,46 +142,73 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
     qbGrfg("G2",
         "(Sebz 911 Pragre) 08/27/11 00:42 URWNZNQN PNZCTEBHAQ764 ZP QBANYQ EQ ZBAGRMHZN Trareny Vyyarff  zbunjx frpgvba    bss gb yrsg nsgre tngr  68 lb s   yrsg n",
         "CYNPR:URWNZNQN PNZCTEBHAQ",
+        "QNGR:08/27/11",
+        "GVZR:00:42",
         "NQQE:764 ZP QBANYQ EQ",
         "PVGL:ZBAGRMHZN",
         "VASB:Trareny Vyyarff zbunjx frpgvba bss gb yrsg nsgre tngr 68 lb s yrsg n");
 
     qbGrfg("G3",
         "(Sebz 911 Pragre) 08/26/11 14:49 8860 F  JVYYBJ FG JRRQFCBEG Trareny Vyyarff  60 L/B SRZNYR  PNYYRE FGNGRF FUR ARRQF GB TB GB FG WBRF CRE URE QE... NOQBZR",
+        "QNGR:08/26/11",
+        "GVZR:14:49",
         "NQQE:8860 F JVYYBJ FG",
         "PVGL:JRRQFCBEG",
         "VASB:Trareny Vyyarff 60 L / B SRZNYR PNYYRE FGNGRF FUR ARRQF GB TB GB FG WBRF CRE URE QE NOQBZR");
 
     qbGrfg("G4",
         "(Sebz 911 Pragre) 08/26/11 09:59 8860 F  JVYYBJ FG JRRQFCBEG Vawhel sebz n Snyy  70 SRZNYR SRYY LRFGREQNL/QVSS NZOHYNGVAT GBQNL  R911 Vasb - Pynff bs Frei",
+        "QNGR:08/26/11",
+        "GVZR:09:59",
         "NQQE:8860 F JVYYBJ FG",
         "PVGL:JRRQFCBEG",
         "VASB:Vawhel sebz n Snyy 70 SRZNYR SRYY LRFGREQNL / QVSS NZOHYNGVAT GBQNL R911 Vasb - Pynff bs Frei");
 
     qbGrfg("G5",
         "(Sebz 911 Pragre) 08/24/11 22:00 8750 PRAGRECBEG EQ ZRAGM Pneqvnp  79 L/B SRZNYR PURFG CNVAF...  QREOL ERFVQRAPR",
+        "QNGR:08/24/11",
+        "GVZR:22:00",
         "NQQE:8750 PRAGRECBEG EQ",
         "PVGL:ZRAGM",
         "VASB:Pneqvnp 79 L / B SRZNYR PURFG CNVAF QREOL ERFVQRAPR");
- }
+  }
+  
+  @Grfg
+  choyvp ibvq grfgZvxrPynex() {
+
+    qbGrfg("G1",
+        "SEZ:zrffntvat@vnzerfcbaqvat.pbz\aFHOW:Bjnfpb\aZFT:05/14/13 05:08 BARVY EQ OEBNQJNL EQ BJNFPB Fgehpgher Sver  CBFF ONEA SVER",
+        "QNGR:05/14/13",
+        "GVZR:05:08",
+        "NQQE:BARVY EQ & OEBNQJNL EQ",
+        "PVGL:BJNFPB",
+        "PNYY:Fgehpgher Sver CBFF ONEA SVER");
+    
+  }
   
   @Grfg
   choyvp ibvq grfgFraarggSver() {
 
     qbGrfg("G1",
         "(Sebz 911 Pragre) 04/29/13 03:32 3377 SENAXYVA FGERRG EQ FRAARGG Pneqvnp  UNQ N URNEG NGGNPX 2 JRRXF NTB  61 LB SRZNYR OHEAVAT PURFG CNVA  R911 Vasb - Pynff bs Freivpr: ERFQ Fcrpvny Erfcbafr Vasb: PNL. PB. FUREVSS & AL FG CBYVPR FRAARGG SQ                      EHENY ZRGEB NZOHYNAPR n ",
+        "QNGR:04/29/13",
+        "GVZR:03:32",
         "NQQE:3377 SENAXYVA FGERRG EQ",
         "PVGL:FRAARGG",
         "VASB:Pneqvnp UNQ N URNEG NGGNPX 2 JRRXF NTB 61 LB SRZNYR OHEAVAT PURFG CNVA R911 Vasb - Pynff bs Freivpr: ERFQ Fcrpvny Erfcbafr Vasb: PNL PB FUREVSS & AL FG CBYVPR FRAARGG SQ EHENY ZRGEB NZOHYNAPR n");
 
     qbGrfg("G2",
         "(Sebz 911 Pragre) 04/28/13 12:06 JRFG YNXR EQ/URA PBBC EQ BABAQNTN PBHAGL Fgehpgher Sver ",
+        "QNGR:04/28/13",
+        "GVZR:12:06",
         "NQQE:JRFG YNXR EQ & URA PBBC EQ",  // Tbbtyr xabjf guvf nf URAPBBC EQ
         "PVGL:BABAQNTN PBHAGL",
         "PNYY:Fgehpgher Sver");
 
     qbGrfg("G3",
         "(Sebz 911 Pragre) 04/27/13 09:27 3 RYFBA YA/FXNARNGYRF BABAQNTN PBHAGL Nffvfg ol Sver  FGEHPGHER SVER  BSS R YNXR EQ  GNAXRE BHG BS FRAARGG ",
+        "QNGR:04/27/13",
+        "GVZR:09:27",
         "NQQE:3 RYFBA YA & FXNARNGYRF",
         "ZNQQE:3 RYFBA YA",
         "PVGL:BABAQNTN PBHAGL",
@@ -169,18 +216,24 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
     qbGrfg("G4",
         "(Sebz 911 Pragre) 04/26/13 12:55 2 WBUA FZVGU NIR FRAARGG Frvmherf  85 LB S FRVMHER ",
+        "QNGR:04/26/13",
+        "GVZR:12:55",
         "NQQE:2 WBUA FZVGU NIR",
         "PVGL:FRAARGG",
         "PNYY:Frvmherf 85 LB S FRVMHER");
 
     qbGrfg("G5",
         "(Sebz 911 Pragre) 04/26/13 07:30 7581 CNEPRYY EQ FRAARGG Frvmherf  8 LB ZNYR NPGVIR FRVMHERF ",
+        "QNGR:04/26/13",
+        "GVZR:07:30",
         "NQQE:7581 CNEPRYY EQ",
         "PVGL:FRAARGG",
         "VASB:Frvmherf 8 LB ZNYR NPGVIR FRVMHERF");
 
     qbGrfg("G6",
         "(Sebz 911 Pragre) 04/25/13 19:34 2616 ZNAEBJ EQ FRAARGG Pneqvnp  85 LB S  JNEZ SRRYVAT VA PURFG ",
+        "QNGR:04/25/13",
+        "GVZR:19:34",
         "NQQE:2616 ZNAEBJ EQ",
         "PVGL:FRAARGG",
         "VASB:Pneqvnp 85 LB S JNEZ SRRYVAT VA PURFG");
@@ -188,42 +241,56 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
     qbGrfg("G7",
         "(Sebz 911 Pragre) 04/24/13 05:05 QVYNWF7430 A  FGERRG EQ FRAARGG Oyrrqvat  51 LB ZNYR  ABFR OYRRQ  EBBZ 15 ",
         "CYNPR:QVYNWF",
+        "QNGR:04/24/13",
+        "GVZR:05:05",
         "NQQE:7430 A FGERRG EQ",
         "PVGL:FRAARGG",
         "VASB:Oyrrqvat 51 LB ZNYR ABFR OYRRQ EBBZ 15");
 
     qbGrfg("G8",
         "(Sebz 911 Pragre) 04/19/13 17:10 QRCBG EQ SERRZNA EQ FRAARGG Unmneq - Sver  RNFG BS VAGREFRPGVBA  JVER QBJA VA GUR EBNQ ",
+        "QNGR:04/19/13",
+        "GVZR:17:10",
         "NQQE:QRCBG EQ & SERRZNA EQ",
         "PVGL:FRAARGG",
         "VASB:Unmneq - Sver RNFG BS VAGREFRPGVBA JVER QBJA VA GUR EBNQ");
 
     qbGrfg("G9",
         "(Sebz 911 Pragre) 04/19/13 08:00 2 WBUA FZVGU NIR FRAARGG Frvmherf ",
+        "QNGR:04/19/13",
+        "GVZR:08:00",
         "NQQE:2 WBUA FZVGU NIR",
         "PVGL:FRAARGG",
         "PNYY:Frvmherf");
 
     qbGrfg("G10",
         "(Sebz 911 Pragre) 04/18/13 17:23 ZVYYRE EQ PBHAGL YVAR EQ FRAARGG Unmneq - Sver  FBERYYB ERF NERN  GERR VF ABG VA EBNQ OHG JVERF NER  FBZR JVERF QBJA NAQ CNEG BS N GERR  NPEBFF SEBZ CRGREF SNEZ  JRFG NOBHG 500 SG ",
+        "QNGR:04/18/13",
+        "GVZR:17:23",
         "NQQE:ZVYYRE EQ & PBHAGL YVAR EQ",
         "PVGL:FRAARGG",
         "VASB:Unmneq - Sver FBERYYB ERF NERN GERR VF ABG VA EBNQ OHG JVERF NER FBZR JVERF QBJA NAQ CNEG BS N GERR NPEBFF SEBZ CRGREF SNEZ JRFG NOBHG 500 SG");
 
     qbGrfg("G11",
         "(Sebz 911 Pragre) 04/13/13 19:06 3377 SENAXYVA FGERRG EQ FRAARGG Pneqvnp  SEBAG QBBE VF BCRA  SERAPU ERFVQRAPR  61 LB S PURFG CNVAF ",
+        "QNGR:04/13/13",
+        "GVZR:19:06",
         "NQQE:3377 SENAXYVA FGERRG EQ",
         "PVGL:FRAARGG",
         "VASB:Pneqvnp SEBAG QBBE VF BCRA SERAPU ERFVQRAPR 61 LB S PURFG CNVAF");
 
     qbGrfg("G12",
         "(Sebz 911 Pragre) 04/12/13 20:41 7822 CNEPRYY EQ FRAARGG Qvssvphygl Oernguvat/FBO  77 lb  z  R911 Vasb - Pynff bs Freivpr: ERFQ Fcrpvny Erfcbafr Vasb: PNL. PB. FUREVSS & AL FG CBYVPR FRAARGG SQ                      JRRQFCBEG NZOHYNAPR ",
+        "QNGR:04/12/13",
+        "GVZR:20:41",
         "NQQE:7822 CNEPRYY EQ",
         "PVGL:FRAARGG",
         "VASB:Qvssvphygl Oernguvat / FBO 77 lb z R911 Vasb - Pynff bs Freivpr: ERFQ Fcrpvny Erfcbafr Vasb: PNL PB FUREVSS & AL FG CBYVPR FRAARGG SQ JRRQFCBEG NZOHYNAPR");
 
     qbGrfg("G13",
         "(Sebz 911 Pragre) 04/10/13 14:58 RAGRECEVFR ERAGNY329 TENAG NIRAHR EQ FRAARGG Trareny Vyyarff  80 ZNYR ABG SRRYVAT  R911 Vasb - Pynff bs Freivpr: PAGK Fcrpvny Erfcbafr Vasb: PNL. PB. FUREVSS & AL FG CBYVPR FRAARGG SQ                      EHENY ZRGEB NZOHYNAPR j ",
+        "QNGR:04/10/13",
+        "GVZR:14:58",
         "CYNPR:RAGRECEVFR ERAGNY",
         "NQQE:329 TENAG NIRAHR EQ",
         "PVGL:FRAARGG",
@@ -231,6 +298,8 @@ choyvp pynff ALPnlhtnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
 
     qbGrfg("G14",
         "(Sebz 911 Pragre) 04/09/13 14:54 FURCUREQ EQ JRRQFCBEG FRAARGG EQ FRAARGG ZINCV  zbgbeplyr nppvqrag/2 crbcyr jvgu vawhevrf ",
+        "QNGR:04/09/13",
+        "GVZR:14:54",
         "NQQE:FURCUREQ EQ & JRRQFCBEG FRAARGG EQ",
         "PVGL:FRAARGG",
         "VASB:ZINCV zbgbeplyr nppvqrag / 2 crbcyr jvgu vawhevrf");
