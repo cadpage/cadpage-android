ba992d0a0864f28ae4f76e754c3aac13f1b533b5
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-05-29 08:23:05 +0000

    Fixed parsing problem with Denton County, TX

--- cadpage/src/net/anei/cadpage/parsers/TX/TXDentonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXDentonCountyParser.java
@@ -12,7 +12,7 @@ public class TXDentonCountyParser extends DispatchOSSIParser {
   
   public TXDentonCountyParser() {
     super(CITY_LIST, "DENTON COUNTY", "TX",
-          "FYI? ( ID ( DATIME SRC? CALL NAME? ADDR/s X/Z+? CITY | ADDR X/Z+? CITY SRC? CALL ) | CALL ADDR CITY ) INFO+");
+          "ID?: ( CANCEL ADDR SRC! INFO+ | FYI? ID ( DATIME ( SRC CALL ( ADDR/Z CITY! | NAME? ADDR! X/Z+? CITY ) | CALL NAME? ADDR/Z CITY! X/Z+? SRC UNIT? INFO+ ) | ADDR X/Z+? CITY SRC CALL! ) )");
   }
   
   @Override
@@ -24,93 +24,53 @@ public class TXDentonCountyParser extends DispatchOSSIParser {
   public String getSponsor() {
     return "Lake Cities Fire Department";
   }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (subject.equals("Message Forwarded by PageGate") && !body.startsWith("CAD:")) body = "CAD:" + body;
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strCity.length() == 4) {
-      String city = CITY_CODES.getProperty(data.strCity);
-      if (city != null) {
-        if (data.strSource.length() == 0) data.strSource = data.strCity;
-        data.strCity = city;
-      }
-    }
-    return true;
-  }
-
-  private class MyIdField extends IdField {
-    public MyIdField() {
-      setPattern(Pattern.compile("\\d{9}"));
-    }
-  }
   
-  
-  
-  private static final Pattern SOURCE_PTN = Pattern.compile("[A-Z]{2}FD"); 
-  private class MySourceField extends SourceField {
-    public MySourceField() {
-      setPattern(SOURCE_PTN);
-    }
-  }
-  
-  
+  private static final Pattern ADDR_PTN = Pattern.compile("\\d+ .*");
   private static final Pattern SERV_PTN = Pattern.compile("\\bSERV\\b");
   private class MyAddressField extends AddressField {
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!ADDR_PTN.matcher(field).matches() && checkAddress(field) == 0) return false;
+      parse(field, data);
+      return true;
+    }
+    
     @Override
     public void parse(String field, Data data) {
       field = SERV_PTN.matcher(field).replaceAll("FRONTAGE");
       super.parse(field, data);
     }
   }
-  
-  private class MyInfoField extends InfoField {
+ 
+  // Throw an error if  cross fields contain more than 2 streets
+  private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
-      Matcher match = SOURCE_PTN.matcher(field);
-      if (match.matches()) {
-        data.strSource = field;
-        if (data.strCity.length() == 0) data.strCity = field;
-        return;
-      }
-      if (checkAddress(field) > 0) {
-        data.strCross = append(data.strCross, " & ", field);
-      } else {
-        super.parse(field, data);
-      }
+      if (data.strCross.contains("&")) abort();
+      super.parse(field, data);
     }
   }
   
-  
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new MyIdField();
-    if (name.equals("DATIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d");
-    if (name.equals("SRC")) return new MySourceField();
+    if (name.equals("CANCEL")) return new CallField("CANCEL", true);
+    if (name.equals("ID")) return new IdField("\\d{9}", true);
+    if (name.equals("DATIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
+    if (name.equals("SRC")) return new SourceField("[A-Z]{2}FD");
     if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+(?:,[A-Z]+\\d+)*", true);
     return super.getField(name);
   }
-  
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "ARFD", "ARGYLE",
-      "CORI", "CORINTH",
-      "LAKE", "LAKE DALLAS",
-      "LVFD", "LEWISVILLE",
-      "HICK", "HICKORY CREEK",
-      "SHAD", "SHADY SHORES",
-      "LCFD", ""
-  });
-  
-  private static final String[] CITY_LIST = new String[]{
-    "ARFD", 
-    "CORI", 
-    "LAKE", 
-    "LVFD", 
-    "HICK", 
-    "SHAD", 
-    "LCFD",
 
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.equals("Message Forwarded by PageGate") && !body.startsWith("CAD:")) body = "CAD:" + body;
+    return super.parseMsg(body, data);
+  }
+
+private static final String[] CITY_LIST = new String[]{
     "ARGYLE",
     "BARTONVILLE",
     "COPPER CANYON",
@@ -119,14 +79,89 @@ public class TXDentonCountyParser extends DispatchOSSIParser {
     "DENTON",
     "DOUBLE OAK",
     "FLOWER MOUND",
+    "JUSTIN",
     "HICKORY CREEK",
+    "KRUM",
     "LAKE DALLAS",
     "LEWISVILLE",
+    "LITTLE ELM",
     "NORTHLAKE",
     "HICKORY CREEK",
+    "PILOT POINT",
+    "SANGER",
     "SHADY SHORES",
+    "THE COLONY",
+    "TROPHY CLUB",
     
     "DENTON COUNTY"
     
   };
 }
+//  
+//  @Override
+//  public String getProgram() {
+//    return super.getProgram().replace(" CITY ", " SRC CITY ");
+//  }
+//  
+//  private class MyCity2Field extends CityField {
+//    @Override
+//    public boolean checkParse(String field, Data data) {
+//      if (field.length() == 4) return false;
+//      return super.checkParse(field, data);
+//    }
+//  }
+//  
+//  private static final Pattern SOURCE_PTN = Pattern.compile("[A-Z]{2}FD"); 
+//  private class MySourceField extends SourceField {
+//    public MySourceField() {
+//      setPattern(SOURCE_PTN);
+//    }
+//  }
+  
+//  
+//  private class MyInfoField extends InfoField {
+//    @Override
+//    public void parse(String field, Data data) {
+//      Matcher match = SOURCE_PTN.matcher(field);
+//      if (match.matches()) {
+//        data.strSource = field;
+//        if (data.strCity.length() == 0) data.strCity = field;
+//        return;
+//      }
+//      if (checkAddress(field) > 0) {
+//        data.strCross = append(data.strCross, " & ", field);
+//      } else {
+//        super.parse(field, data);
+//      }
+//    }
+//    
+//    @Override
+//    public String getFieldNames() {
+//      return "SRC CITY X INFO";
+//    }
+//  }
+  
+//  
+//  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+//      "ARFD", "ARGYLE",
+//      "CORI", "CORINTH",
+//      "LAKE", "LAKE DALLAS",
+//      "LEFD", "LITTLE ELM",
+//      "LVFD", "LEWISVILLE",
+//      "HICK", "HICKORY CREEK",
+//      "PPFD", "PILOT POINT",
+//      "SHAD", "SHADY SHORES",
+//      "TCFD", "THE COLONY",
+//      "LCFD", "LAKE CITIES",
+//  });
+  
+//    "ARFD", 
+//    "CORI", 
+//    "LAKE", 
+//    "LEFD",
+//    "LVFD", 
+//    "HICK", 
+//    "SHAD", 
+//    "PPFD",    
+//    "TCFD",
+//    "LCFD",
