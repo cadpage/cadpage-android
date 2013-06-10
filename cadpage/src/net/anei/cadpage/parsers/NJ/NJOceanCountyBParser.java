00fdace9db2daed8c12625d1487a0c3282d2a6ec
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-06-10 09:27:58 +0000

    skeleton redone for NJOceanCountyBParserTest.

---
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJOceanCountyBParser.java
@@ -0,0 +1,49 @@
+package net.anei.cadpage.parsers.NJ;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA9Parser;
+
+
+/**
+ * Ocean County, NJ
+ */
+public class NJOceanCountyBParser extends DispatchA9Parser {
+  
+  private static final Pattern CITY_PTN = Pattern.compile("(.*?) +(?:BORO|- NON SPECIFI?C?)", Pattern.CASE_INSENSITIVE);
+  
+  public NJOceanCountyBParser() {
+    super(null, "OCEAN COUNTY", "NJ");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "frank@station11.org";
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    
+    // Correct double spacing by iamresponding
+    if (body.startsWith("Rip and Run Report\n\n\n\n~")) body = body.replace("\n\n", "\n"); 
+    
+    if (!super.parseMsg(body, data)) return false;
+    
+    // Dispatch seems to be storing the station code in the map fields
+    data.strSource = data.strMap;
+    data.strMap = "";
+    
+    // Have to clean up some city fields
+    Matcher match = CITY_PTN.matcher(data.strCity);
+    if (match.matches()) data.strCity = match.group(1);
+    if (data.strCity.endsWith(".")) data.strCity = data.strCity.substring(0,data.strCity.length()-1).trim();
+    return true;
+  }
+  
+  @Override
+  public String getProgram() {
+    return "SRC " + super.getProgram();
+  }
+}
