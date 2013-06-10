73e5a3c6e6727f1e875b01ff9b43cb108b5287f5
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-06-10 19:08:29 +0000

    Fixed parsing problem with WOrcester County, MD (A)

--- cadpage/src/net/anei/cadpage/parsers/MD/MDWorcesterCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDWorcesterCountyAParser.java
@@ -1,7 +1,5 @@
 package net.anei.cadpage.parsers.MD;
 
-import java.util.regex.Pattern;
-
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
@@ -10,19 +8,12 @@ public class MDWorcesterCountyAParser extends DispatchOSSIParser {
  
   public MDWorcesterCountyAParser() {
     super(MDWorcesterCountyParser.CITY_LIST, "WORCESTER COUNTY", "MD",
-    		   "SRC? CALL ADDR PLACE? CITY! X X INFO+ DATETIME!");
-  }
-  
-  private static final Pattern SOURCE_PAT = Pattern.compile("[0-9]{1,2}00[A-Z]?|S[0-9]");
-  private class MySourceField extends SourceField {
-    public MySourceField() {
-      setPattern(SOURCE_PAT);
-    }
+    		   "SRC? CALL ADDR PLACE? CITY! X X INFO+ DATETIME");
   }
   
   @Override
   protected Field getField(String name) {
-    if (name.equals("SRC")) return new MySourceField();
+    if (name.equals("SRC")) return new SourceField("[0-9]{1,2}00[A-Z]?|S[0-9]", true);
     return super.getField(name);
   }
 }
