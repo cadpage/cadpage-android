3cca015e7fc98d329409843009ffda5cc46352ac
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-06-15 23:44:44 +0000

    Fixed parsing problem with Genesee County, NY

--- cadpage/src/net/anei/cadpage/parsers/NY/NYGeneseeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYGeneseeCountyParser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.NY;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -8,9 +11,13 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class NYGeneseeCountyParser extends FieldProgramParser {
   
+  private static final Pattern UNIT_PREFIX_PTN = Pattern.compile("^(?:Unit: *([A-Z0-9]+) +)?Status: *(?:Dispatched|At Scene) *(?:\\*\\* +)?");
+  
+  private boolean skipApt;
+  
   public NYGeneseeCountyParser() {
     super(CITY_LIST, "GENESEE COUNTY", "NY",
-           "Unit:UNIT? Status:CALL! PLACE? ADDR CITY? INFO! X DATETIME ID");
+           "CALL PLACE ADDR APT? CALL2! X DATETIME ID INFO+");
   }
   
   @Override
@@ -34,16 +41,17 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
     
     int pt = body.indexOf('\n');
     if (pt >= 0) body = body.substring(0,pt).trim();
-    String saveBody = body;
     
-    if (body.startsWith("Unit:")) {
-      body = body.replace(" Status:", "**Status:");
-    } else if(!body.startsWith("Status:")) {
-      body = "Status: " + body;
+    Matcher match = UNIT_PREFIX_PTN.matcher(body);
+    if (match.find()) {
+      data.strUnit = match.group(1);
+      body = body.substring(match.end());
+    } else if (body.startsWith("** ")) {
+      body = body.substring(3).trim();
     }
     
-    String flds[] = body.split("\\*\\*");
-    return parseFields(flds, 3, data) ||
+    String saveBody = body;
+    return parseFields(body.split("\\*\\*"), 4, data) ||
             data.parseGeneralAlert(this, saveBody);
   }
   
@@ -51,57 +59,35 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
   public String getProgram() {
     return "UNIT " + super.getProgram();
   }
-  
-  private class MyCallField extends CallField {
-    @Override
-    public void parse(String field, Data data) {
-      if (field.startsWith("Dispatched ")) field = field.substring(11).trim();
-      if (field.endsWith("*")) field = field.substring(0,field.length()-1).trim();
-      super.parse(field, data);
-    }
-  }
-  
+
   private class MyAddressField extends AddressField {
     
     @Override
-    public boolean checkParse(String field, Data data) {
+    public void parse(String field, Data data) {
       
       // remove leading asterisk
       if (field.startsWith("*")) field = field.substring(1).trim();
       
       // If field contains comma, parse as address and cross / city
       String sApt = "";
+      skipApt = false;
       int pt = field.indexOf(',');
-      if (pt >= 0) {
-	      String sAddr = field.substring(0,pt).trim();
-	      String sCity = field.substring(pt+1).trim();
-	      int pt2 = sCity.lastIndexOf('-');
-	      if (pt2 >= 0) {
-	        sApt = sCity.substring(pt2+1).trim();
-	        if (sApt.startsWith("APT ")) sApt = sApt.substring(4);
-	        sCity = sCity.substring(0,pt2).trim();
-	      }
-	      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_ONLY_CITY | FLAG_ANCHOR_END, sCity, data);
-        parseAddress(sAddr, data);
-        data.strApt = append(data.strApt, "-", sApt);
-	      return true;
+      if (pt < 0) abort();
+      String sAddr = field.substring(0,pt).trim();
+      String sCity = field.substring(pt+1).trim();
+      int pt2 = sCity.indexOf('-');
+      if (pt2 >= 0) {
+        skipApt = true;
+        sApt = sCity.substring(pt2+1).trim();
+        if (sApt.startsWith("APT ")) sApt = sApt.substring(4);
+        sCity = sCity.substring(0,pt2).trim();
       }
-      
-      // Otherwise, treat it as an address if it starts with a digit
-      // or is recognized as an address
-      if (field.length() < 1) return false;
-      if (Character.isDigit(field.charAt(0)) || checkAddress(field) > 0) {
-        parseAddress(field, data);
-        return true;
-      }
-      
-      // Otherwise, not a valid address field
-      return false;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
+      parseAddress(sAddr, data);
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_ONLY_CITY | FLAG_ANCHOR_END, sCity, data);
+      if (data.strCity.equals("TONAWANDA IR")) data.strCity = "TONAWANDA INDIAN RESERVATION";
+      data.strAddress = append(data.strAddress, " & ", data.strCross);
+      data.strCross = "";
+      data.strApt = append(data.strApt, "-", sApt);
     }
     
     @Override
@@ -110,7 +96,7 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
     }
   }
   
-  private class MyCityField extends CityField {
+  private class MyAptField extends AptField {
     
     @Override
     public boolean canFail() {
@@ -120,22 +106,36 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
     @Override
     public boolean checkParse(String field, Data data) {
       
-      // If we got the city from the address field, we don't need this one
-      if (data.strCity.length() > 0) return false;
-      
-      // Strip off trailing dash
-      if (! field.startsWith(",")) return false;
-      if (! field.endsWith("-")) return false;
-      data.strCity = field.substring(1, field.length()-1).trim();
+      // Older formats included apt in address field
+      // if this is one of those, skip this field
+      if (skipApt) return false;
+      parse(field, data);
       return true;
+    }
+  }
+  
+  private class MyCall2Field extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strCall = append(data.strCall, " - ", field);
+    }
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
     }
   }
 
   @Override
   protected Field getField(String name) {
-    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("CALL")) return new CallField("[^:]*", true);
     if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("CITY")) return new MyCityField();
+    if (name.equals("APT")) return new MyAptField();
+    if (name.equals("CALL2")) return new MyCall2Field();
+    if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
   
@@ -156,6 +156,8 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
     "PAVILION",
     "PEMBROKE",
     "STAFFORD",
+    
+    "TONAWANDA IR",
 
     // Erie County
     "ALDEN",
@@ -164,9 +166,22 @@ public class NYGeneseeCountyParser extends FieldProgramParser {
     // Wyoming County
     "ATTICA",
     "BENNINGTON",
+    "WARSAW",
     
     // Erie County,
     "CLARENCE",
-    "NEWSTEAD"
+    "NEWSTEAD",
+    
+    // Orleans County
+    "BARRE",
+    "CLARENDON",
+    "SHELBY",
+    
+    // Livingston County
+    "CALEDONIA",
+    "YORK",
+    
+    // Monroe County
+    "RIGA"
   };
 }
