4d99c89ae0eea1119e7c2661edea321ace88a3e1
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-06-24 15:32:24 +0000

    Release v1.6.30

--- cadpage/src/net/anei/cadpage/parsers/LA/LAStCharlesParishParser.java
+++ cadpage/src/net/anei/cadpage/parsers/LA/LAStCharlesParishParser.java
@@ -1,26 +1,8 @@
 package net.anei.cadpage.parsers.LA;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
 
 public class LAStCharlesParishParser extends DispatchB2Parser {
-
-  private static final String[] CITY_LIST = new String[] {
-   
-"AMA", 
-"BAYOU GAUCHE",
-"BOUTTE",
-"DES ALLEMANDS",
-"DESTREHAN",
-"HAHNVILLE",
-"KILLONA",
-"LULING",
-"MONTZ",
-"NEW SARPY",
-"NORCO",
-"PARADIS",
-"ST. ROSE",
-"TAFT"};
 		
   public LAStCharlesParishParser() {
     super("911:", CITY_LIST, "ST CHARLES PARISH", "LA");
@@ -31,5 +13,22 @@ public class LAStCharlesParishParser extends DispatchB2Parser {
   protected boolean isPageMsg(String body) {
     return true;
   }
+
+  private static final String[] CITY_LIST = new String[] {
+    "AMA", 
+    "BAYOU GAUCHE",
+    "BOUTTE",
+    "DES ALLEMANDS",
+    "DESTREHAN",
+    "HAHNVILLE",
+    "KILLONA",
+    "LULING",
+    "MONTZ",
+    "NEW SARPY",
+    "NORCO",
+    "PARADIS",
+    "ST ROSE",
+    "TAFT"
+  };
 
 }
