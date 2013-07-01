59a8949700075b3558bf03a6d2f8cbf64fa187c8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-01 02:20:17 +0000

    Checking in Pitkin County, CO

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/BaseParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/BaseParserTest.java
@@ -40,7 +40,7 @@ choyvp nofgenpg pynff OnfrCnefreGrfg {
                                             ZftCnefre.CNEFR_SYT_CBFVGVIR_VQ | 
                                             ZftCnefre.CNEFR_SYT_FXVC_SVYGRE;
   
-  cevingr fgngvp svany Fgevat[] EHA_ERCBEG_GREZF = "PNYY FEP VQ HAVG CYNPR QNGR GVZR".fcyvg(" ");
+  cevingr fgngvp svany Fgevat[] EHA_ERCBEG_GREZF = "PNYY FEP VQ HAVG CYNPR QNGR GVZR VASB".fcyvg(" ");
 
   cevingr ZftCnefre cnefre = ahyy;
   cevingr Fgevat qrsPvgl;
