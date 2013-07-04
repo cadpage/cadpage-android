15d80132ab47c17375ecfe41a372052c1deb7c56
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-04 19:48:53 +0000

    Skeleton for NHMerrimackCountyParserTest is complete with one small section.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NH/NHMerrimackCountyParserTest.java
@@ -0,0 +1,40 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AU;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Zreevznpx Pbhagl, AU
+Ybpngvba: Arj Ybaqba, AU, Havgrq Fgngrf
+Pbagnpg: Npgvir911
+Ntrapl anzr: Arj Ybaqba Sver Qrcg.
+Fraqre: qvfcngpu@arjybaqbacq.hf
+
+(PNQ) RIRAG:0016\aGVZR:07/01/2013 11:32:59\aGLCR:YBT/Ybt Abgr\aYBP:25 FRNZNAF EQ\aGKG:GRFG BS GUR NPGVIR 911 SEBZ QVFCNGPU @1159
+(PNQ) RIRAG:0018\aGVZR:06/17/2013 14:54:22\aGLCR:YBT/Ybt Abgr\aYBP:ARJ YBAQBA\aGKG:GRFG BS NPGVIR 911 SEBZ QVFCNGPU
+(PNQ) RIRAG:0041\aGVZR:04/22/2013 18:10:12\aGLCR:SVER N/Sver Nynez\aYBP:450 UNYY SNEZ EQ\aGKG:ERDHRFG SBE SVER QRCNEGZRAG GB ERFCBAQ SBE N TNF NYN
+(PNQ) RIRAG:0054\aGVZR:04/17/2013 23:30:29\aGLCR:SVER/Sver Pnyy\aYBP:9 FNJLRE ZRNQBJ EQ\aGKG:ERDHRFG SBE N SVEFG NYNEZ VA GUR GBJA BS PEBLQBA SBE 
+(PNQ) RIRAG:0021\aGVZR:04/15/2013 15:30:46\aGLCR:NPP/Genssvp Nppvqrag\aYBP:ZNVA FG/QHAXVA QBAHG\aGKG:
+(PNQ) RIRAG:0011\aGVZR:03/11/2013 07:40:49\aGLCR:SVER/Sver Pnyy\aYBP:112EG4\aGKG:88 ERDHRFG N ARJ YBAQBA RATVAR GB ERFCBAQ GB NOBIR SBE NA RYRPGEV
+(PNQ) RIRAG:0011\aGVZR:03/11/2013 07:40:49\aGLCR:SVER/Sver Pnyy\aYBP:112EG4\aGKG:
+(PNQ) RIRAG:0108\aGVZR:03/05/2013 10:10:44\aGLCR:SVER/Sver Pnyy\aYBP:237 ZNVA FG\aGKG:GRFG PNYY SBE AYSQ
+
+*/
+
+choyvp pynff AUZreevznpxPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp AUZreevznpxPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "ZREEVZNPX PBHAGL", "AU");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+      
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj AUZreevznpxPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
