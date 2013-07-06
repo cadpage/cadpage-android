cf9f6b99ab6c2eede9a3f388c88857ebe4299d9a
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-06 16:52:05 +0000

    Beta release Fixed parsing problem with Ocean City, MD

--- cadpage/src/net/anei/cadpage/parsers/MD/MDOceanCityParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MD/MDOceanCityParser.java
@@ -1,11 +1,14 @@
 package net.anei.cadpage.parsers.MD;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchAegisParser;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
 
-public class MDOceanCityParser extends DispatchAegisParser {
+public class MDOceanCityParser extends DispatchOSSIParser {
 
   private String address1 = null;
   private String address2 = null;
@@ -14,12 +17,12 @@ public class MDOceanCityParser extends DispatchAegisParser {
   
   @Override
   public String getFilter() {
-    return "msg@cfmsg.com,alert@cfmsg.com";
+    return "CAD@mail.oceancitymd.gov";
   }
   
   public MDOceanCityParser() {
     super("OCEAN CITY", "MD",
-           "Nature:CALL! LOC:ADDR PLACE ADD:ADDR Cross_STS:X");
+          "( CANCEL ADDR! INFO+ | FYI ( DATETIME CALL ADDR! | CALL APT? ADDR/Z+? DATETIME! X+ ) )");
   }
   
   @Override
@@ -30,25 +33,48 @@ public class MDOceanCityParser extends DispatchAegisParser {
     addrStat1 = -1;
     addrStat2 = -1;
     
-    // The OCMD city codes just confused things, better to get rid of them
-    body = body.replaceAll(" OCMD ", " ");
     if (! super.parseMsg(subject, body, data)) return false;
     
     // Pick the best address as the address
     // If no place name was found, use the second best address
     if (address1 == null) return false;
     parseAddress(address1, data);
-    if (data.strPlace.length() == 0 && address2 != null) data.strPlace = address2;
+    if (address2 != null) data.strPlace = address2;
     return true;
   }
   
-  // This text format has two address fields, and the real address might
+  
+  private Pattern APT_PTN = Pattern.compile("(?:UNIT|APT|ROOM|RM) +(.*)|(?:SUITE|LOT) +.*|.*\\b(?:FLOOR|FLR)\\b.*|[A-Z0-9]{1,3}", Pattern.CASE_INSENSITIVE);
+  private class MyAptField extends AptField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = APT_PTN.matcher(field);
+      if (!match.matches()) return false;
+      String apt = match.group(1);
+      if (apt == null) apt = field;
+      data.strApt = apt;
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+  
+  // This text format has 1-3 address fields, and the real address might
   // be in either one.
+  private Pattern DIR_PTN = Pattern.compile("\\([NS]\\)");
   private class MyAddressField extends AddressField {
 
     @Override
     public void parse(String field, Data data) {
-      int status = checkAddress(field);
+      int status = (DIR_PTN.matcher(field).find() ? -1 : checkAddress(field));
       if (address1 == null || status > addrStat1) {
         address2 = address1;
         addrStat2 = addrStat1;
@@ -62,18 +88,16 @@ public class MDOceanCityParser extends DispatchAegisParser {
 
     @Override
     public String getFieldNames() {
-      return "ADDR PLACE X CITY";
+      return "ADDR PLACE";
     }
   }
 
   @Override
   protected Field getField(String name) {
+    if (name.equals("CANCEL")) return new CallField("CANCEL");
+    if (name.equals("APT")) return new MyAptField();
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} +\\d\\d:\\d\\d:\\d\\d", true);
     return super.getField(name);
-  }
-  
-  @Override
-  public String getProgram() {
-    return "SRC " + super.getProgram();
   }
 }
