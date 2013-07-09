06959f29ecb5dc2e975853060a9ef2a3bf6fda82
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-09 02:30:03 +0000

    Fixed parsing problem with Clark County, OH

--- cadpage/src/net/anei/cadpage/parsers/OH/OHClarkCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OH/OHClarkCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
+
 import net.anei.cadpage.parsers.dispatch.DispatchA5Parser;
 
 /**
@@ -8,11 +10,15 @@ import net.anei.cadpage.parsers.dispatch.DispatchA5Parser;
 public class OHClarkCountyParser extends DispatchA5Parser {
   
   public OHClarkCountyParser() {
-    super("CLARK COUNTY", "OH");
+    super(CITY_CODES, "CLARK COUNTY", "OH");
   }
   
   @Override
   public String getFilter() {
     return "E911-122@CI.SPRINGFIELD.OH.US";
   }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "N CARLISLE",     "NEW CARLISLE"
+  });
 }
