26411e8b38b73e90dc6c8000ecb421ddee477b31
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-14 18:48:21 +0000

    Checked in Charlotte County, FL location

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJMercerCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJMercerCountyParser.java
@@ -3,62 +3,22 @@ package net.anei.cadpage.parsers.NJ;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA24Parser;
 
 
 /**
  * Mercer County, NJ
  */
-public class NJMercerCountyParser extends FieldProgramParser {
-  
-  private static final Pattern RUN_REPORT_PTN = Pattern.compile("; Disp: ?\\d\\d:\\d\\d;");
-  
-  private static final Pattern UNIT_PTN = Pattern.compile("^UNIT: *([ A-Z0-9]+?) *; *", Pattern.CASE_INSENSITIVE);
-  
+public class NJMercerCountyParser extends DispatchA24Parser {
   
   public NJMercerCountyParser() {
-    super("MERCER COUNTY", "NJ",
-           "CALL:CALL! PLACE:PLACE! ADDR:ADDR! BLDG:APT! APT:APT! CITY:CITY! XSTREETS:X ID:ID! DATE:DATE! TIME:TIME! INFO:INFO");
+    super("MERCER COUNTY", "NJ");
   }
   
   @Override
   public String getFilter() {
     return "noreply_lifecomm@verizon.net";
-  }
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    
-    if (RUN_REPORT_PTN.matcher(body).find()) {
-      data.strCall = "RUN REPORT";
-      data.strPlace = body;
-      return true;
-    }
-    
-    Matcher match = UNIT_PTN.matcher(body);
-    if (!match.find()) return false;
-    data.strUnit = match.group(1);
-    body = body.substring(match.end());
-    
-    if (super.parseFields(body.split(";"), data)) return true;
-    data.strCall = "GENERAL ALERT";
-    data.strPlace = body;
-    return true;
-  }
-  
-  @Override
-  public String getProgram() {
-    return "UNIT " + super.getProgram();
-  }
-  
-  private class MyTimeField extends TimeField {
-    @Override
-    public void parse(String field, Data data) {
-      int pt = field.indexOf('(');
-      if (pt >= 0) field = field.substring(0,pt).trim();
-      super.parse(field, data);
-    }
   }
   
   private static final Pattern INFO_GPS_PTN = Pattern.compile("^Longitude: ([+-]\\d+\\.\\d+),Latitude: ([+-]\\d+\\.\\d+),");
@@ -81,8 +41,8 @@ public class NJMercerCountyParser extends FieldProgramParser {
   
   @Override
   public Field getField(String name) {
-    if (name.equals("TIME")) return new MyTimeField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
+
 }
