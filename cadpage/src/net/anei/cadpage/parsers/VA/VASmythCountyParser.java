82bc8b0b22fe3991036296099d91669210dbe180
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-07-18 08:05:27 +0000

    VASmythCountyParser is completed.

---
+++ cadpage/src/net/anei/cadpage/parsers/VA/VASmythCountyParser.java
@@ -0,0 +1,28 @@
+package net.anei.cadpage.parsers.VA;
+
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+
+
+
+public class VASmythCountyParser extends DispatchSouthernParser {
+  
+  public VASmythCountyParser() {
+    super(CITY_LIST, "SMYTH COUNTY", "VA", DSFLAG_DISPATCH_ID | DSFLAG_LEAD_PLACE);
+  }
+
+  @Override
+  public String getFilter() {
+    return "@smythcounty.org";
+  }
+  
+  private static final String[] CITY_LIST = new String[]{
+    "CHILHOWIE",
+    "MARION",
+    "SALTVILLE",
+    
+    "ADWOLF",
+    "ATKINS",
+    "SEVEN MILE FORD",
+    "SUGAR GROVE"
+  }; 
+}
