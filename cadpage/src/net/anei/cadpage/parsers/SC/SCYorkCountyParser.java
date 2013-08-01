44d58f2485bbe9c3c61a9f94297485f10f874f02
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-01 00:43:21 +0000

    Added priority parsering to York County, SC

--- cadpage/src/net/anei/cadpage/parsers/SC/SCYorkCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCYorkCountyParser.java
@@ -1,51 +1,44 @@
 package net.anei.cadpage.parsers.SC;
 
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.MsgParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 
-public class SCYorkCountyParser extends MsgParser {
-  
-  private static final String[] KEYWORDS = new String[]{"UNIT", "P", "LOC", "X", "NAR", "INC#"}; 
+public class SCYorkCountyParser extends FieldProgramParser {
 
   public SCYorkCountyParser() {
-    super("YORK COUNTY", "SC");
-    setFieldList("UNIT ADDR X CITY PLACE CALL INFO ID");
+    super("YORK COUNTY", "SC",
+          "UNIT P:PRI! LOC:ADDR! X:X! NAR:INFO INC#:ID");
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      
+      Parser p = new Parser(field);
+      data.strCross = p.get('*');
+      data.strPlace = p.get('*');
+      data.strCity = p.get('*');
+      data.strPlace = append(data.strPlace, " - ", p.get('*'));
+      data.strCall = p.get('*') + " - " + p.get('*');
+
+      if (data.strPlace.length() > 0) {
+        String tmp = new Parser(data.strPlace).get(' ');
+        int ipt = data.strCity.indexOf(" " + tmp);
+        if (ipt >= 0) data.strCity = data.strCity.substring(0,ipt).trim();
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "X PLACE CITY CALL";
+    }
   }
 
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    
-    Properties props = parseMessage("UNIT: " + body, KEYWORDS);
-    if (props.getProperty("P") == null) return false;
-    
-    data.strUnit = props.getProperty("UNIT", "");
-    String sAddr = props.getProperty("LOC");
-    if (sAddr == null) return false;
-    parseAddress(sAddr, data);
-    
-    body = props.getProperty("X");
-    if (body == null) return false;
-    
-    Parser p = new Parser(body);
-    data.strCross = p.get('*');
-    data.strPlace = p.get('*');
-    data.strCity = p.get('*');
-    data.strPlace = append(data.strPlace, " - ", p.get('*'));
-    data.strCall = p.get('*') + " - " + p.get('*');
-    
-    data.strSupp = props.getProperty("NAR", "");
-    data.strCallId = props.getProperty("INC#", "");
-
-    if (data.strPlace.length() > 0) {
-      String tmp = new Parser(data.strPlace).get(' ');
-      int ipt = data.strCity.indexOf(" " + tmp);
-      if (ipt >= 0) data.strCity = data.strCity.substring(0,ipt).trim();
-    }
-
-    return true;
+  public Field getField(String name) {
+    if (name.equals("X")) return new MyCrossField();
+    return super.getField(name);
   }
 }
