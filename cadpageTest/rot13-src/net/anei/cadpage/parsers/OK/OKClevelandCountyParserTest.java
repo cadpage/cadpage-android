76c41979f62d1f193cdd26978e603fcdbf0c77b2
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-04 20:35:47 +0000

    Checked in Cleveland County, OK

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/OK/OKClevelandCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OK/OKClevelandCountyParserTest.java
@@ -11,6 +11,15 @@ Ntrapl anzr: Yvggyr Nkr Sver Qrcnegzrag
 Ybpngvba: Yvggyr Nkr, BX, Havgrq Fgngrf
 Fraqre: qvfcngpu.vasb@abeznabx.tbi
 
+(PNQ) SVER TENFF ERQ BNX YA & R SENAXYVA EQ RFA: 92
+(PNQ) SVER ERFVQRAGVNY 6305 PEBBXRQ BNX QE RFA: 267
+(PNQ) FZBXR VAIRFGVTNGVBA R SENAXYVA EQ & 192AQ NIR AR RFA: 92
+(PNQ) SVER TENFF INYYRL IVRJ EQ & EBYYVAT ZRNQBJF RFA: 38
+(PNQ) SVER TENFF 18150 OVKYRE PG RFA: 230
+(PNQ) SVER TENFF 105 YVFN YA RFA: 38
+(PNQ) SVER TENFF 18000 R SENAXYVA EQ RFA: 92
+(PNQ) SVER TENFF R SENAXYVA EQ & 192AQ NIR AR RFA: 92
+(PNQ) SVER TENFF YVFN YA & 192AQ NIR AR RFA: 38
 (PNQ) FZBXR VAIRFGVTNGVBA 180GU NIR AR & R GRPHZFRU EQ RFA: 136
 (PNQ) SVER ERFVQRAGVNY 16650 192AQ NIR AR RFA: 38
 (PNQ) FZBXR VAIRFGVTNGVBA 18401 CRPNA PERRX QE RFA: 38
@@ -39,14 +48,194 @@ Fraqre: qvfcngpu.vasb@abeznabx.tbi
 choyvp pynff BXPyrirynaqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
   choyvp BXPyrirynaqPbhaglCnefreGrfg() {
-    frgCnefre(arj QhzzlCnefre(), "PYRIRYNAQ PBHAGL", "BX");
+    frgCnefre(arj BXPyrirynaqPbhaglCnefre(), "PYRIRYNAQ PBHAGL", "BX");
   }
  
   
   @Grfg
-  choyvp ibvq grfgCnefre() {
-    
-    
+  choyvp ibvq grfgYvggyrNkrSverQrcnegzrag() {
+
+    qbGrfg("G1",
+        "(PNQ) SVER TENFF ERQ BNX YA & R SENAXYVA EQ RFA: 92",
+        "PNYY:SVER TENFF",
+        "NQQE:ERQ BNX YA & R SENAXYVA EQ",  // Nppbeqvat gb tbbtyr, gurfr gb ab vagrefrpg
+        "VQ:92");
+
+    qbGrfg("G2",
+        "(PNQ) SVER ERFVQRAGVNY 6305 PEBBXRQ BNX QE RFA: 267",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:6305 PEBBXRQ BNX QE",
+        "VQ:267");
+
+    qbGrfg("G3",
+        "(PNQ) FZBXR VAIRFGVTNGVBA R SENAXYVA EQ & 192AQ NIR AR RFA: 92",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:R SENAXYVA EQ & 192AQ NIR AR",
+        "VQ:92");
+
+    qbGrfg("G4",
+        "(PNQ) SVER TENFF INYYRL IVRJ EQ & EBYYVAT ZRNQBJF RFA: 38",
+        "PNYY:SVER TENFF",
+        "NQQE:INYYRL IVRJ EQ & EBYYVAT ZRNQBJF",
+        "VQ:38");
+
+    qbGrfg("G5",
+        "(PNQ) SVER TENFF 18150 OVKYRE PG RFA: 230",
+        "PNYY:SVER TENFF",
+        "NQQE:18150 OVKYRE PG",
+        "VQ:230");
+
+    qbGrfg("G6",
+        "(PNQ) SVER TENFF 105 YVFN YA RFA: 38",
+        "PNYY:SVER TENFF",
+        "NQQE:105 YVFN YA",
+        "VQ:38");
+
+    qbGrfg("G7",
+        "(PNQ) SVER TENFF 18000 R SENAXYVA EQ RFA: 92",
+        "PNYY:SVER TENFF",
+        "NQQE:18000 R SENAXYVA EQ",
+        "VQ:92");
+
+    qbGrfg("G8",
+        "(PNQ) SVER TENFF R SENAXYVA EQ & 192AQ NIR AR RFA: 92",
+        "PNYY:SVER TENFF",
+        "NQQE:R SENAXYVA EQ & 192AQ NIR AR",
+        "VQ:92");
+
+    qbGrfg("G9",
+        "(PNQ) SVER TENFF YVFN YA & 192AQ NIR AR RFA: 38",
+        "PNYY:SVER TENFF",
+        "NQQE:YVFN YA & 192AQ NIR AR",
+        "VQ:38");
+
+    qbGrfg("G10",
+        "(PNQ) FZBXR VAIRFGVTNGVBA 180GU NIR AR & R GRPHZFRU EQ RFA: 136",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:180GU NIR AR & R GRPHZFRU EQ",
+        "VQ:136");
+
+    qbGrfg("G11",
+        "(PNQ) SVER ERFVQRAGVNY 16650 192AQ NIR AR RFA: 38",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:16650 192AQ NIR AR",
+        "VQ:38");
+
+    qbGrfg("G12",
+        "(PNQ) FZBXR VAIRFGVTNGVBA 18401 CRPNA PERRX QE RFA: 38",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:18401 CRPNA PERRX QE",
+        "VQ:38");
+
+    qbGrfg("G13",
+        "(PNQ) SVER ZHGHNY NVQR JNHARGG BXYNUBZN RFA:",
+        "PNYY:SVER ZHGHNY NVQR",
+        "NQQE:JNHARGG BXYNUBZN");
+
+    qbGrfg("G14",
+        "(PNQ) FZBXR VAIRFGVTNGVBA 375 JVYXVAFBA QE RFA:",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:375 JVYXVAFBA QE");
+
+    qbGrfg("G15",
+        "(PNQ) SVER ERFVQRAGVNY 18240 311 EQ RFA: 277",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:18240 311 EQ",
+        "VQ:277");
+
+    qbGrfg("G16",
+        "(PNQ) SVER NYNEZ ERFVQRAGVNY 18901 ERQ BNX YA RFA: 92",
+        "PNYY:SVER NYNEZ ERFVQRAGVNY",
+        "NQQE:18901 ERQ BNX YA",
+        "VQ:92");
+
+    qbGrfg("G17",
+        "(PNQ) SVER PBAGEBY OHEA 18801 R FU9 RFA: 182",
+        "PNYY:SVER PBAGEBY OHEA",
+        "NQQE:18801 R FU9",
+        "ZNQQE:18801 R BX 9",
+        "VQ:182");
+
+    qbGrfg("G18",
+        "(PNQ) SVER ERFVQRAGVNY 192AQ NIR AR & CRPNA PERRX QE RFA: 38",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:192AQ NIR AR & CRPNA PERRX QE",
+        "VQ:38");
+
+    qbGrfg("G19",
+        "(PNQ) SVER TENFF 17205 FLPNZBER YA RFA: 37",
+        "PNYY:SVER TENFF",
+        "NQQE:17205 FLPNZBER YA",
+        "VQ:37");
+
+    qbGrfg("G20",
+        "(PNQ) FZBXR VAIRFGVTNGVBA 18750 EBYYVAT BNX EQ RFA: 38",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:18750 EBYYVAT BNX EQ",
+        "VQ:38");
+
+    qbGrfg("G21",
+        "(PNQ) SVER TENFF 19145 F UNEENU EQ RFA:",
+        "PNYY:SVER TENFF",
+        "NQQE:19145 F UNEENU EQ");
+
+    qbGrfg("G22",
+        "(PNQ) CHOYVP NFFVFG 451 192AQ NIR FR RFA: 230",
+        "PNYY:CHOYVP NFFVFG",
+        "NQQE:451 192AQ NIR FR",
+        "VQ:230");
+
+    qbGrfg("G23",
+        "(PNQ) SVER PBAGEBY OHEA 20300 R NYNZRQN FG RFA: 231",
+        "PNYY:SVER PBAGEBY OHEA",
+        "NQQE:20300 R NYNZRQN FG",
+        "VQ:231");
+
+    qbGrfg("G24",
+        "(PNQ) SVER ERFVQRAGVNY 132AQ NIR FR & R VZUBSS EQ RFA: 309",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:132AQ NIR FR & R VZUBSS EQ",
+        "VQ:309");
+
+    qbGrfg("G25",
+        "(PNQ) SVER ZHGHNY NVQR 22634 FUNQL PERRX YA RFA:",
+        "PNYY:SVER ZHGHNY NVQR",
+        "NQQE:22634 FUNQL PERRX YA");
+
+    qbGrfg("G26",
+        "(PNQ) SVER TENFF FXLEVQTR EQ & JBBQEVQTR QE RFA: 92",
+        "PNYY:SVER TENFF",
+        "NQQE:FXLEVQTR EQ & JBBQEVQTR QE",
+        "VQ:92");
+
+    qbGrfg("G27",
+        "(PNQ) SVER ZHGHNY NVQR ZBBFRIVYYR & 192 RFA:",
+        "PNYY:SVER ZHGHNY NVQR ZBBFRIVYYR & 192");
+
+    qbGrfg("G28",
+        "(PNQ) CHOYVP NFFVFG 18718 R VAQVNA UVYYF EQ RFA: 62",
+        "PNYY:CHOYVP NFFVFG",
+        "NQQE:18718 R VAQVNA UVYYF EQ",
+        "VQ:62");
+
+    qbGrfg("G29",
+        "(PNQ) SVER TENFF 19200 R FU9 UJL RFA:",
+        "PNYY:SVER TENFF",
+        "NQQE:19200 R FU9 UJL",
+        "ZNQQE:19200 R BX 9");
+
+    qbGrfg("G30",
+        "(PNQ) SVER ERFVQRAGVNY 156GU NIR & FGRYYN EQ RFA: 16",
+        "PNYY:SVER ERFVQRAGVNY",
+        "NQQE:156GU NIR & FGRYYN EQ",
+        "VQ:16");
+
+    qbGrfg("G31",
+        "(PNQ) FZBXR VAIRFGVTNGVBA 18150 RFPBEG PVE RFA: 276",
+        "PNYY:FZBXR VAIRFGVTNGVBA",
+        "NQQE:18150 RFPBEG PVE",  // Tbbtyr svaqf RFPBEG QE
+        "VQ:276");
+   
   }
 
 
