79d8bbb4c0f719a65110ab450a27b3b63fc2109c
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-08 20:07:55 +0000

    skeleton for CAFresnoCountyParserTest is complete with one section.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CA/CAFresnoCountyParserTest.java
@@ -0,0 +1,133 @@
+cnpxntr arg.narv.pnqcntr.cnefref.PN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Serfab Pbhagl, PN
+Pbagnpg: Npgvir911
+Ntrapl anzr: Serfab Pbhagl RZF Pbzzhavpngvbaf Pragre
+Ybpngvba: Serfab, PN, Havgrq Fgngrf
+Fraqre:  IPZnvy@pb.serfab.pn.hf
+
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:G11            Mbar: 43581        R Funj Nir & A 1fg Fg                             Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqrag Cva-Va             Sbe:R02            Mbar: 39521        A Zvyohea Nir & J Ureaqba Nir                     Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Hapbafpvbhf/Snvagvat(Arne)          Sbe:R12            Mbar: 43544        1620 J Snvezbag Nir                               Ncg:                Orgjrra:A JRFG NIR/A PELFGNY NIR      Ybpngvba Anzr:QVNZBAQ YRNEAVAT PRAGRE
+(IvfvPnq Rznvy) ZRQ-Hapbafpvbhf/Snvagvat(Arne)          Sbe:R14            Mbar: 39517        6430 A Ryyraqnyr Nir                              Ncg:                Orgjrra:J FVREEN NIR/Qrnq Raq         Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Pubxvat                             Sbe:R02            Mbar: 38558        7087 A Gubear Nir                                 Ncg:178             Orgjrra:CEVINGR QE/J FCEHPR NIR       Ybpngvba Anzr:Jvaqzrer Ncnegzragf
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 42555        1718 J Funj Nir                                   Ncg:                Orgjrra:HAANZRQ FGERRG/Qrnq Raq       Ybpngvba Anzr:Yvax Pner Pragre (1718)
+(IvfvPnq Rznvy) Havg:               Vap #:3037428     PE:23:22:53HN:        RE:        NF:        Pyrne:
+(IvfvPnq Rznvy) Havg:Pbzcnal 69     Vap #:3037428     PE:23:22:53HN:23:24:11RE:23:31:46NF:        Pyrne:23:34:34
+(IvfvPnq Rznvy) Havg:               Vap #:3037428     PE:23:22:53HN:        RE:        NF:        Pyrne:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:Pbzcnal 69     Qvfg:ABG SBHAQ    Nqqerff:21311 F Unfyrz Nir                 Ncg:                Orgjrra:J GVTRE NIR/J YHAN QE                   Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Fgno/TFJ/Crargengvat                Sbe:R12            Mbar: 43541        4989 A Ubyg Nir                                   Ncg:                Orgjrra:J SNVEZBAG NIR/J FUNJ NIR     Ybpngvba Anzr:Ab Anzr Ba Pbzcyrk (4989)
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 44547        4294 A Uhturf Nir                                 Ncg:276             Orgjrra:J NFUYNA NIR/J ABEJVPU NIR    Ybpngvba Anzr:Fhaohefg Ncnegzragf
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R14            Mbar: 39521        6800 A Zvyohea Nir                                Ncg:                Orgjrra:J CNYB NYGB NIR/J UREAQBA NIR Ybpngvba Anzr:PIF Cuneznpl  (Zvyohea & Ureaq
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:G11            Mbar: 43573        A Serfab Fg & R Funj Nir                          Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Hapbafpvbhf/Snvagvat(Arne)          Sbe:R12            Mbar: 46545        2530 J Sbhagnva Jnl                               Ncg:201             Orgjrra:A JRORE NIR/A CYRNFNAG NIR    Ybpngvba Anzr:Sbhagnva Jrfg Ncnegzragf (2530
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:G11            Mbar: 37567        200 - 399 J Pebzjryy Nir                          Ncg:                Orgjrra:A QRY ZNE NIR/A ZNEBN NIR     Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Fgno/TFJ/Crargengvat                Sbe:R12            Mbar: 43541        J Snvezbag Nir & A Znexf Nir                      Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:Pbzcnal 67     Qvfg:ABG SBHAQ    Nqqerff:17895 R Rkpryfvbe Nir              Ncg:                Orgjrra:Qrnq Raq/18GU NIR                       Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:G11            Mbar: 41586        1285 R Oebjavat Nir                               Ncg:                Orgjrra:A 4GU FG/A 5GU FG             Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 44546        2404 J Ubyynaq Nir                                Ncg:                Orgjrra:A JBBQFBA NIR/A CYRNFNAG NIR  Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:G11            Mbar: 42573        5207 A Nhthfgn Fg                                 Ncg:                Orgjrra:R FNA WBFR NIR/R FNA OEHAB NIRYbpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R12            Mbar: 38544        7270 A Jneera Nir                                 Ncg:                Orgjrra:J OVEPU NIR/J ZVANERGF NIR    Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R12            Mbar: 42555        5173 A Punaavat Jnl                               Ncg:                Orgjrra:Qrnq Raq/J FNA WBFR NIR       Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) Jbexvat nggvp sver ng n erfvqrapr 1731 F Grvyzna Nir. R3 vf Grvyzna VP
+(IvfvPnq Rznvy) ZRQ2-Qvnorgvp Ceboyrzf                  Sbe:R12            Mbar: 42543        5240 A Ina Arff Oyiq                              Ncg:                Orgjrra:J FNA WBFR NIR/J FNA ENZBA NIRYbpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:R12            Mbar: 47546        2450 J Pyvagba Nir                                Ncg:                Orgjrra:UJL-99 BA ENZC AO/A TBYQRA FGNYbpngvba Anzr:
+(IvfvPnq Rznvy) jbexvat sver ng 4540 r trgglfohet--Qo
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Pneqvnp be Erfc Neerfg              Sbe:R02            Mbar: 40562        585 J Fvreen Nir                                  Ncg:122             Orgjrra:A PBYBAVNY NIR/A ERZVATGBA NIRYbpngvba Anzr:Zbebn Cnex NCnegzragf (585)
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:G11            Mbar: 40576        6145 A Gurfgn Fg                                  Ncg:                Orgjrra:R OHYYNEQ NIR/R RFPNYBA NIR   Ybpngvba Anzr:6145 A Gurfgn
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R12            Mbar: 45546        3957 A Jbbqfba Nir                                Ncg:                Orgjrra:J FNTVANJ JNL/J TEVSSVGU JNL  Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Hapbafpvbhf/Snvagvat(Arne)          Sbe:R14            Mbar: 42523        5310 A Tngrf Nir                                  Ncg:103             Orgjrra:J FNA WBFR NIR/J PBEBAN NIR   Ybpngvba Anzr:Cnyz Tngrf Ncnegzragf (5310)
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:G11            Mbar: 44585        4287 A 1fg Fg                                     Ncg:                Orgjrra:R NFUYNA NIR/R FJVSG NIR      Ybpngvba Anzr:Snzvyl Qragvfgel
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:G11            Mbar: 39573        A Serfab Fg & R Ureaqba Nir                       Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ2-Nffnhyg/Frkhny Nffnhyg             Sbe:R02            Mbar: 41552        J Pryrfgr Nir & A Sehvg Nir                       Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R02            Mbar: 40552        6201 A Qryab Nir                                  Ncg:                Orgjrra:Qrnq Raq/J FNZCYR NIR         Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R12            Mbar: 45533        3360 J Fhffrk Jnl                                 Ncg:                Orgjrra:A UNMRY NIR/A INYRAGVAR NIR   Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:G11            Mbar: 40566        567 J Fnzcyr Nir                                  Ncg:                Orgjrra:A ZNEBN NIR/A PBYBAVNY NIR    Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R02            Mbar: 40544        6258 A Qbyberf Nir                                Ncg:                Orgjrra:J INEGVXVNA NIR/Qrnq Raq      Ybpngvba Anzr:
+(IvfvPnq Rznvy) RZF#:201308050599 ,Vapvqrag Pnapryyrq:Nzohynapr ng Fprar             ,PE:22:39:02 ,HN:22:40:30 ,RE:22:42:45 ,NF:         ,PA:22:49:30
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) Havg:RZF02,Cev:1, Ybp:Ebnq 28 & Ryyvf Fg            ,ZncCntr:GO1049  ,Ncg:             ,Pvgl:Znqren         ,Angher:*Nvepensg Qbja (Bss SLV)      ,RZF#:201308050599, KFgerrg:                                  ,CNF #:13-07186,Qrfg:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ2-Ryrpgebphgvba/Yvtugavat            Sbe:R02            Mbar: 40555        A Jrfg Nir & J Fghneg Nir                         Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) jbexvat ncnegzrag sver ng 5511 A Prqne pebff bs Onefgbj. ab bgure vasbezngvba ng guvf gvzr. -nfugba
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 45538        3025 J Cbagvnp Jnl                                Ncg:101             Orgjrra:A ZNEXF NIR/Qrnq Raq          Ybpngvba Anzr:3025 J Cbagvnp
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R02            Mbar: 47551        1209 J Cevaprgba Nir                              Ncg:                Orgjrra:A JRFG NIR/A PUNAAVAT JNL     Ybpngvba Anzr:
+(IvfvPnq Rznvy) Havg:               Vap #:3037029     PE:13:54:56HN:        RE:        NF:        Pyrne:
+(IvfvPnq Rznvy) Jbexvat tnentr sver ng 4805 A Uhturf
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R14            Mbar: 43546        4705 A Jbbqfba Nir                                Ncg:                Orgjrra:J CVPB NIR/A UBYG NIR         Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) GRFG cntr, Ovyy grkg Zngg vs lbh trg guvf cyrnfr...
+(IvfvPnq Rznvy) GRFG CNTR
+(IvfvPnq Rznvy) GRFG CNTR
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:G11            Mbar: 42576        5066 A Serfab Fg                   Ncg:fgr 111         Orgjrra:R GJNVA NIR/R XRNGF NIR       Ybpngvba Anzr:Zbhagnva Snzvyl Urnygu
+(IvfvPnq Rznvy) GRFG CNTR
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:R12            Mbar: 44548        1565 J Fjvsg Nir                   Ncg:                Orgjrra:A JRFG NIR/A RZREFBA NIR      Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:G11            Mbar: 39575        6587 A Pynex Nir                   Ncg:                Orgjrra:R YBF NYGBF NIR/R JNEARE NIR  Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 42537        5175 A Srynaq                      Ncg:134             Orgjrra:FUNJ/FNA WBFR                 Ybpngvba Anzr:Neobe Snve Ncnegzragf
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) jbexvat erfvqragvny sver, fzbxr naq synzrf sebz gur ebbs @ 4037 r ubyynaq
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Pubxvat                             Sbe:R02            Mbar: 41544        2155 J Ehr Fg Zvpury               Ncg:                Orgjrra:A SBEXARE NIR/A SBEXARE NIR   Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Pubxvat                             Sbe:R12            Mbar: 43534        3107 J Fnagn Nan Nir               Ncg:                Orgjrra:A SRYNAQ NIR/A YRNQ NIR       Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R12            Mbar: 43545        2874 J Npnpvn Nir                  Ncg:                Orgjrra:A ZNEXF NIR/A ZNEXF NIR       Ybpngvba Anzr:SEA - Serfab Sver Fgngvba 12
+(IvfvPnq Rznvy) Jbexvat Fgehpgher Sver (Bhg Ohvyqvat) 20418 J Fuvryqf Nir .. TU
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) Havg:PbzcVapvqrag #:3036829     PE:02:15:01HN:02:15:25RE:        NF:        Pyrne:02:18:13
+(IvfvPnq Rznvy) ZRQ3-Fvpx Crefba                        Sbe:Pbzcnal 67     Qvfg:ABG SBHAQ    Nqqerff:6662 ZHECUL NIR                    Ncg:                Orgjrra:F CHROYB QE/IRENAQN NIR                 Ybpngvba Anzr:
+(IvfvPnq Rznvy) ZRQ-Oernguvat Ceboyrzf                  Sbe:R12            Mbar: 41537        A Fnagn Sr Nir & J Onefgbj Nir     Ncg:                Orgjrra:                              Ybpngvba Anzr:Cnex - Ybtna Cnex
+(IvfvPnq Rznvy) ZRQ-Purfg Cnva                          Sbe:R14            Mbar: 38503        6172 J Zvanergf Nir                Ncg:                Orgjrra:A FNAQEVAV NIR/A UNLRF NIR    Ybpngvba Anzr:
+(IvfvPnq Rznvy) 
+(IvfvPnq Rznvy) ZRQ-Genssvp Nppvqragf                   Sbe:G11            Mbar: 41574        A 1fg Fg & R Ohyyneq Nir           Ncg:                Orgjrra:                              Ybpngvba Anzr:
+(IvfvPnq Rznvy) hcqngr ba wpcraal sver, 2aq nynez vf orvat pnapryyrq
+
+ */
+
+choyvp pynff PNSerfabPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp PNSerfabPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "SERFAB PBHAGL", "PN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+    
+ 
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj PNSerfabPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
