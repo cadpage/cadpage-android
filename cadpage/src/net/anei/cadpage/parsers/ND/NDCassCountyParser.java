a5230874d3bc24244ddaff62bfcaa1b4f4e11bea
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-09 01:34:49 +0000

    Added more city codes to Cass County,ND

--- cadpage/src/net/anei/cadpage/parsers/ND/NDCassCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/ND/NDCassCountyParser.java
@@ -15,7 +15,7 @@ public class NDCassCountyParser extends SmartAddressParser {
  
   public NDCassCountyParser() {
     super(CITY_CODES, "CASS COUNTY", "ND");
-    setFieldList("CALL ADDR APT CITY PLACE DATE TIME ID INFO UNIT");
+    setFieldList("CALL ADDR APT CITY ST PLACE DATE TIME ID INFO UNIT");
   }
   
   @Override
@@ -35,8 +35,16 @@ public class NDCassCountyParser extends SmartAddressParser {
     String sInfo = body.substring(match.end()).trim();
     
     parseAddress(StartType.START_CALL, FLAG_IMPLIED_INTERSECT, sAddr, data);
+    
+    int pt = data.strCity.indexOf('/');
+    if (pt >= 0) {
+      data.strState = data.strCity.substring(pt+1);
+      if (data.strState.equals("ND")) data.strState = "";
+      data.strCity = data.strCity.substring(0,pt).trim();
+    }
+    
     String sPlace = getLeft();
-    int pt = sPlace.indexOf(" - ");
+    pt = sPlace.indexOf(" - ");
     if (pt >= 0) sPlace = sPlace.substring(0,pt).trim();
     if (sPlace.length() <= 1) sPlace = "";
     data.strPlace = sPlace;
@@ -54,13 +62,98 @@ public class NDCassCountyParser extends SmartAddressParser {
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "AMEN",  "AMENIA",
-      "CAST", "CASSELTON",
-      "CHAF", "CHAFFEE",
-      "LEON", "LEONARD",
-      "WHEA", "WHEATLAND",
-      
-      "CASS", ""
+      "ABSA", "ABSARAKA/ND",
+      "ALIC", "ALICE/ND",
+      "AMEN", "AMENIA/ND",
+      "ARGU", "ARGUSVILLE/ND",
+      "ARTH", "ARTHUR/ND",
+      "AVIL", "AVERILL/MN",
+      "AYR",  "AYR/ND",
+      "BAKE", "BAKER/MN",
+      "BARN", "BARNESVILLE/MN",
+      "BORU", "BORUP/MN",
+      "BRIA", "BRIARWOOD/ND",
+      "BUFF", "BUFFALO/ND",
+      "CASS", "",
+      "CAST", "CASSELTON/ND",
+      "CHAF", "CHAFFEE/ND",
+      "CHRI", "CHRISTINE/ND",
+      "CLAY", "CLAY COUNTY/MN",
+      "CLIF", "CLIFFORD/ND",
+      "COMS", "COMSTOCK/MN",
+      "CROM", "CROMWELL TOWNSHIP/MN",
+      "DALE", "DALE/MN",
+      "DAVE", "DAVENPORT/ND",
+      "DILW", "DILWORTH/MN",
+      "DOWN", "DOWNER/MN",
+      "DURB", "DURBIN/ND",
+      "EGLO", "EGLON TOWNSHIP/MN",
+      "ELKT", "ELKTON TOWNSHIP/MN",
+      "ELMW", "ELMWOOD TOWNSHIP/MN",
+      "EMBD", "EMBDEN/ND",
+      "ENDE", "ENDERLIN/ND",
+      "ERIE", "ERIE/ND",
+      "FELT", "FELTON/MN",
+      "FGO",  "FARGO/ND",
+      "FING", "FINGAL/ND",
+      "FLOW", "FLOWING TOWNSHIP/MN",
+      "GALE", "GALESBURG/ND",
+      "GARD", "GARDNER/ND",
+      "GEOR", "GEORGETOWN/MN",
+      "GLYN", "GLYNDON/MN",
+      "GOOS", "GOOSE PRAIRIE TOWNSHIP/MN",
+      "GRAN", "GRANDIN/ND",
+      "HAGE", "HAGEN TOWNSHIP/MN",
+      "HAWL", "HAWLEY/MN",
+      "HICK", "HICKSON/ND",
+      "HIGH", "HIGHLAND GROVE TOWNSHIP/MN",
+      "HITT", "HITTERDAL/MN",
+      "HOLY", "HOLY CROSS TOWNSHIP/MN",
+      "HOPE", "HOPE/ND",
+      "HUMB", "HUMBOLDT TOWNSHIP/MN",
+      "HUNT", "HUNTER/ND",
+      "KEEN", "KEENE TOWNSHIP/MN",
+      "KIND", "KINDRED/ND",
+      "KRAG", "KRAGNESS TOWNSHIP/MN",
+      "KURT", "KURTZ TOWNSHIP/MN",
+      "LEON", "LEONARD/ND",
+      "LPRK", "LAKE PARK/MN",
+      "MAPL", "MAPLETON/ND",
+      "MHD",  "MOORHEAD/MN",
+      "MOLA", "MOLAND TOWNSHIP/MN",
+      "MORK", "MORKEN TOWNSHIP/MN",
+      "NARW", "HARWOOD/ND",
+      "NEWR", "NEW ROCKFORD/ND",
+      "NORA", "HORACE/ND",
+      "NROT", "NORTH RIVER/ND",
+      "OAKP", "OAKPORT TOWNSHIP/MN",
+      "OXBO", "OXBOW/ND",
+      "PAGE", "PAGE/ND",
+      "PARK", "PARKE TOWNSHIP/MN",
+      "PELI", "PELICAN RAPIDS/MN",
+      "PERL", "PERLEY/MN",
+      "PRAI", "PRAIRIE ROSE/ND",
+      "PROS", "PROSPER/ND",
+      "REIL", "REILE'S ACRES/ND",
+      "RIVE", "RIVERTON TOWNSHIP/MN",
+      "ROGE", "ROGERS/ND",
+      "ROLL", "ROLLAG/MN",
+      "RRON", "FRONTIER/ND",
+      "RUST", "RUSTAD CITY/ND",
+      "SABI", "SABIN/MN",
+      "SHEL", "SHELDON/ND",
+      "SKRE", "SKREE TOWNSHIP/MN",
+      "SPRI", "SPRING PRAIRIETOWNSHIP/MN",
+      "TANS", "TANSEM TOWNSHIP/MN",
+      "TOWE", "TOWER CITY/ND",
+      "TWIN", "TWIN VALLEY/MN",
+      "ULEN", "ULEN/MN",
+      "VIDI", "VIDING TOWNSHIP/MN",
+      "WARR", "WARREN/MN",
+      "WFGO", "WEST FARGO/ND",
+      "WHEA", "WHEATLAND/ND",   // Changed from MN
+      "WILD", "WILD RICE/ND",
+      "WOLV", "WOLVERTON/MN",
 
   });
 }
