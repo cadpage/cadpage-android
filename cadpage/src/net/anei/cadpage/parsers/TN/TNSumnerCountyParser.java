1a6fa67c710cc33c4ab8b4a86c5271c25a658b40
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-10 15:44:13 +0000

    Updated sender filter for Cayuga County, NY

--- cadpage/src/net/anei/cadpage/parsers/TN/TNSumnerCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TN/TNSumnerCountyParser.java
@@ -3,8 +3,6 @@ package net.anei.cadpage.parsers.TN;
 /**
  * Sumner County, TN
  */
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
 
 
@@ -14,12 +12,4 @@ public class TNSumnerCountyParser extends DispatchA3Parser {
     super("", "SUMNER COUNTY", "TN",
            "EMPTY ADDR APT UNK CITY PLACE CALL! INFO+");
   }
-  
-//  @Override
-//  protected boolean parseMsg(String body, Data data) {
-//    if (!body.startsWith("* ")) return false;
-//    body = body.substring(2).trim();
-//    if (body.endsWith("*")) body = body + " ";
-//    return parseFields(body.split("\\* "), 6, data);
-//  }
 }
