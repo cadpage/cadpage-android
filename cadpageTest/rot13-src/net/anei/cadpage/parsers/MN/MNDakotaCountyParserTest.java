1962aef0c7d66e47fd6f5f48bf3444372b1d5f58
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-10 09:06:20 +0000

    skeleton for MNDakotaCountyParserTest is complete with one small section. It may really be Goodhue County, MN.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNDakotaCountyParserTest.java
@@ -0,0 +1,47 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZA;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Qnxbgn Pbhagl, ZA
+Pbagnpg: Npgvir911
+Ntrapl anzr: Zvrfivyyr Sver naq Erfphr
+Ybpngvba: Zvrfivyyr, ZA, Havgrq Fgngrf
+Fraqre: PVF Npgvir 911 <abercyl@pvfhfn.bet>
+
+(Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000009\a\aGvzr ercbegrq: 07/13/2013 11:32:21 NZ\aGvzr pbzcyrgrq: 07/13/2013 12:09:46 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\ajvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva. 1146 ubhef: Zvrfivyyr puvrs 3 ba fprar 1149 ubhef: Oehfu 1 fprar
+(Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000009\a\aGvzr ercbegrq: 07/13/2013 11:32:21 NZ\a\a\aHavg(f) erfcbaqrq: \aZV1E\ajvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva.
+(Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000008\a\aGvzr ercbegrq: 07/12/2013 9:32:40 NZ\aGvzr pbzcyrgrq: 07/12/2013 9:51:42 NZ\a\aHavg(f) erfcbaqrq: \aZV1E\a1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr erfvqrapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.  Pnapryyrq cre ZV1E - srznyr vf svar & qvq abg xabj fur cerffrq gur ohggba.  Nzo pnapryyrq.
+(Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000008\a\aGvzr ercbegrq: 07/12/2013 9:32:40 NZ\a\a\aHavg(f) erfcbaqrq: \aZV1E\a1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.
+(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000007\a\aGvzr ercbegrq: 07/11/2013 3:04:49 CZ\aGvzr pbzcyrgrq: 07/11/2013 3:33:40 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.
+(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000007\a\aGvzr ercbegrq: 07/11/2013 3:04:49 CZ\a\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.
+(Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aTenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aTenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000006\a\aGvzr ercbegrq: 07/10/2013 2:30:15 CZ\aGvzr pbzcyrgrq: 07/10/2013 5:18:42 CZ\a\aHavg(f) erfcbaqrq: \aZVSQ\a1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq
+(Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aTenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aTenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000006\a\aGvzr ercbegrq: 07/10/2013 2:30:15 CZ\a\a\aHavg(f) erfcbaqrq: \a1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq
+(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000005\a\aGvzr ercbegrq: 07/04/2013 12:16:22 CZ\aGvzr pbzcyrgrq: 07/04/2013 12:55:07 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu. fur jnyxrq gb gur bssvpr naq fgngrf fur vf svar ab nzohynapr arrqrq
+(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000005\a\aGvzr ercbegrq: 07/04/2013 12:16:22 CZ\a\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu.
+(Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aSver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aSver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000004\a\aGvzr ercbegrq: 07/04/2013 11:56:36 NZ\aGvzr pbzcyrgrq: 07/04/2013 12:17:07 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\anppvqrag jvgu vawhevrf Unfgvatf nzohynapr frag sbe zhghny nvq
+(Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aSver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aSver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000004\a\aGvzr ercbegrq: 07/04/2013 11:56:36 NZ\a\a\aHavg(f) erfcbaqrq: \aZV1E\anppvqrag jvgu vawhevrf
+(Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000003\a\aGvzr ercbegrq: 07/03/2013 4:15:50 CZ\aGvzr pbzcyrgrq: 07/03/2013 4:17:29 CZ\a\aHavg(f) erfcbaqrq: \aZV1E
+(Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000003\a\aGvzr ercbegrq: 07/03/2013 4:15:50 CZ\a\a\aHavg(f) erfcbaqrq: \aZV1E
+
+*/
+
+choyvp pynff ZAQnxbgnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ZAQnxbgnPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "QNXBGN PBHAGL", "ZA");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+   
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ZAQnxbgnPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
