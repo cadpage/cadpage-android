4fa09c7350911d1c28f3f248b6e9ac8a4cc7c72d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-13 15:29:49 +0000

    Fixed parsing problems with Wilson County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCWilsonCountyParser.java
@@ -1,8 +1,10 @@
 package net.anei.cadpage.parsers.NC;
 
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
@@ -19,6 +21,14 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
     return "CAD@Wilson-co.com,930010";
   }
   
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    if (subject.equals("Sanoca")) {
+      if (body.startsWith(";")) body = "CAD:" + body.substring(1).trim();
+    }
+    return super.parseMsg(body,  data);
+  }
+  
   // ID field has to validate it's existence
   private class MyIdField extends IdField {
     public MyIdField() {
@@ -32,12 +42,39 @@ public class NCWilsonCountyParser extends DispatchOSSIParser {
       setPattern(Pattern.compile("\\d\\d[A-Z]\\d\\d[A-Z]?"));
     }
   }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (US_264_DIR.matcher(field).matches()) {
+        parse(field, data);
+        return true;
+      }
+      return super.checkParse(field, data);
+    }
+  }
 
   @Override
   protected Field getField(String name) {
     if (name.equals("ID")) return new MyIdField();
     if (name.equals("CODE")) return new MyCodeField();
+    if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
   
+  
+  @Override
+  public String adjustMapAddress(String address) {
+    Matcher match = US_264_DIR.matcher(address);
+    if (match.find()) {
+      StringBuffer bf = new StringBuffer();
+      do {
+        match.appendReplacement(bf, "US 264");
+        if (match.group(1) != null) bf.append(" ALT");
+      } while (match.find());
+      address = bf.toString();
+    }
+    return address;
+  }
+  private static final Pattern US_264_DIR = Pattern.compile("\\bUS +264(A?)(?: +[NESW])?\\b", Pattern.CASE_INSENSITIVE);
 }
