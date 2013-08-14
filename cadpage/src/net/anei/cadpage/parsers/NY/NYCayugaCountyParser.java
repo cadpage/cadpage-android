25727e6df0f0d840a4a96feb25940c72a7018b22
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-14 08:58:21 +0000

    Fixed parsing problem witih Cayuga County, NY

--- cadpage/src/net/anei/cadpage/parsers/NY/NYCayugaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYCayugaCountyParser.java
@@ -28,70 +28,8 @@ public class NYCayugaCountyParser extends SmartAddressParser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     
-    // Try to pare new format page
-    Matcher match = MARKER1.matcher(body);
-    if (match.find()) {
-      setFieldList("ADDR APT PLACE CITY X CALL INFO DATE TIME ID NAME");
-      data.strDate = match.group(1);
-      data.strTime = match.group(2);
-      data.strCallId = match.group(3);
-      data.strName = match.group(4).trim();
-      body = body.substring(0,match.start());
-      if (body.endsWith(" Uncertainty:  Confidence:")) {
-        body = body.substring(0,body.length()-26).trim();
-      }
-      
-      // Sometimes blanks are missing between address and city name :(
-      body = MISSED_BLANK_PTN.matcher(body).replaceFirst("$1 $2");
-      
-      // Warning extreme ugliness ahead
-      // The occasionally include a place name between the address and
-      // the city, which would normally be handled by a pad field.  But
-      // they also routinely drop the street suffix.  Which really messes
-      // things up if we are searching for a pad field.
-      
-      // SO, we will do an initial parse address without the pad field
-      // then try to reparse the parsed address field to see if we
-      // can find a place name in it
-      parseAddress(StartType.START_ADDR, FLAG_CROSS_FOLLOWS, body, data);
-      if (data.strCity.length() == 0) return false;
-      body = getLeft();
-      
-      String address = data.strAddress;
-      data.strAddress = "";
-      parseAddress(StartType.START_ADDR, address, data);
-      data.strPlace = getLeft();
-      
-      // Next come cross street information, which might come in two parts
-      // separated by a comma
-      String cross = "";
-      int pt = body.indexOf(',');
-      if (pt >= 0) {
-        cross = body.substring(0,pt).trim();
-        if (checkAddress(cross) > 0) {
-          body = body.substring(pt+1).trim();
-        } else {
-          cross = "";
-        }
-      }
-      Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_NO_CITY, body);
-      if (res.getStatus() > 0) {
-        res.getData(data);
-        body = res.getLeft();
-      }
-      data.strCross = append(cross, ", ", data.strCross);
-      
-      // Get call description 
-      getCallInfoFields(body, data);
-      
-      // Clean up city name issues
-      cleanCityName(data);
-      
-      return true;
-    }
-    
-    // Try to parse older format page
-    match = MARKER2.matcher(body);
+    // First try to parse older format page
+    Matcher match = MARKER2.matcher(body);
     if (match.find()) {
       setFieldList("DATE TIME PLACE ADDR CITY CALL X INFO");
       
@@ -160,14 +98,79 @@ public class NYCayugaCountyParser extends SmartAddressParser {
       return true;
     }
     
-    // Not  recognized dispatch pattern.  See if this should be a general alert
-    if (subject.equals("From 911 Center") || subject.equals("From911Center")) {
-      data.strCall = "GENERAL ALERT";
-      data.strPlace = body;
+    // Try to parse new format page
+    boolean good;
+    String saveBody = body;
+    match = MARKER1.matcher(body);
+    if (match.find()) {
+      good = true;
+      data.strDate = match.group(1);
+      data.strTime = match.group(2);
+      data.strCallId = match.group(3);
+      data.strName = match.group(4).trim();
+      
+      body = body.substring(0,match.start());
+      if (body.endsWith(" Uncertainty:  Confidence:")) {
+        body = body.substring(0,body.length()-26).trim();
+      }
+    } else {
+      good = subject.equals("From 911 Center") || subject.equals("From911Center");
+    }
+    
+    if (good) {
+      setFieldList("ADDR APT PLACE CITY X CALL INFO DATE TIME ID NAME");
+
+      // Sometimes blanks are missing between address and city name :(
+      body = MISSED_BLANK_PTN.matcher(body).replaceFirst("$1 $2");
+      
+      // Warning extreme ugliness ahead
+      // The occasionally include a place name between the address and
+      // the city, which would normally be handled by a pad field.  But
+      // they also routinely drop the street suffix.  Which really messes
+      // things up if we are searching for a pad field.
+      
+      // SO, we will do an initial parse address without the pad field
+      // then try to reparse the parsed address field to see if we
+      // can find a place name in it
+      parseAddress(StartType.START_ADDR, FLAG_CROSS_FOLLOWS, body, data);
+      if (data.strCity.length() == 0) {
+        data.parseGeneralAlert(this, saveBody);
+        return true;
+      }
+      body = getLeft();
+      
+      String address = data.strAddress;
+      data.strAddress = "";
+      parseAddress(StartType.START_ADDR, address, data);
+      data.strPlace = getLeft();
+      
+      // Next come cross street information, which might come in multiple parts
+      // separated by a comma
+      String cross = "";
+      while (true) {
+        int pt = body.indexOf(',');
+        if (pt < 0) break;
+        String tmp = body.substring(0,pt).trim();
+        if (checkAddress(tmp) == 0) break;
+        cross = append(cross, ", ", tmp);
+        body = body.substring(pt+1).trim();
+      }
+      Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_NO_CITY, body);
+      if (res.getStatus() > 0) {
+        res.getData(data);
+        body = res.getLeft();
+      }
+      data.strCross = append(cross, ", ", data.strCross);
+      
+      // Get call description 
+      getCallInfoFields(body, data);
+      
+      // Clean up city name issues
+      cleanCityName(data);
+      
       return true;
     }
     
-    // No go
     return false;
   }
 
