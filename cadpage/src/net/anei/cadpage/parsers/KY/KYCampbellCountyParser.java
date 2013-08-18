5f7df0b06b22d1b5d5fabce8c5debac4d56392c5
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-18 04:20:36 +0000

    Checking in Dakota County, MN location

--- cadpage/src/net/anei/cadpage/parsers/KY/KYCampbellCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/KY/KYCampbellCountyParser.java
@@ -1,162 +1,18 @@
 package net.anei.cadpage.parsers.KY;
 
-import java.text.SimpleDateFormat;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.FieldProgramParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 
 /**
- * Louisville, KY
+ * Campbell County, KY
  */
-public class KYCampbellCountyParser extends FieldProgramParser {
-  
-  private static final String PAGE_PREFIX = "Notification from CIS Active 911:";
-  private static final Pattern DELIM_PTN = Pattern.compile("\n{2}");
+public class KYCampbellCountyParser extends DispatchA27Parser {
   
   public KYCampbellCountyParser() {
-    super("CAMPBELL COUNTY", "KY",
-           "ADDRCITY/SC DUP? SRC! Time_reported:DATETIME! Unit(s)_responded:UNIT+");
+    super("CAMPBELL COUNTY", "KY");
   }
   
   @Override
   public String getFilter() {
     return "noreply@cisusa.org";
   }
-  
-  @Override 
-  public boolean parseMsg(String subject, String body, Data data) {
-    
-    if(!body.startsWith(PAGE_PREFIX)) return false;
-    
-    body = body.substring(PAGE_PREFIX.length()).trim();      // Remove message prefix
-    
-    String[] fields = DELIM_PTN.split(body);
-    
-    // If the body does not have the Time Completed label, process message
-    if(!body.contains("Time completed:")) {
-      return super.parseFields(fields, data);
-    }
-    // Otherwise we are a report
-    else {
-      data.strCall = "RUN REPORT";
-      
-      // Extract Units
-      String units = fields[fields.length-1];
-      int colon = units.indexOf(':');
-      units = units.substring(colon+1).trim();
-      parseUnitField(true, units, data);
-      
-      // Extract ID
-      if (fields.length > 0) {
-        int ndx = 1;
-        if (fields.length > 1 && fields[0].trim().equals(fields[1].trim())) ndx++;
-        if (fields.length > ndx) {
-          String[] srcID = fields[ndx].split("-");
-          if (srcID.length > 1) data.strCallId = srcID[1].trim();
-        }
-      }
-      
-      data.strPlace = body;                                               // Put remaining in Place.
-      return true;
-    }
-    
-  }
-  
-  private static final Pattern PTN_FULL_ADDR = Pattern.compile("(.*, .*), \\d{5}");
-  private class MyAddressField extends AddressCityField {
-    
-    @Override 
-    public void parse(String field, Data data) {
-      Matcher m = PTN_FULL_ADDR.matcher(field);   // This will match address, city, and zip
-      if(m.matches()) {                           // If we have a match
-        field = m.group(1);                       // Remove the zipcode
-      }
-      
-      int x = field.indexOf("/unincorp");
-      if(x >= 0) {
-        field = field.substring(0, x) + field.substring(x+9);
-      }
-      
-      super.parse(field, data);
-    }
-  }
-
-  private class DuplField extends SkipField {
-    
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      return field.equals(getRelativeField(-1));
-    }
-  }
-  
-  private class MySrcField extends SourceField {
-    
-    @Override 
-    public void parse(String field, Data data) {
-
-      int delim = field.indexOf(" - ");
-      if(delim >= 0) {
-        data.strSource = field.substring(0, delim).trim();
-        data.strCallId = field.substring(delim+3).trim();
-      }
-      else super.parse(field, data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return super.getFieldNames() + " ID";
-    }
-  }
-  
-  private class MyUnitField extends UnitField {
-    @Override
-    public void parse(String field, Data data) {
-      parseUnitField(false, field, data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "UNIT INFO";
-    }
-  }
-  
-  /**
-   * Parse unit field for both regular pages and run reports
-   * @param runReport true if parsing a run report
-   * @param field Unit field to be parsed
-   * @param data Data object
-   */
-  private void parseUnitField(boolean runReport, String field, Data data) {
-    boolean info = false;
-    for (String token : field.split("\n")) {
-      token = token.trim();
-      if (token.length() == 0) continue;
-      if (!info && !UNIT_PTN.matcher(token).matches()) info = true;
-      if (!info) {
-        data.strUnit = append(data.strUnit, " ", token);
-      } else if (!runReport) {
-        data.strSupp = append(data.strSupp, "\n", token);
-      }
-    }
-  }
-  private static final Pattern UNIT_PTN = Pattern.compile("[A-Z]+\\d+|\\d{8}");
-  
-  
-  @Override
-  public Field getField(String name) {
-      if (name.equals("ADDRCITY")) return new MyAddressField();
-      if (name.equals("DUP")) return new DuplField();
-      if (name.equals("SRC")) return new MySrcField();
-      if (name.equals("DATETIME")) return new DateTimeField(new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa"));
-      if (name.equals("UNIT")) return new MyUnitField();
-    return super.getField(name);
-  }
-
 }
--- cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNDakotaCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNDakotaCountyParserTest.java
@@ -27,19 +27,252 @@ Fraqre: PVF Npgvir 911 <abercyl@pvfhfn.bet>
 (Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000003\a\aGvzr ercbegrq: 07/03/2013 4:15:50 CZ\aGvzr pbzcyrgrq: 07/03/2013 4:17:29 CZ\a\aHavg(f) erfcbaqrq: \aZV1E
 (Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000003\a\aGvzr ercbegrq: 07/03/2013 4:15:50 CZ\a\a\aHavg(f) erfcbaqrq: \aZV1E
 
-Qhcyvpngr XLPnzcoryyPbhagl
 */
 
 choyvp pynff ZAQnxbgnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
   choyvp ZAQnxbgnPbhaglCnefreGrfg() {
-    frgCnefre(arj QhzzlCnefre(), "QNXBGN PBHAGL", "ZA");
+    frgCnefre(arj ZAQnxbgnPbhaglCnefre(), "QNXBGN PBHAGL", "ZA");
   }
   
   @Grfg
-  choyvp ibvq grfgCnefre() {
+  choyvp ibvq grfgZvrfivyyrSverNaqErfphr() {
 
-   
+    qbGrfg("G1",
+        "(Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000009\a\a" +
+        "Gvzr ercbegrq: 07/13/2013 11:32:21 NZ\a" +
+        "Gvzr pbzcyrgrq: 07/13/2013 12:09:46 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "jvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva. 1146 ubhef: Zvrfivyyr puvrs 3 ba fprar 1149 ubhef: Oehfu 1 fprar",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000009",
+        "CYNPR:Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000009\a\aGvzr ercbegrq: 07/13/2013 11:32:21 NZ\aGvzr pbzcyrgrq: 07/13/2013 12:09:46 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\ajvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva. 1146 ubhef: Zvrfivyyr puvrs 3 ba fprar 1149 ubhef: Oehfu 1 fprar");
+
+    qbGrfg("G2",
+        "(Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26674 144gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000009\a\a" +
+        "Gvzr ercbegrq: 07/13/2013 11:32:21 NZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "jvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva.",
+
+        "PNYY:Zrqvpny Pnyy",
+        "NQQE:26674 144gu Nir Jnl",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000009",
+        "QNGR:07/13/2013",
+        "GVZR:11:32:21",
+        "VASB:ZV1E\ajvsr unf snyyra ba ovxr genvy naq unf uvc naq fubhyqre cnva.");
+
+    qbGrfg("G3",
+        "(Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000008\a\a" +
+        "Gvzr ercbegrq: 07/12/2013 9:32:40 NZ\a" +
+        "Gvzr pbzcyrgrq: 07/12/2013 9:51:42 NZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr erfvqrapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.  Pnapryyrq cre ZV1E - srznyr vf svar & qvq abg xabj fur cerffrq gur ohggba.  Nzo pnapryyrq.",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000008",
+        "CYNPR:Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000008\a\aGvzr ercbegrq: 07/12/2013 9:32:40 NZ\aGvzr pbzcyrgrq: 07/12/2013 9:51:42 NZ\a\aHavg(f) erfcbaqrq: \aZV1E\a1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr erfvqrapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.  Pnapryyrq cre ZV1E - srznyr vf svar & qvq abg xabj fur cerffrq gur ohggba.  Nzo pnapryyrq.");
+
+    qbGrfg("G4",
+        "(Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 13179 Uvtujnl 61, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000008\a\a" +
+        "Gvzr ercbegrq: 07/12/2013 9:32:40 NZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.",
+
+        "PNYY:Zrqvpny Pnyy",
+        "NQQE:13179 Uvtujnl 61",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000008",
+        "QNGR:07/12/2013",
+        "GVZR:09:32:40",
+        "VASB:ZV1E\a1210:   Yvsryvar erp'q n fvtany fgngvat gung fur unf snyyra.   Ab erfcbafr.  Qrybevf Ynjerapr cerzvfr cu/651-388-6213  Xrlobk sbe sebag qbbe   pbqr/888  Cerff uryc ohggba bapr ba fprar.");
+
+    qbGrfg("G5",
+        "(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000007\a\a" +
+        "Gvzr ercbegrq: 07/11/2013 3:04:49 CZ\a" +
+        "Gvzr pbzcyrgrq: 07/11/2013 3:33:40 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "erdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000007",
+        "CYNPR:Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000007\a\aGvzr ercbegrq: 07/11/2013 3:04:49 CZ\aGvzr pbzcyrgrq: 07/11/2013 3:33:40 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.");
+
+    qbGrfg("G6",
+        "(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000007\a\a" +
+        "Gvzr ercbegrq: 07/11/2013 3:04:49 CZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "erdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.",
+
+        "PNYY:Zrqvpny Pnyy",
+        "NQQE:26389 Pbhagl 7 Oyiq",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000007",
+        "QNGR:07/11/2013",
+        "GVZR:15:04:49",
+        "VASB:ZV1E\aerdhrfg sbe nzohynapr sbe srznyr rkgerzryl vagbkvpngrq naq aba erfcbafvir.");
+
+    qbGrfg("G7",
+        "(Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\a" +
+        "Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000006\a\a" +
+        "Gvzr ercbegrq: 07/10/2013 2:30:15 CZ\a" +
+        "Gvzr pbzcyrgrq: 07/10/2013 5:18:42 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZVSQ\a" +
+        "1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000006",
+        "CYNPR:Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aTenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000006\a\aGvzr ercbegrq: 07/10/2013 2:30:15 CZ\aGvzr pbzcyrgrq: 07/10/2013 5:18:42 CZ\a\aHavg(f) erfcbaqrq: \aZVSQ\a1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq");
+
+    qbGrfg("G8",
+        "(Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\a" +
+        "Tenff Sver ng Pbhagl 18 Oyiq/Cenvevr Vfynaq Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000006\a\a" +
+        "Gvzr ercbegrq: 07/10/2013 2:30:15 CZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq",
+
+        "PNYY:Tenff Sver",
+        "NQQE:Pbhagl 18 Oyiq & Cenvevr Vfynaq Oyiq",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000006",
+        "QNGR:07/10/2013",
+        "GVZR:14:30:15",
+        "VASB:1211 purpx ba n tenff sver ba 18/cenvevr vfynaq  100 lneq ybat tenff sver ol gur ee genpxf pb 18/cenvevr vfynaq oyiq");
+
+    qbGrfg("G9",
+        "(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000005\a\a" +
+        "Gvzr ercbegrq: 07/04/2013 12:16:22 CZ\a" +
+        "Gvzr pbzcyrgrq: 07/04/2013 12:55:07 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "erdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu. fur jnyxrq gb gur bssvpr naq fgngrf fur vf svar ab nzohynapr arrqrq",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000005",
+        "CYNPR:Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000005\a\aGvzr ercbegrq: 07/04/2013 12:16:22 CZ\aGvzr pbzcyrgrq: 07/04/2013 12:55:07 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\aerdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu. fur jnyxrq gb gur bssvpr naq fgngrf fur vf svar ab nzohynapr arrqrq");
+
+    qbGrfg("G10",
+        "(Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 26389 Pbhagl 7 Oyiq, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000005\a\a" +
+        "Gvzr ercbegrq: 07/04/2013 12:16:22 CZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "erdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu.",
+
+        "PNYY:Zrqvpny Pnyy",
+        "NQQE:26389 Pbhagl 7 Oyiq",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000005",
+        "QNGR:07/04/2013",
+        "GVZR:12:16:22",
+        "VASB:ZV1E\aerdhrfg sbe zrqvpny sbe cnegl cnffrq bhg ba oevqtr va jrypu.");
+
+    qbGrfg("G11",
+        "(Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\a" +
+        "Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000004\a\a" +
+        "Gvzr ercbegrq: 07/04/2013 11:56:36 NZ\a" +
+        "Gvzr pbzcyrgrq: 07/04/2013 12:17:07 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "nppvqrag jvgu vawhevrf Unfgvatf nzohynapr frag sbe zhghny nvq",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000004",
+        "CYNPR:Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aSver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000004\a\aGvzr ercbegrq: 07/04/2013 11:56:36 NZ\aGvzr pbzcyrgrq: 07/04/2013 12:17:07 CZ\a\aHavg(f) erfcbaqrq: \aZV1E\anppvqrag jvgu vawhevrf Unfgvatf nzohynapr frag sbe zhghny nvq");
+
+    qbGrfg("G12",
+        "(Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\a" +
+        "Sver Pnyyf ng Jrypu Gey/130gu Nir, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000004\a\a" +
+        "Gvzr ercbegrq: 07/04/2013 11:56:36 NZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E\a" +
+        "nppvqrag jvgu vawhevrf",
+
+        "PNYY:Sver Pnyyf",
+        "NQQE:Jrypu Gey & 130gu Nir",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000004",
+        "QNGR:07/04/2013",
+        "GVZR:11:56:36",
+        "VASB:ZV1E\anppvqrag jvgu vawhevrf");
+
+    qbGrfg("G13",
+        "(Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000003\a\a" +
+        "Gvzr ercbegrq: 07/03/2013 4:15:50 CZ\a" +
+        "Gvzr pbzcyrgrq: 07/03/2013 4:17:29 CZ\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E",
+
+        "PNYY:EHA ERCBEG",
+        "VQ:201300000003",
+        "CYNPR:Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\aZvrfivyyr Sver Qrcg - 201300000003\a\aGvzr ercbegrq: 07/03/2013 4:15:50 CZ\aGvzr pbzcyrgrq: 07/03/2013 4:17:29 CZ\a\aHavg(f) erfcbaqrq: \aZV1E");
+
+    qbGrfg("G14",
+        "(Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089) Abgvsvpngvba sebz PVF Npgvir 911:\a\a" +
+        "Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zrqvpny Pnyy ng 25330 140gu Nir Jnl, Jrypu, 55089\a\a" +
+        "Zvrfivyyr Sver Qrcg - 201300000003\a\a" +
+        "Gvzr ercbegrq: 07/03/2013 4:15:50 CZ\a\a\a" +
+        "Havg(f) erfcbaqrq: \a" +
+        "ZV1E",
+
+        "PNYY:Zrqvpny Pnyy",
+        "NQQE:25330 140gu Nir Jnl",
+        "PVGL:Jrypu",
+        "FEP:Zvrfivyyr Sver Qrcg",
+        "VQ:201300000003",
+        "QNGR:07/03/2013",
+        "GVZR:16:15:50",
+        "VASB:ZV1E");
+
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
