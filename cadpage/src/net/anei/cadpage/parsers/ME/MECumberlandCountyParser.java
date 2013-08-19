a0c42b6f0fa7dfd07944fed4afae7609bc05318d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-19 09:41:05 +0000

    updates

---
+++ cadpage/src/net/anei/cadpage/parsers/ME/MECumberlandCountyParser.java
@@ -0,0 +1,49 @@
+package net.anei.cadpage.parsers.ME;
+
+
+import net.anei.cadpage.parsers.dispatch.DispatchA16Parser;
+/**
+ * Cumberland County, ME
+ */
+public class MECumberlandCountyParser extends DispatchA16Parser {
+  
+  public MECumberlandCountyParser() {
+    super(CITY_LIST, "CUMBERLAND COUNTY", "ME");
+  }
+
+  @Override
+  public String getFilter() {
+    return "autopage-no-reply@ci.scarborough.me.us";
+  }
+  
+  private static final String[] CITY_LIST= new String[]{
+    "BALDWIN",
+    "BRIDGTON",
+    "BRUNSWICK",
+    "CAPE ELIZABETH",
+    "CASCO",
+    "CHEBEAGUE ISLAND",
+    "CUMBERLAND",
+    "FALMOUTH",
+    "FREEPORT",
+    "FRYE ISLAND",
+    "GORHAM",
+    "GRAY",
+    "HARPSWELL",
+    "HARRISON",
+    "LONG ISLAND",
+    "NAPLES",
+    "NEW GLOUCESTER",
+    "NORTH YARMOUTH",
+    "PORTLAND",
+    "POWNAL",
+    "RAYMOND",
+    "SCARBOROUGH",
+    "SEBAGO",
+    "SOUTH PORTLAND",
+    "STANDISH",
+    "WESTBROOK",
+    "WINDHAM",
+    "YARMOUTH"
+  };
+}
