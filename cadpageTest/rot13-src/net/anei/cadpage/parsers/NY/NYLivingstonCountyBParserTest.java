e45bfefbebb00840704d5dfe5ff88faf2804a658
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-19 08:09:19 +0000

    Skeleton for NYLivingstonCountyBParserTest is complete with one section.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NY/NYLivingstonCountyBParserTest.java
@@ -0,0 +1,72 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AL;
+
+vzcbeg bet.whavg.Grfg;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+
+/*
+Yvivatfgba Pbhagl, AL
+Pbagnpg: Npgvir911
+Ntrapl anzr: Urzybpx Sver Qrcg
+Ybpngvba: Urzybpx, AL, Havgrq Fgngrf
+Fraqre: urzybpxsq@ebpurfgre.ee.pbz, urzybpxsver@ebpurfgre.gjpop.pbz 
+
+(5870 OVT GERR EQ) Qvfcngpu ** 17:37 ** Fgehpgher ** 5870 OVT GERR EQ ** YNXRIVYYR CX/EHFFRYY QE-ABEGU ** WHYVR ZPTRR ** 
+(7427 CYRNFNAG FG) Qvfcngpu **  ** Pbag. Ohea ** 7427 CYRNFNAG FG ** ZNVA FG/R QRNQ RAQ- ** FGRCURA T ZBBER ** 
+(7275 PBL EQ) Qvfcngpu ** 22:01 ** Fgehpgher ** 7275 PBL EQ ** SRQRENY EQ/CYNAX EQ- ** PNEY FCRAPRE ** 
+(URZYBPX UBARBLR EQ) Qvfcngpu ** 20:04 ** ZIN 8 ** URZYBPX UBARBLR EQ/ZNVA FG ** - ** NZORE CHPXARG ** 
+(7618 OVT GERR EQ) Qvfcngpu ** 02:10 ** ZIN 3 ** 7618 OVT GERR EQ ** PYNL FG/BAGNEVB PBHAGL YVAR- ** EBOREG FGRXY ** 
+(OVT GERR EQ) Qvfcngpu **  ** ZIN 3 ** OVT GERR EQ/ZNVA FG ** - ** WNZRF F ZBBER ** 
+(7412 EVK UVYY EQ) Qvfcngpu **  ** Sver Nynez 1 ** 7412 EVK UVYY EQ ** OYNAX EQ/URZYBPX YNHAPU EQ-EBP ** NHGBQVNYRE ** 
+(3164 CYNAX EQ) Qvfcngpu ** 13:48 ** ZIN 3 ** 3164 CYNAX EQ ** TERRAR IVYYNTR QE/FGBAR UVYY E ** TNOEVRYYR LBHAT ** 
+(5129 R YNXR EQ) Qvfcngpu ** 22:17 ** Sver Glcr Hax ** 5129 R YNXR EQ ** UNEGFBA CG/UBYZRF UVYY EQ-YNXR ** FURYGBA ZBLRE ** 
+(21 OVT GERR FG) Qvfcngpu ** 20:57 ** Fgehpgher ** 21 OVT GERR FG ** J YVIBAVN GBJA YVAR/YNXR TYRA ** XRIVA QBHTUREGL ** 
+(5168 ZNEEBJONPX EQ) Qvfcngpu **  ** Pbag. Ohea ** 5168 ZNEEBJONPX EQ ** UNEQRE EQ/AVIRE EQ- ** JVYYVNZ OEBJA ** 
+(4830 PBUJL 36) Qvfcngpu **  ** Fgnaq Ol Sver ** 4830 PBUJL 36 ** HFUJL 20N/OEVTTF FG-UBARBLR EV ** BAGNEVB PBHAGL ** 
+(5141 ONYQ UVYY EQ) Qvfcngpu **  ** Jverf ** 5141 ONYQ UVYY EQ ** BYQ ONYQ UVYY EQ/BAGNEVB PBHAG **  ** 
+(7412 EVK UVYY EQ) Qvfcngpu **  ** Puvzarl ** 7412 EVK UVYY EQ ** OYNAX EQ/URZYBPX YNHAPU EQ-EBP ** URZYBPX JNGRE G ** 
+(4830 PBUJL 36) Qvfcngpu **  ** Fgnaq Ol Sver ** 4830 PBUJL 36 ** HFUJL 20N/OEVTTF FG-UBARBLR EV **  ** 
+(5880 FGUJL 15N) Qvfcngpu **  ** Jverf ** 5880 FGUJL 15N ** CHEPRYY UVYY EQ/YNJERAPR UVYY ** BAGNEVB PB FURE ** 
+(2934 QBENA EQ NERN) Qvfcngpu **  ** Tenff ** 2934 QBENA EQ NERN ** PUNFR EQ/GBJAYVAR EQ- ** WRFFVPN NYYVRG ** 
+(7381 PNANQVPR EQ) Qvfcngpu ** 23:04 ** ZIN 6 ** 7381 PNANQVPR EQ ** ERLABYQF THYY EQ/JURNGBA UVYY ** BAGNEVB PB SVER ** 
+(5992 OVT GERR EQ) Qvfcngpu ** 08:31 ** Erfphr ** 5992 OVT GERR EQ ** EHFFRYY QE/PNZC EHA QE- ** FUNEV QHEAOHET ** 
+(7400 EVK UVYY EQ) Qvfcngpu **  ** Freivpr ** 7400 EVK UVYY EQ ** OYNAX EQ/URZYBPX YNHAPU EQ-URZ ** BAGNEVB PB ** 
+(9730 YNJERAPR UVYY EQ) Qvfcngpu **  ** Sver Nynez 1 ** 9730 YNJERAPR UVYY EQ ** YNJERAPR UVYY EQ/FGUJL 15N- **  ** 
+(8145 F ZNVA FG) Qvfcngpu **  ** Fgnaq Ol Sver ** 8145 F ZNVA FG ** QRCBG EQ/YVZR XVYA EQ-F/JNGRE ** FCEVATJNGRE SVE ** 
+(4830 PBUJL 36) Qvfcngpu ** 11:28 ** Fgnaq Ol Sver ** 4830 PBUJL 36 ** HFUJL 20N/OEVTTF FG-UBARBLR EV ** BAGNEVB PB SVER ** 
+(7440 OVT GERR EQ) Qvfcngpu ** 08:39 ** Pbag. Ohea ** 7440 OVT GERR EQ ** ORGGLEHGU YA/PYNL FG- ** EBANYQ SNEYRL ** 
+(5492 ZVQQYR EQ) Qvfcngpu **  ** Iruvpyr ** 5492 ZVQQYR EQ ** EVPUZBAQ GBJA YVAR/PHEGVF EQ- ** ANGUNA PBBX ** 
+(9486 NPR EQ) Qvfcngpu ** 11:05 ** Fgehpgher ** 9486 NPR EQ ** CHEPRYY EQ/QRNQ RAQ- ** BAGNEVB PB FB ** 
+(5143 OYNAX EQ) Qvfcngpu **  ** Sver Nynez 1 ** 5143 OYNAX EQ ** UNEQRE EQ/AVIRE EQ- ** NQG ** 
+(NQNZF EQ) Qvfcngpu **  ** ZIN 3 ** NQNZF EQ/YVAQFYRL EQ ** - ** GBAL JNFUOHEA ** 
+(6131 OVT GERR EQ) Qvfcngpu ** 09:22 ** Fgehpgher ** 6131 OVT GERR EQ ** OEBAFBA UVYY EQ/CRAARZVGR EQ-P ** NAA UNEEVF ** 
+(4872 F YVIBAVN EQ) Qvfcngpu **  ** ZIN 8 ** 4872 F YVIBAVN EQ ** CEVPR EQ/UNNXF CNFF- ** GBZ GBSSRAL ** 
+(FGUJL 15N) Qvfcngpu **  ** ZIN 3 ** FGUJL 15N/CHEPRYY UVYY EQ ** - ** OVYY ZNEGVA ** 
+(5927 FGUJL 15N) Qvfcngpu **  ** ZIN 3 ** 5927 FGUJL 15N ** CHEPRYY UVYY EQ/YNJERAPR UVYY ** BAGNEVB PB ** 
+(6242 FRERAVGL QE) Qvfcngpu ** 12:11 ** Fgehpgher ** 6242 FRERAVGL QE ** QRNQ RAQ/PBUJL 37- ** YVFN QHMNX ** 
+(7440 OVT GERR EQ) Qvfcngpu **  ** Pbag. Ohea ** 7440 OVT GERR EQ ** ORGGLEHGU YA/PYNL FG- ** EBANYQ SENYRL ** 
+(4213 F YVIBAVN EQ) Qvfcngpu **  ** Fgnaq Ol Sver ** 4213 F YVIBAVN EQ ** F YVIBAVN IVYYNTR YVZV/ZRZBEVN **  ** 
+(7270 NQNZF EQ) Qvfcngpu **  ** Sver Nynez 1 ** 7270 NQNZF EQ ** OYNAX EQ/ZNVA FG- ** GJP ** 
+(FGUJL 15N) Qvfcngpu **  ** Fzbxr Vairfg. ** FGUJL 15N/CHEPRYY UVYY EQ ** - ** BAGNEVB PB ** 
+(3559 CROOYR ORNPU EQ) Qvfcngpu ** 11:04 ** Fgehpgher ** 3559 CROOYR ORNPU EQ ** OVT GERR EQ/PYHAL CG- ** FHFNA JUVGZNA ** 
+(1997 Q"NATRYB QE) Qvfcngpu ** 02:57 ** ZIN 8 ** 1997 Q"NATRYB QE ** J YNXR EQ/R QRNQ RAQ-YVI PB JN ** QNEPL OBBGU ** 
+(4717 R YNXR EQ) Qvfcngpu ** 09:46 ** ZIN 10 ** 4717 R YNXR EQ ** PYRNEL EQ/CEVPR EQ- ** WNFBA OBEQRA ** 
+
+*/
+
+choyvp pynff ALYvivatfgbaPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ALYvivatfgbaPbhaglOCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "YVIVATFGBA PBHAGL", "AL");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+  
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ALYvivatfgbaPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
