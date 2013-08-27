77b64dbd646275d415b55ef77b3548f2b4850dda
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-08-27 15:11:11 +0000

    Fixed parsing problem with Roanoke City, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VARoanokeCityParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VARoanokeCityParser.java
@@ -1,22 +1,16 @@
 package net.anei.cadpage.parsers.VA;
 
-import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
 /**
  * Roanoke City, VA
  */
-public class VARoanokeCityParser extends FieldProgramParser {
-  
-  private static final Pattern MASTER = Pattern.compile("([A-Za-z0-9 ]+) at (.*)");
+public class VARoanokeCityParser extends SmartAddressParser {
   
   public VARoanokeCityParser() {
-    super("ROANOKE CITY", "VA",
-           "ADDR! CMT:INFO! Original_Location:PLACE");
+    super("ROANOKE CITY", "VA");
+    setFieldList("CALL ADDR APT INFO");
   }
   
   @Override
@@ -25,45 +19,23 @@ public class VARoanokeCityParser extends FieldProgramParser {
   }
   
   @Override
-  protected boolean parseMsg(String body, Data data) {
-    Matcher match = MASTER.matcher(body);
-    if (match.matches()) {
-      data.strCall = match.group(1).trim();
-      String sAddr = match.group(2).replace("APT ", " APT").trim();
-      parseAddress(sAddr, data);
-      if (checkAddress(data.strAddress, 2) > 0) return true;
-      data.initialize(this);
-    }
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!subject.equals("E-911")) return false;
+    int pt = body.indexOf(" [Att");
+    if (pt >= 0) body = body.substring(0,pt).trim();
     
-    return super.parseMsg(body, data);
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      int pt = field.indexOf(' ');
-      if (pt < 0) abort();
-      data.strCall = convertCodes(field.substring(0,pt), CALL_CODES);
-      super.parse(field.substring(pt+1).trim(), data);
-    }
+    if (body.length() < 21 || body.charAt(20) != ' ') return false;
+    data.strCall = body.substring(0,20).trim();
+    body = body.substring(21).trim();
     
-    @Override
-    public String getFieldNames() {
-      return "CALL " + super.getFieldNames();
+    pt = body.indexOf(" Original Location :");
+    if (pt >= 0) {
+      parseAddress(body.substring(0,pt).trim(), data);
+      data.strSupp = body.substring(pt+20).trim();
+    } else {
+      parseAddress(StartType.START_ADDR, body, data);
+      data.strSupp = getLeft();
     }
+    return true;
   }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    return super.getField(name);
-  }
-  
-  private static final Properties CALL_CODES = buildCodeTable(new String[]{
-      "F-ALARM",  "FIRE ALARM",
-      "FIRE",     "STRUCTURE FIRE",
-      "GSWI",     "GUN SHOT WOUND",
-      "ROBI",     "ROBBERY IN PROGRESS",
-      "PURSI",    "PURSUIT"
-  });
 }
