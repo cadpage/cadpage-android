49d933f743f2dda7026e084bb88feb7192e826df
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-07 04:46:19 +0000

    Added run report and general alert parsing for St Francois County, MO

--- cadpage/src/net/anei/cadpage/parsers/MO/MOStFrancoisCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MO/MOStFrancoisCountyParser.java
@@ -1,11 +1,16 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchCiscoParser;
 
 
 
 public class MOStFrancoisCountyParser extends DispatchCiscoParser {
+  
+  private static final Pattern SPECIAL_MSG_PTN = Pattern.compile("Unit: *([A-Z0-9]+) *(.*?) -> (.*)");
   
   public MOStFrancoisCountyParser() {
     super("ST FRANCOIS COUNTY", "MO");
@@ -18,6 +23,26 @@ public class MOStFrancoisCountyParser extends DispatchCiscoParser {
   
   @Override
   public boolean parseMsg(String subject, String body, Data data) {
+    Matcher match = SPECIAL_MSG_PTN.matcher(body);
+    if (match.matches()) {
+      data.strUnit = match.group(1);
+      String type = match.group(2).trim();
+      String msg =  match.group(3).trim();
+      if (type.equals("Is Clearing from")) {
+        data.strCall = "RUN REPORT";
+        data.strPlace = type + " -> " + msg;
+        return true;
+      }
+      if (type.equals("Notification")) {
+        data.strCall = "GENERAL ALERT";
+        data.strPlace = msg;
+        return true;
+      }
+      data.strCall = "GENERAL ALERT";
+      data.strPlace = type + " -> " + msg;
+      return true;
+    }
+    
     return super.parseMsg(subject, body.replace('\n', ' ').replace("!",""), data);
   }
 }
