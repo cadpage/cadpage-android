6e30f17356e9e6dbe5e6a26527ecdceb8a792a47
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-16 08:52:13 +0000

    AZYavapaiCountyB and TXNacogdochesCounty are completed.

--- cadpage/src/net/anei/cadpage/parsers/AZ/AZYavapaiCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AZ/AZYavapaiCountyParser.java
@@ -1,95 +1,12 @@
-
 package net.anei.cadpage.parsers.AZ;
 
-import java.util.Properties;
-
-import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
 /**
- * Mesa, AZ (Maricopa County)
+ * Yavapai County, AZ
  */
-
-public class AZYavapaiCountyParser extends FieldProgramParser {
-
+public class AZYavapaiCountyParser extends GroupBestParser {
   public AZYavapaiCountyParser() {
-    super(CITY_CODES, "YAVAPAI COUNTY", "AZ",
-           "CT:ADDR/SC! BOX:BOX! DUE:UNIT!");
+    super(new AZYavapaiCountyAParser(), new AZYavapaiCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "@sedonafire.org";
-  }
-  
-  @Override
-  public int getMapFlags() {
-    return MAP_FLG_SUPPR_LA;
-  }
-  
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "AG",   "AGUILA",
-      "ASHF", "ASH FORK",
-      "BAGD", "BAGDAD",
-      "BCC",  "BLACK CANYON CITY",
-      "CHNO", "CHINO VALLEY",
-      "CHRY", "CHERRY",
-      "CKNG", "CROWN KING",
-      "CLK",  "CLARKDALE",
-      "CLTR", "CLEATOR",
-      "CMPW", "CAMP WOOD",
-      "CNTY", "COUNTY",
-      "CONG", "CONGRESS",
-      "CRDJ", "CORDES JUNCTION",
-      "CRDS", "CORDES LAKES",
-      "CRNV", "CORNVILLE",
-      "CSHS", "CASTLE HOT SPRINGS",
-      "CTWD", "COTTONWOOD",
-      "CV",   "CAMP VERDE",
-      "DATE", "DATE CREEK",
-      "DEHB", "DEWEY HUMBOLT",
-      "DEWY", "DEWEY",
-      "DRAK", "DRAKE",
-      "FLG",  "FLAGSTAFF",
-      "FSER", "FOREST SERVICE",
-      "GRMC", "GROOM CREEK",
-      "HILL", "HILLSIDE",
-      "HPNS", "HPNS",
-      "HUMB", "HUMBOLT",
-      "IRON", "IRON SPRINGS",
-      "JPRW", "JUNIPERWOOD RANCH",
-      "JRME", "JEROME",
-      "KIRK", "KIRKLAND",
-      "LKMT", "LAKE MONTEZUMA",
-      "MAYR", "MAYER",
-      "MCGV", "MCGUIREVILLE",
-      "MHR",  "MAYERHWYRESCUE",
-      "MNDP", "MUNDS PARK",
-      "PAUL", "PAULDEN",
-      "PEEP", "PEEPLES VALLEY",
-      "PERK", "PERKINSVILLE",
-      "POND", "PONDEROSA PARK",
-      "PRES", "PRESCOTT",
-      "PRV",  "PRESCOTT VALLEY",
-      "PWD",  "PINEWOOD",
-      "RIMR", "RIMROCK",
-      "SED",  "SEDONA",
-      "SIM",  "SIMMONS",
-      "SKLV", "SKULL VALLEY",
-      "SLGM", "SELIGMAN",
-      "STAN", "STANTON",
-      "SV",   "SPRING VALLEY",
-      "VV",   "VERDE VALLEY",
-      "WAG",  "WAGONER",
-      "WCHI", "WEST CHINO",
-      "WILV", "WILLIAMSON VALLEY",
-      "WKBG", "WICKENBURG",
-      "WKR",  "WALKER",
-      "WLHT", "WILHOIT",
-      "WNTG", "WALNUT GROVE",
-      "YAN",  "YAVAPAI INDIAN RESERVATION",
-      "YARN", "YARNELL",
-      "YAVA", "YAVA",
-      "YAVC", "YAVAPAI COUNTY",
-
-  });
 }
