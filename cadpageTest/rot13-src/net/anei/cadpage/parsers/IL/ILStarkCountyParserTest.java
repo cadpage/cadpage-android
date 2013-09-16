c6abff64054caa993f9d8ddea099d7f614c14fdd
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-16 09:25:14 +0000

    OHLoganCountyParser, KYGallatinCountyParser, and ILStarkCountyParser are all completed.

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/IL/ILStarkCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/IL/ILStarkCountyParserTest.java
@@ -5,7 +5,7 @@ vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
 vzcbeg bet.whavg.Grfg;
 
 /*
-Fgnexr Pbhagl, VA
+Fgnex Pbhagl, VY
 Ntrapl anzr: Fgnex Pbhagl Nzohynapr
 Ybpngvba: Gbhyba, VY, Havgrq Fgngrf
 Pbagnpg: Npgvir911
