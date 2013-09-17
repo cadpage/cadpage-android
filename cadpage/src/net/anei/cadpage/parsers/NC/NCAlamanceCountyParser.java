417ca87a357fbeda2565464070bfc65fe30c7314
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-17 18:24:06 +0000

    Fixed parsing problem with Alamance County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCAlamanceCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCAlamanceCountyParser.java
@@ -1,6 +1,10 @@
 package net.anei.cadpage.parsers.NC;
 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
@@ -11,13 +15,49 @@ public class NCAlamanceCountyParser extends DispatchOSSIParser {
   
   public NCAlamanceCountyParser() {
     super(CITY_CODES, "ALAMANCE COUNTY", "NC",
-           "ID?: CALL ADDR CITY X X INFO+");
+           "ID?: CALL ADDR APT? CITY/Y! X X INFO+");
   }
   
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (subject.contains(";")) {
+      int pt = body.indexOf(":CAD:");
+      if (pt < 0) return false;
+      pt += 5;
+      body = body.substring(0,pt) + subject + ' ' +  body.substring(pt);
+    }
+    return super.parseMsg(body, data);
+  }
+
   @Override
   public String getFilter() {
     return "CAD@alamance-nc.com";
   }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("APT")) return new MyAptField();
+    return super.getField(name);
+  }
+  
+  private class MyAptField extends AptField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.startsWith("DIST")) {
+        data.strSupp = field;
+        return;
+      }
+      Matcher match = APT_PTN.matcher(field);
+      if (match.matches()) field = match.group(1);
+      super.parse(field, data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "INFO APT";
+    }
+  }
+  private static final Pattern APT_PTN = Pattern.compile("(?:APT|LOT|UNIT|#)[- ]*(.*)");
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "GRAH",  "GRAHAM",
