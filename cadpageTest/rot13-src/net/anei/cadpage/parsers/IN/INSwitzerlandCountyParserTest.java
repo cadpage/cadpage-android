5138a9b07f349c4c0f6c6bda5d1155c05aeb374b
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-19 10:12:16 +0000

    skeleton for INSwitzerlandCountyParserTest is complete with one section.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/IN/INSwitzerlandCountyParserTest.java
@@ -0,0 +1,49 @@
+cnpxntr arg.narv.pnqcntr.cnefref.VA;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Fjvgmreynaq Pbhagl, VA
+Pbagnpg: Npgvir911
+Ntrapl anzr: Fjvgmreynaq Pb. Furevss'f Qrcg.
+Ybpngvba: Irinl, VA, Havgrq Fgngrf
+Fraqre: QVFCNGPU@fpfb-va.bet 
+
+[01-JNVAFPB]-- - Angher: Nhgb Nppvqrag, Cebcregl Qnzntr - Ybpngvba: 12830.00 ZNFBA SNAPURE EQ EVFVAT FHA - Pbzzragf: PNYYRE NQI'Q URE PNE NAQ N OHF JRER VAIBYIRQ VA N 1050CQ. JBHYQ YVXR GB ZNXR N ERCBEG.
+[XUNLRF]-- - Angher: Nzohynapr Pnyy, Orygreen - Ybpngvba: 777.00 ORYGREEN QE SYBERAPR - Pbzzragf: 56LB Z PURFG CNVAF
+[01-JNVAFPB]-- - Angher: Genvavat Pnyy - Ybpngvba: 13038.00 ORGURY EQT EQ IRINL - Pbzzragf: GUVF VF N TERNG GRFG PNYY.
+QVFCNGPU:[01-JNVAFPB]-- - Angher: Genvavat Pnyy - Ybpngvba: 13038.00 ORGURY EQT EQ IRINL - Pbzzragf: GUVF VF N TERNG GRFG PNYY.
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Zvfpryynarbhf - Ybpngvba: 9119.00 GNCCF EVQTR EQ IRINL - Pbzzragf: EBA NQI'Q GUNG UR JNF NG GUR ONPX BS UVF CEBCREGL NAQ UR SBHAQ N YVGGYR TVEYF OVXR, HAQREJRNE NAQ ZRAF HAQREJRNE.
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Genssvp Unmneq - Ybpngvba: 10554.00 QENXRF EQT EQ ORAAVATGBA - Pbzzragf: GERR VA GUR EBNQ
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Genssvp Unmneq - Ybpngvba: 3482.00 FGNGR EBNQ 250 IRINL - Pbzzragf: ybt va ebnq
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Nhgb Nppvqrag, Cebcregl Qnzntr - Ybpngvba: 9152.00 SNVEIVRJ EQ IRINL - Pbzzragf: abg uheg whfg arrqf gbj
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Nzohynapr Pnyy, Orygreen - Ybpngvba: 777.00 ORYGREEN QE SYBERAPR - Pbzzragf: 21 LRNE BYQ SRZNYR JUB PUBXRQ BA SBBQ -- ZRRG VA RZG EBBZ ERF10 NQI'Q ARRQ GB CNTR SBE QEVIRE- 381 NQI'Q UR JBHYQ ZRRG UVZ NG ORYG
+QVFCNGPU:[XUNLRF]-- - Angher: Nzohynapr Pnyy, Orygreen - Ybpngvba: 777.00 ORYGREEN QE SYBERAPR - Pbzzragf: 21 LRNE BYQ SRZNYR JUB PUBXRQ BA SBBQ -- ZRRG VA RZG EBBZ ERF10 NQI'Q ARRQ GB CNTR SBE QEVIRE- 381 NQI'Q UR JBHYQ ZRRG UVZ NG ORYGREE
+QVFCNGPU:[XUNLRF]-- - Angher: Nzohynapr Pnyy - Ybpngvba: 12059.00 RAGRECEVFR QE SYBERAPR - Pbzzragf: PRYY 170-2 F FRPGBE NYG#812-611-1027 K-084.986694 L +038.864157 PS95% CEBDN ZRQVPNY: ARJ PNFR AHZORE NFFVTARQ PNYY 13-0004780 OL AWNZRF (CB
+QVFCNGPU:[AWNZRF]-- - Angher: Nzohynapr Pnyy - Ybpngvba: 12059.00 RAGRECEVFR QE SYBERAPR - Pbzzragf: PRYY 170-2 F FRPGBE NYG#812-611-1027 K-084.986694 L +038.864157 PS95% CEBDN ZRQVPNY: ARJ PNFR AHZORE NFFVTARQ PNYY 13-0004780 OL AWNZRF (CB
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Nzohynapr Pnyy - Ybpngvba: 12059.00 RAGRECEVFR QE SYBERAPR - Pbzzragf: PRYY 170-2 F FRPGBE NYG#812-611-1027 K-084.986694 L+038.864157 PS95% CEBDN ZRQVPNY: ARJ PNFR AHZORE NFFVTARQ PNYY 13-0004780 OL AWNZRF (
+QVFCNGPU:[PUBHFZLRE]-- - Angher: - Ybpngvba: 777.00 ORYGREEN QE SYBERAPR - Pbzzragf: 21 LRNE BYQ SRZNYR JUB PUBXRQ BA SBBQ -- ZRRG VA RZG EBBZ
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Sbyybj Hc Vairfgvtngvba - Ybpngvba: 103.00 ZNCYR FG IRINL - Pbzzragf:
+QVFCNGPU:[PUBHFZLRE]-- - Angher: Zvfpryynarbhf - Ybpngvba: 12589.00 A FGNGR EBNQ 56 IRINL - Pbzzragf: CRBCYR NER FCCBFR GB ZBIR BHG BS UBHFR OL 6 NAQ VF YRNIVAT CNGEVBG ABJ JBHYQ YVXR NA BSSVPRE GB ZRRG URE GURER CBFFVOYR QNZNTR QBAR GB GUR
+QVFCNGPU:[XUNLRF]-- - Angher: Nzohynapr Pnyy - Ybpngvba: 15354.00 FGNGR EBNQ 250 EVFVAT FHA - Pbzzragf: CEBDN ZRQVPNY: ARJ PNFR AHZORE NFFVTARQ PNYY 13- 0004773 OL WNEVPUNEQF (CBF 02) NG 15:02:27 CEBDN ZRQVPNY: QVFCNGPU PNYY 13-0004773 OL W
+[XUNLRF]-- - Angher: Whiravyr Ceboyrz - Ybpngvba:     214.00 R ZNVA FG  \aIRINL - Pbzzragf: QNIVQ ZNEGVA OEBXRA VAGB N YBPXRQ QBBE.
+
+*/
+choyvp pynff VAFjvgmreynaqPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp VAFjvgmreynaqPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "FJVGMREYNAQ PBHAGL", "VA");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+    
+  }
+ 
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj VAFjvgmreynaqPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
