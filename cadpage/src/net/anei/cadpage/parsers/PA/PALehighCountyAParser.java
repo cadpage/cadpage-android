1519f6343edd1d0c88e3f1e204b4106421bd975e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-24 22:05:56 +0000

    Removed FD/ prefix from all units for Lehigh County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PALehighCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PALehighCountyAParser.java
@@ -1,7 +1,9 @@
 package net.anei.cadpage.parsers.PA;
 
 import java.util.Properties;
+import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchPrintrakParser;
 
 /**
@@ -9,24 +11,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchPrintrakParser;
  */
 public class PALehighCountyAParser extends DispatchPrintrakParser {
   
-  private static final Properties CITY_TABLE = buildCodeTable(new String[]{
-      "06 LONGSWAMP", "LONGSWAMP TWP",
-      "HANOVER", "HANOVER TWP",
-      "HEIDELBERG","HEIDELBERG TWP",
-      "LOWER MACUNGIE", "LOWER MACUNGIE TWP",
-      "LOWER MILFORD", "LOWER MILFORD TWP",
-      "LOWHILL", "LOWHILL TWP",
-      "LYNN","LYNN TOWNSHIP",
-      "NORTH WHITEHALL", "NORTH WHITEHALL TWP",
-      "SALISBURY", "SALISBURY TWP",
-      "SOUTH WHITEHALL", "SOUTH WHITEHALL TWP",
-      "UPPER MACUNGIE", "UPPER MACUNGIE TWP",
-      "UPPER MILFORD", "UPPER MILFORD TWP",
-      "UPPER SAUCON", "UPPER SAUCON TWP",
-      "WASHINGTON", "WASHINGTON TWP",
-      "WEISENBERG", "WEISENBERG TWP",
-      "WHITEHALL", "WHITEHALL TWP"
-  });
+  private static final Pattern UNIT_PFX_PTN = Pattern.compile("\\bFD/");
   
   public PALehighCountyAParser() {
     super(CITY_TABLE, "LEHIGH COUNTY", "PA", "UNTS:UNIT", FLG_USE_CMT1_CALL);
@@ -36,4 +21,30 @@ public class PALehighCountyAParser extends DispatchPrintrakParser {
   public String getFilter() {
     return "911@lehighcounty.org,messaging@iamresponding.com";
   }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    data.strUnit = UNIT_PFX_PTN.matcher(data.strUnit).replaceAll("");
+    return true;
+  }
+
+  private static final Properties CITY_TABLE = buildCodeTable(new String[]{
+      "06 LONGSWAMP",     "LONGSWAMP TWP",
+      "HANOVER",          "HANOVER TWP",
+      "HEIDELBERG",       "HEIDELBERG TWP",
+      "LOWER MACUNGIE",   "LOWER MACUNGIE TWP",
+      "LOWER MILFORD",    "LOWER MILFORD TWP",
+      "LOWHILL",          "LOWHILL TWP",
+      "LYNN",             "LYNN TOWNSHIP",
+      "NORTH WHITEHALL",  "NORTH WHITEHALL TWP",
+      "SALISBURY",        "SALISBURY TWP",
+      "SOUTH WHITEHALL",  "SOUTH WHITEHALL TWP",
+      "UPPER MACUNGIE",   "UPPER MACUNGIE TWP",
+      "UPPER MILFORD",    "UPPER MILFORD TWP",
+      "UPPER SAUCON",     "UPPER SAUCON TWP",
+      "WASHINGTON",       "WASHINGTON TWP",
+      "WEISENBERG",       "WEISENBERG TWP",
+      "WHITEHALL",        "WHITEHALL TWP"
+  });
 }
