c617fabeeb1013d812293248a3d8e80cb170c506
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-09-28 00:57:48 +0000

    PALawrenceCountyParser needs some refinements.

---
+++ cadpage/src/net/anei/cadpage/parsers/PA/PALawrenceCountyParser.java
@@ -0,0 +1,70 @@
+package net.anei.cadpage.parsers.PA;
+
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+
+
+
+public class PALawrenceCountyParser extends DispatchB2Parser {
+  
+  public PALawrenceCountyParser() {
+    super("911-CENTER:",CITY_LIST, "LAWRENCE COUNTY", "PA");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "911-CENTER@leoc.net, test@leoc.net";
+  }
+  
+  @Override
+  protected boolean isPageMsg(String body) {
+    return true;
+  }
+  
+  
+  @Override
+  protected boolean parseAddrField(String field, Data data) {
+    field = field.replace('@', '&').replace(" AT ", " & ");
+    return super.parseAddrField(field, data);
+  }
+  
+ 
+  private static final Pattern BYPASS_PTN = Pattern.compile("\\bBYPASS\\b", Pattern.CASE_INSENSITIVE);
+  
+  private static final String[] CITY_LIST = new String[]{
+    // Cities
+        "NEW CASTLE",
+    //  Boroughs
+        "BESSEMER",
+        "ELLPORT",
+        "ELLWOOD CITY // partly in Beaver County",
+        "ENON VALLEY",
+        "NEW BEAVER",
+        "NEW WILMINGTON",
+        "S.N.P.J.",
+        "SOUTH NEW CASTLE",
+        "VOLANT",
+        "WAMPUM",
+     //  Townships
+        "HICKORY TOWNSHIP",
+        "LITTLE BEAVER TOWNSHIP",
+        "MAHONING TOWNSHIP",
+        "NESHANNOCK TOWNSHIP",
+        "NORTH BEAVER TOWNSHIP",
+        "PERRY TOWNSHIP",
+        "PLAIN GROVE TOWNSHIP",
+        "PULASKI TOWNSHIP",
+        "SCOTT TOWNSHIP",
+        "SHENANGO TOWNSHIP",
+        "SLIPPERY ROCK TOWNSHIP",
+        "TAYLOR TOWNSHIP",
+        "UNION TOWNSHIP",
+        "WASHINGTON TOWNSHIP",
+        "WAYNE TOWNSHIP",
+        "WILMINGTON TOWNSHIP"};
+
+
+
+}
