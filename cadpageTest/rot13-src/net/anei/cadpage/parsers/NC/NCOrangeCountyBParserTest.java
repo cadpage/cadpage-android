6c77db29cf41006c742e16d71ca0fde827494e8e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-10-16 04:10:47 +0000

    Updated msg doc

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/NC/NCOrangeCountyBParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NC/NCOrangeCountyBParserTest.java
@@ -16,6 +16,10 @@ Fhowrpg:1306284 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=O75N913P96695NN564\a 
 Fhowrpg:1306284 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=O55N913P96695NN562\a BCF ?\a 86 NAQ UNZCGBA CBVAG\a RZF ZBIR HC
 Fhowrpg:1306284 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=NO5N913P96695NN560\a BCF 1\a 107 SNVESVRYQ PG\a 17O1 Snyy, Cbffvoyl qnatrebhf obql nern\a FHOQ: Snvebnxf
 
+Fhowrpg:131015-13 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=455NN73P96785N110O\aBCF 1\a209 RQTRJNGRE PVE\aSNYY\aFHOQ: Neyra Cnex @ Fbhgurea Ivyyntr
+Fhowrpg:131015-9 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=7Q5NN73P96785N1103\aBCF 1\a2317 QNZNFPHF PUHEPU EQ\aPNEQVNP RZF BAYL\aFHOQ: Qnznfphf Bnxf
+Fhowrpg:131015-7 (RZF)\auggc://cntrgenpx.arg/vapvqrag.cuc?c=715NN73P96785N11SR\aBCF 1\a203 YLAA QE\a17N3T Snyy\aChoyvp Nffvfg (ab vawhevrf/ab cevbevgl flzcgbzf)\aBa gur tebhaq be sybbe\aFHOQ: Jvaqjbbq
+
  */
 
 
@@ -118,7 +122,56 @@ choyvp pynff APBenatrPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "PBQR:17O1",
         "PNYY:RZF - Snyy, Cbffvoyl qnatrebhf obql nern",
         "CYNPR:Snvebnxf");
-  
+
+    qbGrfg("G7",
+        "Fhowrpg:131015-13 (RZF)\a" +
+        "uggc://cntrgenpx.arg/vapvqrag.cuc?c=455NN73P96785N110O\a" +
+        "BCF 1\a" +
+        "209 RQTRJNGRE PVE\a" +
+        "SNYY\a" +
+        "FHOQ: Neyra Cnex @ Fbhgurea Ivyyntr",
+
+        "VQ:131015-13",
+        "HEY:uggc://cntrgenpx.arg/vapvqrag.cuc?c=455NN73P96785N110O",
+        "PU:BCF 1",
+        "NQQE:209 RQTRJNGRE PVE",
+        "PNYY:RZF - SNYY",
+        "CYNPR:Neyra Cnex @ Fbhgurea Ivyyntr");
+
+    qbGrfg("G8",
+        "Fhowrpg:131015-9 (RZF)\a" +
+        "uggc://cntrgenpx.arg/vapvqrag.cuc?c=7Q5NN73P96785N1103\a" +
+        "BCF 1\a" +
+        "2317 QNZNFPHF PUHEPU EQ\a" +
+        "PNEQVNP RZF BAYL\a" +
+        "FHOQ: Qnznfphf Bnxf",
+
+        "VQ:131015-9",
+        "HEY:uggc://cntrgenpx.arg/vapvqrag.cuc?c=7Q5NN73P96785N1103",
+        "PU:BCF 1",
+        "NQQE:2317 QNZNFPHF PUHEPU EQ",
+        "PNYY:RZF - PNEQVNP RZF BAYL",
+        "CYNPR:Qnznfphf Bnxf");
+
+    qbGrfg("G9",
+        "Fhowrpg:131015-7 (RZF)\a" +
+        "uggc://cntrgenpx.arg/vapvqrag.cuc?c=715NN73P96785N11SR\a" +
+        "BCF 1\a" +
+        "203 YLAA QE\a" +
+        "17N3T Snyy\a" +
+        "Choyvp Nffvfg (ab vawhevrf/ab cevbevgl flzcgbzf)\a" +
+        "Ba gur tebhaq be sybbe\a" +
+        "FHOQ: Jvaqjbbq",
+
+        "VQ:131015-7",
+        "HEY:uggc://cntrgenpx.arg/vapvqrag.cuc?c=715NN73P96785N11SR",
+        "PU:BCF 1",
+        "NQQE:203 YLAA QE",
+        "PBQR:17N3T",
+        "PNYY:RZF - Snyy",
+        "VASB:Choyvp Nffvfg (ab vawhevrf/ab cevbevgl flzcgbzf)\aBa gur tebhaq be sybbe",
+        "CYNPR:Jvaqjbbq");
+
   }
 
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
