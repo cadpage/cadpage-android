131fec371142c4bdb3575aca1c3764cde58c6548
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-10-17 04:22:21 +0000

    Fixed parsing problem with Shelby County, AL

--- cadpage/src/net/anei/cadpage/parsers/AL/ALShelbyCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALShelbyCountyParser.java
@@ -12,9 +12,11 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  */
 public class ALShelbyCountyParser extends FieldProgramParser {
   
+  private static final Pattern PREFIX_PTN = Pattern.compile("^(?:NEW - |Arns Alert\n) *", Pattern.CASE_INSENSITIVE);
+  
   public ALShelbyCountyParser() {
-    super("SHELBY COUNTY", "AL",
-           "ID DATETIME SRC_UNIT CALL ADDR! APT X NAME");
+    super(CITY_CODES, "SHELBY COUNTY", "AL",
+           "( ID DATETIME | DATETIME ID ) SRC_UNIT CALL ADDR! APT X NAME");
   }
   
   @Override
@@ -24,33 +26,32 @@ public class ALShelbyCountyParser extends FieldProgramParser {
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
+  
+    Matcher match = PREFIX_PTN.matcher(body);
+    if (match.find()) body = body.substring(match.end());
     
-    do {
-      if (subject.equals("ARNS ALERT") &&  body.startsWith("NEW - ")) {
-        body = body.substring(6).trim();
-        break;
-      }
-      
-      if (body.startsWith("Arns Alert\n")) {
-        body = body.substring(11).trim();
-        break;
-      }
-
-      return false;
-    
-    } while (false);
     return parseFields(body.split("\n"), data);
   }
 
   private static final Pattern DATE_TIME_PTN = Pattern.compile("\\d{4}-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d");
   private class MyDateTimeField extends DateTimeField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) return false;
+      data.strDate = field.substring(5,7) + '/' + field.substring(8,10) + '/' + field.substring(0,4);
+      data.strTime = field.substring(11);
+      return true;
+    }
 
     @Override
     public void parse(String field, Data data) {
-      Matcher match = DATE_TIME_PTN.matcher(field);
-      if (!match.matches()) abort();
-      data.strDate = field.substring(5,7) + '/' + field.substring(8,10) + '/' + field.substring(0,4);
-      data.strTime = field.substring(11);
+      if (!checkParse(field, data)) abort();
     }
   }
   
@@ -69,11 +70,11 @@ public class ALShelbyCountyParser extends FieldProgramParser {
     }
   }
   
-  private static final Pattern ADDR_SPECIAL_PTN = Pattern.compile("([A-Z]{4}) [A-Z]{3}: @(.*?)(?::(.*))?");
+  private static final Pattern ADDR_SPECIAL_PTN = Pattern.compile("([A-Z]{4} [A-Z]{3}): @(.*?)(?::(.*))?");
   private static final Pattern ADDR_SPECIAL_PTN2 = Pattern.compile("(.*?):(.*?): *(\\d{3}-?\\d{3}-\\d{4})");
-  private static final Pattern ADDR_PLACE_PTN = Pattern.compile(" *: ?@?(.*)");
-  private static final Pattern ADDR_CITY_CODE_PTN = Pattern.compile(" +([A-Z]{4}) +[A-Z]{3}$");
-  private static final Pattern ADDR_APT_PTN = Pattern.compile("[ ,:]+(?:(?:APT|RM|ROOM|UNIT|SUITE?)(?![A-Z]) *([^ ]+)|((?:LOT|FL) *[^ ]+)|(?<=,) *(\\d+[A-Z]?))$");
+  private static final Pattern ADDR_PLACE_PTN = Pattern.compile("(LL\\([-+,0-9\\.:]+\\) *|[^:]+): ?@?(.*)");
+  private static final Pattern ADDR_APT_PTN = Pattern.compile("[ ,:]+(?:(?:APT|RM|ROOM|UNIT|SUITE?)(?![A-Z]) *([^ ]+(?: +FLR? *[^ ]+)?)|((?:LOT|FL) *[^ ]+)|(?<=,) *(\\d+[A-Z]?))$");
+  private static final Pattern ADDR_UNIT_PTN = Pattern.compile("^[A-Z]{2}FD\\b");
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
@@ -82,70 +83,76 @@ public class ALShelbyCountyParser extends FieldProgramParser {
       Matcher match = ADDR_SPECIAL_PTN.matcher(field);
       if (match.matches()) {
         data.strCity = convertCodes(match.group(1), CITY_CODES);
-        data.strPlace = match.group(2).trim();
-        parseAddress(getOptGroup(match.group(3)).trim(), data);
-        if (data.strAddress.length() == 0 && data.strPlace.startsWith("MM ")) {
-          data.strAddress = data.strPlace;
-          data.strPlace = "";
+        String place = match.group(2).trim();
+        String addr = getOptGroup(match.group(3));
+        match = ADDR_UNIT_PTN.matcher(place);
+        if (match.find()) {
+          data.strUnit = match.group();
+          place = place.substring(match.end()).trim();
         }
-        return;
-      }
-      
-      // Check for special case Address:Place:Phone
-      match = ADDR_SPECIAL_PTN2.matcher(field);
-      if (match.matches()) {
-        field = match.group(1).trim();
-        data.strPlace = match.group(2).trim();
-        data.strPhone = match.group(3);
-      }
-      
-      // OK, things look "normal"
-      // look for trailing place name
-      
-      // Check for trailing apt
-      match = ADDR_APT_PTN.matcher(field);
-      if (match.find()) {
-        String apt = match.group(1);
-        if (apt == null) apt = match.group(2);
-        if (apt == null) apt = match.group(3);
-        data.strApt = apt;
-        field = field.substring(0,match.start()).trim();
-      }
-      
-      match = ADDR_PLACE_PTN.matcher(field);
-      if (match.find()) {
-        String place = match.group(1);;
-        field = field.substring(0,match.start());
-        if (NUMERIC.matcher(place).matches()) {
-          data.strApt = append(place, "-", data.strApt);
+        if (addr.length() == 0) {
+          addr = place;
+          place = "";
+        }
+        field = addr;
+        if (place.endsWith("FD")) {
+          if (data.strSource.length() == 0) data.strSource = place;
         } else {
           data.strPlace = place;
         }
       }
-      
-      // Check for trailing apt (again)
-      match = ADDR_APT_PTN.matcher(field);
-      if (match.find()) {
-        if (data.strApt.length() == 0) {
+
+      else {
+        
+        // Check for special case Address:Place:Phone
+        match = ADDR_SPECIAL_PTN2.matcher(field);
+        if (match.matches()) {
+          field = match.group(1).trim();
+          data.strPlace = match.group(2).trim();
+          data.strPhone = match.group(3);
+        }
+        
+        // OK, things look "normal"
+        // look for trailing place name
+        
+        // Check for trailing apt
+        match = ADDR_APT_PTN.matcher(field);
+        if (match.find()) {
           String apt = match.group(1);
           if (apt == null) apt = match.group(2);
           if (apt == null) apt = match.group(3);
-          data.strApt = append(apt, "-", data.strApt);
+          data.strApt = apt;
+          field = field.substring(0,match.start()).trim();
         }
-        field = field.substring(0,match.start()).trim();
+        
+        match = ADDR_PLACE_PTN.matcher(field);
+        if (match.find()) {
+          field =  match.group(1).trim();
+          String place = match.group(2).trim();
+          if (NUMERIC.matcher(place).matches()) {
+            data.strApt = append(place, "-", data.strApt);
+          } else {
+            data.strPlace = place;
+          }
+        }
+        
+        // Check for trailing apt (again)
+        match = ADDR_APT_PTN.matcher(field);
+        if (match.find()) {
+          if (data.strApt.length() == 0) {
+            String apt = match.group(1);
+            if (apt == null) apt = match.group(2);
+            if (apt == null) apt = match.group(3);
+            data.strApt = append(apt, "-", data.strApt);
+          }
+          field = field.substring(0,match.start()).trim();
+        }
       }
 
       // Process city code and something that we are ignoring
-      match = ADDR_CITY_CODE_PTN.matcher(field);
-      if (match.find()) {
-        String city = CITY_CODES.getProperty(match.group(1));
-        if (city != null) {
-          data.strCity = city;
-          field = field.substring(0,match.start());
-        }
-      }
-      
-      parseAddress(field, data);
+      String saveCity = data.strCity;
+      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
+      if (saveCity.length() > 0) data.strCity = saveCity;
     }
     
     @Override
@@ -196,36 +203,75 @@ public class ALShelbyCountyParser extends FieldProgramParser {
   public String postAdjustMapAddress(String sAddress) {
     return sAddress.replace("EGG_AND_BUTTER", "EGG AND BUTTER");
   }
-  
-  @Override
-  public String adjustMapCity(String city) {
-    if (city.equals("FOURMILE")) return "SHELBY COUNTY";
-    if (city.equals("SAGINAW")) return "SHELBY COUNTY";
-    return city;
-  }
 
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "ALAB", "ALABASTER",
-      "ALMT", "MONTEVALLO",
-      "BRIR", "BRIERFIELD",
-      "CALE", "CALERA",
-      "CALJ", "CALERA",
-      "COLU", "COLUMBIANA",
-      "FOUR", "FOURMILE",
-      "HELN", "HELENA",
-      "KING", "COLUMBIANA",
-      "MONT", "MONTEVALLO",
-      "NONE", "",
-      "PELH", "PELHAM",
-      "SAGN", "SAGINAW",
-      "SHEL", "SHELBY",
-      "SUMH", "",
-      "WESJ", "WESTOVER",
-      "WEST", "WESTOVER",
-      "WILJ", "WILSONVILLE",
-      "WILS", "WILSONVILLE",
-      "WILT", "WILTON",
-      "WSHE", "MONTEVALLO"
+      
+      "ALA",      "ALABASTER",
+      "ALAB ALA", "ALABASTER",
+      "CALE ALA", "ALABASTER",
+      "CALJ ALA", "ALABASTER",
+      "SAGN ALA", "ALABASTER",
+      "WSHE ALA", "ALABASTER",
+      
+      "BFD",      "BRIERFIELD",
+      "BFLD BFD", "BRIERFIELD",
+      "BRIR BFD", "BRIERFIELD",
+
+      "CAL",      "CALERA",
+      "CALE CAL", "CALERA",
+      "CALJ CAL", "CALERA",
+      "SUMH CAL", "CALERA",
+
+      "COL",      "COLUMBIANA",
+      "COLU COL", "COLUMBIANA",
+      "FOUR COL", "COLUMBIANA",
+      "KING COL", "COLUMBIANA",
+      "NONE COL", "COLUMBIANA",
+      "SAGN COL", "COLUMBIANA",
+      "SUMH COL", "COLUMBIANA",
+      
+      "HRP",      "HARPERSVILLE",
+      "HARP HRP", "HARPERSVILLE",
+      "WESJ HRP", "WESTOVER",
+      "WILJ HRP", "HARPERSVILLE",
+      
+      "ALAB MAY", "ALABASTER",
+
+      "MON",      "MONTEVALLO",
+      "ALMT MON", "MONTEVALLO",
+      "CALE MON", "MONTEVALLO",
+      "CALJ MON", "MONTEVALLO",
+      "DRYV MON", "MONTEVALLO",
+      "MONT MON", "MONTEVALLO",
+      "PEAR MON", "MONTEVALLO",
+      "UOFM MON", "MONTEVALLO",
+      "WILT MON", "WILTON",
+      "WSHE MON", "MONTEVALLO",
+      
+      "HEL",      "HELENA",
+      "HELN HEL", "HELENA",
+      
+      "PEAR MAY", "MONTEVALLO",
+      
+      "SHE",      "SHELBY",
+      "SHEL SHE", "SHELBY",
+
+      "STE",      "STERRETT",
+      "CHEL STE", "STERRETT",
+      "WESJ STE", "WESTOVER",
+      "WEST STE", "WESTOVER",
+      
+      "WIL",      "WILSONVILLE",
+      "FOUR WIL", "WILSONVILLE",
+      "WESJ WIL", "WILSONVILLE",
+      "WILJ WIL", "WILSONVILLE",
+      "WILS WIL", "WILSONVILLE"
+      
+//      "NONE", "",
+//      "PELH", "PELHAM",
+//      "SAGN", "SAGINAW",
+//      "SHEL", "SHELBY",
+//      "SUMH", "", 
       
   });
 }
