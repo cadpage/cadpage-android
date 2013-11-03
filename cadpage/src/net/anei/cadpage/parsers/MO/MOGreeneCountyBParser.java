979278e770e3b82b0f311244d5c4911d99d256bd
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-11-03 22:42:59 +0000

    Checking in Greene County, MO (B)

---
+++ cadpage/src/net/anei/cadpage/parsers/MO/MOGreeneCountyBParser.java
@@ -0,0 +1,48 @@
+package net.anei.cadpage.parsers.MO;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+public class MOGreeneCountyBParser extends FieldProgramParser {
+
+  public MOGreeneCountyBParser() {
+    super("GREENE COUNTY", "MO", "LOC:ADDR! AD:PLACE! DESC:CALL! APT:APT! CRSTR:X! TYP:CODE CMT:INFO! TIME:SKIP!");
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("X")) return new MyCrossField();
+    return super.getField(name);
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.endsWith("/")) field = field.substring(0,field.length()-1).trim();
+      if (field.startsWith("/")) field = field.substring(1).trim();
+      super.parse(field, data);
+    }
+  }
+
+  @Override
+  public String adjustMapAddress(String sAddress) {
+    Matcher frMat = FARM_ROAD.matcher(sAddress);
+    if (frMat.matches()) {
+      sAddress = frMat.group(1) + " FARM ROAD " + frMat.group(2);
+    } else { 
+      Matcher shMat = STATE_HWY.matcher(sAddress);
+      if (shMat.matches()) {
+        sAddress = shMat.group(1) + " MISSOURI PP";
+      }
+    }
+
+    return sAddress;
+  }
+
+  private static Pattern FARM_ROAD = Pattern.compile("(\\d+ [NESW]) FR(\\d+)");
+  private static Pattern STATE_HWY = Pattern.compile("(\\d+ [NESW]) SHPP");
+
+}
