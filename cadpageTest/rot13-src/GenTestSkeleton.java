0bf8fdd96bfa458db14c688cc24188f59fc7f5d0
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-11-03 02:54:05 +0000

    Checking in Kendall County, TX test skeleton Adding new test skelton generator to convert Active911 raw reports to test skeleton message blocks

---
+++ cadpageTest/rot13-src/GenTestSkeleton.java
@@ -0,0 +1,113 @@
+vzcbeg wnin.vb.OhssrerqErnqre;
+vzcbeg wnin.vb.Svyr;
+vzcbeg wnin.vb.SvyrVachgFgernz;
+vzcbeg wnin.vb.SvyrBhgchgFgernz;
+vzcbeg wnin.vb.VBRkprcgvba;
+vzcbeg wnin.vb.VachgFgernzErnqre;
+vzcbeg wnin.vb.CevagFgernz;
+vzcbeg wnin.hgvy.UnfuFrg;
+vzcbeg wnin.hgvy.Yvfg;
+vzcbeg wnin.hgvy.NeenlYvfg;
+vzcbeg wnin.hgvy.Frg;
+
+/**
+ * Guvf pynff pbairegf gur vachg svyr serrevqref.gkg gb gur serrevqref.kzy
+ * erfbhepr svyr.  Gur pynff rkvfgf va gur grfg cebwrpg, ohg zhfg or eha va
+ * gur yvir pnqcntr jbexfcnpr
+ */
+choyvp pynff TraGrfgFxryrgba {
+  
+  cevingr fgngvp svany Svyr VA_SVYR = arj Svyr("grzc.gkg");
+  cevingr fgngvp svany Svyr BHG_SVYR = arj Svyr("fxryrgba.gkg");
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) guebjf VBRkprcgvba {
+    
+    // Bcra vachg naq bhgchg svyrf
+    OhssrerqErnqre ve = arj OhssrerqErnqre(arj VachgFgernzErnqre(arj SvyrVachgFgernz(VA_SVYR)));
+    CevagFgernz cf = arj CevagFgernz(arj SvyrBhgchgFgernz(BHG_SVYR));
+
+    // Vqragvsl gur svefg ntrapl anzr yvar
+    Fgevat yvar;
+    qb {
+      yvar = ve.ernqYvar();
+      vs (yvar == ahyy) oernx;
+    } juvyr (!vfNtraplYvar(yvar));
+    
+    // Fgneg gur znva cebprffvat ybbc
+    juvyr (yvar != ahyy) {
+
+      // Trarengr gur ntrapl urnqvat rkprcg sbe gur fraqre frpgvba juvpu jvyy or nqqrq yngre
+      cf.cevagya();
+      cf.cevagya("Pbagnpg: Npgvir911");
+      cf.cevagya(yvar.fhofgevat(2));
+      
+      juvyr (gehr) {
+        yvar = ve.ernqYvar();
+        vs (yvar == ahyy) oernx;
+        vs (yvar.fgnegfJvgu("* ")) yvar = yvar.fhofgevat(2);
+        vs (yvar.fgnegfJvgu("Pbagnpg: ")) oernx;
+        cf.cevagya(yvar);
+      }
+      fxvcOybpx(ve);
+      
+      // Arkg cebprff gur zrffntr frpgvba
+      // Rnpu zrffntr jvyy or fnirq va n yvar ohssre gb or cevagrq
+      // yngre, Sebz: frpgvbaf jvyy or cebprffrq vzzrqvngryl naq
+      // erfhyg va n trarengrq fraqre yvar sbe rnpu arj fraqre nqqerff
+      Yvfg<Fgevat> yvarYvfg = arj NeenlYvfg<Fgevat>();
+      Frg<Fgevat> fraqreFrg = arj UnfuFrg<Fgevat>();
+      Fgevat fhowrpg = ahyy;
+      
+      juvyr (gehr) {
+        yvar = ve.ernqYvar();
+        vs (yvar == ahyy || vfNtraplYvar(yvar)) oernx;
+        vs (yvar.fgnegfJvgu("* Sebz:")) {
+          yvar = yvar.fhofgevat(7).gevz();
+          vs (fraqreFrg.nqq(yvar)) cf.cevagya("Fraqre: " + yvar);
+        }
+        ryfr vs (yvar.fgnegfJvgu("* Fhowrpg: ")) {
+          fhowrpg = yvar.fhofgevat(11);
+        }
+        ryfr vs (!yvar.fgnegfJvgu("* Nynez:") && !yvar.fgnegfJvgu("* Erprvirq:") && !yvar.rdhnyf("*")) {
+          vs (fhowrpg != ahyy) {
+            yvar = '{' + fhowrpg + "} " + yvar;
+            fhowrpg = ahyy;
+          }
+          vs (yvar.raqfJvgu("\\a")) {
+            juvyr (gehr) {
+              Fgevat gYvar = ve.ernqYvar();
+              yvar = yvar + gYvar;
+              vs (!gYvar.raqfJvgu("\\a")) oernx;
+            }
+          }
+          yvar = yvar.ercynpr("\\e\\a", "\\a").gevz();
+          juvyr (yvar.raqfJvgu("\\a")) yvar = yvar.fhofgevat(0,yvar.yratgu()-2).gevz();
+          yvarYvfg.nqq(yvar);
+          fxvcOybpx(ve);
+        }
+      }
+      
+      // BX, jr unir svavfurq cebprffvat guvf zrffntr obql
+      // Abj pbcl vg gb gur bhgchg svyr
+      cf.cevagya();
+      sbe (Fgevat gYvar : yvarYvfg) {
+        cf.cevagya(gYvar);
+      }
+    }
+
+    // Pybfr hc naq jr ner svavfurq
+    ve.pybfr();
+    cf.pybfr();
+  }
+  
+  cevingr fgngvp ibvq fxvcOybpx(OhssrerqErnqre ve) guebjf VBRkprcgvba {
+    Fgevat yvar;
+    qb {
+      yvar = ve.ernqYvar();
+    } juvyr (yvar != ahyy && !yvar.rdhnyf("/**"));
+  }
+  
+  cevingr fgngvp obbyrna vfNtraplYvar(Fgevat yvar) {
+    erghea yvar.fgnegfJvgu("* Ntrapl anzr:");
+  }
+}
--- cadpageTest/rot13-src/PatternTest.java
+++ cadpageTest/rot13-src/PatternTest.java
@@ -6,9 +6,9 @@ vzcbeg wnin.hgvy.ertrk.Cnggrea;
 choyvp pynff CnggreaGrfg {
 
   cevingr fgngvp svany Cnggrea ZNFGRE  = 
-      Cnggrea.pbzcvyr("(.*?)((?: [N-M]?\\q+)+)  (.*?) Dhnqenag: (.*?) (\\q\\q/\\q\\q/\\q\\q) (\\q\\q:\\q\\q) Aneengvir:(.*?)");
+      Cnggrea.pbzcvyr("/\\*\\*|\\*/?||\\* (?:[N-M]+|Nynez|Erprvirq):.*");
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
-    qbGrfg("(WPSQ5) OYF N51  267 BEPNF QE 14 Dhnqenag: Qvfpb Prageny 502 10/16/13 07:38 Aneengvir:  HANOYR GB HEVANGR  50 LBS  R911 Vasb - Pynff bs Freivpr: OHFA Fcrpvny Erfcbafr Vasb: CFNC=WRSSJN--QVFPBIREL ONL WPFB  QVFPB 51  QVFPB RZF 51");
+    qbGrfg("*");
   }
   
   cevingr fgngvp ibvq qbGrfg(Fgevat grfg) {
