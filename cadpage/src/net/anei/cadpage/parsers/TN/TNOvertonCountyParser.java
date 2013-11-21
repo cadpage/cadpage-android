15c7cba749a4504f688ff5251957a839fbd9dc12
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-11-21 19:26:45 +0000

    Checking in Overton County, TN

--- cadpage/src/net/anei/cadpage/parsers/TN/TNOvertonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TN/TNOvertonCountyParser.java
@@ -1,9 +1,7 @@
 package net.anei.cadpage.parsers.TN;
 
-import java.util.Properties;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchB3Parser;
 
 /**
@@ -11,10 +9,10 @@ import net.anei.cadpage.parsers.dispatch.DispatchB3Parser;
  */
 public class TNOvertonCountyParser extends DispatchB3Parser {
   
-  private static final Pattern MARKER = Pattern.compile("^\\d+:911@OPECD.COM:");
+  private static final Pattern MARKER = Pattern.compile("^(?:\\d+:)?911@OPECD.COM:");
 
   public TNOvertonCountyParser() {
-    super(MARKER, CITY_CODES, "OVERTON COUNTY", "TN");
+    super(MARKER, CITY_LIST, "OVERTON COUNTY", "TN");
   }
   
   @Override
@@ -22,23 +20,25 @@ public class TNOvertonCountyParser extends DispatchB3Parser {
     return "911@OPECD.COM";
   }
   
- 
-  
-  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+  private static final String[] CITY_LIST = new String[]{
 
     // Cities and Towns
       
       "LIVINGSTON",
    
     // Unincorporated communities
-      "ALLONS ",
-      "ALLRED ",
-      "ALPINE ",
-      "CRAWFORD ",
-      "HILHAM ",
+      "ALLONS",
+      "ALLRED",
+      "ALPINE",
+      "CRAWFORD",
+      "HILHAM",
       "MINERAL SPRINGS",
-      "MONROE ",
-      "RICKMAN "
+      "MONROE",
+      "RICKMAN",
       
-  });
+      // Putnam County
+      "COOKEVILLE",
+      "MONTEREY"
+      
+  };
 }
