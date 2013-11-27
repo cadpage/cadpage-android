08bc174c06232f335d950da0545bef8ad5a57bc1
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-11-27 19:35:33 +0000

    Checking in Cape Girardeau County, MO

--- cadpage/src/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyParser.java
@@ -1,5 +1,7 @@
 package net.anei.cadpage.parsers.MO;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA12Parser;
 
@@ -7,26 +9,30 @@ import net.anei.cadpage.parsers.dispatch.DispatchA12Parser;
 public class MOCapeGirardeauCountyParser extends DispatchA12Parser {
   
   public MOCapeGirardeauCountyParser() {
-    super("CAPE GIRARDEAU COUNTY", "MO)");
-  }
-  
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strCity.equals("GRANT CO")) {
-      data.strCity = "GRANT COUNTY";
-      data.strState = "WV";
-    }
-    return true;
-  }
-  
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("CITY", "CITY ST");
+    super("CAPE GIRARDEAU COUNTY", "MO");
   }
   
   @Override
   public String getFilter() {
     return "CAD@CityofCape.org";
   }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    if (data.strPlace.equals("Resident")) data.strPlace = "";
+    else if (PLACE_NAME_PTN.matcher(data.strPlace).matches()) {
+      data.strName = data.strPlace;
+      data.strPlace = "";
+    }
+    return true;
+  }
+    
+  @Override
+  public String getProgram() {
+    return super.getProgram().replace("PLACE", "PLACE NAME");
+  }
+    
+    
+  private static final Pattern PLACE_NAME_PTN = Pattern.compile("[A-Z]+, +[A-Z]+", Pattern.CASE_INSENSITIVE);
 }
--- cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchA12Parser.java
+++ cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchA12Parser.java
@@ -1,5 +1,8 @@
 package net.anei.cadpage.parsers.dispatch;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
@@ -11,29 +14,73 @@ public class DispatchA12Parser extends FieldProgramParser {
           "CALL! Loc:ADDR Rcvd:TIME! Units:UNIT? Comments:INFO");
   }
   
+  private static final Pattern CALL_PTN = Pattern.compile("(.*) \\((.*)\\)");
   private class BaseCallField extends CallField {
     @Override
     public void parse(String field, Data data) {
-      if (!field.endsWith(" ()")) abort();
-      field = field.substring(0,field.length()-3).trim();
-      super.parse(field, data);
+      Matcher match = CALL_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCall = match.group(1).trim();
+      data.strCode = match.group(2).trim();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "CALL CODE";
     }
   }
   
+  private static final Pattern ADDR_DELIM_PTN = Pattern.compile("[;\\[]#|[;\\[]@|[:#;]");
+  private static final Pattern ADDR_APT_BLK_PTN = Pattern.compile(".*\\bBLK", Pattern.CASE_INSENSITIVE);
+  private static final Pattern ADDR_APT_PREFIX_PTN = Pattern.compile("^(?:APT|RM|ROOM|SUITE|STE) *(.*)|(?:LOT.*)|.{1,4}", Pattern.CASE_INSENSITIVE);
   private class BaseAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
-      Parser p = new Parser(field);
-      data.strApt = p.getLastOptional(':');
-      data.strApt = append(p.getLastOptional("[#"), "-", data.strApt);
-      data.strPlace = p.getLastOptional("[@");
-      int pt = data.strPlace.indexOf('#');
-      if (pt >= 0) {
-        data.strApt = data.strPlace.substring(pt+1).trim();
-        data.strPlace = data.strPlace.substring(0,pt).trim();
+      String lastDelim = null;
+      int lastCol = 0;
+      Matcher match = ADDR_DELIM_PTN.matcher(field);
+      while (match.find()) {
+        parseAddressPart(lastDelim, field.substring(lastCol, match.start()).trim(), data);
+        lastDelim = match.group();
+        lastCol = match.end();
       }
-      data.strCity = p.getLastOptional(',');
-      super.parse(p.get(), data);
+      parseAddressPart(lastDelim, field.substring(lastCol).trim(), data);
+    }
+    
+    private void parseAddressPart(String type, String part, Data data) {
+      if (type == null) {
+        int pt = part.lastIndexOf(',');
+        if (pt >= 0) {
+          data.strCity = part.substring(pt+1).trim();
+          part = part.substring(0,pt).trim();
+        }
+        super.parse(part, data);
+      }
+      
+      else if (type.equals(":")  || type.equals("[#") || type.equals("#")) {
+        data.strApt = append(data.strApt, "-", part);
+      }
+      
+      else if (type.equals("[@") || type.equals(";@")) {
+        data.strPlace = append(data.strPlace, " - ", part);
+      }
+      
+      else if (type.equals(";")) {
+        if (ADDR_APT_BLK_PTN.matcher(part).matches()) {
+          data.strAddress = append(part, " ", data.strAddress);
+        }
+        else { 
+          Matcher match = ADDR_APT_PREFIX_PTN.matcher(part);
+          if (match.matches()) {
+            if (match.group(1) != null) part = match.group(1);
+            data.strApt = append(data.strApt, "-", part);
+          } else {
+            data.strPlace = append(data.strPlace, " - ", part);
+          }
+        }
+      }
+      
+      else abort();  // Can't happen unless we screwed up somewhere
     }
     
     @Override
