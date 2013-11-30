474f80cc42b9aa1fa3eadaad6f52f69f078e09b6
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-11-30 22:16:48 +0000

    Fixed parsing problem with Lane County, OR

--- cadpage/src/net/anei/cadpage/parsers/OR/ORLaneCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORLaneCountyParser.java
@@ -5,65 +5,104 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchPrintrakParser;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
-public class ORLaneCountyParser extends DispatchPrintrakParser {
-  
-  private static final Pattern GEN_ALERT_PTN = Pattern.compile("([A-Z]+) +TIME: *([:0-9]+) +(.*)");
+public class ORLaneCountyParser extends DispatchOSSIParser {
   
   public ORLaneCountyParser() {
-    super(CITY_CODES, "LANE COUNTY", "OR");
+    super(CITY_CODES, "LANE COUNTY", "OR",
+          "( STATUS ADDR CITY! | UNIT CH? CALL CH? ADDR ( DATETIME! |  CITY? PLACE+? MAP! CH? CODE? DATETIME! ) )");
   }
   
   @Override
   public String getFilter() {
-    return "ce911@ci.eugene.or.us";
+    return "CAD@ci.eugene.or.us";
   }
-  
+
   @Override
   protected boolean parseMsg(String body, Data data) {
-    Matcher match = GEN_ALERT_PTN.matcher(body);
-    if (match.matches()) {
-      data.strCall = "GENERAL ALERT";
-      data.strUnit = match.group(1);
-      data.strTime = match.group(2);
-      data.strPlace = match.group(3);
-      return true;
-    }
+    Matcher match = FINAL_INTL_PTN.matcher(body);
+    if (match.find()) body = body.substring(0,match.start());
     return super.parseMsg(body, data);
+  }
+  private static Pattern FINAL_INTL_PTN = Pattern.compile(" *;[A-Z]{3}$");
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("STATUS")) return new CallField("UNDER CONTROL", true);
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("CH")) return new ChannelField("[A-Z]+ ?\\d+", true);
+    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("MAP")) return new MapField("(?!RM |SP)[A-Z]{1,2}(?:\\d{1,2}| [A-Z0-9])|\\d{4}|\\d{3}[A-Z]|[A-Z]\\d{3}", true);
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+  
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      Parser p = new Parser(field);
+      data.strSource = p.get(',');
+      data.strUnit = p.get();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "SRC UNIT";
+    }
+  }
+  
+  private static final Pattern PLACE_APT_PTN = Pattern.compile("(?:APT|ROOM|RM|STE)/? *(.*)|(?:LOT|FLR|FLOOR|SP).*|[A-Z0-9]{1,4}");
+  private class MyPlaceField extends PlaceField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = PLACE_APT_PTN.matcher(field);
+      if (match.matches()) {
+        String apt = match.group(1);
+        if (apt == null) apt = field;
+        data.strApt = append(data.strApt, "-", apt);
+      } else {
+        data.strPlace = append(data.strPlace, " - ", field);
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "APT PLACE";
+    }
   }
 
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "UNC BLA", "BLACHLY",
-      "UNC BLU", "BLUE RIVER",
-      "UNC CHE", "CHESHIRE",
-      "UNC COT", "COTTAGE GROVE",
-      "UNC CRE", "CRESWELL",
-      "UNC DEA", "DEADWOOD",
-      "UNC DEX", "DEXTER",
-      "UNC DOR", "DORENA",
-      "UNC ELM", "ELMIRA",
-      "UNC EUG", "EUGENE",
-      "UNC FAL", "FALL CREEK",
-      "UNC FLO", "FLORENCE",
-      "UNC HAR", "HARRISBURG",
-      "UNC JUN", "JUNCTION CITY",
-      "UNC LEA", "LEABURG",
-      "UNC LOR", "LORANE",
-      "UNC LOW", "LOWELL",
-      "UNC MAP", "MAPLETON",
-      "UNC MAR", "MARCOLA",
-      "UNC MON", "MONROE",
-      "UNC NOT", "NOTI",
-      "UNC OAK", "OAKRIDGE",
-      "UNC PLE", "PLEASANT HILL",
-      "UNC SPR", "SPRINGFIELD",
-      "UNC SWI", "SWISSHOME",
-      "UNC VEN", "VENETA",
-      "UNC VID", "VIDA",
-      "UNC WAN", "WALTON",
-      "UNC WEF", "WESTFIR",
-      "UNC WES", "WESTLAKE",
-      "UNC YAC", "YACHATS"
+      "BLA", "BLACHLY",
+      "BLU", "BLUE RIVER",
+      "CHE", "CHESHIRE",
+      "COT", "COTTAGE GROVE",
+      "CRE", "CRESWELL",
+      "DEA", "DEADWOOD",
+      "DEX", "DEXTER",
+      "DOR", "DORENA",
+      "ELM", "ELMIRA",
+      "EUG", "EUGENE",
+      "FAL", "FALL CREEK",
+      "FLO", "FLORENCE",
+      "HAR", "HARRISBURG",
+      "JUN", "JUNCTION CITY",
+      "LEA", "LEABURG",
+      "LOR", "LORANE",
+      "LOW", "LOWELL",
+      "MAP", "MAPLETON",
+      "MAR", "MARCOLA",
+      "MON", "MONROE",
+      "NOT", "NOTI",
+      "OAK", "OAKRIDGE",
+      "PLE", "PLEASANT HILL",
+      "SPR", "SPRINGFIELD",
+      "SWI", "SWISSHOME",
+      "VEN", "VENETA",
+      "VID", "VIDA",
+      "WAN", "WALTON",
+      "WEF", "WESTFIR",
+      "WES", "WESTLAKE",
+      "YAC", "YACHATS"
   });
 }
