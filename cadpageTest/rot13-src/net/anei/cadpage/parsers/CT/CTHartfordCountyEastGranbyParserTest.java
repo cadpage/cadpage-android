e97a61799e8d1f6da47a3ae064ea3fac0fed6e9e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-12-09 02:41:45 +0000

    Checking in CTHartfordCountyEastGranbyParser

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/CT/CTHartfordCountyEastGranbyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CT/CTHartfordCountyEastGranbyParserTest.java
@@ -19,24 +19,123 @@ Fraqre: abercyl@wbgsbez.pbz
 (Qvfcngpu Nyreg) Fvtany 50\ashyyl vaibyirq\a20 tevssra eq\aRnfg Tenaol\aPG\a11-10-2012 23:07\a218416100571243490\aYvfgra gb yvir srrq ng \auggc://nhqvb2.enqvbersrerapr.pbz/703804654
 (Qvfcngpu Nyreg) Fvtany 50\aFzbxr Fubjvat\a4 Ahgzrt eq\aRnfg Tenaol\aPG\a11-10-2012 23:06\a218416040571702931\aYvfgra gb yvir srrq ng \auggc://nhqvb2.enqvbersrerapr.pbz/703804654
 
-**  ABGRF **
-Cerggl onfvp SvryqCebtenzCnefre
-Svefg gjb svryqf tb va PNYY frcnengrq jvgu n " - "
-Frg gur fgngr gb na rzcgl fgevat vs vg raqf hc nf "PG".  Jr qba'g unir gb qvfcynl PG, gung vf gur qrsnhyg.
-Qvfpneq gur yvir srrq HEY
 */
 
 choyvp pynff PGUnegsbeqPbhaglRnfgTenaolCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
   choyvp PGUnegsbeqPbhaglRnfgTenaolCnefreGrfg() {
-    frgCnefre(arj QhzzlCnefre(), "UNEGSBEQ PBHAGL", "PG");
+    frgCnefre(arj PGUnegsbeqPbhaglRnfgTenaolCnefre(), "UNEGSBEQ PBHAGL", "PG");
   }
   
   @Grfg
   choyvp ibvq grfgCnefre() {
+    qbGrfg("G1",
+        "(Qvfcngpu Nyreg) Fvtany 50\a" +
+        "Fzbxr fubjvat\a" +
+        "4 ahgzrt eq\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-27-2012 19:45\a" +
+        "219872783641824518\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
 
-    
+        "PNYY:Fvtany 50 - Fzbxr fubjvat",
+        "NQQE:4 ahgzrt eq",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/27/2012",
+        "GVZR:19:45",
+        "VQ:219872783641824518");
 
+    qbGrfg("G2",
+        "(Qvfcngpu Nyreg) Fvtany 24\a" +
+        "Gi vafgnyy\a" +
+        "7 zrzbevny qe\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-17-2012 21:49\a" +
+        "219016166841849471\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
+
+        "PNYY:Fvtany 24 - Gi vafgnyy",
+        "NQQE:7 zrzbevny qe",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/17/2012",
+        "GVZR:21:49",
+        "VQ:219016166841849471");
+
+    qbGrfg("G3",
+        "(Qvfcngpu Nyreg) Fvtany 24\a" +
+        "grfg 2\a" +
+        "400 ghexrl uvyyf eq\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-10-2012 23:41\a" +
+        "218418111571957200\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
+
+        "PNYY:Fvtany 24 - grfg 2",
+        "NQQE:400 ghexrl uvyyf eq",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/10/2012",
+        "GVZR:23:41",
+        "VQ:218418111571957200");
+
+    qbGrfg("G4",
+        "(Qvfcngpu Nyreg) Fvtany 50\a" +
+        "Grfg\a" +
+        "250 Arjtngr eq\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-10-2012 23:19\a" +
+        "218416834571516565\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
+
+        "PNYY:Fvtany 50 - Grfg",
+        "NQQE:250 Arjtngr eq",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/10/2012",
+        "GVZR:23:19",
+        "VQ:218416834571516565");
+
+    qbGrfg("G5",
+        "(Qvfcngpu Nyreg) Fvtany 50\a" +
+        "shyyl vaibyirq\a" +
+        "20 tevssra eq\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-10-2012 23:07\a" +
+        "218416100571243490\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
+
+        "PNYY:Fvtany 50 - shyyl vaibyirq",
+        "NQQE:20 tevssra eq",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/10/2012",
+        "GVZR:23:07",
+        "VQ:218416100571243490");
+
+    qbGrfg("G6",
+        "(Qvfcngpu Nyreg) Fvtany 50\a" +
+        "Fzbxr Fubjvat\a" +
+        "4 Ahgzrt eq\a" +
+        "Rnfg Tenaol\a" +
+        "PG\a" +
+        "11-10-2012 23:06\a" +
+        "218416040571702931\a" +
+        "Yvfgra gb yvir srrq ng \a" +
+        "uggc://nhqvb2.enqvbersrerapr.pbz/703804654",
+
+        "PNYY:Fvtany 50 - Fzbxr Fubjvat",
+        "NQQE:4 Ahgzrt eq",
+        "PVGL:Rnfg Tenaol",
+        "QNGR:11/10/2012",
+        "GVZR:23:06",
+        "VQ:218416040571702931");
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
