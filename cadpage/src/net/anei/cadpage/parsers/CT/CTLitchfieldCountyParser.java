aeea94e253cdce35c2805abe2933d579b242c75d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-12-22 21:24:16 +0000

    New Location Litchfield County, CT (B)

--- cadpage/src/net/anei/cadpage/parsers/CT/CTLitchfieldCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/CT/CTLitchfieldCountyParser.java
@@ -1,108 +1,15 @@
 package net.anei.cadpage.parsers.CT;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.SmartAddressParser;
-import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.GroupBestParser;
 
 
-public class CTLitchfieldCountyParser extends SmartAddressParser {
-  
-  private static final Pattern MASTER = Pattern.compile("(.*) RESPOND TO (.*?)(?:,|,? +(\\d{1,2}-[A-Z]-\\d{1,2}(?:-[A-Z])?|HOT|ALPHA)) *(?::|--| -)(\\d\\d:\\d\\d)");
-  private static final Pattern MAU_HILL = Pattern.compile("^(.*) MAUWEEHOO H(?:IL)?L (.*)$");
-  private static final Pattern START_PAREN_PTN = Pattern.compile("^\\(.*?\\)");
+public class CTLitchfieldCountyParser extends GroupBestParser {
   
   public CTLitchfieldCountyParser() {
-    super(CITY_LIST, "LITCHFIELD COUNTY", "CT");
-    addExtendedDirections();
-    setFieldList("SRC ADDR X PLACE APT CITY CALL CODE TIME");
+    super(new CTLitchfieldCountyAParser(), new CTLitchfieldCountyBParser());
   }
   
-  @Override
-  public String getFilter() {
-    return "@lcd911.com,89361";
-  }
-  
-  @Override
-  public boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("LCD") && !subject.equals("LCD Message")) return false;
-    
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    data.strSource = match.group(1).trim();
-    String sAddr = match.group(2).trim();
-    data.strCode = getOptGroup(match.group(3));
-    data.strTime = match.group(4);
-    
-    Parser p = new Parser(sAddr);
-    data.strCall = p.getLast(',');
-    data.strPlace = p.getLast(',');
-    sAddr = p.get();
-    if (sAddr.length() == 0) return false;
-    
-    parseAddress(StartType.START_ADDR, FLAG_PAD_FIELD | FLAG_ANCHOR_END, sAddr, data);
-    if (data.strCity.equals("HEMLOCK ROXBURY")) data.strCity = "ROXBURY";
-    String sPlace = getPadField();
-    
-    
-    // There is a street called MAUWEEHOO HILL (or HL) that just confuses
-    // the heck out of the smart parser so we will make some special checks for it
-    if (data.strPlace.length() == 0) {
-      match = MAU_HILL.matcher(data.strAddress);
-      if (match.find()) {
-        data.strAddress = match.group(1) + " MAUWEEHOO HILL";
-        sPlace = match.group(2);
-      }
-    }
-    
-    // OK, there can be some strange things in the pad field
-    // A leading term in parens is an alternative street name that should be appended to the
-    // address.  It will be removed from the map address
-    match = START_PAREN_PTN.matcher(sPlace);
-    if (match.find()) {
-      data.strAddress = append(data.strAddress, " ", match.group());
-      sPlace = sPlace.substring(match.end()).trim();
-    }
-    
-    // What's left is an optional place name followed by an optional cross
-    // street, which may have an alternate street name in parens
-    // So first remove the tentative alternate street name
-    String chkCross = sPlace;
-    String extra = null;
-    if (chkCross.endsWith(")")) {
-      int pt = chkCross.indexOf('(');
-      if (pt >= 0) {
-        extra = chkCross.substring(pt);
-        chkCross = chkCross.substring(0,pt).trim();
-      }
-    }
-    
-    // Now use the smart addresss parser to separate the place name from the cross street
-    String savePlace = data.strPlace;
-    data.strPlace = "";
-    parseAddress(StartType.START_PLACE, FLAG_ONLY_CROSS | FLAG_ANCHOR_END, chkCross, data);
-    
-    // Append the extra paren info the the cross street if we found one
-    // or to the place name if we did not
-    if (extra != null) {
-      if (data.strCross.length() > 0) {
-        data.strCross = append(data.strCross, " ", extra);
-      } else {
-        data.strPlace = append(data.strPlace, " ", extra);
-      }
-    }
-    
-    // If we had a place name before going into this logic, the field we
-    //  just parsed as a place should really be an apartment
-    if (savePlace.length() > 0) {
-      data.strApt = data.strPlace;
-      data.strPlace = savePlace;
-    }
-    return true;
-  }
-  
-  private static String[] CITY_LIST = new String[]{
+  static String[] CITY_LIST = new String[]{
     "BARKHAMSTED",
     "BETHLEHEM",
     "BRIDGEWATER",
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CT/CTLitchfieldCountyBParserTest.java
@@ -0,0 +1,155 @@
+cnpxntr arg.narv.pnqcntr.cnefref.PG;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Yvgpusvryq Pbhagl, PG
+
+Arj Zvysbeq, PG
+Pbagnpg: Tyraa Fpuvrgvatre <ygtyraa13@nby.pbz>
+Fraqre: nqzva@arjzvysbeqcbyvpr.bet
+
+(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***29-O-4***\a47 PUREAVFXR EQ \aARJ ZVYSBEQ\aQvfgevpg: AIY\a12/16/2013 01:25
+(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\aFUREJBBQ PBZZBAF  **1-N-1**\a125 SBEG UVYY EQ NCG 5N\aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/15/2013 15:01
+(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***19-P-4***\a86 INYYRL IVRJ YA \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/15/2013 05:15
+(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\aYNAQTEROR 17-O-1\a35 ZRRGVATUBHFR GRE \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 08:57
+(Vzp Fbyhgvbaf Cntr) NYNEZ - ZRQVPNY\aOBAAN, NYZN **32-O-2**\a19 GERNQJRYY NIR \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 08:31
+(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***28-P-3***\a28 EVIREIVRJ PG \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 00:03
+(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG JVGU VAWHEVRF\a29-O-1\a147 FRPBAQ UVYY EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/15/2013 09:51
+(Vzp Fbyhgvbaf Cntr) PURPX BA CREFBA'F JRYSNER\aJNYZNEG\a164 QNAOHEL EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU 31-Q-3\a12/14/2013 12:02
+(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG JVGU VAWHEVRF\aOVT L SBBQF VAP ***29-O-1***\a3 XRAG EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/18/2013 08:08
+(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG **29-O-1**\a280 QNAOHEL EQ NAQ 1 FGVYY EVIRE QE\aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/19/2013 08:55
+
+*/
+
+
+choyvp pynff PGYvgpusvryqPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp PGYvgpusvryqPbhaglOCnefreGrfg() {
+    frgCnefre(arj PGYvgpusvryqPbhaglOCnefre(), "YVGPUSVRYQ PBHAGL", "PG");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+    qbGrfg("G1",
+        "(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***29-O-4***\a47 PUREAVFXR EQ \aARJ ZVYSBEQ\aQvfgevpg: AIY\a12/16/2013 01:25",
+        "PNYY:NZOHYNAPR PNYY",
+        "PBQR:29-O-4",
+        "NQQE:47 PUREAVFXR EQ",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:AIY",
+        "QNGR:12/16/2013",
+        "GVZR:01:25");
+
+    qbGrfg("G2",
+        "(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a" +
+        "FUREJBBQ PBZZBAF  **1-N-1**\a" +
+        "125 SBEG UVYY EQ NCG 5N\a" +
+        "ARJ ZVYSBEQ\a" +
+        "Qvfgevpg: JJU\a" +
+        "12/15/2013 15:01",
+
+        "PNYY:NZOHYNAPR PNYY",
+        "ANZR:FUREJBBQ PBZZBAF",
+        "PBQR:1-N-1",
+        "NQQE:125 SBEG UVYY EQ",
+        "NCG:5N",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/15/2013",
+        "GVZR:15:01");
+
+    qbGrfg("G3",
+        "(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***19-P-4***\a86 INYYRL IVRJ YA \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/15/2013 05:15",
+        "PNYY:NZOHYNAPR PNYY",
+        "PBQR:19-P-4",
+        "NQQE:86 INYYRL IVRJ YA",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/15/2013",
+        "GVZR:05:15");
+
+    qbGrfg("G4",
+        "(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\aYNAQTEROR 17-O-1\a35 ZRRGVATUBHFR GRE \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 08:57",
+        "PNYY:NZOHYNAPR PNYY",
+        "ANZR:YNAQTEROR",
+        "PBQR:17-O-1",
+        "NQQE:35 ZRRGVATUBHFR GRE",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/14/2013",
+        "GVZR:08:57");
+
+    qbGrfg("G5",
+        "(Vzp Fbyhgvbaf Cntr) NYNEZ - ZRQVPNY\aOBAAN, NYZN **32-O-2**\a19 GERNQJRYY NIR \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 08:31",
+        "PNYY:NYNEZ - ZRQVPNY",
+        "ANZR:OBAAN, NYZN",
+        "PBQR:32-O-2",
+        "NQQE:19 GERNQJRYY NIR",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/14/2013",
+        "GVZR:08:31");
+
+    qbGrfg("G6",
+        "(Vzp Fbyhgvbaf Cntr) NZOHYNAPR PNYY\a***28-P-3***\a28 EVIREIVRJ PG \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/14/2013 00:03",
+        "PNYY:NZOHYNAPR PNYY",
+        "PBQR:28-P-3",
+        "NQQE:28 EVIREIVRJ PG",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/14/2013",
+        "GVZR:00:03");
+
+    qbGrfg("G7",
+        "(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG JVGU VAWHEVRF\a29-O-1\a147 FRPBAQ UVYY EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/15/2013 09:51",
+        "PNYY:ZI NPPVQRAG JVGU VAWHEVRF",
+        "PBQR:29-O-1",
+        "NQQE:147 FRPBAQ UVYY EQ",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/15/2013",
+        "GVZR:09:51");
+
+    qbGrfg("G8",
+        "(Vzp Fbyhgvbaf Cntr) PURPX BA CREFBA'F JRYSNER\aJNYZNEG\a164 QNAOHEL EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU 31-Q-3\a12/14/2013 12:02",
+        "PNYY:PURPX BA CREFBA'F JRYSNER",
+        "ANZR:JNYZNEG",
+        "NQQE:164 QNAOHEL EQ",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "PBQR:31-Q-3",
+        "QNGR:12/14/2013",
+        "GVZR:12:02");
+
+    qbGrfg("G9",
+        "(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG JVGU VAWHEVRF\aOVT L SBBQF VAP ***29-O-1***\a3 XRAG EQ \aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/18/2013 08:08",
+        "PNYY:ZI NPPVQRAG JVGU VAWHEVRF",
+        "CYNPR:OVT L SBBQF VAP",
+        "PBQR:29-O-1",
+        "NQQE:3 XRAG EQ",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/18/2013",
+        "GVZR:08:08");
+
+    qbGrfg("G10",
+        "(Vzp Fbyhgvbaf Cntr) ZI NPPVQRAG **29-O-1**\a280 QNAOHEL EQ NAQ 1 FGVYY EVIRE QE\aARJ ZVYSBEQ\aQvfgevpg: JJU\a12/19/2013 08:55",
+        "PNYY:ZI NPPVQRAG",
+        "PBQR:29-O-1",
+        "NQQE:280 QNAOHEL EQ",
+        "K:1 FGVYY EVIRE QE",
+        "PVGL:ARJ ZVYSBEQ",
+        "HAVG:JJU",
+        "QNGR:12/19/2013",
+        "GVZR:08:55");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj PGYvgpusvryqPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
