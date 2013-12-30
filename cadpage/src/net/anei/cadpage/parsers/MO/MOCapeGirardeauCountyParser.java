d1cf324c223090abee06d036c5877ada821b44d9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-12-30 19:58:33 +0000

    New Cape Girardeau County, MO (B) location

--- cadpage/src/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyParser.java
@@ -1,38 +1,11 @@
 package net.anei.cadpage.parsers.MO;
 
-import java.util.regex.Pattern;
-
-import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA12Parser;
+import net.anei.cadpage.parsers.GroupBestParser;
 
 
-public class MOCapeGirardeauCountyParser extends DispatchA12Parser {
-  
+public class MOCapeGirardeauCountyParser extends GroupBestParser {
+
   public MOCapeGirardeauCountyParser() {
-    super("CAPE GIRARDEAU COUNTY", "MO");
+    super(new MOCapeGirardeauCountyAParser(), new MOCapeGirardeauCountyBParser());
   }
-  
-  @Override
-  public String getFilter() {
-    return "CAD@CityofCape.org";
-  }
-  
-  @Override
-  protected boolean parseMsg(String body, Data data) {
-    if (!super.parseMsg(body, data)) return false;
-    if (data.strPlace.equals("Resident")) data.strPlace = "";
-    else if (PLACE_NAME_PTN.matcher(data.strPlace).matches()) {
-      data.strName = data.strPlace;
-      data.strPlace = "";
-    }
-    return true;
-  }
-    
-  @Override
-  public String getProgram() {
-    return super.getProgram().replace("PLACE", "PLACE NAME");
-  }
-    
-    
-  private static final Pattern PLACE_NAME_PTN = Pattern.compile("[A-Z]+, +[A-Z]+", Pattern.CASE_INSENSITIVE);
 }
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOCapeGirardeauCountyBParserTest.java
@@ -0,0 +1,56 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZB;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+
+/*
+Pncr Tveneqrnh Pbhagl, ZB
+
+Pbagnpg: Frna Zvgpuryy <frnazvgpuryy3541@lnubb.pbz>
+Fraqre: wcq@pvglbspncrtveneqrnh.bet
+
+SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:1032 YRVTU NAA PG [@Sevrqevpx, Ovyy,
+SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:318 TERRAFSREEL EQ,\a
+SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:406 RNFG YA [3,\a
+SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:5500 BYQ PNCR EQ,\a
+
+*/
+
+choyvp pynff ZBPncrTveneqrnhPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ZBPncrTveneqrnhPbhaglOCnefreGrfg() {
+    frgCnefre(arj ZBPncrTveneqrnhPbhaglOCnefre(), "PNCR TVENEQRNH PBHAGL", "ZB");
+  }
+
+  @Grfg
+  choyvp ibvq grfgFrnaZvgpuryy() {
+
+    qbGrfg("G1",
+        "SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:1032 YRVTU NAA PG [@Sevrqevpx, Ovyy,",
+        "PNYY:SVER ZRQVPNY NFFVFG",
+        "NQQE:1032 YRVTU NAA PG");
+
+    qbGrfg("G2",
+        "SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:318 TERRAFSREEL EQ,\a",
+        "PNYY:SVER ZRQVPNY NFFVFG",
+        "NQQE:318 TERRAFSREEL EQ");
+
+    qbGrfg("G3",
+        "SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:406 RNFG YA [3,\a",
+        "PNYY:SVER ZRQVPNY NFFVFG",
+        "NQQE:406 RNFG YA");
+
+    qbGrfg("G4",
+        "SQGLCR:SVER ZRQVPNY NFFVFG, YBPNGVBA:5500 BYQ PNCR EQ,\a",
+        "PNYY:SVER ZRQVPNY NFFVFG",
+        "NQQE:5500 BYQ PNCR EQ");
+    
+  }
+  
+   
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ZBPncrTveneqrnhPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
