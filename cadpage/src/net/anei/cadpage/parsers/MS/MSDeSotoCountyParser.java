28ece18b949fa59a13f8396e8c577d2952b43945
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-01-05 22:37:16 +0000

    Checking in DeSoto County, MS

---
+++ cadpage/src/net/anei/cadpage/parsers/MS/MSDeSotoCountyParser.java
@@ -0,0 +1,42 @@
+package net.anei.cadpage.parsers.MS;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
+
+public class MSDeSotoCountyParser extends SmartAddressParser {
+
+  public MSDeSotoCountyParser() {
+    super("DESOTO COUNTY", "MS");
+    
+    setFieldList("CALL ADDR APT X NAME");
+  }
+  
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    //Disqualifiers
+    if (subject.length() == 0) return false;
+    if (!body.startsWith("911CENTER:")) return false;
+    
+    //CALL and remove 911CENTER: from body
+    data.strCall = subject;
+    body = body.substring(10);
+    
+    //ADDR
+    parseAddress(StartType.START_ADDR, body, data);
+    if (getStatus() == 0) return false;
+    body = getLeft();
+    
+    //X (CV is a street abbreviation for cove)
+    Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS, body);
+    if (res.getStatus() > 0) {
+      res.getData(data);
+      body = res.getLeft();
+    }
+
+    //NAME
+    data.strName = cleanWirelessCarrier(body);
+
+    return true;
+  }
+
+}
