82aa6adf77738a562d916c9c66340e9e07392044
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-01-18 08:38:58 +0000

    Checking in Limstone County, AL

--- cadpage/src/net/anei/cadpage/parsers/IA/IAWarrenCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/IA/IAWarrenCountyParser.java
@@ -1,58 +1,29 @@
 package net.anei.cadpage.parsers.IA;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA38Parser;
 
 
 
-public class IAWarrenCountyParser extends FieldProgramParser {
+public class IAWarrenCountyParser extends DispatchA38Parser {
   
   public IAWarrenCountyParser() {
-    super("WARREN COUNTY", "IA",
-           "CFS#:ID! CallType:CALL! Address:ADDR!");
+    super("WARREN COUNTY", "IA");
   }
   
   @Override
   public String getFilter() {
     return "messaging@iamresponding.com";
   }
-
+  
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    
+  public boolean parseMsg(String subject, String field, Data data) {
     data.strSource = subject;
-    return super.parseMsg(body, data);
+    return super.parseMsg(field, data);
   }
   
   @Override
   public String getProgram() {
     return "SRC " + super.getProgram();
-  }
-  
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      Parser p = new Parser(field);
-      String city = p.getLastOptional(',');
-      if (city.length() == 2) {
-        data.strState = city;
-        city = p.getLastOptional(',');
-      }
-      data.strCity = city;
-      data.strApt = p.getLastOptional("Apt:");
-      parseAddress(p.get(), data);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "ADDR APT CITY ST";
-    }
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{4}-\\d{5}", true);
-    if (name.equals("ADDR")) return new MyAddressField();
-    return super.getField(name);
   }
 }
