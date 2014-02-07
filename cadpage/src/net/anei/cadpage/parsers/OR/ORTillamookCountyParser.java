0a698ae62afb99adb679ca50c06a1fe530535211
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-02-07 09:10:40 +0000

    Checking in Tillamook County, OR

---
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORTillamookCountyParser.java
@@ -0,0 +1,88 @@
+package net.anei.cadpage.parsers.OR;
+
+import java.util.Properties;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
+
+
+public class ORTillamookCountyParser extends SmartAddressParser {
+  
+  public ORTillamookCountyParser() {
+    super(CITY_LIST, "TILLAMOOK COUNTY", "OR");
+    setFieldList("CALL ADDR APT PLACE CITY INFO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "5038122399@vzwpix.com";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    body = body.replace('\n', ' ');
+    parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_IGNORE_AT, body, data);
+    if (data.strCity.length() == 0) return false;
+    data.strSupp = getLeft();
+    
+    String city = CITY_PLACE_TABLE.getProperty(data.strCity.toUpperCase());
+    if (city != null) {
+      data.strPlace = data.strCity;
+      data.strCity = city;
+    }
+    return getStatus() > 0;
+  }
+  
+  private static final String[] CITY_LIST = new String[]{
+
+    // Cities
+    "BAY CITY",
+    "GARIBALDI",
+    "MANZANITA",
+    "NEHALEM",
+    "ROCKAWAY BEACH",
+    "TILLAMOOK",
+    "WHEELER",
+
+    // Unincorporated communities and CDPs
+    "BARVIEW",
+    "BAYOCEAN",
+    "BAYSIDE GARDENS",
+    "BEAVER",
+    "BRIGHTON",
+    "CAPE MEARES",
+    "CASTLE ROCK",
+    "CLOVERDALE",
+    "DOLPH",
+    "FAIRVIEW",
+    "FOSS",
+    "HEBO",
+    "HEMLOCK",
+    "IDAVILLE",
+    "IDIOTVILLE",
+    "JORDAN CREEK",
+    "LEES CAMP",
+    "MANHATTAN BEACH",
+    "MEDA",
+    "MOHLER",
+    "NEAHKAHNIE BEACH",
+    "NEDONNA BEACH",
+    "NESKOWIN",
+    "NETARTS",
+    "OCEANSIDE",
+    "ORETOWN",
+    "PACIFIC CITY",
+    "PLEASANT VALLEY",
+    "SANDLAKE",
+    "TIERRA DEL MAR",
+    "TWIN ROCKS",
+    "WATSECO",
+    "WOODS"
+   
+  };
+  
+  private static final Properties CITY_PLACE_TABLE = buildCodeTable(new String[]{
+      "SANDLAKE",         "Cloverdale"
+  });
+ 
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORTillamookCountyParserTest.java
@@ -0,0 +1,83 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BE;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*    
+Gvyynzbbx Pbhagl
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Arfghppn Sver Qvfg.
+Ybpngvba: Pybireqnyr, BE, Havgrq Fgngrf
+Fraqre: 5038122399@imjcvk.pbz
+
+ZIN UVTUJNL 22 Urob  YBFG PNYYRE  BAR YNAR OYBPXRQ  OYRRQVAT SEBZ UNAQ  FVATYR IRU
+Haxabja 18900 PNCR YBBXBHG EQ Fnaqynxr  QBRFAG NCCRNE GB OR FCNEXVAT  UNIR GB QEVIR BIRE VG  YNLVAT BA GUR TEBHAQ  Pnyy #: 1454 - R911 Vasb - Pynff bs Freivpr: ERFQ  CBJRE YVAR NPEBFF EBNQ  R911 Vasb - Pynff bs Freivpr: ERFQ
+Zrqvpny 12555 BYQ JBBQF EQ Pybireqnyr  Ceboyrz: PURFG CNVA.  Cngvrag Vasb: 1 cngvrag(f); 91 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  91 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Purfg Cnva (Aba-Genhzngvp).  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--
+Zrqvpny 49664 FHES EQ Arfxbjva  76 lrne byq, Srznyr, Pbafpvbhf, Oernguvat.  Ceboyrz: CNVA VA EVTUG FUBHYQRE NAQ NEZ, PURFG CN.  Cngvrag Vasb: 1 cngvrag(f); 76 Lrnef byq; Srznyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--  TNGRQ FBHGU ORNPU QRIRYBCZRAG  R911 Vasb - Pynff bs Freivpr: JCU1
+Sver 7300 FBHGU CENVEVR EQ Gvyynzbbx  ERD PBIRENTR NG 71, GRAQRE SEBZ ARFGHPPN  7150 ERD 2AQ NYNEZ  71 PBZZNAQ NQI FJVGPU GB GNP 7 SBE BCRENGVBAF  GBARQ @ 2115  Pnyy #: 46487 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46486 - R911 Vasb - Pynff bs Freivpr: JCU1  224 NQI  Pnyy #: 46485 - 2 OVT SHRY GNAXF EVTUG ARKG GB GUR SVER  Pnyy #: 46485 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46484 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46478 - 6.Ab bar vf genccrq vafvqr gur fgehpgher.  7.Ab bar vf ercbegrq gb or vawherq.  Pnyy #: 46478 - CebDN Sver Xrl Dhrfgvbaf unir orra pbzcyrgrq; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fznyy ABA-QJRYYVAT ohvyqvat/fgehpgher (furq, qrgnpurq tnentr).  1.Gur pnyyre vf ba fprar (1fg cnegl).  2.Obgu fzbxr naq synzrf ner ivfvoyr.  3.Gur vapvqrag vaibyirf n fznyy ABA-QJRYYVAT fgehpgher: ONEA SHYY BS FGENL=J  4.N fvatyr-yriry fgehpgher vf vaibyirq.  5.Gur\a pnyyre unf abg vaqvpngrq gung gurer ner genccrq crefba(f).-Pbzzragf: ONEA SHYY BS FGENL=J-  Pnyy #: 46478 - CebDN Sver Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fgehpgher Sver.  .Pnyyre Fgngrzrag: ONEA BA SVER.  Pnyy #: 46478 - CebDN Sver Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 69--  qvrfry gnaxf arkg gb gur onea  Pnyy #: 46482 - R911 Vasb - Pynff bs Freivpr: JCU1  R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46480 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46479 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46478 - fjvsyrf unlonea vf ba sver  Pnyy #: 46478 - R911 Vasb - Pynff bs Freivpr: ERFQ
+Zrqvpny 17120 ZG UROB EQ Urob  Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Abg nyreg.  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  1.Ur vf abg pbzcyrgryl nyreg (abg erfcbaqvat nccebcevngryl).  CebDN Zrqvpny Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 26-Q-01  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 26--  R911 Vasb - Pynff bs Freivpr: ERFQ
+ 
+*/
+
+choyvp pynff BEGvyynzbbxPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BEGvyynzbbxPbhaglCnefreGrfg() {
+    frgCnefre(arj BEGvyynzbbxPbhaglCnefre(), "GVYYNZBBX PBHAGL", "BE");
+  }
+
+  @Grfg
+  choyvp ibvq grfgArfghppnSverQvfg() {
+
+    qbGrfg("G1",
+        "ZIN UVTUJNL 22 Urob  YBFG PNYYRE  BAR YNAR OYBPXRQ  OYRRQVAT SEBZ UNAQ  FVATYR IRU",
+        "PNYY:ZIN",
+        "NQQE:UVTUJNL 22",
+        "PVGL:Urob",
+        "VASB:YBFG PNYYRE  BAR YNAR OYBPXRQ  OYRRQVAT SEBZ UNAQ  FVATYR IRU");
+
+    qbGrfg("G2",
+        "Haxabja 18900 PNCR YBBXBHG EQ Fnaqynxr  QBRFAG NCCRNE GB OR FCNEXVAT  UNIR GB QEVIR BIRE VG  YNLVAT BA GUR TEBHAQ  Pnyy #: 1454 - R911 Vasb - Pynff bs Freivpr: ERFQ  CBJRE YVAR NPEBFF EBNQ  R911 Vasb - Pynff bs Freivpr: ERFQ",
+        "PNYY:Haxabja",
+        "NQQE:18900 PNCR YBBXBHG EQ",  // Sbhaq va Pybireqnyr
+        "CYNPR:Fnaqynxr",
+        "PVGL:Pybireqnyr",
+        "VASB:QBRFAG NCCRNE GB OR FCNEXVAT  UNIR GB QEVIR BIRE VG  YNLVAT BA GUR TEBHAQ  Pnyy #: 1454 - R911 Vasb - Pynff bs Freivpr: ERFQ  CBJRE YVAR NPEBFF EBNQ  R911 Vasb - Pynff bs Freivpr: ERFQ");
+
+    qbGrfg("G3",
+        "Zrqvpny 12555 BYQ JBBQF EQ Pybireqnyr  Ceboyrz: PURFG CNVA.  Cngvrag Vasb: 1 cngvrag(f); 91 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  91 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Purfg Cnva (Aba-Genhzngvp).  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--",
+        "PNYY:Zrqvpny",
+        "NQQE:12555 BYQ JBBQF EQ",
+        "PVGL:Pybireqnyr",
+        "VASB:Ceboyrz: PURFG CNVA.  Cngvrag Vasb: 1 cngvrag(f); 91 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  91 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Purfg Cnva (Aba-Genhzngvp).  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--");
+
+    qbGrfg("G4",
+        "Zrqvpny 49664 FHES EQ Arfxbjva  76 lrne byq, Srznyr, Pbafpvbhf, Oernguvat.  Ceboyrz: CNVA VA EVTUG FUBHYQRE NAQ NEZ, PURFG CN.  Cngvrag Vasb: 1 cngvrag(f); 76 Lrnef byq; Srznyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--  TNGRQ FBHGU ORNPU QRIRYBCZRAG  R911 Vasb - Pynff bs Freivpr: JCU1",
+        "PNYY:Zrqvpny",
+        "NQQE:49664 FHES EQ",
+        "PVGL:Arfxbjva",
+        "VASB:76 lrne byq, Srznyr, Pbafpvbhf, Oernguvat.  Ceboyrz: CNVA VA EVTUG FUBHYQRE NAQ NEZ, PURFG CN.  Cngvrag Vasb: 1 cngvrag(f); 76 Lrnef byq; Srznyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 10--  TNGRQ FBHGU ORNPU QRIRYBCZRAG  R911 Vasb - Pynff bs Freivpr: JCU1");
+
+    qbGrfg("G5",
+        "Sver 7300 FBHGU CENVEVR EQ Gvyynzbbx  ERD PBIRENTR NG 71, GRAQRE SEBZ ARFGHPPN  7150 ERD 2AQ NYNEZ  71 PBZZNAQ NQI FJVGPU GB GNP 7 SBE BCRENGVBAF  GBARQ @ 2115  Pnyy #: 46487 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46486 - R911 Vasb - Pynff bs Freivpr: JCU1  224 NQI  Pnyy #: 46485 - 2 OVT SHRY GNAXF EVTUG ARKG GB GUR SVER  Pnyy #: 46485 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46484 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46478 - 6.Ab bar vf genccrq vafvqr gur fgehpgher.  7.Ab bar vf ercbegrq gb or vawherq.  Pnyy #: 46478 - CebDN Sver Xrl Dhrfgvbaf unir orra pbzcyrgrq; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fznyy ABA-QJRYYVAT ohvyqvat/fgehpgher (furq, qrgnpurq tnentr).  1.Gur pnyyre vf ba fprar (1fg cnegl).  2.Obgu fzbxr naq synzrf ner ivfvoyr.  3.Gur vapvqrag vaibyirf n fznyy ABA-QJRYYVAT fgehpgher: ONEA SHYY BS FGENL=J  4.N fvatyr-yriry fgehpgher vf vaibyirq.  5.Gur\a" +
+        " pnyyre unf abg vaqvpngrq gung gurer ner genccrq crefba(f).-Pbzzragf: ONEA SHYY BS FGENL=J-  Pnyy #: 46478 - CebDN Sver Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fgehpgher Sver.  .Pnyyre Fgngrzrag: ONEA BA SVER.  Pnyy #: 46478 - CebDN Sver Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 69--  qvrfry gnaxf arkg gb gur onea  Pnyy #: 46482 - R911 Vasb - Pynff bs Freivpr: JCU1  R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46480 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46479 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46478 - fjvsyrf unlonea vf ba sver  Pnyy #: 46478 - R911 Vasb - Pynff bs Freivpr: ERFQ",
+
+        "PNYY:Sver",
+        "NQQE:7300 FBHGU CENVEVR EQ",
+        "PVGL:Gvyynzbbx",
+        "VASB:ERD PBIRENTR NG 71, GRAQRE SEBZ ARFGHPPN  7150 ERD 2AQ NYNEZ  71 PBZZNAQ NQI FJVGPU GB GNP 7 SBE BCRENGVBAF  GBARQ @ 2115  Pnyy #: 46487 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46486 - R911 Vasb - Pynff bs Freivpr: JCU1  224 NQI  Pnyy #: 46485 - 2 OVT SHRY GNAXF EVTUG ARKG GB GUR SVER  Pnyy #: 46485 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46484 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46478 - 6.Ab bar vf genccrq vafvqr gur fgehpgher.  7.Ab bar vf ercbegrq gb or vawherq.  Pnyy #: 46478 - CebDN Sver Xrl Dhrfgvbaf unir orra pbzcyrgrq; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fznyy ABA-QJRYYVAT ohvyqvat/fgehpgher (furq, qrgnpurq tnentr).  1.Gur pnyyre vf ba fprar (1fg cnegl).  2.Obgu fzbxr naq synzrf ner ivfvoyr.  3.Gur vapvqrag vaibyirf n fznyy ABA-QJRYYVAT fgehpgher: ONEA SHYY BS FGENL=J  4.N fvatyr-yriry fgehpgher vf vaibyirq.  5.Gur  pnyyre unf abg vaqvpngrq gung gurer ner genccrq crefba(f).-Pbzzragf: ONEA SHYY BS FGENL=J-  Pnyy #: 46478 - CebDN Sver Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 69-Q-09  Pnyy #: 46478 - Fgehpgher Sver.  .Pnyyre Fgngrzrag: ONEA BA SVER.  Pnyy #: 46478 - CebDN Sver Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 69--  qvrfry gnaxf arkg gb gur onea  Pnyy #: 46482 - R911 Vasb - Pynff bs Freivpr: JCU1  R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46480 - R911 Vasb - Pynff bs Freivpr: ERFQ  Pnyy #: 46479 - R911 Vasb - Pynff bs Freivpr: JCU1  Pnyy #: 46478 - fjvsyrf unlonea vf ba sver  Pnyy #: 46478 - R911 Vasb - Pynff bs Freivpr: ERFQ");
+
+    qbGrfg("G6",
+        "Zrqvpny 17120 ZG UROB EQ Urob  Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Abg nyreg.  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  1.Ur vf abg pbzcyrgryl nyreg (abg erfcbaqvat nccebcevngryl).  CebDN Zrqvpny Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 26-Q-01  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 26--  R911 Vasb - Pynff bs Freivpr: ERFQ",
+        "PNYY:Zrqvpny",
+        "NQQE:17120 ZG UROB EQ",
+        "PVGL:Urob",
+        "VASB:Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Abg nyreg.  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  1.Ur vf abg pbzcyrgryl nyreg (abg erfcbaqvat nccebcevngryl).  CebDN Zrqvpny Qvfcngpu Zrffntr Frag; Qvfcngpu Pbqr: 26-Q-01  53 lrne byq, Znyr, Pbafpvbhf, Oernguvat.  Fvpx Crefba (Fcrpvsvp Qvntabfvf).  Pnyyre Fgngrzrag: IBZZVGGVAT NAQ QVNEURN.  Ceboyrz: IBZZVGGVAT NAQ QVNEURN.  Cngvrag Vasb: 1 cngvrag(f); 53 Lrnef byq; Znyr; Pbafpvbhf: Lrf; Oernguvat: Lrf;  CebDN Zrqvpny Pnfr Ragel unf orra pbzcyrgrq; Qvfcngpu Pbqr: 26--  R911 Vasb - Pynff bs Freivpr: ERFQ");
+
+  }
+ 
+  choyvp fgngvp ibvq znva(Fgevat netf[]) {
+    arj BEGvyynzbbxPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
