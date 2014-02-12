f1bb91c2b15665661f66f52d19eb0b3e2b626f69
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-02-12 18:38:57 +0000

    Added call code table to Maricopa County, AZ

--- cadpage/src/net/anei/cadpage/parsers/AZ/AZMaricopaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AZ/AZMaricopaCountyParser.java
@@ -16,7 +16,7 @@ public class AZMaricopaCountyParser extends FieldProgramParser {
 
   public AZMaricopaCountyParser() {
     super(CITY_CODES, "MARICOPA COUNTY", "AZ",
-           "( Location:ADDR/S! EVENT:ID! TYPE_CODE:CALL! TIME:TIME! CALLER_NAME:NAME! CALLER_ADDR:SKIP! | " +
+           "( Location:ADDR/S! EVENT:ID! TYPE_CODE:CODE! TIME:TIME! CALLER_NAME:NAME! CALLER_ADDR:SKIP! | " +
            "DATETIME Event_num:ID! Event_type:CODE! Location:ADDR/S! Units:UNIT! EMPTY INFO ) Comments:INFO SPECIAL_ADDRESS_COMMENT:INFO? Grid:MAP Plat:MAP Block:BLOCK INFO+");
   }
   
@@ -44,6 +44,7 @@ public class AZMaricopaCountyParser extends FieldProgramParser {
   public Field getField(String name) {
     if (name.equals("DATETIME")) return new DateTimeField("\\d\\d/\\d\\d/\\d{4} \\d\\d:\\d\\d:\\d\\d", true);
     if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("CODE")) return new MyCodeField();
     if (name.equals("MAP")) return new MyMapField();
     if (name.equals("BLOCK")) return new MyBlockField();
     return super.getField(name);
@@ -80,6 +81,22 @@ public class AZMaricopaCountyParser extends FieldProgramParser {
       return super.getFieldNames() + " APT PLACE X";
     }
   }
+  
+  private static final Pattern CODE_PTN = Pattern.compile("\\d+(.*)");
+  private class MyCodeField extends CodeField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strCode = field;
+      Matcher match = CODE_PTN.matcher(field);
+      if (match.matches()) field = match.group(1);
+      data.strCall = convertCodes(field, CALL_CODES);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "CODE CALL";
+    }
+  }
 
   private class MyMapField extends MapField {
     @Override
@@ -105,6 +122,51 @@ public class AZMaricopaCountyParser extends FieldProgramParser {
       return "MAP INFO";
     }
   }
+
+  private static final Properties CALL_CODES = buildCodeTable(new String[]{
+      "AAID",       "AUTOMATIC AID",
+      "AIRMED",     "INBOUND FLIGHT EMERGENCY",
+      "ALLCALL",    "ENCODE ALL MRDC STATIONS",
+      "ALT1",       "AIRCRAFT STANDBY REQUEST",
+      "ALT2",       "AIRCRAFT TROUBLE INDICATION",
+      "ALT3",       "AIRCRAFT DOWN",
+      "ALT3D",      "ALT3 DRILL",
+      "AZMA",       "ARIZONA MUTUAL AID",
+      "BEES",       "BEE SWARM",
+      "BRUSH",      "WILDLAND FIRE",
+      "CARF",       "CAR FIRE",
+      "CHAPLAIN",   "CHAPLAIN REQ",
+      "CODE",       "NONBREATHER",
+      "DROWN",      "DROWNING",
+      "FIELD",      "FIELD FIRE",
+      "HAZ",        "MINOR HAZ INCIDENT",
+      "HAZMED",     "HAZARDOUS INCIDENT WITH VICTIMS",
+      "HZMAT",      "MAJOR HAZARDOUS MATERIALS INCIDENT",
+      "LABOR",      "CHILDBIRTH",
+      "LR",         "LIGHT RAIL INCIDENT",
+      "MAID",       "MUTUAL AID REQUEST",
+      "MAIDCT",     "MUTUAL AID REQUEST COUNTRY THUNDER",
+      "MED",        "MEDICAL INCIDENT",
+      "MED2",       "MEDICAL INCIDENT CODE 2 RESPONSE",
+      "MEDRET",     "MED RETRIEVAL ACTIVATION",
+      "MOVEUP",     "DEPLOYMENT COVER ASSIGNMENT",
+      "NBCT",       "NUKE/BIO/CHEM/TERROR",
+      "SERVICE",    "SERVICE CALL",
+      "SNAKE",      "SNAKE REMOVAL",
+      "SPEC",       "SPECIAL DUTY",
+      "SPEC2",      "SPECIAL DUTY CODE 2 RESPONSE",
+      "SPECG",      "SPECIAL DUTY GAS LINE BREAK",
+      "STROKE",     "STROKE",
+      "STRU",       "STRUCTURE FIRE",
+      "TASER",      "PERSON TASED BY PD",
+      "TLOOPS",     "TLO OPS",
+      "TRASH",      "TRASH FIRE",
+      "TRT",        "TECHNICIAL RESCUE",
+      "VARMINT",    "VARMINT REMOVAL/RELOCATION",
+      "WORK",       "WORKING FIRE",
+      "ZMED",       "ZULU MED",
+      "ZMED2",      "ZULU MED2"
+  }); 
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
   "SCOT", "SCOTTSDALE",
