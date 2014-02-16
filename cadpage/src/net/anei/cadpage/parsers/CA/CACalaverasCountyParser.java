a5a78c42677ba221cf6b724b56166b3dca8de124
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-02-16 20:03:38 +0000

    Checking in Tuolumne County, CA

--- cadpage/src/net/anei/cadpage/parsers/CA/CACalaverasCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/CA/CACalaverasCountyParser.java
@@ -13,10 +13,14 @@ import net.anei.cadpage.parsers.MsgParser;
 public class CACalaverasCountyParser extends MsgParser {
   
   private static final Pattern MASTER = 
-      Pattern.compile("Inc# (\\d+):([^:]+):(?:([^@]+)@)?(.+?) , *([A-Z_]+) *:Map +([^:]*):(?: :)? LAT/LONG (X: [-+]?\\d+ \\d+\\.\\d+ +Y: [-+]?\\d+ \\d+\\.\\d+): ([^:]*)(?::([^:]*))?(?::.*)?");
+      Pattern.compile("Inc# (\\d+):([^:]+):(?:([^@]+)@)?(.+?) , *([A-Z_]+) *(?:\\(([^]]*?)\\) *)?:Map +([^:]*):(?: :)? LAT/LONG (X: [-+]?\\d+ \\d+\\.\\d+ +Y: [-+]?\\d+ \\d+\\.\\d+): ([^:]*)(?::([^:]*))?(?::.*)?");
   
   public CACalaverasCountyParser() {
-    super("CALAVERAS COUNTY", "CA");
+    this("CALAVERAS COUNTY");
+  }
+  
+  public CACalaverasCountyParser(String defCity) {
+    super(defCity, "CA");
     setFieldList("ID CALL PLACE ADDR APT CITY MAP GPS INFO UNIT");
   }
   
@@ -25,6 +29,11 @@ public class CACalaverasCountyParser extends MsgParser {
     return "tcucad@FIRE.CA.GOV";
   }
   
+  @Override
+  public String getAliasCode() {
+    return "CACalaverasCounty";
+  }
+
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA | MAP_FLG_PREFER_GPS;
@@ -40,10 +49,11 @@ public class CACalaverasCountyParser extends MsgParser {
     data.strPlace = getOptGroup(match.group(3));
     parseAddress(match.group(4).trim(), data);
     data.strCity = match.group(5).replace('_', ' ').trim();
-    data.strMap = match.group(6).trim();
-    setGPSLoc(match.group(7), data);
-    data.strSupp = match.group(8).trim();
-    data.strUnit = getOptGroup(match.group(9));
+    data.strPlace = append(data.strPlace, " - ", getOptGroup(match.group(6)));
+    data.strMap = match.group(7).trim();
+    setGPSLoc(match.group(8), data);
+    data.strSupp = match.group(9).trim();
+    data.strUnit = getOptGroup(match.group(10));
     
     return true;
   }
