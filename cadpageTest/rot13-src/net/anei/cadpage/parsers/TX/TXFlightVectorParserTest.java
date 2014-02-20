11e3bc77ab30613eca88ce003cc2df53dbf63795
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-02-20 02:53:11 +0000

    Checking in Flight Vector, TX instructions

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/TX/TXFlightVectorParserTest.java
@@ -0,0 +1,146 @@
+cnpxntr arg.narv.pnqcntr.cnefref.GK;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Syvtug Irpgbe, GK
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Fna Nagbavb NveYvsr
+Ybpngvba: Fna Nagbavb, GK, Havgrq Fgngrf
+Fraqre: "Syvtug Irpgbe" <cntre@syvtugirpgbe.pbz>
+
+(14-0811 NveYVSR 5 Fprar YNHAPU 019Z 26.32az [29...) 14-0811 NveYVSR 5 Fprar YNHAPU 019Z 26.32az [29 36.86A 099 31.61J] Hgbcvn Zrqvpny Pyvavp 19Sro2014@17:13
+(14-0811 NveYVSR 5 FGNAQOL BA FPRAR 019Z 26.32az...) 14-0811 NveYVSR 5 FGNAQOL BA FPRAR 019Z 26.32az [29 36.86A 099 31.61J] Hgbcvn Zrqvpny Pyvavp Fprar 19Sro2014@17:12
+(14-0810 NveYVSR 2 149Z 21.92az 23131 Juvgr Uvpx...) 14-0810 NveYVSR 2 149Z 21.92az 23131 Juvgr Uvpxbel, Ryzraqbes, GK [29 10.73A 098 23.83J] 19Sro2014@16:41
+(NveYVSR 2 14-0809 Ynhapu 29.21az 105Z [29 20.02...) NveYVSR 2 14-0809 Ynhapu 29.21az 105Z [29 20.02A 098 03.37J] Punzcvbaf Obhyrineq, Yn Ireavn, GK Fprar 19Sro2014@15:52
+(NveYVSR 2 14-0809 105Z 25.82az Yn Ireavn, GK [2...) NveYVSR 2 14-0809 105Z 25.82az Yn Ireavn, GK [29 21.38A 098 06.93J] 19Sro2014@15:34
+(NveYVSR 5 14-0808 285Z 101.41az Ynatgel, Gk [29...) NveYVSR 5 14-0808 285Z 101.41az Ynatgel, Gk [29 48.97A 101 33.56J] 19Sro2014@13:53
+(NveYVSR 2 14-0806 Fprar YNHAPU 262Z 122.97az [2...) NveYVSR 2 14-0806 Fprar YNHAPU 262Z 122.97az [29 22.45A 100 55.63J] QRY EVB VAGY 19Sro2014@13:43
+(NveYVSR 1 14-0807 163Z 79.37az Puevfghf Fcbua O...) NveYVSR 1 14-0807 163Z 79.37az Puevfghf Fcbua Orrivyyr Ubfcvgny [28 24.48A 097 43.95J] 19Sro2014@13:41
+(14-0806 NveYVSR 2 FGNAQOL BA FPRAR 262Z 122.97a...) 14-0806 NveYVSR 2 FGNAQOL BA FPRAR 262Z 122.97az [29 22.45A 100 55.63J] QRY EVB VAGY Fprar 19Sro2014@13:16
+(NveYVSR 3 A407GK BHG BS FREIVPR 19Sro2014@07:35...) NveYVSR 3 A407GK BHG BS FREIVPR 19Sro2014@07:35 Znvagranapr juvyr jrngure vf onq
+(NveYVSR 1 A557HU BHG BS FREIVPR 19Sro2014@05:50...) NveYVSR 1 A557HU BHG BS FREIVPR 19Sro2014@05:50 Znvagranapr BBF QHR GB FPURQHYRQ VAFCRPGVBAF HAGVY 0900 UEF - SYGPBZZ
+(14-0805 NveYVSR 2 FGNAQOL BA FPRAR 325Z 29.61az...) 14-0805 NveYVSR 2 FGNAQOL BA FPRAR 325Z 29.61az [29 56.00A 098 51.81J] 29�56.00'A 098�51.81'J Fprar 19Sro2014@00:49
+(14-0804 NveYVSR 3 FGNAQOL BA FPRAR 109Z 16.49az...) 14-0804 NveYVSR 3 FGNAQOL BA FPRAR 109Z 16.49az [29 56.00A 098 51.81J] 29�56.00'A 098�51.81'J Fprar 19Sro2014@00:48
+(14-0803 Vagreubfcvgny NveYVSR 1 YNHAPU 327Z 70....) 14-0803 Vagreubfcvgny NveYVSR 1 YNHAPU 327Z 70.20az [30 44.57A 098 40.65J] Fpbgg & Juvgr Ubfcvgny - Yynab GB Fg Qnivq'f Abegu Nhfgva Zrqvpny Pragre 18Sro2014@22:10
+(14-0802 Vagreubfcvgny NveYVSR 3 YNHAPU 025Z 48....) 14-0802 Vagreubfcvgny NveYVSR 3 YNHAPU 025Z 48.47az [30 44.57A 098 40.65J] Fpbgg & Juvgr Ubfcvgny - Yynab GB Fg Qnivq'f Abegu Nhfgva Zrqvpny Pragre 18Sro2014@22:09
+(=?vfb-8859-1?D?14-0801_NveYVSR_4_145Z_37.30az_28=O025.02'A_098=O01...?=) 14-0801 NveYVSR 4 145Z 37.30az 28�25.02'A 098�10.50'J [28 25.02A 098 10.50J] 18Sro2014@18:31
+(14-0800 Erybpngvba Syvtug NveYVSR 4 YNHAPU 170Z...) 14-0800 Erybpngvba Syvtug NveYVSR 4 YNHAPU 170Z 32.96az [28 57.55A 098 31.30J] Cyrnfnagba Onfr 18Sro2014@15:37
+(ERGHEA GB FREIVPR 18Sro2014@13:12 NveYVSR 1 A557HU) ERGHEA GB FREIVPR 18Sro2014@13:12 NveYVSR 1 A557HU
+(14-0799 NveYVSR 4 128Z 24.02az sz 775 naq sz 18...) 14-0799 NveYVSR 4 128Z 24.02az sz 775 naq sz 181 syberfivyyr [29 13.79A 098 14.77J] 18Sro2014@13:11
+(14-0798 Erybpngvba Syvtug NveYVSR 4 YNHAPU 325Z...) 14-0798 Erybpngvba Syvtug NveYVSR 4 YNHAPU 325Z 11.61az [29 30.48A 098 34.75J] Havirefvgl Onfr 18Sro2014@11:59
+(ERGHEA GB FREIVPR 18Sro2014@11:35 NveYVSR 4 A556HU) ERGHEA GB FREIVPR 18Sro2014@11:35 NveYVSR 4 A556HU
+(14-0797 NveYVSR 2 FGNAQOL BA FPRAR 262Z 121.11a...) 14-0797 NveYVSR 2 FGNAQOL BA FPRAR 262Z 121.11az [29 22.48A 100 53.49J] Iny Ireqr Ertvbany Zrqvpny Pge Fprar 18Sro2014@10:41
+(14-0796 NveYVSR 3 FGNAQOL BA FPRAR 241Z 99.19az...) 14-0796 NveYVSR 3 FGNAQOL BA FPRAR 241Z 99.19az [29 22.48A 100 53.49J] Iny Ireqr Ertvbany Zrqvpny Pge Fprar 18Sro2014@10:40
+(NveYVSR 4 A556HU UNF ORRA BBF, PBZCYRGVAT EBHGV...) NveYVSR 4 A556HU UNF ORRA BBF, PBZCYRGVAT EBHGVAR VAFCRPGVBAF, NAGVPVCNGR NQQGY 30ZVA. SYGPBZZ
+(NveYVSR 1 A557HU BHG BS FREIVPR 18Sro2014@07:32...) NveYVSR 1 A557HU BHG BS FREIVPR 18Sro2014@07:32 Znvagranapr - SNQRP nhqvb jba'g ghea ba. N/P rkcrpgrq gb erghea gb freivpr va 4 ubhef.
+(14-0793 Fprar NveYVSR 2 YNHAPU 249Z 63.44az [29...) 14-0793 Fprar NveYVSR 2 YNHAPU 249Z 63.44az [29 12.68A 099 44.62J] TNEARE SVRYQ GB Zrgubqvfg Fcrpvnygl & Genafcynag 18Sro2014@02:46
+(14-0792 Vagreubfcvgny NveYVSR 3 YNHAPU 173Z 42....) 14-0792 Vagreubfcvgny NveYVSR 3 YNHAPU 173Z 42.67az [29 20.14A 099 08.09J] Zrqvan Pbzzhavgl Ubfcvgny GB Fna Nagbavb Zvyvgnel Zrqvpny Pragre - Abegu 17Sro2014@19:04
+(14-0791 Vagreubfcvgny NveYVSR 2 YNHAPU 239Z 109...) 14-0791 Vagreubfcvgny NveYVSR 2 YNHAPU 239Z 109.27az [28 42.78A 100 27.27J] Sbeg Qhapna Zrqvpny Pragre GB Zrgubqvfg Grkfna Ubfcvgny 17Sro2014@17:57
+(14-0790 Vagreubfcvgny NveYVSR 4 YNHAPU 123Z 52....) 14-0790 Vagreubfcvgny NveYVSR 4 YNHAPU 123Z 52.88az [28 24.48A 097 43.95J] Puevfghf Fcbua Orrivyyr Ubfcvgny GB Abegu Prageny Oncgvfg 17Sro2014@16:59
+(14-0787 NveYVSR 1 099Z 37.10az Tbamnyrf Zrzbevn...) 14-0787 NveYVSR 1 099Z 37.10az Tbamnyrf Zrzbevny Ubfcvgny [29 31.15A 097 27.05J] 17Sro2014@12:55
+(14-0785 NveYVSR 1 037Z 27.54az 2117 Abegufgne Y...) 14-0785 NveYVSR 1 037Z 27.54az 2117 Abegufgne Ybbc, Arj Oenhasryf, GK 78130, HFN [29 40.78A 098 06.87J] 17Sro2014@12:31
+(14-0784 NveYVSR 2 249Z 64.60az Hinyqr Zrzbevny ...) 14-0784 NveYVSR 2 249Z 64.60az Hinyqr Zrzbevny Ubfcvgny [29 12.83A 099 46.05J] 17Sro2014@12:00
+(14-0783 Erybpngvba Syvtug NveYVSR 2 YNHAPU 325Z...) 14-0783 Erybpngvba Syvtug NveYVSR 2 YNHAPU 325Z 11.61az [29 30.48A 098 34.75J] Havirefvgl Ubfcvgny - Fna Nagbavb 17Sro2014@11:23
+(14-0781 Vagreubfcvgny NveYVSR 2 YNHAPU 266Z 126...) 14-0781 Vagreubfcvgny NveYVSR 2 YNHAPU 266Z 126.68az [29 22.48A 100 53.49J] Iny Ireqr Ertvbany Zrqvpny Pge GB Zrgebcbyvgna Zrgu Ubfcvgny 17Sro2014@10:28
+(ERGHEA GB FREIVPR 17Sro2014@10:05 NveYVSR 2 A555ON) ERGHEA GB FREIVPR 17Sro2014@10:05 NveYVSR 2 A555ON
+(14-0780 NveYVSR 3 FGNAQOL BA FPRAR 240Z 5.44az ...) 14-0780 NveYVSR 3 FGNAQOL BA FPRAR 240Z 5.44az [30 00.51A 099 14.85J] 30�00.51'A 099�14.85'J Fprar 17Sro2014@09:51
+(14-0779 NveYVSR 1 FGNAQOL BA FPRAR 146Z 29.74az...) 14-0779 NveYVSR 1 FGNAQOL BA FPRAR 146Z 29.74az [29 16.19A 097 45.89J] 29�16.19'A 097�45.89'J Fprar 17Sro2014@09:37
+(14-0777 NveYVSR 3 304Z 42.10az Xvzoyr Ubfcvgny ...) 14-0777 NveYVSR 3 304Z 42.10az Xvzoyr Ubfcvgny [30 29.65A 099 46.69J] 17Sro2014@06:23
+(14-0775 Vagreubfcvgny NveYVSR 1 YNHAPU 293Z 101...) 14-0775 Vagreubfcvgny NveYVSR 1 YNHAPU 293Z 101.88az [30 29.65A 099 46.69J] Xvzoyr Ubfcvgny GB Crgrefba Ertvbany Zrq Pge 17Sro2014@03:07
+(14-0774 Vagreubfcvgny NveYVSR 3 YNHAPU 304Z 42....) 14-0774 Vagreubfcvgny NveYVSR 3 YNHAPU 304Z 42.10az [30 29.65A 099 46.69J] Xvzoyr Ubfcvgny GB Crgrefba Ertvbany Zrq Pge 17Sro2014@03:06
+(14-0773 Vagreubfcvgny NveYVSR 1 YNHAPU 244Z 61....) 14-0773 Vagreubfcvgny NveYVSR 1 YNHAPU 244Z 61.19az [29 20.14A 099 08.09J] Zrqvan Pbzzhavgl Ubfcvgny 17Sro2014@02:48
+(14-0772 Vagreubfcvgny NveYVSR 3 YNHAPU 173Z 42....) 14-0772 Vagreubfcvgny NveYVSR 3 YNHAPU 173Z 42.67az [29 20.14A 099 08.09J] Zrqvan Pbzzhavgl Ubfcvgny 17Sro2014@02:47
+(14-0770 Vagreubfcvgny NveYVSR 1 YNHAPU 293Z 101...) 14-0770 Vagreubfcvgny NveYVSR 1 YNHAPU 293Z 101.88az [30 29.65A 099 46.69J] Xvzoyr Ubfcvgny GB Funaaba Ubfcvgny 17Sro2014@02:21
+(14-0769 Vagreubfcvgny NveYVSR 3 YNHAPU 304Z 42....) 14-0769 Vagreubfcvgny NveYVSR 3 YNHAPU 304Z 42.10az [30 29.65A 099 46.69J] Xvzoyr Ubfcvgny GB Funaaba Ubfcvgny 17Sro2014@02:19
+(14-0768 NveYVSR 1 186Z 34.22az Pbaanyyl Zrzbevn...) 14-0768 NveYVSR 1 186Z 34.22az Pbaanyyl Zrzbevny Ubfcvgny [29 08.97A 098 09.73J] 17Sro2014@01:41
+(PNAPRYYRQ Vagreubfcvgny 14-0767 NveYVSR 1) PNAPRYYRQ Vagreubfcvgny 14-0767 NveYVSR 1
+(14-0767 Vagreubfcvgny NveYVSR 1 YNHAPU 105Z 32....) 14-0767 Vagreubfcvgny NveYVSR 1 YNHAPU 105Z 32.73az [29 31.15A 097 27.05J] Tbamnyrf Zrzbevny Ubfcvgny 16Sro2014@20:19
+ErtBcf - Jrrxyl Fhaqnl grfg cntr. Ab npgvba erdhverq. Zrqpbz
+(14-0765 NveYVSR 1 225Z 46.24az ybbc 410 naq enl...) 14-0765 NveYVSR 1 225Z 46.24az ybbc 410 naq enl ryyvfba [29 21.65A 098 37.94J] 16Sro2014@18:10
+(14-0764 Fprar NveYVSR 2 YNHAPU 193Z 9.17az [29 ...) 14-0764 Fprar NveYVSR 2 YNHAPU 193Z 9.17az [29 21.65A 098 37.94J] ybbc 410 naq enl ryyvfba 16Sro2014@17:58
+(14-0764 NveYVSR 2 FGNAQOL BA FPRAR 193Z 9.17az ...) 14-0764 NveYVSR 2 FGNAQOL BA FPRAR 193Z 9.17az [29 21.65A 098 37.94J] ybbc 410 naq enl ryyvfba Fprar 16Sro2014@17:54
+(NveYVSR 4 A557HU BHG BS FREIVPR 16Sro2014@17:34...) NveYVSR 4 A557HU BHG BS FREIVPR 16Sro2014@17:34 Znvagranapr
+(14-0763 Erybpngvba Syvtug NveYVSR 2 YNHAPU 325Z...) 14-0763 Erybpngvba Syvtug NveYVSR 2 YNHAPU 325Z 11.60az [29 30.48A 098 34.75J] Havirefvgl Onfr 16Sro2014@17:34
+(ERGHEA GB FREIVPR 16Sro2014@17:34 NveYVSR 2 A555ON) ERGHEA GB FREIVPR 16Sro2014@17:34 NveYVSR 2 A555ON
+(NveYVSR 2 A555ON BHG BS FREIVPR 16Sro2014@17:25...) NveYVSR 2 A555ON BHG BS FREIVPR 16Sro2014@17:25 Ab Perj - Ab Cvybg
+(14-0762 Erybpngvba Syvtug NveYVSR 2 YNHAPU 146Z...) 14-0762 Erybpngvba Syvtug NveYVSR 2 YNHAPU 146Z 11.63az [29 20.21A 098 28.25J] NveYVSR Unatne (Fgvafba) 16Sro2014@17:15
+(14-0761 NveYVSR 1 087Z 17.28az 224 gerrgbc ynar...) 14-0761 NveYVSR 1 087Z 17.28az 224 gerrgbc ynar yhyvat [29 42.06A 097 42.61J] 16Sro2014@16:47
+(14-0759 Vagreubfcvgny NveYVSR 1 YNHAPU 284Z 61....) 14-0759 Vagreubfcvgny NveYVSR 1 YNHAPU 284Z 61.09az [30 02.60A 099 09.06J] Crgrefba Ertvbany Zrq Pge GB Zrgubqvfg Grkfna Ubfcvgny 16Sro2014@12:29
+(14-0758 Vagreubfcvgny NveYVSR 1 YNHAPU 284Z 61....) 14-0758 Vagreubfcvgny NveYVSR 1 YNHAPU 284Z 61.09az [30 02.60A 099 09.06J] Crgrefba Ertvbany Zrq Pge GB Zrgubqvfg Grkfna Ubfcvgny 16Sro2014@12:14
+(14-0757 Vagreubfcvgny NveYVSR 3 YNHAPU 153Z 0.2...) 14-0757 Vagreubfcvgny NveYVSR 3 YNHAPU 153Z 0.21az [30 02.60A 099 09.06J] Crgrefba Ertvbany Zrq Pge GB Zrgubqvfg Grkfna Ubfcvgny 16Sro2014@12:13
+(14-0756 NveYVSR 2 212Z 66.00az PBGHYYN-YN FNYYR...) 14-0756 NveYVSR 2 212Z 66.00az PBGHYYN-YN FNYYR PBHAGL [28 27.40A 099 13.10J] 16Sro2014@11:57
+(ERGHEA GB FREIVPR 16Sro2014@11:33 NveYVSR 1 A556HU) ERGHEA GB FREIVPR 16Sro2014@11:33 NveYVSR 1 A556HU
+(ERGHEA GB FREIVPR 16Sro2014@09:03 NveYVSR 3 A407GK) ERGHEA GB FREIVPR 16Sro2014@09:03 NveYVSR 3 A407GK
+(ERGHEA GB FREIVPR 16Sro2014@07:37 NveYVSR 2 A555ON) ERGHEA GB FREIVPR 16Sro2014@07:37 NveYVSR 2 A555ON
+(NveYVSR 3 A407GK BHG BS FREIVPR 16Sro2014@06:27...) NveYVSR 3 A407GK BHG BS FREIVPR 16Sro2014@06:27 Ab Perj - Ab Cvybg GSA
+(14-0753 Vagreubfcvgny NveYVSR 3 YNHAPU 215Z 105...) 14-0753 Vagreubfcvgny NveYVSR 3 YNHAPU 215Z 105.06az [28 42.78A 100 27.27J] Sbeg Qhapna Zrqvpny Pragre GB Zrgebcbyvgna Zrgu Ubfcvgny 16Sro2014@06:07
+(14-0752 Vagreubfcvgny NveYVSR 3 YNHAPU 148Z 74....) 14-0752 Vagreubfcvgny NveYVSR 3 YNHAPU 148Z 74.39az [28 56.01A 098 31.52J] Fbhgu Grknf Ertvbany Zrq Pge GB Abegurnfg Oncgvfg Ubfcvgny 16Sro2014@05:20
+(14-0750 NveYVSR 3 FGNAQOL BA FPRAR 079Z 37.68az...) 14-0750 NveYVSR 3 FGNAQOL BA FPRAR 079Z 37.68az [30 05.97A 098 25.76J] Oynapb Uvtu Fpubby Fprar 16Sro2014@03:03
+(14-0749 NveYVSR 3 FGNAQOL BA FPRAR 106Z 30.51az...) 14-0749 NveYVSR 3 FGNAQOL BA FPRAR 106Z 30.51az [29 51.34A 098 36.52J] pbeqvyyren genpr & yrtraq ubyybj Fprar 16Sro2014@02:11
+(14-0747 NveYVSR 3 155Z 42.11az 231 Pbhagl Ebnq ...) 14-0747 NveYVSR 3 155Z 42.11az 231 Pbhagl Ebnq 475, Pnfgebivyyr, GK [29 23.01A 098 53.28J] 15Sro2014@23:41
+(NveYVSR 1 A556HU BHG BS FREIVPR 15Sro2014@23:17...) NveYVSR 1 A556HU BHG BS FREIVPR 15Sro2014@23:17 Znvagranapr qhr gb #1 trarengbe snvyher haxabja gvzr bhg bs freivpr. - sygpbzz
+(14-0746 Vagreubfcvgny NveYVSR 1 YNHAPU 105Z 32....) 14-0746 Vagreubfcvgny NveYVSR 1 YNHAPU 105Z 32.87az [29 31.15A 097 27.05J] Tbamnyrf Zrzbevny Ubfcvgny GB Fg Qnivq'f Zrqvpny Pragre 15Sro2014@21:08
+(14-0745 NveYVSR 1 264Z 68.09az 689 Ubaqb Perrx ...) 14-0745 NveYVSR 1 264Z 68.09az 689 Ubaqb Perrx Ebnq, Gnecyrl, GK [29 40.95A 099 20.86J] 15Sro2014@18:54
+(NveYVSR 2 A555ON BHG BS FREIVPR 15Sro2014@18:44...) NveYVSR 2 A555ON BHG BS FREIVPR 15Sro2014@18:44 Ab Perj - Ab Cvybg NP erybpngrq gb Fgvafba zrqperj gb zna gur SJ- sygpbzz
+(14-0744 NveYVSR 3 041Z 17.37az TVYYRFCVR PBHAGL...) 14-0744 NveYVSR 3 041Z 17.37az TVYYRFCVR PBHAGL [30 14.59A 098 54.55J] 15Sro2014@18:33
+(14-0743 NveYVSR 3 130Z 37.61az fg crgre gur svf...) 14-0743 NveYVSR 3 130Z 37.61az fg crgre gur svfurezna puhepu, iba nezl [29 28.15A 098 31.13J] 15Sro2014@17:29
+(14-0742 NveYVSR 2 041Z 29.49az Eviretngr YM [29...) 14-0742 NveYVSR 2 041Z 29.49az Eviretngr YM [29 50.75A 098 10.17J] 15Sro2014@16:34
+(14-0741 NveYVSR 3 FGNAQOL BA FPRAR 092Z 34.42az...) 14-0741 NveYVSR 3 FGNAQOL BA FPRAR 092Z 34.42az [29 58.33A 098 29.81J] 1600 S-Z Uvtujnl 473, Pbzsbeg, GK Fprar 15Sro2014@16:02
+(14-0740 Vagreubfcvgny NveYVSR 1 YNHAPU 166Z 52....) 14-0740 Vagreubfcvgny NveYVSR 1 YNHAPU 166Z 52.29az [28 51.00A 097 52.72J] Bggb Xnvfre Ubfcvgny 15Sro2014@15:57
+(14-0739 NveYVSR 2 027Z 12.33az Oncgvfg RE - Bir...) 14-0739 NveYVSR 2 027Z 12.33az Oncgvfg RE - Bireybbx [29 40.86A 098 27.25J] 15Sro2014@15:15
+(PNAPRYYRQ Fprar 14-0736 NveYVSR 3) PNAPRYYRQ Fprar 14-0736 NveYVSR 3
+(14-0737 NveYVSR 2 337Z 31.62az Znafsvryq Cnex [...) 14-0737 NveYVSR 2 337Z 31.62az Znafsvryq Cnex [29 43.89A 099 06.30J] 15Sro2014@12:58
+(14-0736 NveYVSR 3 FGNAQOL BA FPRAR 167Z 19.01az...) 14-0736 NveYVSR 3 FGNAQOL BA FPRAR 167Z 19.01az [29 43.89A 099 06.30J] Znafsvryq Cnex Fprar 15Sro2014@12:50
+(14-0735 NveYVSR 2 222Z 28.14az 215 pe 6644 qriv...) 14-0735 NveYVSR 2 222Z 28.14az 215 pe 6644 qrivar gk [29 11.17A 098 58.31J] 15Sro2014@12:44
+(14-0734 NveYVSR 2 281Z 19.88az Ynxruvyyf ISQ [2...) 14-0734 NveYVSR 2 281Z 19.88az Ynxruvyyf ISQ [29 35.77A 098 56.72J] 15Sro2014@12:35
+(14-0733 NveYVSR 2 140Z 29.00az 2250 pe 104 sybe...) 14-0733 NveYVSR 2 140Z 29.00az 2250 pe 104 syberfivyyr, grknf [29 06.48A 098 15.88J] 15Sro2014@10:10
+(14-0732 NveYVSR 1 358Z 4.22az 5600 A Vagrefgngr...) 14-0732 NveYVSR 1 358Z 4.22az 5600 A Vagrefgngr Uvtujnl 35, Arj Oenhasryf, Pbzny, Grknf 78130 [29 46.40A 098 02.32J] 15Sro2014@03:43
+(=?vfb-8859-1?D?14-0731_NveYVSR_3_163Z_35.98az_28=O055.19'A_098=O02...?=) 14-0731 NveYVSR 3 163Z 35.98az 28�55.19'A 098�26.10'J [28 55.19A 098 26.10J] 15Sro2014@03:40
+(14-0730 NveYVSR 2 176Z 17.93az Ujl. 16 & Fzvgu ...) 14-0730 NveYVSR 2 176Z 17.93az Ujl. 16 & Fzvgu Eq., Orkne, GK [29 12.45A 098 35.09J] 15Sro2014@03:37
+
+** ABGRF **
+Nabgure Nve nzohynapr freivpr
+
+Vtaber gur fhowrpg.
+
+Qvfcngpu nyregf nyy ybbx yvxr guvf.  
+14-0811 NveYVSR 5 Fprar YNHAPU 019Z 26.32az [29 36.86A 099 31.61J] Hgbcvn Zrqvpny Pyvavp 19Sro2014@17:13
+
+Svryq beqre fbzrgvzrf inevrf, ohg rirelguvat fubhyq or urer.
+Nalguvat ryfr fubhyq erghea snvyher.  Lbh pna qryrgr gurz sebz gur grfg fnzcyr frg
+
+cnggrea oernxqbja
+VQ:14-0811 
+HAVG:NveYVSR 5
+PNYY:Fprar YNHAPU 
+CYNPR:019Z 26.32az  (npghnyyl na nmvzhgu naq enatr) 
+NQQE:29 36.86A 099 31.61J  (npghnyyl TCF pbbeqvangrf)
+CYNPR:Hgbcvn Zrqvpny Pyvavp  (nccraq gb nmvzhgu naq enatr jvgu " - " 
+QNGRGVZR:19Sro2014@17:13  (Jvyy unir gb hfr n FvzcyrQngrSbezng gb genafyngr guvf. 
+                           whfg cnff gb frgQngrGvzr()
+                           
+Jura jr qb abg vapyhqr n shyy be gur pbeerpg qrsnhyg pvgl naq anzr, vg vf
+arprffnel gb bireevqr gur trgYbpAnzr() zrgubq naq erghea n hfre sevraqyl 
+qvfcynl anzr, yvxr "Syvtug Irpgbe, GK"                           
+
+*/
+
+choyvp pynff GKSyvtugIrpgbeCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp GKSyvtugIrpgbeCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "", "GK");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+    
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj GKSyvtugIrpgbeCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
