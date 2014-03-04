901fd1a596804cb63776d9fa8c84411f8f4c6a56
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-04 08:46:41 +0000

    new skeleton for OHFairviewCountyB. Compliments OHFairviewCounty. KSCrowleyCountyParser MsgFormats list update

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/KS/KSCowleyCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/KS/KSCowleyCountyParserTest.java
@@ -27,12 +27,210 @@ QvfcngpuN19Cnefre fhopynff
 choyvp pynff XFPbjyrlPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
   
   choyvp XFPbjyrlPbhaglCnefreGrfg() {
-    frgCnefre(arj QhzzlCnefre(), "PBJYRL PBHAGL", "XF");
+    frgCnefre(arj XFPbjyrlPbhaglCnefre(), "PBJYRL PBHAGL", "XF");
   }
   
   @Grfg
   choyvp ibvq grfgCnefre() {
- 
+    qbGrfg("G1",
+        "(VAPVQRAG # 1634) YBAT GREZ PNQ#     37338   NPGVIR PNYY# 7\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 07:21:33 03/02/14\a\a" +
+        " Angher: ZRQVPNY PNYY                                Glcr: s\a" +
+        "Nqqerff: 419 F PUHEPU                                Mbar: HSQ\a" +
+        "   Pvgl: Hqnyy\a\a" +
+        "Erfcbaqvat Havgf: E41\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "ZVQ 70'F LRNE BYQ SRZNYR PNEQVNP SNVYHER\a\a" +
+        "Pbagnpg: ENAQL UBSSZNA                     Cubar: (   )   -",
+
+        "VQ:1634/37338/7",
+        "GVZR:07:21:33",
+        "QNGR:03/02/14",
+        "PNYY:ZRQVPNY PNYY",
+        "NQQE:419 F PUHEPU",
+        "ZNC:HSQ",
+        "PVGL:Hqnyy",
+        "HAVG:E41",
+        "VASB:ZVQ 70'F LRNE BYQ SRZNYR PNEQVNP SNVYHER",
+        "ANZR:ENAQL UBSSZNA");
+
+    qbGrfg("G2",
+        "(VAPVQRAG # 1633,J14-00) YBAT GREZ PNQ#     37313   NPGVIR PNYY# 44\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 18:49:12 03/01/14\a\a" +
+        " Angher: ZIN VN                                      Glcr: s\a" +
+        "Nqqerff: 2700 HF 77                                  Mbar: HSQ\a" +
+        "   Pvgl: Pbhagl Yvzvg\a\a" +
+        "Erfcbaqvat Havgf: E71,E41\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "10-48 ebyybire ragenczrag\a\a" +
+        "Pbagnpg: fnz zpqnavry                      Cubar: (620) 214-0742",
+
+        "VQ:1633,J14-00/37313/44",
+        "GVZR:18:49:12",
+        "QNGR:03/01/14",
+        "PNYY:ZIN VN",
+        "NQQE:2700 HF 77",
+        "ZNC:HSQ",
+        "PVGL:Pbhagl Yvzvg",
+        "HAVG:E71,E41",
+        "VASB:10-48 ebyybire ragenczrag",
+        "ANZR:fnz zpqnavry",
+        "CUBAR:(620) 214-0742");
+
+    qbGrfg("G3",
+        "(VAPVQRAG # 1630) YBAT GREZ PNQ#     37235   NPGVIR PNYY# 33\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 13:29:35 02/28/14\a\a" +
+        " Angher: ZRQVPNY PNYY                                Glcr: s\a" +
+        "Nqqerff: 8151 FXL YNAR                               Mbar: HSQ\a" +
+        "   Pvgl: Hqnyy\a\a" +
+        "Erfcbaqvat Havgf: E41\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "UBSSZNA PNYYRQ GB ERCBEG N CBFFVOYR OEBXRA UVC BS N ZVQ 70 LRNE BYQ CNGVRAG\a\a" +
+        "Pbagnpg: UBSSZNA                           Cubar: (   )   -",
+
+        "VQ:1630/37235/33",
+        "GVZR:13:29:35",
+        "QNGR:02/28/14",
+        "PNYY:ZRQVPNY PNYY",
+        "NQQE:8151 FXL YNAR", //qbrf abg znc
+        "ZNC:HSQ",
+        "PVGL:Hqnyy",
+        "HAVG:E41",
+        "VASB:UBSSZNA PNYYRQ GB ERCBEG N CBFFVOYR OEBXRA UVC BS N ZVQ 70 LRNE BYQ CNGVRAG",
+        "ANZR:UBSSZNA");
+
+    qbGrfg("G4",
+        "(VAPVQRAG # 1628) YBAT GREZ PNQ#     37178   NPGVIR PNYY# 38\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 15:36:14 02/27/14\a\a" +
+        " Angher: SVER TENFF                                  Glcr: s\a" +
+        "Nqqerff: 3000 22AQ; PBJYRL 2                         Mbar: HSQ\a" +
+        "   Pvgl: Hqnyy\a\a" +
+        "Erfcbaqvat Havgf: CX44,CX45,G47\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "TENFF SVER\a\a" +
+        "Pbagnpg: BKYRL,TVAN                        Cubar: (316) 393-2640",
+
+        "VQ:1628/37178/38",
+        "GVZR:15:36:14",
+        "QNGR:02/27/14",
+        "PNYY:SVER TENFF",
+        "NQQE:3000 22AQ",
+        "K:PBJYRL 2",
+        "ZNC:HSQ",
+        "PVGL:Hqnyy",
+        "HAVG:CX44,CX45,G47",
+        "VASB:TENFF SVER",
+        "ANZR:BKYRL,TVAN",
+        "CUBAR:(316) 393-2640");
+
+    qbGrfg("G5",
+        "(VAPVQRAG # 1628) YBAT GREZ PNQ#     37178   NPGVIR PNYY# 38\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 15:36:14 02/27/14\a\a" +
+        " Angher: SVER TENFF                                  Glcr: s\a" +
+        "Nqqerff: 3000 22AQ; PBJYRL 2                         Mbar: HSQ\a" +
+        "   Pvgl: Hqnyy\a\a" +
+        "Erfcbaqvat Havgf: CX45,G47\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "TENFF SVER\a\a" +
+        "Pbagnpg: BKYRL,TVAN                        Cubar: (316) 393-2640",
+
+        "VQ:1628/37178/38",
+        "GVZR:15:36:14",
+        "QNGR:02/27/14",
+        "PNYY:SVER TENFF",
+        "NQQE:3000 22AQ",
+        "K:PBJYRL 2",
+        "ZNC:HSQ",
+        "PVGL:Hqnyy",
+        "HAVG:CX45,G47",
+        "VASB:TENFF SVER",
+        "ANZR:BKYRL,TVAN",
+        "CUBAR:(316) 393-2640");
+
+    qbGrfg("G6",
+        "(VAPVQRAG # 1628) YBAT GREZ PNQ#     37178   NPGVIR PNYY# 38\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 15:36:14 02/27/14\a\a" +
+        " Angher: SVER TENFF                                  Glcr: s\a" +
+        "Nqqerff: 3000 22AQ; PBJYRL 2                         Mbar: HSQ\a" +
+        "   Pvgl: Hqnyy\a\a" +
+        "Erfcbaqvat Havgf: CX45\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "TENFF SVER\a\a" +
+        "Pbagnpg: BKYRL,TVAN                        Cubar: (316) 393-2640",
+
+        "VQ:1628/37178/38",
+        "GVZR:15:36:14",
+        "QNGR:02/27/14",
+        "PNYY:SVER TENFF",
+        "NQQE:3000 22AQ",
+        "K:PBJYRL 2",
+        "ZNC:HSQ",
+        "PVGL:Hqnyy",
+        "HAVG:CX45",
+        "VASB:TENFF SVER",
+        "ANZR:BKYRL,TVAN",
+        "CUBAR:(316) 393-2640");
+
+    qbGrfg("G7",
+        "(VAPVQRAG # 1616) YBAT GREZ PNQ#     36822   NPGVIR PNYY# 38\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 16:42:49 02/21/14\a\a" +
+        "Angher: SVER TENFF                                  Glcr: s\a" +
+        "Nqqerff: 919 A EVIRE EBNQ                            Mbar: HSQ\a" +
+        "Pvgl:  HQNYY\a" +
+        " \a" +
+        " Erfcbaqvat Havgf: CX44\a" +
+        " \a" +
+        " Qverpgvbaf:\a" +
+        " \a" +
+        " \a" +
+        " Pbzzragf:\a" +
+        " TENFF SVER\a" +
+        " \a" +
+        " Pbagnpg: OEVNA QBHTYNFF                    Cubar: (316) 208-9068",
+
+        "VQ:1616/36822/38",
+        "GVZR:16:42:49",
+        "QNGR:02/21/14",
+        "PNYY:SVER TENFF",
+        "NQQE:919 A EVIRE EBNQ",
+        "ZNC:HSQ",
+        "PVGL:HQNYY",
+        "HAVG:CX44",
+        "VASB:TENFF SVER",
+        "ANZR:OEVNA QBHTYNFF",
+        "CUBAR:(316) 208-9068");
+
+    qbGrfg("G8",
+        "(VAPVQRAG # 1593) YBAT GREZ PNQ#     36507   NPGVIR PNYY# 41\a" +
+        "CEVBEVGL:                     ERCBEGRQ: 17:10:23 02/16/14\a\a" +
+        "Angher: SVER FGEHPGHER                              Glcr: s\a" +
+        "Nqqerff: 409 f 4gu                                   Mbar: NSQ\a" +
+        "   Pvgl:\a\a" +
+        "Erfcbaqvat Havgf: R41,G25,C29\a\a" +
+        "Qverpgvbaf:\a\a\a" +
+        "Pbzzragf:\a" +
+        "zhghny nvq erdhrfg sbe 2 gnaxre sebz ohgyre pbhagl sbe fgehpgher sver va\a" +
+        "qbhtynff\a\a" +
+        "Pbagnpg:                                   Cubar: (   )   -\a\a" +
+        "---\a" +
+        "Guvf rznvy vf serr sebz ivehfrf naq znyjner orpnhfr ninfg! Nagvivehf cebgrpgvba vf npgvir.\a" +
+        "uggc://jjj.ninfg.pbz",
+
+        "VQ:1593/36507/41",
+        "GVZR:17:10:23",
+        "QNGR:02/16/14",
+        "PNYY:SVER FGEHPGHER",
+        "NQQE:409 f 4gu",
+        "ZNC:NSQ",
+        "HAVG:R41,G25,C29",
+        "VASB:zhghny nvq erdhrfg sbe 2 gnaxre sebz ohgyre pbhagl sbe fgehpgher sver va / qbhtynff");
+
   }
   
   choyvp fgngvp ibvq znva(Fgevat[] netf) {
