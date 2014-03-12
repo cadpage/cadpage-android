0db6dd426f645ef74f790ae621a7c8da50d84c6e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-12 20:38:57 +0000

    Checking in Walla Walla County, WA

--- cadpage/src/net/anei/cadpage/parsers/WA/WAWallaWallaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WA/WAWallaWallaCountyParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.WA;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA5Parser;
 
 /**
@@ -15,9 +16,16 @@ public class WAWallaWallaCountyParser extends DispatchA5Parser {
   
   @Override
   public String getFilter() {
-    return "TBOBBITT@WALLAWALLAWA.GOV";
+    return "@WALLAWALLAWA.GOV";
   }
   
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (data.strCall.equals("E ProQA")) data.strCall = "E Medical";
+    return true;
+  }
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "College Pl",     "COLLEGE PLACE",
       "WW City",        "WALLA WALLA",
