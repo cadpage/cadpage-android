f6fbf8139679a7afdce56e7e2fbc12e7e7362881
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-15 03:17:00 +0000

    ONE MILLION NEW SKELETONS. or twelve.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/KY/KYNexigenParserTest.java
@@ -0,0 +1,91 @@
+cnpxntr arg.narv.pnqcntr.cnefref.XL;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Arkvtra Rzretrapl Erfcbapr, Pnzcoryy Pbhagl, XL
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Arkvtra Rzretrapl Erfcbafr
+Ybpngvba: Arjcbeg, XL, Havgrq Fgngrf
+Fraqre: "nyreg@arkvtra.pbz" <nyreg@arkvtra.pbz>
+Fraqre: Puevf Pbzcgba <PPbzcgba@arkvtra.pbz>
+
+(NQZVA08 Nyreg) PNYY: NQZVA08 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Nqzvavfgengvba<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(SF08 Nyreg) PNYY: SF08 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(ORQGRPU-N1EAXJT Nyreg) PNYY: ORQGRPU-N1EAXJT vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(YZH-QP01 Nyreg) PNYY: YZH-QP01 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(SVER08 Nyreg) PNYY: SVER08 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(PBY-VA-FEI Nyreg) PNYY: PBY-VA-FEI vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Nqzvavfgengvba<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(PZV-FDY01 Nyreg) PNYY: PZV-FDY01 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(GF03 Nyreg) PNYY: GF03 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(TU01 Nyreg) PNYY: TU01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(OQE Nyreg) PNYY: OQE vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(GFNCCF08 Nyreg) PNYY: GFNCCF08 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(GF01 Nyreg) PNYY: GF01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(GFNCCF Nyreg) PNYY: GFNCCF vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(GFNCCF64 Nyreg) PNYY: GFNCCF64 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(CUBAR01 Nyreg) PNYY: CUBAR01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(RKPUNATR01 Nyreg) PNYY: RKPUNATR01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(FDY01 Nyreg) PNYY: FDY01 vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(QP01 Nyreg) PNYY: QP01 vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(FDY01 Nyreg) PNYY: FDY01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(IP01 Nyreg) PNYY: IP01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(OQE3100E Nyreg) PNYY: OQE3100E vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Nqzvavfgengvba<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(HGVYVGVRF-FEI12 Nyreg) PNYY: HGVYVGVRF-FEI12 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(QP03 Nyreg) PNYY: QP03 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(HGVY-OVYY-FIE Nyreg) PNYY: HGVY-OVYY-FIE vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(RKPU01 Nyreg) PNYY: RKPU01 vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(PZV4810 Nyreg) PNYY: PZV4810 vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Hgvyvgvrf<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(PBYZNVY Nyreg) PNYY: PBYZNVY vf Bssyvar<OE>CYNPR: Pvgl bs Ynjeraprohet - Nqzvavfgengvba<OE>NQQE: 230 Jnyahg Fg<OE>PVGL: Ynjeraprohet<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(QP08 Nyreg) PNYY: QP08 vf Bssyvar<OE>CYNPR: OrqGrpuf - Znva bssvpr<OE>NQQE: 551 Zvatre Qe<OE>PVGL: Terraqnyr<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(OZPPCEF1 Nyreg) PNYY: OZPPCEF1 vf Bssyvar<OE>CYNPR: Ovbzrq Fcrpvnygl Cuneznpl - Znva bssvpr<OE>NQQE: 7731 Pbk Ynar<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(PUA-ZFZ118 Nyreg) PNYY: PUA-ZFZ118 vf Bssyvar<OE>CYNPR: Pbura - Znva bssvpr<OE>NQQE: 1723 Jbbqynja Nirahr<OE>PVGL: Zvqqyrgbja<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(NCCFREIRE Nyreg) PNYY: NCCFREIRE vf Bayvar<OE>CYNPR: Svther Jrvtug Ybff - Urnq Dhnegref<OE>NQQE: 157 Oneajbbq qevir<OE>PVGL: Rqtrjbbq<OE>FGNGR: XL<OE>CEV: Uvtu<OE>VASB:
+(ARKONX1 Nyreg) PNYY: ARKONX1 vf Bayvar<OE>CYNPR: Svther Jrvtug Ybff - Urnq Dhnegref<OE>NQQE: 157 Oneajbbq qevir<OE>PVGL: Rqtrjbbq<OE>FGNGR: XL<OE>CEV: Uvtu<OE>VASB:
+(ARKONX1 Nyreg) PNYY: ARKONX1 vf Bssyvar<OE>CYNPR: Svther Jrvtug Ybff - Urnq Dhnegref<OE>NQQE: 157 Oneajbbq qevir<OE>PVGL: Rqtrjbbq<OE>FGNGR: XL<OE>CEV: Uvtu<OE>VASB:
+(%pbzchgreanzr% Nyreg) PNYY: %pbzchgreanzr% vf Bayvar<OE>CYNPR: %PYVRAGANZR% - %YBPNGVBAANZR%<OE>NQQE: %pyvragnqqerff%<OE>PVGL: %pyvragpvgl%<OE>FGNGR: %pyvragfgngr%<OE>CEV: Uvtu<OE>VASB:
+(OZPPCEF1 Nyreg) PNYY: OZPPCEF1 vf Bssyvar<OE>CYNPR: Ovbzrq Fcrpvnygl Cuneznpl - Znva bssvpr<OE>NQQE: 7731 Pbk Ynar<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(QP1 Nyreg) PNYY: QP1 vf Bssyvar<OE>CYNPR: Pvapvaangv Neg Zhfrhz - Cevznel<OE>NQQE: 953 Rqra Cnex Qevir<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(QP1 Nyreg) PNYY: QP1 vf Bssyvar<OE>CYNPR: Pvapvaangv Neg Zhfrhz - Cevznel<OE>NQQE: 953 Rqra Cnex Qevir<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(OZPPCEF1 Nyreg) PNYY: OZPPCEF1 vf Bssyvar<OE>CYNPR: Ovbzrq Fcrpvnygl Cuneznpl - Znva bssvpr<OE>NQQE: 7731 Pbk Ynar<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(GF01 Nyreg) PNYY: GF01 vf Bayvar<OE>CYNPR: PPCV Vapbecbengrq - Znva bssvpr<OE>NQQE: 838 Pureel Fgerrg<OE>PVGL: Oynapurfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(GF01 Nyreg) PNYY: GF01 vf Bssyvar<OE>CYNPR: PPCV Vapbecbengrq - Znva bssvpr<OE>NQQE: 838 Pureel Fgerrg<OE>PVGL: Oynapurfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(OZPPCEF1 Nyreg) PNYY: OZPPCEF1 vf Bssyvar<OE>CYNPR: Ovbzrq Fcrpvnygl Cuneznpl - Znva bssvpr<OE>NQQE: 7731 Pbk Ynar<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(PUA-ZFZ118 Nyreg) PNYY: PUA-ZFZ118 vf Bssyvar<OE>CYNPR: Pbura - Znva bssvpr<OE>NQQE: 1723 Jbbqynja Nirahr<OE>PVGL: Zvqqyrgbja<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(PUA-ZFZ119 Nyreg) PNYY: PUA-ZFZ119 vf Bssyvar<OE>CYNPR: Pbura - Unzvygba Fpenc Cebprffbef<OE>NQQE: 1723 Jbbqynja Nirahr<OE>PVGL: Zvqqyrgbja<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(ORF Nyreg) PNYY: ORF vf Bayvar<OE>CYNPR: Pvapvaangv Neg Zhfrhz - Cevznel<OE>NQQE: 953 Rqra Cnex Qevir<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(ORF Nyreg) PNYY: ORF vf Bssyvar<OE>CYNPR: Pvapvaangv Neg Zhfrhz - Cevznel<OE>NQQE: 953 Rqra Cnex Qevir<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(OZPPCEF1 Nyreg) PNYY: OZPPCEF1 vf Bssyvar<OE>CYNPR: Ovbzrq Fcrpvnygl Cuneznpl - Znva bssvpr<OE>NQQE: 7731 Pbk Ynar<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(64GF2008 Nyreg) PNYY: Pbcl bs OH- Funqbj Cebgrpg Onpxhc Nyreg SNVYRQ ba 64GF2008<OE>CYNPR: cbjre fgrcf - Znva Bssvpr<OE>NQQE: 8930 Tybony Jnl<OE>PVGL: Jrfg Purfgre<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(GEVFGNGRFDY Nyreg) PNYY: Pbcl bs OH- Funqbj Cebgrpg Onpxhc Nyreg SNVYRQ ba GEVFGNGRFDY<OE>CYNPR: Gev-Fgngr Pragref sbe Fvtug - Qngnpragre<OE>NQQE: 2865 Punapryybe Qevir Fhvgr 215<OE>PVGL: Perfgivrj Uvyyf<OE>FGNGR: XL<OE>CEV: Uvtu<OE>VASB:
+(FAVUDSF1 Nyreg) PNYY: Pbcl bs OH- Funqbj Cebgrpg Onpxhc Nyreg SNVYRQ ba FAVUDSF1<OE>CYNPR: Fcrpgehz - Znva Bssvpr<OE>NQQE: 9145 Tbiraref Jnl<OE>PVGL: Pvapvangv<OE>FGNGR: BU<OE>CEV: Uvtu<OE>VASB:
+(FDY01 Nyreg) PNYY: FDY01 vf Bayvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(FDY01 Nyreg) PNYY: FDY01 vf Bssyvar<OE>CYNPR: Yvsrgvzr Erfbheprf - Znva bssvpr<OE>NQQE: 13091 Orarqvpg Qe.<OE>PVGL: Qvyyfobeb<OE>FGNGR: VA<OE>CEV: Uvtu<OE>VASB:
+(ONX01 Nyreg) PNYY: ONX01 vf Bssyvar<OE>CYNPR: Gur Cynfgvp Fhetrel Tebhc - Erqonax<OE>NQQE: 1 Cynfgvp Fhetrel Cynmn<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>HAVGF: Svryq, Urycqrfx, Znantrzrag<OE>CEV: Uvtu<OE>VASB:
+(ONX01 Nyreg) PNYY: ONX01 vf Bssyvar<OE>CYNPR: Gur Cynfgvp Fhetrel Tebhc - Erqonax<OE>NQQE: 1 Cynfgvp Fhetrel Cynmn<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>HAVGF: Svryq, Urycqrfx, Znantrzrag<OE>CEV: Uvtu<OE>VASB:
+(CBEGNY Nyreg) PNYY: CBEGNY vf Bssyvar<OE>CYNPR: PFN - Znva Bssvpr<OE>NQQE: 4705 Ynxr Sberfg Qevir<OE>PVGL: Pvapvaangv<OE>FGNGR: BU<OE>HAVGF: Svryq, Urycqrfx, Znantrzrag<OE>VQ: 16<OE>CEV: Uvtu<OE>QNGR: 11/30/2013 1:35:54 NZ<OE>VASB: Ynfg Pbagnpgrq 11/30/2013 1:17:46 NZ
+(ONX01 Nyreg) PNYY: ONX01 vf Bssyvar<OE>CYNPR: GCFT - Erqonax<OE>NQQE: <OE>PVGL: <OE>FGNGR: <OE>HAVGF: Svryq, Urycqrfx, Znantrzrag<OE>VQ: 25<OE>CEV: Uvtu<OE>QNGR: 11/29/2013 3:27:03 CZ<OE>VASB: Ynfg Pbagnpgrq 9/14/2013 6:00:34 NZ
+(ONX01 Nyreg) PNYY: ONX01 vf Bssyvar<OE>CYNPR: GCFT - Erqonax<OE>NQQE: <OE>PVGL: <OE>FGNGR: <OE>HAVGF: Svryq, Urycqrfx, Znantrzrag<OE>VQ: 25<OE>CEV: Uvtu<OE>QNGR: 11/29/2013 3:25:59 CZ<OE>VASB: Ynfg Pbagnpgrq 9/14/2013 6:00:34 NZ
+PNYY: JROFEI3 vf Bssyvar\a\aCYNPR: Pvapvaangv Neg Zhfrhz - Cevznel\a\aNQQE: 953 Rqra Cnex Qevir\a\aPVGL: Pvapvaangv\a\aFGNGR: BU\a\aVQ: 27\a\aCEV: Uvtu\a\aQNGR: 11/26/2013 4:24:10 CZ\a\aHAVG: Svryq, Urycqrfx, Znantrzrag\a\aVASB: Ynfg Pbagnpgrq 11/19/2013 4:25:57 CZ
+PNYY: JROFEI3 vf Bssyvar\a\aCYNPR: Pvapvaangv Neg Zhfrhz - Cevznel\a\aNQQE: 953 Rqra Cnex Qevir\a\aPVGL: Pvapvaangv\a\aFGNGR: BU\a\aVQ: 27\a\aCEV: Uvtu\a\aQNGR: 11/26/2013 4:24:10 CZ\a\aHAVG: Svryq, Urycqrfx, Znantrzrag\a\aVASB: Ynfg Pbagnpgrq 11/19/2013 4:25:57 CZ
+
+*/
+
+choyvp pynff XLArkvtraCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp XLArkvtraCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "PNZCORYY PBHAGL", "XL");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj XLArkvtraCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
