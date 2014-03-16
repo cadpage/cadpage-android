40c3da3250cf678331fb169a2574bdc5890018a9
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-16 01:04:39 +0000

    Checking in Clarke County, VA

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/VA/VAClarkeCountyParserTest.java
@@ -0,0 +1,78 @@
+cnpxntr arg.narv.pnqcntr.cnefref.IN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+vzcbeg arg.narv.pnqcntr.cnefref.IN.INNppbznpxPbhaglCnefre;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Pynexr Pbhagl, IN  (nyfb Jneera Pbhagl)
+
+Pbagnpg: Puevf Yvpuine <pnyvpuine@tznvy.pbz>
+Fraqre: PynexrRPP@pynexrpbhagl.tbi
+
+PynexrRPP:684 PNYZRF ARPX YA OBLPR, SBK JBBQF YA K EHAAVAT ORNE YA, 16:59:14, NYF YRIRY P, PNAPRE CNGVRAG,QVSSVPHYGL OERNGUVAT\a
+PynexrRPP:19844 OYHR EVQTR ZGA EQ OYHRZBAG, NFUYRL JBBQF YA K *UVTUYNAQ EVQTR YA (Irevsl), 15:35:14, FGNGVBA GENAFSRE, fgngvba svyy
+PynexrRPP:20962 OYHR EVQTR ZGA EQ CNEVF, *NFUYRL JBBQF YA (Irevsl) K UVTUYNAQ EVQTR YA, 16:46:27, UNAT HC PNYY 911, BA GUR GENVY. JR
+PynexrRPP:5000 WBUA ZBFOL UJL OBLPR, GUBEAGBA EQ K OVFUBC ZRNQR EQ, 17:44:41, OYF YRIRY O, 89 LB SRZNYR UVC, ONPX CNVA, TEBHAQ YRIRY
+PynexrRPP:2056 JVYQPNG UBYYBJ EQ OBLPR, PNERSERR YA K TERRAFGBAR YA, 10:17:26, NYF YRIRY P,
+
+*/
+
+choyvp pynff INPynexrPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp INPynexrPbhaglCnefreGrfg() {
+    frgCnefre(arj INPynexrPbhaglCnefre(), "PYNEXR PBHAGL", "IN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgPuevfYvpuine() {
+
+    qbGrfg("G1",
+        "PynexrRPP:684 PNYZRF ARPX YA OBLPR, SBK JBBQF YA K EHAAVAT ORNE YA, 16:59:14, NYF YRIRY P, PNAPRE CNGVRAG,QVSSVPHYGL OERNGUVAT\a",
+        "NQQE:684 PNYZRF ARPX YA",
+        "PVGL:OBLPR",
+        "K:SBK JBBQF YA / EHAAVAT ORNE YA",
+        "GVZR:16:59:14",
+        "PNYY:NYF YRIRY P",
+        "VASB:PNAPRE CNGVRAG, QVSSVPHYGL OERNGUVAT");
+
+    qbGrfg("G2",
+        "PynexrRPP:19844 OYHR EVQTR ZGA EQ OYHRZBAG, NFUYRL JBBQF YA K *UVTUYNAQ EVQTR YA (Irevsl), 15:35:14, FGNGVBA GENAFSRE, fgngvba svyy",
+        "NQQE:19844 OYHR EVQTR ZGA EQ OYHRZBAG",
+        "K:NFUYRL JBBQF YA / *UVTUYNAQ EVQTR YA (Irevsl)",
+        "GVZR:15:35:14",
+        "PNYY:FGNGVBA GENAFSRE",
+        "VASB:fgngvba svyy");
+
+    qbGrfg("G3",
+        "PynexrRPP:20962 OYHR EVQTR ZGA EQ CNEVF, *NFUYRL JBBQF YA (Irevsl) K UVTUYNAQ EVQTR YA, 16:46:27, UNAT HC PNYY 911, BA GUR GENVY. JR",
+        "NQQE:20962 OYHR EVQTR ZGA EQ CNEVF",
+        "K:*NFUYRL JBBQF YA (Irevsl) / UVTUYNAQ EVQTR YA",
+        "GVZR:16:46:27",
+        "PNYY:UNAT HC PNYY 911",
+        "VASB:BA GUR GENVY. JR");
+
+    qbGrfg("G4",
+        "PynexrRPP:5000 WBUA ZBFOL UJL OBLPR, GUBEAGBA EQ K OVFUBC ZRNQR EQ, 17:44:41, OYF YRIRY O, 89 LB SRZNYR UVC, ONPX CNVA, TEBHAQ YRIRY",
+        "NQQE:5000 WBUA ZBFOL UJL",
+        "PVGL:OBLPR",
+        "K:GUBEAGBA EQ / OVFUBC ZRNQR EQ",
+        "GVZR:17:44:41",
+        "PNYY:OYF YRIRY O",
+        "VASB:89 LB SRZNYR UVC, ONPX CNVA, TEBHAQ YRIRY");
+
+    qbGrfg("G5",
+        "PynexrRPP:2056 JVYQPNG UBYYBJ EQ OBLPR, PNERSERR YA K TERRAFGBAR YA, 10:17:26, NYF YRIRY P,",
+        "NQQE:2056 JVYQPNG UBYYBJ EQ",
+        "PVGL:OBLPR",
+        "K:PNERSERR YA / TERRAFGBAR YA",
+        "GVZR:10:17:26",
+        "PNYY:NYF YRIRY P");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj INPynexrPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
