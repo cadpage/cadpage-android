f444aa3808e22fbea2dc23c54dd7f5a06297d260
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-23 01:38:40 +0000

    new skeleton

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/SD/SDLincolnCountyBParserTest.java
@@ -0,0 +1,145 @@
+cnpxntr arg.narv.pnqcntr.cnefref.FQ;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Yvapbya Pbhagl, FQ
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Znevba Nzohynapr
+Ybpngvba: Znevba, FQ, Havgrq Fgngrf
+Fraqre: <yrqf@yvapbyapbhaglfq.bet>
+Fraqre: ab-ercyl@yrqfcbegny.pbz
+
+(Frzv jvgu Ubg oenxr qehzf) Znevba Rgunaby Cynag\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy ZRQVPNY) Znevba Nzohynapr - 44529 270GU FG - 03/15/14 16:22:52 - 55 l/b znyr - purfg cnva 55 LRNE BYQ ZNYR UNIVAT PURFG CNVAF
+(Srznyr univat Ernpgvba gb Zrqvpngvba) 160 Abegu Fg Zbaebr\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 03/06/14 15:07:30 - 83 LBZ gb Fnasbeq RE sbe uvc senpgher
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 03/06/14 15:07:30 - 83 LBZ gb Fnasbeq RE sbe uvc senpgher\aCyrnfr erfcbaq vzzrqvngryl.
+(Sver Pnyy) PnCQ; Znevba Sver - 27181 449GU NIR - 03/02/14 11:30:55 - fxvq ybnqre ba sver va furq\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 210 J ZNCYR FG, Zbaebr, FQ 57047 - 02/27/14 21:08:18 - 71 l/b Z Cg - Hapbafpvbhf & Oernguvat -\aCyrnfr erfcbaq vzzrqvngryl.
+(ZRFFNTR CNTR) - 02/15/14 13:25:08 - Sha Avtug cerc qnl Sro 26gu 10:00 ng gur Sver Unyy.
+(ZRFFNTR) GENVAVAT & ZRRGVAT GBAVTUG @ 6CZ
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 02/09/14 19:28:24 - 97 lb. S Cbff uvc Senpgher.\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 503 GVRFMRA QE # 503, Znevba, FQ 57043 - 02/07/14 21:27:11 - 85 l/b Z cg - Pnapre - Pbafpvbhf & Oernguvat - Cg vf ba Ubfcvpr pner\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 02/02/14 13:29:40 - RYQREYL CNGVRAG SNYY IVPGVZ JVGU PHG BA URNQ
+(ZRFFNTR CNTR) Znevba Sver  02/02/14 07:28:46 - Nyy ninvynoyr sversvtugref cyrnfr pbzr gb gur cnapnxr srrq.
+(ZRFFNTR CNTR) Abar - Znevba SQ - 01/31/14 13:50:22 - Cyrnfr zrrg ng gur sver unyy ng 2:00 CZ ba Fngheqnl gb frg hc sbe gur cnapnxr srrq.
+(Nzohynapr Pnyy) Abar - Gvrfmra Zrzbevny Ubzr - 01/28/14 14:01:00 - 1937 l/b S Cg - Snvagvat naq Urzbeentvat - Genafsre gb Fnasbeq Ubfc
+(Nzohynapr Pnyypneqvnp rzretrapl) Znevba Nzohynapr - 27038 447GU NIR - 01/24/14 04:30:19 - 41 lrne byq srznyr jvgu purfg cnva, oernguvat naq nyreg jvgu ab pneqvnp uvfgbel\aCyrnfr erfcbaq vzzrqvngryl.pneqvnp
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30  NG CNEXRE SVER UNYY.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 321 A WHAVCRE NIR, Cnexre, FQ 57053 - 01/23/14 17:25:59 - 82 l/b S Cg - Snyy Ivpgvz - CG unf orra ynlvat ba gur sybbe sbe frireny ubhef.\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 01/23/14 11:12:25 - Ryqreyl srznyr jvgu ryringrq grzc naq penpxyvat va yhatf
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30 GUHEFQNL NG CNEXRE SVER UNYY.
+(Nzohynapr Pnyy) Znevba Nzohynapr; Zbaebr Sver - 127 J NFU FG, Zbaebr, FQ 57047 - 01/22/14 18:10:50 - 77lb. S Nymurvzre'f cg rkgerzryl pbashfrq.\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyygenafsre gb Fnasbeq RE) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 01/22/14 00:46:44 - genafsre gb fnasbeq re\aCyrnfr erfcbaq vzzrqvngryl.
+(ZRFFNTR CNTR) Znevba Nzohynapr -  - 01/21/14 17:43:08 - Zvffvba Yvsryvar Genvavat @ 6:00.
+(ZRFFNTR CNTR) Znevba Sver 01/19/14 17:57:01 - Genvavat Zbaqnl @ 6:30. Vtaber cerivbhf grkg.
+(ZRFFNTR CNTR) Znevba Sver  01/19/14 17:57:01 - Genvavat Ghrfqnl @ 6:30.
+(Vawhel Nppvqrag VAWHEL NPPVQRAG) Yraabk Nzohynapr; Yraabk Sver - 281FG FG / 465GU NIR - VAWHEL NPPVQRAG HAXABJA VAWHEVRF
+(Nzohynapr Pnyy) Znevba Nzohynapr; Zbaebr Sver - 30 Abegu Fg Jrfg, Zbaebr - 01/10/14 12:11:50 - Cg sryy nobhg 3 uef ntb naq guvaxf fur znl unir fanccrq n ebq sebz fhetrel..
+(Nzohynapr Pnyy) Abar - 312 R FGNGR FG, ZNEVBA, FQ - 01/01/14 20:00:45 - 87 lb s purfg cnva tbvat gb ivobet re\aCyrnfr erfcbaq vzzrqvngryl.
+(Sver Pnyy) Znevba Sver - Ujl 81 & 273eq Fg - Iruvpyr Sver
+(Nzohynapr Pnyy ZRQVPNY) Abar - Gvrfmra Zrzbevny Ubzr -  88 LRNE BYQ SNYY IVPGVZ JVGU SNPVNY YNPRENGVBAF
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 12/28/13 16:10:16 - 81 lrne byq znyr jvgu cbff oebxra hccre nez\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, Znevba, FQ 57043 - 12/20/13 17:44:07 - 90 L/B SRZNYR - OEBXRA SRZHE
+(Sver Pnyy) Znevba Sver - Gvrfmra Zrzbevny Ubzr - 12/15/13 06:44:26 - Sver Nynez.\aCyrnfr erfcbaq vzzrqvngryl. Bira Sver.
+(Sver Pnyy) Abar - 27283 447GU NIR, ZNEVBA, FQ -
+(Nzohynapr Pnyy) Abar - 312 R FGNGR FG, ZNEVBA, FQ - 12/09/13 19:29:42 - 62 L/B SRZNYR SNYY IVPGVZ GB FNASBEQ SBE FGVGPURF\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 601 F PRQNE FG, Znevba - 12/08/13 13:25:50 - 27 lrne byq cerangny srznyr jvgu purfg cnvaf\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 307 R FGNGR FG, ZNEVBA, FQ - 12/06/13 15:26:33 - 83 lb. S .Snyy ynp gb gur snpr.\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 307 R FGNGR FG, ZNEVBA, FQ - 12/06/13 15:26:33 - 83 lb. S .Snyy ynp gb gur snpr.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 26705 447GU NIR - 12/01/13 23:40:09 - 45lb s frvmvat\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Abar - 26705 447GU NIR -  45 LBS frvmvat
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 11/28/13 09:53:50 - 48 LBZ Anhfrn ibzvgvat naq yrgunetl gb Niren RE
+(447 & 268) 1 IRUVPYR EBYY BIRE
+(Nzohynapr Pnyy) Yvsg nffvfg arrqrq\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 203 F OEBNQJNL FG, Znevba - 11/26/13 12:04:31 - 34 l/b S Cg -Qvssvphygl Oernguvat; 11/26/13 12:06:53 - Nfguzn\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr PnyyZRQVPNY) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 11/22/13 13:56:35 - 92 Srznyr univat Purfg Cnva.. 92 LRNE BYQ SRZNYR JVGU PURFG CNVAF
+(genvavat) znevba sver genvavat gbavtug ng 630 cz
+(ZRFFNTR CNTR) Znevba Sver -  - 11/15/13 13:21:40 - Znevba SQ gur Sver Fvera vf BHG bs Freivpr hagvy Shegure Abgvpr..
+(Nzohynapr Pnyy ZRQVPNY) Znevba Nzohynapr - 203 F OEBNQJNL NIR, ZNEVBA, FQ - 11/12/13 04:40:51 - NQHYG SRZNYR QVSSVPHYGL OERNGUVAT 34 LRNE BYQ SRZNYR JVGU NFGUZN, QVSSVPHYGL OERNGUVAT
+(Nzohynapr Pnyy) Znevba Nzohynapr - 120 F - S Cg - Qvss Oernguvat & Purfg Cnvaf - Cyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Znevba Nzohynapr - 312 R FGNGR FG, ZNEVBA, FQ - 11/01/13 12:03:59 - 97 L/B Srznyr gb tb gb Fnasbeq Cbffvoyr carhzbavn Gk gb Fnasbeq cbffvoyr carhzbavn
+(ZRFFNTR) NYY HAVGF BGURE GUNA PRAGREIVYYR SVER & YRAABK NZOHYNAPR ZNL QVFERTNEQ GRKG CNTR
+(28702 466GU NIR) RYQREYL SRZNYR - NOQBZVANY CNVA
+(222 R JNFUVATGBA) PNAPRY  CRE  QRCHGL
+(222 R JNFUVATGBA) FGNAQ OL SBE CBFF BIRE QBFR
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Uheyrl Sver
+Ybpngvba: Uheyrl, FQ, Havgrq Fgngrf
+Fraqre: <yrqf@yvapbyapbhaglfq.bet>
+Fraqre: ab-ercyl@yrqfcbegny.pbz
+
+(Nzohynapr Pnyy) Uheyrl Nzohynapr - 28241 454GU NIR, Uheyrl, FQ 57036 - \aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Uheyrl Nzohynapr - 511 WNAXYBJ QE, Uheyrl, FQ 57036 - 03/08/14 22:35:15 - ryqreyl znyr haerfcbafvir\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyy) Uheyrl Nzohynapr - Cvbarre Zrzbevny Ubfcvgny - 03/05/14 14:37:02 - 88 l/b S Cg - Cbff Fgebxr - Genafsre gb ZpXraana Ubfc\aCyrnfr erfcbaq vzzrqvngryl.
+(Fhfcvpvbhf Iruvpyr(f)) Uheyrl Nzohynapr; GPFB - 28639 458gu, Uheyrl - \aCyrnfr erfcbaq vzzrqvngryl. Cg neeulguzvn ceboyrzf.
+(28639 458GU) ZNYR JVGU PURFG CNVA
+(Nzohynapr Pnyyzrqvpny rzretrapl) Uheyrl Nzohynapr - 603 QNXBGN NIR, UHEYRL, FQ - 02/19/14 00:21:30 - 73 lrne byq srznyr fcvggvat hc obbbq\aCyrnfr erfcbaq vzzrqvngryl.
+(Nzohynapr Pnyyzrqvpny rzretrapl) Uheyrl Nzohynapr - 603 QNXBGN NIR, UHEYRL, FQ - 02/19/14 00:21:30 - 73 lrne byq srznyr fcvggvat hc obbbq\aCyrnfr erfcbaq vzzrqvngryl.
+(CVBARRE ZRZBEVNY) GENAFSRE PNEQVNP GB FVBHK SNYYF
+(Nzohynapr Pnyy) Uheyrl Nzohynapr; GPFB - 203 ZBAEBR FG, Uheyrl, FQ 57036 - 02/14/14 15:48:39 - 14 l/b S cg - Ovggra ol n fgenl qbt - EC qravrq zrqvpny freivprf 3 gvzrf.\aCyrnfr erfcbaq vzzrqvngryl.
+(Aba-Vawhel Nppvqrag) Qnivf Sver; Uheyrl Nzohynapr; GPFB - HF UVTUJNL 18 / 461FG NIR - 01/31/14 15:15:28 - Pnyyre ercbegvat 2 iruvpyr aba-vawhel nppvqrag ng guvf ybpngvba; 01/31/14 15:17:15 - ZEV 0945225 VA QYC 02743 01/31/2014 15:15:19 BHG 01/31/2014 15:15:19 4100 00139\aQE.FQQYC0000.FQ0410000GKG\a ANZ/ZLREF,PU*.QBO/19850324.FRK/S\a\a ZLREF,PURYFRN,YLAA \a 1104 A YVAPBYA NIR\a FVBHK SNYYF, FQ 571041721 \a BYA            : 00886367\a QNGR BS OVEGU  : 1985-03-24 FBP/504063085\a CUL QRFPEVCGVBA: FRK/S. UTG/5-04. JTG/190 YOF. RLR/OYH. \a YVPRAFR PYNFF  : PNE/GEHPX\a YVPRAFR GLCR   : Q - QEVIRE YVPRAFR\a YVPRAFR FGNGHF : NPGVIR/RYVTVOYR\a PQY FGNGHF     : VANPGVIR/RYVTVOYR\a YVPRAFR QNGRF  : VFF/2013-04-19. RKC/2018-03-24.\a BETNA QBABE    : AB\a\a ZLREF,PURYFRN,YLAA \a 3145 J 12GU FG #31\a FVBHK SNYYF, FQ 571043757 \a BYA            : 00963927\a QNGR BS OVEGU  : 1985-03-24 FBP/504063085\a CUL QRFPEVCGVBA: FRK/S. UTG/5-04. JTG/145 YOF. RLR/OYH. \a YVPRAFR PYNFF  : VQRAGVSVPNGVBA PNEQ\a YVPRAFR GLCR   : V - VQRAGVSVPNGVBA PNEQ\a YVPRAFR FGNGHF : VANPGVIR/RYVTVOYR\a PQY FGNGHF     : VANPGVIR/RYVTVOYR\a YVPRAFR QNGRF  : VFF/2006-05-04. RKC/2011-03-24.\a BETNA QBABE    : AB\a\a\a ***   R A Q   B S   Q N G N   ***; 01/31/14 15:37:15 - YVP/1KT051. YVL/2013. YVP-RKC/20140731. YVG/ERTHYNE ABA-PBZZREPVNY IRUVPYR.\aIVA/4N4NE3NH2QR012729. ILE/2013. IZN/ZVGF.\aIZB/BHGYNAQRE FCBEG. IFG/HG. IPB/OYH.\aJTG/3184. SHRY/TNFBYVAR. GBAF/0.\aGAE/130570122. PBHAGL/1. VFFH/20130314.\aANZ/IREA RVQR YRNFVAT YYP\a    WNPDHRYVAR F ZLREF\aNQE/5200 F YBHVFR NIR FVBHK SNYYF FBHGU QNXBGN 57108.\a ** YVRA ERPBEQRQ **\aYVRA UBYQRE/OZB UNEEVF ONAX AN.\aNQE/111 J ZBAEBR. PVF/PUVPNTB,VYYVABVF. MVC/60603.\a\a\a***   R A Q   B S   Q N G N   *** Qrchgl jnagf crbcyr purpxrq
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30  NG CNEXRE SVER UNYY.
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30 GUHEFQNL NG CNEXRE SVER UNYY.
+(Zrffntr) Nyy Havgf bgure guna Znevba SQ - qvfertneq cerivbhf grkg.
+(Sver Pnyy) Znevba Sver - Ujl 81 & 273eq Fg - Iruvpyr Sver
+(Nzohynapr Pnyy) Uheyrl SQ - 606 VYYVABVF NIR, UHEYRL, FQ - 12/18/13 09:10:32 - Srznyr hapbafpvbhf abg oernguvat; 12/18/13 09:15:06 - 619U erdhrfg ynj rasbeprzrag.  61N5 ernq qverpg
+(Nzohynapr Pnyy) Uheyrl Nzohynapr - 606 VYYVABVF NIR, UHEYRL, FQ - 12/18/13 09:10:32 - Srznyr hapbafpvbhf abg oernguvat
+(uheyrl sver) Uheyrl Sver, zrrgvat gbavtug jvgu ryrpgvbaf ng 7CZ
+(Nzohynapr Pnyy zrqvpny) Uheyrl Nzohynapr - 600 ZBAEBR FG, UHEYRL, FQ -  ryqreyl znyr cnffvat oybbq
+(Sver Pnyy) Uheyrl Sver - 45144 284GU FG - 12/10/13 22:18:09 - Fzbxr va gur onfrzrag.\aCyrnfr erfcbaq vzzrqvngryl.
+(zrrgvat) uheyrgl sver zrrgvat gbavtug ng 7 cz sbe ghexrl qenjvat
+(Sver Pnyy) Uheyrl Nzohynapr; Uheyrl Sver - 28122 UJL 19, Uheyrl - 11/22/13 13:02:21 - Fgehpgher Sver\aCyrnfr erfcbaq vzzrqvngryl.
+(ZRFFNTR) NYY HAVGF BGURE GUNA PRAGREIVYYR SVER & YRAABK NZOHYNAPR ZNL QVFERTNEQ GRKG CNTR
+(28702 466GU NIR) RYQREYL SRZNYR - NOQBZVANY CNVA
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Znevba Sver Qrcnegzrag
+Ybpngvba: Znevba, FQ, Havgrq Fgngrf
+Fraqre: <yrqf@yvapbyapbhaglfq.bet>
+Fraqre: ab-ercyl@yrqfcbegny.pbz
+
+(Frzv jvgu Ubg oenxr qehzf) Znevba Rgunaby Cynag\aCyrnfr erfcbaq vzzrqvngryl.
+(ZRFFNTR CNTR) - 02/24/14 17:53:23 - Genvavat gbavtug @ 6:30.
+(ZRFFNTR CNTR) - 02/15/14 13:25:08 - Sha Avtug cerc qnl Sro 26gu 10:00 ng gur Sver Unyy.
+(ZRFFNTR CNTR) Znevba Sver  02/02/14 07:28:46 - Nyy ninvynoyr sversvtugref cyrnfr pbzr gb gur cnapnxr srrq.
+(ZRFFNTR CNTR) Abar - Znevba SQ - 01/31/14 13:50:22 - Cyrnfr zrrg ng gur sver unyy ng 2:00 CZ ba Fngheqnl gb frg hc sbe gur cnapnxr srrq.
+(Nzohynapr Pnyypneqvnp rzretrapl) Znevba Nzohynapr - 27038 447GU NIR - 01/24/14 04:30:19 - 41 lrne byq srznyr jvgu purfg cnva, oernguvat naq nyreg jvgu ab pneqvnp uvfgbel\aCyrnfr erfcbaq vzzrqvngryl.pneqvnp
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30  NG CNEXRE SVER UNYY.
+(ZRFFNTR) GHEARE PBHAGL R.F.N. ZRRGVAT @ 6:30 GUHEFQNL NG CNEXRE SVER UNYY.
+(ZRFFNTR CNTR) Znevba Sver 01/19/14 17:57:01 - Genvavat Zbaqnl @ 6:30. Vtaber cerivbhf grkg.
+(ZRFFNTR CNTR) Znevba Sver  01/19/14 17:57:01 - Genvavat Ghrfqnl @ 6:30.
+(Sver Pnyy) Znevba Sver - Ujl 81 & 273eq Fg - Iruvpyr Sver
+(Sver Pnyy) Znevba Sver - Gvrfmra Zrzbevny Ubzr - 12/15/13 06:44:26 - Sver Nynez.\aCyrnfr erfcbaq vzzrqvngryl. Bira Sver.
+(Sver Pnyy) Abar - 27283 447GU NIR, ZNEVBA, FQ -
+(447 & 268) 1 IRUVPYR EBYY BIRE
+(Nzohynapr Pnyy) Yvsg nffvfg arrqrq\aCyrnfr erfcbaq vzzrqvngryl.
+(genvavat) znevba sver genvavat gbavtug ng 630 cz
+(ZRFFNTR CNTR) Znevba Sver -  - 11/15/13 13:21:40 - Znevba SQ gur Sver Fvera vf BHG bs Freivpr hagvy Shegure Abgvpr..
+(ZRFFNTR) NYY HAVGF BGURE GUNA PRAGREIVYYR SVER & YRAABK NZOHYNAPR ZNL QVFERTNEQ GRKG CNTR
+(28702 466GU NIR) RYQREYL SRZNYR - NOQBZVANY CNVA
+(222 R JNFUVATGBA) PNAPRY  CRE  QRCHGL
+(222 R JNFUVATGBA) FGNAQ OL SBE CBFF BIRE QBFR
+
+*/
+
+choyvp pynff FQYvapbyaPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp FQYvapbyaPbhaglOCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "YVAPBYA PBHAGL", "FQ");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+ 
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj FQYvapbyaPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
