db1540bf0bd041afbb1109df24a17db51b6fac4d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-30 01:38:14 +0000

    Fixed parsing problem witih Chilton County, AL

--- cadpage/src/net/anei/cadpage/parsers/AL/ALChiltonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALChiltonCountyParser.java
@@ -1,6 +1,9 @@
 
 package net.anei.cadpage.parsers.AL;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
@@ -9,6 +12,49 @@ import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
  */
 
 public class ALChiltonCountyParser extends DispatchSouthernParser {
+  
+  private static final Pattern GENERAL_ALERT_PTN = Pattern.compile("Unit ([ A-Za-z0-9]+) Status Note: .* - (\\d+) -.*");
+  private static final Pattern ADDR_EXIT_PTN = Pattern.compile("(\\d+ +EXIT) +(.*)");
+
+  public ALChiltonCountyParser() {
+    super(CITY_LIST, "CHILTON COUNTY", "AL", DSFLAG_LEAD_PLACE);
+    setupMultiWordStreets(
+        "COUNTRY CLUB",
+        "POPLAR SPRINGS");
+    
+    
+  }
+  @Override
+  public String getFilter() {
+    return "dispatch@chiltoncounty.org,dispatch@dispatch.ccso911.net";
+  }
+  
+  @Override
+  public boolean parseMsg(String body, Data data) {
+    
+    Matcher match = GENERAL_ALERT_PTN.matcher(body);
+    if (match.matches()) {
+      data.strCall = "GENERAL ALERT";
+      data.strUnit = match.group(1).trim();
+      data.strCallId = match.group(2);
+      data.strPlace = body;
+      return true;
+    }
+    
+    body = body.replace('\\', '/');
+    body = body.replaceAll("\\bCOUNTY RD\\b", "CO");
+    if (body.startsWith("/")) body = body.substring(1).trim();
+    if (! super.parseMsg(body, data)) return false;
+    
+    data.strAddress = data.strAddress.replaceAll("\\bCO\\b", "COUNTY RD");
+    
+    match = ADDR_EXIT_PTN.matcher(data.strAddress);
+    if (match.matches()) {
+      data.strPlace = append(data.strPlace, " ", match.group(1));
+      data.strAddress = match.group(2).trim();
+    }
+    return true;
+  }
   
   private static final String[] CITY_LIST = new String[]{
     "CLANTON",
@@ -20,25 +66,15 @@ public class ALChiltonCountyParser extends DispatchSouthernParser {
     "THORSBY",
     "VERBENA",
     "BILLINGSLEY", 
+    
+    // Autauga County
+    "MARBURY",
+    
+    // Bibb County
+    "RANDOLPH",
+    
+    // Shelby County
+    "CALERA",
+    "MONTEVALLO"
   };
-
-  public ALChiltonCountyParser() {
-    super(CITY_LIST, "CHILTON COUNTY", "AL", 0);
-    
-    
-  }
-  @Override
-  public String getFilter() {
-    return "dispatch@chiltoncounty.org,dispatch@dispatch.ccso911.net";
-  }
-  
-  @Override
-  public boolean parseMsg(String body, Data data) {
-    body = body.replace('\\', '/');
-    body = body.replaceAll("\\bCOUNTY RD\\b", "CO");
-    if (body.startsWith("/")) body = body.substring(1).trim();
-    if (! super.parseMsg(body, data)) return false;
-    data.strAddress = data.strAddress.replaceAll("\\bCO\\b", "COUNTY RD");
-    return true;
-  }
 }
