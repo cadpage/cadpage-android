cc8dcb7eb17d37eb080cc2b5b04c8465181cb22e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-03-30 23:23:06 +0000

    Replaced Kershaw County, SC parser

--- cadpage/src/net/anei/cadpage/parsers/SC/SCKershawCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCKershawCountyParser.java
@@ -1,16 +1,24 @@
 package net.anei.cadpage.parsers.SC;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.CodeSet;
-import net.anei.cadpage.parsers.dispatch.DispatchB3Parser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
 
 /**
  * Kershire County, SC
  */
-public class SCKershawCountyParser extends DispatchB3Parser {
+public class SCKershawCountyParser extends SmartAddressParser {
+  
+  private static final Pattern SUBJECT_PTN = Pattern.compile("As of (\\d\\d?/\\d\\d?/\\d\\d) \\d\\d");
+  private static final Pattern MASTER_PTN = Pattern.compile("(\\d\\d:\\d\\d) (\\d{4}-\\d{8}) (\\d{4}[A-Z]) - (.*) Unit Org Name Area Types (.*)");
 
   public SCKershawCountyParser() {
-    super("KERSHAWCOUNTY911:", CITY_LIST, "KERSHAW COUNTY", "SC");
-    setupCallList((CodeSet)null);
+    super( CITY_LIST, "KERSHAW COUNTY", "SC");
+    setupCallList(CALL_LIST);
+    setFieldList("DATE TIME ID CODE CALL ADDR APT CITY NAME UNIT");
   }
   
   @Override
@@ -19,9 +27,49 @@ public class SCKershawCountyParser extends DispatchB3Parser {
   }
   
   @Override
-  protected boolean isPageMsg(String body) {
+  public boolean parseMsg(String subject, String body, Data data) {
+    Matcher match = SUBJECT_PTN.matcher(subject);
+    if (!match.matches()) return false;
+    data.strDate = match.group(1);
+    
+    match = MASTER_PTN.matcher(body);
+    if (!match.matches()) return false;
+    data.strTime = match.group(1);
+    data.strCallId = match.group(2);
+    data.strCode = match.group(3);
+    String addr = match.group(4).trim();
+    data.strUnit = match.group(5).trim();
+    
+    int pt = addr.lastIndexOf(',');
+    if (pt >= 0) {
+      parseAddress(StartType.START_ADDR, FLAG_ONLY_CITY, addr.substring(pt+1).trim(), data);
+      if (data.strCity.length() > 0) {
+        data.strName = stripFieldStart(getLeft(), data.strCity);
+        addr = addr.substring(0,pt).trim().replace('@', '&');
+        pt = addr.lastIndexOf(',');
+        if (pt >= 0) {
+          data.strCall = addr.substring(0,pt).trim();
+          parseAddress(addr.substring(pt+1).trim(), data);
+        } else {
+          parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_NO_CITY | FLAG_ANCHOR_END, addr, data);
+        }
+        return true;
+      }
+    }
+    
+    addr = addr.replace('@', '&');
+    parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ, addr, data);
+    data.strName = getLeft();
     return true;
   }
+  
+  private static final CodeSet CALL_LIST = new CodeSet(
+      "FIRE ALARM SOUNDING",
+      "MOTOR VEHICLE ACCIDENT",
+      "STRUCTURE FIRE",
+      "VEHICLE ACCIDENT",
+      "WOODS, BRUSH"
+  );
   
   private static final String[] CITY_LIST = new String[]{
     "ANTIOCH",
@@ -32,6 +80,12 @@ public class SCKershawCountyParser extends DispatchB3Parser {
     "ELGIN",
     "LIBERTY HILL",
     "LUGOFF",
-    "WESTVILLE"
+    "WESTVILLE",
+    
+    // Chester County
+    "JEFFERSON",
+    
+    // Lancaster County
+    "KERSHAW"
   };
 }
