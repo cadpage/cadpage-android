ef63cf880083d6d4abd89e72f0869376e044e634
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-03 07:04:14 +0000

    i dont understand

--- cadpage/src/net/anei/cadpage/parsers/WI/WIRacineCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WI/WIRacineCountyParser.java
@@ -1,15 +1,69 @@
 package net.anei.cadpage.parsers.WI;
 
+import java.util.Properties;
 import net.anei.cadpage.parsers.dispatch.DispatchProphoenixParser;
 
 public class WIRacineCountyParser extends DispatchProphoenixParser {
   
   public WIRacineCountyParser() {
-    super("RACINE COUNTY", "WI");
+    super(CITY_TABLE,"RACINE COUNTY", "WI");
   }
   
   @Override
   public String getFilter() {
     return "PhoenixAlert@goracine.org";
   }
+  
+  private static final Properties CITY_TABLE = buildCodeTable(new String[]{
+      //Cities, villages, and towns
+      
+      "Burlington",
+      "Caledonia",
+      "Dover",
+      "Elmwood Park",
+      "Mount Pleasant",
+      "North Bay",
+      "Norway",
+      "Racine",
+      "Raymond",
+      "Rochester",
+      "Sturtevant",
+      "Union Grove",
+      "Waterford",
+      "Wind Point",
+      "Yorkville",
+
+  //Census-designated places"
+
+      "Bohners Lake",
+      "Browns Lake",
+      "Eagle Lake",
+      "Waterford North",
+      "Wind Lake",
+
+  //Unincorporated communities
+
+      "Beaumont",
+      "Buena Park",
+      "Caldwell",
+      "Cedar Park",
+      "Eagle Lake Manor",
+      "Franksville",
+      "Husher",
+      "Ives Grove",
+      "Kansasville",
+      "Kneeland",
+      "North Cape",
+      "Raymond",
+      "Rosewood",
+      "Sylvania",
+      "Tichigan",
+      "Union Church",
+      "Yorkville",
+
+  //Ghost towns
+
+      "Muskego Settlement",
+      "Raymond Center"
+  });
 }
