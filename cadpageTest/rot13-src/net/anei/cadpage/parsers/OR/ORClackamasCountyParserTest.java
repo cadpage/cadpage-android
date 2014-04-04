075a1d8c1c917312c660c76f3fc15dcbc09af8ca
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-04 08:35:36 +0000

    new skeletons.

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORClackamasCountyParserTest.java
@@ -0,0 +1,133 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BE;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Pynpxnznf Pbhagl, BE
+
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: NZE Pynpxnznf Pb
+Ybpngvba: ZVYJNHXVR, BE, Havgrq Fgngrf
+Fraqre: <cbegynaqpbzz@nze-rzf.pbz>
+
+(IvfvPNQ Rznvy)              HAVG: 269       VAP#: 4520709                                    1235 FJ Ynxrfuber Qe                      Rfgnpnqn           MVC: 97023       KFG: FJ Zncyr Eq/FJ Alzcu Eq                   CEV:21 Hetrag
+(IvfvPNQ Rznvy)              HAVG: 269       VAP#: 4513704   ZREVQVNA CNEX UBFCVGNY           19300 Fj 65gu Nir              RE 5       Ghnyngva           MVC: 97062       KFG: FJ Aloret Fg/Zrevqvna Cnex Ubfcvgny Cnex  CEV:10 NYF < 12 uef Fp
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: V5R - V 5/Ryyvtfra Eq
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: V5O - V 5/Ybjre Obbarf Sreel
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: 1 - Bertba Pvgl (BPFP)
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:269           VAP#: 4513025     PYE: 12:51:07
+(IvfvPNQ Rznvy) Havg #269       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:269            VAP#: 4513025        EPQ: 11:24:18     QFC: 11:24:38     RAE: 11:25:25     FGT:              BAF: 11:33:09     JCG:              GEN: 11:46:23     NGQ: 12:30:14
+(IvfvPNQ Rznvy)              HAVG: 269       VAP#: 4513025                                    39495 Pnfpnqvn Ivyyntr Qe      212        Fnaql              MVC: 97055       KFG: FR Wnpbol Eq/Jnyyvatsbeq Nir              CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 269      VAP#: 4513010       EPQ: 10:55:41     QFC: 10:56:01     RAE: 10:56:05     FGT:              BAF: 11:02:16     JCG:              PYE: 11:05:42
+(IvfvPNQ Rznvy)              HAVG: 269       VAP#: 4513010                                    18944 FR Onxref Sreel Eq                  Obevat             MVC: 97009       KFG: Qrnq Raq/FR Onegba Cnex Eq                CEV:10 Yvsr G
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: 9 - Fnaql
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: OO - Obevat
+(IvfvPNQ Rznvy) HAVG: 269     NFFVTARQ CBFG: 9 - Fnaql
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: 9 - Fnaql
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: JY- Jrypurf
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: 9 - Fnaql
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: QG - Ynxr Bfjrtb
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: 2 - Zvyjnhxvr
+(IvfvPNQ Rznvy) HAVG: 261      VAP#: 4508394       EPQ: 14:04:04     QFC: 14:04:21     RAE: 14:04:25     FGT:              BAF: 14:04:26     JCG:              PYE: 15:04:59
+(IvfvPNQ Rznvy)              HAVG: 261       VAP#: 4508394   ZREVQVNA CNEX UBFCVGNY           19300 FJ 65gu Nir                         Ghnyngva           MVC: 97062       KFG: FJ Aloret Fg/Zrevqvna Cnex Ubfcvgny Cnex  CEV:22 Hetrag
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:261           VAP#: 4508357     PYE: 14:04:18
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:261            VAP#: 4508357        EPQ: 13:03:48     QFC: 13:04:21     RAE: 13:04:28     FGT:              BAF: 13:12:13     JCG:              GEN: 13:29:44     NGQ: 13:46:52
+(IvfvPNQ Rznvy)              HAVG: 261       VAP#: 4508357                                    24310 F UJL 99R #58            58         Pnaol              MVC:             KFG:                                           CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 261      VAP#: 4508342       EPQ: 12:51:12     QFC: 12:51:21     RAE: 12:51:28     FGT:              BAF:              JCG:              PYE: 13:02:55
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.00:10:00
+(IvfvPNQ Rznvy)              HAVG: 261       VAP#: 4508342                                    30256 Fj Gubznf Fg             1106       Jvyfbaivyyr        MVC: 97070       KFG: FJ Ubyyl Ya/FJ Ubyyl Ya                   CEV:10 Yvsr G
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: V5O - V 5/Obbarf Sreel Eq
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: V5R - V 5/Ryyvtfra Eq
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: 4 - Obbarf Sreel/Xehfr Jnl
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:261           VAP#: 4508286     PYE: 11:54:26
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:261            VAP#: 4508286        EPQ: 11:08:02     QFC: 11:08:20     RAE: 11:08:25     FGT:              BAF: 11:12:17     JCG:              GEN: 11:18:39     NGQ: 11:35:57
+(IvfvPNQ Rznvy)              HAVG: 261       VAP#: 4508286                                    232 6GU FG                                Ynxr Bfjrtb        MVC:             KFG: ogja RYYVF NIR & RIRETERRA EQ             CEV:21 Hetrag
+(IvfvPNQ Rznvy) HAVG: 261      VAP#: 4508278       EPQ: 10:52:48     QFC: 10:53:01     RAE: 10:53:32     FGT:              BAF:              JCG:              PYE: 11:03:00
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.00:30:00
+(IvfvPNQ Rznvy) ERFCBAFR CEVBEVGL PUNATRQ GB:          21 Hetrag Bertba            Nqqvgvbany Vasbezngvba
+(IvfvPNQ Rznvy) ERFCBAFR CEVBEVGL PUNATRQ GB:          21 Hetrag Bertba            Nqqvgvbany Vasbezngvba
+(IvfvPNQ Rznvy) ERFCBAFR CEVBEVGL PUNATRQ GB:          14 Cer Nyreg                Nqqvgvbany Vasbezngvba
+(IvfvPNQ Rznvy)              HAVG: 261       VAP#: 4508278                                    232 6GU FG                                Ynxr Bfjrtb        MVC:             KFG: ogja RYYVF NIR & RIRETERRA EQ             CEV:70 NFFVTA
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: V5O - V 5/Obbarf Sreel Eq
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: 1 - Bertba Pvgl (BPFP)
+(IvfvPNQ Rznvy) HAVG: 261     NFFVTARQ CBFG: V5R - V 5/Ryyvtfra Eq
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493397     PYE: 17:43:22
+(IvfvPNQ Rznvy) Havg #267       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493397        EPQ: 16:50:58     QFC: 16:51:21     RAE: 16:51:27     FGT:              BAF: 16:59:59     JCG:              GEN: 17:21:03     NGQ: 17:23:16
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493397                                    8106 FR Ynxr Eq                417        Zvyjnhxvr          MVC: 97267       KFG: FR Enfcoreel Pg/Qrnq Raq                  CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: TY- V 205/Tynqfgbar Rkvg
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: V5O - V 5/Obbarf Sreel Eq
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493336     PYE: 16:16:35
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493336        EPQ: 15:09:15     QFC: 15:09:54     RAE: 15:10:02     FGT:              BAF: 15:32:59     JCG:              GEN: 15:43:32     NGQ: 16:02:10
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493336   CEVAPRGBA IVYYNTR NFFVFGRQ YVI   14370 Fr Bertba Genvy Qe       104        Pynpxnznf          MVC: 97015       KFG: FR 142aq Nir/FR 144gu Nir                 CEV:21 Hetrag
+(IvfvPNQ Rznvy) HAVG: 267      VAP#: 4493332       EPQ: 15:01:52     QFC: 15:02:03     RAE: 15:02:09     FGT:              BAF:              JCG:              PYE: 15:03:54
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493332                                    15510 FR Jnyynpr Eq                       Zvyjnhxvr          MVC: 97267       KFG: FR Nyyra Eq/FR Guvrffra Eq                CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: TY- V 205/Tynqfgbar Rkvg
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 1 - Bertba Pvgl (BPFP)
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493277     PYE: 14:14:27
+(IvfvPNQ Rznvy) Havg #267       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493277        EPQ: 13:17:20     QFC: 13:17:50     RAE: 13:17:59     FGT:              BAF: 13:22:49     JCG:              GEN: 13:26:41     NGQ: 13:39:28
+(IvfvPNQ Rznvy) ERFCBAFR CEVBEVGL PUNATRQ GB:          10 Yvsr Guerngravat Rzret   Nqqvgvbany Vasbezngvba
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493277                                    15485 OBBARF SREEL EQ                     Ynxr Bfjrtb        MVC:             KFG: Pbyyvaf Jnl/Erq Prqne Jnl                 CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267      VAP#: 4493272       EPQ: 13:14:56     QFC: 13:15:35     RAE: 13:15:46     FGT:              BAF:              JCG:              PYE:
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493272   ZREVQVNA CNEX UBFCVGNY           19300 FJ 65gu Nir                         Ghnyngva           MVC: 97062       KFG: FJ Aloret Fg/Zrevqvna Cnex Ubfcvgny Cnex  CEV:22 Hetrag
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: V5O - V 5/Obbarf Sreel Eq
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 10P - 82aq Nir/Wbuafba Perrx
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493195     PYE: 11:53:58
+(IvfvPNQ Rznvy) Havg #267       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493195        EPQ: 10:50:02     QFC: 10:50:21     RAE: 10:50:28     FGT:              BAF: 10:57:51     JCG:              GEN: 11:04:49     NGQ: 11:31:39
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493195                                    4527 FR Eubqrfn Fg                        Zvyjnhxvr          MVC: 97222       KFG: FR 45gu Nir/Qrnq Raq                      CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 7P - 82aq & Nzoyre
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493156     PYE: 10:26:24
+(IvfvPNQ Rznvy) Havg #267       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493156        EPQ: 09:20:43     QFC: 09:22:37     RAE: 09:22:45     FGT:              BAF: 09:30:03     JCG:              GEN: 09:41:55     NGQ: 10:01:22
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493156                                    12900 FR Pnccf Eq                         Pynpxnznf          MVC:             KFG: FR Jvyqr Eq/FR Pnecragre Qe               CEV:10 Yvsr G
+(IvfvPNQ Rznvy) HAVG: 267      VAP#: 4493155       EPQ: 09:20:32     QFC: 09:22:13     RAE:              FGT:              BAF:              JCG:              PYE:
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493155   BERTBA PVGL ERFVQRAGVNY          515 10gu Fg                    11         Bertba Pvgl        MVC: 97045       KFG: Qrnq Raq/Jnfuvatgba Fg                    CEV:10 Yvsr G
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4493137     PYE: 09:18:51
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4493137        EPQ: 08:35:22     QFC: 08:35:25     RAE: 08:35:30     FGT:              BAF: 08:45:59     JCG:              GEN: 09:02:50     NGQ: 09:10:26
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4493137                                    3466 FR Svyoreg Fg                        Zvyjnhxvr          MVC:             KFG: FR 32aq Nir/FR 42aq Nir                   CEV:14 Cer Ny
+(IvfvPNQ Rznvy) Cbfg Nffvtazrag Punatr -- Zbir Gb:715
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 2 - Zvyjnhxvr
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: V5R - V 5/Ryyvtfra Eq
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: V5O - V 5/Obbarf Sreel Eq
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4484965     PYE: 15:19:21
+(IvfvPNQ Rznvy) Havg #267       Uv, 5 zva hagvy V chg lbh va-freivpr.  Nqivfr vs Rkgraqrq.
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4484965        EPQ: 14:08:13     QFC: 14:09:12     RAE: 14:09:17     FGT:              BAF: 14:17:57     JCG:              GEN: 14:28:46     NGQ: 14:55:46
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4484965                                    13355 FR Ghzoyrjrrq Pg                    Unccl Inyyrl       MVC: 97086       KFG: FR 132aq Nir/Qrnq Raq                     CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 7P - 82aq & Nzoyre
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: TY- V 205/Tynqfgbar Rkvg
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 1 - Bertba Pvgl (BPFP)
+(IvfvPNQ Rznvy) ** HAVG PYRNE **  ** NINVYNOYR SBE NFFVTAZRAG **   HAVG:267           VAP#: 4484916     PYE: 13:32:57
+(IvfvPNQ Rznvy) BOGNVA CG. FVTANGHER       HAVG:267            VAP#: 4484916        EPQ: 12:42:56     QFC: 12:43:16     RAE: 12:43:21     FGT:              BAF: 12:51:31     JCG:              GEN: 13:07:04     NGQ: 13:20:40
+(IvfvPNQ Rznvy) Yngr Ba-Fprar Neeviny - Bire Fgnaqneq  Ercbeg Znl Or Erdhverq.
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4484916                                    13145 FR Evire Eq              345        Zvyjnhxvr          MVC: 97222       KFG: FR Rireterra Fg/FR Fvyire Fcevatf Eq      CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267      VAP#: 4484901       EPQ: 12:20:47     QFC: 12:21:17     RAE: 12:21:29     FGT:              BAF: 12:26:48     JCG:              PYE: 12:28:53
+(IvfvPNQ Rznvy)              HAVG: 267       VAP#: 4484901                                    19594 FR Pbggbajbbq Fg                    Zvyjnhxvr          MVC: 97267       KFG: FR Znarjny Ya/FR Zryqehz Nir              CEV:14 Cer Ny
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 1 - Bertba Pvgl (BPFP)
+(IvfvPNQ Rznvy) HAVG: 267     NFFVTARQ CBFG: 4 - Obbarf Sreel/Xehfr Jnl
+
+*/
+
+choyvp pynff BEPynpxnznfPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BEPynpxnznfPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "PYNPXNZNF PBHAGL", "BE");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj BEPynpxnznfPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/VA/VADickensonCountyParserTest.java
@@ -0,0 +1,87 @@
+cnpxntr arg.narv.pnqcntr.cnefref.IN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pyvagjbbq SQ
+Ybpngvba: Pyvagjbbq, IN, Havgrq Fgngrf
+Fraqre: <Fcvyyzna_Cntvat@911.pbz>
+
+(Zrffntr sebz UvcYvax) PJSQ\aPJSVER\a14PJS0051\aQF   - 14.01.34 04/01/14\aPZCYG- 16.01.02
+(Zrffntr sebz UvcYvax) PJSQ\aZIN-AB CV\a8709 QVPXRAFBA UJL\a267S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-ERFVQRAPR\a632 JNYAHG FG\a253S\aPJSVER\aEC NQI XREBFRAR URNGRE BA SVER BA GUR SEBAG CBEPU, ABOBQL VF NEBHAQ VG.
+(Zrffntr sebz UvcYvax) PJSQ\aNYNEZ\a1026 EHFU PERRX EQ\a253S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aFZBXR\a126 PEVZFBA FG\a253S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-NYNEZ\a346 O OBO ZGA\a266S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-OEHFU\a419 BYQ PYVAGJBBQ UJL\a253S\aPJSVER\aARRQ SVER QRCG UNIR N BHGFVQR SVER GUNG VF ARNE N BYQ OHVYQVAT NAQ N GENVYRE.
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a3386 PBROHEA EQ @\a250S\aPJSVER\aunf gur ebnq oybpxrq. qbrfag guvax gurer ner nal vawhevrf ohg hafher. ur jnf nyy\abire gur ebnq orsber ur uvg gur oneevre
+(Zrffntr sebz UvcYvax) arrq 605 gb pnyy qvfcngpu.
+(Zrffntr sebz UvcYvax) NGGA:605 ARRQ LBH GB PNYY QVFCNGPU NFNC
+(Zrffntr sebz UvcYvax) PJSQ\aNFFVFGNAPR\aJVFR PBHAGL\a002\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aCREFBANY FREI\a118 PBYBAVNY PG\a253Y\aPJSVER\a88 l b. ba n jnyxre naq unf n ohfgrq jngre yvar. arrqf gur jngre zrgre phg bss
+(Zrffntr sebz UvcYvax) PJSQ\aFZBXR\a855 JBYSR CRA EQ\a267S\aPJSVER\apne17 jnagf gb xabj vs n sver qrcg zrzrore pbhyq pbzr qbja naq purpx bhg uvf\abira vg vf fzbxrvat\a\a612 nqi ur jvyy or re
+(Zrffntr sebz UvcYvax) PJSQ\aFZBXR\a855 JBYSR CRA EQ\a267S\aPJSVER\apne17 jnagf gb xabj vs n sver qrcg zrzrore pbhyq pbzr qbja naq purpx bhg uvf\abira vg vf fzbxrvat\a\a612 nqi ur jvyy or re
+(Zrffntr sebz UvcYvax) PJSQ\aFZBXR\a855 JBYSR CRA EQ\a267R\aPJSVER\apne17 jnagf gb xabj vs n sver qrcg zrzrore pbhyq pbzr qbja naq purpx bhg uvf\abira vg vf fzbxrvat\a\a612 nqi ur jvyy or re
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-BHGFVQR\aUNEBYQ UHTURF QE\a253S\aPJSVER
+(Zrffntr sebz UvcYvax) PUVRS PNYY QVFCNGPU CYRNFR
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a8462 QVPXRAFBA UJL\a267S\aPJSVER\aERPIQ FRIRENY 911 PNYYF SBEQ FVYIRE GEHPX VA QVGPU, JURRY PNZR BSS NAQ JUBYR\aSEBAG RAQ ZRFFRQ HC. NQIVFRQ GUNG ZNYR FHOWRPG FNVQ UR JNF BX UR JNF TBVAT GB TB\aBA OHG UR JNF OYRRQVAT SEBZ GUR URNQ. O
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a330 QP PNARL EVQTR EQ\a267S\aPJSVER\apnyyrq onpx Guh Zne 13 07 44 16 RQG 2014 tbg snfg ohfl\agurer'f n pne jerpx ba pnarl evqtr oneryl hc sebz 4 jnl, gurl ner va thneqenvy\ajvgu GA gntf. Vg vf n fvyire ohvpx, 2 uvfcnavp thlf.\aQbrfa'g
+(Zrffntr sebz UvcYvax) NAL PREGVSVRQ SBERFGEL CREFBAARY ERFCBAQ GB 6159 OVT EVQTR EQ SBE OEHFU SVER
+(Zrffntr sebz UvcYvax) ULSQ\aSVER-OEHFU\a6159 OVT EVQTR EQ\a250S\aULSQ\aEC NQIVFRQ GURL JRER OHEAVTA GUR UVYY BSS NAQ GUR SVER WHZCRQ GB GUR BGURE FVQR.\aOBGU ERFPHR ZRZOREF NYERNQL RE GB FGNGVBA\a3eq nyreg\a1601 rf\a1620 RF\a544 NQI GUNG GURER NER OHVYQVATF ARNEOL OHG ABG VA
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a100 GHAARY EQ\a267S\aPJSVER
+(Zrffntr sebz UvcYvax) PJRZ\aFGEBXR\a356 FPUBBY UBHFR UBYJ\a255R\aPJRZF\aFGEBXR FVTAF NTNVA SBE N PBHCYR BS UBHEF, FUR UNQ N FGEBXR YNFG JRRX, FUR VF\aPBAFPVBHF, PNA'G JNYX ABJ NF FUR UNQ, URE OC VF TBBQ OHG URE BKLTRA JNF VA GUR\a60'F.\a51 nqi. gb pnyy nve zrq j/ ym ng jrfg
+(Zrffntr sebz UvcYvax) PJSQ\aGENSSVP UNMNEQ\a9695 QVPXRAFBA UJL\a267S\aPJSVER\aNQIVFRQ GURER VF N EBPX FYVQR GUNG VF OYBPXVAT BAR YNAR NAQ NYZBFG NABGURE\aN SVER ZRZORE NQIVFRQ FBZRBAR ARRQF GB FRG BHG SYNERF
+(Zrffntr sebz UvcYvax) PJSQ\aZIN CEBC QNZN\a3800 GUR YNXR EQ\a267S\aPJSVER
+(Zrffntr sebz UvcYvax) PJRZ\aPURFG CNVA\a7692 QP PNARL EVQTR EQ\a263R\aPJRZF\a323 re gb fgngvba\a329 re gb fgngvba\afur vf ng Ninaryy oyrivaf ba pnarl evqtr naq guvaxf Ninaryy vf univat n urneg\anggnpx,fur vf univat purfg cnva naq qvssvphygl oernguvat.84lbs\afur qbrf abg gnxr
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a8709 QVPXRAFBA UJL\a267S\aPJSVER
+(Zrffntr sebz UvcYvax) Zrrgvat sbe gbavtug unf orra pnapryyrq nhgu ol 605
+(Zrffntr sebz UvcYvax) Zrrgvat sbe gbavtug unf orra pnapryrq qhr gb gur jrngure,nhgu ol 605
+(Zrffntr sebz UvcYvax) PJSQ\aZVFFVAT CREFBA\a1326 OYBJVAT EBPX EQ\a266S\aPJSVER\aEBPX UBHFR CNFG GUR BYQ ENATREF PNOVA GUR OYBJ UBYR. UNF FCENVARQ UVF NAXYR NAQ\aARRQF URYC TRGGVAT BHG. YBFG PBAARPGVBA. UNF N SEVRAQ JVGU UVZ. BA GUR GENVY\aEVTUG ABJ TBVAT BSS BS VG. GELVAT
+(Zrffntr sebz UvcYvax) PJRZ\aZVFFVAT CREFBA\a1326 OYBJVAT EBPX EQ\a266R\aRE\aEBPX UBHFR CNFG GUR BYQ ENATREF PNOVA GUR OYBJ UBYR. UNF FCENVARQ UVF NAXYR NAQ\aARRQF URYC TRGGVAT BHG. YBFG PBAARPGVBA. UNF N SEVRAQ JVGU UVZ. BA GUR GENVY\aEVTUG ABJ TBVAT BSS BS VG. GELVAT GB J
+(Zrffntr sebz UvcYvax) PJSQ\aZIN CEBC QNZN\a3880 FXRRGEBPX EQ\a266S\aPJSVER\aFBA CHG GUR IRUVPYR BIRE GUR UVYY. AB VAWHEVRF 451 FBHGU BS GUR ZGA EQ.
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-NYNEZ\a763 PRAGRAAVNY URVTUGF EQ\a250S\aPJSVER\aO OHVYQVAT NCNEG 5. XRLYN ZHYYVAF\aSVER NYNEZ UNF ORRA TBVAT BSS SBE N JUVYR. AB SVER. FBZRBAR UNF OHEAG FBZR SBBQ.\afcbxr jvgu pne11 ur nqi gung natryn oblq vf gur znantre gurer. gevrq gur ah
+(Zrffntr sebz UvcYvax) GVTTRE CYRNFR PNYY QVFCNGPU GUNAXF
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-OEHFU\a396 RZBTRAR YA\a267S\aPJSVER\aoehfu sver nsenvq vg jvyy trg bhg/ nyernql 1 nper naq unys ohearq
+(Zrffntr sebz UvcYvax) PPSQ\aSVER-OEHFU\a995 EHFU PEX\a267S\aPPSVER\aEC NQI SVRYQ NAQ OEHFU VF BA SVER NAQ GURER NER CEBCNAR GNAXF BA GUR FVQR BS GUR\aUBHFR GUR SVRYQ VF OHEAVAT BA.\aEC NQI GJB UBHFR BA GUR YRSG, NAQ IRUVPYRF PNA OR ZBIRQ. EC NQI GERRF NER NOBHG\aGB PNGPU BA
+(Zrffntr sebz UvcYvax) PJSQ\aGENSSVP UNMNEQ\a9525 QVPXRAFBA UJL\a267S\aPJSVER\aoybpxvat 3/4 bs gur ebnq\aQVFC ZHYYVAF PNYYRQ IFC NAQ NQI BS GUVF.\aPNYYRQ TERT YLNYY NAQ NQI BS GUVF. UR NQI UR JBHYQ FRAQ FBZRBAR NFNC
+(Zrffntr sebz UvcYvax) QPFB\aGENSSVP UNMNEQ\a9525 QVPXRAFBA UJL\a267Y\aQPNE6\aoybpxvat 3/4 bs gur ebnq\aQVFC ZHYYVAF PNYYRQ IFC NAQ NQI BS GUVF.\aPNYYRQ TERT YLNYY NAQ NQI BS GUVF. UR NQI UR JBHYQ FRAQ FBZRBAR NFNC
+(Zrffntr sebz UvcYvax) PPSQ\aSVER-ERFVQRAPR\a202 UNEYRL QE\a261S\aPPSVER\aerfvqrapr orfvqr bs gurerf fvatyrjvqr genvyre nqivfrq gur jubyr vafvqr vf ba\asver naq gurer vf bhg ohvyqvat pybfr vg.  nqivfrq ab bar ubzr ng gur erfvqrapr.
+(Zrffntr sebz UvcYvax) PBATENGF GB NAQL NAQ WNPXVR FGNYYNEQ BA GUR NEEVINY BS GURVEONOL OBL NAQERJ OYNXR FGNYYNEQ ZBGURE NAQ ONOL NER QBVAT SVARNAQ NER RKCRPGRQ GB OR UBZR YNGRE GBQNL!
+(Zrffntr sebz UvcYvax) gjb iruvpyr zin nyfb vaibyivat n cbjre cbyr,bayl bar zrzore unf erfcbaqrq
+(Zrffntr sebz UvcYvax) 2aq nyreg gjb pne urnq ba zin,arne penogerr ubyybj,nyfb fgehpxcbjre cbyr bar zrzore gb erfcb
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a1851 PBROHEA EQ\a255S\aPJSVER\a621 rf
+(Zrffntr sebz UvcYvax) gurer jvyy or n unmzng njnerarff pynff ng 9nz ng pyvagjbbq erfphr ohvyqvat sbe nalbar jnagvat gb nggraq
+(Zrffntr sebz UvcYvax) PJSQ\aURZBEEUNTR\a398 FUNEBAF EQ\a261R\aPJSVER\aAHEFR JNAGF URE GB TB GB GUR RE NQIVFRQ UNQ N CYNPR BA URE YRT SEBZ N SNYY GUNG\aJNF QENVAVAT NAQ OYRRQVAT.\aPnyy glcr r erbcrarq ol WY WHFGVPR ng 13 08 29 02/05/14
+(Zrffntr sebz UvcYvax) grfg nyreg. grfg nyreg bayl gb purpxvat cntvat flfgrzf
+(Zrffntr sebz UvcYvax) NYY SVER HAVGF HFR ERFPHR PUNAARY SVER PUNAARY QBJA
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\aTBFCRY YVTUG PUHEPU\a253S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\aFJRRARL QE / QVPXRAFBA UJL\a002\aPJSVER\aGJB IRUVPYR ZIN/\a323 rf\a326 bf qverpgvat genssvp
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\aFJRRARL QE / QVPXRAFBA UJL\a002\aPJSVER\aGJB IRUVPYR ZIN/
+(Zrffntr sebz UvcYvax) PJSQ\aSVER-ERFVQRAPR\a1070 UBFCVGNY QE\a253S\aPJSVER
+(Zrffntr sebz UvcYvax) PPSQ\aZIN CEBC QNZN\a3598 PBROHEA EQ\a255S\aPPSVER\aE/C nqivfrq gung gurer jnf n zna gung jrag gb Zef Fgnayrlf ubhfr gura
+(Zrffntr sebz UvcYvax) PJSQ\aZIN CEBC QNZN\a6964 QVPXRAFBA UJL\a267S\aPJSVER\aIRUVPYR VF CNEGVNYYL OYBPXVAT EBNQ. GUVAXF UR UVG FBZRGUVAT VA GUR EBNQ OHG\aPNA'G FRR NALGUVAT. NQIVFRQ IRUVPYR JBAG RIRA FGNEG ABJ. VG VF FVYIRE CBAGVNP T6\aFES156 NQIVFRQ BF\aPNYYRQ IFC GURL JVY
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a5180 QVPXRAFBA UJL\a253S\aPJSVER\aNQIVFRQ UR JNF VAIBYIRQ VA NA ZIN VA SEBAG BS YBAT WBUAF HAXABJA VAWHEVRF GB GUR\aBGURE CNEGL
+(Zrffntr sebz UvcYvax) zin jvgu vawhel 72 vagrefrpgvba,ab zrzoref gb erfcbaq,2aq nyreg
+(Zrffntr sebz UvcYvax) PJSQ\aZIN VAWHEL\a100 PBROHEA EQ\a267S\aPJSVER
+(Zrffntr sebz UvcYvax) PJSQ\aM GRFG PNYY\a206 IBYHAGRRE NIR\a253S\aPJSVER\aGUVF VF ABG N PNYY NAQ VF SBE GRFGVAT CHECBFRF BAYL
+
+*/
+choyvp pynff INQvpxrafbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp INQvpxrafbaPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "QVPXRAFBA PBHAGL", "IN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+   }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj INQvpxrafbaPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
