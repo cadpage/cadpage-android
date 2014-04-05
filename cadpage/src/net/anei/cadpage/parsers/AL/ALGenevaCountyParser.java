c59a35b09776390f376a659acddbd0dd774af060
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-05 22:25:40 +0000

    Fixed parsin problem witih Geneva County, AL

--- cadpage/src/net/anei/cadpage/parsers/AL/ALGenevaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALGenevaCountyParser.java
@@ -1,25 +1,34 @@
 
 package net.anei.cadpage.parsers.AL;
 
-import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
 
 /**
  * Baldwin County, AL
  */
-public class ALGenevaCountyParser extends DispatchOSSIParser {
+public class ALGenevaCountyParser extends FieldProgramParser {
 
   public ALGenevaCountyParser() {
     super("GENEVA COUNTY", "AL",
-          "CALL ADDR! X+? INFO+");
+          "CALL! Location:ADDR! City:CITY! Subject:CALL! Place:PLACE! Details:INFO!");
   }
     
   @Override
   public String getFilter() {
-    return "CAD@smtp.gmail.com,34Central@smtp.gmail.com";
+    return "34Central@smtp.gmail.com";
   }
   
   @Override
   public int getMapFlags() {
     return MAP_FLG_SUPPR_LA;
+  }
+  
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("34 Central:_")) return false;
+    body = body.substring(12).trim();
+    return super.parseFields(body.split(" //"), data);
   }
 }
