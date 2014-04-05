c2613ad2022d2e9cfb736e6808a0860857c60bd5
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-05 23:23:25 +0000

    More fixes for Geneva County, AL

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/AL/ALGenevaCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/AL/ALGenevaCountyParserTest.java
@@ -1,81 +1,24 @@
 cnpxntr arg.narv.pnqcntr.cnefref.NY;
 
-vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
-vzcbeg arg.narv.pnqcntr.cnefref.NY.NYTrarinPbhaglCnefreGrfg;
+vzcbeg bet.whavg.rkcrevzragny.ehaaref.Rapybfrq;
+vzcbeg bet.whavg.ehaare.EhaJvgu;
 
-vzcbeg bet.whavg.Grfg;
-
-/*
-Trarin Pbhagl, NY
-
-Pbagnpg: wcevpr911@lnubb.pbz
-Fraqre: 34Prageny@fzgc.tznvy.pbz
-
-34 Prageny:_1052 //Ybpngvba: 293 PBHAGL EBNQ 79 //Pvgl: FNZFBA //Fhowrpg: //Cynpr: //Qrgnvyf: AB ERFCBAFR SEBZ FNZFBA RZF , GBARQ TRARIN BHG
-34 Prageny:_10-72 //Ybpngvba: 448 CP OYNPX EQ //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: PBHYQ ABG HAQREFGNAQ ZNYR FHOWRPG, QB ABG XABJ JUNG UR ARRQF
-34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf:
-34 Prageny:_1052 //Ybpngvba: 2163 F FGNGR UVTUJNL 27 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: 83 LBN SRZNYR UNF SNYYRA NAQ UVG URE URNQ BA GUR QBBE. N
-34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: PYRNENAPR TENAGRQ GUEBHTU UNEGSBEQ
-34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf:
-
+/* 
+Pnoneehf Pbhagl, AP
 */
 
-choyvp pynff NYTrarinPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+@EhaJvgu(Rapybfrq.pynff)
+choyvp pynff NYTrarinPbhaglCnefreGrfg {
   
-  choyvp NYTrarinPbhaglCnefreGrfg() {
-    frgCnefre(arj NYTrarinPbhaglCnefre(), "TRARIN PBHAGL", "NY");
+  choyvp fgngvp pynff GrfgN rkgraqf NYTrarinPbhaglNCnefreGrfg {
+    choyvp GrfgN() {
+      frgCnefre(arj NYTrarinPbhaglCnefre());
+    }
   }
   
-  @Grfg
-  choyvp ibvq grfgCnefre() {
-
-
-    qbGrfg("G1",
-        "34 Prageny:_1052 //Ybpngvba: 293 PBHAGL EBNQ 79 //Pvgl: FNZFBA //Fhowrpg: //Cynpr: //Qrgnvyf: AB ERFCBAFR SEBZ FNZFBA RZF , GBARQ TRARIN BHG",
-        "PNYY:1052",
-        "NQQE:293 PBHAGL EBNQ 79",
-        "PVGL:FNZFBA",
-        "VASB:AB ERFCBAFR SEBZ FNZFBA RZF , GBARQ TRARIN BHG");
-
-    qbGrfg("G2",
-        "34 Prageny:_10-72 //Ybpngvba: 448 CP OYNPX EQ //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: PBHYQ ABG HAQREFGNAQ ZNYR FHOWRPG, QB ABG XABJ JUNG UR ARRQF",
-        "PNYY:10-72",
-        "NQQE:448 CP OYNPX EQ",
-        "PVGL:TRARIN",
-        "VASB:PBHYQ ABG HAQREFGNAQ ZNYR FHOWRPG, QB ABG XABJ JUNG UR ARRQF");
-
-    qbGrfg("G3",
-        "34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf:",
-        "PNYY:1097",
-        "NQQE:OHPX INHTUNA EQ NAQ 85",
-        "ZNQQE:OHPX INHTUNA EQ & 85",
-        "PVGL:TRARIN");
-
-    qbGrfg("G4",
-        "34 Prageny:_1052 //Ybpngvba: 2163 F FGNGR UVTUJNL 27 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: 83 LBN SRZNYR UNF SNYYRA NAQ UVG URE URNQ BA GUR QBBE. N",
-        "PNYY:1052",
-        "NQQE:2163 F FGNGR UVTUJNL 27",
-        "ZNQQE:2163 F FGNGR 27",
-        "PVGL:TRARIN",
-        "VASB:83 LBN SRZNYR UNF SNYYRA NAQ UVG URE URNQ BA GUR QBBE. N");
-
-    qbGrfg("G5",
-        "34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf: PYRNENAPR TENAGRQ GUEBHTU UNEGSBEQ",
-        "PNYY:1097",
-        "NQQE:OHPX INHTUNA EQ NAQ 85",
-        "ZNQQE:OHPX INHTUNA EQ & 85",
-        "PVGL:TRARIN",
-        "VASB:PYRNENAPR TENAGRQ GUEBHTU UNEGSBEQ");
-
-    qbGrfg("G6",
-        "34 Prageny:_1097 //Ybpngvba: OHPX INHTUNA EQ NAQ 85 //Pvgl: TRARIN //Fhowrpg: //Cynpr: //Qrgnvyf:",
-        "PNYY:1097",
-        "NQQE:OHPX INHTUNA EQ NAQ 85",
-        "ZNQQE:OHPX INHTUNA EQ & 85",
-        "PVGL:TRARIN");
-  }
-  
-  choyvp fgngvp ibvq znva(Fgevat[] netf) {
-    arj NYTrarinPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  choyvp fgngvp pynff GrfgO rkgraqf NYTrarinPbhaglOCnefreGrfg {
+    choyvp GrfgO() {
+      frgCnefre(arj NYTrarinPbhaglCnefre());
+    }
   }
 }
