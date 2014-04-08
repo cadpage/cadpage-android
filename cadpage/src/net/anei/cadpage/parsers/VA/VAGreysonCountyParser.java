0f69754e53afb5042736a61a457cc43a65731137
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-08 10:46:11 +0000

    did some work on VAGreysonCounty, still not parsing quite right.

---
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAGreysonCountyParser.java
@@ -0,0 +1,47 @@
+package net.anei.cadpage.parsers.VA;
+
+import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.dispatch.DispatchDAPROParser;
+
+/**
+ * Greyson County, VA
+ */
+public class VAGreysonCountyParser extends DispatchDAPROParser {
+  
+  public VAGreysonCountyParser() {
+    super("GREYSON COUNTY", "VA");  
+    setupCallList(CALL_SET);
+  }
+  
+  @Override
+  public String getFilter() {
+    return "MAILBOX@GalaxVa.com";
+  }
+  
+  private static final CodeSet CALL_SET = new CodeSet(
+      
+      "EMS - ABDOMINAL DISTRESS",
+      "EMS - BLEEDING",
+      "EMS - BLOOD PRESSURE",
+      "EMS - BREATHING DIFFICULTY",
+      "EMS - CARDIAC",
+      "EMS - DIABETIC",
+      "EMS - DISORIENTED",
+      "EMS - FAINTING",
+      "EMS - FAINTING / SYNCOPE",
+      "EMS - FALL",
+      "EMS - FEVER",
+      "EMS - INTERFACILITY TRANSPORT",
+      "EMS - NAUSEA / VOMITING",
+      "EMS - NO INFORMATION ON PATIEN",
+      "EMS - PAIN",
+      "EMS - STROKE",
+      "EMS - RETURN TRIP",
+      "EMS - UNRESPONSIVE",
+      "EMS - WEAKNESS",
+      "MENTAL / EMOTIONAL/PSYC",
+      "MOTOR VEHICLE CRASH"
+ 
+  );
+  
+}
