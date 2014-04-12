88180332c974bb98d1c9e2a89a83897c4476f6be
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-12 07:53:41 +0000

    Fixed parsing problem with Rockland County, NY (B)

--- cadpage/src/net/anei/cadpage/parsers/NY/NYRocklandCountyBParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYRocklandCountyBParser.java
@@ -1,6 +1,7 @@
 package net.anei.cadpage.parsers.NY;
 
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,8 +15,9 @@ public class NYRocklandCountyBParser extends SmartAddressParser {
   private static final Pattern ADDR_PTN = Pattern.compile("(.*?)   (?:(\\d{7}) - )?+(.*?)");
   
   public NYRocklandCountyBParser() {
-    super("ROCKLAND COUNTY","NY");
-    setFieldList("UNIT CALL ID PLACE ADDR X");
+    super(CITY_CODES, "ROCKLAND COUNTY","NY");
+    setFieldList("UNIT CALL ID PLACE ADDR APT CITY X");
+    setupGpsLookupTable(GPS_LOOKUP_TABLE);
   }
 
   @Override
@@ -28,8 +30,8 @@ public class NYRocklandCountyBParser extends SmartAddressParser {
     Matcher match = MASTER.matcher(body);
     if (!match.matches()) return false;
     data.strUnit = match.group(1);
-    String sAddr = match.group(2);
-    data.strCross = match.group(3);
+    String sAddr = match.group(2).trim();
+    data.strCross = match.group(3).trim();
     
     match = ADDR_PTN.matcher(sAddr);
     if (match.matches()) {
@@ -50,4 +52,77 @@ public class NYRocklandCountyBParser extends SmartAddressParser {
     }
     return true;
   }
+  
+  
+  @Override
+  protected String adjustGpsLookupAddress(String address) {
+    
+    // For now, they only have one set of coordinates for the Thruway on-ramp
+    if (address.startsWith("TWAY ")) return "TWAY";
+    return null;
+  }
+
+
+  private static final Properties GPS_LOOKUP_TABLE = buildCodeTable(new String[]{
+      "TWAY", "41.154582,-74.188689"
+  });
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "CAN", "Nanuet",
+      "CBA", "Bardonia",
+      "CCN", "Central Nyack",
+      "CCO", "Congers",
+      "CNC", "New City",
+      "CPR", "Pearl River",
+      "CRL", "Rockland Lake",
+      "CSV", "Spring Valley",
+      "CUN", "Central Nyack",
+      "CVC", "Valley Cottage",
+      "CWN", "West Nyack",
+      "HBM", "Bear Mountain",
+      "HGA", "Garnerville",
+      "HHA", "Haverstraw",
+      "HPO", "Pomona",
+      "HST", "Stony Point",
+      "HTH", "Thiells",
+      "HWH", "West Haverstraw",
+      "OBL", "Blauvelt",
+      "OGR", "Grandview-on-Hudson",
+      "ONA", "Nanuet",
+      "ONY", "Nyack",
+      "OOR", "Orangeburg",
+      "OPA", "Palisades",
+      "OPI", "Piermont",
+      "OPR", "Pearl River",
+      "OSN", "South Nyack",
+      "OSP", "Sparkill",
+      "OTP", "Tappan",
+      "OUG", "Upper Grandview",
+      "OWM", "West Nyack",
+      "RAR", "Airmont",
+      "RCR", "Chestnut Ridge",
+      "RHC", "Hillcrest",
+      "RHI", "Hillburn",
+      "RKA", "Kaser",
+      "RLA", "Ladentown",
+      "RMO", "Montebello",
+      "RMS", "Monsey",
+      "RNC", "New City",
+      "RNH", "New Hempstead",
+      "RNS", "New Square",
+      "RPO", "Pomona",
+      "RRA", "Ramapo",
+      "RSG", "Sloatsburg",
+      "RSL", "Sloatsburg",
+      "RSP", "Suffern",
+      "RSU", "Suffern",
+      "RSV", "Spring Valley",
+      "RTA", "Tallman",
+      "RVI", "Viola",
+      "RWE", "Wesley Hills",
+      "SBM", "Bear Mountain",
+      "SST", "Stony Point",
+      "STC", "Tomkins Cove"
+
+  });
 }
