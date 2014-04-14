c46c5f46a5d0c2c406815fcc5368943588ab6c56
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-04-14 08:47:27 +0000

    bunches of new test skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORClatsopCountyParserTest.java
@@ -0,0 +1,113 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BE;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Pyngfbc Pbhagl, BE
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Frnfvqr Sver & Erfphr
+Ybpngvba: Frnfvqr, BE, Havgrq Fgngrf
+Fraqre: pzvercbegvat@pvglbsfrnfvqr.hf
+Fraqre: Revp Jnyyre <rjnyyre@pvglbsfrnfvqr.hf>
+
+(Sver Pnyy) 317 6GU NIR/SVER VAIRFGVTNGVBA
+(Sver Pnyy) RZRET ZRQVPNY ERFCBAFR/FRVMHER
+(Sver Pnyy) 555 NIR T 17/ZIN,UVG & EHA pne if crq
+(Sver Pnyy) 554 Oebnqjnl/ FRVMVAT CG
+(Sver Pnyy) 88854 Uvtujnl 101/RZRET ZRQVPNY ERFCBAFR; znyr snyyra, whfg pnzr gb
+(Sver Pnyy) 1658 Juvfcrevat Cvarf/BQBE TNF/FZBXR/PURZVPNY/ tnf bqbe fzryy
+(Sver Pnyy) 3334 fhafrg oyiq/SVER VAIRFGVTNGVBA
+(Sver Pnyy) 111 Nir H/NFFVFG BGURE NTRAPL cbff fgebxr
+(JNGRE SYBJ NYNEZ) 1111 A EBBFRIRYG 100/NYNEZ SNYFR
+(2 IRU ZIN) OVT SBBG/UJL 101/ZBGBE IRU NPPVQRAG
+(CBFF FGEBXR) FHMNAAR RYVFR/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 1217 F Ebbfriryg Qe/RZRET ZRQVPNY ERFCBAFR; urnq vawhel
+(Sver Pnyy) 401 F Ebbfriryg Qe/RZRET ZRQVPNY ERFCBAFR; frvmher ol erfgebbzf
+(Sver Pnyy) 2161 F Senaxyva Fg/RZRET ZRQVPNY ERFCBAFR; ryqreyl srznyr cbff urneg nggnpx
+(Sver Pnyy) 401 F Ebbfriryg Qe/RZRET ZRQVPNY ERFCBAFR; znyr qbja, oernguvat
+(Sver Pnyy) 510 A Ubyynqnl Qe 2/RZRET ZRQVPNY ERFCBAFR; urneg nggnpx
+(Sver Pnyy) 545 Oebnqjnl/NYNEZ SVER; jngre sybj / qel sybj
+(GJB IRUVPYR NVP) YRJVF & PYNEX / UJL 101 A/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 101 Sberfg Qe/RZRET ZRQVPNY ERFCBAFR; snyy cngvrag, urnq vawhel, pbafpvbhf,
+(Sver Pnyy) 1189 F QBJAVAT/BQBE TNF/FZBXR/PURZVPNY
+(Sver Pnyy) 100 Oyx Oebnqjnl/RZRET ZRQVPNY ERFCBAFR/ Nffhnyg ivpvgz
+(Sver Pnyy) 201 Oebnqjnl/ fzbxr va ohzcre pnef
+(Sver Pnyy) 120 F SENAXYVA/
+(Sver Pnyy) 1120 Oebnqjnl/ onaq ebbz
+(Sver Pnyy) 414 A CEBZ 110/RZRET ZRQVPNY ERFCBAFR; sryy sebz 1fg sybbe onypbal, snpr ynprengvba
+(Sver Pnyy) 414 A CEBZ 110/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 2455 F EBBFRIRYG/NYNEZ SNYFR; 1fg sybbe chyy fgngvba, fbhgu raq
+(Sver Pnyy) 2455 F EBBFRIRYG/NYNEZ SNYFR
+(ZRQVPNY) 1021 F QBJAVAT 5/RZRET ZRQVPNY ERFCBAFR/URNEG VFFHR
+(ZRQVPNY PNYY) 2525 ARNJNAN FG/RZRET ZRQVPNY ERFCBAFR/BA SYBBE  QVSSVPHYGL OERNGUVAT
+(Sver Pnyy) 880 Nirahr V #2/RZRET ZRQVPNY ERFCBAFR; natvan, qvssvphygl oernguvat
+(900 OYX NIR V QHCYRK) PUVZARL SVER/
+(Sver Pnyy) 188 Fhafrg Oyiq CB OBK 24/SVER VAIRFGVTNGVBA/ SVG GRNZ ZRRG PNAABA  ORNPU SQ 10NZ
+(Sver Pnyy) UJL 101 / ANCN/ZBGBE IRU NPPVQRAG/ERCG ABA VAW
+(Sver Pnyy) 12GU / EBBF/BQBE TNF/FZBXR/PURZVPNY/FZRYY BS ANGHENY TNF
+(Sver Pnyy) 1703 12GU Nir 251/RZRET ZRQVPNY ERFCBAFR/9115 PBQR
+(ZRQVPNY) 1703 12GU Nir 20/RZRET ZRQVPNY ERFCBAFR/CBFF URNEG NGGNPX
+(Sver Pnyy) SNYPBA ENBQ SVER ZG/FGEHP SVER 1FG
+(Sver Pnyy) 741 nir o 5/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 1820 pbyhzovn/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 26 Nirahr N/NYNEZ SVER; mbar 1 trareny sver nynez
+(Sver Pnyy) 1921 F Ebbfriryg Qe/SVER VAIRFGVTNGVBA/ Genafsbezre nepvat naq fcnexvat
+(Sver Pnyy) 20 A JNUNAAN Eq 106/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 545 Oebnqjnl 311/RZRET ZRQVPNY ERFCBAFR/ Nffvfg zrqvk jvgu rkgevpngvba
+(Sver Pnyy) 1121 7gu Nir/RZRET ZRQVPNY ERFCBAFR/ Qvssvphygl oernguvat
+(Sver Pnyy) 2481 UVTUJNL 101 A A 10/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 1020 F PBYHZOVN 16/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 111 OEBNQJNL/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 2432 Cvar Fg/RZRET ZRQVPNY ERFCBAFR/ Cbffvoyr urneg nggnpx
+(ZNYR ABG OESRNGUVAT) 1125 F QBJAVAT/RZRET ZRQVPNY ERFCBAFR
+(FRVFHER CNGVRAG) 426 F UBYYNQNL/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 101 Sberfg Qe 164/RZRET ZRQVPNY ERFCBAFR; qvmml, yrsg nez cnva
+(Sver Pnyy) 2328 F Rqtrjbbq Fg/RZRET ZRQVPNY ERFCBAFR; qvssvphygl oernguvat
+(Sver Pnyy) 2601 Zvyy Perrx Ya 102/RZRET ZRQVPNY ERFCBAFR/ Nffvfg jvgu rkgevpngvba
+(Sver Pnyy) 111 Nir H/NYNEZ SVER / Qel Flfgrz Nggvp
+(Sver Pnyy) 725 F Jnunaan Eq 4/RZRET ZRQVPNY ERFCBAFR; pyvavp ebbz 4, ZV
+(Sver Pnyy) 9gu Nir/ Fhofgngvba/ Bhgyrg znyy/SVER VAIRFGVTNGVBA / Gryrcubar cbyr ba sver
+(Sver Pnyy) 414 A cebz/NYNEZ SVER onfrzrag fzbxr naq urng nynez
+(Sver Pnyy) Oebnqjnl Svryq/RZRET ZRQVPNY ERFCBAFR; hzcver uvg va urnq  jvgu ong
+(Sver Pnyy) 1460 OEBNQJNL/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 2500 F Ebbfriryg 144/RZRET ZRQVPNY ERFCBAFR; abg oernguvat
+(Sver Pnyy) 1318 A Ubyynqnl Qe 18/RZRET ZRQVPNY ERFCBAFR; qvssvphygl oernguvat
+(Sver Pnyy) 30 A Cebz/RZRET ZRQVPNY ERFCBAFR/ Nffvfg zrqvk jvgu rkgevpngvba
+(Sver Pnyy) 12gu / Ebbfriryg/RZRET ZRQVPNY ERFCBAFR / 1 iruvpyr ZIN ZBgbeplpyr
+(Sver Pnyy) 1530 F EBBFRIRYG Qe/JRYSNER PURPX
+(Sver Pnyy) 581 F CEBZ/NYNEZ SVER
+(Sver Pnyy) 250 1FG NIR/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 1372 10gu Nir/RZRET ZRQVPNY ERFCBAFR, zragny oernx, jrncbaf, CQ rae
+(Sver Pnyy) 1901 A UBYYNQNL QE/NYNEZ SVER  FRNFVQR UVTU FPUBBY
+(Sver Pnyy) 2500 F ebbfriryg/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 26 Nirahr N/NYNEZ SVER; Mbar 1 trareny sver nynez
+(Sver Pnyy) 130 13GU NIR/NYNEZ SVER
+(Sver Pnyy) 2500 F EBBFRIRYG QE/RZRET ZRQVPNY ERFCBAFR
+(Sver Pnyy) 725 F jnunaan eq/NYNEZ SVER
+(ERFPHR PNYY) 101 SBERFG QE 215/RZRET ZRQVPNY ERFCBAFR - srznyr cngvrag ybj oybbq cerffher - zrqvk erdhrfg
+(Sver Pnyy) 26 Nirahr N/NYNEZ SNYFR / Guvf vf bayl n GRFG
+(Sver Pnyy) 475 F Cebz #508/RZRET ZRQVPNY ERFCBAFR; pbqr 99
+(Sver Pnyy) 2511 Zvyy Perrx Ya #202/NYNEZ SVER/ sver nynez, zbvfgher frra
+(Sver Nynez) 150 Fbhgu Yvapbya\a \a     Revp Jnyyre\a     Frnfvqr Sver naq Erfphr\a \a     150 Fbhgu Yvapbya\a     Frnfvqr, BE 97138\a \a     Qrcg: 503.738.5420\a     Pryy: 503.739.0955\a     Snk: 503.717.9318\a \a     rjnyyre@pvglbsfrnfvqr.hf
+(grfg) Grfg\a \a     Revp Jnyyre\a     Frnfvqr Sver naq Erfphr\a \a     150 Fbhgu Yvapbya\a     Frnfvqr, BE 97138\a \a     Qrcg: 503.738.5420\a     Pryy: 503.739.0955\a     Snk: 503.717.9318\a \a     rjnyyre@pvglbsfrnfvqr.hf
+
+*/
+
+choyvp pynff BEPyngfbcPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BEPyngfbcPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "PYNGFBC PBHAGL", "BE");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+ 
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj BEPyngfbcPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
