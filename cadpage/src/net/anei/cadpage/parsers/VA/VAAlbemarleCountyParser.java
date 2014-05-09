eea23ed2df3e682950ca662a018353bf122a3b58
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-09 20:06:59 +0000

    Update sender filter for Albemarle County, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VAAlbemarleCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAAlbemarleCountyParser.java
@@ -23,7 +23,7 @@ public class VAAlbemarleCountyParser extends FieldProgramParser {
   
   @Override
   public String getFilter() {
-    return "CAD@acuecc.org";
+    return "CAD@acuecc.org,CAD2@acuecc.org ";
   }
 
   @Override
