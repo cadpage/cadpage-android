3ad71b69c069008ad03b963403c3878c2476f98d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-11 22:49:07 +0000

    Checking in latest Swedish language updates

--- cadpage/src/net/anei/cadpage/ManagePreferences.java
+++ cadpage/src/net/anei/cadpage/ManagePreferences.java
@@ -932,10 +932,10 @@ public class ManagePreferences {
    * @param sb StringBuilder object where message is constructed
    */
   public static void addConfigInfo(Context context, StringBuilder sb) {
-    sb.append(String.format("\n\n----------\nSysinfo - %s\nModel: %s\nStatus: %s/%s\n\n",
+    sb.append(String.format("\n\n----------\nSysinfo - %s\nModel: %s\nStatus: %s%s\n\n",
         Build.FINGERPRINT, Build.MODEL, 
         DonationManager.instance().status().toString(),
-        DonationManager.instance().isPaidSubscriber()?"Paid":""));
+        DonationManager.instance().isPaidSubscriber()?"/Paid":""));
     
     sb.append("Preference Configuration:\n");
 
