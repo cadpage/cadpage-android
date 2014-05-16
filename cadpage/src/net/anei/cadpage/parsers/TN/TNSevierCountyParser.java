579ab384db729941e860a5f926639e401dddfbbf
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-16 21:55:51 +0000

    still working on sevier county

---
+++ cadpage/src/net/anei/cadpage/parsers/TN/TNSevierCountyParser.java
@@ -0,0 +1,45 @@
+package net.anei.cadpage.parsers.TN;
+
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernPlusParser;
+
+
+public class TNSevierCountyParser extends DispatchSouthernPlusParser {
+  
+  
+  public TNSevierCountyParser() {
+    super(CITY_LIST, "SEVIER COUNTY", "TN", DSFLAG_FOLLOW_CROSS );
+  }
+  
+  @Override
+  public String getFilter() {
+    return "Central_Dispatch@mydomain.com";
+  }
+  
+  private static final String[] CITY_LIST = new String[]{
+
+    "ALDER BRANCH",
+    "BEECH SPRINGS",
+    "BOYDS CREEK",
+    "CATLETTSBURG",
+    "CATON",
+    "CHEROKEE HILLS",
+    "COUNTRY CASCADES",
+    "COSBY",
+    "DUPONT",
+    "GATLINBURG",
+    "KODAK",
+    "LAUREL",
+    "OLDHAM",
+    "PIGEON FORGE",
+    "PITTMAN CENTER",
+    "REAGANTOWN",
+    "SEVIERVILLE",
+    "SEYMOUR",
+    "SHADY GROVE",
+    "STRAWBERRY PLAINS",
+    "TRUNDLES CROSSROADS",
+    "WEARS VALLEY"
+    
+  };
+
+}
