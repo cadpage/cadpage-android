da85a53b77ce99f1c9f51c02107176d17ef495c8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-17 23:22:13 +0000

    Checking n Sevier County, TN

--- cadpage/src/net/anei/cadpage/parsers/TN/TNSevierCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TN/TNSevierCountyParser.java
@@ -1,10 +1,14 @@
 package net.anei.cadpage.parsers.TN;
 
-import net.anei.cadpage.parsers.dispatch.DispatchSouthernPlusParser;
+import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernPlusParser;
 
 public class TNSevierCountyParser extends DispatchSouthernPlusParser {
   
+  private static final Pattern UNIT_PTN = Pattern.compile("\\d+ \\*\\*\\* .*");
+  private static final Pattern APT_PLACE_PTN = Pattern.compile("[^ ]+ +[^ ]+ +.*");
   
   public TNSevierCountyParser() {
     super(CITY_LIST, "SEVIER COUNTY", "TN", DSFLAG_LEAD_PLACE | DSFLAG_FOLLOW_CROSS );
@@ -15,6 +19,30 @@ public class TNSevierCountyParser extends DispatchSouthernPlusParser {
     return "Central_Dispatch@mydomain.com";
   }
   
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!super.parseMsg(subject, body, data)) return false;
+    if (UNIT_PTN.matcher(data.strSupp).matches()) {
+      data.strUnit = data.strSupp.replace("***", "/");
+      data.strSupp = "";
+    } else if (UNIT_PTN.matcher(data.strCall).matches()) {
+      data.strUnit = data.strCall.replace("***", "/");
+      data.strCall = "ALERT";
+    }
+    data.strApt = stripFieldEnd(data.strApt, "AREA OF");
+    data.strAddress = stripFieldEnd(data.strAddress, "AREA OF");
+    if (data.strPlace.length() == 0 && APT_PLACE_PTN.matcher(data.strApt).matches()) {
+      data.strPlace = data.strApt;
+      data.strApt = "";
+    }
+    return true;
+  }
+  
+  @Override
+  public String getProgram() {
+    return super.getProgram() + " UNIT";
+  }
+
   private static final String[] CITY_LIST = new String[]{
 
     "ALDER BRANCH",
