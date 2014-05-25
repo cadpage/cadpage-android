7400e6e71698ec106ce8629c9efaf0487f7928a3
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-25 15:19:19 +0000

    Checking in Chocktaw County, OK

---
+++ cadpage/src/net/anei/cadpage/parsers/OK/OKChoctawCountyParser.java
@@ -0,0 +1,32 @@
+package net.anei.cadpage.parsers.OK;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
+
+
+public class OKChoctawCountyParser extends SmartAddressParser {
+  
+  public OKChoctawCountyParser() {
+    super("CHOCTAW COUNTY", "OK");
+    setFieldList("CALL ADDR APT INFO");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "405261,81370";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (!body.startsWith(": ")) return false;
+    if (!body.endsWith(" - From: OCSO Dispatch")) return false;
+    body = body.substring(2,body.length()-22).trim();
+    parseAddress(StartType.START_CALL, body, data);
+    if (data.strCall.length() == 0) {
+      data.strCall = getLeft();
+    } else {
+      data.strSupp = getLeft();
+    }
+    return (data.strCall.length() > 0 && data.strAddress.length() > 0);
+  }
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OK/OKChoctawCountyParserTest.java
@@ -0,0 +1,43 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BX;
+
+vzcbeg bet.whavg.Grfg;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+/*
+Pubpgnj Pbhagl, BX
+Pbagnpg: Enl Jnyynpr <fragelzrpunavp@tznvy.pbz>'
+Fraqre: 8137012060,4057960363,8137012064,4052615639,4052615640,4052615644,4052615647
+
+1/1: GRFG CNTR FGNEG GB 2831 A. URAARL - Sebz: BPFB Qvfcngpu 
+1/1: 4601 A PUBPGNJ EQ RBP SVER NPNQRZL 21/Z SNYY 10SG BSS YNQQRE - Sebz: BPFB Qvfcngpu
+
+ */
+
+choyvp pynff BXPubpgnjPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BXPubpgnjPbhaglCnefreGrfg() {
+    frgCnefre(arj BXPubpgnjPbhaglCnefre(), "PUBPGNJ PBHAGL", "BX");
+  }
+ 
+  
+  @Grfg
+  choyvp ibvq grfgEnlJnyynpr() {
+
+
+    qbGrfg("G1",
+        "1/1: GRFG CNTR FGNEG GB 2831 A. URAARL - Sebz: BPFB Qvfcngpu ",
+        "PNYY:GRFG CNTR FGNEG GB",
+        "NQQE:2831 A URAARL");
+
+    qbGrfg("G2",
+        "1/1: 4601 A PUBPGNJ EQ RBP SVER NPNQRZL 21/Z SNYY 10SG BSS YNQQRE - Sebz: BPFB Qvfcngpu",
+        "PNYY:RBP SVER NPNQRZL 21/Z SNYY 10SG BSS YNQQRE",
+        "NQQE:4601 A PUBPGNJ EQ");
+ }
+
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj BXPubpgnjPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
