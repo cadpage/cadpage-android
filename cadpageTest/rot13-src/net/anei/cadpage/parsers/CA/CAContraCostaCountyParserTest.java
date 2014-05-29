4432319b2c94b3f7b9dd5a84e60727dc8e493fab
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-05-29 21:25:32 +0000

    new skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CA/CAContraCostaCountyParserTest.java
@@ -0,0 +1,131 @@
+cnpxntr arg.narv.pnqcntr.cnefref.PN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Pbagen Pbfgn Pbhagl, PN
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pbagen Pbfgn Sver Cebgrpgvba Qvfg
+Ybpngvba: Cyrnfnag Uvyy, PN, Havgrq Fgngrf
+
+VASB CNTR Sver-Fgehp Pbzzy 719 Green Pnyvsbeavn Qe Jnyahg Perrx D101,R103,R115,R117,G141,R105,
+VASB CNTR *RZF-CQ P3 1581 Pbapbeq Nir Pbapbeq NZEN310,FD6
+VASB CNTR *RZF-CQ P3 5450 Ybhvfvnan Qe Pbapbeq NZEN310,R122
+VASB CNTR RZF-Qrygn 4756 Pynlgba Eq Pbapbeq NZEN301,R108
+VASB CNTR RZF-Qrygn 3332 Zvyqerq Ya Ynsnlrggr NZEN305,R117
+VASB CNTR *RZF-CQ P3 3601 Qrre Uvyy Eq Ynsnlrggr NZEN3,R115
+VASB CNTR RZF-Qrygn 1724 Bnxzbag Qe Jnyahg Perrx NZEN302,R103
+VASB CNTR Nynez-Zrqvpny 95 Ynf Ybznf Jnl Jnyahg Perrx NZEN3,R107
+VASB CNTR Nynez-Zrqvpny 2641 Tbyqra Enva Eq Jnyahg Perrx NZEN305,R103
+VASB CNTR RZF-Qrygn 3417 Fragvary Qe Znegvarm NZEN3,R113
+VASB CNTR Sver-Fgehp Pbzzy 5961 Nhghzajbbq Qe Jnyahg Perrx G106,R103,R115,R117,D184,R105,
+VASB CNTR RZF-Puneyvr 2901 Rfghqvyyb Fg Znegvarm NZEN310,D114
+VASB CNTR RZF-Qrygn 2500 Ongrf Nir Pbapbeq NZEN304
+VASB CNTR Nynez-Zrqvpny 1250 Ubbxfgba Eq Pbapbeq NZEN3,R105
+Grfg cntr 1150
+VASB CNTR Nynez-Zrqvpny 1549 Bnxzbag Qe Jnyahg Perrx NZEN304,R103
+VASB CNTR RZF-Qrygn 13 Gur Gerrf Qe Pbapbeq NZEN3,R105
+VASB CNTR *RZF-CQ P3 80 J Ubbxfgba Eq Cyrnfnag Uvyy NZEN304,R102
+VASB CNTR RZF-RPUB 2301 - 2399 Pbagen Pbfgn Oyiq Cyrnfnag Uvyy NZEN304,R105
+VASB CNTR RZF-Qrygn 2807 Rnfgtngr Nir Pbapbeq NZEN304
+VASB CNTR RZF-Puneyvr 1203 Evqtrivrj Cy Cyrnfnag Uvyy NZEN305,R113
+VASB CNTR RZF-Puneyvr 2024 Ylyr Pg Znegvarm NZEN305,R109
+VASB CNTR RZF-Qrygn 112 Yn Pnfn Ivn Jnyahg Perrx NZEN304,R105
+VASB CNTR RZF-Puneyvr 1400 Zbagrtb Jnyahg Perrx NZEN304,D101
+VASB CNTR RZF-Puneyvr 25 Nyyra Fg Znegvarm NZEN305,D114
+VASB CNTR RZF-RPUB 2028 Qnyvf Qe Pbapbeq NZEN3
+VASB CNTR RZF-Qrygn 736 Puneygba Qe Cyrnfnag Uvyy NZEN302,R109
+VASB CNTR RZF-Qrygn 1580 Trnel Eq Jnyahg Perrx NZEN303,R102
+VASB CNTR Nynez-Zrqvpny 1232 Fxlperfg Qe Jnyahg Perrx NZEN301,R115
+VASB CNTR RZF-Qrygn 997 Green Pnyvsbeavn Qe Jnyahg Perrx NZEN303,D101
+VASB CNTR RZF-Puneyvr 5450 Ybhvfvnan Qe Pbapbeq NZEN302,R122
+VASB CNTR *RZF-Craq CebDN 5450 Ybhvfvnan Qe Pbapbeq NZEN302,R122
+VASB CNTR RZF-Qrygn 2166 Fgvyyfcevat Cy Znegvarm NZEN3,R109
+VASB CNTR RZF-RPUB 918 Onapebsg Eq Jnyahg Perrx NZEN303,R110
+VASB CNTR RZF-Puneyvr 1580 Trnel Eq Jnyahg Perrx NZEN304,R102
+VASB CNTR *RZF-CQ P3 204 Serqn Qe Cnpurpb CZ147,R109,NZEN201
+VASB CNTR Nynez-Zrqvpny 735 Fna Whna Pg Pbapbeq NZEN3,R110
+VASB CNTR RZF-Qrygn 1121 Fhfnan Fg Znegvarm NZEN304,D114
+VASB CNTR *RZF-CQ P3 4475 Gerng Oyiq Pbapbeq NZEN302,R108
+VASB CNTR RZF-Puneyvr 175 Pyrnirynaq Eq Cyrnfnag Uvyy NZEN303,R105
+VASB CNTR RZF-Qrygn 2047 Neabyq Vaqhfgevny Jnl Pbapbeq NZEN301,R109
+VASB CNTR RZF-Qrygn 1500 Zbahzrag Oyiq Pbapbeq NZEN3,R105
+VASB CNTR RZF-Puneyvr 3940 Ybf Nenovf Qe Ynsnlrggr NZEN3,R143
+VASB CNTR RZF-Qrygn 1713 Tvyneql Qe Pbapbeq NZEN304,R110
+VASB CNTR Npp-Ovplpyr Zbue Ya / Zbahzrag Oyiq Pbapbeq NZEN303,G106
+VASB CNTR RZF-Qrygn 1050 Fna Zvthry Eq Pbapbeq NZEN305
+VASB CNTR RZF-Qrygn 1776 Neabyq Vaqhfgevny Jnl Pbapbeq NZEN301,R109
+VASB CNTR RZF-Qrygn 1314 Ynav Xnv Qe Pbapbeq NZEN3,G106
+VASB CNTR RZF-Qrygn 36 Onv Tbeel Cy Jnyahg Perrx NZEN303,R110
+VASB CNTR RZF-Qrygn 4272 Tynmvre Pg Pbapbeq NZEN304,R108
+VASB CNTR *RZF-CQ P3 1365 Gerng Oyiq Jnyahg Perrx NZEN3,R102
+VASB CNTR RZF-Qrygn 3503 Cnpurpb Oyiq Znegvarm CZ142,NZEN303,D114
+VASB CNTR RZF-Qrygn 3503 Cnpurpb Oyiq Znegvarm NZEN303,D114
+VASB CNTR RZF-Qrygn 2222 Cnpurpb Fg Pbapbeq NZEN3,FD6
+VASB CNTR RZF-Puneyvr 2065 Fna Ivapragr Qe Pbapbeq NZEN303,G106
+VASB CNTR RZF-Puneyvr 0 Jvyybj Cnff Eq Pbapbeq NZEN305,R109
+VASB CNTR RZF-Puneyvr 1851 Fhggre Fg Pbapbeq NZEN304
+VASB CNTR *RZF-CQ P3 175 Pyrnirynaq Eq Cyrnfnag Uvyy NZEN302,R105
+Sebz-QVFCNGPU:Grfg cntr bayl. /f/ 1150
+VASB CNTR RZF-Puneyvr 2209 Tbyqra Enva Eq Jnyahg Perrx NZEN3,R103 C2-Rzretrapl
+VASB CNTR RZF-Qrygn 2000 Xvexre Cnff Eq Pbapbeq NZEN3 C1-Rzretrapl
+VASB CNTR RZF-Qrygn Ltanpvb Inyyrl Eq / Bnx Tebir Eq Jnyahg Perrx NZEN303,R107 C1-Rzretrapl
+VASB CNTR Nynez-Zrqvpny 3512 Zbentn Oyiq Ynsnlrggr NZEN3,R115
+VASB CNTR Npp-Iru Jvyybj Cnff Eq / Snez Ohernh Eq Pbapbeq NZEN305
+VASB CNTR RZF-Puneyvr 98 Tvyevk Qe Znegvarm NZEN3,D114
+VASB CNTR RZF-Qrygn 3146 Bnx Eq Jnyahg Perrx NZEN302,R102
+VASB CNTR RZF-Qrygn 3736 Oba Ubzzr Jnl Pbapbeq NZEN302
+VASB CNTR *RZF-CQ P3 2833 Cgnezvtna Qe Jnyahg Perrx NZEN301
+VASB CNTR *RZF-Craq CebDN 1011 Fnagn Sr Nir Znegvarm NZEN3,D114 N pryyhyne er-ovq unf bppheerq, purpx gur NAV/NYV Ivrjre sbe qrgnvyf,RZF: SBE Y
+VASB CNTR RZF-Qrygn 1224 Furevqna Eq Pbapbeq NZEN305 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,[Ce RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,[CebDN Fpevcg] Qvfcngpu pbqr
+VASB CNTR RZF-Qrygn 2015 Zg Qvnoyb Oyiq Jnyahg Perrx NZEN302,D101 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: JNYAHG PERRX C RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: JNYAHG PERRX CQ,[CebDN Fpe
+VASB CNTR RZF-Puneyvr 1309 Enzfnl Pve Jnyahg Perrx NZEN303,R102 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: JNYAHG PERRX C RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: JNYAHG PERRX CQ,[CebDN Fpevc
+VASB CNTR RZF-Qrygn 540 Cnggrefba Oyiq Cyrnfnag Uvyy NZEN3,R105 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: CYRNFNAG UVYY RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: CYRNFNAG UVYY CQ,RZF: PBAGEN
+VASB CNTR *RZF-Craq CebDN 1071 Evqtr Cnex Qe Pbapbeq CZ157,NZEN3,G106 SQ: DHREL PNYYRE,RZF: SBE YBPNGVBA,CQ: PBAPBEQ CQ,srznyr fnlvat fur pnag oern SQ: DHREL PNYYRE,RZF: SBE YBPNGVBA,CQ: PBAPBEQ CQ,srznyr fnlvat fur pnag oerngur..,[N
+VASB CNTR *RZF-CQ P3 1071 Evqtr Cnex Pg Pbapbeq NZEN3 SQ: DHREL PNYYRE,RZF: SBE YBPNGVBA,CQ: PBAPBEQ CQ,srznyr fnlvat fur pnag oern SQ: DHREL PNYYRE,RZF: SBE YBPNGVBA,CQ: PBAPBEQ CQ,srznyr fnlvat fur pnag oerngur..,[Nccraqrq, 02:52:4
+VASB CNTR RZF-Qrygn 1053 Zbue Ya Pbapbeq NZEN301,R105 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,[Ce RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,[CebDN Fpevcg] Qvfcngpu pbq
+VASB CNTR RZF-Qrygn 2047 Neabyq Vaqhfgevny Jnl Pbapbeq NZEN305,R109 [CebDN Fpevcg] Qvfcngpu pbqr: 06Q02 Lbh ner erfcbaqvat gb n cngvrag jvgu oerngu [CebDN Fpevcg] Qvfcngpu pbqr: 06Q02 Lbh ner erfcbaqvat gb n cngvrag jvgu oernguvat ceboyr
+VASB CNTR *RZF-CQ P3 3700 Cbeg Puvpntb Ujl Pbapbeq NZEN304,FD6 srznyr snyy pbaf/oernguvat oyrrqvat sebz sbejneq,fgngvba ntragf obbgu,Guvf vapvq srznyr snyy pbaf/oernguvat oyrrqvat sebz sbejneq,fgngvba ntragf obbgu,Guvf vapvqrag 14037562 un
+VASB CNTR RZF-Qrygn 199 Znpnyirl Qe Znegvarm NZEN301,R113 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: ZNEGVARM CQ,[C RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: ZNEGVARM CQ,[CebDN Fpevcg] Qvfcngp
+VASB CNTR Sver-Fgehp-Erf 1936 Angbzn Qe Pbapbeq G106,R108,R186,R105,R109,OP2,O RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,RZF ZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: PBAPBEQ CQ,RZF
+VASB CNTR *RZF-CQ P3 3118 Ebffzbbe Cxjl Jnyahg Perrx NZEN305,R103 bireqbfr - cq ba fprar,Guvf vapvqrag 14037535 unf orra frag gb ZGA ivn gur PNQ2P bireqbfr - cq ba fprar,Guvf vapvqrag 14037535 unf orra frag gb ZGA ivn gur PNQ2PNQ Vagresnpr
+VASB CNTR RZF-Qrygn 5431 Pynlgba Eq Pynlgba NZEN306,R108 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,[CebDN Fpev RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,[CebDN Fpevcg] Qvfcngpu pbqr: 26
+VASB CNTR Nvecbeg Nyeg 3-Penfu 171 Wbua Tyraa Qe Pbapbeq R109,G106,R105,D101,OP2,OP1,NZ [Nqqerff: 171 Wbua Tyraa Qe] [Zrqvhz] [HAXABJA] ABGVSL NVECBEG SVER BS NAL VAPV (24UEF) 646-5723, GBJRE - 685-5744, SVRYQ PRYY 0700-2300 - 383-0005,p
+VASB CNTR RZF-Qrygn 1911 Bnx Cnex Oyiq Cyrnfnag Uvyy NZEN302,R102 RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: CYRNFNAG UVYY RZF: PBAGEN PBFGN SVER - PRAG,SQ: PBAGEN PBFGN SVER - PRAG,CQ: CYRNFNAG UVYY CQ,RZF: PBAG
+VASB CNTR RZF-Puneyvr 2059 Fna Ivapragr Qe Pbapbeq NZEN3,FD6 [CebDN Fpevcg] Qvfcngpu pbqr: 31P02 Lbh ner erfcbaqvat gb n cngvrag jub vf hapb [CebDN Fpevcg] Qvfcngpu pbqr: 31P02 Lbh ner erfcbaqvat gb n cngvrag jub vf hapbafpvbhf (be unf s
+VASB CNTR RZF-Qrygn 1870 Bylzcvp Oyiq Jnyahg Perrx NZEN305,D101 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,[CebDN Fpev RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,[CebDN Fpevcg] Qvfcngpu p
+VASB CNTR RZF-Qrygn 4756 Pynlgba Eq Pbapbeq NZEN304 [Cerzvfr: 4756 Pynlgba Eq] [Zrqvhz] [HAXABJA] Ncg #240 - cyf pnyy Bsp. Byyvr Fn [Cerzvfr: 4756 Pynlgba Eq] [Zrqvhz] [HAXABJA] Ncg #240 - cyf pnyy Bsp. Byyvr Fnafra (925-639-3339) sbe na
+VASB CNTR RZF-Puneyvr 4756 Pynlgba Eq Pbapbeq NZEN304,R108 [Cerzvfr: 4756 Pynlgba Eq] [Zrqvhz] [HAXABJA] Ncg #240 - cyf pnyy Bsp. Byyvr Fn [Cerzvfr: 4756 Pynlgba Eq] [Zrqvhz] [HAXABJA] Ncg #240 - cyf pnyy Bsp. Byyvr Fnafra (925-639-3339)
+VASB CNTR RZF-Qrygn 320 1fg Nir F Cnpurpb CZ152,R109,NZEN302 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: ZNEGVARM CQ,[CebDN Fpevcg] Qvfcngpu pb RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: ZNEGVARM CQ,[CebDN Fpevcg] Qvfcngpu pbqr: 11R01 Lbh ne
+VASB CNTR RZF-RPUB 320 1fg Nir F Cnpurpb NZEN302,R109 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: ZNEGVARM CQ,[CebDN Fpevcg] Qvfcngpu pb RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: ZNEGVARM CQ,[CebDN Fpevcg] Qvfcngpu pbqr: 11R01 Lbh ner erfcb
+VASB CNTR RZF-Puneyvr 3130 Flpnzber Fg Znegvarm NZEN302,D114 N pryyhyne er-ovq unf bppheerq, purpx gur NAV/NYV Ivrjre sbe qrgnvyf,RZF: SBE Y N pryyhyne er-ovq unf bppheerq, purpx gur NAV/NYV Ivrjre sbe qrgnvyf,RZF: SBE YBPNGVBA,SQ: DHRE
+VASB CNTR RZF-Puneyvr 125 Naqrefba Jnl Znegvarm NZEN310,R109 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,N pryyhyne RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PUC TT - PBAGEN PBFGN NERN,N pryyhyne er-ovq unf bppheer
+VASB CNTR RZF-Qrygn PYNLGBA EQ gb XVEXRE CNFF EQ / Xvexre Cnff Eq Pynlgba NZEN302,R122 RZF: SBE YBPNGVBA,SQ: DHREL PNYYRE,CQ: PBAPBEQ CQ,[CebDN Fpevcg] Qvfcngpu pbq baqvat gb n cngvrag vaibyirq va n snyy. Gur cngvrag vf n 4-lrne-byq z
+VASB CNTR RZF-Puneyvr Trnel Eq / Qbebgul Qe Cyrnfnag Uvyy NZEN304,R102
+VASB CNTR Pbapbeq Sver-Fgehp Pbzzy 1160 Zrnqbj Ya |Pvgl: FD6,GP10,D114,R107,R108,R122,O |YNG: 37958959 |YBA: 122041246 14037369 SVER - Fgehpgher Pbzzrepvny
+QVFC:GRFG 2013 Trnel Eq Jnyahg Perrx1150,1150,1150,1150,1150,1150,YNEXRL YA/QBEBGUL QE
+QVFC:GRFG 2013 Trnel Eq Jnyahg Perrx1150,1150,1150,1150,1150,1150,YNEXRL YA/QBEBGUL QE
+QVFC:GRFG 2013 Trnel Eq Jnyahg Perrx1150,1150,1150,1150,1150,1150,YNEXRL YA/QBEBGUL QE
+QVFC:GRFG 2013 Trnel Eq Jnyahg Perrx
+QVFC:GRFG 2013 Trnel Eq Jnyahg Perrx
+
+ */
+
+choyvp pynff PNPbagenPbfgnPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp PNPbagenPbfgnPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "PBAGEN PBFGN PBHAGL", "PN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+  }
+ 
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj PNPbagenPbfgnPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
