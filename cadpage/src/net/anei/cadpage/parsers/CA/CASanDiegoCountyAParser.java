2f30688dc2c88d610e23d16e83f9ba16728ebcac
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-01 15:46:37 +0000

    Fixed parsing problem with San Diego County, CA (A)

--- cadpage/src/net/anei/cadpage/parsers/CA/CASanDiegoCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/CA/CASanDiegoCountyAParser.java
@@ -10,10 +10,13 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  * San Diego County, CA
  */
 public class CASanDiegoCountyAParser extends FieldProgramParser {
+  
+  private static final Pattern RUN_REPORT_PTN = Pattern.compile("INFO:.*\\\\DISP:.*");
 
   public CASanDiegoCountyAParser() {
     super("SAN DIEGO COUNTY", "CA",
-           "ALM:PRI! MSTR_INC#:ID! TYP:CALL! ADR:ADDR! LOC:PLACE! SP:APT% TB:MAP% MAP:MAP% XST:X% TG:CH% TIMEDSP:TIME% UNITS:UNIT% TXT:INFO LOC_CMNTS:INFO");
+           "( ADVISE ALRM:PRI! TYP:CALL! ADDR:ADDR! LOC:PLACE! APT-SP:APT% XST:X% TIMEDSP:TIME% UNITS:UNIT% | " +
+             "ALM:PRI? MSTR_INC#:ID? TYP:CALL! ADR:ADDR! LOC:PLACE! XST:X? SP:APT% TB:MAP% MAP:MAP% TG:CH% TIMEDSP:TIME? UNITS:UNIT INFO+ LOC_CMNTS:INFO+ )");
   }
 
   @Override
@@ -24,6 +27,11 @@ public class CASanDiegoCountyAParser extends FieldProgramParser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("CAD MESSAGE")) return false;
+    if (RUN_REPORT_PTN.matcher(body).matches()) {
+      data.strCall = "RUN REPORT";
+      data.strPlace = body;
+      return true;
+    }
     return parseFields(body.split("\\\\"), data);
   }
   
@@ -32,14 +40,50 @@ public class CASanDiegoCountyAParser extends FieldProgramParser {
     return MAP_FLG_SUPPR_LA;
   }
 
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ADVISE")) return new MyAdvisePageField();
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("APT")) return new MyAptField();
+    if (name.equals("MAP")) return new MyMapField();
+    if (name.equals("CH")) return new MyChannelField();
+    if (name.equals("UNIT")) return new MyUnitField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private class MyAdvisePageField extends CallField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!field.equals("***ADVISORY PAGE***")) return false;
+      data.strCall = "ADVISORY";
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+  
+  private class MyCallField extends CallField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strCall = append(data.strCall, " - ", field);
+    }
+  }
+  
   private static final Pattern APT_TITLE_PTN = Pattern.compile("^(?:APT|SUITE|SPT?|RM) *", Pattern.CASE_INSENSITIVE);
-
   private class MyAptField extends AptField {
     @Override
     public void parse(String field, Data data) {
       Matcher match = APT_TITLE_PTN.matcher(field);
-      if (match.find())
-        field = field.substring(match.end());
+      if (match.find()) field = field.substring(match.end());
       super.parse(field, data);
     }
   }
@@ -54,8 +98,15 @@ public class CASanDiegoCountyAParser extends FieldProgramParser {
   private class MyChannelField extends ChannelField {
     @Override
     public void parse(String field, Data data) {
-      if (field.startsWith("."))
-        field = field.substring(1).trim();
+      field = stripFieldStart(field, ".");
+      super.parse(field, data);
+    }
+  }
+
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, ",");
       super.parse(field, data);
     }
   }
@@ -64,17 +115,10 @@ public class CASanDiegoCountyAParser extends FieldProgramParser {
   private class MyInfoField extends InfoField {
     @Override
     public void parse(String field, Data data) {
+      field = stripFieldStart(field, "TXT:[1]");
       field = SKIP_PTN.matcher(field).replaceAll("");
       super.parse(field, data);
     }
   }
 
-  @Override
-  public Field getField(String name) {
-    if (name.equals("APT")) return new MyAptField();
-    if (name.equals("MAP")) return new MyMapField();
-    if (name.equals("CH")) return new MyChannelField();
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
 }
