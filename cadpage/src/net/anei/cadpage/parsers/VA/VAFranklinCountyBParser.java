eff5327d2cf8be97d23b0018b915ae805a005ca1
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-05 05:38:37 +0000

    Checking in new  Franklin County, VA location

---
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAFranklinCountyBParser.java
@@ -0,0 +1,44 @@
+package net.anei.cadpage.parsers.VA;
+
+import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
+
+/**
+ * Franklin County, VA
+ */
+public class VAFranklinCountyBParser extends DispatchSouthernParser {
+  
+  public VAFranklinCountyBParser() {
+    super(CITY_LIST, "FRANKLIN COUNTY", "VA", DSFLAG_NO_NAME_PHONE);
+  }
+      
+  
+  @Override
+  public String getFilter() {
+    return "911@FCSO.COM";
+  }
+  
+  private static String[] CITY_LIST = new String[]{
+
+    // Cities
+    "BOONES MILL",
+    "ROCKY MOUNT",
+
+    // Unincorporated communities
+    "BENT MOUNTAIN",
+    "CALLAWAY",
+    "FERRUM",
+    "GLADE HILL",
+    "HALES FORD",
+    "NORTH SHORE",
+    "PENHOOK",
+    "REDWOOD",
+    "SNOW CREEK",
+    "UNION HALL",
+    "WESTLAKE CORNER",
+    
+    // Bedford County
+    "MONETA"
+
+  };
+
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/VA/VAFranklinCountyBParserTest.java
@@ -0,0 +1,60 @@
+cnpxntr arg.narv.pnqcntr.cnefref.IN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Senaxyva Pbhagl, IN
+
+Pbagnpg: Wvz Fjna <wfjnarkcerffgex@tznvy.pbz>
+Fraqre: 911@SPFB.PBZ
+911@SPFB.PBZ:560 FPHSSYVAT UVYY EQ EBPXL ZBHAG;2014-024771;18:31:13;PURFG CNVA;trb:36.984397, -79.896364
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Fpehttf Sver Erfphr Qvir 
+Ybpngvba: Zbargn, IN, Havgrq Fgngrf
+Fraqre: <911@SPFB.PBZ>
+
+911@SPFB.PBZ:8254 FPEHTTF EQ ZBARGN;2014-024793;20:06:37;ONPX VAWHEL;trb:37.046963, -79.621372
+
+*/
+choyvp pynff INSenaxyvaPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp INSenaxyvaPbhaglOCnefreGrfg() {
+    frgCnefre(arj INSenaxyvaPbhaglOCnefre(), "SENAXYVA PBHAGL", "IN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgWvzFjna() {
+
+    qbGrfg("G1",
+        "911@SPFB.PBZ:560 FPHSSYVAT UVYY EQ EBPXL ZBHAG;2014-024771;18:31:13;PURFG CNVA;trb:36.984397, -79.896364",
+        "NQQE:560 FPHSSYVAT UVYY EQ",
+        "PVGL:EBPXL ZBHAG",
+        "VQ:2014-024771",
+        "GVZR:18:31:13",
+        "PNYY:PURFG CNVA",
+        "TCF:+36.984397,-79.896364");
+    
+  }
+
+  @Grfg
+  choyvp ibvq grfgFpehttfSverErfphrQvir() {
+
+    qbGrfg("G1",
+        "911@SPFB.PBZ:8254 FPEHTTF EQ ZBARGN;2014-024793;20:06:37;ONPX VAWHEL;trb:37.046963, -79.621372",
+        "NQQE:8254 FPEHTTF EQ",
+        "PVGL:ZBARGN",
+        "VQ:2014-024793",
+        "GVZR:20:06:37",
+        "PNYY:ONPX VAWHEL",
+        "TCF:+37.046963,-79.621372");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat netf[]) {
+    arj INSenaxyvaPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
+  
