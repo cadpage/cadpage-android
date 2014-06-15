b957f5ca294d14a756723407c29851d9da88d76e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-15 17:15:44 +0000

    Fixed parsing problem with Salt Lake County, UT

--- cadpage/src/net/anei/cadpage/parsers/UT/UTSaltLakeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/UT/UTSaltLakeCountyParser.java
@@ -6,7 +6,7 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class UTSaltLakeCountyParser extends FieldProgramParser {
 
   public UTSaltLakeCountyParser() {
-    super("SALT LAKE COUNTY", "UT", "ID! DATETIME! CALL! ( PLACE ADDR/Z X/Z SRC! | ADDR/Z SRC! | ADDR/Z X SRC! | PLACE ADDR/Z SRC! )");
+    super("SALT LAKE COUNTY", "UT", "ID! DATETIME! CALL! ( PLACE ADDR/Z APT! X/Z? SRC! | ADDR/Z APT! X/Z? SRC! | PLACE ADDR/Z X/Z SRC! | ADDR/Z SRC! | ADDR/Z X SRC! | PLACE ADDR/Z SRC! ) CODE INFO+");
   }
 
   @Override
@@ -27,9 +27,19 @@ public class UTSaltLakeCountyParser extends FieldProgramParser {
   public Field getField(String name) {
     if (name.equals("ID")) return new IdField("[A-Z]{2,3}\\d+", true);
     if (name.equals("DATETIME")) return new DateTimeField("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}", true);
+    if (name.equals("APT")) return new AptField("Unit +(.*)", true);
     if (name.equals("X")) return new CrossField("\\d+ [NSEW](?: - \\d+ [NSEW])?", true);
     if (name.equals("SRC")) return new SourceField("CF|SF", true);
+    if (name.equals("CODE")) return new CodeField("\\d{1,2}[A-Z]\\d{1,2}[A-Z]?", true);
+    if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
   }
-
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldEnd(field, "-");
+      super.parse(field, data);
+    }
+  }
 }
