3807fbeb0c1576ff3940014b52b758828c126744
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-16 03:37:33 +0000

    Checking in Nassau County, NY (M)

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NY/NYNassauCountyMParserTest.java
@@ -0,0 +1,131 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AL;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+ALAnffnhPbhagl, AL (Z)
+
+Pbagnpg: wnpxfgre2024 <wnpxfgre2024@ubgznvy.pbz>
+Fraqre: nynezf@bprnafvqrsq.arg
+
+1 bs 2\aSEZ:nynezf@bprnafvqrsq.arg\aZFT:GBN: 10:47 06-13-14 \aTRA - Nhgbzngvp Nynez \a3387 YNJERAPR NIR ,BPRNAFVQR\aYHVF QVNM \a\aPF: J\a(Pba'g) 2 bs 2\aURAEVRGGN NIR / PBAPBEQ NIR \a2014-000712(Raq)
+SEZ:nynezf@bprnafvqrsq.arg\aZFT:GBN: 13:12 06-13-14 \aNHGB - Nhgb Nppvqrag \aBPRNAFVQR EQ ,BPRNAFVQR\a  \a\aPF: URAEVRGGN NIR  2014-000714
+ 1 bs 2\aSEZ:nynezf@bprnafvqrsq.arg\aZFT:GBN: 18:24 06-15-14 \aPB - Ab Ercbegrq Nvqrq \a3325 ZHEQBPX NIR ,BPRNAFVQR\aFCVGMRE \a\aPF: ONZOEVPX FG\a(Pba'g) 2 bs 2\a/ QRNQ RAQ \a2014-000727(Raq)
+ 1 bs 2\aSEZ:nynezf@bprnafvqrsq.arg\aZFT:GBN: 18:51 06-15-14 \aTRA - Trareny Nynez \aTNF YRNX \a3116 YNJFBA OYIQ ,BPRNAFVQR\a  \a\aPF: NAPUBE\a(Pba'g) 2 bs 2\aNIR / J JVAQFBE CXL \a2014-000728(Raq)
+SEZ:nynezf@bprnafvqrsq.arg\aZFT:GBN: 21:35 06-15-14 \aNHGB - Nhgb Nppvqrag \aJ JVAQFBE CXL ,BPRNAFVQR\a  \a\aPF: YBAT ORNPU EQ  2014-000729
+
+*/
+choyvp pynff ALAnffnhPbhaglZCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ALAnffnhPbhaglZCnefreGrfg() {
+    frgCnefre(arj ALAnffnhPbhaglZCnefre(), "ANFFNH PBHAGL", "AL");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgWnpxfgre2024() {
+
+    qbGrfg("G1",
+        "1 bs 2\a" +
+        "SEZ:nynezf@bprnafvqrsq.arg\a" +
+        "ZFT:GBN: 10:47 06-13-14 \a" +
+        "TRA - Nhgbzngvp Nynez \a" +
+        "3387 YNJERAPR NIR ,BPRNAFVQR\a" +
+        "YHVF QVNM \a\a" +
+        "PF: J\a" +
+        "(Pba'g) 2 bs 2\a" +
+        "URAEVRGGN NIR / PBAPBEQ NIR \a" +
+        "2014-000712(Raq)",
+
+        "GVZR:10:47",
+        "QNGR:06-13-14",
+        "PNYY:TRA - Nhgbzngvp Nynez",
+        "NQQE:3387 YNJERAPR NIR",
+        "PVGL:BPRNAFVQR",
+        "ANZR:YHVF QVNM",
+        "K:J URAEVRGGN NIR / PBAPBEQ NIR",
+        "VQ:2014-000712");
+
+    qbGrfg("G2",
+        "SEZ:nynezf@bprnafvqrsq.arg\a" +
+        "ZFT:GBN: 13:12 06-13-14 \a" +
+        "NHGB - Nhgb Nppvqrag \a" +
+        "BPRNAFVQR EQ ,BPRNAFVQR\a" +
+        "  \a\a" +
+        "PF: URAEVRGGN NIR  2014-000714",
+
+        "GVZR:13:12",
+        "QNGR:06-13-14",
+        "PNYY:NHGB - Nhgb Nppvqrag",
+        "NQQE:BPRNAFVQR EQ",
+        "ZNQQE:BPRNAFVQR EQ & URAEVRGGN NIR",
+        "PVGL:BPRNAFVQR",
+        "K:URAEVRGGN NIR",
+        "VQ:2014-000714");
+
+    qbGrfg("G3",
+        " 1 bs 2\a" +
+        "SEZ:nynezf@bprnafvqrsq.arg\a" +
+        "ZFT:GBN: 18:24 06-15-14 \a" +
+        "PB - Ab Ercbegrq Nvqrq \a" +
+        "3325 ZHEQBPX NIR ,BPRNAFVQR\a" +
+        "FCVGMRE \a\a" +
+        "PF: ONZOEVPX FG\a" +
+        "(Pba'g) 2 bs 2\a" +
+        "/ QRNQ RAQ \a" +
+        "2014-000727(Raq)",
+
+        "GVZR:18:24",
+        "QNGR:06-15-14",
+        "PNYY:PB - Ab Ercbegrq Nvqrq",
+        "NQQE:3325 ZHEQBPX NIR",
+        "PVGL:BPRNAFVQR",
+        "ANZR:FCVGMRE",
+        "K:ONZOEVPX FG / QRNQ RAQ",
+        "VQ:2014-000727");
+
+    qbGrfg("G4",
+        " 1 bs 2\a" +
+        "SEZ:nynezf@bprnafvqrsq.arg\a" +
+        "ZFT:GBN: 18:51 06-15-14 \a" +
+        "TRA - Trareny Nynez \a" +
+        "TNF YRNX \a" +
+        "3116 YNJFBA OYIQ ,BPRNAFVQR\a" +
+        "  \a\a" +
+        "PF: NAPUBE\a" +
+        "(Pba'g) 2 bs 2\a" +
+        "NIR / J JVAQFBE CXL \a" +
+        "2014-000728(Raq)",
+
+        "GVZR:18:51",
+        "QNGR:06-15-14",
+        "PNYY:TRA - Trareny Nynez - TNF YRNX",
+        "NQQE:3116 YNJFBA OYIQ",
+        "PVGL:BPRNAFVQR",
+        "K:NAPUBE NIR / J JVAQFBE CXL",
+        "VQ:2014-000728");
+
+    qbGrfg("G5",
+        "SEZ:nynezf@bprnafvqrsq.arg\a" +
+        "ZFT:GBN: 21:35 06-15-14 \a" +
+        "NHGB - Nhgb Nppvqrag \a" +
+        "J JVAQFBE CXL ,BPRNAFVQR\a" +
+        "  \a\a" +
+        "PF: YBAT ORNPU EQ  2014-000729",
+
+        "GVZR:21:35",
+        "QNGR:06-15-14",
+        "PNYY:NHGB - Nhgb Nppvqrag",
+        "NQQE:J JVAQFBE CXL",
+        "ZNQQE:J JVAQFBE CXJL & YBAT ORNPU EQ",
+        "PVGL:BPRNAFVQR",
+        "K:YBAT ORNPU EQ",
+        "VQ:2014-000729");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ALAnffnhPbhaglZCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
