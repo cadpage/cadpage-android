b68a4af68a19ce11fec039f62e3c78fa0e0e206f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-20 03:46:34 +0000

    Fixed parsing problem with Manatee County, FL

--- cadpage/src/net/anei/cadpage/parsers/FL/FLManateeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/FL/FLManateeCountyParser.java
@@ -10,9 +10,11 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class FLManateeCountyParser extends FieldProgramParser {
   
+  private static final Pattern FROM_PREFIX_PTN = Pattern.compile("(?:\\*\\*\\[from \\d+\\]|FWD:) +");
+  
   public FLManateeCountyParser() {
     super(CITY_CODES, "MANATEE COUNTY", "FL",
-        "Location:ADDR/S? Estimated_Address:PLACE? TYPE_CODE:CALL! INSIDE_LOCATION:LOC? SUB_TYPE:INFO TIME:TIME% MAP_GRID:MAP");
+        "Location:ADDR/S? Estimated_Address:PLACE? INSIDE_LOCATION:LOC? TYPE_CODE:CALL! INSIDE_LOCATION:LOC? SUB_TYPE:INFO TIME:TIME% MAP_GRID:MAP TIME:TIME");
   }
 
   @Override
@@ -29,6 +31,9 @@ public class FLManateeCountyParser extends FieldProgramParser {
   public boolean parseMsg(String body, Data data) {
     int pt = body.indexOf("\n\n--\n");
     if (pt >= 0) body = body.substring(0,pt).trim();
+    Matcher match = FROM_PREFIX_PTN.matcher(body);
+    if (match.lookingAt()) body = body.substring(match.end());
+    if (body.startsWith("EA : ")) body = "Location:" + body.substring(5);
     body = body.replaceAll("\\s+", " ").replace("SUB TYPE:", " SUB TYPE:");
     body = body.replace("Estimated Address ", "Estimated Address:");
     if (!super.parseMsg(body, data)) return false;
@@ -41,6 +46,7 @@ public class FLManateeCountyParser extends FieldProgramParser {
     if (name.equals("ADDR")) return new MyAddressField();
     if (name.equals("PLACE")) return new MyPlaceField();
     if (name.equals("LOC")) return new MyLocField();
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d");
     return super.getField(name);
   }
   
@@ -50,7 +56,12 @@ public class FLManateeCountyParser extends FieldProgramParser {
       
       Parser p = new Parser(field);
       data.strPlace = p.getLastOptional(": @");
-      if (data.strPlace.length() == 0) data.strPlace = p.getLastOptional(':');
+      if (data.strPlace.length() > 0) {
+        data.strPlace = append(p.getLastOptional(": @"), " - ", data.strPlace);
+      } else {
+        data.strPlace = p.getLastOptional(':');
+      }
+      p = new Parser(stripFieldEnd(p.get(), " EA"));
       String apt = p.getLastOptional(',');
       super.parse(p.get(), data);
       data.strApt = append(data.strApt, "-", apt);
