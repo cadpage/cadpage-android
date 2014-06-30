d4b1afd3bbfe71b632bb291bfc1660e30a42c7a5
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-06-30 06:21:02 +0000

    Fixed parsing problem with Clearwater County, AB

--- cadpage/src/net/anei/cadpage/parsers/ZCAAB/ZCAABRedDeerCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/ZCAAB/ZCAABRedDeerCountyParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.ZCAAB;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -11,21 +12,24 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
  */
 public class ZCAABRedDeerCountyParser extends FieldProgramParser {
   
+  private static final Pattern CLEAN_CITY_PTN = Pattern.compile("(.*?) +RURAL(?: AREA)?", Pattern.CASE_INSENSITIVE);
+  
+  private String version;
+  
   public ZCAABRedDeerCountyParser() {
     this("RED DEER COUNTY");
   }
-
   
   protected ZCAABRedDeerCountyParser(String defCity) {
     super(defCity, "AB",
-           "Date:DATETIME! Type:CALL! Location:ADDR! Units:UNIT? Latitude:GPS1? Longitude:GPS2? Units_Responding:UNIT");
+           "( SELECT/2 CALL CALL2? LOCATION ADDR VILLAGE_OF? CITY/Z? ( APT UNITS_RESPONDING! | UNITS_RESPONDING! ) UNIT+ | " +
+             "Date:DATETIME! Type:CALL! Location:ADDRCITY! Units:UNIT? Latitude:GPS1? Longitude:GPS2? Units_Responding:UNIT )");
   }
   
   @Override
   public String getAliasCode() {
     return "ZCABRedDeerCounty";
   }
-
 
   @Override
   public String getFilter() {
@@ -42,12 +46,23 @@ public class ZCAABRedDeerCountyParser extends FieldProgramParser {
     if (!subject.equals("CAD Message") && !subject.equals("Incident Message")) {
       data.strSource = subject;
     }
-    body = body.replace("\nLatitudeUnits Responding:", "\nUnits Responding:");
-    if (!parseFields(body.split("\n"), data)) return false;
-    setGPSLoc(data.strGPSLoc, data);
-    if (data.strAddress.startsWith("Unknown Location -") && data.strGPSLoc.length() > 0) {
-      data.strAddress = data.strGPSLoc;
+    
+    body = body.replace("LatitudeUnits Responding", "Units Responding");
+    body = body.replace('\\', '/');
+    if (body.startsWith("/ ")) {
+      version = "2";
+      body = body.replace("\n", " / ");
+      if (!parseFields(body.substring(2).trim().split(" / "), data)) return false;
+    } else {  
+      version = "1";
+      if (!parseFields(body.split("\n"), data)) return false;
+      setGPSLoc(data.strGPSLoc, data);
+      if (data.strAddress.startsWith("Unknown Location -") && data.strGPSLoc.length() > 0) {
+        data.strAddress = data.strGPSLoc;
+      }
     }
+    Matcher match = CLEAN_CITY_PTN.matcher(data.strCity);
+    if (match.matches()) data.strCity = match.group(1);
     return true;
   }
   
@@ -58,20 +73,35 @@ public class ZCAABRedDeerCountyParser extends FieldProgramParser {
   
   @Override
   public String adjustMapAddress(String sAddress) {
-    return RNG_PATTERN.matcher(sAddress).replaceAll("Range");
+    sAddress = RNG_PATTERN.matcher(sAddress).replaceAll("Range");
+    sAddress = RANGE_RD_DASH_PTN.matcher(sAddress).replaceAll("$1$2");
+    return sAddress;
   }
   private static final Pattern RNG_PATTERN = Pattern.compile("\\bRng\\b", Pattern.CASE_INSENSITIVE);
-  
+  private static final Pattern RANGE_RD_DASH_PTN = Pattern.compile("\\b(RANGE +RD +\\d+)-(\\d+)\\b", Pattern.CASE_INSENSITIVE);
+
   @Override
   public String postAdjustMapAddress(String sAddress) {
     return HWY_NN_PTN.matcher(sAddress).replaceAll("ALBERTA $1");  
   }
   private static final Pattern HWY_NN_PTN = Pattern.compile("\\bHWY +(\\d+)\\b", Pattern.CASE_INSENSITIVE);
+
   
   @Override
+  protected String getSelectValue() {
+    return version;
+  }
+
+  @Override
   public Field getField(String name) {
+    if (name.equals("CALL2")) return new CallField("Gas Odor.*");
+    if (name.equals("LOCATION")) return new SkipField("Location", true);
+    if (name.equals("VILLAGE_OF")) return new SkipField("VILLAGE OF");
+    if (name.equals("APT")) return new AptField("Unit +(.*)");
+    if (name.equals("UNITS_RESPONDING")) return new SkipField("Units Responding", true);
     if (name.equals("DATETIME")) return new MyDateTimeField();
-    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("UNIT")) return new MyUnitField();
     return super.getField(name);
   }
 
@@ -83,7 +113,7 @@ public class ZCAABRedDeerCountyParser extends FieldProgramParser {
     }
   }
   
-  private class MyAddressField extends AddressField {
+  private class MyAddressCityField extends AddressCityField {
     @Override
     public void parse(String field, Data data) {
       if (field.endsWith(")")) {
@@ -104,13 +134,20 @@ public class ZCAABRedDeerCountyParser extends FieldProgramParser {
         field = p.get(',');
       }
       
-      super.parse(field.replace('\\', '/'), data);
+      parseAddress(field.replace('\\', '/'), data);
       data.strApt = append(data.strApt, "-", apt);
     }
     
     @Override
     public String getFieldNames() {
-      return super.getFieldNames() + " CITY PLACE";
+      return super.getFieldNames() + " PLACE";
+    }
+  }
+  
+  private class MyUnitField extends UnitField {
+    @Override
+    public void parse(String field, Data data) {
+      data.strUnit = append(data.strUnit, " ", field);
     }
   }
 }
