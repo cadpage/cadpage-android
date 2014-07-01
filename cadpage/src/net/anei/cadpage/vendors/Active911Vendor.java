45a2b812b70ee9a99d36a87208072c07962c4ed7
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-07-01 00:37:12 +0000

    Updated A911 parser table

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -366,6 +366,7 @@ class Active911Vendor extends Vendor {
       "US/OH/Harveysburg",                "OHHarveysburg",
       "US/OH/HolmesCountySheriff",        "OHHolmesCounty",
       "US/OH/Sebring",                    "OHMahoningCounty",
+      "US/OH/WashingtonCountySheriff",    "OHWashingtonCountyA",
       "US/OK/CityofTulsa",                "OKTulsaB",
       "US/OK/CherokeeCounty",             "OKCherokeeCounty",
       "US/OK/LoveCounty",                 "OKLoveCounty",
