967cc65a0a21ad6995d696859f8615d7314fbcba
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-07-17 23:05:56 +0000

    Fixed parsing problem with Travis County, TX

--- cadpage/src/net/anei/cadpage/parsers/TX/TXTravisCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXTravisCountyParser.java
@@ -18,6 +18,9 @@ public class TXTravisCountyParser extends MsgParser {
   private static final Pattern MASTER2 = 
       Pattern.compile("From - ([A-Z0-9]+) - \\d Alarm / (.*?) (?:Box|BOX|\\| RAP) - ([-A-Z0-9]*) @ (.*?) (?:\\| )?XStreets: *(.*?) (?:\\| )?On - ([ A-Z0-9]*) (?:\\| )?Time: (?:\\| )?(?:(\\d\\d:\\d\\d:\\d\\d)|\\d\\d:\\d\\d [AP]M) (?:\\| )?Inc# (\\d+) (?:Case Num:([-A-Z0-9]*) )?(?:\\| )?For - ([A-Z0-9,]+)");
   
+  private static final Pattern MASTER3 = 
+      Pattern.compile("(?:INCIDENT ASSIGNED TO YOU!!*|Response Info from Dispatch) - ?\\d?ALARM -(.*?) - BOX -([-A-Z0-9]*) FC=(.+?)- AT -(.*?) - ASSIGNED UNITS=>([A-Z0-9,]+)- INC# =>(\\d+)");
+  
   private static final Pattern CROSS_PTN = Pattern.compile("(?:(?:N?o )?CrossStreet Found)?/?(.*?)/?(?:No CrossStreet Found)?");
 
   public TXTravisCountyParser() {
@@ -42,11 +45,11 @@ public class TXTravisCountyParser extends MsgParser {
     }
     match = MASTER1.matcher(body);
     if (match.matches()) {
-      setFieldList("INFO SRC CALL BOX CH ADDR APT ID UNIT");
+      setFieldList("INFO SRC CALL MAP CH ADDR APT ID UNIT");
       
       data.strSource = match.group(1);
       data.strCall = match.group(2).trim();
-      data.strBox = match.group(3).trim();
+      data.strMap = match.group(3).trim();
       data.strChannel = match.group(4).trim();
       parseAddress(match.group(5).trim(), data);
       data.strCallId = append(match.group(7), "/", match.group(6));
@@ -57,11 +60,11 @@ public class TXTravisCountyParser extends MsgParser {
     
     match = MASTER2.matcher(body);
     if (match.matches()) {
-      setFieldList("INFO SRC CALL BOX ADDR APT X CH TIME ID UNIT");
+      setFieldList("INFO SRC CALL MAP ADDR APT X CH TIME ID UNIT");
       
       data.strSource = match.group(1);
       data.strCall = match.group(2).trim();
-      data.strBox = match.group(3).trim();
+      data.strMap = match.group(3).trim();
       parseAddress(match.group(4).trim(), data);
       String cross  = match.group(5).trim();
       data.strChannel = match.group(6);
@@ -74,6 +77,18 @@ public class TXTravisCountyParser extends MsgParser {
       
       return true;
       
+    }
+    
+    match = MASTER3.matcher(body);
+    if (match.matches()) {
+      setFieldList("CALL MAP CH ADDR APT UNIT ID");
+      data.strCall = match.group(1);
+      data.strMap = match.group(2);
+      data.strChannel = match.group(3).trim();
+      parseAddress(match.group(4).trim(), data);
+      data.strUnit = match.group(5).trim();
+      data.strCallId = match.group(6);
+      return true;
     }
     
     return false;
