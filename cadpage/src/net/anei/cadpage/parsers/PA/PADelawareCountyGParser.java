3d3a21b38356eb38b0b4d5d717a2ddb7efae12ca
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-08-09 00:04:08 +0000

    Fixed parsing problem with Delaware County, PA (G)

--- cadpage/src/net/anei/cadpage/parsers/PA/PADelawareCountyGParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PADelawareCountyGParser.java
@@ -1,25 +1,29 @@
 package net.anei.cadpage.parsers.PA;
 
+import java.util.regex.Pattern;
+
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class PADelawareCountyGParser extends FieldProgramParser {
   
+  private static final Pattern DELIM = Pattern.compile("\n|\\|");
+  
   public PADelawareCountyGParser() {
     super("DELAWARE COUNTY", "PA",
-          "CALL:CALL! ADDR:ADDR! CITY:CITY! ID:ID! DATE:DATE! TIME:TIME! UNIT:UNIT! INFO:INFO");
+          "( CALL:CALL! ADDR:ADDR! CITY:CITY? ID:ID? DATE:DATE! TIME:TIME! UNIT:UNIT INFO:INFO+ " + 
+          "| ADDR:ADDR! CITY:CITY? CALL:CALL? DATE:DATE? TIME:TIME? ID:ID? INFO:INFO+? UNIT:UNIT )");
   }
   
   @Override
   public String getFilter() {
-    return "admin@springfieldems.com";
+    return "admin@springfieldems.com,alerts@delcodispatch.com";
   }
 
   @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!subject.equals("New call")) return false;
-    return parseMsg(body, data);
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(DELIM.split(body), data);
   }
   
   @Override
