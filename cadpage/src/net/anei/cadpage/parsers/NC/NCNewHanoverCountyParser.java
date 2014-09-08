ac8ab9307a082289bc5e8028848c70a954888b01
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-09-08 01:09:36 +0000

    Fixed additional problems with New Hanover County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCNewHanoverCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCNewHanoverCountyParser.java
@@ -13,7 +13,7 @@ public class NCNewHanoverCountyParser extends DispatchOSSIParser {
   
   public NCNewHanoverCountyParser() {
     super(CITY_CODES, "NEW HANOVER COUNTY", "NC",
-          "( CH2 ADDR CITY/Y CH! | SRC CALL ADDR EXTRA! ) INFO+");
+          "FYI? ( CITY ADDR CALL SRC! | CH2 ADDR CITY/Y! EXTRA2 | SRC CALL ADDR EXTRA! ) INFO+");
   }
   
   @Override
@@ -23,12 +23,64 @@ public class NCNewHanoverCountyParser extends DispatchOSSIParser {
   
   @Override
   protected Field getField(String name) {
-    if (name.equals("CH2")) return new SkipField("PUBLIC SAFETY CHANNEL \\d", true);
-    if (name.equals("CH")) return new ChannelField("PS\\d");
-    if (name.equals("SRC")) return new SourceField("ST\\d+", true);
+    if (name.equals("CH2")) return new MyChannel2Field();
+    if (name.equals("EXTRA2")) return new MyExtra2Field();
+    if (name.equals("SRC")) return new SourceField("ST\\d+|W\\d+|[A-Z]{1,2}FD", true);
     if (name.equals("EXTRA")) return new MyExtraField();
     if (name.equals("INFO")) return new MyInfoField();
     return super.getField(name);
+  }
+  
+  private static final Pattern CH2_PTN = Pattern.compile("PUBLIC SAFETY CHANNEL (\\d+)");
+  private class MyChannel2Field extends ChannelField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      Matcher match = CH2_PTN.matcher(field);
+      if (!match.matches()) return false;
+      data.strChannel = "PS" + match.group(1);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+  
+  private static final Pattern EXTRA2_CH_PTN = Pattern.compile("\\**\\b(PS ?\\d+)\\b\\**");
+  private static final Pattern EXTRA2_APT_PTN = Pattern.compile("(?:APT|LOT|RM|TRLR) +(.*)|(\\d{1,4})");
+  private static final Pattern EXTRA2_ID_PTN = Pattern.compile("\\d{6,}");
+  private class MyExtra2Field extends Field {
+
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = EXTRA2_CH_PTN.matcher(field);
+      if (match.find()) {
+        data.strChannel = match.group(1).replace(" ", "");
+        field = append(field.substring(0,match.start()).trim(), " ", field.substring(match.end()).trim());
+      }
+      
+      match = EXTRA2_APT_PTN.matcher(field);
+      if (match.matches()) {
+        String apt = match.group(1);
+        if (apt == null) apt = match.group(2);
+        data.strApt = append(data.strApt, "-", apt);
+      } else if (EXTRA2_ID_PTN.matcher(field).matches()) {
+        data.strCallId = field;
+      } else {
+        data.strSupp = field;
+      }
+    }
+
+    @Override
+    public String getFieldNames() {
+      return "INFO APT ID CH";
+    }
   }
   
   private static final Pattern ASTERISK_PTN = Pattern.compile("\\*\\*+");
@@ -80,6 +132,7 @@ public class NCNewHanoverCountyParser extends DispatchOSSIParser {
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "CH",   "CASTLE HAYNE",
       "WM",   "WILMINGTON"
   });
 }
