37b2376ec0fe74ef1ad2e2c867c849a0e2790e3a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-10-17 18:25:00 +0000

    Fixed parsing problem with Centre County, PA

--- cadpage/src/net/anei/cadpage/parsers/PA/PACentreCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PACentreCountyParser.java
@@ -8,7 +8,7 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class PACentreCountyParser extends FieldProgramParser {
 
   public PACentreCountyParser() {
-    super("CENTRE COUNTY", "PA", "BOX CALL ADDR NAME! INFO+");
+    super("CENTRE COUNTY", "PA", "BOX CALL ADDR! NAME INFO+");
   }
 
   private static final Pattern NON_ASCII = Pattern.compile("[^\\x00-\\x7f]");
@@ -21,14 +21,14 @@ public class PACentreCountyParser extends FieldProgramParser {
     // kill non ascii
     body = NON_ASCII.matcher(body).replaceAll("");
 
-    return parseFields(body.split(";"), data);
+    return parseFields(body.split(";", -1), data);
   }
 
   @Override
   protected Field getField(String name) {
     if (name.equals("BOX")) return new BoxField("BOX ?(\\d*)", true);
     if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("NAME")) return new NameField("NAME-(.*)", true);
+    if (name.equals("NAME")) return new NameField("NAME-(.*)|", true);
     return super.getField(name);
   }
 
