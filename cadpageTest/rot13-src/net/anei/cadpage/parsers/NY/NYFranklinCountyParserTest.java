2eaab8ca01d0ba03e67dabf5ac9bed2835e96d86
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-11-02 01:54:45 +0000

    Checking in Franklin County, NY

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NY/NYFranklinCountyParserTest.java
@@ -0,0 +1,76 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AL;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+
+/*
+Senaxyva Pbhagl, AL
+
+Pbagnpg: wnzrf.p.nyrknaqre@ubgznvy.pbz
+Fraqre: 4702193648
+
+SENPB911  QVPXVAFBA  RZF  14 PUHEPU FG, QVPXVAFBA PRAGRE, 12930  (AVYRF EQ/PBHAGL EBHGR 5)  FAVPUBYF PNAPRY 
+SENPB911  QVPXVAFBA  ZHGHNY NVQ SVER  122 FCEVAT FG, FNVAG ERTVF SNYYF, 12980  (EVIREF NIR/XVQARL NIR)  POBHVFFRL PNAPRY 
+SENPB911  QVPXVAFBA  GERR/JVERF QBJA  176 PBHAGL EBHGR 5, QVPXVAFBA PRAGRE, 12930  (CBEG XRAG EQ/A ZNVA FG)  PPBBX PNAPRY 
+SENPB911  QVPXVAFBA  ZIN  677 CBEG XRAG EQ  wtenggba PNAPRY 
+SENPB911  QVPXVAFBA  ZHGHNY NVQ RZF  22 FGNGR EBHGR 11O, QVPXVAFBA PRAGRE, 12930  (FARYY EQ/)  wynpebvk PNAPRY 
+
+*/
+
+choyvp pynff ALSenaxyvaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ALSenaxyvaPbhaglCnefreGrfg() {
+    frgCnefre(arj ALSenaxyvaPbhaglCnefre(), "SENAXYVA PBHAGL", "AL");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgWnzrfPNyrknaqre() {
+
+    qbGrfg("G1",
+        "SENPB911  QVPXVAFBA  RZF  14 PUHEPU FG, QVPXVAFBA PRAGRE, 12930  (AVYRF EQ/PBHAGL EBHGR 5)  FAVPUBYF PNAPRY ",
+        "FEP:QVPXVAFBA",
+        "PNYY:RZF",
+        "NQQE:14 PUHEPU FG",
+        "PVGL:QVPXVAFBA PRAGRE",
+        "K:AVYRF EQ/PBHAGL EBHGR 5");
+
+    qbGrfg("G2",
+        "SENPB911  QVPXVAFBA  ZHGHNY NVQ SVER  122 FCEVAT FG, FNVAG ERTVF SNYYF, 12980  (EVIREF NIR/XVQARL NIR)  POBHVFFRL PNAPRY ",
+        "FEP:QVPXVAFBA",
+        "PNYY:ZHGHNY NVQ SVER",
+        "NQQE:122 FCEVAT FG",
+        "PVGL:FNVAG ERTVF SNYYF",
+        "K:EVIREF NIR/XVQARL NIR");
+
+    qbGrfg("G3",
+        "SENPB911  QVPXVAFBA  GERR/JVERF QBJA  176 PBHAGL EBHGR 5, QVPXVAFBA PRAGRE, 12930  (CBEG XRAG EQ/A ZNVA FG)  PPBBX PNAPRY ",
+        "FEP:QVPXVAFBA",
+        "PNYY:GERR/JVERF QBJA",
+        "NQQE:176 PBHAGL EBHGR 5",
+        "ZNQQE:176 PBHAGL EBNQ 5",
+        "PVGL:QVPXVAFBA PRAGRE",
+        "K:CBEG XRAG EQ/A ZNVA FG");
+
+    qbGrfg("G4",
+        "SENPB911  QVPXVAFBA  ZIN  677 CBEG XRAG EQ  wtenggba PNAPRY ",
+        "FEP:QVPXVAFBA",
+        "PNYY:ZIN",
+        "NQQE:677 CBEG XRAG EQ");
+
+    qbGrfg("G5",
+        "SENPB911  QVPXVAFBA  ZHGHNY NVQ RZF  22 FGNGR EBHGR 11O, QVPXVAFBA PRAGRE, 12930  (FARYY EQ/)  wynpebvk PNAPRY ",
+        "FEP:QVPXVAFBA",
+        "PNYY:ZHGHNY NVQ RZF",
+        "NQQE:22 FGNGR EBHGR 11O",
+        "ZNQQE:22 FGNGR 11O",
+        "PVGL:QVPXVAFBA PRAGRE",
+        "K:FARYY EQ");
+  
+  }
+ 
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ALSenaxyvaPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
