3f9e06f647eaf79367e93abd3ee5aecf46d4d743
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-11-14 08:04:24 +0000

    Update A911 parser table

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -627,6 +627,7 @@ class Active911Vendor extends Vendor {
       "US/VA/LoudounCounty",              "VALoudounCounty",
       "US/VA/MecklenburgCounty",          "VAMecklenburgCounty",
       "US/VA/MontgomeryCounty",           "VAMontgomeryCounty",
+      "US/VA/NewportNews",                "VANewportNews",
       "US/VA/NorthumberlandCounty",       "VANorthumberland",
       "US/VA/JamesCityCounty",            "VAJamesCityCounty",
       "US/VA/PrinceWilliamCounty",        "VAPrinceWilliamCountyA",
