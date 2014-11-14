7b571fe6fbbb4e1894c043f9bf4bb78987758f33
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-11-14 08:01:31 +0000

    Checking in Newport News, VA

--- cadpage/src/net/anei/cadpage/parsers/PA/PAMercerCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/PA/PAMercerCountyParser.java
@@ -4,20 +4,17 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA50Parser;
 
 /**
  * Mercer County, PA
  */
-public class PAMercerCountyParser extends FieldProgramParser {
+public class PAMercerCountyParser extends DispatchA50Parser {
   
   public PAMercerCountyParser() {
-    super(CITY_CODES, "MERCER COUNTY", "PA",
-           "Location:ADDR/S? EID:ID! TYPE_CODE:CALL! CALLER_NAME:NAME CALLER_ADDR:ADDR/S TIME:TIME");
+    super(CALL_CODES, CITY_CODES, "MERCER COUNTY", "PA");
   }
   
   @Override
@@ -40,65 +37,6 @@ public class PAMercerCountyParser extends FieldProgramParser {
   @Override
   public String getProgram() {
     return super.getProgram().replace("CITY", "CITY ST");
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("CALL")) return new MyCallField();
-    return super.getField(name);
-  }
-  
-  private static final Pattern PLACE_MARKER = Pattern.compile(" *:? *@ *| *: *alias *| *\\*+ *");
-  private class MyAddressField extends AddressField {
-    @Override
-    public void parse(String field, Data data) {
-      if (data.strAddress.length() > 0) return;
-      
-      String sPlace = "";
-      Matcher match = PLACE_MARKER.matcher(field);
-      if (match.find()) {
-        sPlace = field.substring(match.end()).trim();
-        field = field.substring(0,match.start()).trim();
-      }
-      String apt = "";
-      int pt = field.lastIndexOf(',');
-      if (pt >= 0) {
-        apt = field.substring(pt+1).trim();
-        field = field.substring(0,pt).trim();
-      }
-      field = stripFieldEnd(field, ": EST");
-      super.parse(field, data);
-      data.strApt = append(data.strApt, "-", apt);
-
-      Result res = parseAddress(StartType.START_OTHER, FLAG_ONLY_CITY | FLAG_ANCHOR_END, sPlace);
-      if (res.isValid()) {
-        res.getData(data);
-        sPlace = res.getStart();
-      }
-      data.strPlace = sPlace;
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return super.getFieldNames() + " PLACE";
-    }
-  }
-  
-  private class MyCallField extends CallField {
-    @Override
-    public void parse(String field, Data data) {
-      if (field.length() >= 2 && field.charAt(1) == '-') {
-        data.strPriority = field.substring(0,1).trim();
-        field = field.substring(2).trim();
-      }
-      data.strCall = convertCodes(field, CALL_CODES);
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "PRI CALL";
-    }
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
