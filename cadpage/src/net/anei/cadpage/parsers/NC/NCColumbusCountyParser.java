65150db1917b742331ab967678e124732703fbf3
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-11-22 18:54:35 +0000

    Update A911 parser table

--- cadpage/src/net/anei/cadpage/parsers/NC/NCColumbusCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCColumbusCountyParser.java
@@ -1,6 +1,7 @@
 
 package net.anei.cadpage.parsers.NC;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 
@@ -11,16 +12,28 @@ public class NCColumbusCountyParser extends DispatchSouthernParser {
     super(CITY_LIST, "COLUMBUS COUNTY", "NC", DSFLAG_DISPATCH_ID | DSFLAG_LEAD_PLACE | DSFLAG_CROSS_NAME_PHONE);
   }
   
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    if (data.strApt.equals("SHOPPING CTR")) {
+      data.strAddress = append(data.strAddress, " ", data.strApt);
+      data.strApt = "";
+    }
+    return true;
+  }
+
   private static final String[] CITY_LIST = new String[]{
     "BOARDMAN",
     "BOLTON",
     "BRUNSWICK",
     "CERRO GORDO",
     "CHADBOURN",
+    "CLARENDON",
     "FAIR BLUFF",
     "LAKE WACCAMAW",
     "SANDYFIELD",
     "TABOR CITY",
     "WHITEVILLE"
+    
   };
 }
