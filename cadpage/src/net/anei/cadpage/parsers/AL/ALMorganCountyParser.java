4fc2cd817f4df050a34fb534bc022fefaa1cc40e
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-12-18 18:05:51 +0000

    Fixed parsing problem with Morgan County, AL

--- cadpage/src/net/anei/cadpage/parsers/AL/ALMorganCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALMorganCountyParser.java
@@ -1,24 +1,55 @@
 
 package net.anei.cadpage.parsers.AL;
 
-import java.util.regex.Matcher;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
-import net.anei.cadpage.parsers.SmartAddressParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 /**
  * Morgan County, AL
  */
 
-public class ALMorganCountyParser extends SmartAddressParser {
+public class ALMorganCountyParser extends DispatchOSSIParser {
   
-  private static final Pattern MASTER = Pattern.compile("(?:CAD:)?(.*?)(?: (\\d{8}))?(?: (DFS\\d|MTFS))?");
-  private static final Pattern CAT_PTN = Pattern.compile(" +CAT(\\d)\\b *");
+  // The program string we want to use and nicely would solve everything is
+  // PLACE? ADDR CALL! X? X? ID? UNIT? CH
+  // But we can not do anything that simple because we want to avoid relying
+  // on address and cross street fields to make decisions.
+  //
+  // The most reliable indicator is the ID field.  And if it is the 6th field
+  // we can reliably conclude that the place and both cross fields are present
+  
+  // If that doesn't work, we will try to rely on the call description being
+  // in position 2 or 3 to identify if the place field is present
+  // If that doesn't fly, we will just have to depend on the main address
+  // field to determine if we have a place field
 
   public ALMorganCountyParser() {
-    super("MORGAN COUNTY", "AL");
-    setFieldList("PLACE ADDR CALL PRI X ID SRC");
+    super("MORGAN COUNTY", "AL",
+        // The simple program string we want to use is
+        // PLACE? ADDR CALL! X? X? ID? UNIT CH
+        // But of course that will never work, so we have to do something
+        // complicated
+        
+        // If we can identify the call field, things are pretty simple
+        "( PLACE ADDR/Z CALL X/Z+? ( ID UNIT | UNIT ) " +
+        "| ADDR/Z CALL X/Z+? ( ID UNIT | UNIT ) " +
+          
+        // If the call field is unknown, things get complicated
+        // next see if the unit is in the 7th place, which means all
+        // of the optional fields are present
+        "| PLACE ADDR/Z CALL/Z X/Z X/Z ID/Z UNIT " +
+        
+        // Still no luck.  All we can do is hope the address
+        // is recognizable
+        "| PLACE? ADDR/s CALL! X/Z+? ( ID UNIT | UNIT ) " +
+        
+        // And finally an optional channel at the end
+        ") CH");
   }
   
   @Override
@@ -27,33 +58,79 @@ public class ALMorganCountyParser extends SmartAddressParser {
   }
   
   @Override
-  public boolean parseMsg(String body, Data data) {
-    Matcher match = MASTER.matcher(body);
-    if (!match.matches()) return false;
-    body = match.group(1);
-    data.strCallId = getOptGroup(match.group(2));
-    data.strSource = getOptGroup(match.group(3));
-    
-    match = CAT_PTN.matcher(body);
-    if (match.find()) {
-      data.strPriority = match.group(1);
-      Result res = parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, body.substring(match.end()));
-      res.getData(data);
-      body = body.substring(0,match.start());
-    }
-    
-    parseAddress(StartType.START_PLACE, body, data);
-    
-    // What's left is usually the call description
-    // but if we didn't find a priority and cross street, it might be
-    // a combine description and place name
-    String left = getLeft();
-    if (data.strPriority.length() == 0) {
-      Result res = parseAddress(StartType.START_CALL, FLAG_START_FLD_REQ | FLAG_ONLY_CROSS | FLAG_IMPLIED_INTERSECT | FLAG_ANCHOR_END, left);
-      res.getData(data);
-    } else {
-      data.strCall = left;
-    }
-    return true;
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("CAD:")) body = "CAD:" + body;
+    return super.parseMsg(body, data);
   }
+
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CALL")) return new MyCallField();
+    if (name.equals("X")) return new MyCrossField();
+    if (name.equals("ID")) return new IdField("\\d{8}", true);
+    if (name.equals("UNIT")) return new UnitField("(?!TAC)[A-Z]{3}[A-Z0-9]", true);
+    if (name.equals("CH")) return new ChannelField("TAC? ?\\d{1,2}", true);
+    return super.getField(name);
+  }
+  
+  private class MyCallField extends CallField {
+    
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!checkCall(field)) return false;
+      super.parse(field, data);
+      return true;
+    }
+  }
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      // If we encounter a 3rd cross street, bail out
+      if (data.strCross.contains("&")) abort();
+      super.parse(field, data);
+    }
+  }
+  
+  @Override
+  public boolean checkCall(String field) {
+    return (CALL_CAT_PTN.matcher(field).matches() ||
+            CALL_LIST.contains(field));
+    
+  }
+  
+  private static final Pattern CALL_CAT_PTN = Pattern.compile(".* CAT ?\\d");
+  private Set<String> CALL_LIST = new HashSet<String>(Arrays.asList(
+      "BUSINESS FIRE ALARM",
+      "CARBON MONOXIDE ALARM",
+      "CAR FIRE",
+      "CONTROLLED BURN",
+      "FIRE OTHER",
+      "FIRE PR EVENT",
+      "GAS LEAK",
+      "GRASS FIRE",
+      "HIT AND RUN NO INJURY",
+      "HIT AND RUN WITH INJURY",
+      "MEDICAL TRANSPORT",
+      "RESIDENTIAL FIRE ALARM",
+      "SHOOTING",
+      "SMOKE INVESTIGATION",
+      "STABBING",
+      "STRUCTURE FIRE",
+      "SUICIDE",
+      "SUICIDE ATTEMPTED",
+      "SUICIDE THREATENED",
+      "URGENT TRANSPORT",
+      "WATER RESCUE WITH INJURY",
+      "WEATHER RELATED",
+      "WRECK NO INJURY",
+      "WRECK WITH ENTRAPMENT",
+      "WRECK WITH INJURY",
+      "WRECK WITH UNKNOWN INJURY"
+  ));
 }
