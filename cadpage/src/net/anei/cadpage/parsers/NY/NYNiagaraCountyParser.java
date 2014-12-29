e624127304fbd99b7c787f90aac01effc9ca38d8
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-12-29 20:36:50 +0000

    Fixed parsing problem with Niagara County, NY

--- cadpage/src/net/anei/cadpage/parsers/NY/NYNiagaraCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYNiagaraCountyParser.java
@@ -10,111 +10,71 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 
 public class NYNiagaraCountyParser extends SmartAddressParser {
+
+  private static final Pattern SRC_SUBJECT_PTN = Pattern.compile("TCAS|Station \\d+");
+  private static final Pattern TRAIL_TIME_PTN = Pattern.compile(" +(?:(\\d\\d):?(\\d\\d)(?: ?HRS)?|(\\d:\\d{2}))$", Pattern.CASE_INSENSITIVE);
+  private static final Pattern TRAIL_DATE_PTN = Pattern.compile(" +(\\d{1,2}/\\d{1,2}(?:/\\d{2}(?:\\d{2})?)?)$");
+
+  public NYNiagaraCountyParser() {
+    super("NIAGARA COUNTY", "NY");
+    setFieldList("SRC CALL ADDR APT INFO DATE TIME");
+    removeWords("CORD");
+  }
   
-    private static final Pattern SUBJECT_PTN = Pattern.compile("[\\(\\[]?(\\d+)[\\)\\]]? NCFC .*");
-    private static final Pattern MARKER1_PTN = Pattern.compile("[\\(\\[]?(\\d+)");
-    private static final Pattern MARKER2_PTN = Pattern.compile("^NCFC [A-Z]+ *[\\)\\] ]");
-    
-    private static final Pattern DATE_TIME_PTN = Pattern.compile("\\b(\\d{1,2}/\\d{1,2}(?:/\\d{2,4})?) (\\d{4})$");
-    private static final Pattern TIME_PTN = Pattern.compile("\\b(\\d{4})(?:hrs)?$");
-    private static final Pattern DATE_TIME_PTN2 = 
-      Pattern.compile("(?: - *)?(?:(\\b\\d\\d/\\d\\d/\\d\\d(?:\\d\\d)?))?(?: *@ *)?(\\b\\d{4}) */?hrs(?: *- )?");
-    
-    private static final Pattern CALL_AT_PTN = Pattern.compile("^([A-Z ]+)@");
-    
-    public NYNiagaraCountyParser() {
-      super("NIAGARA COUNTY", "NY");
-      setFieldList("ID CALL PLACE ADDR APT INFO DATE TIME");
-    }
-    
-    @Override
-    public String getFilter() {
-      return "@niagaracounty.com";
-    }
+  @Override
+  public String getFilter() {
+    return "@niagaracounty.com,777";
+  }
 
-	  @Override
-	  protected boolean parseMsg(String subject, String body, Data data) { 
-	    
-	    do {
-	      Matcher match = SUBJECT_PTN.matcher(subject);
-	      if (match.matches()) {
-	        data.strCallId = match.group(1);
-	        break;
-	      }
-	      
-	      Matcher match1 = MARKER1_PTN.matcher(subject);
-	      if (match1.matches()) {
-	        Matcher match2 = MARKER2_PTN.matcher(body);
-	        if (match2.find()) {
-	          data.strCallId = match1.group(1);
-	          body = body.substring(match2.end()).trim();
-	          break;
-	        }
-	      }
-	      return false;
-	    } while (false);
-	    
-	    String save = body;
-	    
-	    Matcher match = DATE_TIME_PTN.matcher(body);
-	    if (match.find()) {
-	      data.strDate = match.group(1);
-	      data.strTime = expandTime(match.group(2));
-	      body = body.substring(0,match.start()).trim();
-	    } else {
-	      match = TIME_PTN.matcher(body);
-	      if (match.find()) {
-	        data.strTime = expandTime(match.group(1));
-	        body = body.substring(0,match.start()).trim();
-	      }
-	    }
-	    
-	    match = DATE_TIME_PTN2.matcher(body);
-	    if (match.find()) {
-	      String sDate = match.group(1);
-	      if (sDate != null) data.strDate = sDate;
-	      data.strTime = expandTime(match.group(2));
-	      body = body.substring(0,match.start()).trim() + " " + body.substring(match.end()).trim();
-	    }
-	    
-	    body = body.replace(',', ' ').replace('.', ' ').replaceAll("//s+", " ");
-	    
-	    StartType st = StartType.START_CALL;
-	    match = CALL_AT_PTN.matcher(body); 
-	    if (match.find()) {
-	      data.strCall = match.group(1).trim();
-	      body = body.substring(match.end()).trim();
-	      st = StartType.START_PLACE;
-	    }
-	    
-	    parseAddress(st, FLAG_NO_IMPLIED_APT, body, data);
-	    if (!isValidAddress()) {
-	      String callId = data.strCallId;
-	      if (!data.parseGeneralAlert(this, save)) return false;
-	      data.strCallId = callId;
-	      return true;
-	    }
-	    
-	    data.strSupp = unDash(getLeft());
-	    
-	    data.strCall = unDash(data.strCall);
-	    if (data.strCall.length() == 0) {
-	      data.strCall = data.strSupp;
-	      data.strSupp = "";
-	    }
-	    
-	    data.strPlace = unDash(data.strPlace);
-	    return true;
-	  }
-
-    private String expandTime(String time) {
-      return time.substring(0,2) + ":" +time.substring(2,4);
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    
+    Matcher match = SRC_SUBJECT_PTN.matcher(subject);
+    if (!match.matches()) return false;
+    data.strSource = subject;
+    
+    body = body.replace("\n", "");
+    
+    // Look for trailing date and time, or time and date
+    
+    match = TRAIL_DATE_PTN.matcher(body);
+    if (match.find()) {
+      body = body.substring(0,match.start());
+      data.strDate = match.group(1);
     }
     
-    private String unDash(String field) {
-      if (field.startsWith("- ")) field = field.substring(2).trim();
-      if (field.endsWith(" -")) field = field.substring(0,field.length()-2).trim();
-      return field;
+    match = TRAIL_TIME_PTN.matcher(body);
+    if (match.find()) {
+      body = body.substring(0,match.start());
+      data.strTime = match.group(3);
+      if (data.strTime == null) {
+        data.strTime = match.group(1) + ':' + match.group(2);
+      }
+      if (data.strDate.length() == 0) {
+        match = TRAIL_DATE_PTN.matcher(body);
+        if (match.find()) {
+          body = body.substring(0,match.start());
+          data.strDate = match.group(1);
+        }
+      }
     }
-	}
-	
+    
+    // That is about all we can get.  Use SAP for everything else
+    parseAddress(StartType.START_CALL, FLAG_IGNORE_AT | FLAG_NO_IMPLIED_APT, body, data);
+    String left = getLeft();
+    left = stripFieldStart(left, "-");
+    left = stripFieldStart(left, "/");
+    if (data.strCall.length() == 0) {
+      if (left.length() > 0) {
+        data.strCall = left;
+      } else {
+        data.strCall = data.strAddress;
+        data.strAddress = "";
+      }
+    } else {
+      data.strSupp = left;
+    }
+    
+    return true;
+  }
+}
