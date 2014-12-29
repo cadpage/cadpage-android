b01f0c46509a4821226dc1e4197fe7eaca9f550a
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2014-12-29 19:06:10 +0000

    Updated test class

--- cadpage/src/net/anei/cadpage/parsers/KS/KSHaysParser.java
+++ cadpage/src/net/anei/cadpage/parsers/KS/KSHaysParser.java
@@ -1,6 +1,5 @@
 package net.anei.cadpage.parsers.KS;
 
-import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA25Parser;
 
 
@@ -14,12 +13,4 @@ public class KSHaysParser extends DispatchA25Parser {
   public String getFilter() {
     return "1-HPDDispatch@haysusa.com";
   }
-
-  @Override
-  protected boolean parseMsg(String subject, String body, Data data) {
-    if (!super.parseMsg(subject, body, data)) return false;
-    if (data.strCity.equalsIgnoreCase("COUNTY")) data.strCity = "";
-    return true;
-  }
-  
 }
