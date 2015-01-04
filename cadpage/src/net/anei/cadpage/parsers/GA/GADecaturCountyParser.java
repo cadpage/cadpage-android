e9f9f40def6fb86257c31bc5298338151b2ad740
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-01-04 05:58:33 +0000

    Checking in Decatur County, GA

---
+++ cadpage/src/net/anei/cadpage/parsers/GA/GADecaturCountyParser.java
@@ -0,0 +1,40 @@
+package net.anei.cadpage.parsers.GA;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
+
+public class GADecaturCountyParser extends SmartAddressParser {
+
+  public GADecaturCountyParser() {
+      super("DECATUR COUNTY", "GA");
+      setFieldList("CALL ADDR APT INFO");
+      setupMultiWordStreets(
+          "BOOSTER CLUB",
+          "COOL SPRINGS"
+      );
+      
+  }
+  
+  public boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("DG911\n")) return false;
+    body = body.substring(6).trim();
+    int pt = body.indexOf("\n>---");
+    if (pt >= 0) body = body.substring(0,pt).trim();
+    
+    // Check on how  many newlines we have
+    // if exactly two, discard the first part as duplicate information
+    // if more than two, reject
+    String[] parts = body.split("\n");
+    if (parts.length > 2) return false;
+    if (parts.length == 2) body = parts[1].trim();
+    
+    // Use smart address parser to get what we can
+    parseAddress(StartType.START_CALL, body, data);
+    if (data.strCall.length() == 0) {
+      data.strCall = getLeft();
+    } else {
+      data.strSupp = getLeft();
+    }
+    return true;
+  }
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/GA/GADecaturCountyParserTest.java
@@ -0,0 +1,70 @@
+cnpxntr arg.narv.pnqcntr.cnefref.TN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Qrpnghe Pbhagl, TN
+
+Pbagnpg: arryfabyrf <arryfabyrf@lnubb.pbz>
+Fraqre: 8506943382
+QT911\aJERPX JVGU VAWHEVRF   PBBY FCEVATF EBNQ NG GUR  TN/ SYN YVAR
+QT911\aFGEHPGHER SVER\aFGEHPGHER SVER ** 140 SYBEVQN NIR ** SNPRIVYYR QVFGEVFG **
+QT911\aSNYY  ARRQ 1FG ERFCBAQREF  964 OBBFGRE PYHO EBNQ CRE RZF
+QT911\a2245 OBBFGRE PYHO EQ YNXR FRZVABYR FUBC SVER NYNEZ
+QT911\aOehfu Sver - 168 Funrssre Eq\aOehfu Sver ng 168 Funrssre Eq - Snprivyyr & Sbjyfgbja qvfcngpurq
+QT911\aFGEHPGHER SVER \aFGEHPGHER SVER ** 140 SYBEVQN NIR ** SNPRIVYYR QVFGEVFG **\a>-------> arryfabyrf <--------<
+
+*/
+
+choyvp pynff TNQrpnghePbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp TNQrpnghePbhaglCnefreGrfg() {
+    frgCnefre(arj TNQrpnghePbhaglCnefre(), "QRPNGHE PBHAGL", "TN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgArryFabyrf() {
+
+    qbGrfg("G1",
+        "QT911\aJERPX JVGU VAWHEVRF   PBBY FCEVATF EBNQ NG GUR  TN/ SYN YVAR",
+        "PNYY:JERPX JVGU VAWHEVRF",
+        "NQQE:PBBY FCEVATF EBNQ",
+        "VASB:NG GUR  TN/ SYN YVAR");
+
+    qbGrfg("G2",
+        "QT911\aFGEHPGHER SVER\aFGEHPGHER SVER ** 140 SYBEVQN NIR ** SNPRIVYYR QVFGEVFG **",
+        "PNYY:FGEHPGHER SVER **",
+        "NQQE:140 SYBEVQN NIR",
+        "VASB:** SNPRIVYYR QVFGEVFG **");
+
+    qbGrfg("G3",
+        "QT911\aSNYY  ARRQ 1FG ERFCBAQREF  964 OBBFGRE PYHO EBNQ CRE RZF",
+        "PNYY:SNYY  ARRQ 1FG ERFCBAQREF",
+        "NQQE:964 OBBFGRE PYHO EBNQ",
+        "VASB:CRE RZF");
+
+    qbGrfg("G4",
+        "QT911\a2245 OBBFGRE PYHO EQ YNXR FRZVABYR FUBC SVER NYNEZ",
+        "PNYY:YNXR FRZVABYR FUBC SVER NYNEZ",
+        "NQQE:2245 OBBFGRE PYHO EQ");
+
+    qbGrfg("G5",
+        "QT911\aOehfu Sver - 168 Funrssre Eq\aOehfu Sver ng 168 Funrssre Eq - Snprivyyr & Sbjyfgbja qvfcngpurq",
+        "PNYY:Oehfu Sver",
+        "NQQE:168 Funrssre Eq",
+        "VASB:- Snprivyyr & Sbjyfgbja qvfcngpurq");
+
+    qbGrfg("G6",
+        "QT911\aFGEHPGHER SVER \aFGEHPGHER SVER ** 140 SYBEVQN NIR ** SNPRIVYYR QVFGEVFG **\a>-------> arryfabyrf <--------<",
+        "PNYY:FGEHPGHER SVER **",
+        "NQQE:140 SYBEVQN NIR",
+        "VASB:** SNPRIVYYR QVFGEVFG **");
+    
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj TNQrpnghePbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
