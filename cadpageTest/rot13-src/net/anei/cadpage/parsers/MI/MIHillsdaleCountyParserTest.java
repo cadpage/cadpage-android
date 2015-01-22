fb1cf1b393ccbbb5165a7cb9a99fb5b708d1624d
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-01-22 03:54:31 +0000

    Checking in Hillsdale County, MI

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MI/MIHillsdaleCountyParserTest.java
@@ -0,0 +1,113 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZV;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+
+/*
+Uvyyfqnyr Pbhagl, ZV
+
+Pbagnpg: Fpbgg Sreevre <FSreevre@inyvpbe.pbz>
+Fraqre: uppq@pb.uvyyfqnyr.zv.hf
+Flfgrz: VQ argjbexf 3.3
+
+SVER-FGEPG, 12171 SEBAGVRE EQ, JBBQOEVQTR GJC, ZV  49232, R PNZQRA EQ//R UNEZBA EQ, 1/12/2015 9:02:28 NZ
+SVER-FGEPG, 13520 TVYZBER EQ, PNZQRA GJC, ZV  49232, NHFGVA EQ//FNZCFBA EQ, 1/2/2015 7:49:40 CZ
+NPP-CV, J PNZQRA EQ/CNGEVPX EQ, PNZQRA GJC, //, 12/26/2014 6:15:04 NZ
+SVER-FGEPG, 201 A ZNVA FG, PNZQRA IVYYNTR, ZV  49232, NYIBEQ FG//FGRJNEG NIR, 12/17/2014 3:34:06 NZ
+SVER-FGEPG, 7451 QVZZREF EQ, PNZQRA GJC, ZV  49232, NOOBGG EQ//F RQBA EQ, 12/2/2014 7:47:42 NZ
+ZRQ-NFG, 13089 PNECRAGRE EQ, NZOBL GJC, ZV  49232, ENVYEBNQ FG//RJVAT FG, 10/15/2014 1:18:36 NZ
+
+14 fgngvbaf
+
+1 Nyyra
+2 Pnzqra
+3 Uvyyfqnyr Pvgl
+4 Uvyyfqnyr  Eheny
+5 Wbarfivyyr 
+6 Yvgpusvryq 
+7 Zbagtbzrel 
+8 Zbfpbj 
+9 Abegu Nqnzf
+10 Cvggfsbeq 
+11 Ernqvat
+12 Fbzrefrg
+13 Jbbqoevqtr 
+14 Jnyqeba
+
+*/
+
+choyvp pynff ZVUvyyfqnyrPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ZVUvyyfqnyrPbhaglCnefreGrfg() {
+	  frgCnefre(arj ZVUvyyfqnyrPbhaglCnefre(), "UVYYFQNYR PBHAGL", "ZV");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgFpbggSreevre() {
+
+    qbGrfg("G1",
+        "SVER-FGEPG, 12171 SEBAGVRE EQ, JBBQOEVQTR GJC, ZV  49232, R PNZQRA EQ//R UNEZBA EQ, 1/12/2015 9:02:28 NZ",
+        "PNYY:SVER-FGEPG",
+        "NQQE:12171 SEBAGVRE EQ",
+        "PVGL:JBBQOEVQTR GJC",
+        "FG:ZV",
+        "K:R PNZQRA EQ/R UNEZBA EQ",
+        "QNGR:1/12/2015",
+        "GVZR:09:02:28");
+
+    qbGrfg("G2",
+        "SVER-FGEPG, 13520 TVYZBER EQ, PNZQRA GJC, ZV  49232, NHFGVA EQ//FNZCFBA EQ, 1/2/2015 7:49:40 CZ",
+        "PNYY:SVER-FGEPG",
+        "NQQE:13520 TVYZBER EQ",
+        "PVGL:PNZQRA GJC",
+        "FG:ZV",
+        "K:NHFGVA EQ/FNZCFBA EQ",
+        "QNGR:1/2/2015",
+        "GVZR:19:49:40");
+
+    qbGrfg("G3",
+        "NPP-CV, J PNZQRA EQ/CNGEVPX EQ, PNZQRA GJC, //, 12/26/2014 6:15:04 NZ",
+        "PNYY:NPP-CV",
+        "NQQE:J PNZQRA EQ & CNGEVPX EQ",
+        "PVGL:PNZQRA GJC",
+        "QNGR:12/26/2014",
+        "GVZR:06:15:04");
+
+    qbGrfg("G4",
+        "SVER-FGEPG, 201 A ZNVA FG, PNZQRA IVYYNTR, ZV  49232, NYIBEQ FG//FGRJNEG NIR, 12/17/2014 3:34:06 NZ",
+        "PNYY:SVER-FGEPG",
+        "NQQE:201 A ZNVA FG",
+        "PVGL:PNZQRA IVYYNTR",
+        "FG:ZV",
+        "K:NYIBEQ FG/FGRJNEG NIR",
+        "QNGR:12/17/2014",
+        "GVZR:03:34:06");
+
+    qbGrfg("G5",
+        "SVER-FGEPG, 7451 QVZZREF EQ, PNZQRA GJC, ZV  49232, NOOBGG EQ//F RQBA EQ, 12/2/2014 7:47:42 NZ",
+        "PNYY:SVER-FGEPG",
+        "NQQE:7451 QVZZREF EQ",
+        "PVGL:PNZQRA GJC",
+        "FG:ZV",
+        "K:NOOBGG EQ/F RQBA EQ",
+        "QNGR:12/2/2014",
+        "GVZR:07:47:42");
+
+    qbGrfg("G6",
+        "ZRQ-NFG, 13089 PNECRAGRE EQ, NZOBL GJC, ZV  49232, ENVYEBNQ FG//RJVAT FG, 10/15/2014 1:18:36 NZ",
+        "PNYY:ZRQ-NFG",
+        "NQQE:13089 PNECRAGRE EQ",
+        "PVGL:NZOBL GJC",
+        "FG:ZV",
+        "K:ENVYEBNQ FG/RJVAT FG",
+        "QNGR:10/15/2014",
+        "GVZR:01:18:36");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ZVUvyyfqnyrPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
