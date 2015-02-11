38aaead7a43772b8875608ce05ecd22f8986fd74
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-02-11 22:47:20 +0000

    Checking in parser instructinos

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OR/ORMultnomahCountyCParserTest.java
@@ -0,0 +1,127 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BE;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Zhygabznu Pbhagl, BE
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Zbhagnva Jnir F.N.E.
+Ybpngvba: Terfunz, BE, Havgrq Fgngrf
+
+(911) 911;27500 J Yrt Ebnq;Tbireazrag Pnzc;15-012 PPFB Snyyra pyvzoref, PBZF & Ynaqvat Mbar arrqrq. ZJ31 503-780-4145.
+(911) 911; Jnupyryyn snyyf;Pnfpnqr Ybpxf;15-011 1-30-15 23:59 X9 npgvingvba sbe\aUbbq Evire 3 zvffvat uvxref. Arrq nve fprag qbt, fhccbeg, pbzzf. Pbagnpg ZJ8\aEhff 503-200-0447
+(911) 911;Gevyyvhz Ynxr Genvyurnq;Tbireazrag Pnzc;15-009 1.15.15@20:30 PPFB Zvffvat Uvxref.(4) PBZF. ZJ31 503-780-4145.
+(911) 911;45 ebnq ng Uhagre ebnq;Pbygba;15-007 1.22.15@5:45 PPFB erdhrfgvat (2) rkgerzr 4k4 gb trg vagb na nern orybj Tbng Zbhagnva jurer fbzrbar sryy. Fznyy PBZF grnz gbb. ZJ31
+(911 10:50) 911;24800 R Ybyb Cnff Eq;Eubqbqraqeba;15-006 @10:50 Erdhrfgvat NVEFPRAG & TEBHAQ FRNEPUREF. ZJ31\a\a\aZJ31
+(911 9:30) 911;24800 R Ybyb Cnff Eq.;Eubqbqraqeba;15-006@9:30 PPFB Ybfg uvxre. Onfr sberfg freivpr znvagranapr ybg. Shyy PBZF. ZJ31 503-780-4145.
+(911 abj) 911;70000 H.F. 26;Eubqbqraqeba;15-006;1.20.15@7:20 PPFB ybfg uvxre, PBZF erdhrfgrq. Onfr Fnyzba Evire Eq Genvy urnq. (Fhojnl Jrypurf) ZJ31 503-780-4145.
+(911) 911;Fnyzba Evire Eq;Jrypurf;15-006 PPFB @4:27 zvffvat uvxre, Onfr ba Fnyzba Evire ebnq ng genvy urnq. fznyy PBZF grnz arrqrq gb fhccbeg CAJ. ZJ31 503-780-4145.
+(911 ERYVRS CYNA ARRQRQ) 911;7000 R Ujl 26;Jrypurf;15-005;1-11-15@02:37 PPFB Fgvyy arrq gb VQRAGVSL\an fgnaqol eryvrs perj sbe 0800, 1 yrnq cyhf 3 perj. Fhowrpg unf orra\aybpngrq ohg rkgenpgvba vf tbvat gb gnxr n juvyr. Cyrnfr pbagnpg ZJ66\a503-866-0178 sbe shegure.
+(911 Fgnaqol Eryvrs) 911;90290 H.F. 26;Tbireazrag Pnzc;15-004;1-11-15@0203 PPFB\aArrq fgnaqol eryvrs bs 3 perj naq 1 yrnq gung pna erfcbaq sbe 0800. ZJ66 503-866-0178
+(911) 911;90290 H.F. 26;Tbireazrag Pnzc;15-004;1-10-15@21:06 PPFB Onfr ng Mvtmnt\aEnatre Fgngvba sbe zvffvat uvxr ba Zg Ubbq Pbzzf naq 4k4 arrqrq. ZJ50\a503-327-3434 800Zum
+(911) 911;27500 J Yrt Ebnq;Tbireazrag Pnzc;15-004 1.7.15@10:01 PPFB Onfr Gvzoreyvar, vawherq 60lbz fxvre. PBZF, bayl 1 erfcbaqre. Arrq zber uryc. ZJ31 503-780-4145.\a\a\aZJ31
+(911) 911;27500 J Yrt Ebnq;Gvzoreyvar Ybqtr;15-004 1.7.15@9:15 PPFB Onfr Gvzoreyvar, vawherq 60lbz fxvre. ZJ31 503-780-4145.
+(911) 911;2104 AR Nyoregn;Cbegynaq;15-003;1-2-15@10:05 ZPFB UEQ X9/Fhccbeg.Onfr Nzrevpna Yrtvba Unyy. ZJ31 503-780-4145.
+(911) 911;4815 A.R. 15 nir;Cbegynaq;15-002;ZPFB Rivqrapr UEQ X9'f & Fhccbeg. ZJ31 50-780-4145.
+(911) 911:Rntyr Perrx Genvyurnq;Pnfpnqr Ybpxf;14-151;12.30.14@22:05 Ubbq Evire, Zvffvat uvxre, X9 NVEFPRAG & PBZF, Rkvg 41 V84. ZJ31 503-780-4145
+(911 Fhaqnl 0700-1500) 911;Uvtujnl 26;Tbireazrag Pnzc;14-150 JNFPB Pb. ZC62 Ujl 26, 12-13-14@16:30 Qnl 5, PBZF & YRNQ ARRQRQ. ZJ31 503-780-4145.
+(911 Fngheqnl 0700) 911;Sebt Ynxr Fab-Cnex;Tbireazrag Pnzc;14-149 12-12-14@17:23 JNFPB Fngheqnl 0700. PBZF, 4k4'f, X9'f Erdhrfgrq. Grkg zr lbhe ninvynovyvgl. ZJ31 503-780-4145.
+(911 Sevqnl 7nz X9) 911;Sebt Ynxr Fab Cnex;Tbireazrag Pnzc;14-148;12.11.14@18:00 JNFPB Pb. Onfr ZC62 Ujl26, PBZF & X9 NVEFPRAG. Arrq Qvrfry shry.Fgneg 0700 Sevqnl. ZJ31 503-780-4145.
+(911 SEVQNL 7nz) 911;45.2292281-121.6992948;Sebt Ynxr;14-147;12.11.14@13:15 JNFPB Pb. PBZF GRNZ 0700 Sevqnl. ZJ31 503-780-4145.
+(911 Nsgreabba eryvrs) 911;45.2292281  -121.6992948;Sebt Ynxr;14-147;12.11.14@12:42 JNFPB Pb. Arrq PBZf eryvrs yngr nsgreabba 17-1800vfu) ZJ31 503-780-4145.\a\a\aZJ31
+(911 Guhefqnl 7nz Yrnq) 911;45.2292281  -121.6992948;Sebt Ynxr;14-146;12.10.14@16:30;JNFPB Pb. Arrq Pbzznaq/YRNQ sbe 7nz Guhefqnl. ZJ31 503-780-4145.\a\a\aZJ31
+(911 CZ Eryvrs & 7nz Guhefqnl) 911;45.2292281  -121.6992948;Sebt Ynxr;14-146;12-10.14@10:00 JNFPB Pb. Pnzcre, Nfxvat sbe NINVYNOVYVGL sbe bireavtug bcrengvbaf & 7nz Guhefqnl.ZJ31
+(911 JRQARFQNL 7nz) 911;Zvyrcbfg 62 UJL26;Sebt Ynxr;14-146;JNFPB Pb;\a12.10.14@7nz Fgneg gvzr - Zvffvat Pnzcre, erdhrfgvat PBZF & NVEFPRAG X9. ZJ31 503-780-4145.
+(911) 911;Zvyrcbfg 62 UJL26;Sebt Ynxr;14-146;JNFPB Pb;\a12.9.14@17:30 Zvffvat Pnzcre, erdhrfgvat PBZF & NVEFPRAG X9. ZJ31 503-780-4145. Sebt Ynxr Fab-Cnex 12 zvyrf rnfg Tbi.Pnzc.\a\a\aZJ31
+(911 Ghrfqnl 9NZ) 911;230 FJ Zvyy Fgerrg;Furevqna;14-144;12-2-14 Furevqna Pvgl Unyy @9nz Lnzuvyy Pb Rivqrapr Vairfgvtngvba UEQ X9, X9 Fhccbeg. ZJ31 503-780-4145.
+(911) 911; Gvzoreyvar Ybqtr; 11.26.2014@16:00<znvygb:11.26.2014@16:00> PPFB Zvffvat Pyvzore Arrq Pbzf. VGP vf ZJ66 503-866-0178. Zbavgbevat Fvyxr naq Zg Ubbq IUS.
+(411) 11.19.14@8:20 ZPFB zvffvba 14-142. Fhowrpg ybpngrq qrprnfrq ol X9 Arkhf. Ab shegure erfbheprf arrqrq. Jenc hc 2-3 ubhef. ZJ31
+(911 Eryvrs 17:00) 911;102aq/Sbfgre;Cbegynaq;14-141;11.18.14@13:36 X9GG, PBZF. ZJ31 503-780-4145.
+(911) 911;ZPFB Unafba ohvyqvat\a122aq/Tyvfna;Cbegynaq;14-141;11.18.14@8:43 X9GG, Fhccbeg, PBZF. ZJ31 503-780-4145.\a\a\a\aV srry cevivyrtrq gb orybat gb n tebhc bs crbcyr jub jvyy qebc jung gurl ner qbvat, gb uryc fbzrbar gurl qba'g xabj, sbe ABGUVAT va erghea. ZJ31
+(911) 911;ZPFB Unafba ohvyqvat\a122aq/Tyvfna;Cbegynaq;14-141;11.18.14@8:43 X9GG, Fhccbeg, PBZF. ZJ31 503-780-4145.
+(911) 911;25 AJ Jnancn Fg.;Pnfpnqr Ybpxf;14-140;11.17.14@20:10 Ubbq Evire Pb. X9 GG Fhccbeg. ZJ31
+(911 - 7nz CRBCYR ARRQRQ) 911;24800 R Ybyb Cnff Eq.;Eubqbqraqeba;14-135;11.9.\a14@00:02 PPFB Ybfg uvxre. Onfr sberfg freivpr znvagranapr ybg.  ARRQ\aCRBCYR. PBZF eryvrs & 4K4. ZJ57 503-467-9377
+(911 Fhaqnl 7nz) 911;24800 R Ybyb Cnff Eq.;Eubqbqraqeba;14-135;11.9.\a14@00:02 PPFB Ybfg uvxre. Onfr sberfg freivpr znvagranapr ybg. PBZF eryvrs\a& 4K4. ZJ57 503-467-9377
+(911) 911;24800 R Ybyb Cnff Eq.;Eubqbqraqeba;14-135;11.8.14@20:07 PPFB Ybfg uvxre. Onfr sberfg freivpr znvagranapr ybg. PBZF. ZJ31 503-780-4145.
+(911 Fngheqnl 8nz) 911;Treznagbja Eq/Fxlyvar;Cbegynaq;14-132;11.4.14@11:45 ZPFB sbe Fngheqnl 11.8.14@8nz Onfr teniry ybg Treznagbja/Fxlyvar. Erdhrfgvat Pbzzf & UEQ qbtf. ZJ31 503-780-4145.
+(911) 911;2060 AR Ynsnlrggr Nir;ZpZvaaivyyr;14-131;11.3.14@6:24 Lnzuvyy Pbhagl, Ybfg zhfuebbz cvpxref. Onfr Pbhagl Fubcf. 4k4'f & Pryy genpxvat.ZJ31 503-780-4145.
+(911 Fnaql) 911;17460 Oehaf Nir;Fnaql;14-130;11.2.14@6:30 PPFB zhfuebbz cvpxre.Pbzf, ERCRNGRE ARRQRQ. Onfr Fnaql Sver Fgngvba. ZJ31 503-780-4145.\a\aFrag sebz zl vCubar
+(911 Fnaql) 911;17460 Oehaf Nir;Fnaql;14-130;11.2.14@5:52 PPFB zvffvat zhfuebbz cvpxre.Pbzf & 4k4'f. Onfr Svejbbq Sverfgngvba. ZJ31 503-780-4145.
+(911 Abj) 911;UJL 19 & UJL 206;Pbaqba; Tvyyvnz Pbhagl Erdhrfg GG & NVEFPRAG X9. Zvffvat ryqreyl zna jnyxrq njnl sebz uvf pne yrsg vg ehaavat. Sver naq nzohynapr perjf ner bhg HGY. ZJ31 503-780-4145.
+(911 Fhaqnl 8nz) 911;2060 AR Ynsnlrggr Nir;ZpZvaaivyyr;14-128;11.1.14@15:15 Lnzuvyy Pbhagl, X9 UEQ, arrq Pbzf & X9 Fhccbeg. Onfr Pbhagl Fubcf @8nz. ZJ31 503-780-4145.
+(119) Fhowrpg ybpngrq naq erfcbaqref fgnaq qbja. Cyrnfr ercbeg zvyrntr. ZJ50
+(911) 911;2060 AJ 238gu Qe.;Jbbq Ivyyntr;10.24.14@18:34 ZPFB, Onfr ng 238gu naq\aUnyfrl, Zvffvat jnyx njnl, Genpxvat Genvyvat X9, ZJ50 Jrf Jnygref
+(911) 911;Gvzbgul Ynxr ASQ Ebnq 57;Rfgnpnqn;14-114;10.14.14@18:25 PPFB, Gbaa pnaabg svaq ure. Onfr ng Gvzbgul Ynxr Qnz. PBZF, NVEFPRAG 4k4'f.ZJ31 Gevpvn
+(911) 911;Gvzbgul Ynxr ASQ Ebnq 57;Rfgnpnqn;14-114;10.14.14@17:00 PPFB, Onfr ng Gvzbgul Ynxr Qnz\aZvffvat 60lbs zhfu cvpxre, PBZF,GG X9, Pryy genpxvat, Pne ba 4280 ebnq. ZJ31 Gevpvn 503-780-4145
+(911) 911;Gvzoreyvar Ybqtr;Tbireazrag Pnzc;14-111;10.3.14@18:08 PPFB, hacercnerq pyvzore arrqf nffvfgnapr pbzvat qbja. Pbzf bayl. ZJ31 503-780-4145.
+(119) 119;AJ Jbys Perrx Ebnq;Gvzore;14-108;9.23.14@21:38 Nyy havgf pyrnevat. ZJ31
+(911 Eryvrs@15:00) 911;AJ Jbys Perrx Ebnq;Gvzore;14-108;9.23.14@12:20. Onfr arrqf Pbzznaq eryvrs naq Pbzf fhccbeg ng 15:00. X9'f erdhrfgrq sbe rneyvre vs ninvynoyr. Bcrengvbany crevbq gb fgbc ng QNEX. ZJ31
+(911 Ghrfqnl 7nz) 911;AJ Jbys Perrx Eq;Gvzore;14-108;9.22.14@17:22 JPFB bireqhr uhagre, 37lb qvnorgvp.ARRQ NVEFPRAG X9 & SHYY PBZF Grnz. Zrrg ng Gvzore Whapgvba ng 7nz. Oevrsvat 7:30nz. ZJ31 503-780-4145.
+(911 ZBAQNL 9NZ) 911;AJ Jbys Perrx Eq;Gvzore;14-107;9.21.14@22:10 JPFB nffvfgvat Pbyhzovn Pb, bireqhr uhagre, 37lb qvnorgvp.ARRQ NVEFPRAG X9 & SHYY PBZF Grnz. Onfr 1 zvyr Jrfg bs Gvzore Whapgvba.ZJ31 503-780-4145.
+(911 14-106 Eryvrs #2) 911;27287 Uvyybpxohea Ebnq;Rfgnpnqn;14-106;9.20.14@22:00 PPFB Onfr ng Qbqtr\aPuhepu. PBZF, Znccvat, Zrqvpny & 4k4 Cngeby. Pbzznaq arrqrq ZJ57\a503-467-9377.
+(911 Fhaqnl 7:30nz) 911;27287 Uvyybpxohea Ebnq;Rfgnpnqn;14-106;9.20.14@22:00 PPFB Onfr fgvyy ng Qbqtr Puhepu. Zvffvba cebivqr PBZF, Znccvat, Zrqvpny & 4k4 Cngeby. ZJ31 503-780-4145.
+(911) 911;Onfr ba Uvyybpxohea ng Qbqtr puhepu;14-105; 9.20.14@16:30 PPFB,Pbzzf\a4k4'F. Sver Rinphngvba. ZJ31 503-780-4145
+(911) 911; 35800 R Uvfgbevp Pbyhzovn Evire Ujl;Pbeorgg;14-104;9.19.14@22:20\aX9 Genpxvat Genvyvat, Onfr Pbeorgg Tenqr fpubby. Qrchgl Olorr jvyy zrrg grnz@fpubby. ZJ31 503-780-4145.
+(119) 119;261 FR 5gu Nirahr;Rfgnpnqn;14-101;9-16-14@16:30 Rfgnpnqn Sver Qrcyblzrag pbzcyrgrq. Nyy havgf pyrnevat. ZJ31 503-780-4145.
+(911 ABJ) 911;261 FR 5gu Nirahr;Rfgnpnqn;14-100;9.15.14@15:20 FRPBAQ SVER FDHNJ ZGA. Ercbeg gb Rfgnpnqn Sver NFNC. Rinphngvba nffvfg. ZJ14 Ovyy VGP (503) 970-0650.
+(411 Nsgreabba/Riravat) 411;261 FR 5gu Nirahr;Rfgnpnqn;14-100;9.15.14@12:30 QNL 2 eryvrs arrqrq - jub pna uryc? Onfr Rfgnpnqn Sver, arrq PBZF & 4k4'f, bireavtug perj. ZJ14 Ovyy VGP (503) 970-0650.
+(911 Zbaqnl 0800) 911;261 FR 5gu Nirahr;Rfgnpnqn;14-100;9.14.14@20:40 QNL 2. Onfr Rfgnpnqn Sver, arrq PBZF & 4k4'f. ZJ31 503-780-4145.
+(911) 911;261 FR 5gu Nirahr;Rfgnpnqn;14-099;9.14.14@11:20, Onfr Rfgnpnqn Sver, arrq PBZF & SVRYQ GRNZ sbe rinphngvbaf ba gur hccre Pynpxnznf Evire. ZJ31 503-780-4145
+(911) 911;20024 Fvyire Snyyf Ujl FR;Fhoyvzvgl;14-098;9.12.14@13:30 sbe FNGHEQNL. JPFB nfxvat sbe ZNEVBA PB. Zna zvffvat sbe n jrrx. Onfr vagrefrpgvba bs Ybbxbhg Zg. Eq & Ujl213. Arrq X9 UEQ, NVEFPRAG & PBZF. ZJ31 503-780-4145
+(911) 911;12980 FR Nyzbaq pg.;Pynpxnznf;14-094;8.29.14@14:11 PPFB, Zvffvat 83 L/B\aNymurvzre'f, Srznyr gnyy guva\aJuvgr unve, Oebja fuveg j/sybeny cevag\aOynpx fubrf, Pbashfrq, Xabjf anzr\aWnavpr Jntare. Onfr ng Pynpxnznf Uvtu Fpubby. Erdhrfgvat: 4k4, X9 GG \aPBZF. Zvffvat 30 zvahgrf sebz \aUnccl Snzvyl Pner Ubzr. ZJ31 503-780-4145.
+(911) 911;45500 Jvyfba Evire Ujl;Gvyynzbbx;14-093;8-22-14 @8:50 Gvyynzbbx Pb. FB, erdhrfgvat Pbzf, 4k4'f, Dhnqf sbe FNGHEQNL/FHAQNL bcrengvbaf 0700-2000.ZJ31 503-780-4145
+(911) 911;45500 Jvyfba Evire Ujl;Gvyynzbbx;14-093;8-21-14@19:15 Gvyynzbbx Pb, 2 zvffvat zra, PBZF, 4k4'f, Dhnqf. Sbe 5nz fgneg gvzr Sevqnl zbeavat @ Bertba Sberfgel Pragre ZC23 ba Ujl6. ZJ31 503-780-4145
+911;Gvzoreyvar Ybqtr;Gvzoreyvar Ybqtr;14-090;08.14.14@10:50 PPFB Zvffvat\auhfo & jvsr, onfr ng Gvzoreyvar. Arrq PBZF. Ivpxl ZJ7 503-871-0716
+(911) 911;Enzban snyyf;Mvt Mnt;14-089;8.12.14@15:15 PPFB Zna sryy va gur Fnaql Evire. PBZF & 4k4'f. ZJ31 503-780-4145.
+(119) 119;Qbqtr Cnex;Fnaql;14-088;8.11.14@12:50 PPFB zvffvat fhowrpg ybpngrq. Nyy shgher erfcbaqref fgnaq qbja. ZJ64 erfcbaqvat zrqvpny. ZJ31
+(911) 911;Qbqtr Cnex;Fnaql;14-088;8.11.14@8:30 PPFB zvffvat 59lbz. X9 GenpxGenvy & Fhccbeg. Zj31 503-780-4145.
+(911) 911;Ubefrgnvy Snyyf;Pbeorgg;14-0 86;8.6.14@5:15 ZPFB zvffvat uvxre. PBZF. ZJ31 503-780-4145
+(119) 119;10500 Xvypuvf Evire Eq;\aGvyynzbbx;14-085 8.2.14@17:15 Gvyynzbbx Pb. FHOWRPG YBPNGRQ! Nyy havgf\aFgnaq Qbja! ZJ66 503-866-0178
+(119 YBPNGRQ) 911;10500 Xvypuvf Evire Eq;\aGvyynzbbx;14-085 8.2.14@17:15 Gvyynzbbx Pb. FHOWRPG YBPNGRQ! Nyy havgf\aFgnaq Qbja! ZJ66 503-866-0178
+(911) 911;10500 Xvypuvf Evire Eq;\aGvyynzbbx;14-085 8.2.14@17:15 Gvyynzbbx Pb. Zvffvat 11lbs fvapr abba. X9 NVE, GG, PBZF arrqrq. ZJ66 503-866-0178
+(411) Zvffvba 14-84;Onfr @ Gvzoreyvar Ybqtr;08-02-14;vawherq uvxre. VGP vf abj\aZJ66; 503-866-0178;zbavgbevat Fvyxr, 800 & Zg Ubbq Unz.
+(411) \a\aZvffvba 14-84;Onfr @ Gvzoreyvar Ybqtr;08-02-14;vawherq uvxre. VGP vf ZJ81;grkg 503-710-2349;zbavgbevat Fvyxr,800,NETZUI/147.120;ZJ81
+(911) 911; 27500 J Yrt Ebnq; Gvzoreyvar Ybqtr; zvffvba ahzore;8.2.14@14:30 PPFB vawherq uvxre CPG. Onfr Gvzoreyvar. PBZF ZJ31 503-780-4145
+(411) 14-081 zvffvba hcqngr. ZJ81 vf abj VGP. Zbavgbevat Fvyxl, 146.960 grkgf gb 503-710-2349.
+911;535 AR 5gu Fg;ZpZvaaivyyr;14-081;7.31.14 Lnzuvyy Pb zvffvat 38 ibs, arrq\aebnqf ena va nern. ZJ31 503-780-4145
+(911) 911;535 AR 5gu fgerrg; ZpZvaaivyyr;14-081;7.31.14@8:30-17:00 Lnzuvyy Pb. Furevss zvffvat 38lbs, arrq ebnqf ena va gur nern. ZJ31 503-780-4145.
+(911) 911;401 R 3eq Fgerrg;Arjoret;14-081;7.31.14@8:30-17:00 Lnzuvyy Pb. Furevss zvffvat 38lbs, arrq ebnqf ena va gur nern. Gur nqqerff vf gur Arjoret pvgl unyy - V qb abg xabj jurer onfr vf be vs gurer vf bar. V whfg hfrq vg sbe ersrerapr. ZJ31 503-780-4145.
+(14-078 119) Nyy havgf ner frphevat.  Cyrnfr gkg raqvat zvyrntr naq gvzr jvgu ZJ # \agb 360-907-3663.  GAK  Eba
+(411) \a411;9157 AJ Trezna Gbja Ebnq;Cbegynaq;14-078;7.28.14@20:50 NQRDHNGR \aNFFRGF NG ONFR AB SHEGURE ARRQRQ GUNAXF ZJ67 360-907-3663
+\a411;9157 AJ Trezna Gbja Ebnq;Cbegynaq;14-078;7.28.14@16:30 ZPFB ARRQ \aGENAFCBEG SBE X9f. ZJ67 360-907-3663
+(Er: 911) Ba Zbaqnl, Whyl 28, 2014, Gevpvn Urjyrgg <urjyrgg.gevpvn@lnubb.pbz> jebgr:\a\a> 911;9157 AJ Trezna Gbja Ebnq;Cbegynaq;14-078;7.28.14@16:30 ZPFB Arrq X9\a> NVE & GG. ZJ31 503-780-4145\a>\a>\a\a-- \aFrag sebz Tznvy ba zl vCubar.
+(911) 911;9157 AJ Trezna Gbja Ebnq;Cbegynaq;14-078;7.28.14@16:30 ZPFB Arrq X9 NVE & GG. ZJ31 503-780-4145
+(119) 119;Enzban Snyyf;Mvt Mnt;14-077;7.27.14@22:16; Fhowrpg ybpngrq. Nyy havgf ergheavat. Ercbeg erghea gb ZJ66 cyrnfr.
+(411 Onfr Punatr) 411;Onfr punatrq gb Enzban Snyyf Genvyurnq ;Enzban Snyyf;14-077;7.27.14@19:50;ZJ66 503-866-0178. Zbavgbevat Fvyxr, 800 naq Zg Ubbq IUS Unz. Onfr vf abj Enzban Snyyf
+(411 VGP) 411;Onfr ng HFSF znvag lneq bss Ybyb Cnff;Mvt Mnt;14-077;7.27.14@18:38;VGP vf ZJ66 503-866-0178. Zbavgbevat Fvyxr, 800 naq Zg Ubbq IUS Unz.
+(911) 911;Onfr ng HFSF znvag lneq bss Ybyb Cnff;Mvt Mnt;14-077;7.27.14@18:15 PPFB FCBG npgvingvba, PBZF. ZC41 Ujl26. ZJ31 503-780-4145.
+(119) 119;32788 Yvggyr Abegusbex Eq FR;\aYlbaf;14-076;7.27.14@11:20 Znevba Pb. Fhowrpgf ybpngrq. Jr unq ab bar tb....ZJ31
+(911) 911;32788 Yvggyr Abegusbex Eq FR;\aYlbaf;14-076;7.27.14@3:15 Znevba Pb.Onfr Ryx Ubea Sver Fgngvba arne Bcny perrx genvy urnq bss Ujl 22. 3 zvffvat grraf. PBZF & Tebhaq cbhaqref. ZJ31 503-780-4145.
+(911) 911;69634 H.F. 26, Jrypurf;14-072;7.15.14@14:42 PPFB Qnl 2, Zvyjnhxvr CQ nffvfg. Arrq Shyy Pbzf & 4k4'f gb eha ebnqf va Jrypurf nern gbavtug. Onfr Ubbqynaq Sver. ZC40 Ujl 26 ZJ31 503-780-4145
+(119) 119;90290 H.F. 26;Tbireazrag Pnzc;14-071;PPFB 7.14.14@22:10 Zvffvat 51lbs, zvffvba qbar @Terra Evire PT bss Fnyzba Evire Eq. ZJ31
+(911) 911;90290 H.F. 26;Tbireazrag Pnzc;14-071;PPFB 7.14.14@17:00 Zvffvat 51lbs, FZNYY PBZF grnz. Mvt Mnt Enatre Fgngvba ZC41 UJL26. ZJ31 503-780-4145.
+
+Cerggl onfvP PNYY NQQE PVGL VQ VASB
+VQ vf fbzrgvzrf pbzovarq jvgu vasb svryq.
+VASB svryq *hfhnyyl* fgnegf bhg jvgu n qngr/gvzr
+
+*/
+
+choyvp pynff BEZhygabznuPbhaglPCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BEZhygabznuPbhaglPCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "ZHYGABZNU PBHAGL", "BE");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+  }
+
+  
+  choyvp fgngvp ibvq znva(Fgevat netf[]) {
+    arj BEZhygabznuPbhaglPCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
