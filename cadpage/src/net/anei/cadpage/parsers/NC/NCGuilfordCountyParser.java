604746138ede5e939e1a402d760ee80a3edfdb43
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-02-19 03:10:22 +0000

    Fixed parsing problems with Guilford County,  NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCGuilfordCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCGuilfordCountyParser.java
@@ -1,6 +1,9 @@
 package net.anei.cadpage.parsers.NC;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,12 +19,12 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
   
   public NCGuilfordCountyParser() {
     super("GUILFORD COUNTY", "NC",
-           "FYI? ID? ( CALL2 ADDR! | PRI/Z MUTUAL ADDR! | ( SRC SRC PRI | PRI? ) CODE? CALL ADDR! ) EXTRA? ( X X? | PLACE X X? | ) ( PRI UNIT SRC SRC | ) XINFO+? UNIT");
+          "FYI? ID? ( CALL2 ADDR! | PRI/Z MUTUAL ADDR! | ( SRC SRC PRI | PRI? ) CODE? CALL ADDR! ) CODE? CITY? ( NAME ID | ID? ) EXTRA? ( X X? | PLACE X X? | ) ( PRI UNIT? SRC SRC | ) XINFO+? UNIT CITY? INFO+");
   }
   
   @Override
   public String getFilter() {
-    return "@edispatches.com,93001,firedistrict13@listserve.com,CAD@greensboro-nc.gov,mhfd38all@listserve.com";
+    return "@edispatches.com,93001,firedistrict13@listserve.com,CAD@greensboro-nc.gov,mhfd38all@listserve.com,@summerfieldfire.com";
   }
   
   @Override
@@ -47,6 +50,20 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
     return super.getProgram().replace("SRC", "SRC CITY");
   }
 
+  @Override
+  protected Field getField(String name) {
+    if (name.equals("ID")) return new IdField("\\d{7,}", true);
+    if (name.equals("CALL2")) return new Call2Field();
+    if (name.equals("MUTUAL")) return new MutualField();
+    if (name.equals("SRC")) return new MySourceField();
+    if (name.equals("PRI")) return new PriorityField("\\d", true);
+    if (name.equals("CITY")) return new MyCityField();
+    if (name.equals("EXTRA")) return new ExtraField();
+    if (name.equals("CODE")) return new MyCodeField();
+    if (name.equals("XINFO")) return new CrossInfoField();
+    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+(?:,[A-Z]+\\d+)*", true);
+    return super.getField(name);
+  }
 
   private class Call2Field extends CallField {
     
@@ -85,8 +102,31 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
     }
   }
   
+  private class MyCityField extends CityField {
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override 
+    public boolean checkParse(String field, Data data) {
+      field = field.toUpperCase();
+      String city = CITY_CODES.getProperty(field);
+      if (city != null) {
+        data.strCity = city;
+        return true;
+      }
+      
+      if (CITY_SET.contains(field)) {
+        data.strCity = field;
+        return true;
+      }
+      
+      return false;
+    }
+  }
+  
   private static final Pattern CODE_PTN = Pattern.compile("\\d\\d[A-Z]\\d\\d[A-Za-z]?");
-  private static final Pattern APT_PTN = Pattern.compile("(?:APT |RM:|ROOM ) *([^ ]*) *(.*)");
   private class MyCodeField extends CodeField {
     public MyCodeField() {
       setPattern(CODE_PTN, true);
@@ -94,6 +134,8 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
   }
   
   // Extra information field
+  private static final Pattern APT_PTN = Pattern.compile("(?:APT|RM|ROOM|SUITE|LOT)[: ] *(.*)");
+  private static final Pattern SRC_PTN = Pattern.compile("[A-Z]{4}");
   private class ExtraField extends SkipField {
     
     @Override
@@ -107,9 +149,19 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
         data.strSupp = field;
         return true;
       }
-      if (field.startsWith("APT ")) {
-        data.strApt = field.substring(4).trim();
+      Matcher match = APT_PTN.matcher(field);
+      if (match.matches()) {
+        data.strApt = append(data.strApt, "-", match.group(1));
         return true;
+      }
+      if (NUMERIC.matcher(field).matches()) {
+        
+        // See if this might be a priority field that will be followed
+        // by two source fields.
+        if (field.length() > 1 || !SRC_PTN.matcher(getRelativeField(+2)).matches()) {
+          data.strApt = append(data.strApt, "-", field);
+          return true;
+        }
       }
       return false;
     }
@@ -124,13 +176,12 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
 
     @Override
     public void parse(String field, Data data) {
-      String city = CITY_CODES.getProperty(field);
       Matcher match = APT_PTN.matcher(field);
       if (match.matches()) {
         data.strApt = append(data.strApt, " - ", match.group(1));
-        field = match.group(2);
-        if (field.length() == 0) return;
+        return;
       }
+      String city = CITY_CODES.getProperty(field);
       if (city != null) {
         data.strCity = city;;
       } else if (field.startsWith("TAC ")) {
@@ -149,20 +200,6 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
       return "X CITY INFO CODE CH CALL";
     }
   }
-
-  @Override
-  protected Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{7,}", true);
-    if (name.equals("CALL2")) return new Call2Field();
-    if (name.equals("MUTUAL")) return new MutualField();
-    if (name.equals("SRC")) return new MySourceField();
-    if (name.equals("PRI")) return new PriorityField("\\d", true);
-    if (name.equals("EXTRA")) return new ExtraField();
-    if (name.equals("CODE")) return new MyCodeField();
-    if (name.equals("XINFO")) return new CrossInfoField();
-    if (name.equals("UNIT")) return new UnitField("[A-Z]+\\d+(?:,[A-Z]+\\d+)*", true);
-    return super.getField(name);
-  }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "A",    "ARCHDALE",
@@ -179,9 +216,11 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
       "GI",   "GIBSONVILLE",
       "GUIL", "GUILFORD COUNTY",
       "H",    "HIGH POINT",
+      "HP",   "HIGH POINT",
       "J",    "JAMESTOWN",
       "JU",   "JULIAN",
       "K",    "KERNERSVILLE",
+      "KV",   "KERNERSVILLE",
       "L",    "LIBERTY",
       "M",    "MCLEANSVILLE",
       "OAK",  "OAK RIDGE",
@@ -197,5 +236,84 @@ public class NCGuilfordCountyParser extends DispatchOSSIParser {
       "W",    "WHITSETT",
       "X",    "UNIDENTIFIED"
   });
+  
+  private static final Set<String> CITY_SET = new HashSet<String>(Arrays.asList(
+
+      // Cities
+      "ARCHDALE",
+      "BURLINGTON",
+      "GREENSBORO",
+      "HIGH POINT",
+
+      // Towns
+      "GIBSONVILLE",
+      "JAMESTOWN",
+      "KERNERSVILLE",
+      "OAK RIDGE",
+      "PLEASANT GARDEN",
+      "SEDALIA",
+      "STOKESDALE",
+      "SUMMERFIELD",
+      "WHITSETT",
+
+      // Townships
+      "BRUCE",
+      "CENTER GROVE",
+      "CLAY",
+      "DEEP RIVER",
+      "FENTRESS",
+      "FRIENDSHIP",
+      "GILMER",
+      "GREENE",
+      "HIGH POINT",
+      "JAMESTOWN",
+      "JEFFERSON",
+      "MADISON",
+      "MONROE",
+      "MOREHEAD",
+      "OAK RIDGE",
+      "ROCK CREEK",
+      "SUMNER",
+      "WASHINGTON",
+
+      // Census-designated places
+      "FOREST OAKS",
+      "JULIAN",
+      "MCLEANSVILLE",
+
+      // Unincorporated communities
+      "BROWN SUMMIT",
+      "CLIMAX",
+      "COLFAX",
+      "MONTICELLO",
+      
+      // Alamance County
+      "ELON",
+      
+      // Davidson County
+      "THOMASVILLE",
+      
+      // Forsyth County
+      "KERNERSVILLE",
+      
+      // Guilford County
+      "SEDALIA",
+      
+      // Randolph County
+      "LIBERTY",
+      "RANDLEMAN",
+      
+      // Rockingham County
+      "EDEN",
+      "REIDSVILLE",
+      
+      // Counties
+      "ALAMANCE COUNTY",
+      "DAVIDSON COUNTY",
+      "FORSYTH COUNTY",
+      "GUILFORD COUNTY",
+      "RANDOLPH COUNTY",
+      "ROCKINGHAM COUNTY"
+  ));
   
 }
