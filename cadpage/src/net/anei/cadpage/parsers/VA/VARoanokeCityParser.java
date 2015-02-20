b15e3d652fc4bbc486a83d609cb55a167cb39c32
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-02-20 04:33:10 +0000

    Fixed parsing problem with Roanoke, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VARoanokeCityParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VARoanokeCityParser.java
@@ -1,16 +1,15 @@
 package net.anei.cadpage.parsers.VA;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.SmartAddressParser;
+import net.anei.cadpage.parsers.dispatch.DispatchPrintrakParser;
 
 /**
  * Roanoke City, VA
  */
-public class VARoanokeCityParser extends SmartAddressParser {
+public class VARoanokeCityParser extends DispatchPrintrakParser {
   
   public VARoanokeCityParser() {
     super("ROANOKE CITY", "VA");
-    setFieldList("CALL ADDR APT INFO");
   }
   
   @Override
@@ -21,9 +20,23 @@ public class VARoanokeCityParser extends SmartAddressParser {
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!subject.equals("E-911")) return false;
+    
+    // Regular Printrak format
+    if (body.startsWith("PRI:")) return super.parseMsg(body, data);
+    setFieldList("CALL ADDR APT INFO");
+    
     int pt = body.indexOf(" [Att");
     if (pt >= 0) body = body.substring(0,pt).trim();
     
+    //  Structure fires have their own special format
+    if (body.startsWith("F-STRUCTURE FIRE ")) {
+      data.strCall = body.substring(0,16);
+      parseAddress(StartType.START_ADDR, body.substring(17).trim(), data);
+      data.strSupp = getLeft();
+      return true;
+    }
+    
+    // Otherwise fallback to old format, that may or may not still be in use
     if (body.length() < 21 || body.charAt(20) != ' ') return false;
     data.strCall = body.substring(0,20).trim();
     body = body.substring(21).trim();
