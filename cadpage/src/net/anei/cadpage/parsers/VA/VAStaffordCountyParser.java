aec7453be9d2f428d8c9d32eec9af8f4896b0383
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-02-22 02:38:29 +0000

    Checking in Stafford County, VA

---
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAStaffordCountyParser.java
@@ -0,0 +1,22 @@
+package net.anei.cadpage.parsers.VA;
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+
+
+public class VAStaffordCountyParser extends FieldProgramParser {
+  
+  
+  public VAStaffordCountyParser() {
+    super("STAFFORD COUNTY", "VA",
+          "CALL:CALL! PLACE:PLACE! ADDR:ADDR! CITY:CITY! ID:ID! PRI:PRI! DATE:DATE! TIME:TIME! UNIT:UNIT! INFO:INFO");
+  }
+
+  @Override
+  protected Field getField(String name) {
+    if (name.equals("ID")) return new IdField("\\d{10}", true);
+    if (name.equals("PRI")) return new PriorityField("\\d", true);
+    if (name.equals("DATE")) return new DateField("\\d\\d/\\d\\d/\\d{4}", true);
+    if (name.equals("TIME")) return new TimeField("\\d\\d:\\d\\d:\\d\\d", true);
+    return super.getField(name);
+  }
+}
