fd48c0658025b676893c7bbb71b602892ee1d332
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-03-29 17:44:23 +0000

    Fixed parsing problem with Butler County, KS

--- cadpage/src/net/anei/cadpage/parsers/KS/KSButlerCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/KS/KSButlerCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.KS;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -10,15 +11,13 @@ import net.anei.cadpage.parsers.dispatch.DispatchProQAParser;
  * Butler County, KS
  */
 public class KSButlerCountyParser extends FieldProgramParser {
-  
-  private static final String DEF_CITY = "BUTLER COUNTY";
-  private static final String DEF_STATE = "KS";
-  
-  private static final Pattern PARENS = Pattern.compile("\\(([A-Z ]+)\\):");
+
+  private static final Pattern ALT_MASTER = Pattern.compile("(?:([A-Z0-9]+)  )?([A-Z0-9]+) (.*) (\\d{4}-\\d{8})");
+  private static final Pattern PARENS = Pattern.compile("\\((ADDRESS|MAP PAGE|CROSS ST|NAME|RP PHONE NUMBER|NARR)\\):?");
 
   public KSButlerCountyParser() {
-    super(CITY_LIST, DEF_CITY, DEF_STATE,
-           "CALL! ADDRESS:ADDR/iS! MAP_PAGE:MAP CROSS_ST:X MAP_PAGE:MAP RP:NAME RP_PHONE_NUMBER:PHONE NARR:INFO");
+    super(CITY_LIST, "BUTLER COUNTY", "KS",
+           "CALL! ADDRESS:ADDR/iS! MAP_PAGE:MAP CROSS_ST:X MAP_PAGE:MAP NAME:NAME RP_PHONE_NUMBER:PHONE NARR:INFO");
   }
   
   @Override
@@ -30,21 +29,37 @@ public class KSButlerCountyParser extends FieldProgramParser {
   protected boolean parseMsg(String subject, String body, Data data) {
     
     if (!subject.contains("Incident Notification")) return false;
-    body = PARENS.matcher(body).replaceAll(" $1:");
-    body = body.replace(" (RP PHONE NUMBER) ", " RP PHONE NUMBER: "); 
-    if (super.parseMsg(body, data)) {
-      
-      // They use "COUNTY" as a generic city name when the alert is in
-      // Butler County.  But they also put the name of a neighboring
-      // count in the address for mutual aid calls.  In both cases the
-      // "County" city name should go away, but in the later case it
-      // should be appended back to the address.
-      if (data.strCity.equalsIgnoreCase("COUNTY")) {
-        if (!data.strAddress.contains(" ")) data.strAddress = append(data.strAddress, " ", data.strCity);
-        data.strCity = "";
+    
+    Matcher match = ALT_MASTER.matcher(body);
+    if (match.matches()) {
+      setFieldList("SRC UNIT ADDR APT CITY CALL ID");
+      data.strSource =  getOptGroup(match.group(1));
+      data.strUnit = match.group(2);
+      String sAddr = match.group(3).trim();
+      data.strCallId = match.group(4);
+      parseAddress(StartType.START_ADDR, sAddr, data);
+      data.strCall = getLeft();
+      if (data.strCall.length() == 0) {
+        return data.parseGeneralAlert(this, body);
       }
-    } else {
-      data.parseGeneralAlert(this, body);
+    }
+    
+    else {
+      body = body.replace("(RP)", "(NAME)");
+      body = PARENS.matcher(body).replaceAll(" $1:");
+      if (!super.parseMsg(body, data)) {
+        return data.parseGeneralAlert(this, body);
+      }
+    }
+    
+    // They use "COUNTY" as a generic city name when the alert is in
+    // Butler County.  But they also put the name of a neighboring
+    // count in the address for mutual aid calls.  In both cases the
+    // "County" city name should go away, but in the later case it
+    // should be appended back to the address.
+    if (data.strCity.equalsIgnoreCase("COUNTY")) {
+      if (!data.strAddress.contains(" ")) data.strAddress = append(data.strAddress, " ", data.strCity);
+      data.strCity = "";
     }
     return true;
   }
