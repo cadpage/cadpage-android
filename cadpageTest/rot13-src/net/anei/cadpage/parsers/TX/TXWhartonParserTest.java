f927890b707848c0f7861060e944f307c234ef7a
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-04-07 05:14:40 +0000

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/TX/TXWhartonParserTest.java
@@ -0,0 +1,117 @@
+cnpxntr arg.narv.pnqcntr.cnefref.GK;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Junegba, GK
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pvgl bs Junegba RZF
+Ybpngvba: Junegba, GK, Havgrq Fgngrf
+Fraqre: "pnqnyregf@rsbeprfbsgjner.pbz" <pnqnyregf@rsbeprfbsgjner.pbz>
+
+(Qvfcngpu Nyreg) Pnyy Glcr: PNEQVNP Pvgl: JUNEGBA Nqqerff: 501 J JVYYBJ YA
+(Qvfcngpu Nyreg) Pnyy Glcr: NAVZNY OVGR Pvgl: JUNEGBA Nqqerff: 1108 A SHYGBA FG
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: VPH GB BNXORAQ VPH
+(Qvfcngpu Nyreg) Pnyy Glcr:  Pvgl: JUNEGBA Nqqerff: 1316 XVATFGBA QE
+(Qvfcngpu Nyreg) Pnyy Glcr: QVFGHEONAPR Pvgl: JUNEGBA Nqqerff: 501 J JVYYBJ YA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 416 UNZVYGBA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 100 F SHYGBA FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 1523 PBYHZOVAR
+(Qvfcngpu Nyreg) Pnyy Glcr: ERFVQRAGVNY FGEHPGHER SVER Pvgl: JUNEGBA Nqqerff: 2500 WHAVBE PBYYRTR
+(Qvfcngpu Nyreg) Pnyy Glcr: IRUVPYR VA QVGPU  Pvgl:  Nqqerff: SZ 102/FCNAVFU PNZC
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl:  Nqqerff: 10338 F HF 59
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl: JUNEGBA Nqqerff: 11042 F FU 60 UJL
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl:  Nqqerff: 1220 FHAAL YA
+(Qvfcngpu Nyreg) Pnyy Glcr: 911 FVYRAG Pvgl:  Nqqerff: 4219 PE 125
+(Qvfcngpu Nyreg) Pnyy Glcr: PNEQVNP Pvgl:  Nqqerff: JPFB WNVY
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 402 SENAXVR
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 108 R JNLFVQR
+(Qvfcngpu Nyreg) Pnyy Glcr: NFFNHYG Pvgl: JUNEGBA Nqqerff: 407 R RZVYL
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 525 J OHEYRFBA
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl: JUNEGBA Nqqerff: 119 R ORYYR FG
+(Qvfcngpu Nyreg) Pnyy Glcr: PBZIN Pvgl:  Nqqerff: 11900 PE 170
+(Qvfcngpu Nyreg) Pnyy Glcr: NFFNHYG Pvgl:  Nqqerff: 500 F PNARL
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: OBYVAT Nqqerff: 6714 TJLARGU
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl:  Nqqerff: 315 R RYZ FG
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl: JUNEGBA Nqqerff: 1408 BEVBYR YA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 1423 A EHFX FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 4611 PE 137 EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl: JUNEGBA Nqqerff: 310 HAVIREFVGL
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 1220 FHAAL YA/JAE
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPVPH RQ 807>TNEQRA IVYYN
+(Qvfcngpu Nyreg) Pnyy Glcr: ZVABE  PBYYVFVBA Pvgl:  Nqqerff: PNARL/OHF 59
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE>BXNORAQ JVYYVNZF JNL
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: RNFG OREANEQ Nqqerff: 8115 J SZ 1161 EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: BOFGRGEVPF Pvgl: JUNEGBA Nqqerff: 501 J JVYYBJ YA
+(Qvfcngpu Nyreg) Pnyy Glcr: JRYSNER PBAPREA  Pvgl: JUNEGBA Nqqerff: EVIRE ORAQ NCGF 2500 WHAVBE PBYYRTR
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE 3>IN UBFCVGNY
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 1717 OEVNE YA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: NINYBA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: QNIVGN 103 J NUYQNT
+(Qvfcngpu Nyreg) Pnyy Glcr: PBZIN Pvgl:  Nqqerff: UJL 60 A/ 1 ZV A BS UHATRESBEQ
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl: JUNEGBA Nqqerff: 501 J JVYYBJ YA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 1220 FHAAL
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl: JUNEGBA Nqqerff: 1820 ERQ EVIRE
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 3818 SZ 1299 EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: PNEQVNP Pvgl:  Nqqerff: 103 J NUYQNT
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl: JUNEGBA Nqqerff: 320 GRAAVR FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 468 PE 150 EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: NYNEZ/OHETYNEL Pvgl: JUNEGBA Nqqerff: 1515 PBAAVR FG
+(Qvfcngpu Nyreg) Pnyy Glcr: IRUVPYR VA QVGPU  Pvgl:  Nqqerff: 1161/PE 239
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl: JUNEGBA Nqqerff: TPRE VPH 1> NGEVHZ FGNSSBEQ UBFCVGNY
+(Qvfcngpu Nyreg) Pnyy Glcr: ZVABE  PBYYVFVBA Pvgl:  Nqqerff: HAVIREFVGL/PNEGRE
+(Qvfcngpu Nyreg) Pnyy Glcr: JERPX Pvgl:  Nqqerff: UJL 59 FO/WHAVBEF
+(Qvfcngpu Nyreg) Pnyy Glcr: IRUVPYR SVER Pvgl: JUNEGBA Nqqerff: 146 F EVPUZBAQ EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: PBZIN Pvgl:  Nqqerff: UJL 59FO/ PE 212
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl: JUNEGBA Nqqerff: 514 A EHFX FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 6615 ZNPUNOVYYR EQ
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 1818 PE 219
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: G2>ZRZ GK QJGJA
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPVPH>JRFG UBHFGBA UBFCVGNY
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl:  Nqqerff: 1821 ERQ EVIRE
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: JAE
+(Qvfcngpu Nyreg) Pnyy Glcr: 911 UNATHC Pvgl: JUNEGBA Nqqerff: 320 R ZVYNZ FG/OW'F ZVAV ZNEG
+(Qvfcngpu Nyreg) Pnyy Glcr: PBZIN Pvgl:  Nqqerff: SZ 1301/ PE 107
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE >BNXORAQ WNPXFBA VPH 1
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 1007 A EVPUZBAQ EQ JUNEGBA GENPGBE
+(Qvfcngpu Nyreg) Pnyy Glcr: ZVABE  PBYYVFVBA Pvgl:  Nqqerff: EVPUZBAQ/YVAA
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE>NINYBA
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: JUNEGBA Nqqerff: 1405 INYUNYYN QE
+(Qvfcngpu Nyreg) Pnyy Glcr: BOFGRGEVPF Pvgl:  Nqqerff: SZ 1301/PE 123
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 1216 YNQRYYR
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl: RNFG OREANEQ Nqqerff: 7715 UNOREZNPURE FG
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE>ZRGU FY
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl:  Nqqerff: 9839 PE 162
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 131 UNETEBIR
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl: JUNEGBA Nqqerff: 1619 ONVYRL FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 10400 UJL 59/ 786 GEHPX FGBC
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: QVIRGN QLYNFVF  GB JUNEGBA AHEFVAT
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl:  Nqqerff: SNAPL ANVYF
+(Qvfcngpu Nyreg) Pnyy Glcr: FVPX PNYY  Pvgl:  Nqqerff: 2327 SZ 2817
+(Qvfcngpu Nyreg) Pnyy Glcr: YVSRYVAR Pvgl: JUNEGBA Nqqerff: 1504 PBAAVR FG
+(Qvfcngpu Nyreg) Pnyy Glcr: ZRQVPNY RZRETRAPL Pvgl:  Nqqerff: 9815 FB UJL 60 YNAR PVGL FGBER
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: RE 3 GB RY PNZCB
+(Qvfcngpu Nyreg) Pnyy Glcr: GENHZN Pvgl: JUNEGBA Nqqerff: 1930 OEVNE YA/URNEGFGBAR
+(Qvfcngpu Nyreg) Pnyy Glcr:  Pvgl:  Nqqerff:
+(Qvfcngpu Nyreg) Pnyy Glcr: GENAFSRE Pvgl:  Nqqerff: TPRE>RY PNZCB
+(Qvfcngpu Nyreg) Pnyy Glcr: JERPX Pvgl:  Nqqerff: HF UJL 59 AO CNFF UVAMRF
+*/
+
+choyvp pynff GKJunegbaCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp GKJunegbaCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "JUNEGBA", "GK");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj GKJunegbaCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
