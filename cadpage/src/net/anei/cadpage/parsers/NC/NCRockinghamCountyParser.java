f1e85eecee8bf56784b5357b2681202f14d9871e
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-04-10 17:30:44 +0000

    Fixed parsing problem with Rockingham County, NC

--- cadpage/src/net/anei/cadpage/parsers/NC/NCRockinghamCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NC/NCRockinghamCountyParser.java
@@ -1,8 +1,6 @@
 package net.anei.cadpage.parsers.NC;
 
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
@@ -14,21 +12,31 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
   
   public NCRockinghamCountyParser() {
     super(CITY_CODES, "ROCKINGHAM COUNTY", "NC",
-           "ID?: CALL ADDR X/Z+? CITY ( PLACE CODE UNIT | CODE UNIT | PLACE? UNIT ) CH? X? INFO+");
+           "ID?: FYI? ( ADDR/Z CITY X/Z+? CALL UNIT | CALL ADDR APT? X/Z+? ( CITY ID? APT2? ( CODE UNIT? CH? | UNIT CH? | CH | PLACE CODE UNIT? CH? | PLACE UNIT CH? | PLACE CH | ) | ID CODE? UNIT? CH? | CODE UNIT? CH? | UNIT CH? | CH ) ) INFO+");
   }
   
   @Override
   public String getFilter() {
-    return "cad@co.rockingham.nc.us";
+    return "@co.rockingham.nc.us,14101,messaging@iamresponding.com";
   }
   
   @Override
+  protected boolean parseMsg(String body, Data data) {
+    int pt = body.indexOf('\n');
+    if (pt >= 0) body = body.substring(0,pt).trim();
+
+    return super.parseMsg(body, data);
+  }
+
+  @Override
   protected Field getField(String name) {
     if (name.equals("CALL")) return new MyCallField();
-    if (name.equals("PLACE")) return new MyPlaceField();
+    if (name.equals("APT")) return new AptField("[A-Z]|\\d{1,3}", true);
+    if (name.equals("ID")) return new IdField("\\d{8}", true);
+    if (name.equals("APT2")) return new AptField("(?:APT|RM|LOT) *(.*)", true);
     if (name.equals("CODE")) return new CodeField("\\d{1,2}[A-Z]\\d{1,2}[A-Za-z]?", true);
-    if (name.equals("UNIT")) return new UnitField("[A-Z]{0,3}\\d{1,3}[A-Z]?", true);
-    if (name.equals("CH")) return new ChannelField("(?:Radio Channel: *)?(TAC.*)");
+    if (name.equals("UNIT")) return new UnitField("(?!TAC)[A-Z]{0,3}\\d{1,3}[A-Z]?", true);
+    if (name.equals("CH")) return new ChannelField("(?:Radio Channel: *)?(TAC.*)", true);
     return super.getField(name);
   }
   
@@ -51,21 +59,6 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
     }
   }
   
-  private static final Pattern PLACE_APT_PTN = Pattern.compile("APT *(.*)|((?:RM|LOT).*)", Pattern.CASE_INSENSITIVE);
-  private class MyPlaceField extends PlaceField {
-    @Override
-    public void parse(String field, Data data) {
-      Matcher match = PLACE_APT_PTN.matcher(field);
-      if (match.matches()) {
-        String apt = match.group(1);
-        if (apt == null) apt = match.group(2);
-        data.strApt = apt;
-      } else {
-        super.parse(field, data);
-      }
-    }
-  }
-  
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "EDEN", "EDEN",
       "MAD",  "MADISON",
@@ -74,6 +67,7 @@ public class NCRockinghamCountyParser extends DispatchOSSIParser {
       "REID", "REIDSVILLE",
       "RUFF", "RUFFIN",
       "STON", "STONEVILLE",
-      "STOK", "STOKESDALE"
+      "STOK", "STOKESDALE",
+      "SUMM", "SUMMERFIELD"
   });
 }
