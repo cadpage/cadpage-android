459afe4694ed2dd48cf4f7466289aef41481152c
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-05-04 20:42:27 +0000

    some skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CA/CAMarinCountyParserTest.java
@@ -0,0 +1,38 @@
+cnpxntr arg.narv.pnqcntr.cnefref.PN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Zneva Pbhagl, PN
+
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Fbhgurea Zneva Sver Qvfgevpg
+Ybpngvba: Zvyy Inyyrl, PN, Havgrq Fgngrf
+Fraqre: <PNQ@znevafurevss.bet>
+
+fzsq_nyy_fgngvbaf: 04/27/15 10:15:49 Sebz: PAFY 1:ANGHENY TNF YRNX[5259 ]41 YVAPBYA QE ,FNF ogja JVYYVNZ PG & ARINQN FG\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+fzsq_nyy_fgngvbaf: 03/27/15 13:16:52 Sebz: PAFY 1:FGEHPGHER SVER[5260 ]630 ARINQN FG ,FNF ogja GBZNYRF FG & OHPUNANA QE\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+fzsq_nyy_fgngvbaf: 03/22/15 15:30:47 Sebz: PAFY 1:ANGHENY TNF YRNX[5160 ]411 PBYBZN FG ,FNF ogja TNGR 5 EQ & OEVQTRJNL OY\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+fzsq_nyy_fgngvbaf: 03/17/15 11:17:40 Sebz: PAFY 1:ANGHENY TNF YRNX[4856 ]170 WNARF FG ,ZIC ogja FHAEVFR NI & ZBAGSBEQ NI\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+fzsq_nyy_fgngvbaf: 02/23/15 06:36:45 Sebz: PAFY 1:FGEHPGHER SVER[4960 ]146 ONEONERR JL ,FOL ogja TERRAJBBQ PBIR QE & TERRAJBBQ PBIR QE\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+fzsq_nyy_fgngvbaf: 02/21/15 09:42:30 Sebz: PAFY 1:FGEHPGHER SVER[5056 ]384 CVARUVYY EQ ,GNZ ogja JBBQFVQR NI & JRAQL JL\a\aRznvy Qvfpynvzre: uggc://jjj.znevapbhagl.bet/znva/qvfpynvzref
+ */
+
+choyvp pynff PNZnevaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp PNZnevaPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "ZNEVA PBHAGL", "PN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj PNZnevaPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
