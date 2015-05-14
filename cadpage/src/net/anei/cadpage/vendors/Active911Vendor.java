193b75dbbdd4b39b1d961edfd9f32cd89a8a2594
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-05-14 17:04:27 +0000

    Update A911 parser table

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -294,6 +294,7 @@ class Active911Vendor extends Vendor {
       "US/IA/WorthCounty",                "IAWorthCounty",
       "US/ID/BlaineCounty",               "IDBlaineCounty",
       "US/ID/KootenaiCounty",             "IDKootenaiCountyParser",
+      "US/IA/MitchellCountySheriff",      "IAMitchellCounty",
       "US/IL/AdamsCounty",                "ILAdamsCounty",
       "US/IL/BooneCounty",                "ILBooneCounty",
       "US/IL/CityofElgin",                "ILElgin",
