a04f1eff528302af07af7fa637265115f4a2841a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-06-10 00:56:56 +0000

    Fixed parsing problem with Stamford, CT

--- cadpage/src/net/anei/cadpage/parsers/CT/CTStamfordParser.java
+++ cadpage/src/net/anei/cadpage/parsers/CT/CTStamfordParser.java
@@ -11,9 +11,12 @@ import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
  */
 public class CTStamfordParser extends DispatchA3Parser {
   
+  private static final Pattern DELIM = Pattern.compile("\\*?\\n");
+  
   private static final Pattern PREFIX_PTN = Pattern.compile("(?:(?:911Fire:|STAMFORD 911:)?\\*|STAMFORD 911:Stamford|911Fire:Stamford)\\n");
   public CTStamfordParser() {
-    super(0, "", "STAMFORD", "CT");
+    super("STAMFORD", "CT",
+          "ADDR EMPTY EMPTY EMPTY X X EMPTY CMT CALL! EMPTY NAME EMPTY EMPTY EMPTY EMPTY INFO+");
   }
 
   @Override
@@ -35,7 +38,61 @@ public class CTStamfordParser extends DispatchA3Parser {
       return false;
     } while (false);
 
-    return super.parseMsg(body, data);
+    body = stripFieldEnd(body, "*");
+    return parseFields(DELIM.split(body), data) && data.strCall.length() > 0;
   }
   
+  @Override
+  public Field getField(String name) {
+    if (name.equals("CMT")) return new MyCommentField();
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private static final Pattern COMMENT_PTN = Pattern.compile("(?:Landmark|Geo|Place|Special) Comment::? *(.*)");
+  private class MyCommentField extends BaseInfo1Field {
+    
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (field.length() == 0) return true;
+      Matcher match = COMMENT_PTN.matcher(field);
+      if (!match.matches()) return false;
+      super.parse(match.group(1), data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      if (!checkParse(field, data)) abort();
+    }
+  }
+  
+  private static final Pattern INFO_PTN = Pattern.compile("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d .*");
+  private class MyInfoField extends BaseInfoField {
+    
+    @Override
+    public boolean canFail() {
+      return true;
+    }
+    
+    @Override
+    public boolean checkParse(String field, Data data) {
+      if (!INFO_PTN.matcher(field).matches()) return false;
+      parse(field, data);
+      return true;
+    }
+    
+    @Override
+    public void parse(String field, Data data) {
+      String saveCross = data.strCross;
+      data.strCross = "";
+      super.parse(field, data);
+      if (data.strCross.length() == 0) data.strCross = saveCross;
+    }
+  }
 }
