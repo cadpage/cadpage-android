baecd4a6b700bc57951eb83de786b5d1b893d7a4
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-06-24 18:56:53 +0000

    Fixed parsing issues with Suffolk County, NY (H)

--- cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyHParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NY/NYSuffolkCountyHParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.NY;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,11 +10,11 @@ import net.anei.cadpage.parsers.SmartAddressParser;
 
 public class NYSuffolkCountyHParser extends SmartAddressParser {
   
-  private static final Pattern MASTER = Pattern.compile("(\\d\\d/\\d\\d/\\d{4}) (\\d\\d:\\d\\d) (.*?) (AM|EH) (.*)");
+  private static final Pattern MASTER = Pattern.compile("(\\d\\d/\\d\\d/\\d{4}) (\\d\\d:\\d\\d) (.*?) (AM|EH|MO|SP) (.*)");
   
   public NYSuffolkCountyHParser() {
     super(CITY_LIST, "SUFFOLK COUNTY", "NY");
-    setFieldList("DATE TIME CALL SRC ADDR APT CITY INFO");
+    setFieldList("DATE TIME CALL CITY ADDR APT PLACE INFO");
     setupSpecialStreets(
         "BEACHWAY",
         "CAPTAINS WALK",
@@ -43,14 +44,27 @@ public class NYSuffolkCountyHParser extends SmartAddressParser {
     data.strDate = match.group(1);
     data.strTime = match.group(2);
     data.strCall = match.group(3).trim();
-    data.strSource = match.group(4);
+    String code = match.group(4);
     String addr = match.group(5).trim();
-    parseAddress(StartType.START_ADDR, addr, data);
+    parseAddress(StartType.START_ADDR, FLAG_CROSS_FOLLOWS | FLAG_IGNORE_AT, addr, data);
     data.strSupp = getLeft();
+    data.strPlace = data.strCity;
+    data.strCity = convertCodes(code, CITY_CODES);
+    if (data.strPlace.equals(data.strCity)) data.strPlace = "";
     return true;
   }
   
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "AM", "AMAGANSETT",
+      "EH", "EAST HAMPTON",
+      "MO", "MONTAUK",
+      "SP", "SPRINGS AMGANSETT"
+  });
+  
   private static final String[] CITY_LIST = new String[]{
-    "EAST HAMPTON"
+    "AMAGANSETT",
+    "EAST HAMPTON",
+    "BARNES LANDING BERGER",
+    "SPRINGS AMAM"
   };
 }
