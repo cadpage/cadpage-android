10ab142fb4a931759434c2ba17ba56f08954b9aa
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-07-04 21:16:52 +0000

    Update sender filter for Prince William County, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VAPrinceWilliamCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAPrinceWilliamCountyParser.java
@@ -27,7 +27,7 @@ public class VAPrinceWilliamCountyParser extends FieldProgramParser {
 
   @Override
   public String getFilter() {
-    return "cc_message_notification@usamobility.net,@rsan.pwcgov.org,PWRSAN,89361";
+    return "cc_message_notification@usamobility.net,@rsan.pwcgov.org,PWRSAN,89361,87844";
   }
   
   private String select = null;
