dcafca3c1ef59f7ef191a03fa7f4288d73a593f1
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-07-13 17:21:23 +0000

    Fixed parsing problem with Gallatin County, KY (A)

--- cadpage/src/net/anei/cadpage/parsers/KY/KYGallatinCountyAParser.java
+++ cadpage/src/net/anei/cadpage/parsers/KY/KYGallatinCountyAParser.java
@@ -41,6 +41,12 @@ public class KYGallatinCountyAParser extends DispatchA29Parser {
     return true;
   }
 
+  @Override
+  protected void parseAddress(StartType sType, int flags, String address, Data data) {
+    address = address.replace('@', '&');
+    super.parseAddress(sType, flags, address, data);
+  }
+
   private static final String[] CITY_LIST = new String[]{
     "GALLATIN COUNTY",
     "GLENCOE",
@@ -52,32 +58,56 @@ public class KYGallatinCountyAParser extends DispatchA29Parser {
     "VERONA",
     
     "CARROLL COUNTY",
-    "SANDERS"
+    "GHENT",
+    "SANDERS",
+    
+    "KENTON COUNTY",
+    "ERLANGER"
   };
   
   private static final CodeSet CALL_LIST = new CodeSet(
       "46 TRAFFIC ACCIDENT-INJURIES",
+      "0560",
+      "1015",
+      "1015 1015",
+      "45D TRAFFIC ACCIDENT - VS DEER",
+      "8001 ALARM-BURGLAR-COMMERCIAL",
       "8007 ASSIST ANOTHER AGENCY",
+      "8050 FIRE ALARM",
+      "8111 MOTORIST ASSIST",
+      "8014 BURGLARY-COMMERCIAL",
       "8015 LOUD MUSIC COMPLAINT",
       "8029 DOMESTIC COMPLAINT",
+      "8032 DRIVING UNDER INFLUENCE",
       "8038 JUVENILE PROBLEMS",
+      "8040 RESCUE/AMBULANCE",
+      "8045 FIRE-GENERAL",
       "8081 SUSPICIOUS PERSON",
       "8086 THEFT",
       "8090 TRAFFIC COMPLAINT",
       "8106 VANDALISM",
       "8112 LOCK OUT",
+      "8113 LOUD NOISE COMP",
       "8130 CUSTODY EXCHANGE",
+      "8157 SUICIDAL SUBJECT",
       "8201 MEDICAL",
       "8215 REQUEST SPEAK OFFICER",
       "8216 WELFARE CHECK",
       "8218 ATTEMPT TO SERVE",
       "8220 COMPLAINT",
       "8223 TRAFFIC ACCIDENT NON-INJURY",
+      "8224 B  AND  E",
+      "8225 ABANDONED VEHICLE",
       "8227 1012",
       "8241 WEATHER ALERTS",
       "8245 1088",
+      "8247 4-WHEELER COMPLAINT",
+      "8249 ALCOHOL INTOXICATION",
       "8259 LINES DOWN (ELECTRIC, TELEPHONE, ETC.)",
+      "8263 HARASSING COMMUNICATIONS",
       "8268 DOG BITE",
+      "T808 TRAFFIC STOP",
+      "SP SPEEDWAY POLICE",
       "TRNSPRT NON-EMERGENCY TRANSPORT"
   );
 }
