0d45e04dfd204060a2b75e140e6ff97e29d6dc0f
Ken Corbin <ken@cadpage.org>
2015-07-15 22:36:46 -0700

    Fixed parsing problem with Oklahoma County, OK

--- cadpage/src/net/anei/cadpage/parsers/OK/OKOklahomaCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OK/OKOklahomaCountyParser.java
@@ -14,19 +14,29 @@ public class OKOklahomaCountyParser extends SmartAddressParser {
   
   @Override
   public String getFilter() {
-    return "405261,81370,4058352432";
+    return "405261,81370,4058352432,4057960363";
   }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
     if (!body.startsWith(": ")) return false;
-    if (!body.endsWith(" - From: OCSO Dispatch")) return false;
-    body = body.substring(2,body.length()-22).trim();
-    parseAddress(StartType.START_CALL, body, data);
-    if (data.strCall.length() == 0) {
-      data.strCall = getLeft();
-    } else {
-      data.strSupp = getLeft();
+    int pt = body.indexOf(" - From: ");
+    if (pt < 0) return false;
+    body = body.substring(2,pt).trim();
+    
+    pt = body.indexOf("- ");
+    if (pt >= 0) {
+      parseAddress(body.substring(0,pt).trim(), data);
+      data.strCall = body.substring(pt+2).trim();
+    } 
+    
+    else {
+      parseAddress(StartType.START_CALL, FLAG_IGNORE_AT, body, data);
+      if (data.strCall.length() == 0) {
+        data.strCall = getLeft();
+      } else {
+        data.strSupp = getLeft();
+      }
     }
     return (data.strCall.length() > 0 && data.strAddress.length() > 0);
   }
