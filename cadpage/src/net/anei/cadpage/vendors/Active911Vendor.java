d835760fb59db685194647ba37e0378f871ac5ab
Ken Corbin <ken@cadpage.org>
2015-07-19 15:35:49 -0700

    Checking in Cascade County, MT

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -548,6 +548,7 @@ class Active911Vendor extends Vendor {
       "US/MS/CityofHernando",             "MSHernando",
       "US/MS/DesotoCounty",               "MSDeSotoCountyA",
       "US/MS/Southaven",                  "MSDesotoCountyB",
+      "US/MT/CityofBlackEagle",           "MTCascadeCounty",
       "US/MT/GallatinCounty",             "MTGallatinCounty",
       "US/MT/MissoulaCounty",             "MTMissoulaCounty",
       "US/MT/ParkCounty",                 "MTParkCounty",
