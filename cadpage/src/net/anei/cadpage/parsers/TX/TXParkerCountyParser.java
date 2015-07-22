df6ab0cd313a0af2e3bcfe6bf009c5b45b9a9561
Jean Goul <jean@cadpage.org>
2015-07-22 09:05:52 -0700

    Adding to files missed the first time around

---
+++ cadpage/src/net/anei/cadpage/parsers/TX/TXParkerCountyParser.java
@@ -0,0 +1,13 @@
+package net.anei.cadpage.parsers.TX;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+/**
+ * Parker County, TX
+ */
+public class TXParkerCountyParser extends GroupBestParser {
+  
+  public TXParkerCountyParser() {
+    super(new TXParkerCountyAParser(), new TXParkerCountyBParser());
+  }
+}
