5d552700d2f6cb47b31b5838ad6a33e05a44eb24
Jamie Harper <snailtk@gmail.com>
2015-07-23 19:59:58 -0700

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/KS/KSPottawatomieCountyParserTest.java
@@ -0,0 +1,75 @@
+cnpxntr arg.narv.pnqcntr.cnefref.XF;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Pbagnpg: Npgvir911
+Ntrapl anzr: Cbggnjngbzvr Pbhagl RZF
+Ybpngvba: Jnzrtb, XF, Havgrq Fgngrf
+Fraqre: RagrecbyNyregf@cgfurevss.pbz
+
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 212 A 2aq Fgerrg, Byfohet\aErcbegvat Crefba: RYYRA FUNJ QVPX, Cubar: 785-468-2388\aQrgnvy: YVSG NFFVFG
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy: jup gb gbcrxn
+(ARJVAP #????-0 OYF - OYF genafcbeg) ARJVAP #????-0 OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 410 1fg Fgerrg, Fg Trbetr\aErcbegvat Crefba: ULQR QBA, Cubar: 785-494-2273\aQrgnvy: 64 uneq gvzr oernguvat-frirer urnq vawhel naq cnpr znxre
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 510 Zncyr Fgerrg, Fg Trbetr\aErcbegvat Crefba: orggl jnyfu, Cubar: 785-477-4731\aQrgnvy: gbbx fbzr zrqf naq vf univat n ernpgvba  gebgnmbar  trarevp    gbbx yvxr ng 8    guvf vf gur svefg gvzr fur unf gnxra vg  52 lbn   unf uvfg frvfher
+(ARJBPP #BHGF  NYF - NYF Genafcbeg) ARJBPP #BHGF  NYF - NYF Genafcbeg\aNqqerff: Fg. Znelf Urnygu Pragre - 206 F Tenaq Nir, Fg Znelf\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy: 203
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy: 205  pub gb fgbezbag invy  0.3 zvyrf 10-23 pub
+(ARJBPP #15-115  ZINV - Vawhel Zbgbe Iruvpyr Nppvqrag) ARJBPP #15-115  ZINV - Vawhel Zbgbe Iruvpyr Nppvqrag\aNqqerff: Syhfu Ebnq naq Zlref Inyyrl Ebnq - Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: XVAQYR TENL, Cubar: 217-836-1972\aQrgnvy: NPPVQRAG,
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy: 205 pub gb fgbezbag  0
+(ARJBPP #BHGF  choyvp - Choyvp Nffvfg) ARJBPP #BHGF  choyvp - Choyvp Nffvfg\aNqqerff: 606 O A 5gu Fgerrg (Ncg 7), Fg Znelf\aErcbegvat Crefba: SNEZNA C, Cubar: 785-321-1252\aQrgnvy: snyy ab vawhel whfg arrqf uryc hc
+(ARJBPP #BHGF  fubgf - Fubgf sverq) ARJBPP #BHGF  fubgf - Fubgf sverq\aNqqerff: 110 F 4gu, Jrfgzberynaq\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Fg Pbyhzxvyyrf Puhepu - 13315 Oragba Fgerrg, Oynvar\aErcbegvat Crefba: , Cubar: 785-410-2246\aQrgnvy: 96 lb znyr cbffvoyr urng fgebxr
+(ARJBPP #15-113  Ovgr - Navzny Ovgr) ARJBPP #15-113  Ovgr - Navzny Ovgr\aNqqerff: 402 F Cvar\aErcbegvat Crefba: gnzzl, Cubar: 785-556-3726\aQrgnvy: cvg ohyy whfg ovg gur fba
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 3765 Zvygba Pvepyr, Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: GLYRE tneire, Cubar: \aQrgnvy: GEBHOYR OERNGUVAT
+(ARJBPP #BHGF  vairfg - Vairfgvtngr Crefba/Nhgb) ARJBPP #BHGF  vairfg - Vairfgvtngr Crefba/Nhgb\aNqqerff: Ujl  63 naq Qryvn Ebnq - Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: QRRNA RZF, Cubar: \aQrgnvy: IRUVPYR VA GUR QVGPU JVGU N GERR GUBEHTU GUR JVAQBJ EC QRRAN  JVGU RZF URE FBA PBZVAT SEBZ FG ZNELF FNJ VG BA UVF JNL GB JBXE NG WRSSRELF NAQ PNYYRQ URE UR QVQAG XABJ JUNG GB QB UR QVQAG ORYVRIR GURER JNF NALBAR NEBHAQ GUR IRUVPYR
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #15-111  ZINA - Zbgbe Iruvpyr Nppvqrag Aba-vaw) ARJBPP #15-111  ZINA - Zbgbe Iruvpyr Nppvqrag Aba-vaw\aNqqerff: Ujl 99 naq Fnl Ebnq - Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: Zrtna Roreg, Cubar: 785-458-2586\aQrgnvy: n gehpx jrag bss gur ebnq
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy: JUP GENAFSRE GB IVN PUEVFGV
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy: PUB GB GENAFSRE GB GBCRXN
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Jnzrtb Fcbegf Pbzcyrk - 1001 Onyqrefba Obhyrineq, Jnzrtb\aErcbegvat Crefba: nyyvf, Cubar: 78\aQrgnvy: cbffvoyr fgebxr 58 l/b
+(ARJBPP #15-110  punfr - Punfr) ARJBPP #15-110  punfr - Punfr\aNqqerff: Xnj Inyyrl Ebnq, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Qbyyne Trareny - 1410 Uvtujnl 24, Jnzrtb\aErcbegvat Crefba: , Cubar: 456-1663\aQrgnvy: SNYY HAXABJA VAWHEVRF
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Jrfgzberynaq Urnygu Pragre - 302 Znva Fgerrg, Jrfgzberynaq\aErcbegvat Crefba: , Cubar: \aQrgnvy: xnlyn - rzf gb pyvavp gb genafcbeg gb fgbezbag va gbcrxn -40 lrne byq srznyr certanag jvgu ulcregrafvba-37 jrrxf nybat, oybbq cerffher 180/104 qe jvyyvnzf jnagf oveguvat pragre
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Ivn Puevfgv Ubfcvgny - 1823 Pbyyrtr Nirahr, Znaunggna\aErcbegvat Crefba: , Cubar: \aQrgnvy: ivn puevfgv gb jup
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 11024 Yrtvba Qevir (P), Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: , Cubar: 785-844-3040\aQrgnvy: fhssrevat sebz na nfguzn nggnpx\a53 le byq
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Jrfgl Pbzzhavgl Pner Ubzr - 105 Uvtujnl 99, Jrfgzberynaq\aErcbegvat Crefba: , Cubar: 785-457-2801\aQrgnvy: 83 le byq znyr\aarrqf gb tb gb jnzrtb
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Jrfgl Pbzzhavgl Pner Ubzr - 105 Uvtujnl 99, Jrfgzberynaq\aErcbegvat Crefba: , Cubar: \aQrgnvy: 81 le byq znyr gebhoyr oernguvat\ahccre yriry
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Jnzrtb Urnygu Pragre - 711 Traa Qevir, Jnzrtb\aErcbegvat Crefba: , Cubar: \aQrgnvy: ivn puevfgv
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 4455 Oynpxwnpx eq, Znaunggna\aErcbegvat Crefba: , Cubar: 785-844-5169\aQrgnvy: pbhfva pnyyrq ure n yvggyr ovg ntb naq gubhtug tenaqzn jnf univat n fgbexr  93 lrne byq srznyr
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: Ybar Gerr  Ebnq naq Jurngba  Ebnq\aErcbegvat Crefba: , Cubar: \aQrgnvy: a bs jurngba xf  hapyr genpgbe
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 601 Znva  Fgerrg (8)\aErcbegvat Crefba: , Cubar: 785-456-5976\aQrgnvy: purfg cnva 42/z
+(ARJBPP #BHGF  zhgnvq - Zhghny Nvq) ARJBPP #BHGF  zhgnvq - Zhghny Nvq\aNqqerff: 31592 Grzcyr\aErcbegvat Crefba: , Cubar: \aQrgnvy: jnonhfarr gbjafuvc   32 lrne srznyr freir  cnva fgrebvq vawrpgvba    x 18- jnfuvatgba  naq gura jrfg
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Ivn Puevfgv Urnygu Pragre Pyvavp - 457 R Cblagm Fgerrg, Znaunggna\aErcbegvat Crefba: , Cubar: \aQrgnvy: GB FGBEZBAG INVY VA GBCRXN
+(ARJBPP #BHGF  zbgbe - Zbgbevfg Nffvfg/qvfnoyrq iruvp) ARJBPP #BHGF  zbgbe - Zbgbevfg Nffvfg/qvfnoyrq iruvp\aNqqerff: Ujl 99 naq Ybhvfivyyr  Ebnq\aErcbegvat Crefba: qnera jvyyneq, Cubar: 7852106214\aQrgnvy: srznyr uvg
+(ARJBPP #15-107  ZINA - Zbgbe Iruvpyr Nppvqrag Aba-vaw) ARJBPP #15-107  ZINA - Zbgbe Iruvpyr Nppvqrag Aba-vaw\aNqqerff: 3548 Qrzcfrl Ebnq, Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: pVAQL tNEARE, Cubar: 785-776-6893\aQrgnvy: FBZRBAR XABPXRQ BHG URE ZNVYOBK   WHFG UNCCRARQ FUR QVQ ABG FRR JUB QVQ VG BAYL URNEQ VG, GURL NYFB TBG GUR ARVTUOBEF ZNVYOBK
+(ARJBPP #15-106  fhvpv - Fhvpvqr/fhvpvqr nggrzcg) ARJBPP #15-106  fhvpv - Fhvpvqr/fhvpvqr nggrzcg\aNqqerff: 13350 Jbbqynaq Qevir, Cbggnjngbzvr Pbhagl\aErcbegvat Crefba: QBHTYNF JVYFBA, Cubar: 785-564-1372\aQrgnvy: YRSG FBZR
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Ivn Puevfgv Urnygu Pragre Pyvavp - 457 R Cblagm Fgerrg, Znaunggna\aErcbegvat Crefba: , Cubar: \aQrgnvy: SEBZ IVN PUEVFGV GB FGBEZBAG INVY
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 416 2aq Fgerrg, Fg Trbetr\aErcbegvat Crefba: urngure, Cubar: \aQrgnvy: snyy 83 lb znyr \asryy n srj ubhe
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 203 Zncyr Fgerrg, Jnzrtb\aErcbegvat Crefba: oneonen, Cubar: 785-313-3470\aQrgnvy: e/c sryy naq uvg ure urnq\ayrsg bs gur sebag qbbe haqre gur jvaqbj frry
+(ARJVAP #????-0 OYF - OYF genafcbeg) ARJVAP #????-0 OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+(ARJBPP #BHGF  OYF - OYF genafcbeg) ARJBPP #BHGF  OYF - OYF genafcbeg\aNqqerff: Pbzzhavgl Ubfcvgny - Bantn - 120 J 8gu Fgerrg, Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy: GENAFSRE SEBZ PUB GB FGBEZBAG INVY
+(ARJBPP #BHGF  zrq - Zrqvpny) ARJBPP #BHGF  zrq - Zrqvpny\aNqqerff: 509 J Jvyybj Fgerrg (Ncg 2), Fg Znelf\aErcbegvat Crefba: ZNELF CUNEZNPL FG, Cubar: 785-437-2967\aQrgnvy:
+(ARJBPP #BHGF  yvsr - Yvsrfgne ynaqvat) ARJBPP #BHGF  yvsr - Yvsrfgne ynaqvat\aNqqerff: NVECBEG - Bantn\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+*/
+
+choyvp pynff XFCbggnjngbzvrPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp XFCbggnjngbzvrPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "CBGGNJNGBZVR PBHAGL", "XF");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+ 
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj XFCbggnjngbzvrPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
