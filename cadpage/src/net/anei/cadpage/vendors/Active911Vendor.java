49f573dbb35fb95cb7a5fd8c1f33d20966e34f4d
Ken Corbin <ken@cadpage.org>
2015-08-10 21:09:30 -0700

    Update A911 parser table

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -726,6 +726,7 @@ class Active911Vendor extends Vendor {
       "US/OH/CityofVandalia",             "OHMontgomeryCountyB",
       "US/OH/CityofXenia",                "OHGreeneCounty",
       "US/OH/ClermontCounty",             "OHClermontCounty",
+      "US/OH/CrawfordCountySheriff",      "OHCrawfordCounty",
       "US/OH/DarkeCountySheriff",         "OHDarkeCounty",
       "US/OH/Fairfield",                  "OHFairfieldCounty",
       "US/OH/FultonCounty",               "OHFultonCounty",
