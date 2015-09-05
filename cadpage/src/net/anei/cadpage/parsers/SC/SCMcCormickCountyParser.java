0810a4265b03af41ad20fc8fdfe3143020baec9f
Ken Corbin <ken@cadpage.org>
2015-09-04 20:16:35 -0700

    Fixed parsing problem with McCormick County, SC

--- cadpage/src/net/anei/cadpage/parsers/SC/SCMcCormickCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCMcCormickCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.SC;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchGeoconxParser;
 
 
@@ -11,7 +12,17 @@ public class SCMcCormickCountyParser extends DispatchGeoconxParser {
 
   @Override
   public String getFilter() {
-    return "dispatch@911email.org";
+    return "geoconex@nlamerica.com,dispatch@911email.org,dispatch@911email.net";
+  }
+
+  @Override
+  protected boolean parseMsg(String subject, String body, Data data) {
+    if (body.startsWith("Message-Id:")) {
+      int pt = body.indexOf('\n');
+      if (pt < 0) return false;
+      body = body.substring(pt+1);
+    }
+    return super.parseMsg(subject, body, data);
   }
   
 }
