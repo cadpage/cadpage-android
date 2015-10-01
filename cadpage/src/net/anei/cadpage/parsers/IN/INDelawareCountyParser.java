75159085a437d264b9e65e01ce7b71f8981fab2b
Jamie Harper <snailtk@gmail.com>
2015-10-01 16:38:26 -0700

    parsers

---
+++ cadpage/src/net/anei/cadpage/parsers/IN/INDelawareCountyParser.java
@@ -0,0 +1,21 @@
+package net.anei.cadpage.parsers.IN;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.dispatch.DispatchA9Parser;
+
+
+/**
+ * Delaware County, IN
+ */
+public class INDelawareCountyParser extends DispatchA9Parser {
+  
+  public INDelawareCountyParser() {
+    super(null, "DELAWARE COUNTY", "IN");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "admin@co.delaware.in.us";
+  }
+  
+}
---
+++ cadpage/src/net/anei/cadpage/parsers/LA/LATangipahoaParishParser.java
@@ -0,0 +1,15 @@
+package net.anei.cadpage.parsers.LA;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA46Parser;
+
+public class LATangipahoaParishParser extends DispatchA46Parser {
+
+  public LATangipahoaParishParser() {
+    super("TANGIPAHOA PARISH", "LA");
+    }
+
+  @Override
+  public String getFilter() {
+    return "PTS@ptssolutions.com";
+    }
+  }
