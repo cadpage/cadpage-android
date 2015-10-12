608387b4f70df063b3f392708971a164cdf889b9
Ken Corbin <ken@cadpage.org>
2015-10-12 15:16:41 -0700

    Fixed parsing problem with Yuma County, AZ

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/AZ/AZYumaCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/AZ/AZYumaCountyParserTest.java
@@ -14,6 +14,7 @@ ZFT:PU5 OE NFFVTA SBE R9 NERN BS 2900 OYX BS R UJL 95 / LCQ FGNGRQ JR PBHYQ PK G
 1 bs 5\aSEZ:lhznpbzz@ezrgeb.pbz\aZFT:PU5 PBZZ NFFVTA: R9, R5, R2, R8, N9, O7 - 1818 F Yrgiva Nir, Onexyrl Frrq Pb sbe ercbeg bs sver\a(Pba'g) 2 bs 5\anynez\a***Guvf pbzzhavpngvba znl pbagnva pbasvqragvny naq/be cebcevrgnel vasbezngvba naq znl abg or qvfpybfrq gb nalbar bgure guna gur\a(Pba'g) 3 bs 5\avagraqrq nqqerffrr. Nal bgure qvfpybfher vf fgevpgyl cebuvovgrq ol ynj. Vs lbh ner abg gur vagraqrq nqqerffrr, lbh unir erprvirq guvf\a(Pba'g) 4 bs 5\apbzzhavpngvba va reebe. Cyrnfr abgvsl gur fraqre vzzrqvngryl naq qrfgebl gur pbzzhavpngvba vapyhqvat nyy pbagrag naq nal nggnpuzragf.\a(Pba'g) 5 bs 5\aGunax lbh.***(Raq)
 1 bs 5\aSEZ:lhznpbzz@ezrgeb.pbz\aZFT:PU1 ERF NFFVTA: R10, R2, R9, R5, N9, O7 - 12183 F ZNTTVR YA, YNF ONENAPNF NERN SBE ERCBEG BS CBFF PNEOBA\a(Pba'g) 2 bs 5\aZBABKVQR, BPPHCNAGF RINPHNGRQ\a***Guvf pbzzhavpngvba znl pbagnva pbasvqragvny naq/be cebcevrgnel vasbezngvba naq znl abg or qvfpybfrq gb\a(Pba'g) 3 bs 5\analbar bgure guna gur vagraqrq nqqerffrr. Nal bgure qvfpybfher vf fgevpgyl cebuvovgrq ol ynj. Vs lbh ner abg gur vagraqrq nqqerffrr, lbh\a(Pba'g) 4 bs 5\aunir erprvirq guvf pbzzhavpngvba va reebe. Cyrnfr abgvsl gur fraqre vzzrqvngryl naq qrfgebl gur pbzzhavpngvba vapyhqvat nyy pbagrag naq\a(Pba'g) 5 bs 5\anal nggnpuzragf. Gunax lbh.***(Raq)
 1 bs 5\aSEZ:lhznpbzz@ezrgeb.pbz\aZFT:pu 1 pbzzrepvny 11274 f sbeghan eq (byq sel'f fubccvat pragre) sbe r2, r10, r9, r8, o7, ercbegrq nf oynpx\a(Pba'g) 2 bs 5\afzbxr oruvaq gur fubccvat pragre (cbffvoyl oruvaq Gur\a***Guvf pbzzhavpngvba znl pbagnva pbasvqragvny naq/be cebcevrgnel vasbezngvba naq\a(Pba'g) 3 bs 5\aznl abg or qvfpybfrq gb nalbar bgure guna gur vagraqrq nqqerffrr. Nal bgure qvfpybfher vf fgevpgyl cebuvovgrq ol ynj. Vs lbh ner abg gur\a(Pba'g) 4 bs 5\avagraqrq nqqerffrr, lbh unir erprvirq guvf pbzzhavpngvba va reebe. Cyrnfr abgvsl gur fraqre vzzrqvngryl naq qrfgebl gur pbzzhavpngvba\a(Pba'g) 5 bs 5\avapyhqvat nyy pbagrag naq nal nggnpuzragf. Gunax lbh.***(Raq)
+1 bs 5\aSEZ:lhznpbzz@ezrgeb.pbz\aZFT:PU 5 PBZZ NFFVTA SBE R5, R9, R2, E5, R8, O7 - 3390 J 5GU FG - ONAA GENVYRE CX SBE N CNYZ GERR BA SVER.\a(Pba'g) 2 bs 5\aft\a***Guvf pbzzhavpngvba znl pbagnva pbasvqragvny naq/be cebcevrgnel vasbezngvba naq znl abg or qvfpybfrq gb nalbar bgure guna gur\a(Pba'g) 3 bs 5\avagraqrq nqqerffrr. Nal bgure qvfpybfher vf fgevpgyl cebuvovgrq ol ynj. Vs lbh ner abg gur vagraqrq nqqerffrr, lbh unir erprvirq guvf\a(Pba'g) 4 bs 5\apbzzhavpngvba va reebe. Cyrnfr abgvsl gur fraqre vzzrqvngryl naq qrfgebl gur pbzzhavpngvba vapyhqvat nyy pbagrag naq nal nggnpuzragf.\a(Pba'g) 5 bs 5\aGunax lbh.***(Raq)
 
 */
 
@@ -103,6 +104,25 @@ choyvp pynff NMLhznPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:11274 f sbeghan eq",
         "CYNPR:byq sel'f fubccvat pragre",
         "HAVG:R2, R10, R9, R8, O7");
+
+    qbGrfg("G5",
+        "1 bs 5\a" +
+        "SEZ:lhznpbzz@ezrgeb.pbz\a" +
+        "ZFT:PU 5 PBZZ NFFVTA SBE R5, R9, R2, E5, R8, O7 - 3390 J 5GU FG - ONAA GENVYRE CX SBE N CNYZ GERR BA SVER.\a" +
+        "(Pba'g) 2 bs 5\a" +
+        "ft\a" +
+        "***Guvf pbzzhavpngvba znl pbagnva pbasvqragvny naq/be cebcevrgnel vasbezngvba naq znl abg or qvfpybfrq gb nalbar bgure guna gur\a" +
+        "(Pba'g) 3 bs 5\a" +
+        "vagraqrq nqqerffrr. Nal bgure qvfpybfher vf fgevpgyl cebuvovgrq ol ynj. Vs lbh ner abg gur vagraqrq nqqerffrr, lbh unir erprvirq guvf\a" +
+        "(Pba'g) 4 bs 5\a" +
+        "pbzzhavpngvba va reebe. Cyrnfr abgvsl gur fraqre vzzrqvngryl naq qrfgebl gur pbzzhavpngvba vapyhqvat nyy pbagrag naq nal nggnpuzragf.\a" +
+        "(Pba'g) 5 bs 5\a" +
+        "Gunax lbh.***(Raq)",
+
+        "PU:PU5",
+        "PNYY:PBZZ - ONAA GENVYRE CX SBE N CNYZ GERR BA SVER. ft",
+        "HAVG:R5, R9, R2, E5, R8, O7",
+        "NQQE:3390 J 5GU FG");
     
   }
   
