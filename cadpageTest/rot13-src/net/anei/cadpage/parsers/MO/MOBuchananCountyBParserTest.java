9eab69b21244164cba0176fedc21bed2a8187fdc
Ken Corbin <ken@cadpage.org>
2015-10-14 11:22:34 -0700

    Checking in Buchanan County, MO parser instructions

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOBuchananCountyBParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOBuchananCountyBParserTest.java
@@ -11,15 +11,15 @@ Ohpunana Pbhagl, ZB (O)
 Pbagnpg: Npgvir911
 Ntrapl anzr: Ohpunana Pbhagl RZF
 Ybpngvba: Fnvag Wbfrcu, ZB, Havgrq Fgngrf
-Fraqre: <NCERJVGG@urnegynaq-urnygu.pbz>
-Fraqre: <ERAFVATZ@urnegynaq-urnygu.pbz>
-Fraqre: <BYVIREX@urnegynaq-urnygu.pbz>
+Fraqre: @urnegynaq-urnygu.pbz
 
 NCERJVGG:Z23 CEV:2 Nqqe 108 A 16gu Fg PGL:FG WBFRCU CEBO:10 PURFG CNVA 509773 RAQ-BS-ZFT
 NCERJVGG:Z23 #509772 Nqqe 5325 SNENBA FG ERPI 22:19 NYEG 22:28 RAEG 22:29 000.0 NEEI QRCG 000.0 QRFG 000.0 PBZC 22:33 GZ000.0 Raq-bs-Zft
 NCERJVGG:Z23 #509771 Nqqe 1010 RYYFJBEGU NIR ERPI 21:50 NYEG 21:51 RAEG 21:52 000.0 NEEI 22:10 QRCG 000.0 QRFG 000.0 PBZC 22:28 GZ000.0 Raq-bs-Zft
+{{{
 NCERJVGG:CNEG 2 QFGAZ:YVIVAT PBZZHAVGL BS FG WBFRCU QFGNQ:1202 A URNEGYNAQ EQ PGL: FG. WBFRCU EZ: F206O RAQ-BS-ZFT
 NCERJVGG:Z23 #509772 C/H: 22:19 CG: QRAAVF OBAJRYY CHAZ:ZBFNVP YVSR PNER RE Nqqe:5325 SNENBA FG PGL:FG. WBFRCU EZ:1534 JP:A --PBAG--
+}}}
 NCERJVGG:Z27 #509770 Nqqe 59 UJL/3 ZVYRF R BS NGPUVFBA ERPI 21:31 NYEG 21:33 RAEG 21:34 000.0 NEEI 21:50 QRCG 21:59 016.5 QRFG 22:18 021.1 PBZC GZ004.6 Raq-bs-Zft
 ERAFVATZ:Z23 CEV:3 Nqqe 1010 RYYFJBEGU NIR PGL:FG. WBFRCU CEBO:26 FVPX CREFBA 509771 RAQ-BS-ZFT
 NCERJVGG:Havg:Z22 -RC- CBFG 11 2200 JNYAHG
@@ -27,7 +27,6 @@ NCERJVGG:Havg:Z23 -RC- XVAT UVYY NIR & R YNXR OYIQ
 NCERJVGG:Havg:Z22 -RC- 3703 SERQREVPX NIR
 NCERJVGG:Havg:Z22 -RC- 4950 SERQREVPX NIR
 ERAFVATZ:Z27 CEV:1 Nqqe 59 UJL/3 ZVYRF R BS NGPUVFBA PGL:EHFUIVYYR CEBO:29 GENSSVP NPP 509770 RAQ-BS-ZFT
-NCERJVGG:Fgneg Fuvsg Z22 10/13/2015 21:00
 NCERJVGG:Havg:Z23 -RC- 1001 A 4GU
 NCERJVGG:Havg:Z21 -RC- 4950 SERQREVPX NIR
 NCERJVGG:Havg:Z27 -RC- CBFG 10 YNXR NAQ J ULQR
@@ -35,7 +34,6 @@ NCERJVGG:Havg:Z21 -RC- 3703 SERQREVPX NIR
 ERAFVATZ:Z27 #509769 Nqqe 801 A 10gu Fg ERPI 20:14 NYEG 20:16 RAEG 20:16 000.0 NEEI 20:20 QRCG 20:31 000.0 QRFG 20:44 004.8 PBZC GZ004.8 Raq-bs-Zft
 NCERJVGG:Havg:Z23 -RC- CBFG 11 2200 JNYAHG
 NCERJVGG:Z27 CEV:1 Nqqe 801 A 10gu Fg PGL:FG WBFRCU CEBO:10 PURFG CNVA 509769 RAQ-BS-ZFT
-ERAFVATZ:Fgneg Fuvsg Z21 10/13/2015 20:10
 NCERJVGG:Havg:Z29 -RC- CBFG 1 5325 SNENBA FG
 NCERJVGG:Havg:Z27 -RC- CBFG 11 2200 JNYAHG
 ERAFVATZ:Z23 #509766 Nqqe 5701 Ynxr Seba g Ya FJ ERPI 19:14 NYEG 19:15 RAEG 19:16 000.0 NEEI 19:33 QRCG 19:46 011.6 QRFG 20:05 023.8 PBZC GZ012.2 Raq-bs-Zft
@@ -44,7 +42,6 @@ BYVIREX:Z29 #509767 Nqqe 1109 F 13gu Fg ERPI 19:20 NYEG 19:21 RAEG 19:22 000.0 N
 BYVIREX:Z27 CEV:1 Nqqe 3108 FUREEL YA PGL:FG. WBFRCU CEBO:10 PURFG CNVA 509768 RAQ-BS-ZFT
 BYVIREX:Z29 CEV:2 Nqqe 1109 F 13gu Fg NygN PBZR VA BSS 14 GU FGERRG PGL:FG WBFRCU CEBO:28 FGEBXR PIN 509767 RAQ-BS-ZFT
 BYVIREX:Z23 CEV:2 Nqqe 5701 Ynxr Seba g Ya FJ NygN 46 PGL:FG WBFRCU CEBO:23 BQ/CBVFBAVA 509766 RAQ-BS-ZFT
-ERAFVATZ:Fgneg Fuvsg Z23 10/13/2015 19:05
 ERAFVATZ:Z27 #509763 Nqqe 811 A 9GU FG ERPI 18:12 NYEG 18:13 RAEG 18:13 000.0 NEEI 18:26 QRCG 18:50 005.1 QRFG 19:04 010.3 PBZC GZ005.2 Raq-bs-Zft
 BYVIREX:Z21 #509764 Nqqe 30 FGBARPERFG FG ERPI 18:16 NYEG 18:17 RAEG 18:18 000.0 NEEI 18:24 QRCG 000.0 QRFG 000.0 PBZC 18:58 GZ000.0 Raq-bs-Zft
 ERAFVATZ:Z29 #509765 Nqqe 3505 SERQREVPX NIR ERPI 18:24 NYEG 18:24 RAEG 18:25 000.0 NEEI 18:33 QRCG 18:44 003.1 QRFG 18:54 006.2 PBZC GZ003.1 Raq-bs-Zft
@@ -54,7 +51,6 @@ BYVIREX:Z21 CEV:2 Nqqe 30 FGBARPERFG FG PGL:FG. WBFRCU CEBO:17 SNYY/ONPX V 50976
 ERAFVATZ:Z27 CEV:3 Nqqe 811 A 9GU FG PGL:FG WBFRCU CEBO:33 VAGRESNPVYV 509763 RAQ-BS-ZFT
 ERAFVATZ:Z29 #509760 Nqqe 2205 F 6gu Fg ERPI 17:30 NYEG 17:31 RAEG 17:33 000.0 NEEI 17:36 QRCG 17:42 002.3 QRFG 17:58 008.3 PBZC GZ006.0 Raq-bs-Zft
 EBR:Z22 CEV:2 Nqqe 1202 A URNEGYN AQ EQ NygN 206 O 2 JRFG PGL:FG. WBFRCU CEBO:33 VAGRESNPVYV 509762 RAQ-BS-ZFT
-ERAFVATZ:Fgneg Fuvsg Z27 10/13/2015 17:55
 EBR:Z21 #509761 Nqqe 504 F 5gu Fg ERPI 17:38 NYEG 17:39 RAEG 17:40 000.0 NEEI 17:47 QRCG 000.0 QRFG 000.0 PBZC 17:48 GZ000.0 Raq-bs-Zft
 EBR:Z21 CEV:3 Nqqe 504 F 5gu Fg PGL:FG WBFRCU CEBO:23 BQ/CBVFBAVA 509761 RAQ-BS-ZFT
 EBR:Z21 #509759 Nqqe 3815 XVAT UVYY NIR ERPI 17:00 NYEG 17:01 RAEG 17:03 000.0 NEEI 17:10 QRCG 000.0 QRFG 000.0 PBZC 17:32 GZ000.0 Raq-bs-Zft
@@ -65,9 +61,44 @@ EBR:Z26 #509756 Nqqe 5325 SNENBA FG ERPI 16:11 NYEG 16:28 RAEG 16:29 000.0 NEEI 
 EBR:Z21 CEV:3 Nqqe 3815 XVAT UVYY NIR PGL:FG. WBFRCU CEBO:4 NFFNHYG ENCR 509759 RAQ-BS-ZFT
 EBR:Z24 CEV:2 Nqqe 6401 TBEQBA NI R PGL:FG WBFRCU CEBO:17 SNYY/ONPX V 509758 RAQ-BS-ZFT
 EBR:Z22 CEV:2 Nqqe 501 SNENBA PGL:FNVAG WBFRCU CEBO:23 BQ/CBVFBAVA 509757 RAQ-BS-ZFT
+{{{
 ERAFVATZ:CNEG 2 QFGAZ:PBHAGEL FDHVER QFGNQ:1602 OHPXVATUNZ PGL: FG WBFRCU EZ: 226 RAQ-BS-ZFT
 ERAFVATZ:Z26 #509756 C/H: 16:45 CG: ANBZV XYVATRAFZVGU CHAZ:ZBFNVP YVSR PNER VACNGVRAG Nqqe:5325 SNENBA FG PGL:FG WBFRCU EZ:2104 JP:A --PBAG--
+}}}
 
+Gurer ner n pbhcyr qvssrerag sbezngf urer.  Nyy fgneg jvgu n qvfcngpure cersvk naq raq jvgu na RAQ-BS-ZFT vaqvpngbe
+
+Naq gurer ner gjb fcyvg nyregf, nyregf fcyvg vagb gjb zrffntrf.   Gurl ner  znexrq jvgu gur 
+{{{ - }}} oenpxrgf naq fubhyq or unaqyrq ol gur grfg trarengbe.  Ohg lbh qb unir gb qb fbzr
+fcrpvny fghss va gur cnefre.
+
+  Bireevqr trgNpgvir911FcyvgZftBcgvbaf() naq unir vg erghea n arj FcyvgZftBcgvbafPhfgbz() bowrpg.
+  Gung gryyf cnefvat vasenfgehpgher ubj gb unaqyr fcyvg nyregf.
+  
+  Jura lbh trg n zrffntr gung raqf jvgu --PBAG-- vafgrnq bs gur hfhny RAQ-BS-ZFT vaqvpngbe,
+  fgevc bss gur --PBAG-- synt naq cnefr gur erznvaqre nf orfg lbh pna.  Naq frg qngn.rkcrpgZber
+  gb gryy gur cnefre gung lbh rkcrpg nabgure zrffntr jvgu guvf nyreg
+  
+  Jura zhygvcyr zrffntrf ner zretrq gbtrgure, gur cnefre jvyy or cnffrq bar fgevat jvgu gurz
+  nyy nccraqrq gbtrgure jvgu fcnpr frcnengbe.  Va guvf cnegvphyne pnfr gung jvyy erfhyg va
+  fbzr rkgenarbhf grkg pbafvfgvat bs gur --PBAG-- synt sbyybjrq ol gur qvfcngpu anzr naq
+  cneg ahzore juvpu jvyy svefg arrq gb or erzbirq.
+  
+Cebonoyl n frg bs cnggrea zngpurf sbe gur qvssrerag sbezngf jvyy jbex orfg.  Va pnfrf yvxr guvf, lbh pna
+pnyy frgSvryqYvfg() va gur cnefvat pbqr vafgrnq bs gur pbafgehpgbe fb lbh pna cnff qvssrerag svryq yvfgf
+sbe qvssrerag sbezngf.
+
+Gur CEBO: xrljbeq vf sbyybjrq ol n pnyy pbqr, pnyy qrfpevcgvba naq pnyy VQ.
+
+Gurer unf orra n punatr va ubj jr unaqyr eha ercbegf (gur zrffntrf gung ercbeg havg gvzrf).  Abj lbh whfg 
+frg nyy svryqf abeznyy, vapyhqvat gur fgeFhcc svryq.  Naq synt gur erfhyg nf n eha ercbeg ol frggvat
+qngn.zftGlcr = ZftGlcr.EHA_ERCBEG.
+Lbh unir gb pnyy frgSvryqYvfg() gb gryy hf jung svryqf unir orra frg.
+
+Gur gjb ybat (fcyvg) nyregf nccrne gb or va n pynff ol gurzfryirf.  Gurl ybbx yvxr cngvrag genafsre erdhrfgf
+sebz bar ubfcvgny gb nabgure.  Cnefre gur cvpxhc vafvgvghvba vasb abeznyy, naq gur qrfgvangvba vasbezngvba
+va gur VASB: svryqf.
+  
 */
 
 choyvp pynff ZBOhpunanaPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
