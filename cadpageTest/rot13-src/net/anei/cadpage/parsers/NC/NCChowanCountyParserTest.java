e029c94621b90f370c5642f55012db7cb7186edc
Ken Corbin <ken@cadpage.org>
2015-10-22 13:50:09 -0700

    Checking in Chowan County, NC

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NC/NCChowanCountyParserTest.java
@@ -0,0 +1,56 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AP;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/* 
+Pubjna Pbhagl, AP
+
+Pbagnpg: jnygpbcrynaq <jnygpbcrynaq@rzonedznvy.pbz>
+Fraqre: @pubjna.ap.tbi  
+F: Z:XREV:112 JNYXRE QE 19:46:41 ZRQVPNY-RZRETRAPL 79 lrne byq srznyr pbashfrq, ceboyrzf jvgu fhtne, yriry vf 248 BPN: 1510-1362
+( 1 bs  2) F: Z:XREV:105 PUREBXRR GEY RQRAGBA PUREBXRR GEY K PUREBXRR GEY 15:16:34 SVER-FGEHPGHER PNYYRE NQIVFRF FBZRGUVAT VF OHEAVAT HAQRE
+( 1 bs  2) F: Z:XREV:1 BPRNA UJL RQRAGBA 09:32:37 P-naq-E--Pneryrff-Erpxyrff LRYYBJ CVPX HC GEHPX BA GUR PUBJNA EVIRE OEVQTR NYY BIRE GUR EB
+
+*/
+
+choyvp pynff APPubjnaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp APPubjnaPbhaglCnefreGrfg() {
+    frgCnefre(arj APPubjnaPbhaglCnefre(), "PUBJNA PBHAGL", "AP");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgJnyPbcrynaq() {
+
+    qbGrfg("G1",
+        "F: Z:XREV:112 JNYXRE QE 19:46:41 ZRQVPNY-RZRETRAPL 79 lrne byq srznyr pbashfrq, ceboyrzf jvgu fhtne, yriry vf 248 BPN: 1510-1362",
+        "NQQE:112 JNYXRE QE",
+        "VQ:1510-1362",
+        "GVZR:19:46:41",
+        "PNYY:ZRQVPNY-RZRETRAPL",
+        "VASB:79 lrne byq srznyr pbashfrq, ceboyrzf jvgu fhtne, yriry vf 248");
+
+    qbGrfg("G2",
+        "( 1 bs  2) F: Z:XREV:105 PUREBXRR GEY RQRAGBA PUREBXRR GEY K PUREBXRR GEY 15:16:34 SVER-FGEHPGHER PNYYRE NQIVFRF FBZRGUVAT VF OHEAVAT HAQRE",
+        "NQQE:105 PUREBXRR GEY",
+        "PVGL:RQRAGBA",
+        "K:PUREBXRR GEY / PUREBXRR GEY",
+        "GVZR:15:16:34",
+        "VASB:SVER-FGEHPGHER PNYYRE NQIVFRF FBZRGUVAT VF OHEAVAT HAQRE");
+
+    qbGrfg("G3",
+        "( 1 bs  2) F: Z:XREV:1 BPRNA UJL RQRAGBA 09:32:37 P-naq-E--Pneryrff-Erpxyrff LRYYBJ CVPX HC GEHPX BA GUR PUBJNA EVIRE OEVQTR NYY BIRE GUR EB",
+        "NQQE:BPRNA UJL",
+        "PVGL:RQRAGBA",
+        "GVZR:09:32:37",
+        "PNYY:P-",
+        "VASB:naq-E--Pneryrff-Erpxyrff LRYYBJ CVPX HC GEHPX BA GUR PUBJNA EVIRE OEVQTR NYY BIRE GUR EB");
+   
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj APPubjnaPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
