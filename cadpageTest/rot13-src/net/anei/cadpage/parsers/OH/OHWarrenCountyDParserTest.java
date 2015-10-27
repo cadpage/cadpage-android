b2441a7f09fd9bcffbe8682ef113ce95a6c747e2
Jamie Harper <snailtk@gmail.com>
2015-10-27 14:36:30 -0700

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OH/OHWarrenCountyDParserTest.java
@@ -0,0 +1,133 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BU;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Gur Orpu Jngrecnex QCF
+Ybpngvba: Znfba, BU, Havgrq Fgngrf
+Fraqre: Tneergg Cbcbivpu <tneerggcbcbivpu.bu@tznvy.pbz>
+Fraqre: Tneergg Cbcbivpu <tcbcbivpu@gurornpujngrecnex.pbz>
+Fraqre: Fnaqen Tber <ftber@gurornpujngrecnex.pbz>
+
+Pnyy: Pne Penfu jvgu vawhevrf\aYbpngvba: Znfba Zbagtbzrel Eq., Gnlybefivyyr Ebnq\a\aGuerr pnef vaibyirq nveontf qrcyblrq jvgu vawhevrf
+Pnyy: Trareny vyyarff\aYbpngvba: 6300 Xvatf Vfynaq Qevir\a\aErfcbaq gb Xvatf Vfynaq svefg nvq fgngvba sbe ercbeg bs n trareny vyyarff
+Pnyy: Fzbxr Nynez\aYbpngvba: 203 Xbuy Fg\a\aTrareny fzbxr nynez 2aq fgbel 
+Pnyy: Oebxra Nez\aYbpngvba: 3829 Unabire Qe\aHavg: Zrqvp 52\a\a19 lrne byq znyr oebxra nez, haerfcbafvir 
+Pnyy: purfg cnva nynez npgvingvba\aYbpngvba: 5207 Nevry Pbheg\aHavg: Zrqvp 22\a\a76-lrne-byq srznyr uvfgbel bs purfg cnva, nynez npgvingvba. 
+Pnyy: penfu jvgu vawhevrf\aYbpngvba: Znfba Zbagtbzrel Ebnq/Jrfgrea Ebj\a\aGjb pne pbyyvfvba jvgu frirer urnq vawhevrf
+Pnyy: ORNPU JNGRECNEX QCF\aHavg: vasb bayl \a\aTrarenyvmrq nynez abarzretrapl
+Pnyy: cbffvoyr pneqvnp neerfg\aYbpngvba:  2793 Jngre Cnex Qe\aHavg: Zrqvp 55 47-lrne-byq znyr cbffvoyr pneqvnp neerfg abg erfcbaqvat
+Pnyy: Oernguvat Gebhoyr \aYbpngvba: 6851 Yrkvatgba Cnex Oyiq\a\a12 lrne byq znyr gebhoyr oernguvat 
+Pnyy: Puvyqovegu Nffvfgnapr\aYbpngvba: 71 F 21/1\aHavg: Zrqvp 55\a\a33-lrne-byq srznyr univat pbagenpgvbaf ba gur fvqr bs gur uvtujnl. 
+Pnyy: gebhoyr oernguvat\aYbpngvba: 7660 Oebbxsnez Pg\a\a23-lrne-byq gebhoyr oernguvat
+Pnyy: Pne vagb gryrcubar cbyr\aYbpngvba: Xvatf vfynaq qevir\a\aBar iruvpyr vagb gryrcubar cbyr, haxabja vawhevrf, haxabja vs jverf ner qbja
+Pnyy: Pne Nppvqrag \aYbpngvba: Znfba Zbagtbzrel-Jrfgrea Ebj Ebnq\a\aGjb iruvpyrf vaibyirq haxabja vawhevrf
+Pnyy: Qvnorgvp Rzretrapl\aYbpngvba: 8619 Puneyrfgba Jbbqf Qe\a\a76 lrne-byq znyr qvnorgvp rzretrapl
+Pnyy: Haxabja Zrqvpny \aYbpngvba: 4381 F Znyyneq Pi\a\a12 lrne byq znyr jbxr hc jvgu frirer cnva ba uvf onpx fcvany pbyhza
+Pnyy: Sver Nynez\aYbpngvba: 5084 Yrkvatgba Pg\a\aTrareny Sver Nynez, fzbxr qrgrpgbe znfgre orqebbz
+Pnyy: Haxabja Zrqvpny \aYbpngvba: 5601–5687 Xvatf Vfynaq Qe\aHavg: Zrqvp 55\a\a42 lrne byq srznyr haerfcbafvir
+Pnyy: Haxabja Zrqvpny\aYbpngvba: 3712 Jvyq Pureel Jnl\aHavg: Zrqvp 33\a28 lrne byq znyr abg noyr gb zbir uvf yrsg nez 
+Pnyy: Gebhoyr oernguvat\aYbpngvba: 5589 Xvatf Zvyyf Eq\aHavg: zrqvp 55\a\a31-lrne-byq znyr univat gebhoyr oernguvat,  fnlf ur vf univat fubegarff bs oerngu. Ebbz 326
+Pnyy: RAUNAPRQ SVER QNATRE\aYbpngvba: Jneera Pbhagl\a\aAbgrf: RAUNAPRQ SVER QNATRE GUEBHTU RNEYL GUVF RIRAVAT. Zber vasbezngvba<uggc://vajf.jeu.abnn.tbi/jrngure/nyregvasb/26450750>.
+Pnyy: Pneqvnp neerfg\aYbpngvba: 4177 F Fuber Qe\a\a\a76 lrne byq srznyr cbffvoyr pneqvnp neerfg
+Pnyy: CQ Erdhrfg \aYbpngvba: 4100 Uvpxbel Jbbqf Qe\a\aZnyr 23 lrnef byq pbzcynvavat gung ur vf n qvnorgvp naq unf ybj oybbq fhtne ~ cbyvpr erdhrfg
+Pnyy: Zrqvpny Rzretrapl \aHavg: Zrqvp 55\aYbpngvba: 5210 Xvatf Zvyyf Eq\a\a43 lrne byq znyr oyrrqvat sebz gur xarrpnc 
+Pnyy: Fgnghf Hcqngr\a\aAbgrf: Npgvir 911 vf abj hcqngr jvgu lbhe PNQ flfgrz.
+Pnyy: Flfgrz Qbja Nyreg\a\aNpgvir 911 jvyy or qbja sebz 8:15-9:15 cz RFG sbe n PNQ hcqngr. 
+Pnyy: EI vagb qvgpu\aYbpngvba: 71 A zvyr 21/8 \a\aEI vagb qvgpu ba gur evtug fvqr bs gur ebnq,  haxabja vawhevrf
+Pnyy: Xvgpura  Sver\aYbpngvba:  8398 Nfuzbag Jnl\a\aPnyyre fnlf xvgpura vf ba sver. Hanoyr gb chg bhg jvgu sver rkgvathvfure. 
+Pnyy: Zrqvpny Rzretrapl \aYbpngvba: 226 ornpu Oyiq \a22 lrne byq abg gnyxvat
+Pnyy: Sver Nynez\aYbpngvba: Znfba uvtu fpubby\a\aTrareny sver nynez 
+Pnyy: pne nppvqrag\aYbpngvba: 71 F zvyr 24\a\aGuerr pne pbyyvfvba jvgu vawhevrf
+Pnyy: Sver Nynez\aYbpngvba: 3972 Urevgntr Cbvagr Oyiq\a\aFrpbaq-fgbel sver nynez, frafvat fzbxr vf cerfrag. 
+Pnyy: Haxabja Zrqvpny \aYbpngvba: 5551 Bnx Fg\aHavg: Zrqvp 55\a\a37-lrne-byq znyr univat cnva va uvf srrg."
+Pnyy: Oebxra Nez\aYbpngvba: 1821 Evirejbbq Gey\aHavg: Zrqvp 55\a\a13 lrne byq srznyr, oebxra evtug nez
+Pnyy: Bireqbfr\aYbpngvba: 2793 Jngre Cnex Qe\aHavg: zrqvp 55\a\a23 lrne byq znyr cbffvoyr bireqbrf 
+Pnyy: pneqvnp neerfg\aYbpngvba: 3799 Oebbxunira Pg\a\a48 lrne byq znyr abg oernguvat, cbffvoyr pbaihyfvbaf. 
+Pnyy: Urneg Ceboyrzf \aYbpngvba: 6724 Fhzzre Svryq Qe\a\a88 lrne byq srznyr fnlvat fur vf univat znwbe urne cnva. 
+Pnyy: Pne Nppvqrag \aYbpngvba: 105 Ghpxre Qe\aHavg: Zrqvp 28\a\a2 pne nppvqrag. Haxabja vawherf 
+Pnyy: Bireqbfr \aYbpngvba: 3593 Evirefvqr Qe\aHavg: Zrqvp 53\a\a17 lrne byq znyr cbffvoyr bireqbfr 
+Pnyy: Haxabja zrqvpny \aHavg: Zrqvp 33\aYbpngvba: 4507 Znetnerg Pg\a\a77 lrne byq Haxabja zrqvpny fnlvat fur arrqf uryc. Cbyvpr va-ebhgr 
+Pnyy: Haerfcbafvir CG\aybpngvba: 439 Zbeebj Eq\aHavg: Zrqvp 77\a\a88 lrne byq znyr haerfcbafvir cbffvoyr QBN
+Pnyy: Bireqbfr\aYbpngvba: 5589 Xvatf Zvyyf Eq\aHavg: Zrqvp 55 Rat 44\a\aZna fyhzcrq bire gur envyvat. Arrqyr nccrnef gb or va evtug nez. Cbyvpr va-ebhgr
+Pnyy: Hafgnoyr Cngvrag\aYbpngvba: 5290 Jnaqrevat Jnl\aHavg: Zrqvp 33\a\a18 lrne byq znyr hafgnoyr,  oernguvat encvqyl, rkgerzr hahfhny urnegorng
+Pnyy: Haxabja Zrqvpny \aYbpngvba: 5163 Cnexfvqr Qe\aHavg: Zrqvp 34\a\a76 lrne byq srznyr univat cnva va uvf obql. 
+Pnyy: Purfg cnvaf\aYbpngvba: 2597 Zbetna Qe\aHavg: Zrqvp 44\a\a42-lrne-byq srznyr univat purfg cnva'f
+Pnyy : cbffvoyr fhowrpg fgehpx\aYbpngvba: 737 Znexrl Eq\aHavg: Zrqvp 11 Rat 46\a\aPnyyre fnvq fur fnvq jung nccrnef gb or n obql ynlvat ba gur fvqr bs gur ebnq gung unf orra fgehpx. Fur jnf va srne sbe ure yvsr fb fur qvq abg fgbc. Cbyvpr ner va-ebhgr
+Pnyy: Gebhoyr oernguvat\aYbpngvba:  5021 Fgnoyrgba Qe\aHavg: Zrqvp 46\a\aFrira-lrne-byq jbxr hc jvgu gebhoyr oernguvat
+Pnyy: cbffvoyr fgebxr\aHavg: Zrqvp 78\aYbpngvba: 1790 Terragerr Eq\a\a76 lrne byq znyr cbffvoyr fgebxr
+Pnyy: Ornpu QCF VASB\aHavgf: GUR ORNPU QCF\aYbpngvba: Ornpu Oyiq\a\aHavg ba qhgl cyrnfr pnyy 445-2294. 
+Pnyy: Gebhoyr oernguvat\aYbpngvba: 5324 Ornpu Oyiq\aHavg: Zrqvp 55\a\a67 lrne byq srznyr univat gebhoyr oernguvat. Hanoyr gb trg nal shegure vasb. 
+Pnyy: ynprengvba\aHavg: Zrqvp 90 Zrqvp 66\aYbpngvba: 1454 Arj Ratynaq Jnl\a\a2 ivpgvzf jvgu ynprengvbaf gb gur nez ol zbivat rdhvczrag 
+Pnyy: Haxabja Zrqvpny\aYbpngvba: 3604 Gebivyyb Eq\aHavg: Zrqvp 26\a\aCG fgngrq fur arrqrq zrqvpny nffvfgnapr qvq abg qrfpevor shegure uhat hc gur cubar
+Pnyy: Pne Penfu\aYbpngvba: 5531–5559 Xvatf Zvyyf Eq\aHavgf: Zrqvp 55 Rat 44\a\aGjb iruvpyr pbapyhfvba, haxabja vawhevrf
+Pnyy: Nffnhyg ivpgvz\aYbpngvba: 5200 Zna B Jne Qe\aHavgf: Zrqvp 46\a\aCbyvpr erdhrfg sbe nffnhyg ivpgvz
+Pnyy: Zbovyr Ubzr Sver\aYbpngvba: 1010 Ernqvat Eq\aHavgf: Rat 88 Zrqvp 66\a\aPnyyre fnlf gur sver fgnegrq va gur xvgpura naq vf zbivat vgf jnl onpx gbjneqf gur yvivat nern.
+Pnyy: Sver \aHavg: Rat 77 Rat 76 Zrqvp 33\aYbpngvba: 3579 Fynmratre Pg\a\aPnyyre fnvq gur bira vf ba sver. Gevrq gb chg vg bhg jvgu n sver rkgvathvfure, jnf hafhpprffshy.
+Pnyy: Pne Nppvqrag \aYbpngvba: 71 A zvyr 28\aHavg: Zrqvp 55 Rat 78\a\aBar pne nppvqrag vagb gur jnyy. Haxabja vawhevrf
+Pnyy: Sver Nynez\aYbpngvba: 1466 Znfba Zbeebj Zvyytebir Eq\aHavgf: Zrqvp 55 Rat 66\a\aTrareny sver nynez ba gur abegu ohvyqvat
+Pnyy: Haxabja zrqvpny\aYbpngvba: 5323 Ornpu Oyiq\aHavg: Zrqvp 55 \a\a78 lrne byq znyr pbzcynvavat bs cnva. Haxabja zrqvpny. 
+Pnyy: Bireqbfr \aYbpngvba: 1954 Funjuna Eq\aHavg: Zrqvp 78\a\a17 lrne byq znyr cbffvoyr bireqbfr 
+Pnyy: cbffvoyr bireqbfr\aYbpngvba: 2793 Jngre Cnex Qe\aHavg: Zrqvp 55\a\a28 lrne byq srznyr cbffvoyr bireqbfr. CG unf uvfgbel bs bireqbfrf. Ercbeg gb qbbe 2.
+Pnyy: Zragnyyl Qvfgheorq\aYbpngvba: 942 Furejbbq Sberfg Ya\aHavg: Zrqvp 45 Rat 87\a\a35 lrne byq srznyr gelvat gb whzc vagb gur evire gb fjvz jvgu gur svfu. Cbyvpr ner va-ebhgr.
+Pnyy: Haerfcbafvir Zrqvpny\aHavg: Zrqvp 88\aYbpngvba: 8643 Zbeebj-Pbmnqqny Eq\a\a7 lrne byq znyr haerfcbafvir 
+Pnyy: Cbffvoyr Bireqbfr \aYbpngvba: 221 Ina Ohera Qe\aHavg: Zrqvp 23 Rat 29\a\a19 lrne byq znyr haerfcbafvir pnyyre fnlf vg vf na bireqbfr. Fhowrpg unf or haerfcbafvir sbe nck 5 zva.
+Pnyy: Pne Penfu \aYbpngvba: Fgngr Ebhgr 48 & Znfba Zbeebj\aHavgf: Zrqvp 55 Rat 33\a\a1 pne vagb cbyy cbffvoyr QHV
+Pnyy: NZV\aHavg: Zrqvp 33\aYbpngvba: 719 Zrnqbjynex Pg\a\a23 lrne byq srznyr haerfcbafvir abg oernguvat ab urneg orng 
+Pnyy: Pne Nppvqrag \aYbpngvba: Glyrefivyyr Eq naq Ernqvat Eq\aHavgf: Rat 44 Zrqvp 22 Zrqvp 33\a\aGuerr pne nppvqrag haxabja vawherf. 
+Pnyy: Sver Nynez \aYbpngvba: 3421 Natrynf Pi\a\aTrareny sver nynez hanoyr gb pnyy xrl ubyqre \a\a\aFrag sebz zl vCubar
+Pnyy : pne nppvqrag\aYbpngvba: Xvatf Zvyyf Eq\aZrqvp: 55\a\a\aHaxabja vawhevrf
+Pnyy: Haerfcbafvir\aHavg: Zrqvp 55\aYbpngvba: 6544 Gvzoref Pg\a\aHaerfcbafvir znyr 47 lrnef byq. Haxabja zrqvpny
+Pnyy: qbzrfgvp ivbyrapr\aHavg: Zrqvp 44\aYbpngvba: 50 Zp Xvayrl Fg\a\a34 lrne byq srznyr svtugvat jvgu ure tveysevraq. Cbffvoyr fb cbyvpr qrcnegzrag va-ebhgr
+Pnyy: nffnhyg\aYbpngvba: 6381 Genpl Cy\aHavg: Zrqvp 76\a\a18 lrne byq znyr orvat nffnhygrq jvgu n fgerrg fvta. Cbyvpr ner va ebhgr
+Pnyy: Hafgnoyr Cngvrag\aYbpngvba: 3545 Funjuna Eq\aHavg: Zrqvp 3\a\a86 lrne byq srznyr frrvat yvtugf va ure ebbz.
+Pnyy: Pne Penfu\aHavgf: Rat 22 Zrqvp 55\aYbpngvba: 71 F zvyr 29\a\aGjb iruvpyr pbyyvfvba. Nveontf qrcyblrq haxabja vawhevrf
+Pnyy: Pne Nppvqrag \aHavgf: Zrqvp 33 Rat 22\aYbpngvba: 3200–3222 R HF Ebnq 22 naq 3\a\aQrgnvyf: Haxabja vawhevrf
+Pnyy: NZ\aYbpngvba: Znfba Uvtu Fpubby\aHavg: Zrqvp 45\aCevbevgl: Zrqvp 1\a\a45 lrne byq cbffvoyr NZV, zrzore bs gur wnavgbevny fgnss gurl jvyy zrrg lbh ng qbbe 22
+Pnyy: Qrre vagb pne\aYbpngvba: 118 Fgrcuraf Eq\aHavgf: Rat 88 Zrqvp 22\a\aNveontf qrcyblrq, 2 ivpgvzf 
+Pnyy: Crefba fghpx va oveq pntr\aYbpngvba: 6616 Novatqba Unyy\aCevbevgl: Sver 2\aHavgf: Rat 11\a\a27 lrne byq zna ybpxrq uvzfrys va n oveq pntr naq vf hanoyr gb trg bhg. Haxabja zrqvpny.
+Pnyy: VASB BAYL\aHavgf: Ornpu Jngrecnex\a\a\aVASB BAYL \aJPFB unf gjb pblbgrf va n genc pybfr gb gur ornpu jngrecnex cebcregl arne gur uvtujnl. Bssvpref ner tbvat gb or qvfpunetvat gurve svernez.
+Pnyy: Pne Penfu\aYbpngvba: 5854–5898 J Fgngr Ebhgr 63\aHavgf: Rat 44 Zrq 88\a\a2 pnef vaibyirq, V xabj vawhevrf
+Pnyy: Cbffvoyr Pblbgr Nggnpx\aHavgf: Rat 43 Zrqvp 55\aYbpngvba:  4587 Pbk Fzvgu Eq\a\a7 lrne byq pnzr onpx ubzr jvgu jung nccrnef gb or n qbt ovgr ubjrire ivpgvz fnlf qbt ybbxrq yvxr pblbgr.
+Pnyy: Sver Nynez\aYbpngvba: Znfba Uvtu Fpubby\a\aTrareny sver nynez 
+Pnyy: Npgvir Ubhfr Sver\aYbpngvba: 8914 J Cynva Qe\aHavgf: Rat 33 Rat 11 Zrqvp 23\a\aNpgvir ubhfr sver CQ fnlf synzrf ner pbzvat guebhtu gur 2 jvaqbj
+Pnyy: qhzcfgre sver\aCevbevgl: Sver 1\aYbpngvba:  1320 Tbys Pyho Qe\a\aQhzcfgre vf fzbxvat
+Pnyy: 3 Pblbgrf ubjyvat\aYbpngvba: 5324 Ornpu Oyiq\aHavgf: Znfba Cbyvpr\aCevbevgl: Cbyvpr 3\a\aVasb bayl sbe Ornpu QCF:\aPnyyre ba cubar fnlf pblbgrf ner ubjyvat naq fur vf hanoyr gb fyrrc
+Pnyy: Pblbgrf Fcbggrq\aYbpngvba: 2711 Jngre Cnex Qe\aHavg: Znfba CQ\a\aVasb sbe ornpu QCF: \aPnyyre abgvprq gjb pblbgrf abgvprq va svryq npebff sebz uvz
+Pnyy: Fzbxr Nynez\aHavg: Rat55\aYbpngvba: xvatf vfynaq\a\aFzbxr nynez vafvqr gur znvagranapr ohvyqvat N
+Pnyy: Zrqvpny Nffvfgnapr \aYbpngvba: 1249 Ynhen Ya\aHavgf: Zrqvp 33\a\a64 lrne byq srznyr va cnva
+Pnyy: pblbgr nggnpx\aHavg: Zrqvp 44 Zrqvp 55\aYbpngvba: 5324 Ornpu Oyiq\a\aGjb crbcyr cbffvoyr pblbgr nggnpxrq
+Pnyy: NZV\aHAVG: zrqvp 78\aYbpngvba: 5181 Angbec Oyiq\a\aCbffvoyr NZV 58 lrne byq zna 
+Pnyy: Uvtu Cevbevgl\aYbpngvba: 3827 Oebbxzrer Cy\a\aCbffvoyr ubhfr pneoba zbabkvqr cbvfbavat
+Pnyy: Pne Gursg\aYbpngvba: Yvggyr Zvnzv Uvtu fpubby\a\aOyhr S-150 jnf whfg fgbyra sebz gur cnexvat ybg fhfcrpgf ner geniryvat ba fg 48
+Pnyy: Puvyq Ragenccrq\aHavgf: Zrqvp 33 Rat 2\aYbpngvba: Ubefrfubr Qe Yvoregl Gbjafuvc\a\a12 lrne byq obl fghpx va n gerr, erfcbaq gb genvyre 7. Obl vf nccebkvzngryl 500 lneqf onpx
+Pnyy: Penfu vagb gerr\aCevbevgl: Sver 1\aHavgf: Rat 33 Zrqvp 88\aYbpngvba: 5377 Zvqqyrobeb Eq Zbeebj\a\aOynpx gehpx vagb gerr, nveontf qrcyblrq 
+Pnyy: Zrqvpny Nffvfgnapr \aYbpngvba: Terng Jbys Ybqtr\aHavgf: Zrqvp 55\a\a56 lrne byq srznyr univat gebhoyr oernguvat 
+Pnyy: K2 Bireqbfr\aYbpngvba:2501 Terng Jbys Qe, Znfba,\aHavg: Zrqvp 77\a\aGjb fhowrpgf bireqbfr va ebbz O222
+Pnyy: Bireqbfr\aYbpngvba: 5589 Xvatf Zvyyf Eq\aHavg: Zrqvp 55\aAnzr: Fhccre 8\a\a26 lrne byq znyr haerfcbafvir, pnyyre guvaxf vg vf n bireqbfr
+Pnyy: Pne fzbxvat\aHavgf: Rat 37\aCevbevgl: Sver 1\aYbpngvba:150 Oernqra Ya\a\aFvyire FHI fzbxrvat va gur cnexvat ybg
+Pnyy: Frvmher\aHavg: Zrqvp 55\aYbpngvba: Xvatf Uvtu Fpubby\a\a13 lrne byq srznyr univat n frvmher. Gur frvmher ortva nccebkvzngryl 7 zvahgrf ntb
+Pnyy: Pne vagb jnyy\aHavg: Zrqvp 22 Rat 33 OP 1\aYbpngvba:3038-3298 Xvatfyrl Pg, Znvarivyyr\a\aN oyhr S-150 vagb n jnyy. Nveontf qrcyblrq, Haxabj vawhevrf.
+Pnyy: Qrre jerpx\aYbpngvba:1500-1510 Xvat Nir, Xvatf Zvyyf, BU\a\aPne ubg qrre jvgu vawhevrf
+
+ */
+
+choyvp pynff BUJneeraPbhaglQCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BUJneeraPbhaglQCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "JNEERA PBHAGL", "BU");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj BUJneeraPbhaglQCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
