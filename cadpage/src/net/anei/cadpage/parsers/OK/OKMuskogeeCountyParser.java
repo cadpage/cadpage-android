1455189b019f79fa7efd4aabd5b59bfe7b6d16c0
Fishlicka <Fishlicka@gmail.com>
2015-11-02 18:20:11 -0800

    OKMuskCountyParser

---
+++ cadpage/src/net/anei/cadpage/parsers/OK/OKMuskogeeCountyParser.java
@@ -0,0 +1,89 @@
+package net.anei.cadpage.parsers.OK;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.SmartAddressParser;
+
+public class OKMuskogeeCountyParser extends SmartAddressParser {
+
+  public OKMuskogeeCountyParser() {
+    super(CITY_LIST, "MUSKOGEE COUNTY", "OK");
+    setFieldList("UNIT MAP ADDR APT CITY PLACE CALL ID DATE TIME INFO");
+  }
+  
+  private static Pattern MASTER = Pattern.compile("((?:[A-Z]\\d+ +)+(?:Alarm +)?)(?:Territory (\\d[A-Z]) +)?(.*)(\\d{4}-\\d{8})([^\n]+)?(?:\n *(.*))?", Pattern.DOTALL);
+  private static Pattern DISCARD = Pattern.compile("  (?:Call Number \\d+ was created from Call Number \\d+\\(([^ ]+ \\d{1,2} \\d{4}) +(\\d{1,2}:\\d{1,2}[AP]M)\\)|Dispatch received by unit \\d+)");
+  
+  @Override
+  public boolean parseMsg(String subject, String body, Data data) {
+    
+    if (!subject.equals("Incident")) return false;
+    
+    Matcher mat = MASTER.matcher(body);
+    if (!mat.matches()) return false;
+    
+    data.strUnit = mat.group(1).trim();
+    data.strMap = getOptGroup(mat.group(2));
+    
+    // ADDR APT CITY CALL
+    parseAddress(StartType.START_ADDR, FLAG_IMPLIED_INTERSECT | FLAG_PAD_FIELD, mat.group(3).trim(), data);
+    data.strCall = getLeft();
+    
+    // abort if no CITY or CALL
+    if (data.strCall.length() == 0 || data.strCity.length() == 0) return false;
+    
+    // PLACE ID
+    data.strPlace = getPadField();
+    data.strCallId = mat.group(4);
+    
+    // parse 
+    String firstInfoLine = mat.group(5);
+    if (firstInfoLine != null) {
+      Matcher dmat = DISCARD.matcher(firstInfoLine);
+      
+      // look for first Call Number construct to parse DATE TIME from
+      int fstart = 0;
+      while (dmat.find(fstart)) {
+        fstart = dmat.end();
+        String g1 = dmat.group(1);
+        if (g1 == null) continue;
+        
+        data.strDate = g1;
+        data.strTime = dmat.group(2);
+        break;
+      }
+      
+      // useful info goes in INFO
+      data.strSupp = dmat.replaceAll("").trim();
+    } 
+    
+    // append the rest of the page to INFO
+    data.strSupp = append(data.strSupp, "\n", getOptGroup(mat.group(6)));
+    
+    return true;
+  }
+
+  private static String[] CITY_LIST = new String[]{
+      "MUSKOGEE",
+      "BOYNTON",
+      "BRAGGS",
+      "COUNCIL HILL",
+      "FORT GIBSON",
+      "HASKELL",
+      "OKTAHA",
+      "PORUM",
+      "TAFT",
+      "WAINWRIGHT",
+      "WARNER",
+      "WEBBERS FALLS",
+      "RIVER BOTTOM",
+      "SAND HILLS",
+      "SIMMS",
+      "SOUR JOHN",
+      "SUMMIT",
+      "PUMPKIN CENTER",
+  };
+  
+}
