d2afe778b10fba81efb0888d4a307023d2470790
Ken Corbin <ken@cadpage.org>
2015-11-22 16:33:57 -0800

    Checking in Washington County, GA

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/GA/GAWashingtonCountyParserTest.java
@@ -0,0 +1,36 @@
+cnpxntr arg.narv.pnqcntr.cnefref.TN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Jnfuvatgba Pbhagl, TN
+
+Pbagnpg: gbqqrinaf12 <gbqqrinaf12@ngg.arg>
+Fraqre: JNFUVATGBAPB911@911.pbz
+JNFUVATGBAPB911:RZFBGU RZF BGURE 383 JNYXRE QNVEL EQ 4782473720 JNEGURA YA
+JNFUVATGBAPB911:RZFBGU RZF BGURE 405 JUVGR YVAR FG 4785525888 TVYJBBQ FG
+JNFUVATGBAPB911:RZFFVP FVPX CREFBA 525 JVTTVAF FG 4783574248 PNEIRE FG
+JNFUVATGBAPB911:RZFFVP FVPX CREFBA 145 OEVYRL EQ 4785521540 F OEBJA FG
+JNFUVATGBAPB911:RZFSNY SNYY 647 SREAPERFG QE 4783571858 Ncg: 9O Oyqt FCNEGN EQ
+JNFUVATGBAPB911:RZFFVP FVPX CREFBA 441 PNEIRE FG 4782323597 YBIRGG FG
+JNFUVATGBAPB911:RZFFVP FVPX CREFBA 313 R RYQRE FG 4782326975 J SYBLQ FG
+
+*/
+
+choyvp pynff TNJnfuvatgbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp TNJnfuvatgbaPbhaglCnefreGrfg() {
+    frgCnefre(arj TNJnfuvatgbaPbhaglCnefre(), "JNFUVATGBA PBHAGL", "TN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgGbqqRinaf() {
+    
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj TNJnfuvatgbaPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
