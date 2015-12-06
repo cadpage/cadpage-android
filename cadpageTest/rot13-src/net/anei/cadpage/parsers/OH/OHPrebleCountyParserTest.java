d32376ea9bcad926274ee1fa1bc3f14a4a3e653b
Jamie Harper <snailtk@gmail.com>
2015-12-06 10:47:34 -0800

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/OH/OHPrebleCountyParserTest.java
@@ -0,0 +1,56 @@
+cnpxntr arg.narv.pnqcntr.cnefref.BU;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pnzqra Fbzref-GJC Sver & Erfphr
+Ybpngvba: Pnzqra, BU, Havgrq Fgngrf
+Fraqre: ceroyrsvercntr@tznvy.pbz
+Fraqre: <CEROYRFUREVSS@fjbuvb.gjpop.pbz>
+
+{Pnzqra Sver cntr erprvirq ng 15:35:59 ba 12/04/15} Pnzqra Sver Cntr Erprvirq ng 15:35:59 ba 12/04/15
+{Pnzqra Sver Cntr Erprvirq ng 15:35:59 ba 12/04/15} Pnzqra Sver Cntr Erprvirq ng 15:35:59 ba 12/04/15
+{Pnzqra Sver cntr erprvirq ng 08:52:23 ba 12/03/15} Pnzqra Sver Cntr Erprvirq ng 08:52:23 ba 12/03/15
+{Pnzqra Sver Cntr Erprvirq ng 08:52:23 ba 12/03/15} Pnzqra Sver Cntr Erprvirq ng 08:52:23 ba 12/03/15
+{Pnzqra Sver cntr erprvirq ng 18:55:35 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 18:55:35 ba 12/02/15
+{Pnzqra Sver Cntr Erprvirq ng 18:55:35 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 18:55:35 ba 12/02/15
+{Pnzqra Sver cntr erprvirq ng 17:34:35 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:34:35 ba 12/02/15
+{Pnzqra Sver Cntr Erprvirq ng 17:34:35 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:34:35 ba 12/02/15
+{Pnzqra Sver cntr erprvirq ng 17:29:40 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:29:40 ba 12/02/15
+{Pnzqra Sver Cntr Erprvirq ng 17:29:40 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:29:40 ba 12/02/15
+{Pnzqra Sver cntr erprvirq ng 17:02:07 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:02:07 ba 12/02/15
+{Pnzqra Sver Cntr Erprvirq ng 17:02:07 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 17:02:07 ba 12/02/15
+{Pnzqra Sver cntr erprvirq ng 16:45:41 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 16:45:41 ba 12/02/15
+{Pnzqra Sver Cntr Erprvirq ng 16:45:41 ba 12/02/15} Pnzqra Sver Cntr Erprvirq ng 16:45:41 ba 12/02/15
+CEROYRFUREVSS:[244]- ANGHER: 28 YBPNGVBA: 90 FUNJARR CYNVAF ABEGU PNZQRA NPPRFF OL RVQFBA FG PBZZRAGF: XVGPURA SVER
+CEROYRFUREVSS:[134]--Angher: Penfu CV - Ybpngvba: 11694.00 Pbhagl Ebnq 227 Pnzqra
+CEROYRFUREVSS:[134]--406Angher: Sver - Ybpngvba: 12386.00 Fgngr Ebhgr 732 Pnzqra - Pbzzragf: SVRYQ SVER
+CEROYRFUREVSS:[134]- ANGHER: 4 YBPNGVBA: F H F EBHGR 127 / R PNZQRA PBYYRTR PBEARE EQ PNZQRA
+CEROYRFUREVSS:[134]- ANGHER: 28Z YBPNGVBA: 1027 OBJRE PBZZRAGF: GNAXRE NAQ ZNACBJRE, FGEHPGHER SVER ZVYSBEQ GBJAFUVC PEBFFRF BS UNZVYGBA RNGBA NAQ SNHE PBHEG
+CEROYRFUREVSS:[137]- ANGHER: 28 YBPNGVBA: 4022 PNZQRA JRFG RYXGBA EQ FBZREIVYYR ORGJRRA CYNGG EQ / ABEGUREA EQ PBZZRAGF: CBFF FGEHPGHER SVER
+CEROYRFUREVSS:[134]--Angher: Sver - Ybpngvba: 4995.00 J Fgngr Ebhgr 122 Rngba - Pbzzragf: QVGPU BA SVER
+CEROYRFUREVSS:[134]- ANGHER: 28 YBPNGVBA: 310 F RNFG FG TENGVF ANGHER: 28 YBPNGVBA: 310 F RNFG FG TENGVF
+CEROYRFUREVSS:[310]--GRYRCUBAR CBYR BA SVER
+CEROYRFUREVSS:[139]- ANGHER: 4 YBPNGVBA: 910 R FGNGR EBHGR 725 PNZQRA ORGJRRA PNZQRA JRFG RYXGBA EQ / HCCRE FBZREF EQ PBZZRAGF: BAR IRU EBYYBIRE
+CEROYRFUREVSS:[104]--Grfg Grkg Cntr gb Fgngvba 400 @ 1015 Ubhef
+*/
+
+choyvp pynff BUCeroyrPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp BUCeroyrPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "CEROYR PBHAGL", "BU");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj BUCeroyrPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
