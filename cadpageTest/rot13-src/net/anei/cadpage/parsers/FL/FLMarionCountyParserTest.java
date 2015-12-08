3fd1405ca852b9a281a8ee35f93052cca25ae0f5
Ken Corbin <ken@cadpage.org>
2015-12-08 10:36:32 -0800

    Fixed parsing problem with Marion County, FL

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/FL/FLMarionCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/FL/FLMarionCountyParserTest.java
@@ -12,6 +12,10 @@ Pbagnpg: Zreyvan, Oergg <Oergg.Zreyvan@znevbapbhaglsy.bet>
 ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-063313 Ers:77 - ZIP . Nqqerff: 6793 Fj Ujl 200 Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 166/1Q ,
 ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-063811 Ers:L Nynezf / Uvtu Nqqerff: 5664 Fj 60gu Nir Ybp Anzr:GUR PRAGREF BS BPNYN Ncg/Ybg/Ez#: Oyqt/Jvat:4 Mvc 34474 Znc Cntr ct 166/2O ,
 ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070481 Ers:L Fgehpg Sver/ ENqqerff: 7631 Fj 82aq Cy Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 165/3Q ,
+ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070567 Ers:29 - Genssvp PenNqqerff: FJ 60gu Nir/FJ 80gu Fg Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 166/2Q ,
+ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070674 Ers:29 - Genssvp PenNqqerff: 8505 FJ UJL 200 Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34481 Znc Cntr ct 188/2P ,
+ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070767 Ers:ZIP - PbzzrepvnyNqqerff: 8600 Fj Fo V75 Ybp Anzr:ZZ 346 Fbhguobhaq Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 190/1N
+ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070828 Ers:L Nynezf / PbzzrNqqerff: 9676 Fj 62aq Ybbc Ybp Anzr:FGBARPERRX TBYS PYHOUBHFR Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34481 Znc Cntr ct 165/1O ,
 
  */
 
@@ -52,6 +56,45 @@ choyvp pynff SYZnevbaPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
         "NQQE:7631 Fj 82aq Cy",
         "PVGL:34476",
         "ZNC:165/3Q");
+
+    qbGrfg("G4",
+        "ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070567 Ers:29 - Genssvp PenNqqerff: FJ 60gu Nir/FJ 80gu Fg Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 166/2Q ,",
+        "HAVG:R521",
+        "VQ:2015-070567",
+        "PNYY:29 - Genssvp Pen",
+        "NQQE:FJ 60gu Nir & FJ 80gu Fg",
+        "PVGL:34476",
+        "ZNC:166/2Q");
+
+    qbGrfg("G5",
+        "ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070674 Ers:29 - Genssvp PenNqqerff: 8505 FJ UJL 200 Ybp Anzr: Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34481 Znc Cntr ct 188/2P ,",
+        "HAVG:R521",
+        "VQ:2015-070674",
+        "PNYY:29 - Genssvp Pen",
+        "NQQE:8505 FJ UJL 200",
+        "PVGL:34481",
+        "ZNC:188/2P");
+
+    qbGrfg("G6",
+        "ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070767 Ers:ZIP - PbzzrepvnyNqqerff: 8600 Fj Fo V75 Ybp Anzr:ZZ 346 Fbhguobhaq Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34476 Znc Cntr ct 190/1N",
+        "HAVG:R521",
+        "VQ:2015-070767",
+        "PNYY:ZIP - Pbzzrepvny",
+        "NQQE:8600 Fj Fo V75",
+        "ZNQQE:8600 Fj V 75",
+        "CYNPR:ZZ 346 Fbhguobhaq",
+        "PVGL:34476",
+        "ZNC:190/1N");
+
+    qbGrfg("G7",
+        "ab fhowrpg / ***PNYY NYREG*** R521 Pnfr #2015-070828 Ers:L Nynezf / PbzzrNqqerff: 9676 Fj 62aq Ybbc Ybp Anzr:FGBARPERRX TBYS PYHOUBHFR Ncg/Ybg/Ez#: Oyqt/Jvat: Mvc 34481 Znc Cntr ct 165/1O ,",
+        "HAVG:R521",
+        "VQ:2015-070828",
+        "PNYY:L Nynezf / Pbzzr",
+        "NQQE:9676 Fj 62aq Ybbc",
+        "CYNPR:FGBARPERRX TBYS PYHOUBHFR",
+        "PVGL:34481",
+        "ZNC:165/1O");
     
   }
  
