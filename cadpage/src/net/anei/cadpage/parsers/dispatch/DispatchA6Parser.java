bb716c77218bd184b17577c35c7741d1405a066f
Ken Corbin <ken@cadpage.org>
2015-12-09 10:45:12 -0800

    Fixed parsing problem with St J oseph County, IN

--- cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchA6Parser.java
+++ cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchA6Parser.java
@@ -6,23 +6,50 @@ import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.SmartAddressParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 public class DispatchA6Parser extends SmartAddressParser {
   
   public DispatchA6Parser(Properties cityCodes, String defCity, String defState) {
     super(cityCodes, defCity, defState);
-    setFieldList("CALL ID MAP DATE ADDR APT CITY PLACE ID X INFO TIME UNIT");
   }
   
+  private static final Pattern RUN_REPORT_PTN = Pattern.compile("->Inc Addr = (.*?)Equip = (.*?)(Create = .*)Rpt # = *([-0-9]*)");
+  private static final Pattern RUN_REPORT_TIME = Pattern.compile(" *=(?: (\\d\\d? \\d\\d \\d\\d))?");
   private static final Pattern LEAD_DATE_PAT = Pattern.compile("^(?:([- A-Z0-9]+) )?(?:\\d\\d/\\d\\d/\\d{4} T?ARTADDR# )?(?:(?:Ic# +(.*?) +Ds# ([^ ]*?) (?:Al# ([^ ]*?)|Cntrl)? Utl# (?:([- A-Z0-9]*?) +)?)?(\\d\\d/\\d\\d/\\d\\d(?:\\d\\d)?)|\\^ ) *-? *");
   private static final Pattern CROSS_MARK2_PAT = Pattern.compile(" :\\\\?\\( *(\\d*) *\\) | <> ");
   private static final Pattern CROSS_MARK1_PAT = Pattern.compile("^\\{ *\\d+ *\\} *");
   private static final Pattern TIME_UNIT_PAT = Pattern.compile("(?<!\\d)(\\d{4}),(\\d{3})");
-  
+ 
   @Override
   protected boolean parseMsg(String body, Data data) {
     
-    Matcher match = LEAD_DATE_PAT.matcher(body);
+    
+    // Check for a run report pattern
+    Matcher match = RUN_REPORT_PTN.matcher(body);
+    if (match.matches()) {
+      setFieldList("ADDR APT UNIT INFO ID");
+      data.msgType = MsgType.RUN_REPORT;
+      parseAddress(match.group(1).trim(), data);
+      data.strUnit = match.group(2).trim();
+      String times = match.group(3).trim();
+      data.strCallId = match.group(4);
+      
+      match = RUN_REPORT_TIME.matcher(times);
+      StringBuffer sb = new StringBuffer();
+      while (match.find()) {
+        String time = match.group(1);
+        if (time == null) time = "\n";
+        else time = "  "+time.replace(' ', ':')+'\n';
+        match.appendReplacement(sb, time);
+      }
+      match.appendTail(sb);
+      data.strSupp = sb.toString();
+      return true;
+    }
+    
+    setFieldList("CALL ID MAP DATE ADDR APT CITY PLACE ID X INFO TIME UNIT");
+    match = LEAD_DATE_PAT.matcher(body);
     if (!match.find()) return false;
     body = body.substring(match.end());
     data.strCall = getOptGroup(match.group(1));
