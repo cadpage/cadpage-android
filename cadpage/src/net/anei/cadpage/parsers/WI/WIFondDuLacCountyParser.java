356164ef3f38ff462ec4a95f53d0ac4a7f5d0ffc
Ken Corbin <ken@cadpage.org>
2015-12-15 13:16:50 -0800

    Update FondDuLacCounty, WI parser

--- cadpage/src/net/anei/cadpage/parsers/WI/WIFondDuLacCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WI/WIFondDuLacCountyParser.java
@@ -2,12 +2,13 @@ package net.anei.cadpage.parsers.WI;
 
 import java.util.Properties;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchA41Parser;
 
 public class WIFondDuLacCountyParser extends DispatchA41Parser {
 
   public WIFondDuLacCountyParser() {
-    super("CALLALERT:", CITY_CODES, "FOND DU LAC COUNTY", "WI", "FA|FF");
+    super("CALLALERT:", CITY_CODES, "FOND DU LAC COUNTY", "WI", "F[AFPS]");
   }
   
   @Override
@@ -15,6 +16,19 @@ public class WIFondDuLacCountyParser extends DispatchA41Parser {
     return "cadmail@fdlco.wi.gov";
   }
   
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!super.parseMsg(body, data)) return false;
+    data.strSource = data.strChannel;
+    data.strChannel = "";
+    return true;
+  }
+  
+  @Override
+  public String getProgram() {
+    return super.getProgram().replace("CH", "SRC");
+  }
+
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
       "FDL", "FOND DU LAC",
   });
