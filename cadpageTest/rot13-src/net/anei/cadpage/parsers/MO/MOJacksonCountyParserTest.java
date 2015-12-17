a61fa2479779a2918b63db8d277261a48f0d49d5
Ken Corbin <ken@cadpage.org>
2015-12-17 09:44:58 -0800

    Removed last references to HtmlCleaner library

--- cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOJacksonCountyParserTest.java
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOJacksonCountyParserTest.java
@@ -11,17 +11,9 @@ Wnpxfba Pbhagl, ZB
 @EhaJvgu(Rapybfrq.pynff)
 choyvp pynff ZBWnpxfbaPbhaglCnefreGrfg {
   
-  choyvp fgngvp pynff GrfgN rkgraqf ZBWnpxfbaPbhaglNCnefreGrfg {
-    choyvp GrfgN() {
-      frgCnefre(arj ZBWnpxfbaPbhaglCnefre());
-    }
-  }
-  
   choyvp fgngvp pynff GrfgO rkgraqf ZBWnpxfbaPbhaglOCnefreGrfg {
     choyvp GrfgO() {
       frgCnefre(arj ZBWnpxfbaPbhaglCnefre());
     }
   }
-  
- 
 }
