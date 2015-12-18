ee53ba2b89d0ff2a616ee3eb9b1ca46eb9497436
Ken Corbin <ken@cadpage.org>
2015-12-18 12:01:22 -0800

    Fixed parsing problem with Darlington County, SC

--- cadpage/src/net/anei/cadpage/parsers/SC/SCDarlingtonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCDarlingtonCountyParser.java
@@ -1,13 +1,14 @@
 
 package net.anei.cadpage.parsers.SC;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchSouthernParser;
 
 
 public class SCDarlingtonCountyParser extends DispatchSouthernParser {
 
   public SCDarlingtonCountyParser() {
-    super(CITY_LIST, "DARLINGTON COUNTY", "SC", DSFLAG_NO_NAME_PHONE);
+    super(CITY_LIST, "DARLINGTON COUNTY", "SC", DSFLAG_FOLLOW_CROSS);
   }
   
   @Override
@@ -15,6 +16,19 @@ public class SCDarlingtonCountyParser extends DispatchSouthernParser {
     return MAP_FLG_PREFER_GPS;
   }
     
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (!body.startsWith("CAD:")) return false;
+    body = body.substring(4).trim();
+    return super.parseMsg(body, data);
+  }
+
+  @Override
+  protected boolean isNotExtraApt(String apt) {
+    if (apt.startsWith("MM")) return true;
+    return super.isNotExtraApt(apt);
+  }
+
   private static final String[] CITY_LIST = new String[]{
     "CLYDE",
     "DARLINGTON",
@@ -28,6 +42,7 @@ public class SCDarlingtonCountyParser extends DispatchSouthernParser {
     "TIMMONSVILLE",
     
     // Lee County
+    "LEE COUNTY",
     "BISHOPVILLE"
   };
 }
