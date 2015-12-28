13820ff50f022f71a556157886a1daf4f6165d82
Jean Goul <jean@cadpage.org>
2015-12-28 09:50:16 -0800

    Checking in new parsers

---
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORLaneCountyParser.java
@@ -0,0 +1,13 @@
+package net.anei.cadpage.parsers.OR;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+/**
+* Lane County, OR
+ */
+
+public class ORLaneCountyParser extends GroupBestParser {
+  public ORLaneCountyParser() {
+    super(new ORLaneCountyAParser(), new ORLaneCountyBParser());
+  }
+}
