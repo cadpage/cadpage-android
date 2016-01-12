bf99fa48e36bb438d6181483972d9276d94ab4a1
Ken Corbin <ken@cadpage.org>
2016-01-12 00:13:36 -0800

    Fixed parsing problem with Shelby County, AL

--- cadpage/src/net/anei/cadpage/parsers/AL/ALShelbyCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALShelbyCountyParser.java
@@ -1,22 +1,30 @@
 package net.anei.cadpage.parsers.AL;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 /**
  * Shelby County, AL
  */
 public class ALShelbyCountyParser extends FieldProgramParser {
   
-  private static final Pattern PREFIX_PTN = Pattern.compile("^(?:NEW - |Arns Alert\n) *", Pattern.CASE_INSENSITIVE);
+  private static final Pattern PREFIX_PTN = Pattern.compile("^(?:NEW - |Arns Alert\n|Incident Time:) *", Pattern.CASE_INSENSITIVE);
   
   public ALShelbyCountyParser() {
     super("SHELBY COUNTY", "AL",
-          "( ID DATETIME | DATETIME ID ) SRC_UNIT CALL ADDR! APT X NAME");
+          "( Unit(s)_Assigned:UNIT ID | ID DATETIME | DATETIME ID ) SRC CALL ADDR! APT X EXTRA INFO/N+");
+    setupProtectedNames("EGG AND BUTTER");
+    setupSpecialStreets("FREDERICK PASS");
+    removeWords("EST", "LA");
   }
   
   @Override
@@ -26,11 +34,24 @@ public class ALShelbyCountyParser extends FieldProgramParser {
   
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
+    
+    if (subject.startsWith("Event Closed:")) data.msgType = MsgType.RUN_REPORT;
   
     Matcher match = PREFIX_PTN.matcher(body);
     if (match.find()) body = body.substring(match.end());
     
     return parseFields(body.split("\n"), data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("TYPE")) return new SkipField("Cad.*", true);
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("SRC_UNIT")) return new SourceUnitField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    if (name.equals("X"))  return new MyCrossField();
+    if (name.equals("EXTRA")) return new MyExtraField();
+    return super.getField(name);
   }
 
   private static final Pattern DATE_TIME_PTN = Pattern.compile("\\d{4}-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d");
@@ -71,14 +92,28 @@ public class ALShelbyCountyParser extends FieldProgramParser {
   }
   
   private static final Pattern ADDR_SPECIAL_PTN = Pattern.compile("([A-Z]{4} [A-Z]{3}): @(.*?)(?::(.*))?");
-  private static final Pattern ADDR_SPECIAL_PTN2 = Pattern.compile("(.*?):(.*?): *(\\d{3}-?\\d{3}-\\d{4})");
-  private static final Pattern ADDR_PLACE_PTN = Pattern.compile("(LL\\([-+,0-9\\.:]+\\) *|[^:]+): ?@?(.*)");
-  private static final Pattern ADDR_APT_PTN = Pattern.compile("[ ,:]+(?:(?:APT|RM|ROOM|UNIT|SUITE?)(?![A-Z]) *([^ ]+(?: +FLR? *[^ ]+)?)|((?:LOT|FL) *[^ ]+)|(?<=,) *(\\d+[A-Z]?))$");
+  private static final Pattern ADDR_DELIM_PTN = Pattern.compile(" *(?:[@:;,]|: @) *"); 
+  private static final Pattern ADDR_APT_PTN = Pattern.compile("(?:#|(?:APT|RM|ROOM|UNIT|SUITE?|STE|LOT)(?![A-Z])) *(.*)|(?:LOT|FL) *.*|[A-Z]?\\d{1,5}[A-Z]?|[A-Z]|\\d+(?:ST|ND|RD|TH|) *FLR?");
+  private static final Pattern PLACE_APT_PTN = Pattern.compile("(.*)\\b(?:APT|RM|ROOM|UNIT|SUITE?|STE|LOT)(?![A-Z]) *(.+)|((?:LOT|FLR?) *.+)");
+  private static final Pattern ADDR_PHONE_PTN = Pattern.compile("\\d{3}-\\d{3}-\\d{4}|\\(\\d{3}\\) \\d{3}[- ]\\d{4}|\\d{3}-\\d{4}");
   private static final Pattern ADDR_UNIT_PTN = Pattern.compile("^[A-Z]{2}FD\\b");
   private static final Pattern ADDR_CITY_PTN = Pattern.compile("(.*?) *\\b([A-Z][A-Z0-9]{3}) ([A-Z]{3})");
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
+      
+      // Check for LL() GPS coordinates
+      if (field.startsWith("LL(")) {
+        int pt = field.indexOf(')', 2);
+        if (pt < 0) pt = field.length();
+        data.strAddress = field.substring(0,pt+1);
+        field = field.substring(pt+1).trim();
+      }
+      
+      String part1 = null;
+      String part2 = null;
+      List<String> aptList = new ArrayList<String>();
+      String place = "";
       
       // Check special city src: @place:address pattern
       Matcher match = ADDR_SPECIAL_PTN.matcher(field);
@@ -86,10 +121,10 @@ public class ALShelbyCountyParser extends FieldProgramParser {
         String city = match.group(1);
         String city2 = stripCity(city, data);
         if (city2.length() > 0) data.strCity = city;
-        String place = match.group(2).trim();
+        place = match.group(2).trim();
         String addr = getOptGroup(match.group(3));
         match = ADDR_UNIT_PTN.matcher(place);
-        if (match.find()) {
+        if (match.lookingAt()) {
           data.strUnit = match.group();
           place = place.substring(match.end()).trim();
         }
@@ -97,84 +132,113 @@ public class ALShelbyCountyParser extends FieldProgramParser {
           addr = place;
           place = "";
         }
-        field = addr;
+        addr = stripCity(addr, data);
+        parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, addr, data);
         if (place.endsWith("FD")) {
           if (data.strSource.length() == 0) data.strSource = place;
+          place = "";
         } else {
-          data.strPlace = place;
+          match = PLACE_APT_PTN.matcher(place);
+          if (match.matches()) {
+            place = match.group(1).trim();
+            String tmp = match.group(2);
+            if (tmp == null) tmp = match.group(3);
+            aptList.add(tmp);
+          }
         }
       }
 
       else {
-        
-        // Check for special case Address:Place:Phone
-        match = ADDR_SPECIAL_PTN2.matcher(field);
-        if (match.matches()) {
-          field = match.group(1).trim();
-          data.strPlace = match.group(2).trim();
-          data.strPhone = match.group(3);
-        }
-        
-        // OK, things look "normal"
-        // look for trailing place name
-        
-        // Check for trailing apt
-        match = ADDR_APT_PTN.matcher(field);
-        if (match.find()) {
-          String apt = match.group(1);
-          if (apt == null) apt = match.group(2);
-          if (apt == null) apt = match.group(3);
-          data.strApt = apt;
-          field = field.substring(0,match.start()).trim();
-        }
-        
-        match = ADDR_PLACE_PTN.matcher(field);
-        if (match.find()) {
-          field =  match.group(1).trim();
-          String place = match.group(2).trim();
-          if (NUMERIC.matcher(place).matches()) {
-            data.strApt = append(place, "-", data.strApt);
-          } else {
-            data.strPlace = place;
+
+        // Break field up into component parts
+        for (String part : ADDR_DELIM_PTN.split(field)) {
+          
+          if (part.length() == 0) continue;
+          if (part.equals("NONE")) continue;
+          
+          part = stripCity(part, data);
+          
+          if (part.endsWith("FD")) {
+            if (data.strSource.length() == 0) data.strSource = part;
+            continue;
           }
-        }
-        
-        // Check for trailing apt (again)
-        match = ADDR_APT_PTN.matcher(field);
-        if (match.find()) {
-          if (data.strApt.length() == 0) {
-            String apt = match.group(1);
-            if (apt == null) apt = match.group(2);
-            if (apt == null) apt = match.group(3);
-            data.strApt = append(apt, "-", data.strApt);
+          
+          if (data.strAddress.length() == 0) {
+            if (part1 == null) {
+              part1 = part;
+              continue;
+            }
           }
-          field = field.substring(0,match.start()).trim();
+          
+          match = ADDR_APT_PTN.matcher(part);
+          if (match.matches()) {
+            String tmp = match.group(1);
+            if (tmp == null) tmp = part;
+            aptList.add(tmp);
+            continue;
+          }
+
+          if (ADDR_PHONE_PTN.matcher(part).matches()) {
+            data.strPhone = part;
+            continue;
+          }
+
+          if (data.strAddress.length() == 0 && part2 == null) {
+            part2 = part;
+            continue;
+          }
+          place = append(place, " - ", part);
         }
       }
       
-      // Check for a comma separated Apt, which may include it's own city name
-      if (!field.startsWith("LL")) {
-        int pt = field.lastIndexOf(',');
-        if (pt >= 0) {
-          String apt = field.substring(pt+1).trim();
-          apt = stripFieldStart(apt, "#");
-          field = field.substring(0,pt).trim();
-          apt = stripCity(apt, data);
-          if (data.strApt.length() == 0) {
-            data.strApt = apt;
+      // If we did not identify a GPS address earlier on, see what we can identify as an address
+      // from the first two parts
+      if (data.strAddress.length() == 0 && part1 != null) {
+        if (part2 == null) {
+          parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, part1, data);
+        } else if (part2.startsWith("BETWEEN ")){
+          parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, part1, data);
+          data.strCross = part2.substring(8).replace('&', '/');
+        } else {
+          Result res1 = parseAddress(StartType.START_ADDR, FLAG_CHECK_STATUS | FLAG_ANCHOR_END, part1);
+          Result res2 = parseAddress(StartType.START_ADDR, FLAG_CHECK_STATUS | FLAG_ANCHOR_END, part2);
+          if (res2.getStatus() > res1.getStatus()) {
+            res1 = res2;
+            part2 = part1;
+          }
+          res1.getData(data);
+          if (isValidAddress(part2)) {
+            data.strCross = part2;
           } else {
-            if (!apt.equals(data.strApt)) {
-              if (!Pattern.compile("\\b" + apt + "\\b").matcher(data.strApt).find()) {
-                data.strApt = append(apt,  "-",  data.strApt);
-              }
+            match = PLACE_APT_PTN.matcher(part2);
+            if (match.matches()) {
+              part2 = match.group(1).trim();
+              String tmp = match.group(2);
+              if (tmp ==  null) tmp = match.group(3);
+              aptList.add(0,tmp);
             }
+            
+            place = append(part2, " - ", place);
           }
         }
       }
-
-      // Process city code and something that we are ignoring
-      field = stripCity(field, data);
-      parseAddress(StartType.START_ADDR, FLAG_ANCHOR_END, field, data);
+      
+      // append apt and place
+      data.strPlace = append(data.strPlace, " - ", place);
+      
+      if (data.strApt.length() > 0) {
+        aptList.add(0,data.strApt);
+        data.strApt = "";
+      }
+      
+      if (aptList.size() == 1) {
+        data.strApt = aptList.get(0);
+      } else if (aptList.size() > 1) {
+        Set<String> aptSet = new HashSet<String>();
+        for (String apt : aptList) {
+          if (aptSet.add(apt)) data.strApt = append(data.strApt, "-", apt);
+        }
+      }
     }
     
     private String stripCity(String field, Data data) {
@@ -218,32 +282,40 @@ public class ALShelbyCountyParser extends FieldProgramParser {
       if (field.endsWith("/")) {
         field = field.substring(0,field.length()-1).trim();
       }
-      super.parse(field, data);
+      data.strCross = append(data.strCross, "/", field);
+    }
+  }
+  
+  private static final Pattern EXTRA_PHONE_GPS_PTN = Pattern.compile("[A-Z &]+(\\(\\d{3}\\) \\d{3}-\\d{4}) ([-+]\\d+\\.\\d{6} [-+]\\d+\\.\\d{6})X?[- ]*");
+  private class MyExtraField extends Field {
+    @Override
+    public void parse(String field, Data data) {
+      if (!field.startsWith("-")) {
+        data.strName = field;
+      } else {
+        field = field.substring(1).trim();
+        Matcher match = EXTRA_PHONE_GPS_PTN.matcher(field);
+        if (match.lookingAt()) {
+          data.strPhone = match.group(1);
+          setGPSLoc(match.group(2), data);
+          field = field.substring(match.end());
+        }
+        data.strSupp = field;
+      }
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "PHONE GPS INFO";
     }
   }
   
   @Override
-  public Field getField(String name) {
-    if (name.equals("TYPE")) return new SkipField("Cad.*", true);
-    if (name.equals("DATETIME")) return new MyDateTimeField();
-    if (name.equals("SRC_UNIT")) return new SourceUnitField();
-    if (name.equals("ADDR")) return new MyAddressField();
-    if (name.equals("X"))  return new MyCrossField();
-    return super.getField(name);
-  }
-  
-  @Override
   public String adjustMapAddress(String sAddress) {
-    sAddress = sAddress.replace("EGG AND BUTTER", "EGG_AND_BUTTER");
     sAddress = CLFS_PTN.matcher(sAddress).replaceAll("CLIFFS");
     return super.adjustMapAddress(sAddress);
   }
   private static final Pattern CLFS_PTN = Pattern.compile("\\bCLFS\\b", Pattern.CASE_INSENSITIVE);
-
-  @Override
-  public String postAdjustMapAddress(String sAddress) {
-    return sAddress.replace("EGG_AND_BUTTER", "EGG AND BUTTER");
-  }
   
   @Override
   public String adjustMapCity(String city) {
