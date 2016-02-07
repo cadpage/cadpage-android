576a3b0ea9205b75d6c40f85c745d9679c45da4c
Ken Corbin <ken@cadpage.org>
2016-02-07 09:50:15 -0800

    Fixed parsing problem with Henry County, VA

--- cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/VA/VAHenryCountyParser.java
@@ -24,8 +24,6 @@ public class VAHenryCountyParser extends DispatchSouthernParser {
   
   private static final Pattern UNIT_PRI_PTN = Pattern.compile(" +([A-Z0-9]+)-\\((\\d)\\) +");
   private static final Pattern LEAD_PRIORITY_PTN = Pattern.compile("^(0?\\d)\\b");
-  private static final Pattern TRAIL_PRIORITY_PTN = Pattern.compile(" +(0?\\d)$");
-  private static final Pattern INLINE_PRIORITY_PTN = Pattern.compile(" +(0?\\d) +");
   private static final Pattern APT_MAP_PTN = Pattern.compile("(?:(.*) )?([NSEW]{1,2} SECTOR?)");
   private static final Pattern APT_MAP_PTN2 = Pattern.compile("SECTOR?");
   private static final Pattern ADDR_DIR_PTN = Pattern.compile("(.*) ([NSEW]{1,2})");
@@ -44,25 +42,25 @@ public class VAHenryCountyParser extends DispatchSouthernParser {
     // Dispatch - Google spat
     data.strAddress = data.strAddress.replace("WILLIAM F STONE PARK ", "WILLIAM F STONE ");
     
-    // See if we can find a priority anywhere
+    // Fix DEER TRL RD
+    data.strAddress = data.strAddress.replace("DEER TRL & RD", "DEER TRL RD");
+    
+    // The apparently have eliminated all blanks from call descriptions recently
+    // so if we did not find a call match, split call description at first blank
+    String code = CALL_LIST.getCode(data.strCall);
+    if (code == null || code.length() != data.strCall.length()) {
+      String info = append(data.strCall, " ", data.strSupp);
+      int pt = info.indexOf(' ');
+      if (pt >= 0) {
+        data.strSupp = info.substring(pt+1).trim();
+        data.strCall = info.substring(0,pt);
+      }
+    }
+    
+    // See if we can find a priority in front of what is left
     if ((match = LEAD_PRIORITY_PTN.matcher(data.strSupp)).find()) {
       data.strPriority = match.group(1);
       data.strSupp = data.strSupp.substring(match.end()).trim();
-    }
-    
-    else if ((match = TRAIL_PRIORITY_PTN.matcher(data.strCall)).find()) {
-      data.strCall = data.strCall.substring(0,match.start()).trim();
-      data.strPriority = match.group(1);
-    }
-    
-    else if (CALL_LIST.getCode(data.strCall) == null) {
-      String info = append(data.strCall, " ", data.strSupp);
-      match = INLINE_PRIORITY_PTN.matcher(info);
-      if (match.find()) {
-        data.strCall = info.substring(0,match.start());
-        data.strPriority = match.group(1);
-        data.strSupp = info.substring(match.end());
-      }
     }
     
     match = APT_MAP_PTN.matcher(data.strApt);
@@ -121,64 +119,136 @@ public class VAHenryCountyParser extends DispatchSouthernParser {
   
   private static final CodeSet CALL_LIST = new CodeSet(
       "911 - 911 OPEN LINE - HANG UP",
+      "911---911-OPEN-LINE---HANG-UP",
       "ABDOMINAL / BACK PAIN",
+      "ABDOMINAL-/-BACK-PAIN",
       "ADMIN MAINTENANCE",
+      "ADMIN-MAINTENANCE",
       "ALARM - RESIDENCE",
+      "ALARM---RESIDENCE",
       "ALLERGIC REACTION",
+      "ALLERGIC-REACTION",
       "ANIMAL / SNAKE BITE",
+      "ANIMAL-/-SNAKE-BITE",
       "ASSAULT AGGRAVATED",
+      "ASSAULT-AGGRAVATED",
+      "ASSAULT-GENERAL",
       "ASSAULT WITH INJURY",
+      "ASSAULT-WITH-INJURY",
+      "ASSIST-MOTORIST",
+      "ASSIST-STATE-POLICE",
       "BLEEDING (NON-TRAUMATIC)",
+      "BLEEDING-(NON-TRAUMATIC)",
       "CALL BY PHONE",
+      "CALL-BY-PHONE",
       "CARDIAC (WITH PREVIOUS HISTORY)",
+      "CARDIAC-(WITH-PREVIOUS-HISTORY)",
       "CHEST PAINS",
+      "CHEST-PAINS",
       "CHOKING",
+      "CODE-BLUE-(CARDIAC-ARREST)",
+      "CODE-GRAY-(SUBJECT-DECEASED)",
+      "DELIVER-MESSAGE",
       "DIABETIC ILLNESS",
+      "DIABETIC-ILLNESS",
       "DIFFICULTY BREATHING",
+      "DIFFICULTY-BREATHING",
+      "DISTURBANCE",
       "DISTURBANCE",
       "DIZZINESS, WEAKNESS",
+      "DIZZINESS,-WEAKNESS",
+      "DOG CASE",
       "DOMESTIC - ASSAULT",
+      "DOMESTIC---ASSAULT",
+      "EMBEZZLEMENT",
+      "ENVIRONMENTAL-EMERGENCIES",
+      "EYE-PROBLEM-/-INJURY",
       "F-BRUSH FIRE",
+      "F-BRUSH-FIRE",
       "F-CHIMNEY FIRE",
+      "F-CHIMNEY-FIRE",
       "F-CONTROLLED BURN",
+      "F-CONTROLLED-BURN",
       "F-DUMPSTER FIRE",
+      "F-DUMPSTER-FIRE",
       "F-EXPLOSION W-FIRE",
+      "F-EXPLOSION-W-FIRE",
       "F-FIRE ALARM",
-      "F-HELICOPTER LANDING ZONE",
-      "F-OTHER FIRE - EXPLAIN",
-      "F-POWER LINE DOWN",
+      "F-FIRE-ALARM",
       "F-GAS SMELL-FUMES",
+      "F-GAS-SMELL-FUMES",
+      "F-HELICOPTER LANDING ZONE",
+      "F-HELICOPTER-LANDING-ZONE",
+      "F-OTHER FIRE - EXPLAIN",
+      "F-OTHER-FIRE---EXPLAIN",
+      "F-POWER LINE DOWN",
+      "F-POWER-LINE-DOWN",
       "F-SMOKE INVESTIGATION",
+      "F-SMOKE-INVESTIGATION",
       "F-STRUCTURE FIRE",
+      "F-STRUCTURE-FIRE",
       "F-UNAUTHORIZED BURNING",
+      "F-UNAUTHORIZED-BURNING",
       "F-VEHICLE FIRE",
-      "FALL",
+      "F-VEHICLE-FIRE",
       "FALL - FRACTURE",
+      "FALL",
+      "FALL---FRACTURE",
       "FAR-ARCING-SHORTED ELEC EQPT",
+      "FAR-ARCING-SHORTED-ELEC-EQPT",
+      "HEAD-INJURY-/-NEUROLOGICAL",
       "HEADACHE",
       "HIGH BLOOD PRESSURE",
+      "HIGH-BLOOD-PRESSURE",
       "HIT & RUN",
+      "HIT-&-RUN",
       "LOG FOR RECORD",
+      "LOG-FOR-RECORD",
       "MEDICAL ALARM",
+      "MEDICAL-ALARM",
       "MOTOR VEHICLE CRASH",
+      "MOTOR-VEHICLE-CRASH",
       "MOTOR VEHICL CRASH / PEDESTRIAN",
+      "MOTOR-VEHICL-CRASH-/-PEDESTRIAN",
       "MOTOR VEHICLE CRASH W/INJURY",
+      "MOTOR-VEHICLE-CRASH-W/INJURY",
       "MOTOR VEHICLE CRASH W/NO INJURY",
+      "MOTOR-VEHICLE-CRASH-W/NO-INJURY",
       "OB-GYN (PREGNANCY - MISCARRIAGE)",
+      "OB-GYN-(PREGNANCY---MISCARRIAGE)",
       "OVERDOSE",
+      "OVERDOSE-/-POISONING",
+      "PUBLIC-WORKS-GENERAL",
       "PSYCHIATRIC PATIENT MENTAL",
+      "PSYCHIATRIC-PATIENT-MENTAL",
+      "PSYCHIATRIC-PATIENT-/-MENTAL",
+      "REFUSING-TO-LEAVE",
       "SEIZURES",
       "SERVING WARRANT",
+      "SERVING-WARRANT",
       "SICK",
       "SICK / UNKNOWN",
+      "SICK-/-UNKNOWN",
       "SICK FLU LIKE SYMPTOMS",
+      "SICK-FLU-LIKE-SYMPTOMS",
+      "STABBING-/-GUNSHOT-VICTIM",
+      "STANDBY",
       "STROKE",
       "SUICIDE SUICIDE ATTEMPT",
+      "SUICIDE-SUICIDE-ATTEMPT",
+      "SUICIDE-/-SUICIDE-ATTEMPT",
       "SUSPICIOUS CIRCUMSTANCES",
+      "SUSPICIOUS-CIRCUMSTANCES",
       "SUSPICIOUS PERSON",
+      "SUSPICIOUS-PERSON",
+      "SUSPICIOUS-VEHICLE",
       "TRANSPORT (HOSPITAL-DR OFFICE ETC.)",
+      "TRANSPORT-(HOSPITAL-DR-OFFICE-ETC.)",
       "TRAUMA",
       "UNCONSCIOUS / UNRESPONSIVE SYNCOPE",
-      "WELLBEING CHECK"
+      "UNCONSCIOUS-/-UNRESPONSIVE-SYNCOPE",
+      "WEATHER-ALERTS",
+      "WELLBEING CHECK",
+      "WELLBEING-CHECK"
   );
 }
--- cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchSouthernParser.java
+++ cadpage/src/net/anei/cadpage/parsers/dispatch/DispatchSouthernParser.java
@@ -61,7 +61,7 @@ public class DispatchSouthernParser extends FieldProgramParser {
   private static final Pattern NAKED_TIME_PTN = Pattern.compile("([ ,;]) *(\\d\\d:\\d\\d:\\d\\d)(?:\\1|$)");
   private static final Pattern OCA_TRAIL_PTN = Pattern.compile("\\bOCA: *([-0-9]+)$");
   private static final Pattern ID_PTN = Pattern.compile("\\b\\d{2,4}-?(?:\\d\\d-)?\\d{4,8}$");
-  private static final Pattern CALL_PTN = Pattern.compile("^([A-Z0-9\\- /]+)\\b[ \\.,-]*");
+  private static final Pattern CALL_PTN = Pattern.compile("^([A-Z0-9\\- /()]+)\\b[ \\.,-]*");
   private static final Pattern PHONE_PTN = Pattern.compile("\\b\\d{10}\\b");
   private static final Pattern EXTRA_CROSS_PTN = Pattern.compile("(?:AND +|[/&] *)(.*)", Pattern.CASE_INSENSITIVE);
   private static final Pattern CALL_BRK_PTN = Pattern.compile(" +/+ *");
