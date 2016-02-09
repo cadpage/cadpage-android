96f473e8471ce5d996023e08fed5d8a57c5f3c84
Ken Corbin <ken@cadpage.org>
2016-02-08 21:02:10 -0800

    Fixed parsing problem with Auglaize County, OH

--- cadpage/src/net/anei/cadpage/parsers/OH/OHAuglaizeCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OH/OHAuglaizeCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.OH;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +10,8 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class OHAuglaizeCountyParser extends FieldProgramParser {
 
   public OHAuglaizeCountyParser() {
-    super("AUGLAIZE COUNTY", "OH", "SIG:CODE! NAME:NAME! LOC:ADDR! CROSS:X! DESC:INFO! CFS:ID! DATE:DATE! TIME:TIME! CITY:CITY! SECTOR:MAP! DISTRICT:MAP! SIG_DESC:CALL!");
+    super(CITY_CODES, "AUGLAIZE COUNTY", "OH", 
+          "SIG:CODE! NAME:NAME! LOC:ADDR/S! CROSS:X! DESC:INFO! CFS:ID! DATE:DATE! TIME:TIME! CITY:CITY! SECTOR:MAP! DISTRICT:MAP! SIG_DESC:CALL!");
   }
 
   @Override
@@ -35,16 +37,29 @@ public class OHAuglaizeCountyParser extends FieldProgramParser {
     if (name.equals("ID")) return new IdField("\\d{2}-\\d{6}", true);
     if (name.equals("DATE")) return new DateField("\\d{1,2}/\\d{1,2}/\\d{4}", true);
     if (name.equals("TIME")) return new TimeField("\\d{2}:\\d{2}:\\d{2}", true);
+    if (name.equals("CITY")) return new MyCityField();
     if (name.equals("MAP")) return new MapField("\\d+[A-Z]+");
     if (name.equals("CALL")) return new MyCallField();
     return super.getField(name);
   }
 
+  private static final Pattern ADDR_UNIT_PTN = Pattern.compile("(.*) (?:UNIT|APT) +(\\S+)");
   private class MyAddressField extends AddressField {
     @Override
     public void parse(String field, Data data) {
+      
+      // Strip out apt
+      String apt = "";
+      Matcher match = ADDR_UNIT_PTN.matcher(field);
+      if (match.matches()) {
+        field = match.group(1).trim();
+        apt = match.group(2);
+      }
+      
       // road@road to road & road
-      parseAddress(field.replace("@", " & "), data);
+      super.parse(field.replace("@", " & "), data);
+      
+      data.strApt = append(data.strApt, "-", apt);
     }
   }
 
@@ -55,9 +70,31 @@ public class OHAuglaizeCountyParser extends FieldProgramParser {
 
     @Override
     public void parse(String field, Data data) {
+      
+      // Strip out city codes
+      String saveCity = data.strCity;
+      StringBuilder sb = new StringBuilder();
+      for (String part : field.split("&")) {
+        part = part.trim();
+        parseAddress(StartType.START_ADDR, FLAG_ONLY_CROSS | FLAG_ONLY_CITY | FLAG_ANCHOR_END, part, data);
+        if (data.strCross.length() > 0) {
+          if (sb.length() > 0) sb.append(" & ");
+          sb.append(data.strCross);
+        }
+      }
+      data.strCross = sb.toString();
+      if (saveCity.length() > 0) data.strCity = saveCity;
+      
       // consolidate entries like "US 33 & US 33"
-      Matcher mat = REDUNDANT_X.matcher(field);
-      if (mat.matches()) field = mat.group(1);
+      Matcher mat = REDUNDANT_X.matcher(data.strCross);
+      if (mat.matches()) data.strCross = mat.group(1);
+    }
+  }
+  
+  private class MyCityField extends CityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.length() == 0) return;
       super.parse(field, data);
     }
   }
@@ -72,4 +109,23 @@ public class OHAuglaizeCountyParser extends FieldProgramParser {
       }
     }
   }
+  
+  private static final Properties CITY_CODES = buildCodeTable(new String[]{
+      "CV", "CRIDERSVILLE",
+      "FR", "WAPAKONETA",
+      "MN", "MINSTER",
+      "NH", "NEW HAMPSHIRE",
+      "SJ", "ST JOHNS",
+      "SM", "ST MARYS",
+      "UN", "UNIOPOLIS",
+      "WA", "WAPAKONETA",
+      "WF", "WAYNESFIELD",
+      
+      "ALLEN CO",    "ALLEN COUNTY",
+      "HARDIN CO",   "HARDIN COUNTY",
+      "LOGAN CO",    "LOGAN COUNTY",
+      "MERCER CO",   "MERCER COUNTY",
+      "SHELBY CO",   "SHELBY COUNTY",
+      "VAN WERT CO", "VAN WERT COUNTY"
+  });
 }
