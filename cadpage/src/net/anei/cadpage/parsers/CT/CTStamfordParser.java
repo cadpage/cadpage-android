f6d97cdeb2ec8daa2b831d421adae4f720491f75
Ken Corbin <ken@cadpage.org>
2016-02-14 12:13:45 -0800

    Fixed parsing problem with Staamford, CT

--- cadpage/src/net/anei/cadpage/parsers/CT/CTStamfordParser.java
+++ cadpage/src/net/anei/cadpage/parsers/CT/CTStamfordParser.java
@@ -1,98 +1,46 @@
 package net.anei.cadpage.parsers.CT;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import net.anei.cadpage.parsers.MsgInfo.Data;
-import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
+import net.anei.cadpage.parsers.MsgParser;
 
 /**
  * Stamford, CT
  */
-public class CTStamfordParser extends DispatchA3Parser {
+public class CTStamfordParser extends MsgParser {
   
-  private static final Pattern DELIM = Pattern.compile("\\*?\\n");
-  
-  private static final Pattern PREFIX_PTN = Pattern.compile("(?:(?:911Fire:|STAMFORD 911:)?\\*|STAMFORD 911:Stamford|911Fire:Stamford)\\n");
   public CTStamfordParser() {
-    super("STAMFORD", "CT",
-          "ADDR EMPTY EMPTY EMPTY X X EMPTY CMT CALL! EMPTY NAME EMPTY EMPTY EMPTY EMPTY INFO+");
+    super("STAMFORD", "CT");
+    setFieldList("ADDR CITY APT X NAME CALL");
   }
 
   @Override
   public String getFilter() {
-    return "911fire@ci.Stamford.ct.us,911Fire@ci.stamford.ct.us";
+    return "SFD.NoReply@StamfordCT.Gov";
   }
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
-    do {
-      if (subject.equals("From Stamford 911") || subject.equals("From- Stamford 911")) break;
-      
-      Matcher match = PREFIX_PTN.matcher(body);
-      if (match.lookingAt()) {
-        body = body.substring(match.end()).trim();
-        break;
-      }
-      
-      return false;
-    } while (false);
-
-    body = stripFieldEnd(body, "*");
-    return parseFields(DELIM.split(body), data) && data.strCall.length() > 0;
-  }
-  
-  @Override
-  public Field getField(String name) {
-    if (name.equals("CMT")) return new MyCommentField();
-    if (name.equals("INFO")) return new MyInfoField();
-    return super.getField(name);
-  }
-  
-  private static final Pattern COMMENT_PTN = Pattern.compile("(?:Landmark|Geo|Place|Special) Comment::? *(.*)");
-  private class MyCommentField extends BaseInfo1Field {
     
-    @Override
-    public boolean canFail() {
-      return true;
-    }
+    if (!subject.equals("Stamford 911")) return false;
     
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (field.length() == 0) return true;
-      Matcher match = COMMENT_PTN.matcher(field);
-      if (!match.matches()) return false;
-      super.parse(match.group(1), data);
-      return true;
-    }
+    FParser fp = new FParser(body);
+    parseAddress(fp.get(30), data);
+    if (!fp.check(",")) return false;
+    data.strCity = fp.get(8);
+    if (!fp.check(",CT.")) return false;
+    data.strApt = fp.get(8);
     
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-  }
-  
-  private static final Pattern INFO_PTN = Pattern.compile("\\d\\d?/\\d\\d?/\\d{4} \\d\\d:\\d\\d:\\d\\d .*");
-  private class MyInfoField extends BaseInfoField {
+    if (fp.checkAhead(56, "F ")) {
+      data.strCross = fp.get(22); 
+      data.strName = fp.get(34);
+      data.strCall = fp.get();
+    } 
     
-    @Override
-    public boolean canFail() {
-      return true;
+    else {
+      data.strCross = fp.get(20);
+      data.strName = fp.get(52);
+      data.strCall = fp.get(30);
     }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      if (!INFO_PTN.matcher(field).matches()) return false;
-      parse(field, data);
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      String saveCross = data.strCross;
-      data.strCross = "";
-      super.parse(field, data);
-      if (data.strCross.length() == 0) data.strCross = saveCross;
-    }
+    return true;
   }
 }
