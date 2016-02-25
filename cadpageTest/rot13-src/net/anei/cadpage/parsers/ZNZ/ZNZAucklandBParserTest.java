f889b0f98a4eb572b0801beab4863edaad265f4c
Jamie Harper <snailtk@gmail.com>
2016-02-25 14:49:52 -0800

    skellitons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/ZNZ/ZNZAucklandBParserTest.java
@@ -0,0 +1,62 @@
+cnpxntr arg.narv.pnqcntr.cnefref.MAM;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pbnfgthneq Abegu Fuber
+Ybpngvba: Nhpxynaq, Nhpxynaq, Arj Mrnynaq
+Fraqre: "perjznfgre@ptaf.pb.am" <perjznfgre@ptaf.pb.am>
+Fraqre: Fvzba Nyyneq <fvzba@nyyneq.arg.am>
+Fraqre: Ora Zvyyre <Ora.Zvyyre@pbnfgthneq.bet.am>
+
+Abegu Fuber: GENPGBE QEVIRE ERDHVERQ
+Abegu Fuber: OBNG PNYYBHG
+OBNG PNYYBHG;18 Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+Abegu Fuber: FXVCCRE PNYY PAE
+Abegu Fuber:GENPGBE CNTR. CYF PNYY GUR OBNG
+Abegu Fuber:OBNG BA CNTRE
+Abegu Fuber: FCBGGRE ERDHVERQ FBHGUREA RAQ BS OEBJAF ONL. CYRNFR PNYY BCF VS NINVYNOYR.
+NPV Nyy Sbe Ebhgvar pnyybhgf. YVBA: OBNG PNYYBHG
+Abegu Fuber:ERZVAQRE, LBH NER QHGL OBNG GBQNL :-)
+GENPGBE QEVIRE EDQ;18 Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+HETRAG OBNG PNYYBHG;18 Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+grfg;18 Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+oynu oynu;18 Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+OBNG PNYYBHG;Furq
+OBNG PNYYBHG;19 Znayl Rfcynanqr
+OBNG PNYYBHG;-36.713308, 174.749367
+OBNG PNYYBHG;10 Ornpusebag Ya  Oebjaf Onl
+OBNG PNYYBHG;12 Ornpusebag Ya  Oebjaf Onl
+OBNG PNYYBHG;12 Ornpusebag Ynar, Oebjaf Onl
+OBNG PNYYBHG;10-12 Ornpusebag Ynar, Oebjaf Onl
+OBNG PNYYBHG;10-12 Ornpusebag Ya
+OBNG PNYYBHG;Oebjaf Onl Znevar Prager
+OBNG PNYYBHG;1-5 Onl Ivrj Ebnq
+OBNG PNYYBHG;20 Onl Ivrj Ebnq
+OBNG PNYYBHG;20 Znayl Rfcynanqr
+OBNG PNYYBHG;18a Ornpusebag Ynar Oebjaf Onl Nhpxynaq
+04/12 - SLV Xnjnh naq Uvovfphf 1 ner va Qel Qbpx HSA.
+<04/12/15 1440uef> Genpgbe Qevire Erdhverq, Cubar Obng. PAE
+OBNG PNYYBHG - GVEV CNFFNTR, ZRPUNAVPNY ONPX GB TUZ, 36sg PEHVFRE. ORA @PAE
+
+*/
+
+choyvp pynff MAMNhpxynaqOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp MAMNhpxynaqOCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "NHPXYNAQ", "");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj MAMNhpxynaqOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
