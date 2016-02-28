e6426bd80ecd04a43f2764ae09246fab9d752dd6
Ken Corbin <ken@cadpage.org>
2016-02-28 11:18:41 -0800

    Fixed parsing problem with Chatham County, GA

--- cadpage/src/net/anei/cadpage/parsers/GA/GAChathamCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/GA/GAChathamCountyParser.java
@@ -4,6 +4,7 @@ import java.util.regex.*;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
 import net.anei.cadpage.parsers.MsgInfo.Data;
+import net.anei.cadpage.parsers.MsgInfo.MsgType;
 
 /**
  * Chatham County, GA
@@ -17,18 +18,21 @@ public class GAChathamCountyParser extends FieldProgramParser {
   }
 
   private static final Pattern CRN_PATTERN
-    = Pattern.compile("^(?:(?:YOUR +)?CRN\\s*\\&\\s*TIMES(?: +ARE)?\\:\\s*CRN\\:|"
-+                         "COMPLETED +CALL\\s*\\/\\s*REFUSAL TIMES\\:\\s*CRN\\:|"
-+                         "COMPLETE\\s*\\/\\s*REFUSAL\\:\\s*CRN\\:)\\s*(\\d{4}(?:\\d|[A-Z])\\d{4}).*");
-  private static final Pattern APT_PATTERN
-    = Pattern.compile("(.*?)APT\\#?\\:?(.*)");
+    = Pattern.compile("^((?:YOUR +)?CRN\\s*\\&\\s*TIMES(?: +ARE)?|"
++                         "COMPLETED +CALL\\s*\\/\\s*REFUSAL TIMES|"
++                         "COMPLETE\\s*\\/\\s*REFUSAL)\\:\\s*CRN\\:\\s*(\\d{4}(?:\\d|[A-Z])\\d{4}) +(.*)");
+  private static final Pattern TIMES_BRK_PTN = Pattern.compile(" +(?=(?:RECEIVED|DISPATCHED|ENROUTE|AT SCENE|DEPART|AT DEST|MILEAGE):)");
+  private static final Pattern APT_PATTERN = Pattern.compile("(.*?)APT\\#?\\:?(.*)");
+  
   @Override
   protected boolean parseMsg(String body, Data data) {
     Matcher m = CRN_PATTERN.matcher(body);
     if (m.matches()) {
-      data.strCall = "RUN REPORT";
-      data.strPlace = body;
-      data.strCallId = m.group(1);
+      setFieldList("CALL ID INFO");
+      data.msgType = MsgType.RUN_REPORT;
+      data.strCall = m.group(1);
+      data.strCallId = m.group(2);
+      data.strSupp = TIMES_BRK_PTN.matcher(m.group(3).trim()).replaceAll("\n");
       return true;
     }
     m = APT_PATTERN.matcher(body);
@@ -37,7 +41,22 @@ public class GAChathamCountyParser extends FieldProgramParser {
     else
       body = body.replace("PRI:", " APT: PRI:");
     body = body.replace("CRN:", " CRN:");
+    body = body.replace("COMPLAINT:", " COMPLAINT:");
     body = body.replace("C/O:", "COMPLAINT:");
     return super.parseMsg(body, data);
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("INFO")) return new MyInfoField();
+    return super.getField(name);
+  }
+  
+  private class MyInfoField extends InfoField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldStart(field, "Comment:");
+      super.parse(field, data);
+    }
   }
 }
