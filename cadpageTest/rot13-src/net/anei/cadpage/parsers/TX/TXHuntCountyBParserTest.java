58fe25153d494dde7dc5c3215692201f83912d60
Ken Corbin <ken@cadpage.org>
2016-03-25 09:10:02 -0700

    Checking in Hunt County, TX (B)

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/TX/TXHuntCountyBParserTest.java
@@ -0,0 +1,89 @@
+cnpxntr arg.narv.pnqcntr.cnefref.GK;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/* 
+Uhag Pbhagl, GK
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pbzzrepr Sver & Erfphr
+Ybpngvba: Pbzzrepr, GK, Havgrq Fgngrf
+Fraqre: wbfu.pngb@tznvy.pbz
+
+{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: GRFG CNTR\a\aVapvqrag Ybpngvba: :: 1103 Flpnzber Fg.\a\aPvgl ::\a\aGvzrfgnzc :: 3/24/2016 16:58:31\a\aPvgl: :: Pbzzrepr,Grfg,
+{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: GRFG CNTR\a\aVapvqrag Ybpngvba: :: 1103 Flpnzber Fg.\a\a\aPvgl ::\a\aGvzrfgnzc :: 3/24/2016 15:58:13\a\aPvgl: :: Pbzzrepr,,
+{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: Zrqvpny Erfcbafr\a\aVapvqrag Ybpngvba: :: 1103 Flpnzber Fg\a\aPvgl ::\a\aGvzrfgnzc :: 3/22/2016 9:31:12\a\aPvgl: :: Pbzzrepr,,
+{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: Bgure Abg Yvfgrq\a\aVapvqrag Ybpngvba: :: 1103 Flpnzber Fg\a\aGRFG\a\aPvgl ::\a\aGvzrfgnzc :: 3/22/2016 9:20:51\a\aPvgl: :: Pbzzrepr,,
+
+*/
+choyvp pynff GKUhagPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp GKUhagPbhaglOCnefreGrfg() {
+    frgCnefre(arj GKUhagPbhaglOCnefre(), "UHAG PBHAGL", "GK");
+  }
+
+  @Grfg
+  choyvp ibvq grfgPbzzreprSverErfphr() {
+
+    qbGrfg("G1",
+        "{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: GRFG CNTR\a\a" +
+        "Vapvqrag Ybpngvba: :: 1103 Flpnzber Fg.\a\a" +
+        "Pvgl ::\a\a" +
+        "Gvzrfgnzc :: 3/24/2016 16:58:31\a\a" +
+        "Pvgl: :: Pbzzrepr,Grfg,",
+
+        "PNYY:GRFG CNTR",
+        "NQQE:1103 Flpnzber Fg",
+        "PVGL:Pbzzrepr",
+        "QNGR:3/24/2016",
+        "GVZR:16:58:31",
+        "VASB:Grfg");
+
+    qbGrfg("G2",
+        "{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: GRFG CNTR\a\a" +
+        "Vapvqrag Ybpngvba: :: 1103 Flpnzber Fg.\a\a\a" +
+        "Pvgl ::\a\a" +
+        "Gvzrfgnzc :: 3/24/2016 15:58:13\a\a" +
+        "Pvgl: :: Pbzzrepr,,",
+
+        "PNYY:GRFG CNTR",
+        "NQQE:1103 Flpnzber Fg",
+        "PVGL:Pbzzrepr",
+        "QNGR:3/24/2016",
+        "GVZR:15:58:13");
+
+    qbGrfg("G3",
+        "{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: Zrqvpny Erfcbafr\a\a" +
+        "Vapvqrag Ybpngvba: :: 1103 Flpnzber Fg\a\a" +
+        "Pvgl ::\a\a" +
+        "Gvzrfgnzc :: 3/22/2016 9:31:12\a\a" +
+        "Pvgl: :: Pbzzrepr,,",
+
+        "PNYY:Zrqvpny Erfcbafr",
+        "NQQE:1103 Flpnzber Fg",
+        "PVGL:Pbzzrepr",
+        "QNGR:3/22/2016",
+        "GVZR:9:31:12");
+
+    qbGrfg("G4",
+        "{Pbzzrepr Sver & Erfphr Nyreg} Vapvqrag Glcr: :: Bgure Abg Yvfgrq\a\a" +
+        "Vapvqrag Ybpngvba: :: 1103 Flpnzber Fg\a\a" +
+        "GRFG\a\a" +
+        "Pvgl ::\a\a" +
+        "Gvzrfgnzc :: 3/22/2016 9:20:51\a\a" +
+        "Pvgl: :: Pbzzrepr,,",
+
+        "PNYY:Bgure Abg Yvfgrq",
+        "NQQE:1103 Flpnzber Fg",
+        "PVGL:Pbzzrepr",
+        "QNGR:3/22/2016",
+        "GVZR:9:20:51");
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj GKUhagPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
