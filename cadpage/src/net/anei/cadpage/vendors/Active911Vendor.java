2f751711fc9ada506cf125ca0043e6464c056ddb
Ken Corbin <ken@cadpage.org>
2016-04-22 17:40:38 -0700

    Checking in ALJeffersonCountyH

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -191,6 +191,7 @@ class Active911Vendor extends Vendor {
       "US/AL/Colbert",                    "ALColbertCounty",
       "US/AL/DallasCounty",               "ALDallasCounty",
       "US/AL/Dothan",                     "ALDothan",
+      "US/AL/Gardendale",                 "ALJeffersonCountyH",
       "US/AL/Hoover",                     "ALHoover",
       "US/AL/JeffersonCounty",            "ALJeffersonCountyC,ALJeffersonCountyE",
       "US/AL/LeeCounty",                  "ALLeeCounty",
