267b6062d486ed86a162de9526d7b42660510885
Ken Corbin <ken@cadpage.org>
2016-05-06 18:54:29 -0700

    Updated msg docc

--- cadpage/src/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/WV/WVLincolnCountyParser.java
@@ -2,6 +2,7 @@ package net.anei.cadpage.parsers.WV;
 
 import java.util.regex.Pattern;
 
+import net.anei.cadpage.parsers.CodeSet;
 import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
 
@@ -10,16 +11,29 @@ import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
 public class WVLincolnCountyParser extends DispatchB2Parser {
   
   public WVLincolnCountyParser() {
-    super("LINCOLN 911:",CITY_LIST, "LINCOLN COUNTY", "WV");
+    super("LINCOLN 911:", CITY_LIST, "LINCOLN COUNTY", "WV");
     setupMultiWordStreets(
+        "A PARK",
         "BIG LAUREL",
+        "DR STEELE FARM",
+        "END OF MONDAY",
+        "HILL VIEW",
+        "INDIAN VALLEY",
         "LINCOLN PANTHER",
         "LOWER MUD RIVER",
+        "MARY JANE",
+        "MUD RIVER",
         "ONE MILE",
         "PIN OAK",
+        "SILVER MINE",
         "SLASH BR BELLOMY",
+        "STONES TRAILER",
+        "SUGAR TREE",
         "UPPER MUD RIVER",
-        "WEST PARK");
+        "WEST PARK"
+    );
+    setupCallList(CALL_LIST);
+    setupDoctorNames("STEELE");
   }
   
   @Override
@@ -44,6 +58,41 @@ public class WVLincolnCountyParser extends DispatchB2Parser {
     return true;
   }
   private static final Pattern TO_PTN = Pattern.compile(" +TO +", Pattern.CASE_INSENSITIVE);
+  
+  private static CodeSet CALL_LIST = new CodeSet(
+      "AUTO ACCIDENT NO INJURIES",
+      "AUTO ACCIDENT UNKNOWN INJURIES",
+      "AUTO ACCIDENT W/ENTRAPMENT",
+      "AUTO ACCIDENT WITH INJURIES",
+      "BOX ALARM",
+      "BREATHING DIFFICULTY",
+      "BRUSH FIRE",
+      "CARDIAC ARREST",
+      "CHEST PAINS",
+      "CODE 3 DIFFICULTY BREATHING",
+      "CODE3 UNCONSCIOUS/UNRESPONSIVE",
+      "FIRE INVESTIGATION",
+      "FLASH FLOOD WARNING",
+      "FLOOD WARNING",
+      "GAS LEAK",
+      "HOUSE FIRE",
+      "LANDING ZONE SECURED",
+      "LINE DOWN",
+      "LOCK OUT WITH ENTRAPMENT",
+      "OTHER FIRE CALL",
+      "OVERDOSE",
+      "PUBLIC ASSISTANCE NON EMERGENC",
+      "RELEASED FROM",
+      "SHOOTING",
+      "SICK UNKNOWN",
+      "SMOKE INVESTIGATION",
+      "STRUCTURE FIRE",
+      "TRAFFIC PROBLEM",
+      "TRAILER FIRE",
+      "TRAUMA",
+      "TREE DOWN",
+      "VEHICLE FIRE"
+  );
   
   private static final String[] CITY_LIST = new String[]{
     
@@ -70,6 +119,8 @@ public class WVLincolnCountyParser extends DispatchB2Parser {
     "HARTS",
     "LEET",
     "MIDKIFF",
+    "MYRA",
+    "PALERMO",
     "PLEASANT VIEW",
     "RANGER",
     "RECTOR",
@@ -80,6 +131,9 @@ public class WVLincolnCountyParser extends DispatchB2Parser {
     "WARREN",
     "WEWANTA",
     "YAWKEY",
+    
+    "CABELL COUNTY",
+    "CULLODEN",
     
     "PUTNAM COUNTY"
   };
