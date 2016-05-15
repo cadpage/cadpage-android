c9c28634f2574641764157dfeca9acd3cfd1065e
Jamie Harper <snailtk@gmail.com>
2016-05-15 15:32:18 -0700

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/CT/CTHartfordCountyWindsorLocksParserTest.java
@@ -0,0 +1,32 @@
+cnpxntr arg.narv.pnqcntr.cnefref.PG;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Unegsbeq Pbhagl, PGf
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Jvaqfbe Ybpxf SQ
+Ybpngvba: Jvaqfbe Ybpxf, PG, Havgrq Fgngrf
+Fraqre: jysqpbz@freire.njejroqrfvta.pbz
+
+{GRFG} Sver Nynez; 295 Ryyn Tenffb Gheacvxr; Jvaqfbe Ybpxf; R5,G1,P11
+*/
+
+choyvp pynff PGUnegsbeqPbhaglJvaqfbeYbpxfCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp PGUnegsbeqPbhaglJvaqfbeYbpxfCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "UNEGSBEQ PBHAGL", "PG");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj PGUnegsbeqPbhaglJvaqfbeYbpxfCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
