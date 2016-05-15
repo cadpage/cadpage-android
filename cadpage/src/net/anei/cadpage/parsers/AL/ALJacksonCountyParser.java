a6cb86f0485bd1b29a06c229b4d74f7e8b8fa02e
Jamie Harper <snailtk@gmail.com>
2016-05-14 19:38:11 -0700

    parsers

--- cadpage/src/net/anei/cadpage/parsers/AL/ALJacksonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/AL/ALJacksonCountyParser.java
@@ -24,6 +24,7 @@ public class ALJacksonCountyParser extends DispatchA37Parser {
   private static final String[] CITY_LIST = new String[]{
       
       "JACKSON COUNTY",
+      
   //Cities
 
       "BRIDGEPORT",
