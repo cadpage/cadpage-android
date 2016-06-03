5a6741231fced3096a749d874451f200bbd7e88e
Ken Corbin <ken@cadpage.org>
2016-06-03 14:24:41 -0700

    Checking in Pope County, MN

--- cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
+++ cadpage/src/net/anei/cadpage/vendors/Active911Vendor.java
@@ -659,6 +659,7 @@ class Active911Vendor extends Vendor {
       "US/MN/NormanCounty",               "Cadpage2",
       "US/MN/OtterTailCounty",            "MNOtterTailCounty",
       "US/MN/PenningtonCounty",           "MNPenningtonCounty",
+      "US/MN/PopeCounty",                 "MNPopeCounty",
       "US/MN/RamseyCounty",               "MNRamseyCounty",
       "US/MN/RenvilleCounty",             "MNRenvilleCounty",
       "US/MN/ScottCounty",                "MNScottCounty",
