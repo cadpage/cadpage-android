30866d12c172b38155af49c8998d81e6e156c599
Ken Corbin <ken@cadpage.org>
2016-06-02 22:07:02 -0700

    Checking n Itasaca County, MN and 7 others

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNKoochichingCountyParserTest.java
@@ -0,0 +1,14 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZA;
+
+/*
+Xbbpuvpuvat Pbhagl, ZA
+
+*/
+
+choyvp pynff ZAXbbpuvpuvatPbhaglCnefreGrfg rkgraqf ZAVgnfpnPbhaglCnefreGrfg {
+  
+  choyvp ZAXbbpuvpuvatPbhaglCnefreGrfg() {
+    frgCnefre(arj ZAXbbpuvpuvatPbhaglCnefre(), "XBBPUVPUVAT PBHAGL", "ZA");
+    frgRkcYbpPbqr("ZAXbbpuvpuvatPbhagl");
+  }
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MN/MNStLouisCountyParserTest.java
@@ -0,0 +1,14 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZA;
+
+/*
+Fg Ybhvf Pbhagl, ZA
+
+*/
+
+choyvp pynff ZAFgYbhvfPbhaglCnefreGrfg rkgraqf ZAVgnfpnPbhaglCnefreGrfg {
+  
+  choyvp ZAFgYbhvfPbhaglCnefreGrfg() {
+    frgCnefre(arj ZAFgYbhvfPbhaglCnefre(), "FG YBHVF PBHAGL", "ZA");
+    frgRkcYbpPbqr("ZAFgYbhvfPbhagl");
+  }
+}
