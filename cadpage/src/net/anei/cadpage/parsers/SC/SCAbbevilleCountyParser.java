11e796876d28a1f6e46228d01531b0867c0288bf
Ken Corbin <ken@cadpage.org>
2016-07-03 13:56:32 -0700

    Checking in Abbevile County, SC

---
+++ cadpage/src/net/anei/cadpage/parsers/SC/SCAbbevilleCountyParser.java
@@ -0,0 +1,35 @@
+package net.anei.cadpage.parsers.SC;
+import net.anei.cadpage.parsers.CodeSet;
+import net.anei.cadpage.parsers.dispatch.DispatchB2Parser;
+
+
+public class SCAbbevilleCountyParser extends DispatchB2Parser {
+ 
+  public SCAbbevilleCountyParser() {
+    super("stat12:", CITY_LIST, "ABBEVILLE COUNTY", "SC");
+    setupCallList(CALL_LIST);
+  }
+  
+  private static CodeSet CALL_LIST = new CodeSet(
+      "52C-CARDIAC ARREST",
+      "50PI-TRAFFIC ACCIDENT W/INJURY"
+  );
+  
+  private static final String[] CITY_LIST = new String[]{
+
+    // City
+    "ABBEVILLE",
+
+    // Towns
+    "CALHOUN FALLS",
+    "DONALDS",
+    "DUE WEST",
+    "HONEA PATH",
+    "LOWNDESVILLE",
+    "WARE SHOALS",
+
+    // Unincorporated communities
+    "ANTREVILLE",
+    "LAKE SECESSION"
+  };
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/SC/SCAbbevilleCountyParserTest.java
@@ -0,0 +1,54 @@
+cnpxntr arg.narv.pnqcntr.cnefref.FP;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Noorivyyr Pbhagl, FP
+
+Pbagnpg: Wrerzl Znaavat <pssqsverzna@tznvy.pbz>
+Fraqre: R911@noorivyyr911.pbz
+fgng12: RZFP >52P-PNEQVNP NEERFG 402 A OHEGBA QE KF: FNINAANU FG RKG PNYUBHA SNYYF QNYBEVF TENUNZ 8644188857 Znc: Tevqf:0,0 Pnq: 2016-0000038267
+fgng12: ZINCV >50CV-GENSSVP NPPVQRAG J/VAWHEL R FNINAANU FG KF: SEBZ PSCQ GBJNEQ NOO PNYUBHA SNYYF ERANR JVYFBA 7063527152 Pnq: 2016-0000039966
+
+ */
+
+choyvp pynff FPNoorivyyrPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp FPNoorivyyrPbhaglCnefreGrfg() {
+    frgCnefre(arj FPNoorivyyrPbhaglCnefre(), "NOORIVYYR PBHAGL", "FP");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgWrerzlZnaavat() {
+
+    qbGrfg("G1",
+        "fgng12: RZFP >52P-PNEQVNP NEERFG 402 A OHEGBA QE KF: FNINAANU FG RKG PNYUBHA SNYYF QNYBEVF TENUNZ 8644188857 Znc: Tevqf:0,0 Pnq: 2016-0000038267",
+        "PBQR:RZFP",
+        "PNYY:52P-PNEQVNP NEERFG",
+        "NQQE:402 A OHEGBA QE",
+        "K:FNINAANU FG RKG",
+        "PVGL:PNYUBHA SNYYF",
+        "ANZR:QNYBEVF TENUNZ",
+        "CUBAR:8644188857",
+        "VQ:2016-0000038267");
+
+    qbGrfg("G2",
+        "fgng12: ZINCV >50CV-GENSSVP NPPVQRAG J/VAWHEL R FNINAANU FG KF: SEBZ PSCQ GBJNEQ NOO PNYUBHA SNYYF ERANR JVYFBA 7063527152 Pnq: 2016-0000039966",
+        "PBQR:ZINCV",
+        "PNYY:50CV-GENSSVP NPPVQRAG J/VAWHEL",
+        "NQQE:R FNINAANU FG",
+        "ZNQQE:R FNINAANU FG & SEBZ PSCQ GBJNEQ NOO",
+        "K:SEBZ PSCQ GBJNEQ NOO",
+        "PVGL:PNYUBHA SNYYF",
+        "ANZR:ERANR JVYFBA",
+        "CUBAR:7063527152",
+        "VQ:2016-0000039966");
+   
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj FPNoorivyyrPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
