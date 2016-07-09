dcbbe295e80611e550236f89417bc68d79e94669
Ken Corbin <ken@cadpage.org>
2016-07-08 18:18:48 -0700

    Checking in Chittenden County, VT (C)

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/VT/VTChittendenCountyCParserTest.java
@@ -0,0 +1,13 @@
+cnpxntr arg.narv.pnqcntr.cnefref.IG;
+
+/*
+Puvggraqra Pbhagl, IG (P)
+*/
+
+choyvp pynff IGPuvggraqraPbhaglPCnefreGrfg rkgraqf IGYnzbvyyrPbhaglCnefreGrfg {
+  
+  choyvp IGPuvggraqraPbhaglPCnefreGrfg() {
+    frgCnefre(arj IGPuvggraqraPbhaglPCnefre(), "PUVGGRAQRA PBHAGL", "IG");
+    frgRkcYbpPbqr("IGPuvggraqraPbhaglP");
+  }
+}
