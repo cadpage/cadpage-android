2d8c111476308f219f484b8d1726adb4fd7a072f
Ken Corbin <ken@cadpage.org>
2016-07-17 12:43:23 -0700

    Fixed parsing problem with Josephine County, OR

--- cadpage/src/net/anei/cadpage/parsers/OR/ORJosephineCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/OR/ORJosephineCountyParser.java
@@ -2,7 +2,6 @@ package net.anei.cadpage.parsers.OR;
 
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.anei.cadpage.parsers.FieldProgramParser;
@@ -11,21 +10,21 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 
 public class ORJosephineCountyParser extends FieldProgramParser {
   
-  private static final Pattern LAT_LON_PTN = Pattern.compile("\\bLAT: *([-+]?[\\d\\.]+),? +LON: *([-+]?[\\d\\.]+)\\b");
-  private static final Pattern LAT_LON_PTN2 = Pattern.compile("\\bLAT:([-+]?[\\d\\.]+) LON:([-+]?[\\d\\.]+)\\b");
-  private static final Pattern DELIM = Pattern.compile("\n|: |(?<=[AP]M):");
-  
   private static final Pattern UNITS_PTN = Pattern.compile("Units: +");
   
   public ORJosephineCountyParser() {
     super("JOSEPHINE COUNTY", "OR",
-          "( ID CALL ADDRCITY/SXa PLACE X/Z? SRC DATETIME! UNIT | DATE_TIME_CALL ADDR_CITY_X/SXa! Units:UNIT | CALL ADDRCITY/SXa PLACE DATETIME ID! UNIT ) INFO+");
+          "( ID CALL ADDRCITY/SXa PLACE X/Z? SRC DATETIME! UNIT | DATETIME CALL ADDR_CITY_X/SXa! Units:UNIT | CALL ADDRCITY/SXa PLACE DATETIME ID! UNIT ) GPS1? GPS2? INFO/S+");
   }
   
   @Override
   public String getFilter() {
     return "Dispatch@Pacific.com";
   }
+  
+  private static final Pattern LAT_LON_PTN = Pattern.compile("\\bLAT: *([-+]?[\\d\\.]+),? +LON: *([-+]?[\\d\\.]+)\\b");
+  private static final Pattern LAT_LON_PTN2 = Pattern.compile("\\bLAT:([-+]?[\\d\\.]+) LON:([-+]?[\\d\\.]+)\\b");
+  private static final Pattern DELIM = Pattern.compile("\n|: |(?<=[AP]M)(:| {2,})");
 
   @Override
   protected boolean parseMsg(String subject, String body, Data data) {
@@ -37,52 +36,28 @@ public class ORJosephineCountyParser extends FieldProgramParser {
     if (! subject.trim().equals("!")) return false;
     body = LAT_LON_PTN.matcher(body).replaceAll("LAT:$1 LON:$2");
     body = UNITS_PTN.matcher(body).replaceFirst("Units:");
-    if (!parseFields(DELIM.split(body), data)) return false;
+    String[] flds = DELIM.split(body);
+    if (flds.length < 4) flds = body.split("  ");
+    if (!parseFields(flds, data)) return false;
     data.strAddress = LAT_LON_PTN2.matcher(data.strAddress).replaceFirst("LAT: $1, LON: $2");
     return true;
   }
   
+  private static final String GPS_PTN_STR = "[-+]?\\d{2,3}\\.\\d{6,}";
+
+  private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
+
   @Override
   public Field getField(String name) {
-    if (name.equals("DATE_TIME_CALL")) return new MyDateTimeCallField();
+    if (name.equals("DATETIME")) return new DateTimeField("\\d\\d?/\\d\\d?/\\d{4} +\\d\\d?:\\d\\d:\\d\\d? [AP]M", DATE_TIME_FMT, true);
     if (name.equals("ADDR_CITY_X")) return new MyAddressCityCrossField();
     if (name.equals("ADDRCITY")) return new MyAddressCityField();
+    if (name.equals("X")) return new MyCrossField();
     if (name.equals("SRC")) return new SourceField("\\d{4}(?:, *\\d{4})*");
-    if (name.equals("DATETIME")) return new MyDateTimeField();
     if (name.equals("ID")) return new IdField("\\d+|ODF", true);
-    if (name.equals("INFO")) return new MyInfoField();
+    if (name.equals("GPS1")) return new GPSField(1, GPS_PTN_STR, true);
+    if (name.equals("GPS2")) return new GPSField(2, GPS_PTN_STR, true);
     return super.getField(name);
-  }
-  
-  private static final Pattern DATE_TIME_PREFIX_PTN = Pattern.compile("^(\\d\\d?/\\d\\d?/\\d{4} +\\d\\d?:\\d\\d:\\d\\d? [AP]M) +");
-  private static final DateFormat DATE_TIME_FMT = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
-  private class MyDateTimeCallField extends CallField {
-    @Override
-    public boolean canFail() {
-      return true;
-    }
-    
-    @Override
-    public boolean checkParse(String field, Data data) {
-      
-      Matcher match = DATE_TIME_PREFIX_PTN.matcher(field);
-      if (!match.find()) return false;
-      
-      setTime(DATE_TIME_FMT, match.group(1), data);
-      field = field.substring(match.end());
-      super.parse(field, data);
-      return true;
-    }
-    
-    @Override
-    public void parse(String field, Data data) {
-      if (!checkParse(field, data)) abort();
-    }
-    
-    @Override
-    public String getFieldNames() {
-      return "DATE TIME CALL";
-    }
   }
   
   private class MyAddressCityCrossField extends AddressField {
@@ -91,7 +66,8 @@ public class ORJosephineCountyParser extends FieldProgramParser {
       Parser p = new Parser(field);
       super.parse(p.get(',').replace('@', '&'), data);
       data.strCity = p.get("  ");
-      data.strCross = p.get();
+      String cross = p.get();
+      if (!cross.equals("No Cross Streets Found")) data.strCross = cross;
     }
     
     @Override
@@ -109,23 +85,14 @@ public class ORJosephineCountyParser extends FieldProgramParser {
     }
   }
   
-  private class MyDateTimeField extends DateTimeField {
-    public MyDateTimeField() {
-      setPattern("\\d{1,2}/\\d{1,2}/\\d{4} \\d{1,2}:\\d{2}:\\d{2} [AP]M", true);
-    }
-    
+  private class MyCrossField extends CrossField {
     @Override
     public void parse(String field, Data data) {
-      setDateTime(DATE_TIME_FMT, field, data);
+      if (field.equals("No Cross Streets Found")) return;
+      super.parse(field, data);
     }
   }
   
-  private class MyInfoField extends InfoField {
-    @Override
-    public void parse(String field, Data data) {
-      data.strSupp = append(data.strSupp, " ", field);
-    }
-  }
 
   @Override
   public String adjustMapAddress(String address) {
