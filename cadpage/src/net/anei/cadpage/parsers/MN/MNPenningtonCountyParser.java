6ab524eddbee7b4cbd23d710f380ac2d83beb262
Ken Corbin <ken@cadpage.org>
2016-07-18 20:56:49 -0700

    Fixed parsing problem with Pennington County, MN

--- cadpage/src/net/anei/cadpage/parsers/MN/MNPenningtonCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MN/MNPenningtonCountyParser.java
@@ -1,28 +1,45 @@
 package net.anei.cadpage.parsers.MN;
 
-import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
 
 /**
  * Pennington County, MN
  */
 
-public class MNPenningtonCountyParser extends DispatchA27Parser {
+public class MNPenningtonCountyParser extends FieldProgramParser {
   
   public MNPenningtonCountyParser() {
-    super(CITY_LIST, "PENNINGTON COUNTY", "MN", "S\\d{4}");
+    super("PENNINGTON COUNTY", "MN", 
+          "CALL:CALL! PLACE:PLACE? ADDR:ADDR! CITY:CITY? ID:ID! PRI:PRI? GPS:GPS? INFO/N+");
   }
   
   @Override
-  public String getFilter() {
-    return "noreply@cisusa.org";
+  protected boolean parseMsg(String body, Data data) {
+    return parseFields(body.split(";"), data);
   }
-
-
-  private static final String[] CITY_LIST = new String[]{
-    "GOODRIDGE",
-    "RED LAKE FALLS",  
-    "ST HILAIRE",
-    "THIEF RIVER FALLS"
-  };
   
+  @Override
+  public Field getField(String name) {
+    if (name.equals("PRI")) return new MyPriorityField();
+    if (name.equals("GPS")) return new MyGPSField();
+    return super.getField(name);
+  }
+  
+  private class MyPriorityField extends PriorityField {
+    @Override
+    public void parse(String field, Data data) {
+      if (field.length() == 0) return;
+      super.parse(field.substring(0,1), data);
+    }
+  }
+  
+  private class MyGPSField extends GPSField {
+    @Override
+    public void parse(String field, Data data) {
+      field = stripFieldStart(field, "Latitude ");
+      field = field.replace(" Longitude ", ",");
+      super.parse(field, data);
+    }
+  }
 }
