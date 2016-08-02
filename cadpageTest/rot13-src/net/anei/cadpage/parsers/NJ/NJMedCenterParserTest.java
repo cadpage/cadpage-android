59fc3f85380c41744b6b25fd42b3306cf6d9dbcc
Ken Corbin <ken@cadpage.org>
2016-08-02 12:30:03 -0700

    Checking in MedCenter, NJ

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/NJ/NJMedCenterParserTest.java
@@ -0,0 +1,80 @@
+cnpxntr arg.narv.pnqcntr.cnefref.AW;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Zrq Pragre, AW
+
+Pbagnpg: Noenunz Rvfraoret <norrvfraoret@tznvy.pbz>
+Fraqre: pnq@ejwhu.rqh
+1F01 2016-0053804 Pvgl:Rnfg Oehafjvpx Nqqe:16 Pvivp Pragre Qe Oyt: Ncg:17 Pebff:Elqref Ya/Ehrf Ya Ceboyrz:Erfcvengbel-NYF Nffvta:16:55
+Pnapryyrq 2016-0053804 ERPRVIRQ: 16:54 NFFVTARQ: 16:55 RAEBHGR: 16:56 NEEVIRQ: NINVY: 17:07 Pnapryyrq Raebhgr
+1F01 2016-0053797 Pvgl:Senaxyva (Fbzrefrg) Nqqe:14 Senaxyva Pg Oyt: Ncg:20 NEOBEF Pebff:Senaxyva Oyiq/Qrnq Raq Ceboyrz:Bgure Vyyarff-Zrqvpny-NYF Nffvta:16:24
+1F01 2016-0053769Pvgl:Enujnl Nqqe:865 Fgbar Fg Oyt: Ncg:pngu yEJWHU @ Enujnl Pebff:Puneybggr Cy/Guvemn Cy Ceboyrz:Pevgvpny Pner Genafcbeg Nffvta:15:07
+2016-0053790 ERPRVIRQ: 16:16 NFFVTARQ: 16:17 RAEBHGR: 16:18 NEEVIRQ: NINVY: 16:19 Pnapryyrq Raebhgr
+
+*/
+
+choyvp pynff AWZrqPragreCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp AWZrqPragreCnefreGrfg() {
+    frgCnefre(arj AWZrqPragreCnefre(), "", "AW");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgNoenunzRvfraoret() {
+
+    qbGrfg("G1",
+        "1F01 2016-0053804 Pvgl:Rnfg Oehafjvpx Nqqe:16 Pvivp Pragre Qe Oyt: Ncg:17 Pebff:Elqref Ya/Ehrf Ya Ceboyrz:Erfcvengbel-NYF Nffvta:16:55",
+        "HAVG:1F01",
+        "VQ:2016-0053804",
+        "PVGL:Rnfg Oehafjvpx",
+        "NQQE:16 Pvivp Pragre Qe",
+        "NCG:17",
+        "K:Elqref Ya/Ehrf Ya",
+        "PNYY:Erfcvengbel-NYF",
+        "GVZR:16:55");
+
+    qbGrfg("G2",
+        "Pnapryyrq 2016-0053804 ERPRVIRQ: 16:54 NFFVTARQ: 16:55 RAEBHGR: 16:56 NEEVIRQ: NINVY: 17:07 Pnapryyrq Raebhgr",
+        "GLCR:EHA_ERCBEG",
+        "VQ:2016-0053804",
+        "VASB:ERPRVIRQ: 16:54\aNFFVTARQ: 16:55\aRAEBHGR: 16:56\aNEEVIRQ:\aNINVY: 17:07\aPnapryyrq Raebhgr");
+
+    qbGrfg("G3",
+        "1F01 2016-0053797 Pvgl:Senaxyva (Fbzrefrg) Nqqe:14 Senaxyva Pg Oyt: Ncg:20 NEOBEF Pebff:Senaxyva Oyiq/Qrnq Raq Ceboyrz:Bgure Vyyarff-Zrqvpny-NYF Nffvta:16:24",
+        "HAVG:1F01",
+        "VQ:2016-0053797",
+        "PVGL:Senaxyva (Fbzrefrg)",
+        "ZPVGL:Fbzrefrg",
+        "NQQE:14 Senaxyva Pg",
+        "NCG:20 NEOBEF",
+        "K:Senaxyva Oyiq/Qrnq Raq",
+        "PNYY:Bgure Vyyarff-Zrqvpny-NYF",
+        "GVZR:16:24");
+
+    qbGrfg("G4",
+        "1F01 2016-0053769Pvgl:Enujnl Nqqe:865 Fgbar Fg Oyt: Ncg:pngu yEJWHU @ Enujnl Pebff:Puneybggr Cy/Guvemn Cy Ceboyrz:Pevgvpny Pner Genafcbeg Nffvta:15:07",
+        "HAVG:1F01",
+        "VQ:2016-0053769",
+        "PVGL:Enujnl",
+        "NQQE:865 Fgbar Fg",
+        "NCG:pngu yEJWHU @ Enujnl",
+        "K:Puneybggr Cy/Guvemn Cy",
+        "PNYY:Pevgvpny Pner Genafcbeg",
+        "GVZR:15:07");
+
+    qbGrfg("G5",
+        "2016-0053790 ERPRVIRQ: 16:16 NFFVTARQ: 16:17 RAEBHGR: 16:18 NEEVIRQ: NINVY: 16:19 Pnapryyrq Raebhgr",
+        "GLCR:EHA_ERCBEG",
+        "VQ:2016-0053790",
+        "VASB:ERPRVIRQ: 16:16\aNFFVTARQ: 16:17\aRAEBHGR: 16:18\aNEEVIRQ:\aNINVY: 16:19\aPnapryyrq Raebhgr");
+    
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj AWZrqPragreCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
