81753af2834b5116434552e0e72687bd51f53afa
Jamie Harper <snailtk@gmail.com>
2016-08-11 15:55:00 -0700

    addr checks,skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/AL/ALMontgomeryCountyParserTest.java
@@ -0,0 +1,54 @@
+cnpxntr arg.narv.pnqcntr.cnefref.NY;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+vzcbeg arg.narv.pnqcntr.cnefref.NY.NYZbagtbzrelPbhaglCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Cvxr Ebnq SQ
+Ybpngvba: Cvxr Ebnq, NY, Havgrq Fgngrf
+Fraqre: <zpfb_r911@zp-nyn.bet>
+Fraqre: Zngg Zvffvyqvar <ZZvffvyqvar@cvxrebnqsver.pbz>
+
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 324 ORNA YA FRPGBE R 4, CVXR EBNQ, NY 36064
+{Qvfcngpu} ZPFB Qvfcngpu: G50 - NPPVQRAG @ inhtua eq wj jnyynungpurr
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 1444 HF UJL 82
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 4502 CVXR EQ
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 609 ORYFRE PG
+{Qvfcngpu} ZPFB Qvfcngpu: G70 - SVER  @ 100   PNCEV QE
+{Qvfcngpu} ZPFB Qvfcngpu: 6E - NYNEZ - ERFVQRAPR @ 221 YNXR FUBER  QE
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 2464 zneyre eq
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 
+{Qvfcngpu} ZPFB Qvfcngpu: G70 - SVER  @ ZBFR YRR EQ
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 663   ONEARF EQ
+{Qvfcngpu} ZPFB Qvfcngpu: G50 - NPPVQRAG @ ZNEYRE  EQ NG INHTUA EQ
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 494 UBARLFHPXYR YA
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 1027 PNAGL YNAR
+{Qvfcngpu} ZPFB Qvfcngpu: G70 - SVER  @ 835 ZREEL YNXR QE CVXR EBNQ
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 825 PUNFRJNL  QE
+{Qvfcngpu} ZPFB Qvfcngpu: G70 - SVER  @ 9512 PERFPRAG YBQTR  QE
+{Qvfcngpu} ZPFB Qvfcngpu: G50 - NPPVQRAG @ KK CVXR EQ/INHTUA EQ, CVXR EBNQ, NY 
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ KK UJL 110/BHGRE YBBC, CVXR EBNQ, NY 
+{Qvfcngpu} ZPFB Qvfcngpu: G50 - NPPVQRAG @ 5040 BYQ CVXR GENPR, CVXR EBNQ, NY 
+{Qvfcngpu} ZPFB Qvfcngpu: G52 - NZOHYNAPR ARRQRQ @ 201 YNXR YA, ZBAGTBZREL, NY 
+*/
+
+choyvp pynff NYZbagtbzrelPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp NYZbagtbzrelPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "ZBAGTBZREL PBHAGL", "NY");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj NYZbagtbzrelPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/KY/KYAllenCountyParserTest.java
@@ -0,0 +1,60 @@
+cnpxntr arg.narv.pnqcntr.cnefref.XL;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Fpbggfivyyr Nyyra Pb Erfphr Fdhnq
+Ybpngvba: Fpbggfivyyr, XL, Havgrq Fgngrf
+Fraqre: nyyrapbxl911@911rznvy.arg
+
+{R911 Qbphzrag: 99941985} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941985\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911 Qbphzrag: 99941931} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941931\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a721 ARJ TNYYNGVA EQ\aMBAR 1 SVER NYNEZ
+{R911} NPP JVGU VAWHEVRF\a2400 ONEERA EVIRE QNZ EQ\aQHZC GEHPX SYVCCRQ, 1 RAGENCCRQ
+{R911} NPP JVGU VAWHEVRF\a2400 ONEERA EVIRE QNZ EQ
+{R911 Qbphzrag: 99941767} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941767\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a71 ZBGYRL YA
+{R911 Qbphzrag: 99941541} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941541\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} NPP JVGU VAWHEVRF\a10215 FZVGUF TEBIR EQ
+{R911 Qbphzrag: 99941351} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941351\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} JNGRE ERFPHR\a20 OEBJAF SBEQ OBNG ENZC EQ\aOBNG OEBXR QBJA ORGJRRA OEBJAFSBEQ & JNYAHG PERRX BA ONEERA EVIRE
+{R911 Qbphzrag: 99941314} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941314\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a1333 YNSNLRGGR EQ, FPBGGFIVYYR\aUBHFR BA SVER / SHYY BS FZBXR
+{R911 Qbphzrag: 99941202} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99941202\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a1222 PNEGRE PUHEPU EQ\aFGEHPGHER SVER / RIRELBAR BHG BS UBZR NF BS EVTUG ABJ
+{R911} SVER\a1222 PNEGRE PUHEPU EQ\aFGEHPGHER SVER / RIRELBAR BHG BS UBZR NF
+{R911 Qbphzrag: 99940958} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940958\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911 Qbphzrag: 99940951} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940951\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a100 PYVSGBA PUHEPU EQ\aIRU BA SVER
+{R911 Qbphzrag: 99940885} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940885\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} NZO\a149 ZVFGL EBJ EQ\a35 LE BYQ ZNYR / FUBG UVZFRYS VA UNAQ JUVYR PYRNAVAT UVF THA
+{R911} NZO\a567 JNFUVATGBA FPUBBY EQ\a19 L/B S CHG JEVFG GUEBHTU JVAQBJ/OYRRQVAT GUEBHTU GBJRY
+{R911 Qbphzrag: 99940723} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940723\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911 Qbphzrag: 99940715} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940715\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a1501 OHEAYRL EQ\aTRARENY SVER NYZ
+{R911 Qbphzrag: 99940662} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940662\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a2988 BYQ UNEGFIVYYR EQ, FPBGGFIVYYR
+{R911 Qbphzrag: 99940479} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940479\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911 Qbphzrag: 99940426} Gur nggnpurq qbphzrag pbagnvaf vasbezngvba sbe gur sbyybjvat vapvqragf: \a\a99940426\a\aCyrnfr frr nggnpurq qbphzrag.
+{R911} SVER\a192 J JBBQ FG\aGRFG SBE NPGVIR 911 BAYL *** GRFG BAYL**
+*/
+
+choyvp pynff XLNyyraPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp XLNyyraPbhaglCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "NYYRA PBHAGL", "XL");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+ 
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj XLNyyraPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
