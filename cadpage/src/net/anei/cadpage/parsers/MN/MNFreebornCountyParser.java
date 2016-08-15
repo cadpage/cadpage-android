1658e27a3172ed7d2d4aaeafd5cdd3acfc3d0df1
Ken Corbin <ken@cadpage.org>
2016-08-15 07:46:21 -0700

    Fixed parsing problem with Freeborn County, MN

--- cadpage/src/net/anei/cadpage/parsers/MN/MNFreebornCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MN/MNFreebornCountyParser.java
@@ -6,7 +6,7 @@ import net.anei.cadpage.parsers.dispatch.DispatchA27Parser;
 public class MNFreebornCountyParser extends DispatchA27Parser {
   
   public MNFreebornCountyParser() {
-    super("FREEBORN COUNTY", "MN", "\\d{8}");
+    super("FREEBORN COUNTY", "MN", "\\d{3,4}[A-Z]{1,3}|[A-Z]+\\d+");
   }
   
   @Override
