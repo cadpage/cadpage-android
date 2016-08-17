7ad5289f14c6b76aab3fdfa6dd76ce221e6724e7
Ken Corbin <ken@cadpage.org>
2016-08-16 17:01:07 -0700

    Fixed parsing problem with Kalamazoo County, MI

--- cadpage/src/net/anei/cadpage/parsers/MI/MIKalamazooCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/MI/MIKalamazooCountyParser.java
@@ -17,10 +17,29 @@ public class MIKalamazooCountyParser extends DispatchA50Parser {
   }
   
   private static final Properties CITY_CODES = buildCodeTable(new String[]{
-      "COMS",  "COMSTOCK",
+      "ALAM",  "ALAMO TWP",
+      "AUGU",  "AUGUSTA",
+      "BRAD",  "BRADY TWP",
+      "CHAR",  "CHARLESTON TWP",
+      "CLXT",  "CLIMAX TWP",
+      "CLXV",  "CLIMAX",
+      "COMS",  "COMSTOCK TWP",
+      "COOP",  "COOPER TWP",
+      "GALE",  "GALESBURG",
+      "KALC",  "KALAMAZOO",
+      "KALT",  "KALAMAZOO TWP",
       "OSHT",  "OSHTEMO TWP",
+      "PARC",  "PARCHMENT",
+      "PAVI",  "PAVILION TWP",
       "PORT",  "PORTAGE",
-      "TEXA",  "TEXAS TWP"
-
+      "PRRO",  "PRAIRIE RONDE TWP",
+      "RICT",  "RICHLAND TWP",
+      "RICV",  "RICHLAND",
+      "ROSS",  "ROSS TWP",
+      "SCHT",  "SCHOOLCRAFT TWP",
+      "SCHV",  "SCHOOLCRAFT",
+      "TEXA",  "TEXAS TWP",
+      "VICK",  "VICKSBURG",
+      "WAKE",  "WAKESHMA TWP"
   });
 }
