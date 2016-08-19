de2595bab8888f907bcd45514b7a079886aa4dcf
Jamie Harper <snailtk@gmail.com>
2016-08-18 17:31:27 -0700

    skeletons

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/MO/MOCentraliaParserTest.java
@@ -0,0 +1,51 @@
+cnpxntr arg.narv.pnqcntr.cnefref.ZB;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+
+/*
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Pragenyvn SQ
+Ybpngvba: Pragenyvn, ZB, Havgrq Fgngrf
+Fraqre: RagrecbyNyregf@pragenyvncq.bet
+
+{ARJBPP #16-390  RZF - RZF Nffvfg} ARJBPP #16-390  RZF - RZF Nffvfg\aNqqerff: 404 Wraxvaf Fg F, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: 41 lrne byq srznyr purfg cnvaf; unf unq erprag oenva fhethel
+{ARJBPP #16-389  RZF - RZF Nffvfg} ARJBPP #16-389  RZF - RZF Nffvfg\aNqqerff: 311 Ebyyvaf Fg F, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: puneyvr erfcbafr frvmher
+{ARJBPP #16-388  RZF - RZF Nffvfg} ARJBPP #16-388  RZF - RZF Nffvfg\aNqqerff: Pragenyvn Ncnegzragf - 525 Fbhgutngr Fg J (8), Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Puneyvr erfcbapr, frmvher
+{ARJBPP #16-387  RZF - RZF Nffvfg} ARJBPP #16-387  RZF - RZF Nffvfg\aNqqerff: Urevgntr Unyy Ahefvat Ubzr - 750 Ujl 22 R, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Puneyvr erfcbapr, rzretrapl genafsre.
+{ARJBPP #16-386  RfpS - Rfpbeg - Shareny} ARJBPP #16-386  RfpS - Rfpbeg - Shareny\aNqqerff: 121 Wrssrefba Fg A, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: oenib erfcbafr, haxabja ceboyrz
+{ARJBPP #16-385  RZF - RZF Nffvfg} ARJBPP #16-385  RZF - RZF Nffvfg\aNqqerff: 404 Pbyyvre Fg F, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+{ARJBPP #16-384  RZF - RZF Nffvfg} ARJBPP #16-384  RZF - RZF Nffvfg\aNqqerff: 611 Ebqarl Tevssva Fg J, Pragenyvn\aErcbegvat Crefba: Wrffvr Jvyyvnzf, Cubar: \aQrgnvy: 75 le byq jbzna pna'g frg hc pnag gnyx
+{ARJBPP #16-383  RZF - RZF Nffvfg} ARJBPP #16-383  RZF - RZF Nffvfg\aNqqerff: Urevgntr Unyy Ahefvat Ubzr - 750 Ujl 22 R, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Rzretrapl genafsre puneyvr erfcbafr
+{ARJBPP #16-382  RZF - RZF Nffvfg} ARJBPP #16-382  RZF - RZF Nffvfg\aNqqerff: 525 Fbhgutngr Fg R (7), Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Urnqnpur puneyvr erfcbafr
+{ARJBPP #16-381  RZF - RZF Nffvfg} ARJBPP #16-381  RZF - RZF Nffvfg\aNqqerff: 525 Fbhgutngr Fg R (15), Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy:
+{ARJBPP #16-380  RZF - RZF Nffvfg} ARJBPP #16-380  RZF - RZF Nffvfg\aNqqerff: 720 Prageny Fg F, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: cbffvoyr vagragvbany bireqbfr; srznyr
+{ARJBPP #16-379  RZF - RZF Nffvfg} ARJBPP #16-379  RZF - RZF Nffvfg\aNqqerff: Fhojnl - 1239 Ujl 22 R, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: 18 lrne byq znyr phg uvf svatre; ybbxf yvxr ur vf tbvat gb snag
+{ARJBPP #16-378  RZF - RZF Nffvfg} ARJBPP #16-378  RZF - RZF Nffvfg\aNqqerff: 1332 Elna Qe, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: fvpx crefba
+{ARJBPP #16-377  RZF - RZF Nffvfg} ARJBPP #16-377  RZF - RZF Nffvfg\aNqqerff: Oevfgby Znabe - 610 Wrssrefba Fg A, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: 1100 bafprar oevfgby znabe pbzznaq
+{ARJBPP #16-376  RZF - RZF Nffvfg} ARJBPP #16-376  RZF - RZF Nffvfg\aNqqerff: Tnab  Punapr Eq R naq Wrssrefba Fg F - Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Iruvpyr  Pbyyvfvba  haxabja vawhevrf
+{ARJBPP #16-375  RZF - RZF Nffvfg} ARJBPP #16-375  RZF - RZF Nffvfg\aNqqerff: Urevgntr Unyy Ahefvat Ubzr - 750 Ujl 22 R, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: Rzretrapl Genafsre
+{ARJBPP #16-374  RZF - RZF Nffvfg} ARJBPP #16-374  RZF - RZF Nffvfg\aNqqerff: Fbhgutngr Fg R naq Uvpxzna Fg A - Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: iruvpyr pbyyvfvba jvgu syhvq yrnx
+{ARJBPP #16-373  RZF - RZF Nffvfg} ARJBPP #16-373  RZF - RZF Nffvfg\aNqqerff: 329 Pbyhzovn Fg A, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: zrqvpny erfcbafr, fvpx crefba
+{ARJBPP #16-372  RZF - RZF Nffvfg} ARJBPP #16-372  RZF - RZF Nffvfg\aNqqerff: Pragenyvn CQ - 114 Ebyyvaf Fg F, Pragenyvn\aErcbegvat Crefba: , Cubar: \aQrgnvy: GRFG BAYL QB ABG ERFCBAQ
+
+*/
+
+choyvp pynff ZBPragenyvnCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp ZBPragenyvnCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "", "ZB");
+  }
+
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+  }
+
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj ZBPragenyvnCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
