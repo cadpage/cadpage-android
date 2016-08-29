ff8bac48f06b98525747baffa8ab4a8ab4baa46b
Ken Corbin <ken@cadpage.org>
2016-08-29 14:12:22 -0700

    Fixed parsing problem with Passaic County, TX

--- cadpage/src/net/anei/cadpage/parsers/NJ/NJPassaicCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NJ/NJPassaicCountyParser.java
@@ -8,20 +8,34 @@ import net.anei.cadpage.parsers.MsgInfo.Data;
 public class NJPassaicCountyParser extends FieldProgramParser {
 
   public NJPassaicCountyParser() {
-    super("PASSAIC COUNTY", "NJ", "ID! DATETIME! CALL! ADDRCITY! INFO+");
+    super("PASSAIC COUNTY", "NJ", 
+          "CAD#:ID! Time:TIME! Address:ADDR! Cross:X! INFO/N+");
   }
   
-  @Override protected boolean parseMsg(String subject, String body, Data data) {
-    boolean success = parseFields(body.split("\n"), data);
-    if (subject.length() > 0 && success) data.strCall = append(data.strCall, " / ", subject);
-    return success;
+  @Override
+  public String getFilter() {
+    return "FireDispatch@cityofpassaicnj.gov";
+  }
+  
+  @Override protected boolean parseMsg(String body, Data data) {
+    body = stripFieldStart(body, "Fire Page / ");
+    return parseFields(body.split("\n"), data);
   }
   
   @Override
   public Field getField(String name) {
-    if (name.equals("ID")) return new IdField("\\d{2}-\\d{5}");
-    if (name.equals("DATETIME")) return new DateTimeField(new SimpleDateFormat("M/d/yyyy h:mm:ss a"));
+    if (name.equals("TIME")) return new DateTimeField(new SimpleDateFormat("hh:mm:ss aa"), true);
+    if (name.equals("X")) return new MyCrossField();
     return super.getField(name);
   }
- 
+  
+  private class MyCrossField extends CrossField {
+    @Override
+    public void parse(String field, Data data) {
+      field = field.replace(" - ", " / ");
+      field = stripFieldStart(field, "-");
+      field = stripFieldEnd(field, "-");
+      super.parse(field, data);
+    }
+  }
 }
