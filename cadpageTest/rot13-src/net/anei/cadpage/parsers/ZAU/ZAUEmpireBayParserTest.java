6c7dcd8e062a0fea43c924002aa0262c531c10c8
Jamie Harper <snailtk@gmail.com>
2016-08-29 21:08:41 -0700

    skeletons and parsers

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/ZAU/ZAUEmpireBayParserTest.java
@@ -0,0 +1,58 @@
+cnpxntr arg.narv.pnqcntr.cnefref.MNH;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Rzcver Onl, Nhfgenyvn
+
+Pbagnpg: Npgvir911
+Ntrapl anzr: Rzcver Onl ESF
+Ybpngvba: Rzcver Onl, A.F.J, Nhfgenyvn
+Fraqre: cntre@cntregbfzf.pbz
+Fraqre: xlyrqrj <xlyrqrj@tznvy.pbz>
+Fraqre: Xlyr Qrj <xlyrqrj@tznvy.pbz>
+Fraqre: ESF CNTRE <cntre@cntregbfzf.pbz>
+Fraqre: CntreGbFZF Grnz <sbejneqvat-abercyl@tbbtyr.pbz>
+
+{04:36:03 30-08-16 30 Nhthfg 2016 04:35:56  **TBFSBEQ SVER PNYY** ON PERJF ERDHVERQ - RZCVER ONL SVER PNYY} 04:36:03 30-08-16 30 Nhthfg 2016 04:35:56  **TBFSBEQ SVER PNYY** ON PERJF ERDHVERQ - RZCVER ONL SVER PNYY 
+{06:14:21 28-08-16 28 Nhthfg 2016 06:14:15  **TBFSBEQ SVER PNYY** ON PERJF ERDHVERQ  6:13:59  PNQ145683 - RZCVER ONL SVER PNYY} 06:14:21 28-08-16 28 Nhthfg 2016 06:14:15  **TBFSBEQ SVER PNYY** ON PERJF ERDHVERQ  6:13:59  PNQ145683 - RZCVER ONL SVER PNYY 
+{Rzcvee Onl SVER PNYY} \aOHFU NYVTUG - 22 furyyl ornpu ebnq
+{sverpnyy GRFG} \aOhfu nyvtug grfg - 10 furyyl ornpu ebnq, rzcver onl
+{18:37:12 22-08-16 22 Nhthfg 2016 18:36:54  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 18:37:12 22-08-16 22 Nhthfg 2016 18:36:54  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{18:37:00 22-08-16 22 Nhthfg 2016 18:36:54  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 18:37:00 22-08-16 22 Nhthfg 2016 18:36:54  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{18:41:34 21-08-16 21 Nhthfg 2016 18:41:28  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 18:41:34 21-08-16 21 Nhthfg 2016 18:41:28  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{08:40:27 21-08-16 21 Nhthfg 2016 08:40:21  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 08:40:27 21-08-16 21 Nhthfg 2016 08:40:21  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{20:18:27 20-08-16 20 Nhthfg 2016 20:18:20  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 20:18:27 20-08-16 20 Nhthfg 2016 20:18:20  **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{15:13:59 20-08-16 20 Nhthfg 2016 15:13:55 **QVFERTNEQ SVER PNYY-AB YBATRE ERDHVERQ** - RZCVER ONL SVER PNYY} 15:13:59 20-08-16 20 Nhthfg 2016 15:13:55 **QVFERTNEQ SVER PNYY-AB YBATRE ERDHVERQ** - RZCVER ONL SVER PNYY 
+{15:08:53 20-08-16 20 Nhthfg 2016 15:08:49 **TBFSBEQ SVER PNYY** 3:08:46 CZ - RZCVER ONL SVER PNYY} 15:08:53 20-08-16 20 Nhthfg 2016 15:08:49 **TBFSBEQ SVER PNYY** 3:08:46 CZ - RZCVER ONL SVER PNYY 
+{08:21:10 18-08-16 18 Nhthfg 2016 08:21:06 **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 08:21:10 18-08-16 18 Nhthfg 2016 08:21:06 **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{08:13:13 18-08-16 18 Nhthfg 2016 08:13:09 **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY} 08:13:13 18-08-16 18 Nhthfg 2016 08:13:09 **TBFSBEQ SVER PNYY** - RZCVER ONL SVER PNYY 
+{***TBFSBEQ SVER PNYY***} TNENTR NYVTUG - 12 jbl jbl ebnq jbl jbl
+{SVER PNYY} OHFU NYVTUG - 22 furyyl ornpu ebnq
+{Sver pnyy 2} Ohfu nyvtug - 11 furyyl ornpu ebnq rzcver onl
+{sverpnyy} ohfu nyvtug grfg - 10 furyyl ornpu ebnq, rzcver onl
+{14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F} 40 xraqnyy ebnq\arzcver onl\aa.f.j
+{14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F} 46 xraqnyy ebnq rzcver onl afj
+{14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F} 48 xraqnyy ebnq, rzcver onl, afj
+{14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F} 14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F\a50 xraqnyy ebnq, rzcver onl, a.f.j
+{14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F} 14:42:45 14-08-15 **TBFSBEQ SVER PNYY** - RZCVER ONL C2F
+{(#705342877) CntreGbFZF Sbejneqvat Pbasvezngvba - Erprvir Znvy sebz cntre@cntregbfzf.pbz} cntre@cntregbfzf.pbz unf erdhrfgrq gb nhgbzngvpnyyl sbejneq znvy gb lbhe rznvy\anqqerff 18412-MwOZWC2clURr2kgJ@nyreg.npgvir911.pbz.\aPbasvezngvba pbqr: 705342877\a\aGb nyybj cntre@cntregbfzf.pbz gb nhgbzngvpnyyl sbejneq znvy gb lbhe nqqerff,\acyrnfr pyvpx gur yvax orybj gb pbasvez gur erdhrfg:\a\auggcf://znvy-frggvatf.tbbtyr.pbz/znvy/is-%5ONATwqW83xyTgL3eKYHn_axD_DK_bjvKs6Z0MExTEZoCCouTwip9dKx066DQNMlIHRuLeBMSQLQfojlwoXLgAXd5E6I2N-9pf3xymwxPdY0vRnVYa9On3rOXjRKXnu2H%5Q--rRUEfkkoNrUt6ZOqE91laVI4tV\a\aVs lbh pyvpx gur yvax naq vg nccrnef gb or oebxra, cyrnfr pbcl naq cnfgr vg\avagb n arj oebjfre jvaqbj. Vs lbh nera'g noyr gb npprff gur yvax, lbh\apna fraq gur pbasvezngvba pbqr\a705342877 gb cntre@cntregbfzf.pbz.\a\aGunaxf sbe hfvat CntreGbFZF!\a\aFvapreryl,\a\aGur CntreGbFZF Grnz\a\aVs lbh qb abg nccebir bs guvf erdhrfg, ab shegure npgvba vf erdhverq.\acntre@cntregbfzf.pbz pnaabg nhgbzngvpnyyl sbejneq zrffntrf gb lbhe rznvy nqqerff\ahayrff lbh pbasvez gur erdhrfg ol pyvpxvat gur yvax nobir. Vs lbh nppvqragnyyl\apyvpxrq gur yvax, ohg lbh qb abg jnag gb nyybj cntre@cntregbfzf.pbz gb\anhgbzngvpnyyl sbejneq zrffntrf gb lbhe nqqerff, pyvpx guvf yvax gb pnapry guvf\airevsvpngvba:\auggcf://znvy-frggvatf.tbbtyr.pbz/znvy/hs-%5ONATwqW8imFJwZQtEVPFkLXkWU-ar4MK1gOUE8fmBR7990eWmVqbysIV16e2aDQL9-ORNEKaumVCQVWCFi3HlZ0_I5lTcTdpodn5fwRy_-Uvy4fQJMR4BtiiOl7Tf8WL%5Q--rRUEfkkoNrUt6ZOqE91laVI4tV\a\aGb yrnea zber nobhg jul lbh zvtug unir erprvirq guvf zrffntr, cyrnfr\aivfvg: uggc://fhccbeg.tbbtyr.pbz/znvy/ova/nafjre.cl?nafjre=184973.\a\aCyrnfr qb abg erfcbaq gb guvf zrffntr. Vs lbh'q yvxr gb pbagnpg gur\aTbbtyr.pbz Grnz, cyrnfr ybt va gb lbhe nppbhag naq pyvpx 'Uryc' ng\agur gbc bs nal cntr. Gura, pyvpx 'Pbagnpg Hf' nybat gur obggbz bs gur\aUryc Pragre.
+*/
+
+choyvp pynff MNHRzcverOnlCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp MNHRzcverOnlCnefreGrfg() {
+    frgCnefre(arj QhzzlCnefre(), "", "MNH");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+ 
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj MNHRzcverOnlCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
