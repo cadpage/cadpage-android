9dafba7203784978cf71b5c02cf67380ea0012b0
Ken Corbin <ken@cadpage.org>
2016-10-12 18:40:02 -0700

    Fixed parsing problem with Chaves County, NM

--- cadpage/src/net/anei/cadpage/parsers/NM/NMChavesCountyParser.java
+++ cadpage/src/net/anei/cadpage/parsers/NM/NMChavesCountyParser.java
@@ -1,5 +1,6 @@
 package net.anei.cadpage.parsers.NM;
 
+import net.anei.cadpage.parsers.MsgInfo.Data;
 import net.anei.cadpage.parsers.dispatch.DispatchOSSIParser;
 
 
@@ -13,6 +14,12 @@ public class NMChavesCountyParser extends DispatchOSSIParser {
 
   @Override
   public String getFilter() {
-    return "CAD@roswellpolice.com";
+    return "CAD@roswellpolice.com,Cad@roswell-nm.gov";
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    if (body.startsWith("FYI:")) body = "CAD:" + body;
+    return super.parseMsg(body, data);
   }
 }
