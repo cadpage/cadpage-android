a03d8768e4bf9fe4310c8de6a4415ecc315c5130
Ken Corbin <ken@cadpage.org>
2016-10-14 12:39:41 -0700

    Checking in Fairfax County, VA (B)

---
+++ cadpageTest/rot13-src/net/anei/cadpage/parsers/VA/VAFairfaxCountyBParserTest.java
@@ -0,0 +1,80 @@
+cnpxntr arg.narv.pnqcntr.cnefref.IN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/* 
+Snvesnk Pbhagl, IN
+
+Pbagnpg: Fgevpxynaq, Erk <Erk.Fgevpxynaq@snvesnkpbhagl.tbi>
+Fraqre: flf.pnqcntre@snvesnkpbhagl.tbi
+
+(PNQ Vasbezngvba) YBPNGVBA: 3300 TNYYBJF EQ SYPU: @SNVESNK UBFCVGNY   RIRAG GLCR: SUBFC   RIRAG #: R162872384   SVER OBK: 43023   GNYXTEBHC: 4Q
+(PNQ Vasbezngvba) YBPNGVBA: 13803 PBHAGEL PEBFFVAT FG PUNA   RIRAG GLCR: SUBH   RIRAG #: R162880833   SVER OBK: 41501   GNYXTEBHC: 4O
+(PNQ Vasbezngvba) YBPNGVBA: 6301 FGRIRAFBA NIR NYRK PVGL   RIRAG GLCR: SOYQT   RIRAG #: R162880517   SVER OBK: 25893   GNYXTEBHC: 2Q
+(PNQ Vasbezngvba) YBPNGVBA: 7128 NEYVATGBA OYIQ SYPU   RIRAG GLCR: SUBH   RIRAG #: R162880924   SVER OBK: 41800   GNYXTEBHC: 4U  Qvfc: R418,R106Z,G10
+(PNQ Vasbezngvba) YBPNGVBA: 6301 Fgrirafba Nir:   RIRAG GLCR: SOYQT   RIRAG #: R162880517   SVER OBK:    GNYXTEBHC: 4O
+
+*/
+
+choyvp pynff INSnvesnkPbhaglOCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp INSnvesnkPbhaglOCnefreGrfg() {
+    frgCnefre(arj INSnvesnkPbhaglOCnefre(), "SNVESNK PBHAGL", "IN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgErkFgevpxynaq() {
+
+    qbGrfg("G1",
+        "(PNQ Vasbezngvba) YBPNGVBA: 3300 TNYYBJF EQ SYPU: @SNVESNK UBFCVGNY   RIRAG GLCR: SUBFC   RIRAG #: R162872384   SVER OBK: 43023   GNYXTEBHC: 4Q",
+        "NQQE:3300 TNYYBJF EQ",
+        "CYNPR:SNVESNK UBFCVGNY",
+        "PVGL:SNYYF PUHEPU",
+        "PNYY:SUBFC",
+        "VQ:R162872384",
+        "OBK:43023",
+        "PU:4Q");
+
+    qbGrfg("G2",
+        "(PNQ Vasbezngvba) YBPNGVBA: 13803 PBHAGEL PEBFFVAT FG PUNA   RIRAG GLCR: SUBH   RIRAG #: R162880833   SVER OBK: 41501   GNYXTEBHC: 4O",
+        "NQQE:13803 PBHAGEL PEBFFVAT FG",
+        "PVGL:PUNAGVYL",
+        "PNYY:SUBH",
+        "VQ:R162880833",
+        "OBK:41501",
+        "PU:4O");
+
+    qbGrfg("G3",
+        "(PNQ Vasbezngvba) YBPNGVBA: 6301 FGRIRAFBA NIR NYRK PVGL   RIRAG GLCR: SOYQT   RIRAG #: R162880517   SVER OBK: 25893   GNYXTEBHC: 2Q",
+        "NQQE:6301 FGRIRAFBA NIR",
+        "PVGL:NYRKNAQRE",
+        "PNYY:SOYQT",
+        "VQ:R162880517",
+        "OBK:25893",
+        "PU:2Q");
+
+    qbGrfg("G4",
+        "(PNQ Vasbezngvba) YBPNGVBA: 7128 NEYVATGBA OYIQ SYPU   RIRAG GLCR: SUBH   RIRAG #: R162880924   SVER OBK: 41800   GNYXTEBHC: 4U  Qvfc: R418,R106Z,G10",
+        "NQQE:7128 NEYVATGBA OYIQ",
+        "PVGL:SNYYF PUHEPU",
+        "PNYY:SUBH",
+        "VQ:R162880924",
+        "OBK:41800",
+        "PU:4U",
+        "HAVG:R418,R106Z,G10");
+
+    qbGrfg("G5",
+        "(PNQ Vasbezngvba) YBPNGVBA: 6301 Fgrirafba Nir:   RIRAG GLCR: SOYQT   RIRAG #: R162880517   SVER OBK:    GNYXTEBHC: 4O",
+        "NQQE:6301 Fgrirafba Nir",
+        "PNYY:SOYQT",
+        "VQ:R162880517",
+        "PU:4O");
+
+  }
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj INSnvesnkPbhaglOCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
